970c0302760ab5d2b6d09e16169dd6eaf1572dc9
Haralan Dobrev <hkdobrev@gmail.com>
2016-02-17 01:10:04 +0200

    Add releases tab to every repo page

--- extension/content.js
+++ extension/content.js
@@ -3,8 +3,10 @@
 const path = location.pathname;
 const isDashboard = path === '/';
 const isRepo = /^\/[^/]+\/[^/]+/.test(path);
+const vendorName = path.split('/')[1];
 const repoName = path.split('/')[2];
 const isPR = () => /^\/[^/]+\/[^/]+\/pull\/\d+$/.test(location.pathname);
+const isReleases = () => isRepo && /^\/[^/]+\/[^/]+\/releases/.test(location.pathname);
 const getUsername = () => $('meta[name="user-login"]').attr('content');
 
 function linkifyBranchRefs() {
@@ -24,6 +26,28 @@ function linkifyBranchRefs() {
 
 		$(el).wrap(`<a href="https://github.com/${username}/${branchRepo}/tree/${branch}">`);
 	});
+}
+
+function addReleasesTab() {
+	const releasesTabTemplate = '<span itemscope="" itemtype="http://schema.org/ListItem" itemprop="itemListElement">' +
+		`<a href="/${vendorName}/${repoName}/releases" class="reponav-item" data-hotkey="g r" data-selected-links="repo_releases /${vendorName}/${repoName}/releases" itemprop="url">` +
+			'<svg aria-hidden="true" class="octicon octicon-tag" height="16" role="img" version="1.1" viewBox="0 0 14 16" width="14"><path d="M6.73 2.73c-0.47-0.47-1.11-0.73-1.77-0.73H2.5C1.13 2 0 3.13 0 4.5v2.47c0 0.66 0.27 1.3 0.73 1.77l6.06 6.06c0.39 0.39 1.02 0.39 1.41 0l4.59-4.59c0.39-0.39 0.39-1.02 0-1.41L6.73 2.73zM1.38 8.09c-0.31-0.3-0.47-0.7-0.47-1.13V4.5c0-0.88 0.72-1.59 1.59-1.59h2.47c0.42 0 0.83 0.16 1.13 0.47l6.14 6.13-4.73 4.73L1.38 8.09z m0.63-4.09h2v2H2V4z"></path></svg>' +
+			'&nbsp;<span itemprop="name">Releases</span>' +
+			'<meta itemprop="position" content="6">' +
+		'</a>' +
+	'</span>';
+	const $releasesTab = $(releasesTabTemplate);
+	const $repoNav = $('.js-repo-nav');
+
+	if (isReleases()) {
+		$releasesTab.children('a')
+			.addClass('js-selected-navigation-item selected');
+
+		$repoNav.find('.selected')
+			.removeClass('js-selected-navigation-item selected');
+	}
+
+	$repoNav.append($releasesTab);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -59,6 +83,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	if (isRepo) {
 		gitHubInjection(window, () => {
+			addReleasesTab();
 			if (isPR()) {
 				linkifyBranchRefs();
 			}
