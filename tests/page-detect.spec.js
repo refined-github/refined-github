68ee8d5c38f7926167b93707d3a5e2c5acaf8596
Andrew Levine <theandrewtizer@gmail.com>
2016-04-03 20:20:01 -0500

    more tests

--- tests/page-detect.spec.js
+++ tests/page-detect.spec.js
@@ -45,4 +45,58 @@ test('isIssueList', t => {
 	]);
 });
 
-test.todo('isIssue');
+test('isIssue', t => {
+	urisMatch(t, pageDetect.isIssue, [
+		'https://github.com/sindresorhus/refined-github/issues/146'
+	]);
+});
+
+test('isPRList', t => {
+	urisMatch(t, pageDetect.isPRList, [
+		'https://github.com/sindresorhus/refined-github/pulls'
+	]);
+});
+
+test('isPR', t => {
+	urisMatch(t, pageDetect.isPR, [
+		'https://github.com/sindresorhus/refined-github/pull/148'
+	]);
+});
+
+test('isPRFiles', t => {
+	urisMatch(t, pageDetect.isPRFiles, [
+		'https://github.com/sindresorhus/refined-github/pull/148/files'
+	]);
+});
+
+test('isPRCommit', t => {
+	urisMatch(t, pageDetect.isPRCommit, [
+		'https://github.com/sindresorhus/refined-github/pull/148/commits/0019603b83bd97c2f7ef240969f49e6126c5ec85'
+	]);
+});
+
+test('isCommitList', t => {
+	urisMatch(t, pageDetect.isCommitList, [
+		'https://github.com/sindresorhus/refined-github/commits/master'
+	]);
+});
+
+test('isSingleCommit', t => {
+	urisMatch(t, pageDetect.isSingleCommit, [
+		'https://github.com/sindresorhus/refined-github/commit/5b614b9035f2035b839f48b4db7bd5c3298d526f'
+	]);
+});
+
+test.todo('isCommit');
+
+test('isReleases', t => {
+	urisMatch(t, pageDetect.isReleases, [
+		'https://github.com/sindresorhus/refined-github/releases'
+	]);
+});
+
+test('isBlame', t => {
+	urisMatch(t, pageDetect.isBlame, [
+		'https://github.com/sindresorhus/refined-github/blame/master/package.json'
+	]);
+});
