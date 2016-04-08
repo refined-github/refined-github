97fee63dbd2f9fd0ee3f0507776ea63ffcf7c52d
Paul Molluzzo <paul@molluzzo.com>
2016-04-08 11:27:57 -0400

    Move deploy script to Travis deploy process for master with tags only; remove unnecessary tests from bash script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -ev
-if ([ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]) && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo `zip -r extension.zip extension/`
-  echo `grunt`
-fi
+echo `zip -r extension.zip extension/`
+echo `grunt`
