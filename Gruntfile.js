462a81174cf12a463beb9a4f8f0a3da67b96febf
Paul Molluzzo <paulmolluzzo@users.noreply.github.com>
2016-04-11 06:23:04 -0400

    Auto deploy to Chrome Webstore on new git tags (#172)

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+const zipFileName = 'extension.zip';
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		webstoreUpload: {
+			accounts: {
+				default: {
+					cli_auth: true, // eslint-disable-line
+					publish: true,
+					client_id: process.env.CLIENT_ID, // eslint-disable-line
+					client_secret: process.env.CLIENT_SECRET, // eslint-disable-line
+					refresh_token: process.env.REFRESH_TOKEN // eslint-disable-line
+				}
+			},
+			extensions: {
+				refinedGitHub: {
+					appID: 'hlepfoohegkhhmjieoechaddaejaokhf', // App ID from chrome webstore
+					publish: true,
+					zip: zipFileName
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-webstore-upload');
+	grunt.registerTask('default', ['webstoreUpload']);
+};
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -ev
+zip -r extension.zip extension/
+grunt
