5f196473922ec4d1579e9cf0277eead05094d5e1
Jacob Bearden <jacob.bearden0@gmail.com>
2016-05-22 20:07:30 -0600

    move page detect

--- extension/content.js
+++ extension/content.js
@@ -71,10 +71,6 @@ function addReleasesTab() {
 function addTrendingMenuItem() {
 	const $secondListItem = $('.header-nav.left .header-nav-item:nth-child(2)');
 
-	if (pageDetect.isGist()) {
-		return;
-	}
-
 	$secondListItem.after(`
 		<li class="header-nav-item">
 			<a href="/trending" class="header-nav-link" data-hotkey="g t">Trending</a>
@@ -289,7 +285,9 @@ $(document).on('click', event => {
 document.addEventListener('DOMContentLoaded', () => {
 	const username = getUsername();
 
-	addTrendingMenuItem();
+	if (!pageDetect.isGist()) {
+		addTrendingMenuItem();
+	}
 
 	if (pageDetect.isDashboard()) {
 		// hide other users starring/forking your repos
