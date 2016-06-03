a97132030350f6da229eecb8d28e1ce31deedad7
Andrew Levine <DrewML@users.noreply.github.com>
2016-06-03 04:37:46 -0500

    Fix "Copy on Y" bug (#238)

--- extension/copy-on-y.js
+++ extension/copy-on-y.js
@@ -4,13 +4,22 @@
 
 const Y_KEYCODE = 89;
 
-window.enableCopyOnY = () => {
-	$(document).on('keyup', ({keyCode}) => {
-		if (keyCode === Y_KEYCODE) {
+window.enableCopyOnY = (() => {
+	const handler = ({keyCode, target}) => {
+		if (keyCode === Y_KEYCODE && target.nodeName !== 'INPUT') {
 			const commitIsh = $('.commit-tease-sha').text().trim();
 			const uri = location.href.replace(/\/blob\/[\w-]+\//, `/blob/${commitIsh}/`);
 
 			utils.copyToClipboard(uri);
 		}
-	});
-};
+	};
+
+	return {
+		setup: () => {
+			window.addEventListener('keyup', handler);
+		},
+		destroy: () => {
+			window.removeEventListener('keyup', handler);
+		}
+	};
+})();
