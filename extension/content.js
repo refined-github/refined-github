28602c87e5c7f4bdb9e3a4d66f8c868708e2d187
Jacob Bearden <jacob@bearden.io>
2016-08-26 03:35:51 -0600

    Fix trending menu item and readme edit button (#287)

--- extension/content.js
+++ extension/content.js
@@ -75,7 +75,7 @@ function addReleasesTab() {
 }
 
 function addTrendingMenuItem() {
-	const $secondListItem = $('.header-nav.left .header-nav-item:nth-child(2)');
+	const $secondListItem = $('.header-nav.float-left .header-nav-item:nth-child(2)');
 
 	$secondListItem.after(`
 		<li class="header-nav-item">
@@ -112,7 +112,7 @@ function addReadmeEditButton() {
 
 	const readmeName = $('#readme > h3').text().trim();
 	const path = $('.js-repo-root ~ .js-path-segment, .final-path').map((idx, el) => $(el).text()).get().join('/');
-	const currentBranch = $('.file-navigation .select-menu.left button.select-menu-button span').text();
+	const currentBranch = $('.file-navigation .select-menu.float-left button.select-menu-button span').text();
 	const editHref = `/${repoUrl}/edit/${currentBranch}/${path ? `${path}/` : ''}${readmeName}`;
 	const editButtonHtml = `<div id="refined-github-readme-edit-link">
 		<a href="${editHref}">
