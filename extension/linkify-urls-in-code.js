365a32374e1d374a0530af8f6f87fd725c970c0c
Paul Molluzzo <paul@molluzzo.com>
2017-04-24 13:54:44 -0400

    Linkify URLs and issues in code comments

---
+++ extension/linkify-urls-in-code.js
@@ -0,0 +1,37 @@
+window.linkifyURLsInCode = (() => {
+	const issueRegex = /#[0-9]+/;
+	const URLRegex = /(http(s)?(:\/\/))?(www\.)?[a-zA-Z0-9-_.]+(\.[a-zA-Z0-9]{2,})([-a-zA-Z0-9:%_+.~#?&//=]*)/;
+	const linkifyIssue = (repoPath, issue) => `<a href="https://github.com/${repoPath}/issues/${issue.replace('#', '')}">${issue}</a>`;
+	const linkifyURL = url => `<a href="${url}">${url}</a>`;
+
+	const hasIssue = text => issueRegex.test(text);
+	const hasURL = text => URLRegex.test(text);
+
+	const linkifyCode = repoPath => {
+		const codeBlobs = $('.blob-code-inner');
+
+		$(codeBlobs)
+		.toArray()
+		.forEach(blob => {
+			let blobHTML = blob.innerHTML;
+			if (hasURL(blobHTML)) {
+				// Match URLs and remove < or > from beginning or end
+				const URLmatch = blobHTML.match(URLRegex)[0].replace(/(^&lt)|(&gt$)/, '');
+				blobHTML = blobHTML.replace(URLmatch, linkifyURL(URLmatch));
+			}
+
+			if (hasIssue(blobHTML)) {
+				const issueMatch = blobHTML.match(issueRegex)[0];
+				blobHTML = blobHTML.replace(issueMatch, linkifyIssue(repoPath, issueMatch));
+			}
+
+			blob.innerHTML = blobHTML;
+		});
+	};
+
+	return {
+		hasIssue,
+		hasURL,
+		linkifyCode
+	};
+})();
---
+++ test/linkify-urls-in-code.js
@@ -0,0 +1,40 @@
+import test from 'ava';
+import Window from './fixtures/window';
+
+global.window = new Window();
+global.location = window.location;
+
+require('../extension/linkify-urls-in-code.js'); // eslint-disable-line import/no-unassigned-import
+
+const {linkifyURLsInCode} = window;
+
+function batchTestText(t, detectFn, shouldMatch = [], shouldNotMatch = []) {
+	for (const text of shouldMatch) {
+		t.true(detectFn(text));
+	}
+
+	for (const text of shouldNotMatch) {
+		t.false(detectFn(text));
+	}
+}
+
+test('isIssue', batchTestText, linkifyURLsInCode.hasIssue, [
+	'#1',
+	'#12',
+	'an issue in the middle #123 of some text'
+], [
+	'#a',
+	'1234'
+]);
+
+test('isURL', batchTestText, linkifyURLsInCode.hasURL, [
+	'http://github.com/',
+	'https://www.github.com',
+	'github.com',
+	'https://github.com/orgs/test/dashboard',
+	'a long string of text with a url at the end: https://github.com/dashboard?param=test'
+], [
+	'https://github',
+	'github',
+	'github/whatever/text'
+]);
