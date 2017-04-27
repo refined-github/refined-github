8a3ce7ff0478e4ce4f7ce773c870bbfd30a366e0
Jonas Gierer <jgierer12@outlook.com>
2017-04-27 19:48:34 +0200

    Make `MutationObserver` work on diffs in the conversation timeline

--- extension/content.js
+++ extension/content.js
@@ -418,7 +418,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 			if (pageDetect.hasDiff()) {
 				removeDiffSigns();
-				new MutationObserver(removeDiffSigns).observe($('#files')[0], {childList: true, subtree: true});
+				new MutationObserver(removeDiffSigns).observe($('.js-discussion, .js-diff-progressive-container')[0], {childList: true, subtree: true});
 			}
 
 			if (pageDetect.isPR() || pageDetect.isIssue() || pageDetect.isCommit()) {
