2f09aa7a51bf1be842a643b759ba5149fee2c779
Paul Molluzzo <paul@molluzzo.com>
2017-05-02 20:14:26 -0400

    Use @jgierer12's code 😊

--- extension/content.js
+++ extension/content.js
@@ -179,11 +179,13 @@ function addPatchDiffLinks() {
 }
 
 function removeDiffSigns() {
-	$('.blob-code-deletion .blob-code-inner:not(.refined-github-diff-signs), .blob-code-addition .blob-code-inner:not(.refined-github-diff-signs)').each((index, element) => {
-		const textNode = element.childNodes[0];
-		textNode.nodeValue = textNode.nodeValue.replace(/^[+-]/, ' ');
-		$(element).addClass('refined-github-diff-signs');
-	});
+	$('.blob-code-addition, .blob-code-deletion')
+		.find('.blob-code-inner:not(.refined-github-diff-signs)')
+		.each((index, element) => {
+			const $element = $(element);
+			$element.html(` ${$element.html().slice(1)}`);
+			$element.addClass('refined-github-diff-signs');
+		});
 }
 
 function markMergeCommitsInList() {
