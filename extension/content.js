4faaa610137cf42cf76b48448c8a7c43c2055ba9
Jonas Gierer <jgierer12@outlook.com>
2017-05-11 23:06:37 +0200

    Move `addUploadBtn` out of content.js

--- extension/content.js
+++ extension/content.js
@@ -1,4 +1,4 @@
-/* globals gitHubInjection, pageDetect, icons, diffFileHeader, addReactionParticipants, addFileCopyButton, addGistCopyButton, enableCopyOnY, showRealNames, markUnread, linkifyURLsInCode */
+/* globals gitHubInjection, pageDetect, icons, diffFileHeader, addReactionParticipants, addFileCopyButton, addGistCopyButton, enableCopyOnY, showRealNames, markUnread, linkifyURLsInCode, addUploadBtn */
 
 'use strict';
 const {ownerName, repoName} = pageDetect.getOwnerAndRepo();
@@ -341,38 +341,6 @@ function addFilterCommentsByYou() {
 	const lastFilter = $('.subnav-search-context').find('.select-menu-list > a:last-child');
 	if (!lastFilter.prev().hasClass('refined-github-filter')) {
 		lastFilter.before(newFilter);
-	}
-}
-
-function addUploadBtn() {
-	if (pageDetect.hasCommentForm()) {
-		$('.js-previewable-comment-form').each((index, element) => {
-			const $element = $(element);
-			if (!$element.hasClass('refined-github-has-upload-btn')) {
-				const uploadBtn = `<label for="refined-github-upload-btn-${index}" class="toolbar-item tooltipped tooltipped-nw refined-github-upload-btn" aria-label="Upload a file">
-						${icons.cloudUpload}
-					</label>`;
-
-				const $uploadBtn = $element
-					.find('.comment-form-head .toolbar-commenting .toolbar-group:last-child')
-					.append(uploadBtn)
-					.find('.refined-github-upload-btn');
-
-				const keydownHandler = event => {
-					if (event.which === 85 && event.ctrlKey) {
-						event.preventDefault();
-						$uploadBtn.click();
-					}
-				};
-				$element
-					.find('.js-comment-field')
-					.focus(() => $(document).on('keydown', keydownHandler))
-					.blur(() => $(document).off('keydown', keydownHandler));
-
-				$element.find('.js-write-bucket .drag-and-drop .default .js-manual-file-chooser').attr('id', `refined-github-upload-btn-${index}`);
-				$element.addClass('refined-github-has-upload-btn');
-			}
-		});
 	}
 }
 
---
+++ extension/upload-button.js
@@ -0,0 +1,31 @@
+window.addUploadBtn = () => {
+	if (pageDetect.hasCommentForm()) {
+		$('.js-previewable-comment-form').each((index, element) => {
+			const $element = $(element);
+			if (!$element.hasClass('refined-github-has-upload-btn')) {
+				const uploadBtn = `<label for="refined-github-upload-btn-${index}" class="toolbar-item tooltipped tooltipped-nw refined-github-upload-btn" aria-label="Upload a file">
+						${icons.cloudUpload}
+					</label>`;
+
+				const $uploadBtn = $element
+					.find('.comment-form-head .toolbar-commenting .toolbar-group:last-child')
+					.append(uploadBtn)
+					.find('.refined-github-upload-btn');
+
+				const keydownHandler = event => {
+					if (event.which === 85 && event.ctrlKey) {
+						event.preventDefault();
+						$uploadBtn.click();
+					}
+				};
+				$element
+					.find('.js-comment-field')
+					.focus(() => $(document).on('keydown', keydownHandler))
+					.blur(() => $(document).off('keydown', keydownHandler));
+
+				$element.find('.js-write-bucket .drag-and-drop .default .js-manual-file-chooser').attr('id', `refined-github-upload-btn-${index}`);
+				$element.addClass('refined-github-has-upload-btn');
+			}
+		});
+	}
+}
