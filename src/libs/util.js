536beaf6c254d97f20b0aee7319b96520716588d
Federico Brigante <github@bfred.it>
2017-06-07 14:06:16 +0800

    Move from string+regex parsing to explicit piece-by-piece handling

--- src/libs/util.js
+++ src/libs/util.js
@@ -1,29 +1,85 @@
 import {getRepoURL} from './page-detect';
 
-export const shortenUrl = url => url
-	// Drop HTTPS, but not HTTP
-	.replace(/^https:\/\//, '')
+export const shortenUrl = plainUrl => {
+	let {
+		origin,
+		pathname,
+		search,
+		hash
+	} = new URL(plainUrl);
 
-	// Drop host if pathname makes sense on its own
-	.replace(`${location.host}/`, (host, i, url) => {
-		if (i === 0 && url.split('/').length > 3) {
-			return '';
+	let [
+		, // Always empty
+		user,
+		repo,
+		type,
+		revision,
+		...filePath
+	] = pathname.split('/');
+
+	const isLocalUrl = origin === location.origin;
+	const isCurrentRepo = isLocalUrl && getRepoURL() === `${user}/${repo}`;
+
+	const isRaw = [
+		'https://raw.githubusercontent.com',
+		'https://cdn.rawgit.com',
+		'https://rawgit.com'
+	].includes(origin);
+
+	const isFile = [
+		'blob',
+		'blame'
+	].includes(type);
+
+	// Drop origin if pathname makes sense on its own
+	if ((isRaw || isLocalUrl) && pathname.split('/').length > 3) {
+		origin = '';
+	} else {
+		// Otherwise just drop HTTPS
+		origin = origin.replace('https://', '');
+	}
+
+	// Shorten GitHub file URLs
+	if (isFile || isRaw) {
+		let newPath = '';
+
+		// Drop 'user/repo' where possible
+		if (!isCurrentRepo) {
+			newPath += `/${user}/${repo}`;
 		}
-		return host;
-	})
 
-	// Shorten and highlight commit hashes
-	.replace(/([^/]+\/[^/]+)\/blob\/([\da-f]{7,})/i, (m, repo, hash) => {
-		return `${repo}/<code>${hash.substr(0, 7)}</code>`;
-	})
+		// Shift variables on "raw" urls because they don't have a type
+		if (isRaw) {
+			filePath.unshift(revision);
+			revision = type;
+		} else if (type !== 'blob') {
+			// Highlight 'type'; drop it if it's 'blob'
+			newPath += `/<em>${type}</em>`;
+		}
 
-	// Shorten and highlight branches
-	.replace(/([^/]+\/[^/]+)\/(blob|blame)\/([^/]+)/, (m, repo, type, revision) => {
-		return `${repo}/${type === 'blob' ? '' : `${type}/`}<code>${revision}</code>`;
-	})
+		if (revision) {
+			// Shorten hashes
+			if (/^[0-9a-f]{40}$/.test(revision)) {
+				revision = revision.substr(0, 7);
+			}
+			// Highlight hashes and branches
+			newPath += `/<code>${revision}</code>`;
+		}
 
-	// Shorten and highlight branches
-	.replace(`${getRepoURL()}/`, '');
+		if (filePath.length > 0) {
+			filePath = filePath.join('/');
+			if (isRaw) {
+				filePath = `<code>${filePath}</code>`;
+			}
+			newPath += `:${filePath}`;
+		}
+
+		pathname = newPath;
+	}
+
+	// Join all; drop possible starting backslash
+	return `${origin}${pathname}${search}${hash}`.replace(/^[/]/, '');
+};
 
 export const issueRegex = /([a-zA-Z0-9-_.]+\/[a-zA-Z0-9-_.]+)?#[0-9]+/;
 export const linkifyIssueRef = (repoPath, issue, attrs) => {
