6b5b9db25ec02bffe7e944e6ff55aa2f94e813c0
Federico Brigante <github@bfred.it>
2017-06-14 11:40:01 +0800

    Drop tagged template support in html()

--- src/libs/domify.js
+++ src/libs/domify.js
@@ -1,9 +1,2 @@
 // Get DOM node from HTML
-export default html => {
-	// Shortcut for html`text` instead of html(`text`)
-	if (html.raw) {
-		html = String.raw(...arguments);
-	}
-
-	return document.createRange().createContextualFragment(html);
-};
+export default html => document.createRange().createContextualFragment(html);
