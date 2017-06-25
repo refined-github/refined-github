636843eaec6becd2ed5f12d94d783ef9bfaf78b6
Federico Brigante <github@bfred.it>
2017-06-25 14:55:22 +0800

    Allow customization of the rebase title

--- src/content.js
+++ src/content.js
@@ -424,7 +424,11 @@ function removeProjectsTab() {
 }
 
 function fixSquashAndMergeTitle() {
-	$('.btn-group-squash button[type=submit]').click(() => {
+	const btn = select('.merge-message .btn-group-squash [type=submit]');
+	if (!btn) {
+		return;
+	}
+	btn.addEventListener('click', () => {
 		const title = select('.js-issue-title').textContent;
 		const number = select('.gh-header-number').textContent;
 		select('#merge_title_field').value = `${title.trim()} (${number})`;
