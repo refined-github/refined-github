82dc8a25a2538ea161e0dcc75febb78897109809
Ram Lmn <ramlmn@outlook.com>
2017-07-07 21:16:04 +0530

    Prevent domify from making unecessory network requests

--- src/libs/domify.js
+++ src/libs/domify.js
@@ -1,9 +1,7 @@
 export default html => {
-	const fragment = document.createRange().createContextualFragment(html);
+	const fragment = document.createElement('template');
+	fragment.innerHTML = html;
 
-	// If it's a single element, return just the element
-	if (fragment.firstChild === fragment.lastChild) {
-		return fragment.firstChild;
-	}
+	// Return template for querying
 	return fragment;
 };
