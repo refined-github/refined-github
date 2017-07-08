43b07c3af840b6367d75b9220e81041687708df5
Ram Lmn <ramlmn@outlook.com>
2017-07-08 08:07:11 +0530

    Return Template.#content instead of Template for querying

--- src/libs/domify.js
+++ src/libs/domify.js
@@ -2,6 +2,13 @@ export default html => {
 	const fragment = document.createElement('template');
 	fragment.innerHTML = html;
 
+	const content = fragment.content;
+
+	// If it's a single element, return just the element
+	if (content.firstChild === content.lastChild) {
+		return content.firstChild;
+	}
+
 	// Return template for querying
-	return fragment;
+	return content;
 };
