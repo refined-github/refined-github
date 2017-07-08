9359bc1f20260fa69368c5762e53eaac8f0cd7e1
Ram Lmn <ramlmn@users.noreply.github.com>
2017-07-08 12:41:53 +0530

    Avoid unnecessary network requests when parsing HTML (#582)

--- src/libs/domify.js
+++ src/libs/domify.js
@@ -1,9 +1,14 @@
 export default html => {
-	const fragment = document.createRange().createContextualFragment(html);
+	const template = document.createElement('template');
+	template.innerHTML = html;
+
+	const fragment = template.content;
 
 	// If it's a single element, return just the element
 	if (fragment.firstChild === fragment.lastChild) {
 		return fragment.firstChild;
 	}
+
+	// Return fragment for querying
 	return fragment;
 };
