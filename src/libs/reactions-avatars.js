40653f34729ada816fff7bddb295324c4b1aa631
Federico Brigante <github@bfred.it>
2017-07-27 17:48:54 +0800

    Show more than three Avatars for Reactions (#644)

--- src/libs/reactions-avatars.js
+++ src/libs/reactions-avatars.js
@@ -1,33 +1,51 @@
 import debounce from 'debounce-fn';
 import select from 'select-dom';
 import {h} from 'dom-chef';
-import {getUsername} from './utils';
+import {getUsername, flatZip} from './utils';
+
+const arbitraryAvatarLimit = 39;
+const approximateHeaderLength = 3; // Each button header takes about as much as 3 avatars
+
+function getParticipants(container) {
+	const currentUser = getUsername();
+	return container.getAttribute('aria-label')
+		.replace(/ reacted with.*/, '')
+		.replace(/,? and /, ', ')
+		.replace(/, \d+ more/, '')
+		.split(', ')
+		.filter(username => username !== currentUser)
+		.map(username => ({
+			container,
+			username
+		}));
+}
 
 function add() {
-	const currentUser = getUsername();
-	for (const element of select.all(`
-		.comment-reactions.has-reactions
-		.comment-reactions-options
-		.reaction-summary-item[aria-label]:not(.rgh-reactions)
-	`)) {
-		element.classList.add('rgh-reactions');
-		const participants = element.getAttribute('aria-label')
-			.replace(/ reacted with.*/, '')
-			.replace(/,? and /, ', ')
-			.replace(/, \d+ more/, '')
-			.split(', ')
-			.filter(username => username !== currentUser)
-			.filter((u, i) => i < 3) // Limit to 3 avatars
-			.map(user => (
-				<a href={`/${user}`}>
-					<img src={`/${user}.png`}/>
-				</a>
-			));
+	for (const list of select.all(`.has-reactions .comment-reactions-options:not(.rgh-reactions)`)) {
+		const avatarLimit = arbitraryAvatarLimit - (list.children.length * approximateHeaderLength);
 
-		element.append(...participants);
+		const participantByReaction = [].map.call(list.children, getParticipants);
+		const flatParticipants = flatZip(participantByReaction, avatarLimit);
+
+		for (const participant of flatParticipants) {
+			participant.container.append(
+				<a href={`/${participant.username}`}>
+					<img src={`/${participant.username}.png?size=${window.devicePixelRatio * 20}`}/>
+				</a>
+			);
+		}
+
+		list.classList.add('rgh-reactions');
+
+		// Overlap reaction avatars when near the avatarLimit
+		if (flatParticipants.length > avatarLimit * 0.9) {
+			list.classList.add('rgh-reactions-near-limit');
+		}
 	}
 }
 
+// Feature testable on
+// https://github.com/babel/babel/pull/3646
 export default () => {
 	add();
 	document.addEventListener('socket:message', debounce(add, {wait: 100}));
