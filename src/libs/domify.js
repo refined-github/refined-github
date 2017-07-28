a73614b7f28d057e6a32192eee250892d3736935
Federico Brigante <github@bfred.it>
2017-07-28 18:45:44 +0800

    Remove unused domify uncertainty
    This behavior is unused and undesired.
    What does ` <i>a</i>` return?
    1 el
    
    What does ` <i>a</i><i>a</i>` return?
    frag with 3 nodes
    
    Now it always returns a frag.
    
    If you want just one element and discard the whitespace, use:
    domify(` <i>a</i>`).firstElementChild

--- src/libs/domify.js
+++ src/libs/domify.js
@@ -1,14 +1,5 @@
 export default html => {
 	const template = document.createElement('template');
 	template.innerHTML = html;
-
-	const fragment = template.content;
-
-	// If it's a single element, return just the element
-	if (fragment.firstChild === fragment.lastChild) {
-		return fragment.firstChild;
-	}
-
-	// Return fragment for querying
-	return fragment;
+	return template.content;
 };
