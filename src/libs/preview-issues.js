4cfe7b6b7fc71f66f1b0af5a3910d477d6616ee6
Kai Hao <kevin830726@gmail.com>
2017-08-11 20:58:22 +0800

    add preview issues title in issues/PR

---
+++ src/libs/preview-issues.js
@@ -0,0 +1,30 @@
+import { observeEl } from './utils';
+
+const loadingText = 'loading...';
+
+const addToolTip = (issueLink) => {
+	issueLink.classList.add('tooltipped', 'tooltipped-se');
+
+	const title = issueLink.getAttribute('aria-label') !== loadingText
+		? issueLink.getAttribute('aria-label')
+		: issueLink.getAttribute('title');
+
+	if (title) {
+		issueLink.setAttribute('aria-label', title);
+		issueLink.removeAttribute('title');
+	} else {
+		issueLink.setAttribute('aria-label', loadingText);
+	}
+};
+
+const preview = () => {
+	const issueLinks = Array.from(document.querySelectorAll('.comment .issue-link'));
+
+	issueLinks.forEach((issueLink) => {
+		observeEl(issueLink, () => addToolTip(issueLink), {
+			attributeFilter: ['title'],
+		});
+	});
+};
+
+export default preview;
