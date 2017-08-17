26fa1e5b8643ea20d7454776230406f171cc2167
kevin940726 <kevin830726@gmail.com>
2017-08-17 20:34:38 +0800

    disable when there is tooltipster installed

--- src/libs/preview-issues.js
+++ src/libs/preview-issues.js
@@ -4,8 +4,28 @@ import {observeEl} from './utils';
 const loadingText = 'loading...';
 
 const addToolTip = issueLink => {
+	// Has already injected tooltipster
+	if (issueLink.classList.contains('tooltipstered')) {
+		// Has both tooltip and tooltipster, remove the tooltip to prevent duplicated tooltip
+		if (issueLink.classList.contains('tooltipped')) {
+			const label = issueLink.getAttribute('aria-label');
+			const title = issueLink.getAttribute('title');
+
+			issueLink.classList.remove('tooltipped');
+			issueLink.removeAttribute('aria-label');
+			if (!title) {
+				issueLink.setAttribute('title', label);
+			}
+		}
+
+		// Skip adding tooltip
+		return;
+	}
+
+	// Add github original tooltip classes
 	issueLink.classList.add('tooltipped', 'tooltipped-se');
 
+	// `title` attribute is loaded asynchronously
 	const title = issueLink.getAttribute('aria-label') === loadingText ?
 		issueLink.getAttribute('title') :
 		issueLink.getAttribute('aria-label');
@@ -14,6 +34,7 @@ const addToolTip = issueLink => {
 		issueLink.setAttribute('aria-label', title);
 		issueLink.removeAttribute('title');
 	} else {
+		// Is loading
 		issueLink.setAttribute('aria-label', loadingText);
 	}
 };
@@ -23,7 +44,7 @@ const preview = () => {
 
 	for (const issueLink of issueLinks) {
 		observeEl(issueLink, () => addToolTip(issueLink), {
-			attributeFilter: ['title']
+			attributeFilter: ['title', 'class']
 		});
 	}
 };
