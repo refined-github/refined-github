94f193c4704a47ef219443cdcd0b2e6bba4a8e20
Federico Brigante <github@bfred.it>
2017-10-20 20:19:44 +0800

    Avoid jQuery where possible

--- src/libs/scroll-to-top-on-collapse.js
+++ src/libs/scroll-to-top-on-collapse.js
@@ -1,7 +1,11 @@
+import select from 'select-dom';
+
 export default () => {
+	const toolbar = select('.pr-toolbar');
+
 	$('.js-diff-progressive-container').on('details:toggled', '.file', ({target}) => {
-		const toolbarHeight = $('.pr-toolbar').height();
 		const elOffset = target.getBoundingClientRect().top;
+		const toolbarHeight = toolbar.getBoundingClientRect().top;
 
 		if (elOffset < toolbarHeight) {
 			window.scrollBy(0, elOffset - toolbarHeight);
