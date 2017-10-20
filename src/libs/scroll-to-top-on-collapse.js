3d9b9aff034884707da05f2ad846cfa7c099cce5
Federico Brigante <github@bfred.it>
2017-10-20 20:19:32 +0800

    Shorten scroll logic with scrollBy

--- src/libs/scroll-to-top-on-collapse.js
+++ src/libs/scroll-to-top-on-collapse.js
@@ -1,18 +1,10 @@
 export default () => {
 	$('.js-diff-progressive-container').on('details:toggled', '.file', ({target}) => {
-		// Calculate the scroll offset
-		// See: https://stackoverflow.com/a/11396681/3736051
-		const bodyOffset = document.body.getBoundingClientRect().top;
-		const parentOffset = target.getBoundingClientRect().top;
 		const toolbarHeight = $('.pr-toolbar').height();
+		const elOffset = target.getBoundingClientRect().top;
 
-		// Parent offset is relative to the viewport
-		if (parentOffset < toolbarHeight) {
-			window.scrollTo(
-				// Maintain the horizontal scroll
-				window.scrollX,
-				// Put the top of the container at the foot of the toolbar
-				parentOffset - bodyOffset - toolbarHeight);
+		if (elOffset < toolbarHeight) {
+			window.scrollBy(0, elOffset - toolbarHeight);
 		}
 	});
 };
