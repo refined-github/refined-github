d52b25ee88dfa8d92cc4a786349b2a290aed87fe
Federico Brigante <github@bfred.it>
2017-10-20 07:32:05 -0500

    Add comment

--- src/libs/scroll-to-top-on-collapse.js
+++ src/libs/scroll-to-top-on-collapse.js
@@ -6,7 +6,8 @@ export default () => {
 	$('.js-diff-progressive-container').on('details:toggled', '.file', ({target}) => {
 		const elOffset = target.getBoundingClientRect().top;
 		const toolbarHeight = toolbar.getBoundingClientRect().top;
-
+		
+		// Bring element in view if it's above the PR toolbar
 		if (elOffset < toolbarHeight) {
 			window.scrollBy(0, elOffset - toolbarHeight);
 		}
