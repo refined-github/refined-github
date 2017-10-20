03f413559b57e8f795cb4e3845367afb26361499
Federico Brigante <github@bfred.it>
2017-10-20 20:06:25 +0800

    Use toggle-specific event

--- src/libs/scroll-to-top-on-collapse.js
+++ src/libs/scroll-to-top-on-collapse.js
@@ -1,12 +1,9 @@
 export default () => {
-	$('.js-diff-progressive-container').on('click', '.js-details-target', function () {
-		// Get the parent file container
-		const parent = $(this).closest('.file')[0];
-
+	$('.js-diff-progressive-container').on('details:toggled', '.file', ({target}) => {
 		// Calculate the scroll offset
 		// See: https://stackoverflow.com/a/11396681/3736051
 		const bodyOffset = document.body.getBoundingClientRect().top;
-		const parentOffset = parent.getBoundingClientRect().top;
+		const parentOffset = target.getBoundingClientRect().top;
 		const toolbarHeight = $('.pr-toolbar').height();
 
 		// Parent offset is relative to the viewport
