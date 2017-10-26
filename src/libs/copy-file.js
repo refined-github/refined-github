8dc53a0c38f6a079b4cd7c138623f9ac964c75c3
Federico Brigante <github@bfred.it>
2017-10-27 02:51:39 +0800

    Merge copy-gist and copy-file

--- src/libs/copy-file.js
+++ src/libs/copy-file.js
@@ -1,22 +1,23 @@
-import copyToClipboard from 'copy-text-to-clipboard';
 import select from 'select-dom';
 import {h} from 'dom-chef';
+import {groupButtons} from './utils';
 
-export default () => {
-	// Button already added (partial page nav), or non-text file
-	if (select.exists('.copy-btn') || !select.exists('[data-line-number="1"]')) {
-		return;
+export default function () {
+	for (const code of select.all('.file .highlight:not(.rgh-copy-file)')) {
+		const file = code.closest('.file');
+		file.classList.add('rgh-copy-file');
+
+		// Enable copy behavior
+		file.classList.add('js-zeroclipboard-container');
+		code.classList.add('js-zeroclipboard-target');
+
+		// Prepend to list of buttons
+		const firstAction = select('.file-actions .btn', file);
+		firstAction.before(
+			<button class="btn btn-sm copy-btn js-zeroclipboard tooltipped tooltipped-n" aria-label="Copy file to clipboard" data-copied-hint="Copied!" type="button">Copy</button>
+		);
+
+		// Group buttons if necessary
+		groupButtons(firstAction.parentNode.children);
 	}
-
-	const targetSibling = select('#raw-url');
-	const fileUri = targetSibling.getAttribute('href');
-	targetSibling.before(
-		<a href={fileUri} class="btn btn-sm BtnGroup-item copy-btn">Copy</a>
-	);
-
-	$(document).on('click', '.copy-btn', e => {
-		e.preventDefault();
-		const fileContents = select('.js-file-line-container').innerText;
-		copyToClipboard(fileContents);
-	});
-};
+}
