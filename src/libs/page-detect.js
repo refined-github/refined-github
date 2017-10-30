9f2dcffa88e888f39e837c78d3e02b253e80d0ba
Federico Brigante <github@bfred.it>
2017-10-31 00:18:28 +0800

    Equivalent: sort page-detect + tests

--- src/libs/page-detect.js
+++ src/libs/page-detect.js
@@ -4,80 +4,76 @@ import select from 'select-dom';
 // but getRepoPath returns undefined if it doesn't find a repo
 // so .test(X) works but X.startsWith() doesn't
 /* eslint-disable unicorn/prefer-starts-ends-with */
-
-export const isGist = () => location.hostname.startsWith('gist.') || location.pathname.startsWith('gist/');
-
-export const isDashboard = () => location.pathname === '/' || /^(\/orgs\/[^/]+)?\/dashboard/.test(location.pathname);
-
-export const isTrending = () => location.pathname.startsWith('/trending');
-
-export const isEnterprise = () => location.hostname !== 'github.com' && location.hostname !== 'gist.github.com';
-
-export const isNotifications = () => /^\/(?:[^/]+\/[^/]+\/)?notifications/.test(location.pathname);
-
-// @todo Replace with DOM-based test because this is too generic #708
-export const isRepo = () => /^\/[^/]+\/[^/]+/.test(location.pathname) &&
-	!isGist() &&
-	!isTrending() &&
-	!isNotifications();
+/* eslint-disable no-use-before-define */
 
 export const getRepoPath = () => (/^(?:[/][^/]+){2}[/]?(.*)$/.exec(location.pathname.replace(/[/]$/, '')) || [])[1];
 
 export const getRepoURL = () => location.pathname.slice(1).split('/', 2).join('/');
 
-export const isRepoRoot = () => /^(tree[/][^/]+)?$/.test(getRepoPath());
+export const getOwnerAndRepo = () => {
+	const [, ownerName, repoName] = location.pathname.split('/');
+	return {ownerName, repoName};
+};
 
-export const isRepoTree = () => /^tree\//.test(getRepoPath());
-
-export const isIssueSearch = () => location.pathname.startsWith('/issues');
-
-export const isIssueList = () => /^issues\/?$/.test(getRepoPath());
-
-export const isIssue = () => /^issues\/\d+/.test(getRepoPath());
-
-export const isPRSearch = () => location.pathname.startsWith('/pulls');
-
-export const isPRList = () => /^pulls\/?$/.test(getRepoPath());
-
-export const isPR = () => /^pull\/\d+/.test(getRepoPath());
-
-export const isPRFiles = () => /^pull\/\d+\/files/.test(getRepoPath());
-
-export const isPRCommit = () => /^pull\/\d+\/commits\/[0-9a-f]{5,40}/.test(getRepoPath());
-
-export const isMilestoneList = () => /^milestones\/?$/.test(getRepoPath());
-
-export const isMilestone = () => /^milestone\/\d+/.test(getRepoPath());
-
-export const isLabelList = () => /^labels\/?(((?=\?).*)|$)/.test(getRepoPath());
-
-export const isLabel = () => /^labels\/\w+/.test(getRepoPath());
-
-export const isCommitList = () => /^commits\//.test(getRepoPath());
-
-export const isSingleCommit = () => /^commit\/[0-9a-f]{5,40}/.test(getRepoPath());
+export const isBlame = () => /^blame\//.test(getRepoPath());
 
 export const isCommit = () => isSingleCommit() || isPRCommit() || (isPRFiles() && select.exists('.full-commit'));
 
+export const isCommitList = () => /^commits\//.test(getRepoPath());
+
 export const isCompare = () => /^compare/.test(getRepoPath());
+
+export const isDashboard = () => location.pathname === '/' || /^(\/orgs\/[^/]+)?\/dashboard/.test(location.pathname);
+
+export const isEnterprise = () => location.hostname !== 'github.com' && location.hostname !== 'gist.github.com';
+
+export const isGist = () => location.hostname.startsWith('gist.') || location.pathname.startsWith('gist/');
+
+export const isIssue = () => /^issues\/\d+/.test(getRepoPath());
+
+export const isIssueList = () => /^issues\/?$/.test(getRepoPath());
+
+export const isIssueSearch = () => location.pathname.startsWith('/issues');
+
+export const isLabel = () => /^labels\/\w+/.test(getRepoPath());
+
+export const isLabelList = () => /^labels\/?(((?=\?).*)|$)/.test(getRepoPath());
+
+export const isMilestone = () => /^milestone\/\d+/.test(getRepoPath());
+
+export const isMilestoneList = () => /^milestones\/?$/.test(getRepoPath());
+
+export const isNotifications = () => /^\/(?:[^/]+\/[^/]+\/)?notifications/.test(location.pathname);
+
+export const isPR = () => /^pull\/\d+/.test(getRepoPath());
+
+export const isPRCommit = () => /^pull\/\d+\/commits\/[0-9a-f]{5,40}/.test(getRepoPath());
+
+export const isPRFiles = () => /^pull\/\d+\/files/.test(getRepoPath());
+
+export const isPRList = () => /^pulls\/?$/.test(getRepoPath());
+
+export const isPRSearch = () => location.pathname.startsWith('/pulls');
 
 export const isQuickPR = () => isCompare() && /[?&]quick_pull=1(&|$)/.test(location.search);
 
 export const isReleases = () => /^(releases|tags)/.test(getRepoPath());
 
-export const isBlame = () => /^blame\//.test(getRepoPath());
+export const isRepo = () => /^\/[^/]+\/[^/]+/.test(location.pathname) &&
+	!isGist() &&
+	!isTrending() &&
+	!isNotifications();
+
+export const isRepoRoot = () => /^(tree[/][^/]+)?$/.test(getRepoPath());
 
 export const isRepoSettings = () => /^settings/.test(getRepoPath());
 
-export const getOwnerAndRepo = () => {
-	const [, ownerName, repoName] = location.pathname.split('/');
+export const isRepoTree = () => /^tree\//.test(getRepoPath());
 
-	return {
-		ownerName,
-		repoName
-	};
-};
+export const isSingleCommit = () => /^commit\/[0-9a-f]{5,40}/.test(getRepoPath());
 
 export const isSingleFile = () => /^blob\//.test(getRepoPath());
+
+export const isTrending = () => location.pathname.startsWith('/trending');
 
 export const hasCommentForm = () => select.exists('.js-previewable-comment-form');
--- test/page-detect.js
+++ test/page-detect.js
@@ -17,253 +17,6 @@ function urlMatcherMacro(t, detectFn, shouldMatch = [], shouldNotMatch = []) {
 	}
 }
 
-test('isGist', urlMatcherMacro, pageDetect.isGist, [
-	'https://gist.github.com',
-	'http://gist.github.com',
-	'https://gist.github.com/sindresorhus/0ea3c2845718a0a0f0beb579ff14f064'
-], [
-	'https://github.com',
-	'https://help.github.com/'
-]);
-
-test('isDashboard', urlMatcherMacro, pageDetect.isDashboard, [
-	'https://github.com/',
-	'https://github.com',
-	'https://github.com/orgs/test/dashboard',
-	'https://github.com/dashboard'
-], [
-	'https://github.com/sindresorhus'
-]);
-
-test('isEnterprise', urlMatcherMacro, pageDetect.isEnterprise, [
-	'https://github.big-corp.com/',
-	'https://not-github.com/',
-	'https://my-little-hub.com/'
-], [
-	'https://github.com/',
-	'https://gist.github.com/'
-]);
-
-test('isTrending', urlMatcherMacro, pageDetect.isTrending, [
-	'https://github.com/trending',
-	'https://github.com/trending/developers',
-	'https://github.com/trending/unknown'
-], [
-	'https://github.com/settings/trending',
-	'https://github.com/watching',
-	'https://github.com/jaredhanson/node-trending/tree/master/lib/trending'
-]);
-
-test('isRepo', urlMatcherMacro, pageDetect.isRepo, [
-	'http://github.com/sindresorhus/refined-github',
-	'https://github.com/sindresorhus/refined-github/issues/146',
-	'https://github.com/sindresorhus/notifications/',
-	'https://github.com/sindresorhus/refined-github/pull/145'
-], [
-	'https://github.com/sindresorhus',
-	'https://github.com',
-	'https://github.com/stars',
-	'http://github.com/sindresorhus/refined-github/notifications',
-	'https://github.com/sindresorhus/notifications/notifications',
-	'https://github.com/trending/developers'
-]);
-
-test('isRepoRoot', urlMatcherMacro, pageDetect.isRepoRoot, [
-	'https://github.com/sindresorhus/refined-github',
-	'https://github.com/sindresorhus/refined-github/',
-	'https://github.com/sindresorhus/refined-github/tree/native-copy-buttons',
-	'https://github.com/sindresorhus/refined-github/tree/native-copy-buttons/',
-	'https://github.com/sindresorhus/refined-github/tree/03fa6b8b4d6e68dea9dc9bee1d197ef5d992fbd6',
-	'https://github.com/sindresorhus/refined-github/tree/03fa6b8b4d6e68dea9dc9bee1d197ef5d992fbd6/'
-], [
-	'https://github.com/',
-	'https://github.com/tree/master/issues',
-	'https://github.com/sindresorhus/refined-github/issues',
-	'https://github.com/sindresorhus/refined-github/tree/master/extension',
-	'https://github.com/sindresorhus/refined-github/tree/master/tree/master'
-]);
-
-test('isRepoTree', urlMatcherMacro, pageDetect.isRepoTree, [
-	'https://github.com/sindresorhus/refined-github/tree/master/extension',
-	'https://github.com/sindresorhus/refined-github/tree/0.13.0/extension',
-	'https://github.com/sindresorhus/refined-github/tree/57bf435ee12d14b482df0bbd88013a2814c7512e/extension',
-	'https://github.com/sindresorhus/refined-github/tree/57bf4'
-], [
-	'https://github.com/sindresorhus/refined-github/issues',
-	'https://github.com/sindresorhus/refined-github'
-]);
-
-test('isIssueSearch', urlMatcherMacro, pageDetect.isIssueSearch, [
-	'https://github.com/issues'
-], [
-	'https://github.com/sindresorhus/refined-github/issues',
-	'https://github.com/sindresorhus/refined-github/issues/170'
-]);
-
-test('isIssueList', urlMatcherMacro, pageDetect.isIssueList, [
-	'http://github.com/sindresorhus/ava/issues'
-], [
-	'http://github.com/sindresorhus/ava',
-	'https://github.com',
-	'https://github.com/sindresorhus/refined-github/issues/170'
-]);
-
-test('isIssue', urlMatcherMacro, pageDetect.isIssue, [
-	'https://github.com/sindresorhus/refined-github/issues/146'
-], [
-	'http://github.com/sindresorhus/ava',
-	'https://github.com',
-	'https://github.com/sindresorhus/refined-github/issues'
-]);
-
-test('isPRSearch', urlMatcherMacro, pageDetect.isPRSearch, [
-	'https://github.com/pulls'
-], [
-	'https://github.com/sindresorhus/refined-github/pulls',
-	'https://github.com/sindresorhus/refined-github/pull/148'
-]);
-
-test('isPRList', urlMatcherMacro, pageDetect.isPRList, [
-	'https://github.com/sindresorhus/refined-github/pulls'
-], [
-	'http://github.com/sindresorhus/ava',
-	'https://github.com',
-	'https://github.com/sindresorhus/refined-github/pull/148'
-]);
-
-test('isPR', urlMatcherMacro, pageDetect.isPR, [
-	'https://github.com/sindresorhus/refined-github/pull/148'
-], [
-	'http://github.com/sindresorhus/ava',
-	'https://github.com',
-	'https://github.com/sindresorhus/refined-github/pulls'
-]);
-
-test('isPRFiles', urlMatcherMacro, pageDetect.isPRFiles, [
-	'https://github.com/sindresorhus/refined-github/pull/148/files'
-], [
-	'https://github.com/sindresorhus/refined-github/pull/148',
-	'https://github.com/sindresorhus/refined-github/pull/commits',
-	'https://github.com/sindresorhus/refined-github/pulls'
-]);
-
-test('isPRCommit', urlMatcherMacro, pageDetect.isPRCommit, [
-	'https://github.com/sindresorhus/refined-github/pull/148/commits/0019603b83bd97c2f7ef240969f49e6126c5ec85',
-	'https://github.com/sindresorhus/refined-github/pull/148/commits/00196'
-], [
-	'https://github.com/sindresorhus/refined-github/pull/148',
-	'https://github.com/sindresorhus/refined-github/pull/commits',
-	'https://github.com/sindresorhus/refined-github/pulls'
-]);
-
-test('isMilestone', urlMatcherMacro, pageDetect.isMilestone, [
-	'https://github.com/sindresorhus/refined-github/milestone/12'
-], [
-	'http://github.com/sindresorhus/ava',
-	'https://github.com',
-	'https://github.com/sindresorhus/refined-github/milestones'
-]);
-
-test('isCommitList', urlMatcherMacro, pageDetect.isCommitList, [
-	'https://github.com/sindresorhus/refined-github/commits/master?page=2',
-	'https://github.com/sindresorhus/refined-github/commits/test-branch',
-	'https://github.com/sindresorhus/refined-github/commits/0.13.0',
-	'https://github.com/sindresorhus/refined-github/commits/230c2',
-	'https://github.com/sindresorhus/refined-github/commits/230c2935fc5aea9a681174ddbeba6255ca040d63'
-], [
-	'https://github.com/sindresorhus/refined-github/pull/148',
-	'https://github.com/sindresorhus/refined-github/pull/commits',
-	'https://github.com/sindresorhus/refined-github/branches'
-]);
-
-test('isSingleCommit', urlMatcherMacro, pageDetect.isSingleCommit, [
-	'https://github.com/sindresorhus/refined-github/commit/5b614b9035f2035b839f48b4db7bd5c3298d526f',
-	'https://github.com/sindresorhus/refined-github/commit/5b614'
-], [
-	'https://github.com/sindresorhus/refined-github/pull/148/commits',
-	'https://github.com/sindresorhus/refined-github/branches'
-]);
-
-test('isReleases', urlMatcherMacro, pageDetect.isReleases, [
-	'https://github.com/sindresorhus/refined-github/releases'
-], [
-	'https://github.com/sindresorhus/refined-github',
-	'https://github.com/sindresorhus/refined-github/graphs'
-]);
-
-test('isBlame', urlMatcherMacro, pageDetect.isBlame, [
-	'https://github.com/sindresorhus/refined-github/blame/master/package.json'
-], [
-	'https://github.com/sindresorhus/refined-github/blob/master/package.json'
-]);
-
-test('isNotifications', urlMatcherMacro, pageDetect.isNotifications, [
-	'https://github.com/notifications',
-	'https://github.com/notifications/participating',
-	'http://github.com/sindresorhus/refined-github/notifications',
-	'https://github.com/sindresorhus/notifications/notifications',
-	'https://github.com/notifications?all=1'
-], [
-	'https://github.com/settings/notifications',
-	'https://github.com/watching',
-	'https://github.com/sindresorhus/notifications/',
-	'https://github.com/jaredhanson/node-notifications/tree/master/lib/notifications'
-]);
-
-test('isSingleFile', urlMatcherMacro, pageDetect.isSingleFile, [
-	'https://github.com/sindresorhus/refined-github/blob/master/.gitattributes',
-	'https://github.com/sindresorhus/refined-github/blob/fix-narrow-diff/extension/content.css'
-], [
-	'https://github.com/sindresorhus/refined-github/pull/164/files',
-	'https://github.com/sindresorhus/refined-github/commit/57bf4'
-]);
-
-test('isRepoSettings', urlMatcherMacro, pageDetect.isRepoSettings, [
-	'https://github.com/sindresorhus/refined-github/settings',
-	'https://github.com/sindresorhus/refined-github/settings/branches'
-], [
-	'https://github.com/sindresorhus/refined-github/releases'
-]);
-
-test('isCompare', urlMatcherMacro, pageDetect.isCompare, [
-	'https://github.com/sindresorhus/refined-github/compare',
-	'https://github.com/sindresorhus/refined-github/compare/'
-], [
-	'https://github.com/sindresorhus/refined-github',
-	'https://github.com/sindresorhus/refined-github/graphs'
-]);
-
-test('isQuickPR', urlMatcherMacro, pageDetect.isQuickPR, [
-	'https://github.com/sindresorhus/refined-github/compare/master...branch-name?quick_pull=1',
-	'https://github.com/sindresorhus/refined-github/compare/branch-1...branch-2?quick_pull=1',
-	'https://github.com/sindresorhus/refined-github/compare/test-branch?quick_pull=1'
-], [
-	'https://github.com/sindresorhus/refined-github',
-	'https://github.com/sindresorhus/refined-github/compare',
-	'https://github.com/sindresorhus/refined-github/compare/',
-	'https://github.com/sindresorhus/refined-github/compare/test-branch',
-	'https://github.com/sindresorhus/refined-github/compare/branch-1...branch-2',
-	'https://github.com/sindresorhus/refined-github/compare/branch-1...branch-2?expand=1'
-]);
-
-test('getOwnerAndRepo', t => {
-	const ownerAndRepo = {
-		'https://github.com/sindresorhus/refined-github/pull/148': {
-			ownerName: 'sindresorhus',
-			repoName: 'refined-github'
-		},
-		'https://github.com/DrewML/GifHub/blob/master/.gitignore': {
-			ownerName: 'DrewML',
-			repoName: 'GifHub'
-		}
-	};
-
-	Object.keys(ownerAndRepo).forEach(url => {
-		location.href = url;
-		t.deepEqual(ownerAndRepo[url], pageDetect.getOwnerAndRepo());
-	});
-});
-
 test('getRepoPath', t => {
 	const pairs = new Map([
 		[
@@ -305,3 +58,251 @@ test('getRepoPath', t => {
 		t.is(result, pageDetect.getRepoPath(url));
 	}
 });
+
+test('getOwnerAndRepo', t => {
+	const ownerAndRepo = {
+		'https://github.com/sindresorhus/refined-github/pull/148': {
+			ownerName: 'sindresorhus',
+			repoName: 'refined-github'
+		},
+		'https://github.com/DrewML/GifHub/blob/master/.gitignore': {
+			ownerName: 'DrewML',
+			repoName: 'GifHub'
+		}
+	};
+
+	Object.keys(ownerAndRepo).forEach(url => {
+		location.href = url;
+		t.deepEqual(ownerAndRepo[url], pageDetect.getOwnerAndRepo());
+	});
+});
+
+test('isBlame', urlMatcherMacro, pageDetect.isBlame, [
+	'https://github.com/sindresorhus/refined-github/blame/master/package.json'
+], [
+	'https://github.com/sindresorhus/refined-github/blob/master/package.json'
+]);
+
+test('isCommitList', urlMatcherMacro, pageDetect.isCommitList, [
+	'https://github.com/sindresorhus/refined-github/commits/master?page=2',
+	'https://github.com/sindresorhus/refined-github/commits/test-branch',
+	'https://github.com/sindresorhus/refined-github/commits/0.13.0',
+	'https://github.com/sindresorhus/refined-github/commits/230c2',
+	'https://github.com/sindresorhus/refined-github/commits/230c2935fc5aea9a681174ddbeba6255ca040d63'
+], [
+	'https://github.com/sindresorhus/refined-github/pull/148',
+	'https://github.com/sindresorhus/refined-github/pull/commits',
+	'https://github.com/sindresorhus/refined-github/branches'
+]);
+
+test('isCompare', urlMatcherMacro, pageDetect.isCompare, [
+	'https://github.com/sindresorhus/refined-github/compare',
+	'https://github.com/sindresorhus/refined-github/compare/'
+], [
+	'https://github.com/sindresorhus/refined-github',
+	'https://github.com/sindresorhus/refined-github/graphs'
+]);
+
+test('isDashboard', urlMatcherMacro, pageDetect.isDashboard, [
+	'https://github.com/',
+	'https://github.com',
+	'https://github.com/orgs/test/dashboard',
+	'https://github.com/dashboard'
+], [
+	'https://github.com/sindresorhus'
+]);
+
+test('isEnterprise', urlMatcherMacro, pageDetect.isEnterprise, [
+	'https://github.big-corp.com/',
+	'https://not-github.com/',
+	'https://my-little-hub.com/'
+], [
+	'https://github.com/',
+	'https://gist.github.com/'
+]);
+
+test('isGist', urlMatcherMacro, pageDetect.isGist, [
+	'https://gist.github.com',
+	'http://gist.github.com',
+	'https://gist.github.com/sindresorhus/0ea3c2845718a0a0f0beb579ff14f064'
+], [
+	'https://github.com',
+	'https://help.github.com/'
+]);
+
+test('isIssue', urlMatcherMacro, pageDetect.isIssue, [
+	'https://github.com/sindresorhus/refined-github/issues/146'
+], [
+	'http://github.com/sindresorhus/ava',
+	'https://github.com',
+	'https://github.com/sindresorhus/refined-github/issues'
+]);
+
+test('isIssueList', urlMatcherMacro, pageDetect.isIssueList, [
+	'http://github.com/sindresorhus/ava/issues'
+], [
+	'http://github.com/sindresorhus/ava',
+	'https://github.com',
+	'https://github.com/sindresorhus/refined-github/issues/170'
+]);
+
+test('isIssueSearch', urlMatcherMacro, pageDetect.isIssueSearch, [
+	'https://github.com/issues'
+], [
+	'https://github.com/sindresorhus/refined-github/issues',
+	'https://github.com/sindresorhus/refined-github/issues/170'
+]);
+
+test('isMilestone', urlMatcherMacro, pageDetect.isMilestone, [
+	'https://github.com/sindresorhus/refined-github/milestone/12'
+], [
+	'http://github.com/sindresorhus/ava',
+	'https://github.com',
+	'https://github.com/sindresorhus/refined-github/milestones'
+]);
+
+test('isNotifications', urlMatcherMacro, pageDetect.isNotifications, [
+	'https://github.com/notifications',
+	'https://github.com/notifications/participating',
+	'http://github.com/sindresorhus/refined-github/notifications',
+	'https://github.com/sindresorhus/notifications/notifications',
+	'https://github.com/notifications?all=1'
+], [
+	'https://github.com/settings/notifications',
+	'https://github.com/watching',
+	'https://github.com/sindresorhus/notifications/',
+	'https://github.com/jaredhanson/node-notifications/tree/master/lib/notifications'
+]);
+
+test('isPR', urlMatcherMacro, pageDetect.isPR, [
+	'https://github.com/sindresorhus/refined-github/pull/148'
+], [
+	'http://github.com/sindresorhus/ava',
+	'https://github.com',
+	'https://github.com/sindresorhus/refined-github/pulls'
+]);
+
+test('isPRCommit', urlMatcherMacro, pageDetect.isPRCommit, [
+	'https://github.com/sindresorhus/refined-github/pull/148/commits/0019603b83bd97c2f7ef240969f49e6126c5ec85',
+	'https://github.com/sindresorhus/refined-github/pull/148/commits/00196'
+], [
+	'https://github.com/sindresorhus/refined-github/pull/148',
+	'https://github.com/sindresorhus/refined-github/pull/commits',
+	'https://github.com/sindresorhus/refined-github/pulls'
+]);
+
+test('isPRFiles', urlMatcherMacro, pageDetect.isPRFiles, [
+	'https://github.com/sindresorhus/refined-github/pull/148/files'
+], [
+	'https://github.com/sindresorhus/refined-github/pull/148',
+	'https://github.com/sindresorhus/refined-github/pull/commits',
+	'https://github.com/sindresorhus/refined-github/pulls'
+]);
+
+test('isPRList', urlMatcherMacro, pageDetect.isPRList, [
+	'https://github.com/sindresorhus/refined-github/pulls'
+], [
+	'http://github.com/sindresorhus/ava',
+	'https://github.com',
+	'https://github.com/sindresorhus/refined-github/pull/148'
+]);
+
+test('isPRSearch', urlMatcherMacro, pageDetect.isPRSearch, [
+	'https://github.com/pulls'
+], [
+	'https://github.com/sindresorhus/refined-github/pulls',
+	'https://github.com/sindresorhus/refined-github/pull/148'
+]);
+
+test('isQuickPR', urlMatcherMacro, pageDetect.isQuickPR, [
+	'https://github.com/sindresorhus/refined-github/compare/master...branch-name?quick_pull=1',
+	'https://github.com/sindresorhus/refined-github/compare/branch-1...branch-2?quick_pull=1',
+	'https://github.com/sindresorhus/refined-github/compare/test-branch?quick_pull=1'
+], [
+	'https://github.com/sindresorhus/refined-github',
+	'https://github.com/sindresorhus/refined-github/compare',
+	'https://github.com/sindresorhus/refined-github/compare/',
+	'https://github.com/sindresorhus/refined-github/compare/test-branch',
+	'https://github.com/sindresorhus/refined-github/compare/branch-1...branch-2',
+	'https://github.com/sindresorhus/refined-github/compare/branch-1...branch-2?expand=1'
+]);
+
+test('isReleases', urlMatcherMacro, pageDetect.isReleases, [
+	'https://github.com/sindresorhus/refined-github/releases'
+], [
+	'https://github.com/sindresorhus/refined-github',
+	'https://github.com/sindresorhus/refined-github/graphs'
+]);
+
+test('isRepo', urlMatcherMacro, pageDetect.isRepo, [
+	'http://github.com/sindresorhus/refined-github',
+	'https://github.com/sindresorhus/refined-github/issues/146',
+	'https://github.com/sindresorhus/notifications/',
+	'https://github.com/sindresorhus/refined-github/pull/145'
+], [
+	'https://github.com/sindresorhus',
+	'https://github.com',
+	'https://github.com/stars',
+	'http://github.com/sindresorhus/refined-github/notifications',
+	'https://github.com/sindresorhus/notifications/notifications',
+	'https://github.com/trending/developers'
+]);
+
+test('isRepoRoot', urlMatcherMacro, pageDetect.isRepoRoot, [
+	'https://github.com/sindresorhus/refined-github',
+	'https://github.com/sindresorhus/refined-github/',
+	'https://github.com/sindresorhus/refined-github/tree/native-copy-buttons',
+	'https://github.com/sindresorhus/refined-github/tree/native-copy-buttons/',
+	'https://github.com/sindresorhus/refined-github/tree/03fa6b8b4d6e68dea9dc9bee1d197ef5d992fbd6',
+	'https://github.com/sindresorhus/refined-github/tree/03fa6b8b4d6e68dea9dc9bee1d197ef5d992fbd6/'
+], [
+	'https://github.com/',
+	'https://github.com/tree/master/issues',
+	'https://github.com/sindresorhus/refined-github/issues',
+	'https://github.com/sindresorhus/refined-github/tree/master/extension',
+	'https://github.com/sindresorhus/refined-github/tree/master/tree/master'
+]);
+
+test('isRepoSettings', urlMatcherMacro, pageDetect.isRepoSettings, [
+	'https://github.com/sindresorhus/refined-github/settings',
+	'https://github.com/sindresorhus/refined-github/settings/branches'
+], [
+	'https://github.com/sindresorhus/refined-github/releases'
+]);
+
+test('isRepoTree', urlMatcherMacro, pageDetect.isRepoTree, [
+	'https://github.com/sindresorhus/refined-github/tree/master/extension',
+	'https://github.com/sindresorhus/refined-github/tree/0.13.0/extension',
+	'https://github.com/sindresorhus/refined-github/tree/57bf435ee12d14b482df0bbd88013a2814c7512e/extension',
+	'https://github.com/sindresorhus/refined-github/tree/57bf4'
+], [
+	'https://github.com/sindresorhus/refined-github/issues',
+	'https://github.com/sindresorhus/refined-github'
+]);
+
+test('isSingleCommit', urlMatcherMacro, pageDetect.isSingleCommit, [
+	'https://github.com/sindresorhus/refined-github/commit/5b614b9035f2035b839f48b4db7bd5c3298d526f',
+	'https://github.com/sindresorhus/refined-github/commit/5b614'
+], [
+	'https://github.com/sindresorhus/refined-github/pull/148/commits',
+	'https://github.com/sindresorhus/refined-github/branches'
+]);
+
+test('isSingleFile', urlMatcherMacro, pageDetect.isSingleFile, [
+	'https://github.com/sindresorhus/refined-github/blob/master/.gitattributes',
+	'https://github.com/sindresorhus/refined-github/blob/fix-narrow-diff/extension/content.css'
+], [
+	'https://github.com/sindresorhus/refined-github/pull/164/files',
+	'https://github.com/sindresorhus/refined-github/commit/57bf4'
+]);
+
+test('isTrending', urlMatcherMacro, pageDetect.isTrending, [
+	'https://github.com/trending',
+	'https://github.com/trending/developers',
+	'https://github.com/trending/unknown'
+], [
+	'https://github.com/settings/trending',
+	'https://github.com/watching',
+	'https://github.com/jaredhanson/node-trending/tree/master/lib/trending'
+]);
+
