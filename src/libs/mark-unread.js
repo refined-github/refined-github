e1498a73f7d4e5581d332fb08fbeb6f3edd90cfc
Federico Brigante <github@bfred.it>
2017-11-03 02:21:15 +0800

    Put me to sleep when 2017 ends

--- src/libs/mark-unread.js
+++ src/libs/mark-unread.js
@@ -341,6 +341,11 @@ async function setup() {
 	gitHubInjection(() => {
 		destroy();
 
+		// Remove old data from previous storage
+		// Drop code in 2018
+		localStorage.removeItem('_unreadNotifications_migrated');
+		localStorage.removeItem('unreadNotifications');
+
 		if (pageDetect.isNotifications()) {
 			renderNotifications();
 			addCustomAllReadBtn();
