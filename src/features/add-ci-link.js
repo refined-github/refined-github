c208f6ac0c23075a5d55734edca51f7345b13f77
Federico Brigante <github@bfred.it>
2017-11-06 20:32:08 +0800

    Avoid piling up when navigating history on ajaxed pages

--- src/features/add-ci-link.js
+++ src/features/add-ci-link.js
@@ -3,9 +3,13 @@ import domify from '../libs/domify';
 import {getRepoURL} from '../libs/page-detect';
 
 export default async function () {
+	if (select.exists('.rgh-ci-link')) {
+		return;
+	}
 	const html = await fetch(`${location.origin}/${getRepoURL()}/commits/`).then(r => r.text());
 	const status = select('.commit-build-statuses', domify(html));
 	if (status) {
+		status.classList.add('rgh-ci-link');
 		select('.pagehead [itemprop="name"]').append(status);
 	}
 }
