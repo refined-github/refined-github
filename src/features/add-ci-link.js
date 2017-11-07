8b756893de7dc2c737e3f3a6a0bf46369fe3d1c6
Federico Brigante <github@bfred.it>
2017-11-08 00:16:53 +0800

    Make code more readable
    + Prevent quick repeated executions from piling up

--- src/features/add-ci-link.js
+++ src/features/add-ci-link.js
@@ -3,33 +3,42 @@ import domify from '../libs/domify';
 import {getRepoURL} from '../libs/page-detect';
 
 // This var will be:
-// undefined on first load
-// a dom element after a successful fetch
-// false after a failed fetch
-let status;
+// - undefined on first load
+// - a Promised dom element after a successful fetch
+// - false after a failed fetch
+let request;
+
+async function fetchStatus() {
+	const url = `${location.origin}/${getRepoURL()}/commits/`;
+	const dom = await fetch(url).then(r => r.text()).then(domify);
+
+	const icon = select('.commit-build-statuses', dom);
+
+	// This will error if the element isn't found.
+	// It's caught later.
+	icon.classList.add('rgh-ci-link');
+
+	return icon;
+}
 
 export default async function () {
 	// Avoid duplicates and avoid on pages that already failed to load
-	if (select.exists('.rgh-ci-link') || status === false) {
+	if (request === false || select.exists('.rgh-ci-link')) {
 		return;
 	}
 
-	if (status) {
-		// If truthy, we're in an ajaxed:load event
-		// Skip status re-animation
-		status.style.animation = 'none';
-	} else {
-		// It's undefined, we're displaying it for the first time
-		try {
-			const html = await fetch(`${location.origin}/${getRepoURL()}/commits/`).then(r => r.text());
-			status = select('.commit-build-statuses', domify(html));
-			status.classList.add('rgh-ci-link');
-		} catch (err) {
-			// Network failure or missing element
-			status = false;
-			return;
+	try {
+		if (request) {
+			// Skip icon re-animation because
+			// it was probably already animated once
+			(await request).style.animation = 'none';
+		} else {
+			request = fetchStatus();
 		}
+		select('.pagehead [itemprop="name"]').append(await request);
+	} catch (err) {
+		// Network failure or no CI status found.
+		// Don’t try again
+		request = false;
 	}
-
-	select('.pagehead [itemprop="name"]').append(status);
 }
