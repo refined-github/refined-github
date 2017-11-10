9750e8b39b91fdff4b6c8ed95b982dcf00019b21
Federico Brigante <github@bfred.it>
2017-11-10 16:13:13 +0800

    Turn IF positive

--- src/features/remove-upload-files-button.js
+++ src/features/remove-upload-files-button.js
@@ -4,12 +4,10 @@ import * as pageDetect from '../libs/page-detect';
 const repoUrl = pageDetect.getRepoURL();
 
 export default () => {
-	if (!pageDetect.isRepoRoot()) {
-		return;
-	}
-
-	const uploadFilesButton = select(`a[href^="/${repoUrl}/upload"]`);
-	if (uploadFilesButton) {
-		uploadFilesButton.remove();
+	if (pageDetect.isRepoRoot()) {
+		const uploadFilesButton = select(`.file-navigation a[href^="/${repoUrl}/upload"]`);
+		if (uploadFilesButton) {
+			uploadFilesButton.remove();
+		}
 	}
 };
