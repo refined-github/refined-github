d9f4adb79878e8caed4faa1d929d7d6e8ec2e68e
Federico Brigante <github@bfred.it>
2017-11-15 10:37:05 +0800

    Only show "delete fork" link if the user is allowed to (#805)

--- src/features/add-delete-fork-link.js
+++ src/features/add-delete-fork-link.js
@@ -5,9 +5,10 @@ import * as pageDetect from '../libs/page-detect';
 const repoUrl = pageDetect.getRepoURL();
 
 export default function () {
+	const canDeleteFork = select.exists('.reponav-item [data-selected-links~="repo_settings"]');
 	const postMergeDescription = select('#partial-pull-merging .merge-branch-description');
 
-	if (postMergeDescription) {
+	if (canDeleteFork && postMergeDescription) {
 		const currentBranch = postMergeDescription.querySelector('.commit-ref');
 		const forkPath = currentBranch ? currentBranch.title.split(':')[0] : null;
 
