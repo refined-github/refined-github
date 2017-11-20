50f2249fa479811d85b5d4720c26ba2ee2ff8cba
donocode <donohoe.nick@gmail.com>
2017-11-20 23:48:15 +0000

    use JSX for gist element

--- src/features/embed-gist-inline.js
+++ src/features/embed-gist-inline.js
@@ -1,3 +1,4 @@
+import {h} from 'dom-chef';
 import select from 'select-dom';
 
 const gistRegex = /gist\.github\.com\/\w*?\/\w*/;
@@ -7,9 +8,12 @@ const getJsonUrl = href => `${href}.json`.replace(/\.jso?n?\.json$/, '.json');
 const getGistData = href => fetch(getJsonUrl(href)).then(response => response.json());
 
 const createGistElement = gistData => {
-	const el = document.createElement('div');
-	const style = `<link rel="stylesheet" href="${gistData.stylesheet}">`;
-	el.innerHTML = style + gistData.div;
+	const el = (
+		<div>
+			<link rel="stylesheet" href={gistData.stylesheet}/>
+			<div dangerouslySetInnerHTML={{__html: gistData.div}} />
+		</div>
+	);
 	return el;
 };
 
