9d4c85c54bd72880f9eb052ca5105312b5ff13e0
Simon Gabler <sgr@miamed.de>
2017-11-20 21:16:52 +0100

    Implement code improvements suggested in review

--- src/features/expand-collapse-outdated-comments.js
+++ src/features/expand-collapse-outdated-comments.js
@@ -2,29 +2,29 @@ import select from 'select-dom';
 
 function addTooltip(button) {
 	button.setAttribute('aria-label', 'Alt + click to expand/collapse all outdated comments');
-	button.classList.add('tooltipped', 'tooltipped-n');
+	button.classList.add('rgh-tooltipped', 'tooltipped', 'tooltipped-n');
 }
 
 export default () => {
 	const showOutdatedButtons = select.all('.show-outdated-button, .hide-outdated-button');
-	showOutdatedButtons.forEach(button => {
+	for (const button of showOutdatedButtons) {
 		addTooltip(button);
 		button.addEventListener('click', e => {
 			if (e.altKey) {
 				const parentElement = e.target.parentNode;
 				const viewportOffset = parentElement.getBoundingClientRect().top;
+
+				let buttons;
 				if (e.target.classList.contains('show-outdated-button')) {
-					select.all('.outdated-comment:not(.open) .show-outdated-button').forEach(button => {
-						if (button !== e.target) {
-							button.click();
-						}
-					});
+					buttons = select.all('.outdated-comment:not(.open) .show-outdated-button');
 				} else {
-					select.all('.outdated-comment.open .hide-outdated-button').forEach(button => {
-						if (button !== e.target) {
-							button.click();
-						}
-					});
+					buttons = select.all('.outdated-comment.open .hide-outdated-button');
+				}
+
+				for (const button of buttons) {
+					if (button !== e.target) {
+						button.click();
+					}
 				}
 				// Scroll to original position where the click occurred after the rendering of all click events is done
 				requestAnimationFrame(() => {
@@ -33,5 +33,5 @@ export default () => {
 				});
 			}
 		});
-	});
+	}
 };
