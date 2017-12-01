48a4f1cfe21f41659e487c5c29cfb025b1dfbce5
Federico Brigante <github@bfred.it>
2017-12-01 12:32:32 +0700

    Extract function to top level

--- src/features/expand-collapse-outdated-comments.js
+++ src/features/expand-collapse-outdated-comments.js
@@ -5,35 +5,36 @@ function addTooltip(button) {
 	button.classList.add('rgh-tooltipped', 'tooltipped', 'tooltipped-n');
 }
 
-export default () => {
-	const showOutdatedButtons = select.all('.show-outdated-button, .hide-outdated-button');
-	$('.js-discussion').on('click', '.show-outdated-button, .hide-outdated-button', event => {
-		if (event.altKey) {
-			const parentElement = event.target.parentNode;
-			const viewportOffset = parentElement.getBoundingClientRect().top;
+function handleClick(event) {
+	if (event.altKey) {
+		const parentElement = event.target.parentNode;
+		const viewportOffset = parentElement.getBoundingClientRect().top;
 
-			let buttons;
-			if (event.target.classList.contains('show-outdated-button')) {
-				buttons = select.all('.outdated-comment:not(.open) .show-outdated-button');
-			} else {
-				buttons = select.all('.outdated-comment.open .hide-outdated-button');
-			}
-
-			for (const button of buttons) {
-				if (button !== event.target) {
-					button.click();
-				}
-			}
-
-			// Scroll to original position where the click occurred after the rendering of all click events is done
-			requestAnimationFrame(() => {
-				const newOffset = parentElement.getBoundingClientRect().top;
-				window.scrollBy(0, newOffset - viewportOffset);
-			});
+		let buttons;
+		if (event.target.classList.contains('show-outdated-button')) {
+			buttons = select.all('.outdated-comment:not(.open) .show-outdated-button');
+		} else {
+			buttons = select.all('.outdated-comment.open .hide-outdated-button');
 		}
-	});
 
-	for (const button of showOutdatedButtons) {
+		for (const button of buttons) {
+			if (button !== event.target) {
+				button.click();
+			}
+		}
+
+		// Scroll to original position where the click occurred after the rendering of all click events is done
+		requestAnimationFrame(() => {
+			const newOffset = parentElement.getBoundingClientRect().top;
+			window.scrollBy(0, newOffset - viewportOffset);
+		});
+	}
+}
+
+export default function () {
+	$('.js-discussion').on('click', '.show-outdated-button, .hide-outdated-button', handleClick);
+
+	for (const button of select.all('.show-outdated-button, .hide-outdated-button')) {
 		addTooltip(button);
 	}
-};
+}
