0452cf4c32660060c7accbd2e7d3c5165aaaa568
donocode <donohoe.nick@gmail.com>
2017-12-06 22:21:11 +0000

    fix xo lint errors

--- src/features/embed-gist-inline.js
+++ src/features/embed-gist-inline.js
@@ -1,30 +1,29 @@
-import { h } from 'dom-chef';
 import select from 'select-dom';
 
 const isGist = link => link.hostname.startsWith('gist.') || link.pathname.startsWith('gist/');
 
 const getJsonUrl = href => `${href}.json`.replace(/\.(js|json)\.json$/, '.json');
 const getGistData = async href => {
-  const response = await fetch(getJsonUrl(href));
-  return await response.json();
+	const response = await fetch(getJsonUrl(href));
+	return response.json();
 };
 
 const createGistElement = gistData => (
-  <div>
-    <link rel="stylesheet" href={gistData.stylesheet} />
-    <div dangerouslySetInnerHTML={{ __html: gistData.div }} />
-  </div>
+	<div>
+		<link rel="stylesheet" href={gistData.stylesheet} />
+		<div dangerouslySetInnerHTML={{__html: gistData.div}} />
+	</div>
 );
 
 export default async () => {
-  const gistLinks = select.all('.js-comment-body p a:only-child').filter(isGist);
+	const gistLinks = select.all('.js-comment-body p a:only-child').filter(isGist);
 
-  for (let link of gistLinks) {
-    try {
-      const gistData = await getGistData(link.href);
-      const gistEl = createGistElement(gistData);
-      const linkParent = link.parentNode;
-      linkParent.parentNode.replaceChild(gistEl, linkParent);
-    } catch (err) {}
-  }
+	for (const link of gistLinks) {
+		try {
+			const gistData = await getGistData(link.href);
+			const gistEl = createGistElement(gistData);
+			const linkParent = link.parentNode;
+			linkParent.parentNode.replaceChild(gistEl, linkParent);
+		} catch (err) {}
+	}
 };
