5518228061dc6655f37682b826df67e0396fbf2b
donocode <donohoe.nick@gmail.com>
2017-12-06 18:36:12 +0000

    minor review changes

--- src/features/embed-gist-inline.js
+++ src/features/embed-gist-inline.js
@@ -1,33 +1,30 @@
-import {h} from 'dom-chef';
+import { h } from 'dom-chef';
 import select from 'select-dom';
 
-const gistRegex = /gist\.github\.com\/\w*?\/\w*/;
-const isGist = link => gistRegex.test(link.href);
+const isGist = link => link.hostname.startsWith('gist.') || link.pathname.startsWith('gist/');
 
-const getJsonUrl = href => `${href}.json`.replace(/\.jso?n?\.json$/, '.json');
-const getGistData = href => fetch(getJsonUrl(href)).then(response => response.json());
-
-const createGistElement = gistData => {
-	const el = (
-		<div>
-			<link rel="stylesheet" href={gistData.stylesheet} />
-			<div dangerouslySetInnerHTML={{__html: gistData.div}} />
-		</div>
-	);
-	return el;
+const getJsonUrl = href => `${href}.json`.replace(/\.(js|json)\.json$/, '.json');
+const getGistData = async href => {
+  const response = await fetch(getJsonUrl(href));
+  return await response.json();
 };
 
-export default async () => {
-	const gistLinks = select
-		.all('.js-comment-body p a[href^="https://gist.github.com"]:only-child')
-		.filter(isGist);
+const createGistElement = gistData => (
+  <div>
+    <link rel="stylesheet" href={gistData.stylesheet} />
+    <div dangerouslySetInnerHTML={{ __html: gistData.div }} />
+  </div>
+);
 
-	for (let link of gistLinks) {
-		try {
-			const gistData = await getGistData(link.href);
-			const gistEl = createGistElement(gistData);
-			const linkParent = link.parentNode;
-			linkParent.parentNode.replaceChild(gistEl, linkParent);
-		} catch (err) {}
-	}
+export default async () => {
+  const gistLinks = select.all('.js-comment-body p a:only-child').filter(isGist);
+
+  for (let link of gistLinks) {
+    try {
+      const gistData = await getGistData(link.href);
+      const gistEl = createGistElement(gistData);
+      const linkParent = link.parentNode;
+      linkParent.parentNode.replaceChild(gistEl, linkParent);
+    } catch (err) {}
+  }
 };
