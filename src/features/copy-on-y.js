447004786f1c99293b1f34e1d46d611c3bbc55fd
Federico Brigante <github@bfred.it>
2017-12-07 23:32:09 +0800

    Use native copy feature
    Also fixes support for branches with slashes

--- src/features/copy-on-y.js
+++ src/features/copy-on-y.js
@@ -1,14 +1,10 @@
-import copyToClipboard from 'copy-text-to-clipboard';
 import select from 'select-dom';
 
 const Y_KEYCODE = 89;
 
 const handler = ({keyCode, target}) => {
 	if (keyCode === Y_KEYCODE && target.nodeName !== 'INPUT') {
-		const commitIsh = select('.commit-tease-sha').href.split('/').pop();
-		const uri = location.href.replace(/\/blob\/[\w-]+\//, `/blob/${commitIsh}/`);
-
-		copyToClipboard(uri);
+		select('#js-copy-permalink').click();
 	}
 };
 
