533190711f44608852b841b66514ab9ec4e0f5ab
Federico Brigante <github@bfred.it>
2017-12-08 00:44:59 +0800

    Lower compression for AMO reviewers

--- webpack.config.js
+++ webpack.config.js
@@ -43,15 +43,15 @@ module.exports = {
 if (process.env.NODE_ENV === 'production') {
 	module.exports.plugins.push(
 		new UglifyJSPlugin({
-			sourceMap: true,
 			uglifyOptions: {
+				// Keep it somewhat readable for AMO reviewers
 				mangle: false,
+				compress: false,
 				output: {
-					// Keep it somewhat readable for AMO reviewers
 					beautify: true,
 
 					// Reduce beautification indentation from 4 spaces to 1 to save space
-					indent_level: 1 // eslint-disable-line camelcase
+					indent_level: 2 // eslint-disable-line camelcase
 				}
 			}
 		})
