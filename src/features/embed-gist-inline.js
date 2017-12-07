9329687878f68b05704b01881a49b12f650e9d06
Federico Brigante <github@bfred.it>
2017-12-07 23:12:11 +0800

    Fix; Use Shadow DOM version; Exclude embeds

--- src/features/embed-gist-inline.js
+++ src/features/embed-gist-inline.js
@@ -1,12 +1,12 @@
+import {h} from 'dom-chef';
 import select from 'select-dom';
 
-const isGist = link => link.hostname.startsWith('gist.') || link.pathname.startsWith('gist/');
-
-const getJsonUrl = href => `${href}.json`.replace(/\.(js|json)\.json$/, '.json');
-const getGistData = async href => {
-	const response = await fetch(getJsonUrl(href));
-	return response.json();
-};
+const isGist = link =>
+	!link.pathname.includes('.') && // Exclude links to embed files
+	(
+		link.hostname.startsWith('gist.') ||
+		link.pathname.startsWith('gist/')
+	);
 
 const createGistElement = gistData => (
 	<div>
@@ -15,15 +15,14 @@ const createGistElement = gistData => (
 	</div>
 );
 
-export default async () => {
-	const gistLinks = select.all('.js-comment-body p a:only-child').filter(isGist);
-
-	for (const link of gistLinks) {
-		try {
-			const gistData = await getGistData(link.href);
-			const gistEl = createGistElement(gistData);
-			const linkParent = link.parentNode;
-			linkParent.parentNode.replaceChild(gistEl, linkParent);
-		} catch (err) {}
-	}
+async function embedGist(link) {
+	const response = await fetch(link.href + '.json');
+	const gistData = await response.json();
+	const gistEl = createGistElement(gistData);
+	link.parentNode.attachShadow({mode: 'open'}).append(gistEl);
+}
+export default () => {
+	select.all('.js-comment-body p a:only-child')
+		.filter(isGist)
+		.forEach(embedGist);
 };
