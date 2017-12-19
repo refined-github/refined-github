d3b80f96c8e932a20514cbc431423ce5c2a82303
Federico Brigante <github@bfred.it>
2017-12-19 18:48:12 +0800

    Catch errors
    Avoids "loading" forever

--- source/features/embed-gist-inline.js
+++ source/features/embed-gist-inline.js
@@ -13,25 +13,29 @@ async function embedGist(link) {
 	const info = <em> (loading)</em>;
 	link.after(info);
 
-	const response = await fetch(`${link.href}.json`);
-	const gistData = await response.json();
+	try {
+		const response = await fetch(`${link.href}.json`);
+		const gistData = await response.json();
 
-	const files = domify(gistData.div).firstElementChild;
-	const fileCount = files.children.length;
+		const files = domify(gistData.div).firstElementChild;
+		const fileCount = files.children.length;
 
-	if (fileCount > 1) {
-		info.textContent = ` (${fileCount} files)`;
-	} else {
-		link.parentNode.attachShadow({mode: 'open'}).append(
-			<style>{`
-				.gist .gist-data {
-					max-height: 16em;
-					overflow-y: auto;
-				}
-			`}</style>,
-			<link rel="stylesheet" href={gistData.stylesheet} />,
-			files
-		);
+		if (fileCount > 1) {
+			info.textContent = ` (${fileCount} files)`;
+		} else {
+			link.parentNode.attachShadow({mode: 'open'}).append(
+				<style>{`
+					.gist .gist-data {
+						max-height: 16em;
+						overflow-y: auto;
+					}
+				`}</style>,
+				<link rel="stylesheet" href={gistData.stylesheet} />,
+				files
+			);
+		}
+	} catch (err) {
+		info.remove(' (embed failed)');
 	}
 }
 export default () => {
