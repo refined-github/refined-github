cbe26d75cb09cdb446cd38065bffdcd1fc770afe
Simon Legg <leggsimon@gmail.com>
2017-12-21 17:28:43 +0000

    show public gist count

--- source/features/add-gists-link-to-profile.js
+++ source/features/add-gists-link-to-profile.js
@@ -1,13 +1,16 @@
 import {h} from 'dom-chef';
 import select from 'select-dom';
 import {getCleanPathname} from '../libs/page-detect';
+import api from '../libs/api';
 
-export default () => {
+export default async () => {
 	const username = getCleanPathname();
+	const { public_gists: publicGists } = await api(`users/${username}`)
 
 	select('.UnderlineNav-body').append(
 		<a href={`https://gist.github.com/${username}`} class="UnderlineNav-item" role="tab" title="Gists">
-			Gists
+			{'Gists '}
+			<span class="Counter">{publicGists}</span>
 		</a>
 	);
 };
