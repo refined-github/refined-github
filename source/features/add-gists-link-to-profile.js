1efa49023dfac4348ba56421cd80bfae4e228860
Simon Legg <leggsimon@gmail.com>
2017-12-25 21:04:27 +0000

    handle github enterprise gist href

--- source/features/add-gists-link-to-profile.js
+++ source/features/add-gists-link-to-profile.js
@@ -1,13 +1,14 @@
 import {h} from 'dom-chef';
 import select from 'select-dom';
-import {getCleanPathname} from '../libs/page-detect';
+import {getCleanPathname, isEnterprise} from '../libs/page-detect';
 import api from '../libs/api';
 
 export default async () => {
 	const username = getCleanPathname();
+	const href = isEnterprise() ? `/gist/${username}` : `https://gist.github.com/${username}`;
 
 	select('.UnderlineNav-body').append(
-		<a href={`https://gist.github.com/${username}`} class="UnderlineNav-item usernav-gists" role="tab" title="Gists">
+		<a href={href} class="UnderlineNav-item usernav-gists" role="tab" title="Gists">
 			{'Gists '}
 		</a>
 	);
