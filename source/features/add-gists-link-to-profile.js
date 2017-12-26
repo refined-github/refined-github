5a7b2d5a699c30ec3ded902b448700f3d76b8067
Simon Legg <leggsimon@gmail.com>
2017-12-26 15:34:18 +0000

    fix: handle elements existing

--- source/features/add-gists-link-to-profile.js
+++ source/features/add-gists-link-to-profile.js
@@ -7,15 +7,23 @@ export default async () => {
 	const username = getCleanPathname();
 	const href = isEnterprise() ? `/gist/${username}` : `https://gist.github.com/${username}`;
 
+	if (select.exists('.usernav-gists')) {
+		return;
+	}
+
 	select('.UnderlineNav-body').append(
 		<a href={href} class="UnderlineNav-item usernav-gists" role="tab" title="Gists">
 			{'Gists '}
 		</a>
 	);
 
+	if (select.exists('.usernav-gists-count')) {
+		return;
+	}
+
 	const {public_gists: publicGists} = await api(`users/${username}`);
 
 	select('.usernav-gists').appendChild(
-		<span class="Counter">{publicGists}</span>
+		<span class="Counter usernav-gists-count">{publicGists}</span>
 	)
 };
