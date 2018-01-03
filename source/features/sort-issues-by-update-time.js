cad44bd65f270a0794a176187990ad1ad279e0d4
Federico Brigante <github@bfred.it>
2018-01-03 18:17:59 +0700

    Extract query-less URLs handler

--- source/features/sort-issues-by-update-time.js
+++ source/features/sort-issues-by-update-time.js
@@ -1,6 +1,24 @@
 import select from 'select-dom';
 import {getUsername} from '../libs/utils';
 
+function getDefaultQueries(link) {
+	// Query-less URLs imply some queries.
+	// When we explicitly set ?q=* they're overridden,
+	// so they need to be manually added again.
+	const queries = [];
+
+	// Repo example: is:issue is:open
+	queries.push(/\/pulls\/?$/.test(link.pathname) ? 'is:pr' : 'is:issue');
+	queries.push('is:open');
+
+	// Header nav example: is:open is:issue author:you archived:false
+	if (link.pathname === '/issues' || link.pathname === '/pulls') {
+		queries.push(`author:${getUsername()}`);
+		queries.push('archived:false');
+	}
+	return queries;
+}
+
 export default function () {
 	// Get issues links that don't already have a specific sorting applied
 	for (const link of select.all(`
@@ -13,25 +31,8 @@ export default function () {
 		}
 
 		const search = new URLSearchParams(link.search);
-		const queries = search.get('q') ? search.get('q').split(/\s/) : [];
-		const isPRList = /\/pulls\/?$/.test(link.pathname) || queries.includes('is:pr');
+		const queries = search.get('q') ? search.get('q').split(/\s/) : getDefaultQueries(link);
 
-		// Query-less URLs imply some queries.
-		// When we explicitly set ?q=* they're overridden,
-		// so they need to be manually added again.
-		if (queries.length === 0) {
-			// Repo example: is:issue is:open
-			queries.push(isPRList ? 'is:pr' : 'is:issue');
-			queries.push('is:open');
-
-			// Header nav example: is:open is:issue author:you archived:false
-			if (link.pathname === '/issues' || link.pathname === '/pulls') {
-				queries.push(`author:${getUsername()}`);
-				queries.push('archived:false');
-			}
-		}
-
-		// Add sorting last
 		queries.push('sort:updated-desc');
 
 		search.set('q', queries.join(' '));
