b4bab6ef08513d290dc1b216cf37186967d532fa
Federico Brigante <github@bfred.it>
2018-01-03 15:47:24 +0700

    Add is:pr to PR lists

--- source/features/sort-issues-by-update-time.js
+++ source/features/sort-issues-by-update-time.js
@@ -3,27 +3,33 @@ import {anySelector} from '../libs/utils';
 
 export default function () {
 	// Get issues links that don't already have a specific sorting applied
-	for (const link of select.all(anySelector(`
+	const links = select.all(anySelector(`
 		:any(
 			[href*="/issues"],
 			[href*="/pulls"]
 		):not([href*="sort%3A"]):not(.issues-reset-query)
-	`))) {
+	`));
+	for (const link of links) {
 		// Pick only links to lists, not single issues
-		if (/(issues|pulls)\/?$/.test(link.pathname)) {
-			const search = new URLSearchParams(link.search);
-			const queries = (search.get('q') || '').split(' ');
-
-			// The /issues/ listings will also include PRs unless `is:issue` is specified
-			if (/(issues)\/?$/.test(link.pathname) && !queries.includes('is:pr')) {
-				queries.push('is:issue');
-			}
-
-			// Add sorting last
-			queries.push('sort:updated-desc');
-
-			search.set('q', queries.join(' ').trim());
-			link.search = search;
+		if (!/(issues|pulls)\/?$/.test(link.pathname)) {
+			continue;
 		}
+
+		const search = new URLSearchParams(link.search);
+		const queries = (search.get('q') || '').split(/\s/);
+		const isPRList = /\/pulls\/?$/.test(link.pathname) || queries.includes('is:pr');
+
+		// /pulls/ and /issues/ imply some search queries;
+		// when we set ?q=* we override them, so they need to be manually added again.
+		const type = isPRList ? 'is:pr' : 'is:issue';
+		if (!queries.includes(type)) {
+			queries.push(type);
+		}
+
+		// Add sorting last
+		queries.push('sort:updated-desc');
+
+		search.set('q', queries.join(' ').trim());
+		link.search = search;
 	}
 }
