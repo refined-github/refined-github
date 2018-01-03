64768b6e89955992693ea233ac9e371c64c67724
Federico Brigante <github@bfred.it>
2018-01-03 18:12:17 +0700

    Entirely void URLs with explicit queries

--- source/features/sort-issues-by-update-time.js
+++ source/features/sort-issues-by-update-time.js
@@ -1,4 +1,5 @@
 import select from 'select-dom';
+import {getUsername} from '../libs/utils';
 
 export default function () {
 	// Get issues links that don't already have a specific sorting applied
@@ -12,24 +13,28 @@ export default function () {
 		}
 
 		const search = new URLSearchParams(link.search);
-		const queries = (search.get('q') || '').split(/\s/);
+		const queries = search.get('q') ? search.get('q').split(/\s/) : [];
 		const isPRList = /\/pulls\/?$/.test(link.pathname) || queries.includes('is:pr');
 
-		// /pulls/ and /issues/ imply some search queries;
-		// when we set ?q=* we override them, so they need to be manually added again.
-		const type = isPRList ? 'is:pr' : 'is:issue';
-		if (!queries.includes(type)) {
-			queries.push(type);
-		}
-
-		if (!queries.includes('is:closed') && !queries.includes('is:open') && !queries.includes('is:merged')) {
+		// Query-less URLs imply some queries.
+		// When we explicitly set ?q=* they're overridden,
+		// so they need to be manually added again.
+		if (queries.length === 0) {
+			// Repo example: is:issue is:open
+			queries.push(isPRList ? 'is:pr' : 'is:issue');
 			queries.push('is:open');
+
+			// Header nav example: is:open is:issue author:you archived:false
+			if (link.pathname === '/issues' || link.pathname === '/pulls') {
+				queries.push(`author:${getUsername()}`);
+				queries.push('archived:false');
+			}
 		}
 
 		// Add sorting last
 		queries.push('sort:updated-desc');
 
-		search.set('q', queries.join(' ').trim());
+		search.set('q', queries.join(' '));
 		link.search = search;
 	}
 
