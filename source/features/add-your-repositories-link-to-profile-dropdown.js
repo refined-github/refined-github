9f1a23e6549031fb6475569112cbe890cd556ec0
Federico Brigante <github@bfred.it>
2018-01-12 18:34:39 +0700

    Shorten

--- source/features/add-your-repositories-link-to-profile-dropdown.js
+++ source/features/add-your-repositories-link-to-profile-dropdown.js
@@ -3,13 +3,11 @@ import select from 'select-dom';
 import {getUsername} from '../libs/utils';
 
 export default function () {
-	const userName = getUsername();
-
-	const yourProfileItem = select.all(`.user-nav .dropdown-menu .dropdown-item[href="/${userName}"]`).pop().parentNode;
-
-	if (!yourProfileItem) {
-		return;
-	}
-	const yourRepoItem = <li><a class="dropdown-item" href={`/${userName}?tab=repositories`}>Your repositories</a></li>;
-	yourProfileItem.after(yourRepoItem);
+	select('.user-nav .dropdown-menu li:nth-child(3)').after(
+		<li>
+			<a class="dropdown-item" href={`/${getUsername()}?tab=repositories`}>
+				Your repositories
+			</a>
+		</li>
+	);
 }
