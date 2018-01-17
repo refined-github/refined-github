cb09d9a623be6a4e756fa6f41b2a2cf6815e9abd
Scott Busche <swbusche@gmail.com>
2018-01-17 11:23:23 -0600

    Fix 'Your Repository' link on GHE
    Currently GHE does not have the wrapping `<li>` on the `<a>` resulting in a null returned from the current selector.
    This solution is not super elegant, but will allow GHE to just work once it's updated to the same markup as GH is using.

--- source/features/add-your-repositories-link-to-profile-dropdown.js
+++ source/features/add-your-repositories-link-to-profile-dropdown.js
@@ -3,11 +3,21 @@ import select from 'select-dom';
 import {getUsername} from '../libs/utils';
 
 export default function () {
-	select('.user-nav .dropdown-menu li:nth-child(3)').after(
-		<li>
+	const ghMenuItem = select('.user-nav .dropdown-menu li:nth-child(3)');
+	if (ghMenuItem) {
+		ghMenuItem.after(
+			<li>
+				<a class="dropdown-item" href={`/${getUsername()}?tab=repositories`}>
+					Your repositories
+				</a>
+			</li>
+		);
+	} else {
+		// GHE does not have the li wrapper currently
+		select('.user-nav .dropdown-menu a:nth-child(3)').after(
 			<a class="dropdown-item" href={`/${getUsername()}?tab=repositories`}>
 				Your repositories
 			</a>
-		</li>
-	);
+		);
+	}
 }
