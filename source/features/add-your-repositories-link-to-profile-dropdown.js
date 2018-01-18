cdae0057dd59aaa9daea6934bdd126101c94ff47
Federico Brigante <github@bfred.it>
2018-01-18 15:28:01 +0700

    Avoid repetition, strengthen selector

--- source/features/add-your-repositories-link-to-profile-dropdown.js
+++ source/features/add-your-repositories-link-to-profile-dropdown.js
@@ -3,21 +3,13 @@ import select from 'select-dom';
 import {getUsername} from '../libs/utils';
 
 export default function () {
-	const ghMenuItem = select('.user-nav .dropdown-menu li:nth-child(3)');
-	if (ghMenuItem) {
-		ghMenuItem.after(
-			<li>
-				<a class="dropdown-item" href={`/${getUsername()}?tab=repositories`}>
-					Your repositories
-				</a>
-			</li>
-		);
-	} else {
-		// GHE does not have the li wrapper currently
-		select('.user-nav .dropdown-menu a:nth-child(3)').after(
-			<a class="dropdown-item" href={`/${getUsername()}?tab=repositories`}>
-				Your repositories
-			</a>
-		);
-	}
+	const position = select('.user-nav [aria-label~="profile"] + .dropdown-menu > :nth-child(3)');
+	const link = (
+		<a class="dropdown-item" href={`/${getUsername()}?tab=repositories`}>
+			Your repositories
+		</a>
+	);
+
+	// GHE doesn't wrap links in <li> yet
+	position.after(position.tagName === 'LI' ? <li>{link}</li> : link)
 }
