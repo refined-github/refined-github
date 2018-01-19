083e6f699cdbdc8017d45ab83a1d37dd1a6bd554
Tom Sherman <the.tomsherman@gmail.com>
2018-01-19 18:27:19 +0000

    Now works on all dropdowns!

--- source/features/close-out-of-view-dropdowns.js
+++ source/features/close-out-of-view-dropdowns.js
@@ -1,42 +1,41 @@
 import select from 'select-dom';
 import delegate from 'delegate';
 
-// Observe all <details> based dropdown menus.
-const observeDetails = () => {
-	const observer = new IntersectionObserver(([{intersectionRatio, target}]) => {
-		if (intersectionRatio === 0) {
+const observer = new IntersectionObserver(([{intersectionRatio, target}]) => {
+	if (intersectionRatio === 0) {
+		if (target.classList.contains('dropdown-menu') && select.exists('.dropdown-details[open]')) {
+      // Close <details> dropdowns if it's still open
 			select('.dropdown-details[open]').open = false;
-			observer.unobserve(target);
-			console.log('Unobserved: ', target);
+		} else if (target.classList.contains('js-active-navigation-container') || select.exists('.js-active-navigation-container', target)) {
+			// Close .js-select-menu dropdowns if still open
+			select('.select-menu-button.selected').click();
 		}
-	});
 
-	delegate('.dropdown-details', 'click', event => {
-		const dropdownDetails = event.delegateTarget;
-		const dropdownMenu = select('.dropdown-menu', dropdownDetails);
+		observer.unobserve(target);
+	}
+});
 
-		if (dropdownDetails.open) {
-			observer.unobserve(dropdownMenu);
-			console.log('Unobserved: ', dropdownMenu);
-		} else {
-			observer.observe(dropdownMenu);
-			console.log('Observing: ', dropdownMenu);
-		}
-	});
-}
+// Observe all <details> based dropdown menus.
+const observeDetails = () => delegate('.dropdown-details', 'click', event => {
+	const dropdownTarget = event.delegateTarget;
+	const dropdownMenu = select('.dropdown-menu', dropdownTarget);
 
-// Observe all <a> based dropdown menus.
-const observeAnchors = () => {
-  // TODO
-}
+	if (!dropdownTarget.open) {
+		observer.observe(dropdownMenu);
+	}
+});
 
-// Observe all <button> based dropdown menus.
-const observeButtons = () => {
-  // TODO
-}
+// Observe all .js-select-menu based dropdowns
+const observeSelectMenus = () => delegate('.js-select-menu', 'click', event => {
+	const dropdownTarget = event.delegateTarget;
+	const dropdownMenu = select('.select-menu-modal-holder', dropdownTarget);
+
+	if (dropdownTarget.classList.contains('active')) {
+		observer.observe(dropdownMenu);
+	}
+});
 
 export default () => {
 	observeDetails();
-	observeAnchors();
-	observeButtons();
+	observeSelectMenus();
 };
