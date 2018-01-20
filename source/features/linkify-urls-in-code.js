c9827c39221afc95852b966489ae088798a97ac2
Ram Lmn <ramlmn@users.noreply.github.com>
2018-01-21 02:51:19 +0530

    Fix issue where URLs in commented diffs aren't linkified (#1013)

--- source/features/linkify-urls-in-code.js
+++ source/features/linkify-urls-in-code.js
@@ -34,7 +34,7 @@ export const editTextNodes = (fn, el) => {
 };
 
 export default () => {
-	const wrappers = select.all(`.highlight:not(.${linkifiedURLClass})`);
+	const wrappers = select.all(`.blob-wrapper:not(.${linkifiedURLClass})`);
 
 	// Don't linkify any already linkified code
 	if (wrappers.length === 0) {
