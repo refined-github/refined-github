8f25482c6f13022d439a36c772e6ab90e4ea19aa
⦇⦀∙ˇ∎ˇ∙⦀⦈ <lxynox@users.noreply.github.com>
2018-01-23 06:50:07 -0500

    Avoid duplicate Download Folder buttons (#1025)

--- source/features/add-download-folder-button.js
+++ source/features/add-download-folder-button.js
@@ -5,10 +5,10 @@ import * as pageDetect from '../libs/page-detect';
 export default () => {
 	if (pageDetect.isRepoTree()) {
 		const buttonGroup = select(`.file-navigation .BtnGroup.float-right`);
-		if (buttonGroup) {
+		if (buttonGroup && !select.exists('.rgh-download-folder')) {
 			buttonGroup.prepend(
 				<a
-					class="btn btn-sm BtnGroup-item"
+					class="btn btn-sm BtnGroup-item rgh-download-folder"
 					href={`https://download-directory.github.io/?url=${location.href}`}>
 					Download
 				</a>);
