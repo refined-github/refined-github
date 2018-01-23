1c5bdc391e224c9300b444306f8b12895aba2913
lxynox <lxynox@gmail.com>
2018-01-22 23:49:34 -0500

    Prevent add-download-folder-button from reappending

--- source/features/add-download-folder-button.js
+++ source/features/add-download-folder-button.js
@@ -5,7 +5,7 @@ import * as pageDetect from '../libs/page-detect';
 export default () => {
 	if (pageDetect.isRepoTree()) {
 		const buttonGroup = select(`.file-navigation .BtnGroup.float-right`);
-		if (buttonGroup) {
+		if (buttonGroup && !buttonGroup.textContent.includes('Download')) {
 			buttonGroup.prepend(
 				<a
 					class="btn btn-sm BtnGroup-item"
