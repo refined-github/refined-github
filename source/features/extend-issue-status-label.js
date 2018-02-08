b444265ce1391e75fa52ddc83c2bde9dbe51e82a
Federico Brigante <github@bfred.it>
2018-02-08 19:32:04 +0700

    Show issue resolution in header

---
+++ source/features/extend-issue-status-label.js
@@ -0,0 +1,35 @@
+import {h} from 'dom-chef';
+import select from 'select-dom';
+import {wrap} from '../libs/utils';
+
+export default function () {
+	const lastAction = select.all(`
+		.discussion-item-closed,
+		.discussion-item-reopened
+	`).pop();
+
+	if (!lastAction) {
+		return;
+	}
+
+	let refEl = select(`
+		[href*="/pull/"],
+		[href*="/issues/"],
+		[href*="/commit/"]
+	`, lastAction);
+
+	if (!refEl) {
+		return;
+	}
+
+	// Commits are wrapped in `<code>`
+	refEl = refEl.closest('code') || refEl;
+
+	// Add extra info
+	const label = select('.gh-header-meta .State');
+	label.append(' in ', refEl.cloneNode(true));
+
+	// Link label to event in timeline
+	const fragment = select('.discussion-item-header[id]', lastAction).id;
+	wrap(label, <a href={'#' + fragment}></a>);
+}
