5e2869d40bcbc432579ccc91b6915db0901a4031
Federico Brigante <github@bfred.it>
2018-02-11 18:07:05 +0700

    Unify/linearize repo+all and confirmation+no-confirmation handling
    Spaghetti as only good on a plate :spaghetti:

--- source/features/open-all-notifications.js
+++ source/features/open-all-notifications.js
@@ -1,6 +1,7 @@
 import {h} from 'dom-chef';
 import select from 'select-dom';
 import delegate from 'delegate';
+import pEvent from 'p-event';
 import gitHubInjection from 'github-injection';
 import observeEl from '../libs/simplified-element-observer';
 import * as icons from '../libs/icons';
@@ -9,40 +10,42 @@ import {isNotifications} from '../libs/page-detect';
 
 const confirmationRequiredCount = 10;
 const unreadNotificationsClass = '.unread .js-notification-target';
-let notificationsToOpen = [];
 
-const faceboxTrigger = (
-	<a href="#open-all-in-tabs" id="facebox-trigger" rel="facebox" style={{display: 'none'}}></a>
-);
+async function openNotifications({delegateTarget}) {
+	const container = delegateTarget.closest('.boxed-group, .notification-center');
 
-faceboxTrigger.addEventListener('click', async () => {
-	await safeElementReady('#facebox-description');
+	// Ask for confirmation
+	const unreadNotifications = select.all(unreadNotificationsClass, container);
+	if (unreadNotifications.length >= confirmationRequiredCount) {
+		// Open confirmation box
+		select('#rgh-open-notifications-confirmation-trigger').click();
 
-	select('#facebox-description').textContent = `Are you sure you want to open ${notificationsToOpen.length} tabs?`;
-});
+		await safeElementReady('#facebox-description');
+		select('#facebox-description').textContent = `Are you sure you want to open ${unreadNotifications.length} tabs?`;
 
-function openNotifications(unreadNotifications = notificationsToOpen) {
-	const urls = unreadNotifications.map(el => el.href);
+		// Wait for confirm or close
+		const event = await Promise.race([
+			pEvent(select('#rgh-open-notifications-confirm'), 'click'),
+			pEvent(document, 'facebox:close')
+		]);
 
+		if (event.type === 'facebox:close') {
+			return;
+		}
+
+		// Close modal and continue
+		select('.js-facebox-close').click();
+	}
+
+	// Open all in tabs
 	browser.runtime.sendMessage({
-		urls,
+		urls: unreadNotifications.map(el => el.href),
 		action: 'openAllInTabs'
 	});
 
-	for (const notification of unreadNotifications) {
-		const listItem = notification.closest('.js-notification');
-		listItem.classList.remove('unread');
-		listItem.classList.add('read');
-	}
-}
-
-function tryOpeningNotifications(container) {
-	notificationsToOpen = select.all(unreadNotificationsClass, container);
-
-	if (notificationsToOpen.length < confirmationRequiredCount) {
-		openNotifications();
-	} else {
-		faceboxTrigger.click();
+	// Mark all as read, this also enables the native "marked as read notification"
+	for (const button of select.all('.mark-all-as-read', container)) {
+		button.click();
 	}
 }
 
@@ -65,49 +68,38 @@ function addOpenReposButton() {
 		const repo = select('.notifications-repo-link', repoNotificationContainer).textContent.split('/')[1];
 
 		const openNotificationsButton = (
-			<button type="button" class="open-repo-notifications tooltipped tooltipped-w rgh-tooltipped" aria-label={`Open all unread \`${repo}\` notifications in tabs`}>
+			<button type="button" class="open-repo-notifications tooltipped tooltipped-w rgh-tooltipped rgh-open-notifications-button" aria-label={`Open all unread \`${repo}\` notifications in tabs`}>
 				{icons.externalLink()}
 			</button>
 		);
-
-		openNotificationsButton.addEventListener('click', event => {
-			const button = event.target;
-			const repo = button.closest('.boxed-group');
-
-			tryOpeningNotifications(repo);
-			repo.classList.add('hide-notification-actions');
-		});
 
 		actions.insertBefore(openNotificationsButton, firstActionButton);
 	}
 }
 
 function addOpenAllButton() {
-	if (!select.exists('#open-all-tabs-trigger')) {
+	if (!select.exists('.rgh-open-notifications-button')) {
 		// Move out the extra node that messes with .BtnGroup-item:last-child
 		document.body.append(select('#mark_as_read_confirm_box') || '');
 
 		// Create an open button and add it into a button group
 		const group = select('.tabnav .float-right');
 		group.prepend(
-			<a href="#open-all-in-tabs" id="open-all-tabs-trigger" class="btn btn-sm">Open all unread in tabs</a>
+			<button class="btn btn-sm rgh-open-notifications-button">Open all unread in tabs</button>
 		);
 		groupButtons([...group.children]);
 	}
 }
 
 function addConfirmationBox() {
-	if (!select.exists('#open-all-in-tabs')) {
+	if (!select.exists('#rgh-open-notifications-confirmation')) {
 		document.body.append(
-			<div id="open-all-in-tabs" style={{display: 'none'}}>
-				{faceboxTrigger}
-
+			<a href="#rgh-open-notifications-confirmation" id="rgh-open-notifications-confirmation-trigger" rel="facebox" style={{display: 'none'}}></a>,
+			<div id="rgh-open-notifications-confirmation" style={{display: 'none'}}>
 				<h2 class="facebox-header" data-facebox-id="facebox-header">Are you sure?</h2>
-
 				<p data-facebox-id="facebox-description"></p>
-
 				<div class="full-button">
-					<button class="btn btn-block" id="open-all-notifications">Open all notifications</button>
+					<button class="btn btn-block" id="rgh-open-notifications-confirm">Open all notifications</button>
 				</div>
 			</div>
 		);
@@ -133,13 +125,7 @@ export default async function () {
 		return;
 	}
 
-	delegate('#open-all-notifications', 'click', () => {
-		openNotifications();
-		select('.js-facebox-close').click(); // Close modal
-	});
-	delegate('#open-all-tabs-trigger', 'click', () => {
-		tryOpeningNotifications();
-	});
+	delegate('.rgh-open-notifications-button', 'click', openNotifications);
 
 	gitHubInjection(() => {
 		// Add support for Mark as Unread
