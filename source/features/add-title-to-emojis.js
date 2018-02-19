daf4dc2edbb5efd1b33858bec345fe4d51f3aef7
Federico Brigante <github@bfred.it>
2018-02-19 22:49:02 +0700

    Don't break pre-existing tooltips on emojis

--- source/features/add-title-to-emojis.js
+++ source/features/add-title-to-emojis.js
@@ -2,6 +2,9 @@ import select from 'select-dom';
 
 export default function () {
 	for (const emoji of select.all('g-emoji')) {
-		emoji.setAttribute('title', `:${emoji.getAttribute('alias')}:`);
+		// Don’t add a title if the emoji’s parents already have one #1097
+		if (!emoji.closest('[title]')) {
+			emoji.setAttribute('title', `:${emoji.getAttribute('alias')}:`);
+		}
 	}
 }
