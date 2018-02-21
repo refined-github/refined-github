ffd7b1bbb032bfac872326d685e992525f77d37f
Niklas Higi <niklas@shroudedcode.com>
2018-02-21 19:07:32 +0100

    Allow features to register their own shortcuts

--- source/features/add-shortcut-help.js
+++ source/features/add-shortcut-help.js
@@ -2,16 +2,36 @@ import {h} from 'dom-chef';
 import select from 'select-dom';
 import observeEl from '../libs/simplified-element-observer';
 
+const shortcuts = [];
+
+export function registerShortcut(hotkey, description) {
+	if (shortcuts.some(shortcut => shortcut.hotkey === hotkey)) {
+		return;
+	}
+	shortcuts.push({hotkey, description});
+}
+
 function addShortcuts() {
 	const modal = select('.shortcuts');
-	const groups = select.all('.keyboard-mappings tbody', modal);
-	groups[1].append(
-		<tr>
-			<td class="keys">
-				<kbd>g</kbd> <kbd>r</kbd>
-			</td>
-			<td>Go to Releases</td>
-		</tr>
+	const thirdColumn = select.all('.column .keyboard-mappings', modal)[2];
+	thirdColumn.append(
+		<tbody>
+			<tr>
+				<th/>
+				<th>Refined GitHub</th>
+			</tr>
+			{
+				shortcuts.map(({hotkey, description}) => (
+					<tr>
+						<td class="keys">
+							{/* This is a monstrosity. Plese help me get rid of it. */}
+							{hotkey.split(' ').join(', ,').split(',').map(key => key === ' ' ? ' ' : <kbd>{key}</kbd>)}
+						</td>
+						<td>{description}</td>
+					</tr>
+				))
+			}
+		</tbody>
 	);
 }
 
