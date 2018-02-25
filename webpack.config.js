110a4f0d1c3472cc027a2ef68c59d03d3754d42e
Federico Brigante <github@bfred.it>
2018-02-26 00:02:42 +0700

    Restore partial minification

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = (env, argv) => ({
+module.exports = () => ({
 	mode: 'production', // Without this, function names will be garbled and enableFeature won't work
 	entry: {
 		content: './source/content',
@@ -36,17 +36,14 @@ module.exports = (env, argv) => ({
 		])
 	],
 	optimization: {
-		minimize: false,
-		minimizer: argv.watch ? [] : [
+		// Automatically enabled on prod; keeps it somewhat readable for AMO reviewers
+		minimizer: [
 			new UglifyJsPlugin({
 				uglifyOptions: {
-					// Keep it somewhat readable for AMO reviewers
 					mangle: false,
 					compress: false,
 					output: {
 						beautify: true,
-
-						// Reduce beautification indentation from 4 spaces to 1 to save space
 						indent_level: 2 // eslint-disable-line camelcase
 					}
 				}
