6a93e7dceaa30c5961c46525abaa7f3aaf8a5441
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-02-28 09:59:34 +0100

    Take last button.

--- source/features/add-update-branch-hotkey.js
+++ source/features/add-update-branch-hotkey.js
@@ -1,7 +1,7 @@
 import select from 'select-dom';
 
 export default function () {
-	const updateButton = select(`.branch-action-item form button`);
+	const updateButton = select(`.branch-action-item form button:last-child`);
 
 	if (updateButton) {
 		updateButton.setAttribute('data-hotkey', 'u');
