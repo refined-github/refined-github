5c9c72be89e43759c4cb6f0638edf0f529fbb0f6
Federico Brigante <github@bfred.it>
2018-02-28 12:59:52 +0700

    Update style for merge commits

--- source/features/mark-merge-commits-in-list.js
+++ source/features/mark-merge-commits-in-list.js
@@ -5,8 +5,10 @@ export default function () {
 	for (const commit of select.all('.commits-list-item:not(.refined-github-merge-commit)')) {
 		if (select.exists('[title^="Merge pull request"]', commit)) {
 			commit.classList.add('refined-github-merge-commit');
-			commit.querySelector('.commit-avatar-cell').prepend(icons.mergedPullRequest());
-			commit.querySelector('.avatar').classList.add('avatar-child');
+			const icon = icons.mergedPullRequest();
+			icon.classList.add('avatar');
+			select('.AvatarStack-body', commit).prepend(icon);
+			select('.AvatarStack', commit).classList.add('AvatarStack--two');
 		}
 	}
 }
