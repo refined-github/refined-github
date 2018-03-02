a570d13ec4e9ec16a28f902b995d3314af2528e4
Federico Brigante <github@bfred.it>
2018-03-03 01:56:34 +0700

    Move next to title

--- source/features/mark-merge-commits-in-list.js
+++ source/features/mark-merge-commits-in-list.js
@@ -6,9 +6,7 @@ export default function () {
 		if (select.exists('[title^="Merge pull request"]', commit)) {
 			commit.classList.add('refined-github-merge-commit');
 			const icon = icons.mergedPullRequest();
-			icon.classList.add('avatar');
-			select('.AvatarStack-body', commit).prepend(icon);
-			select('.AvatarStack', commit).classList.add('AvatarStack--two');
+			select('.commit-title', commit).prepend(icon);
 		}
 	}
 }
