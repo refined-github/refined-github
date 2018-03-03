c82b5440d2952bbc3b19df391086e7a8befdbd00
Tanmay <tanmay.rajani@ishisystems.com>
2018-03-03 09:38:08 +0530

    css change at relevant place with comment explaining, check for existing early in function

--- source/features/add-commented-menu-item.js
+++ source/features/add-commented-menu-item.js
@@ -4,13 +4,13 @@ import * as pageDetect from '../libs/page-detect';
 import {getUsername} from '../libs/utils';
 
 export default function () {
-	const pageName = pageDetect.isIssueSearch() ? 'issues' : 'pulls';
-	const type = pageDetect.isIssueSearch() ? 'issue' : 'pr';
-	const username = getUsername();
-
 	if (select.exists('.refined-github-commented')) {
 		return;
 	}
+
+	const pageName = pageDetect.isIssueSearch() ? 'issues' : 'pulls';
+	const type = pageDetect.isIssueSearch() ? 'issue' : 'pr';
+	const username = getUsername();
 
 	const commentedMenuItem = <a href={`/${pageName}?q=is%3Aopen+is%3A${type}+commenter%3A${username}`} class="subnav-item refined-github-commented">Commented</a>;
 
