7d81d68153c6ce72baaf7eed998a7b0867350ef7
Bruno Lemos <brunolemos@users.noreply.github.com>
2018-03-06 11:33:24 -0300

    Enable toggleAllThingsWithAlt on all commits (#1042)

--- source/content.js
+++ source/content.js
@@ -234,6 +234,11 @@ function ajaxedPagesHandler() {
 
 	if (pageDetect.isCommit()) {
 		enableFeature(addPatchDiffLinks);
+		enableFeature(toggleAllThingsWithAlt);
+	}
+
+	if (pageDetect.isCompare()) {
+		enableFeature(toggleAllThingsWithAlt);
 	}
 
 	if (pageDetect.isPR() || pageDetect.isIssue() || pageDetect.isCommit() || pageDetect.isDiscussion()) {
