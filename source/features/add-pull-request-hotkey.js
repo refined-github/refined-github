e28910eceb0c588b6c222387c8b316ca992b9049
Federico Brigante <github@bfred.it>
2018-03-14 12:45:19 +0700

    Loop it up

--- source/features/add-pull-request-hotkey.js
+++ source/features/add-pull-request-hotkey.js
@@ -1,31 +1,16 @@
 import select from 'select-dom';
 
 export default function () {
-	const tabnavSelector = '.tabnav-tab.js-pjax-history-navigate';
+	const tabs = select.all('.tabnav-pr .tabnav-tabs > a');
+	const selectedIndex = tabs.indexOf(select('.tabnav-pr .selected'));
 
-	const conversation = select(`${tabnavSelector} > .octicon-comment-discussion`)
-		.parentNode;
-	const commits = select(`${tabnavSelector} > .octicon-git-commit`).parentNode;
-	const files = select(`${tabnavSelector} > .octicon-diff`).parentNode;
-
-	const conversationHotKey = ['p 1'];
-	const commitsHotKey = ['p 2'];
-	const filesHotKey = ['p 3'];
-
-	if (conversation.className.includes('selected')) {
-		commitsHotKey.push('p ArrowRight');
+	for (const [index, tab] of tabs.entries()) {
+		const keys = [`p ${index + 1}`];
+		if (index === selectedIndex - 1) {
+			keys.push('p ArrowLeft');
+		} else if (index === selectedIndex + 1) {
+			keys.push('p ArrowRight');
+		}
+		tab.setAttribute('data-hotkey', keys);
 	}
-
-	if (commits.className.includes('selected')) {
-		conversationHotKey.push('p ArrowLeft');
-		filesHotKey.push('p ArrowRight');
-	}
-
-	if (files.className.includes('selected')) {
-		commitsHotKey.push('p ArrowLeft');
-	}
-
-	conversation.setAttribute('data-hotkey', conversationHotKey);
-	commits.setAttribute('data-hotkey', commitsHotKey);
-	files.setAttribute('data-hotkey', filesHotKey);
 }
