405f3941abf66a4f3c1f62ff7c60f07b56c0198c
Federico Brigante <github@bfred.it>
2018-03-16 02:48:57 +0700

    Cycle through tabs

--- source/features/add-pull-request-hotkey.js
+++ source/features/add-pull-request-hotkey.js
@@ -3,12 +3,13 @@ import select from 'select-dom';
 export default function () {
 	const tabs = select.all('.tabnav-pr .tabnav-tab');
 	const selectedIndex = tabs.indexOf(select('.tabnav-pr .selected'));
+	const lastTab = tabs.length - 1;
 
 	for (const [index, tab] of tabs.entries()) {
 		const keys = [`p ${index + 1}`];
-		if (index === selectedIndex - 1) {
+		if (index === selectedIndex - 1 || (index === 0 && selectedIndex === lastTab)) {
 			keys.push('p ArrowLeft');
-		} else if (index === selectedIndex + 1) {
+		} else if (index === selectedIndex + 1 || (index === lastTab && selectedIndex === 0)) {
 			keys.push('p ArrowRight');
 		}
 		tab.setAttribute('data-hotkey', keys);
