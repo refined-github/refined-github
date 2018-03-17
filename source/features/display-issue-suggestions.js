d99293b0c26c570fe77a08d6baf30f9cb80791a8
Niklas Higi <niklas@shroudedcode.com>
2018-03-17 15:51:16 +0100

    Implement basic functionality

--- source/features/display-issue-suggestions.js
+++ source/features/display-issue-suggestions.js
@@ -1,27 +1,26 @@
-import select from 'select-dom';
 import {h} from 'dom-chef';
+import select from 'select-dom';
+import {getRepoURL} from '../libs/page-detect';
+import fetchApi from '../libs/api';
 import {openIssue, closedIssue} from '../libs/icons';
 
-export default function () {
+let insertedSidebarItem;
+
+async function displayIssueSuggestions(title = 'issue comment') {
+	const repo = getRepoURL();
+	const query = encodeURIComponent(`${title.replace(/:/g, '\\:')} repo:${repo} type:issue`);
+	const url = `search/issues?q=${query}`;
+	const issues = (await fetchApi(url)).items;
 	const sidebar = select('.discussion-sidebar');
-	const issues = [
-		{
-			title: 'My cat has grown tentacles and turned purple, should I worry?',
-			open: false
-		},
-		{
-			title: 'Help, my cat\'s bugging me to install a browser extension!',
-			open: true
-		}
-	];
-	sidebar.prepend(
+
+	const sidebarItem = (
 		<div class="discussion-sidebar-item">
 			<div class="text-bold mb-2">Possibly related issues</div>
 			<div class="Box Box--condensed">
 				{
 					issues.map(issue => (
-						<a class="Box-row d-flex px-2" href="#1321">
-							<div class="flex-shrink">{issue.open ? openIssue() : closedIssue()}</div>
+						<a class="Box-row d-flex px-2" href={issue.html_url}>
+							<div class="flex-shrink">{issue.state === 'open' ? openIssue() : closedIssue()}</div>
 							<div class="flex-grow pl-2">{issue.title}</div>
 						</a>
 					))
@@ -29,4 +28,16 @@ export default function () {
 			</div>
 		</div>
 	);
+
+	if (insertedSidebarItem) {
+		insertedSidebarItem.replaceWith(sidebarItem);
+	} else {
+		sidebar.prepend(sidebarItem);
+	}
+	insertedSidebarItem = sidebarItem;
+}
+
+export default function () {
+	console.log(select('#issue_title'));
+	select('#issue_title').addEventListener('blur', ({target}) => displayIssueSuggestions(target.value));
 }
