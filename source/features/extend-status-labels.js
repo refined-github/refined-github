f568ecd8c01bb54505f3f76fd42e9298a1c6bb2a
Ram Lmn <ramlmn@outlook.com>
2018-03-21 22:50:53 +0530

    Extend status labels for PRs too

--- source/features/extend-status-labels.js
+++ source/features/extend-status-labels.js
@@ -6,7 +6,8 @@ export default function () {
 	const lastActionRef = select.all(`
 		.discussion-item-closed [href*="/pull/"],
 		.discussion-item-closed code,
-		.discussion-item-reopened
+		.discussion-item-reopened,
+		.discussion-item .discussion-item-entity
 	`).pop();
 
 	// Leave if it was never closed or if it was reopened or if it’s already linked
@@ -14,9 +15,18 @@ export default function () {
 		return;
 	}
 
-	// Add extra info
 	const label = select('.gh-header-meta .State');
-	label.append(' in ', lastActionRef.cloneNode(true));
+	const lastActionRefClone = lastActionRef.cloneNode(true);
+
+	// Exists on PRs that are merged
+	if (lastActionRef.matches('.discussion-item-entity')) {
+		// Need to wrap ref in PRs for consistency with issues
+		label.append(' as ', (
+			<a href={lastActionRef.closest('a').href}>{lastActionRefClone}</a>
+		));
+	} else {
+		label.append(' in ', lastActionRefClone);
+	}
 
 	// Link label to event in timeline
 	wrap(label, <a href={'#' + lastActionRef.closest('[id]').id}></a>);
