5457bfa31364ab3ee25153150c0233755ab2a03b
Alex Bonder <lexbonder@gmail.com>
2018-03-23 10:23:17 -0600

    Update auto-load-contribution-activities.js

--- source/features/auto-load-contribution-activities.js
+++ source/features/auto-load-contribution-activities.js
@@ -2,7 +2,7 @@ const autoLoadContributionActivities = () => {
 	document.addEventListener('scroll', () => {
 		const button = document.querySelector('.contribution-activity-show-more');
 		if (
-			document.body.scrollHeight ===
+			document.body.scrollHeight <=
 			document.documentElement.scrollTop + window.innerHeight
 		) {
 			button.click();
