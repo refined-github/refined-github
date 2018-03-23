d6814610e17f94571454d291115609dc21d93cf0
Federico Brigante <github@bfred.it>
2018-03-23 23:20:48 +0800

    Indent with tab in the CSS box

--- source/options.js
+++ source/options.js
@@ -1,3 +1,11 @@
 import OptionsSync from 'webext-options-sync';
+import indentTextarea from './libs/indent-textarea';
+
+document.querySelector('[name="customCSS"]').addEventListener('keydown', event => {
+	if (event.key === 'Tab' && !event.shiftKey) {
+		indentTextarea(event.target);
+		event.preventDefault();
+	}
+});
 
 new OptionsSync().syncForm('#options-form');
