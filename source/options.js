c20bd64648a69c559d009bd97f1bd3b4c1554d06
Federico Brigante <github@bfred.it>
2018-03-25 01:20:23 +0800

    Limit to modern input events

--- source/options.js
+++ source/options.js
@@ -2,7 +2,9 @@ import Textarea from 'storm-textarea';
 import OptionsSync from 'webext-options-sync';
 import indentTextarea from './libs/indent-textarea';
 
-Textarea.init('textarea');
+Textarea.init('textarea', {
+	events: ['input']
+});
 
 document.querySelector('[name="customCSS"]').addEventListener('keydown', event => {
 	if (event.key === 'Tab' && !event.shiftKey) {
