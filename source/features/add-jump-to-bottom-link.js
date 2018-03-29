37418b47d50ed48e6564250e1bc0fa77858f0e8d
Federico Brigante <github@bfred.it>
2018-03-29 15:46:59 +0700

    Fix Jump to Bottom link
    Fixes #1228

--- source/features/add-jump-to-bottom-link.js
+++ source/features/add-jump-to-bottom-link.js
@@ -11,7 +11,7 @@ function add() {
 
 	meta.append(
 		' · ',
-		<a href="#partial-timeline-marker" id="refined-github-jump-to-bottom-link">Jump to bottom</a>
+		<a href="#partial-timeline" id="refined-github-jump-to-bottom-link">Jump to bottom</a>
 	);
 }
 
