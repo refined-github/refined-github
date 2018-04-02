e7edbe325f89d928834638e1256a9e46778ddd16
maxscores <maxstackhouse@gmail.com>
2018-04-02 13:56:18 -0600

    Add event listener to open tab
    Co-Authored-By: Tyler Madsen <madsen.tyler@gmail.com>
    Co-Authored-By: Alex Barnes <abarnes26@users.noreply.github.com>

--- source/features/open-notification-in-new-tab.js
+++ source/features/open-notification-in-new-tab.js
@@ -4,12 +4,13 @@ import {registerShortcut} from './improve-shortcut-help';
 export default function() {
   registerShortcut('notifications', 'shift o', 'Open Notification in New Tab');
 
-  document.addEventListener('keydown', event => {
+  document.addEventListener('keyup', event => {
     let notification = select('.js-notification, .navigation-focus')
-    let isO = event.key === "O"
+    let isO = (event.key === "O")
+    let url = document.querySelector('.navigation-focus').querySelector('a')
 
     if (notification && isO) {
-      window.open('https://www.google.com', '_blank')
+      window.open(url, '_blank')
     }
   })
 }
