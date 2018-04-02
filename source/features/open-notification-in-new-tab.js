524ba782b9b75d93bf5c4ef35f10c9d4acecad61
Max Stackhouse <maxstackhouse@gmail.com>
2018-04-02 14:41:23 -0600

    Spaces > tabs

--- source/features/open-notification-in-new-tab.js
+++ source/features/open-notification-in-new-tab.js
@@ -2,15 +2,15 @@ import select from 'select-dom';
 import {registerShortcut} from './improve-shortcut-help';
 
 export default function () {
-  registerShortcut('notifications', 'shift o', 'Open Notification in New Tab');
+	registerShortcut('notifications', 'shift o', 'Open Notification in New Tab');
 
-  document.addEventListener('keyup', event => {
-		const notification = select('.js-notification, .navigation-focus');
-		const isO = (event.key === 'O');
-		const url = document.querySelector('.navigation-focus').querySelector('a');
+	document.addEventListener('keyup', event => {
+			const notification = select('.js-notification, .navigation-focus');
+			const isO = (event.key === 'O');
+			const url = document.querySelector('.navigation-focus').querySelector('a');
 
-		if (notification && isO) {
-			window.open(url, '_blank');
-		}
-	});
+			if (notification && isO) {
+				window.open(url, '_blank');
+			}
+		});
 }
