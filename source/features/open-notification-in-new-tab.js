cd78c08722c3c7f92ccc2c4c26b8d6676e880906
maxscores <maxstackhouse@gmail.com>
2018-04-03 07:52:22 -0600

    Update PR Review Comments
    Co-Authored-By: Tyler Madsen <madsen.tyler@gmail.com>
    Co-Authored-By: Alex Barnes <abarnes26@users.noreply.github.com>

--- source/features/open-notification-in-new-tab.js
+++ source/features/open-notification-in-new-tab.js
@@ -2,14 +2,13 @@ import select from 'select-dom';
 import {registerShortcut} from './improve-shortcut-help';
 
 export default function () {
-	registerShortcut('notifications', 'shift o', 'Open Notification in New Tab');
+	registerShortcut('notifications', 'shift o', 'Open Selected Notification in New Tab');
 
 	document.addEventListener('keyup', event => {
 		const notification = select('.js-notification, .navigation-focus');
-		const isO = (event.key === 'O');
-		const url = document.querySelector('.navigation-focus').querySelector('a');
+		const url = document.querySelector('.navigation-focus a');
 
-		if (notification && isO) {
+		if (notification && event.key === 'O') {
 			window.open(url, '_blank');
 		}
 	});
