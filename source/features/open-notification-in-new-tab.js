9a8605ed4386079bb6f59c1f446f91445877871f
Federico Brigante <github@bfred.it>
2018-04-03 23:15:05 +0700

    Shorten code, open any selection

--- source/features/open-notification-in-new-tab.js
+++ source/features/open-notification-in-new-tab.js
@@ -2,14 +2,12 @@ import select from 'select-dom';
 import {registerShortcut} from './improve-shortcut-help';
 
 export default function () {
-	registerShortcut('notifications', 'shift o', 'Open Selected Notification in New Tab');
+	registerShortcut('notifications', 'shift o', 'Open selection in new tab');
 
-	document.addEventListener('keyup', event => {
-		const notification = select('.js-notification, .navigation-focus');
-		const url = document.querySelector('.navigation-focus a');
-
-		if (notification && event.key === 'O') {
-			window.open(url, '_blank');
+	document.addEventListener('keypress', event => {
+		const selected = document.querySelector('.navigation-focus .js-navigation-open[href]');
+		if (selected && event.key === 'O') {
+			window.open(selected, '_blank');
 		}
 	});
 }
