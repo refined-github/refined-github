1d466074220fe688f1581f6c2a5b9ea902e0a89e
Federico Brigante <github@bfred.it>
2018-04-03 23:16:01 +0700

    Update shortcut position

--- source/features/open-notification-in-new-tab.js
+++ source/features/open-notification-in-new-tab.js
@@ -2,7 +2,7 @@ import select from 'select-dom';
 import {registerShortcut} from './improve-shortcut-help';
 
 export default function () {
-	registerShortcut('notifications', 'shift o', 'Open selection in new tab');
+	registerShortcut('site', 'shift o', 'Open selection in new tab');
 
 	document.addEventListener('keypress', event => {
 		const selected = document.querySelector('.navigation-focus .js-navigation-open[href]');
