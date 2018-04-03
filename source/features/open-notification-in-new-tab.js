2f497142653c34afb85e1d78ff2637b5d67386d8
Federico Brigante <github@bfred.it>
2018-04-03 23:18:23 +0700

    Use select

--- source/features/open-notification-in-new-tab.js
+++ source/features/open-notification-in-new-tab.js
@@ -5,7 +5,7 @@ export default function () {
 	registerShortcut('site', 'shift o', 'Open selection in new tab');
 
 	document.addEventListener('keypress', event => {
-		const selected = document.querySelector('.navigation-focus .js-navigation-open[href]');
+		const selected = select('.navigation-focus .js-navigation-open[href]');
 		if (selected && event.key === 'O') {
 			window.open(selected, '_blank');
 		}
