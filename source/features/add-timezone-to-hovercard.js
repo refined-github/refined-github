e3e9f76590d64d4f7ad7ac34298294f6a0599f47
Jorge Gonzalez <jrgqhd@gmail.com>
2018-04-06 00:36:55 -0400

    Add clock icon left of timezone

--- source/features/add-timezone-to-hovercard.js
+++ source/features/add-timezone-to-hovercard.js
@@ -1,6 +1,25 @@
+import {h} from 'dom-chef';
 import select from 'select-dom';
 import moment from 'moment-timezone';
 import domify from '../libs/domify';
+
+function clockIcon() {
+	return (
+		<svg
+			class="octicon octicon-clock text-gray-light ml-5"
+			width="20"
+			height="16"
+			viewBox="0 0 14 16"
+			version="1.1"
+			aria-hidden="true"
+		>
+			<path
+				fill-rule="evenodd"
+				d="M8 8h3v2H7c-.55 0-1-.45-1-1V4h2v4zM7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7z"
+			/>
+		</svg>
+	);
+}
 
 function createTimezoneRequestURL(lat, lng) {
 	const timezoneAPI = 'https://maps.googleapis.com/maps/api/timezone/json?location=';
@@ -15,7 +34,7 @@ async function getTimezone(location) {
 	const locationResponse = await locationRequest.json();
 
 	try {
-		const {lat, lng} = locationResponse.results[0].geometry.location;
+		const { lat, lng } = locationResponse.results[0].geometry.location;
 		const timezoneRequest = await fetch(createTimezoneRequestURL(lat, lng));
 		const timezoneResponse = await timezoneRequest.json();
 
@@ -69,7 +88,8 @@ export default async function () {
 				const {timeZoneId} = timeZone;
 				const time = moment.tz(moment(), timeZoneId).format('HH:mm');
 				const abbr = moment.tz(timeZoneId).zoneAbbr();
-				hoverCard.appendChild(domify(`${time} ${abbr}`));
+				hoverCard.append(clockIcon());
+				hoverCard.append(domify(`${time} ${abbr}`));
 			}
 		} catch (e) {
 			// Silently fail when there are too many API requests
