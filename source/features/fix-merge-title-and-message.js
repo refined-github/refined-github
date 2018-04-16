3992a8a8be0005119eabf8006dda40a8703d2f86
alex-111-gh <alex.ti.771@gmail.com>
2018-04-16 13:36:21 -0700

    Indentation to use tabs

--- source/features/fix-merge-title-and-message.js
+++ source/features/fix-merge-title-and-message.js
@@ -1,17 +1,17 @@
 import select from 'select-dom';
 
 export default function () {
-    const btn = select('.merge-message .btn-group-merge [type=submit]');
-    if (!btn) {
-        return;
-    }
-    btn.addEventListener('click', () => {
-        // fix merge commit title
-        const title = select('.js-issue-title').textContent;
-        const number = select('.gh-header-number').textContent;
-        select('#merge_title_field').value = `${title.trim()} (${number})`;
-        // fix merge commit message
-        const merge_message = select('.mt-0 .js-comment-body').textContent;
-        select('#merge_message_field').value = `${merge_message.trim()}`;
-    });
+	const btn = select('.merge-message .btn-group-merge [type=submit]');
+	if (!btn) {
+		return;
+	}
+	btn.addEventListener('click', () => {
+		// Fix merge commit title
+		const title = select('.js-issue-title').textContent;
+		const number = select('.gh-header-number').textContent;
+		select('#merge_title_field').value = `${title.trim()} (${number})`;
+		// Fix merge commit message
+		const mergeMessage = select('.mt-0 .js-comment-body').textContent;
+		select('#merge_message_field').value = `${mergeMessage.trim()}`;
+	});
 }
