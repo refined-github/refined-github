8d640d00446f5c531b2ffe58502e9c52523c8a14
Federico Brigante <github@bfred.it>
2018-05-09 18:31:05 +0300

    Add feature description as per #1275

--- source/features/hide-navigation-hover-highlight.js
+++ source/features/hide-navigation-hover-highlight.js
@@ -1,3 +1,12 @@
+/*
+Some lists like notifications, file lists, and issue lists,
+are highlighted as you move the mouse over them. This highlight
+is useful when navigating via the keyboard (j/k), but annoying
+when just moving the mouse around.
+
+This feature will hide the highlight until the first keyboard
+navigation, then it will be displayed until the next full reload.
+*/
 const className = 'rgh-no-navigation-highlight';
 
 export default function () {
