c70342181df337c9d62d25f09df152983b7a7b7a
Xiao Ling <xiaoling@users.noreply.github.com>
2018-05-10 13:01:38 -0700

    Create fix-squash-and-merge-message.js
    uses the PR description as the squash-and-merge commit message.

---
+++ source/features/fix-squash-and-merge-message.js
@@ -0,0 +1,12 @@
+import select from 'select-dom';
+
+export default function () {
+	const btn = select('.merge-message .btn-group-squash [type=submit]');
+	if (!btn) {
+		return;
+	}
+	btn.addEventListener('click', () => {
+		const desc = select_dom_default()(".comment-form-textarea[name='pull_request[body]']").textContent;
+    select_dom_default()("#merge_message_field").value = `${desc}`;
+	});
+}
