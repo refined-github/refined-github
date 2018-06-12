93b5be6594348a6ec5dfca6c68216d0f6630accb
Federico Brigante <github@bfred.it>
2018-06-12 15:43:23 +0200

    Drop transitionary code in mark-unread

--- source/features/mark-unread.js
+++ source/features/mark-unread.js
@@ -121,15 +121,12 @@ function getNotification(notification) {
 		return item;
 	}
 
-	// Already on L17. Only needed here to fix pre-existing unread notifications. Remove in 2018.
-	const clippedParticipants = participants.slice(0, 3);
-
-	const usernames = clippedParticipants
+	const usernames = participants
 		.map(participant => participant.username)
 		.join(' and ')
 		.replace(/ and (.+) and/, ', $1, and'); // 3 people only: A, B, and C
 
-	const avatars = clippedParticipants.map(participant =>
+	const avatars = participants.map(participant =>
 		<a href={`/${participant.username}`} class="avatar">
 			<img alt={`@${participant.username}`} height="20" src={participant.avatar} width="20"/>
 		</a>
