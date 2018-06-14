07d1b14d1aa6dce694af3c8bf1f557074a8af33c
Saul Costa <saulcosta18@gmail.com>
2018-06-14 12:51:27 -0700

    Switched to a plain object for API headers. Improved personal token UI. #1374.

--- source/libs/api.js
+++ source/libs/api.js
@@ -1,7 +1,7 @@
 import OptionsSync from 'webext-options-sync';
 
 const cache = new Map();
-const headers = new Headers();
+const headers = {};
 const options = new OptionsSync().getAll();
 
 export default async endpoint => {
@@ -10,7 +10,7 @@ export default async endpoint => {
 	}
 	const {personalToken = ''} = await options;
 	if (personalToken) {
-		headers.set('Authorization', `token ${personalToken}`);
+		headers.Authorization = `token ${personalToken}`;
 	}
 	const api = location.hostname === 'github.com' ? 'https://api.github.com/' : `${location.origin}/api/`;
 	const response = await fetch(api + endpoint, {headers});
