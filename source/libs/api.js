8f2fcff85fe849a643101eafe5cd6acc717b05c8
Federico Brigante <github@bfred.it>
2018-06-17 15:06:04 +0200

    Handle API errors

--- source/libs/api.js
+++ source/libs/api.js
@@ -14,6 +14,24 @@ export default async endpoint => {
 	const api = location.hostname === 'github.com' ? 'https://api.github.com/' : `${location.origin}/api/`;
 	const response = await fetch(api + endpoint, {headers});
 	const json = await response.json();
-	cache.set(endpoint, json);
+
+	if (response.ok) {
+		cache.set(endpoint, json);
+	} else if (json.message.includes('API rate limit exceeded')) {
+		console.error(
+			'Refined GitHub hit GitHub API’s rate limit. Set your token in the options or take a walk! 🍃 🌞'
+		);
+	} else if (json.message === 'Bad credentials') {
+		console.error(
+			'Refined GitHub couldn’t use GitHub’s API because the token seems to be incorrect or expired. Update it in the options.'
+		);
+	} else {
+		console.error(
+			'Refined GitHub wasn’t able to fetch GitHub’s API.',
+			personalToken ? 'Ensure that your token has access to this repo.' : 'Maybe adding a token in the options will fix this issue.',
+			'\n',
+			JSON.stringify(json, null, '\t')
+		);
+	}
 	return json;
 };
