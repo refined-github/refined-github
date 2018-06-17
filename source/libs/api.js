42b4759e489a78936c17bff3607b9bb9568f2c6c
Saul Costa <saul@codevolve.com>
2018-06-17 05:47:02 -0700

    Add support for personal access tokens (#1375)

--- source/libs/api.js
+++ source/libs/api.js
@@ -1,11 +1,18 @@
+import OptionsSync from 'webext-options-sync';
+
 const cache = new Map();
 
 export default async endpoint => {
 	if (cache.has(endpoint)) {
 		return cache.get(endpoint);
 	}
+	const headers = {};
+	const {personalToken} = await new OptionsSync().getAll();
+	if (personalToken) {
+		headers.Authorization = `token ${personalToken}`;
+	}
 	const api = location.hostname === 'github.com' ? 'https://api.github.com/' : `${location.origin}/api/`;
-	const response = await fetch(api + endpoint);
+	const response = await fetch(api + endpoint, {headers});
 	const json = await response.json();
 	cache.set(endpoint, json);
 	return json;
