d9447a5537eb958bc335370effc64f62da218152
Krista Handel <k.handel66@gmail.com>
2018-07-03 13:07:57 -0600

    Get list of all file headers Add delete anchor tag to all headers

--- source/features/add-delete-to-pr-files.js
+++ source/features/add-delete-to-pr-files.js
@@ -1,11 +1,10 @@
-// might add to line 278
-
 import select from 'select-dom';
 import {h} from 'dom-chef';
 import * as pageDetect from '../libs/page-detect';
 
 export default function () {
-  const fileActions = select('.file-actions')
-  const deleteFile = fileActions.append(<a href='#' className='delete-file' aria-label='Delete this file from the pull request'>Delete </a>)
-  getBranchName()
-}
+  const fileActions = select.all('.file-actions');
+  console.log(fileActions)
+  const deleteFile = fileActions.map(fileAction => fileAction.append(<a href='#' className='delete-file' aria-label='Delete this file from the pull request'>Delete </a>));
+  getBranchName();
+};
