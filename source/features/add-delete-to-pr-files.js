aecda5eb4075d7bbeb06eaac5cd170d1a3837dee
Krista Handel <k.handel66@gmail.com>
2018-07-03 13:28:40 -0600

    Add logic for getting branch name

--- source/features/add-delete-to-pr-files.js
+++ source/features/add-delete-to-pr-files.js
@@ -2,9 +2,13 @@ import select from 'select-dom';
 import {h} from 'dom-chef';
 import * as pageDetect from '../libs/page-detect';
 
+const repoURL = pageDetect.getRepoURL();
+
 export default function () {
   const fileActions = select.all('.file-actions');
   console.log(fileActions)
   const deleteFile = fileActions.map(fileAction => fileAction.append(<a href='#' className='delete-file' aria-label='Delete this file from the pull request'>Delete </a>));
-  getBranchName();
+  const findBranch = select('.head-ref').innerText;
+  const branchName = findBranch.includes(':') ? findBranch.split(':')[1] : findBranch;
+  // select('.delete-file').href = 'https://github.com/' + repoUrl + '/delete/' + branchName + fileName
 };
