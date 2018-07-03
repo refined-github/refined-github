11bab887b281b4c76bc51623b38a3ffddea641ad
Krista Handel <k.handel66@gmail.com>
2018-07-03 15:16:01 -0600

    Refactor Add styling for link

--- source/features/add-delete-to-pr-files.js
+++ source/features/add-delete-to-pr-files.js
@@ -2,24 +2,15 @@ import select from 'select-dom';
 import {h} from 'dom-chef';
 import * as pageDetect from '../libs/page-detect';
 
-// https://github.com/<username>/<repo>/delete/<branch>/<filename>
-
 const repoURL = pageDetect.getRepoURL();
 
 export default function () {
-  let fileName
-  const fileActions = select.all('.file-actions');
-  const deleteFile = fileActions.map(fileAction => fileAction.append(<a href='#' className='delete-file' aria-label='Delete this file from the pull request'>Delete </a>));
   const findBranch = select('.head-ref').innerText;
   const branchName = findBranch.includes(':') ? findBranch.split(':')[1] : findBranch;
-  const deleteLinks = select.all('.delete-file')
-  const getFileName = (e) => {
-    fileName = e.path[2].children[1].childNodes[3].innerText
-  }
 
-  deleteLinks.map(deleteLink => deleteLink.addEventListener('click', (e) => getFileName(e)));
+  const filesInfo = Array.from(select.all('.file-info'));
+  const generateURL = filesInfo.map(file => 'https://github.com/' + repoURL + '/delete/' + branchName + '/' + file.children[1].title)
 
-  // need to map through deleteLinks and update href for getAll
-  //right now this only adds an href to the first delete-file element
-  select('.delete-file').href = 'https://github.com/' + repoUrl + '/delete/' + branchName + '/' + fileName;
+  const fileActionContainers = select.all('div.BtnGroup');
+  const appendDeleteLinks = fileActionContainers.map((container, index) => container.prepend(<a href={generateURL[index]} className='btn btn-sm tooltipped tooltipped-s BtnGroup-item' aria-label='Delete this file from the pull request'>Delete </a>));
 };
