722dc2765dfa872062ee6208f61ecd95fe1c6b1c
Krista Handel <k.handel66@gmail.com>
2018-07-04 21:12:49 -0600

    Update logic for appending delete anchor There is a hidden div with class BtnGroup which was causing the wrong link to be appended

--- source/features/add-delete-to-pr-files.js
+++ source/features/add-delete-to-pr-files.js
@@ -4,13 +4,19 @@ import * as pageDetect from '../libs/page-detect';
 
 const repoURL = pageDetect.getRepoURL();
 
+// very last link isn't working?
+
 export default function () {
   const findBranch = select('.head-ref').innerText;
   const branchName = findBranch.includes(':') ? findBranch.split(':')[1] : findBranch;
 
-  const filesInfo = Array.from(select.all('.file-info'));
-  const generateURL = filesInfo.map(file => 'https://github.com/' + repoURL + '/delete/' + branchName + '/' + file.children[1].title)
+  const filesInfo = select.all('.file-info');
+  const generateURL = filesInfo.map(file => 'https://github.com/' + repoURL + '/delete/' + branchName + '/' + file.children[1].title);
 
   const fileActionContainers = select.all('div.BtnGroup');
-  const appendDeleteLinks = fileActionContainers.map((container, index) => container.prepend(<a href={generateURL[index]} className='btn btn-sm tooltipped tooltipped-s BtnGroup-item' aria-label='Delete this file from the pull request'>Delete </a>));
+  const appendDeleteLinks = fileActionContainers.map((container, index) => {
+    if (index !== 0) {
+      container.prepend(<a href={generateURL[index - 1]} className='btn btn-sm tooltipped tooltipped-s BtnGroup-item' aria-label='Delete this file from the pull request'>Delete </a>)
+    }
+  });
 };
