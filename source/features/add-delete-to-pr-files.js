6e1e5d6a0871657d367200f53b069d2cfa27c76d
Krista Handel <k.handel66@gmail.com>
2018-07-09 16:30:54 -0600

    Clean logic for selecting file containers

--- source/features/add-delete-to-pr-files.js
+++ source/features/add-delete-to-pr-files.js
@@ -7,14 +7,16 @@ const repoURL = pageDetect.getRepoURL();
 export default function () {
 	const branchName = select('.head-ref').textContent.split(':').pop();
 
-	const filesInfo = select.all('.file-info');
-	const generateURL = filesInfo.map(file => `/${repoURL}/delete/${branchName}/${file.children[1].title}`);
-
-	const fileActionContainers = select.all('div.BtnGroup').slice(1);
-	fileActionContainers.forEach((container, index) => {
-		container.prepend(<a
-			href={generateURL[index]}
-			className="btn btn-sm tooltipped tooltipped-s BtnGroup-item"
-			aria-label="Delete this file from the pull request">Delete</a>);
-	});
+	for (const file of select.all('.file-header')) {
+		const fileName = select('.file-info a', file).title
+		const url = `/${repoURL}/delete/${branchName}/${fileName}`
+		select('.BtnGroup', file).prepend(
+			<a
+				href={url}
+				className="btn btn-sm tooltipped tooltipped-s BtnGroup-item"
+				aria-label="Delete this file from the pull request">
+				Delete
+			</a>
+		);
+	}
 }
