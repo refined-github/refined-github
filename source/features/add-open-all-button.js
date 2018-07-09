28deda1d147e62d546d70c6a1e14ec5500ec66e7
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2018-07-09 19:49:58 +0100

    fix the linting issues

--- source/features/add-open-all-button.js
+++ source/features/add-open-all-button.js
@@ -10,9 +10,9 @@ export default function () {
 	}
 
 	const buttonText = pageDetect.isIssueSearch() ? `Open all issues` : `Open all PRs`;
-	const openAllButton = <button id="openAllButton" onClick={openAll} class="btn btn-primary float-right rgh-open-all-issues-button" role="button" data-hotkey="c">{buttonText}</button>
+	const openAllButton = <button id="openAllButton" onClick={openAll} class="btn btn-primary float-right rgh-open-all-issues-button" role="button" data-hotkey="o">{buttonText}</button>;
 
-	if (select.exists('.issues-listing') && select.all('.link-gray-dark').length) {
+	if (select.exists('.issues-listing') && select.all('.link-gray-dark').length !== 0) {
 		select('.issues-listing>.subnav').append(openAllButton);
 	}
 }
