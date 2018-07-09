fcf05593fb07d5ac87b105c8b1b7f04dc87d10f7
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2018-07-09 19:43:30 +0100

    ad the add open all issues or PRs button

---
+++ source/features/add-open-all-button.js
@@ -0,0 +1,18 @@
+import {h} from 'dom-chef';
+import select from 'select-dom';
+import * as pageDetect from '../libs/page-detect';
+
+export default function () {
+	function openAll() {
+		select.all('.link-gray-dark').forEach(link => {
+			window.open(link.href);
+		});
+	}
+
+	const buttonText = pageDetect.isIssueSearch() ? `Open all issues` : `Open all PRs`;
+	const openAllButton = <button id="openAllButton" onClick={openAll} class="btn btn-primary float-right rgh-open-all-issues-button" role="button" data-hotkey="c">{buttonText}</button>
+
+	if (select.exists('.issues-listing') && select.all('.link-gray-dark').length) {
+		select('.issues-listing>.subnav').append(openAllButton);
+	}
+}
