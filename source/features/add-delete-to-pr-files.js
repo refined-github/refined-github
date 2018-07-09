8afbe24e4ab8d37030033753ec98b580e87ca3a4
Krista Handel <k.handel66@gmail.com>
2018-07-09 14:20:21 -0600

    Run linter and fix errors

--- source/features/add-delete-to-pr-files.js
+++ source/features/add-delete-to-pr-files.js
@@ -1,7 +1,6 @@
 import select from 'select-dom';
 import {h} from 'dom-chef';
 import * as pageDetect from '../libs/page-detect';
-import {getCleanPathname, isEnterprise} from '../libs/page-detect';
 
 const repoURL = pageDetect.getRepoURL();
 
@@ -13,10 +12,10 @@ export default function () {
 	const generateURL = filesInfo.map(file => `/${repoURL}/delete/${branchName}/${file.children[1].title}`);
 
 	const fileActionContainers = select.all('div.BtnGroup').slice(1);
-	fileActionContainers.map((container, index) => {
-			container.prepend(<a
-				href={generateURL[index]}
-				className="btn btn-sm tooltipped tooltipped-s BtnGroup-item"
-				aria-label="Delete this file from the pull request">Delete</a>);
+	fileActionContainers.forEach((container, index) => {
+		container.prepend(<a
+			href={generateURL[index]}
+			className="btn btn-sm tooltipped tooltipped-s BtnGroup-item"
+			aria-label="Delete this file from the pull request">Delete</a>);
 	});
 }
