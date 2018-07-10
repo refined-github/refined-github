107b2ccfc2af1c23f3e2ea93bea86d7f03243d05
Federico Brigante <github@bfred.it>
2018-07-10 20:40:00 +0200

    Fix delete link
    It should point to the source repo, not the current one.
    
    Conveniently, the edit button has a nearly-identical URL

--- source/features/add-delete-to-pr-files.js
+++ source/features/add-delete-to-pr-files.js
@@ -1,17 +1,14 @@
 import {h} from 'dom-chef';
 import select from 'select-dom';
 import * as icons from '../libs/icons';
-import {getRepoURL} from '../libs/page-detect';
 
 export default function () {
-	const repoURL = getRepoURL();
-	const branchName = select('.head-ref').textContent.split(':').pop();
+	for (const {parentElement: editLink} of select.all('.file-header .octicon-pencil')) {
+		// Transform the `edit` URL into a `delete` url
+		const deleteUrl = editLink.pathname.replace(/^(\/[^/]+\/[^/]+\/)edit/, '$1delete');
 
-	for (const file of select.all('.file-header')) {
-		const fileName = select('.file-info a', file).title;
-		const url = `/${repoURL}/delete/${branchName}/${fileName}`;
-		select('.octicon-pencil', file).parentElement.after(
-			<a href={url} className="btn-octicon btn-octicon-danger">
+		editLink.after(
+			<a href={deleteUrl} className="btn-octicon btn-octicon-danger">
 				{icons.trashcan()}
 			</a>
 		);
