bb20e4cc01ba9360d6655e1beace1e1ac0105fd3
Krista Handel <k.handel66@gmail.com>
2018-07-09 21:57:19 -0600

    Move line five into main funciton

--- source/features/add-delete-to-pr-files.js
+++ source/features/add-delete-to-pr-files.js
@@ -2,12 +2,11 @@ import {h} from 'dom-chef';
 import select from 'select-dom';
 import * as pageDetect from '../libs/page-detect';
 
-const repoURL = pageDetect.getRepoURL();
-
 export default function () {
 	const branchName = select('.head-ref').textContent.split(':').pop();
 
 	for (const file of select.all('.file-header')) {
+		const repoURL = pageDetect.getRepoURL();
 		const fileName = select('.file-info a', file).title;
 		const url = `/${repoURL}/delete/${branchName}/${fileName}`;
 		select('.BtnGroup', file).prepend(
