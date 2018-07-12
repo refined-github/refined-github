686dec238f2fdb2628b3c0aa4d92bc66cae57c02
Federico Brigante <github@bfred.it>
2018-07-12 16:48:56 +0200

    Only add delete button to PRs you can modify
    Fixes https://github.com/sindresorhus/refined-github/pull/1398#issuecomment-404506362

--- source/features/add-delete-to-pr-files.js
+++ source/features/add-delete-to-pr-files.js
@@ -3,7 +3,7 @@ import select from 'select-dom';
 import * as icons from '../libs/icons';
 
 export default function () {
-	for (const {parentElement: editLink} of select.all('.file-header .octicon-pencil')) {
+	for (const {parentElement: editLink} of select.all('.file-header [href] .octicon-pencil')) {
 		// Transform the `edit` URL into a `delete` url
 		const deleteUrl = editLink.pathname.replace(/^(\/[^/]+\/[^/]+\/)edit/, '$1delete');
 
