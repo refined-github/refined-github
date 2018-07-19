ea76de0b44bee815715a7613c6472040ed7c63c6
Federico Brigante <github@bfred.it>
2018-07-19 22:17:50 +0200

    Generate the dropdown on demand; add comments

--- source/features/hide-comments-faster.js
+++ source/features/hide-comments-faster.js
@@ -1,12 +1,12 @@
 import {h} from 'dom-chef';
 import select from 'select-dom';
+import onetime from 'onetime';
 import {onDeferredComments} from '../libs/events';
 
 function createMenu(hideButton) {
 	const comment = hideButton.closest('.unminimized-comment');
 	const form = select('.js-comment-minimize', comment);
-	form.classList.add('dropdown-menu', 'dropdown-menu-sw', 'text-gray-dark', 'show-more-popover', 'anim-scale-in');
-	form.hidden = true;
+	const details = hideButton.closest('details');
 
 	// Generate dropdown items
 	for (const reason of select.all('[name="classifier"] :not([value=""])', comment)) {
@@ -25,19 +25,26 @@ function createMenu(hideButton) {
 	select('.btn', form).remove();
 	select('[name="classifier"]', form).remove();
 
+	// Imitate existing menu
+	form.classList.add('dropdown-menu', 'dropdown-menu-sw', 'text-gray-dark', 'show-more-popover', 'anim-scale-in');
+	details.append(form);
+
+	// Hide this menu when the existing menu closes
+	details.addEventListener('toggle', () => {
+		form.hidden = true;
+	});
 	return form;
 }
 
 function createMenus() {
 	for (const hideButton of select.all('.unminimized-comment .js-comment-hide-button')) {
-		const menu = createMenu(hideButton);
-		hideButton.closest('.dropdown-menu').after(menu);
-		hideButton.classList.remove('js-comment-hide-button');
+		hideButton.classList.remove('js-comment-hide-button'); // Disable default behavior
+
+		// The menu will be generated on the first click.
+		// On successive clicks it will just be shown
+		const getMenu = onetime(() => createMenu(hideButton));
 		hideButton.addEventListener('click', () => {
-			menu.hidden = false;
-		});
-		hideButton.closest('details').addEventListener('toggle', () => {
-			menu.hidden = true;
+			getMenu().hidden = false;
 		});
 	}
 }
