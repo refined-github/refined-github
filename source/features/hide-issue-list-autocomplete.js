3e1fd51ef07038de5cb4cc5e8079278f21672b03
Federico Brigante <github@bfred.it>
2018-07-20 10:13:48 +0200

    Drop duplicate isIssueList check

--- source/features/hide-issue-list-autocomplete.js
+++ source/features/hide-issue-list-autocomplete.js
@@ -1,9 +1,5 @@
 import select from 'select-dom';
-import * as pageDetect from '../libs/page-detect';
 
 export default function () {
-	if (pageDetect.isIssueList()) {
-		const searchBar = select('.subnav-search');
-		searchBar.setAttribute('autocomplete', 'off');
-	}
+	select('.subnav-search').setAttribute('autocomplete', 'off');
 }
