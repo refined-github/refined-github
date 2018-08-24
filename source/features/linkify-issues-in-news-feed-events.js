4eb00c1964ea20e68e1d7e1e4fda24d00f901637
Connor Brathwaite <cbrathwaite@aldogroup.com>
2018-08-24 14:44:58 -0400

    (style): fits on one line

--- source/features/linkify-issues-in-news-feed-events.js
+++ source/features/linkify-issues-in-news-feed-events.js
@@ -5,9 +5,7 @@ import {editTextNodes} from './linkify-urls-in-code';
 
 export default function () {
 	observeEl('#dashboard .news', () => {
-		const feedEvents = select.all(
-			'#dashboard .news .watch_started, #dashboard .news .create, #dashboard .news .push, #dashboard .news .issues_opened',
-		);
+		const feedEvents = select.all('#dashboard .news .watch_started, #dashboard .news .create, #dashboard .news .push, #dashboard .news .issues_opened');
 		for (const feedEvent of feedEvents) {
 			const issues = select.all('blockquote, .f4.text-gray-light.ml-1', feedEvent);
 			for (const issue of issues) {
