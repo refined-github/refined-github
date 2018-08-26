b2219088b3abbf35bee6703a0de58d1505a698d2
momothereal <momothereal.mc@gmail.com>
2018-08-26 19:05:49 -0400

    Show total stars received for user

---
+++ source/features/show-user-stars-received.js
@@ -0,0 +1,45 @@
+import {h} from 'dom-chef';
+import select from 'select-dom';
+import * as icons from '../libs/icons';
+import fetchApi from '../libs/api';
+import {getCleanPathname} from '../libs/page-detect';
+
+export default async () => {
+	// Get all repositories
+	const username = getCleanPathname();
+
+	// Check if this is in fact a user (not an org)
+	const userContainer = select('[itemtype="http://schema.org/Person"]');
+	if (!userContainer) {
+		return;
+	}
+	const detailsContainer = select('.vcard-details', userContainer);
+	if (!detailsContainer) {
+		return;
+	}
+
+	// Count the stars
+	let starCount = 0;
+	let page = 1;
+	for (;;) {
+		// eslint-disable-next-line no-await-in-loop
+		const repoList = await fetchApi(`users/${username}/repos?type=owner&page=${page}`).catch(() => null);
+		if (!repoList || repoList.length === 0) {
+			break;
+		}
+		for (const repoObj of repoList) {
+			if (Object.prototype.hasOwnProperty.call(repoObj, 'stargazers_count')) {
+				starCount += repoObj.stargazers_count;
+			}
+		}
+		page++;
+	}
+
+	detailsContainer.append(
+		<li itemprop={'starCount'} itemProple={'false'} aria-label={'Star Count'}
+			className={'vcard-detail pt-1 css-truncate css-truncate-target'}>
+			{icons.star()}
+			<span className={'p-label'}>Stars Received: {starCount}</span>
+		</li>
+	);
+};
