574f3d3f4e17ea482efb6e5e1d34f145b50d0c2e
dutiyesh <dutiyesh@gmail.com>
2018-09-06 22:42:19 +0530

    Update link without URLSearchParams

--- source/features/set-default-repositories-type-to-sources.js
+++ source/features/set-default-repositories-type-to-sources.js
@@ -2,12 +2,9 @@ import select from 'select-dom';
 
 export default function () {
 	// Get repositories link from user profile navigation
-	const link = select('.user-profile-nav:not(.is-placeholder) a[href*="tab=repositories"]');
-	const search = new URLSearchParams(link.search);
+	const link = select('.user-profile-nav a[href*="tab=repositories"]');
 
-	// Set default type to source if not present
-	if (!search.get('type')) {
-		search.set('type', 'source');
-		link.search = search;
+	if (link) {
+		link.search += '&type=source';
 	}
 }
