59981eb11d7f982f6ab5764710d74dcc2719ed4a
Federico Brigante <github@bfred.it>
2018-09-14 23:40:44 +0800

    Fix regex

--- source/features/hide-useless-comments.js
+++ source/features/hide-useless-comments.js
@@ -10,7 +10,7 @@ export default function () {
 	let uselessCount = 0;
 	for (const commentText of select.all('.comment-body > p:only-child')) {
 		// Find useless comments
-		if (!/^([+-]\d+!*|👍|🙏||)+$/.test(commentText.textContent.trim())) {
+		if (!/^([+-]\d+!*|👍|🙏|👎|👌)+$/.test(commentText.textContent.trim())) {
 			continue;
 		}
 
