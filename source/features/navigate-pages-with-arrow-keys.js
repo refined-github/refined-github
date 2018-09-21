51a8da0714df13a4807b6f647bc174e5c29be9fb
Sebastian Trzpiot <mail@trzpiot.de>
2018-09-21 09:22:57 +0200

    Change the groupID at registration of the shortcut

--- source/features/navigate-pages-with-arrow-keys.js
+++ source/features/navigate-pages-with-arrow-keys.js
@@ -2,12 +2,12 @@ import select from 'select-dom';
 import {registerShortcut} from './improve-shortcut-help';
 
 export default function () {
-	registerShortcut('Site wide shortcuts', 'ArrowRight', 'Go to the next page.');
+	registerShortcut('site', 'ArrowRight', 'Go to the next page.');
 	const createNextPageButton = select('a.next_page');
 	if (createNextPageButton) {
 		createNextPageButton.setAttribute('data-hotkey', 'ArrowRight');
 	}
-	registerShortcut('Site wide shortcuts', 'ArrowLeft', 'Go to the previous page.');
+	registerShortcut('site', 'ArrowLeft', 'Go to the previous page.');
 	const createPreviousPageButton = select('a.previous_page');
 	if (createPreviousPageButton) {
 		createPreviousPageButton.setAttribute('data-hotkey', 'ArrowLeft');
