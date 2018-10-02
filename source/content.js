4720906158d456b4a4b9bcb850e52918e322413b
Sebastian Trzpiot <mail@trzpiot.de>
2018-10-02 06:19:07 +0200

    Add navigation through pages via arrow keys (#1542)

--- source/content.js
+++ source/content.js
@@ -83,6 +83,7 @@ import hideIssueListAutocomplete from './features/hide-issue-list-autocomplete';
 import userProfileFollowerBadge from './features/user-profile-follower-badge';
 import setDefaultRepositoriesTypeToSources from './features/set-default-repositories-type-to-sources';
 import markPrivateOrgs from './features/mark-private-orgs';
+import navigatePagesWithArrowKeys from './features/navigate-pages-with-arrow-keys';
 
 import * as pageDetect from './libs/page-detect';
 import {safeElementReady, enableFeature, safeOnAjaxedPages, injectCustomCSS} from './libs/utils';
@@ -195,6 +196,7 @@ function ajaxedPagesHandler() {
 	enableFeature(linkifyBranchRefs);
 	enableFeature(openAllSelected);
 	enableFeature(hideUselessComments);
+	enableFeature(navigatePagesWithArrowKeys);
 
 	if (pageDetect.isIssueSearch() || pageDetect.isPRSearch()) {
 		enableFeature(addYoursMenuItem);
