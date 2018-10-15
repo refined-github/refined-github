08bef8f1a841467c88f2f11296669cd550bf78bb
Waseem Dahman <dwaseem@icloud.com>
2018-10-14 23:45:15 -0400

    Additional refactor and rename feature

--- source/content.js
+++ source/content.js
@@ -49,7 +49,6 @@ import removeProjectsTab from './features/remove-projects-tab';
 import hideUselessComments from './features/hide-useless-comments';
 import fixSquashAndMergeTitle from './features/fix-squash-and-merge-title';
 import fixSquashAndMergeMessage from './features/fix-squash-and-merge-message';
-import stickyFileHeader from './features/sticky-file-header';
 import addTitleToEmojis from './features/add-title-to-emojis';
 import sortMilestonesByClosestDueDate from './features/sort-milestones-by-closest-due-date';
 import openCIDetailsInNewTab from './features/open-ci-details-in-new-tab';
@@ -65,6 +64,7 @@ import addQuickReviewButtons from './features/add-quick-review-buttons';
 import extendDiffExpander from './features/extend-diff-expander';
 import sortIssuesByUpdateTime from './features/sort-issues-by-update-time';
 import makeDiscussionSidebarSticky from './features/make-discussion-sidebar-sticky';
+import makeHeadersSticky from './features/make-headers-sticky';
 import shortenLinks from './features/shorten-links';
 import waitForBuild from './features/wait-for-build';
 import addDownloadFolderButton from './features/add-download-folder-button';
@@ -307,7 +307,7 @@ function ajaxedPagesHandler() {
 		pageDetect.isPRSearch() ||
 		pageDetect.isSingleFile()
 	) {
-		enableFeature(stickyFileHeader);
+		enableFeature(makeHeadersSticky);
 	}
 
 	if (pageDetect.isSingleFile()) {
--- source/features/make-headers-sticky.js
+++ source/features/make-headers-sticky.js
@@ -1,21 +1,10 @@
 import select from 'select-dom';
 
-const className = 'rgh-sticky-file-header';
-
 export default function () {
-	const fileHeaderSelectors = `
-		.file .file-header,
-		.issues-listing .table-list-header,
-		.pull-request-tab-content .diff-view .file-header
-	`;
-
-	for (const el of select.all(fileHeaderSelectors)) {
-		el.classList.add(className);
-	}
-
+	document.documentElement.classList.add('rgh-sticky-header');
 	// Flipping all tooltips inside all .file-header to their opposite direction
 	// to prevent them from cutting off.
-	for (const el of select.all(`.${className} [class*=tooltipped-n]`)) {
+	for (const el of select.all('.file-header [class*=tooltipped-n]')) {
 		// Replace the direction suffix from north to south
 		// example: -n → -s, -nw → -sw, or -ne → -se
 		el.className = el.className.replace('tooltipped-n', 'tooltipped-s');
