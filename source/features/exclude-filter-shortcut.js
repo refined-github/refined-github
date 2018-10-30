6522ff8f1a9dc68d7f35d8093255016242eabfbf
Philipp Beck <dertieran@gmail.com>
2018-10-30 20:40:06 +0100

    Parse clicked filter item
    Build the filter field and text from the clicked item.
    There is still the issue that the negated filter will not be added when the "normal" filter is already set.

--- source/features/exclude-filter-shortcut.js
+++ source/features/exclude-filter-shortcut.js
@@ -5,26 +5,73 @@ This feature ...
 import {h} from 'dom-chef';
 import select from 'select-dom';
 import delegate from 'delegate';
+import {getUsername} from '../libs/utils';
 
-const qualifier = [
-	'author',
-	'assignee',
-	'label',
-	'review',
-	'reviewed-by'
-];
+const FILTER_FIELD_MAP = {
+	author: 'author',
+	label: 'label',
+	// NOTE: Doesn't work because it uses the project id
+	// projects: 'projects',
+	milestones: 'milestone',
+	assigns: 'assignee'
+};
 
-// TODO: We guess the last qualifier is the right one
-// This might not be always correct, specifically other features could influence this
-// Also if e.g. two labels are selected the alt clicking would remove it and negate the other one
-// To solve this we would need to parse the item to see what filter it would build
-const lastQualifier = new RegExp(`((?:${qualifier.join('|')}):[^:]+)$`);
+const getFilterField = item => {
+	const filter = item.parentElement.getAttribute('data-filterable-for') || '';
+	const name = filter.replace('-filter-field', '');
+	return FILTER_FIELD_MAP[name];
+};
 
-const negateSearch = item => {
+// NOTE: All filter text that contains a space will be wrapped with double quotes
+const normalizeText = text => {
+	const normalized = text.trim();
+	return normalized.includes(' ') ? `"${normalized}"` : normalized;
+};
+
+const getFilter = item => {
+	const field = getFilterField(item);
+	if (!field) {
+		return;
+	}
+
+	const itemText = select('.select-menu-item-text', item);
+	if (!itemText) {
+		return;
+	}
+
+	if (field === 'label') {
+		const name = select('.name', itemText);
+		const text = normalizeText(name.textContent);
+		return `label:${text}`;
+	}
+
+	// NOTE: This worked for all tested filter fields
+	const first = itemText.childNodes[0];
+	const text = normalizeText(first.textContent);
+	if (field) {
+		return `${field}:${text}`;
+	}
+
+	// NOTE: Speciall cases
+	if (text === 'Reviewed by you') {
+		return `reviewed-by:${getUsername()}`;
+	}
+};
+
+const buildSearch = item => {
+	const filter = getFilter(item);
+	if (!filter) {
+		return;
+	}
+
 	const search = new URLSearchParams(item.search);
-	const q = search.get('q') || '';
-	const negated = q.replace(lastQualifier, '-$1');
+	const query = search.get('q') || '';
+
+	// NOTE: The item search will not contain the filter even if the negated filter is set
+	// TODO: We don't set the negated filter if the non negated filter is already set
+	const negated = query.replace(filter, `-${filter}`);
 	search.set('q', negated);
+
 	return search;
 };
 
@@ -35,17 +82,15 @@ export default function () {
 		}
 
 		const item = event.delegateTarget;
-		const negatedSearch = negateSearch(item);
-		// NOTE: We don't need to handle the click if we didn't change the url
-		if (item.search === negatedSearch) {
+		const search = buildSearch(item);
+		if (!search) {
 			return;
 		}
 
 		event.preventDefault();
-		item.search = negatedSearch;
+		item.search = search;
 		item.click();
 	};
 
-	const base = select('.table-list-filters');
-	delegate(base, 'a.select-menu-item', 'click', onItemClicked, false);
+	delegate('.table-list-filters', 'a.select-menu-item', 'click', onItemClicked, false);
 }
