d98e10ee234bd20ceee5e8edb9810261bbd360ac
Adam LaCombe <adam@edgeworkscreative.com>
2018-11-07 08:26:40 -0500

    style: add missing ; / change let to const / " -> '

--- source/features/add-star-repo-hotkey.js
+++ source/features/add-star-repo-hotkey.js
@@ -1,17 +1,17 @@
-import {registerShortcut} from './improve-shortcut-help';
 import select from 'select-dom';
+import {registerShortcut} from './improve-shortcut-help';
 
 export default function () {
 	registerShortcut('repos', 'g s', 'Star and unstar repository');
 	let lastKey;
-	document.addEventListener('keydown', (e) => {
-		if ((lastKey === "g" && e.key === "s") || (e.key === "g" && lastKey === "s")) {
-			let starButtonSelector = (select('div.js-social-container.starring-container').classList.contains('on')) ? 'form.starred.js-social-form > button' : 'form.unstarred.js-social-form > button';
+	document.addEventListener('keydown', e => {
+		if ((lastKey === 'g' && e.key === 's') || (e.key === 'g' && lastKey === 's')) {
+			const starButtonSelector = (select('div.js-social-container.starring-container').classList.contains('on')) ? 'form.starred.js-social-form > button' : 'form.unstarred.js-social-form > button';
 			select(starButtonSelector).click();
 		}
 		lastKey = e.key;
 	});
 	document.addEventListener('keyup', () => {
 		lastKey = null;
-	})
+	});
 }
