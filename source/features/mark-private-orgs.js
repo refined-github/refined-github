257842eb7a5b9a306835efa9eb851f35e7353fa4
Federico Brigante <github@bfred.it>
2018-11-12 20:03:42 +0700

    Skip API call if no orgs are on the page

--- source/features/mark-private-orgs.js
+++ source/features/mark-private-orgs.js
@@ -4,13 +4,16 @@ import * as icons from '../libs/icons';
 import * as api from '../libs/api';
 
 export default async function () {
-	let publicOrgs = await api.v3(`users/${getUsername()}/orgs`);
-	publicOrgs = publicOrgs.map(orgData => `/${orgData.login}`);
+	const orgs = select.all('.avatar-group-item[data-hovercard-type="organization"]');
+	if (orgs.length > 0) {
+		let publicOrgs = await api.v3(`users/${getUsername()}/orgs`);
+		publicOrgs = publicOrgs.map(orgData => `/${orgData.login}`);
 
-	for (const org of select.all('.avatar-group-item[data-hovercard-type="organization"]')) {
-		if (!publicOrgs.includes(org.pathname)) {
-			org.classList.add('rgh-private-org');
-			org.append(icons.privateLockFilled());
+		for (const org of orgs) {
+			if (!publicOrgs.includes(org.pathname)) {
+				org.classList.add('rgh-private-org');
+				org.append(icons.privateLockFilled());
+			}
 		}
 	}
 }
