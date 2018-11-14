eac78164d0039d57dfa63083a3d36118686c09ab
Federico Brigante <github@bfred.it>
2018-11-14 16:45:12 +0700

    Turn into a plain link + document

--- source/features/show-user-top-repositories.js
+++ source/features/show-user-top-repositories.js
@@ -1,159 +1,16 @@
+/*
+Find a user’s most starred repositories in their profile.
+https://user-images.githubusercontent.com/1402241/48474026-43e3ae80-e82c-11e8-93de-159ad4c6f283.png
+*/
+
 import {h} from 'dom-chef';
 import select from 'select-dom';
 import {getCleanPathname} from '../libs/page-detect';
-import fetchGraph from '../libs/graph';
-import * as icons from '../libs/icons';
-
-async function onTopRepositoriesSelect(titleNode) {
-	const username = getCleanPathname();
-	// Get two most starred repos
-	const query = `
-	{
-		search(type: REPOSITORY, query: "user:${username} is:public stars:>0", first: 6) {
-			edges {
-				node {
-					... on Repository {
-						descriptionHTML
-						primaryLanguage {
-							color
-							name
-						}
-						url
-						name
-						owner {
-							login
-							__typename
-						}
-						stargazers {
-							totalCount
-						}
-						forkCount
-						isFork
-						parent {
-							nameWithOwner
-							url
-						}
-					}
-				}
-			}
-		}
-	}
-	`;
-	const result = await fetchGraph(query);
-	const repoList = result.search.edges;
-	if (repoList.length === 0) {
-		// No repos
-		return;
-	}
-	const reposContainer = select('.pinned-repos-list');
-	const originalReposContainer = reposContainer.innerHTML;
-	reposContainer.innerHTML = '';
-
-	function repoForkLabel(repo) {
-		if (repo.isFork) {
-			return <p className={'text-gray text-small mb-2'}>
-				Forked from
-				<a href={repo.parent.url}>{repo.parent.nameWithOwner}</a>
-			</p>;
-		}
-		return '';
-	}
-
-	function repoLanguageColor(repo) {
-		if (repo.primaryLanguage) {
-			return <span className={'repo-language-color pinned-repo-meta'} style={{backgroundColor: repo.primaryLanguage.color}}/>;
-		}
-		return '';
-	}
-
-	function repoForkCount(repo) {
-		if (repo.forkCount > 0) {
-			return <a className={'pinned-repo-meta muted-link'}
-				href={repo.url + '/network'}>{icons.fork()} {simplifyNumber(repo.forkCount)}</a>;
-		}
-		return '';
-	}
-
-	function simplifyNumber(count) {
-		const num = Number(count);
-		if (num >= 1000) {
-			return (Math.round(num / 100) / 10) + 'k';
-		}
-		return count;
-	}
-
-	for (const repo of repoList) {
-		reposContainer.append(
-			<li className={`pinned-repo-item ${repo.node.owner.__typename === 'Organization' ? 'org-pinned-repo-item' : ''} p-3 mb-3 border border-gray-dark rounded-1 public ${repo.node.isFork ? 'fork' : 'source'}`}>
-				<span className={'pinned-repo-item-content'}>
-					<span className={'d-block'}>
-						<a href={repo.node.url} className={'text-bold'}>
-							<span className={'repo js-repo'}>{repo.node.name}</span>
-						</a>
-						{repoForkLabel(repo.node)}
-					</span>
-					<p className={'pinned-repo-desc text-gray text-small d-block mt-2 mb-3'}
-						dangerouslySetInnerHTML={{__html: repo.node.descriptionHTML}}/>
-					<p className={'mb-0 f6 text-gray'}>
-						{repoLanguageColor(repo.node)}
-						{repo.node.primaryLanguage ? '  ' + repo.node.primaryLanguage.name + '  ' : ''}
-						<a className={'pinned-repo-meta muted-link'}
-							href={repo.node.url + '/stargazers'}>{icons.star()} {simplifyNumber(repo.node.stargazers.totalCount)} </a>
-						&nbsp;{repoForkCount(repo.node)}
-					</p>
-				</span>
-			</li>
-		);
-	}
-
-	// Switch the title
-	titleNode.innerHTML = '';
-	titleNode.append(
-		<span>
-			<span className={'mr-2'}>
-				<a href={'#'} onClick={() => onPinnedRepositoriesSelect(titleNode, originalReposContainer)}>
-					Pinned repositories
-				</a>
-			</span>
-			/
-			<span className={'ml-2'}>
-				Top repositories
-			</span>
-		</span>
-	);
-}
-
-function onPinnedRepositoriesSelect(showcaseTitle, originalReposContainer) {
-	if (originalReposContainer) {
-		select('.pinned-repos-list').innerHTML = originalReposContainer;
-	}
-	showcaseTitle.innerHTML = '';
-	showcaseTitle.append(
-		<span>
-			<span className={'mr-2'}>Pinned repositories</span>
-			/
-			<span className={'ml-2'}>
-				<a href={'#'} onClick={() => onTopRepositoriesSelect(showcaseTitle)}>
-					Top repositories
-				</a>
-			</span>
-		</span>
-	);
-}
 
 export default async function () {
-	// Check if the user has no repositories to show
-	const noShowcaseArea = select('.js-pinned-repos-reorder-container .blankslate');
-	if (noShowcaseArea) {
-		return;
-	}
-	// The "Pinned repositories" title
 	const showcaseTitle = select('.js-pinned-repos-reorder-container .text-normal');
-	if (!showcaseTitle) {
-		return;
+	if (showcaseTitle) {
+		const url = `/search?o=desc&q=user%3A${getCleanPathname()}&s=stars&type=Repositories`;
+		showcaseTitle.firstChild.after(' / ', <a href={url}>Top repositories</a>)
 	}
-	if (!showcaseTitle.textContent.includes('Pinned')) {
-		return;
-	}
-	onPinnedRepositoriesSelect(showcaseTitle);
 }
