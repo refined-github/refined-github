d3c8c27b117a36fad17fc0e4967fb66a8421cec8
Federico Brigante <github@bfred.it>
2018-11-26 01:05:53 +0800

    Shorten
    - Select travis check links directly and loop them
    - Select travis-ci.com link directly
    - Avoid setting rel and target (the first one is ironically set to noopener for the local link but not the external link; the second one is already _blank on both)

--- source/features/bypass-checks-travis.js
+++ source/features/bypass-checks-travis.js
@@ -1,48 +1,21 @@
 import select from 'select-dom';
 import domify from '../libs/domify';
 
-export default () => {
-	const checks = select.all('.merge-status-item');
-	const checkTravisBranch = checks.find(isTravisBranchCheck);
-	const checkTravisPR = checks.find(isTravisPRCheck);
-
-	if (checkTravisBranch) {
-		bypassCheck(checkTravisBranch);
+export default function () {
+	for (const check of select.all('[href="/apps/travis-ci"]')) {
+		const details = select('.status-actions', check.parentNode);
+		bypassCheck(details);
 	}
-	if (checkTravisPR) {
-		bypassCheck(checkTravisPR);
-	}
-};
+}
 
-async function bypassCheck(check) {
-	const details = select('.status-actions', check);
-	const checkUrl = details.getAttribute('href');
-
-	const response = await fetch(checkUrl, {
+async function bypassCheck(details) {
+	const response = await fetch(details.href, {
 		credentials: 'include'
 	});
 
 	if (response.ok) {
 		const dom = domify(await response.text());
-		const links = select.all('[id^="check_run_"] a', dom);
-		const viewMoreLink = links.find(isViewMoreLink);
-
-		if (viewMoreLink) {
-			details.href = viewMoreLink.href;
-			details.rel = viewMoreLink.rel;
-			details.target = viewMoreLink.target;
-		}
+		const directLink = select('[href^="https://travis-ci.com"].text-small', dom);
+		details.href = directLink.href;
 	}
-}
-
-function isTravisPRCheck(check) {
-	return /Travis CI - Pull Request/.test(check.textContent);
-}
-
-function isTravisBranchCheck(check) {
-	return /Travis CI - Branch/.test(check.textContent);
-}
-
-function isViewMoreLink(link) {
-	return /View more details on Travis CI/.test(link.textContent);
 }
