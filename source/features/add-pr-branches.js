70d0bd1f1d83f6c0d341c2d3a391cffaf089cb39
Jeroen van Warmerdam <jeronevw@hotmail.com>
2018-11-29 22:39:23 +0100

    Use one query for all PRs

--- source/features/add-pr-branches.js
+++ source/features/add-pr-branches.js
@@ -4,11 +4,14 @@ import * as cache from '../libs/cache';
 import * as api from '../libs/api';
 import {getOwnerAndRepo} from '../libs/page-detect';
 
-async function fetchFromApi(owner, repo, number) {
-	const response = await api.v4(`
+function buildQuery(owner, repo, numbers) {
+	let query = `
 {
-  repository(owner: "${owner}", name: "${repo}") {
-    pullRequest(number: ${number}) {
+  repository(owner: "${owner}", name: "${repo}") {`;
+
+	for (const number of numbers) {
+		query += `
+    ${number}: pullRequest(number: ${number.replace('issue_', '')}) {
       baseRef {
         name
         repository {
@@ -27,26 +30,46 @@ async function fetchFromApi(owner, repo, number) {
           }
         }
       }
-    }
+    }`;
+	}
+
+	query += `
   }
-}`);
-	if (response.data) {
-		const pr = response.data.repository.pullRequest;
-		return {
-			base: {
-				label: pr.baseRef.name,
-				url: `${pr.baseRef.repository.url}/tree/${pr.baseRef.name}`
-			},
-			head: {
-				label: (pr.headRef.repository.owner.login === owner ? pr.headRef.name : `${pr.headRef.repository.owner.login}:${pr.headRef.name}`),
-				url: `${pr.headRef.repository.url}/tree/${pr.headRef.name}`
-			}
-		};
+}`;
+	return query;
+}
+
+async function fetchFromApi(owner, repo, numbers) {
+	const query = buildQuery(owner, repo, numbers);
+	const response = await api.v4(query);
+	console.log('add-pr-branches 3:', numbers, response);
+
+	if (response.data && response.data.repository) {
+		const d = {};
+		for (const [id, data] of Object.entries(response.data.repository)) {
+			d[id] = extractBranches(owner, data);
+		}
+		return d;
 	}
 }
 
+function extractBranches(owner, data) {
+	console.log('add-pr-branches 5:', data);
+
+	return {
+		base: {
+			label: data.baseRef.name,
+			url: `${data.baseRef.repository.url}/tree/${data.baseRef.name}`
+		},
+		head: {
+			label: (data.headRef.repository.owner.login === owner ? data.headRef.name : `${data.headRef.repository.owner.login}:${data.headRef.name}`),
+			url: `${data.headRef.repository.url}/tree/${data.headRef.name}`
+		}
+	};
+}
+
 function getPullBranches(owner, repo, number) {
-	return cache.getSet(`pull-branches:${owner}/${repo}/pull/${number}`,
+	return cache.getSet(`pull-branches:${owner}/${repo}`,
 		() => fetchFromApi(owner, repo, number)
 		, 1
 	);
@@ -54,25 +77,31 @@ function getPullBranches(owner, repo, number) {
 
 export default async function () {
 	const {ownerName, repoName} = getOwnerAndRepo();
+	const elements = select.all('.issues-listing .js-navigation-container .js-navigation-item');
+	const ids = elements.map(pr => pr.id);
+	const branches = await getPullBranches(ownerName, repoName, ids);
+	console.log('add-pr-branches 1:', ownerName, repoName, elements, ids, branches);
 
-	const prs = select.all('.issues-listing .js-navigation-container .js-navigation-item');
-	for (const pr of prs) {
-		const number = pr.id.replace('issue_', '');
-		const pull = await getPullBranches(ownerName, repoName, number);
-		const section = select('.col-9.lh-condensed', pr);
-		if (pull && section) {
-			section.appendChild(
-				<div class="mt-1 text-small text-gray">
-					Merge
-					<span class="commit-ref css-truncate user-select-contain">
-						<a title={pull.head.label} href={pull.head.url}>{pull.head.label}</a>
-					</span>
-					into
-					<span class="commit-ref css-truncate user-select-contain">
-						<a title={pull.base.label} href={pull.base.url}>{pull.base.label}</a>
-					</span>
-				</div>
-			);
+	if (branches) {
+		for (const element of elements) {
+			const pull = branches[element.id];
+			const section = select('.col-9.lh-condensed', element);
+			console.log('add-pr-branches 2:', pull, element, section);
+
+			if (pull && section) {
+				section.appendChild(
+					<div class="mt-1 text-small text-gray">
+						Merge
+						<span class="commit-ref css-truncate user-select-contain">
+							<a title={pull.head.label} href={pull.head.url}>{pull.head.label}</a>
+						</span>
+						into
+						<span class="commit-ref css-truncate user-select-contain">
+							<a title={pull.base.label} href={pull.base.url}>{pull.base.label}</a>
+						</span>
+					</div>
+				);
+			}
 		}
 	}
 }
