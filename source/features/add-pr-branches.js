a53dff8494a723954437b65d8ef6b5059d6ab6d8
Jeroen van Warmerdam <jeronevw@hotmail.com>
2018-11-29 06:58:01 +0100

    Linting

--- source/features/add-pr-branches.js
+++ source/features/add-pr-branches.js
@@ -1,8 +1,8 @@
-import { h } from 'dom-chef';
+import {h} from 'dom-chef';
 import select from 'select-dom';
 import * as cache from '../libs/cache';
 import * as api from '../libs/api';
-import { getOwnerAndRepo } from '../libs/page-detect';
+import {getOwnerAndRepo} from '../libs/page-detect';
 
 async function fetchFromApi(owner, repo, number) {
 	const response = await api.v3(`repos/${owner}/${repo}/pulls/${number}`); // https://developer.github.com/v3/pulls/#get-a-single-pull-request --> /repos/:owner/:repo/pulls/:number
@@ -20,7 +20,7 @@ async function fetchFromApi(owner, repo, number) {
 	}
 }
 
-async function getPullBranches(owner, repo, number) {
+function getPullBranches(owner, repo, number) {
 	return cache.getSet(`pull-branches:${owner}/${repo}/pull/${number}`,
 		() => fetchFromApi(owner, repo, number)
 		, 1
@@ -28,27 +28,26 @@ async function getPullBranches(owner, repo, number) {
 }
 
 export default async function () {
-	const { ownerName, repoName } = getOwnerAndRepo();
+	const {ownerName, repoName} = getOwnerAndRepo();
 
-	const prs = select.all(".issues-listing .js-navigation-container .js-navigation-item");
+	const prs = select.all('.issues-listing .js-navigation-container .js-navigation-item');
 	for (const pr of prs) {
 		const number = pr.id.replace('issue_', '');
 		const pull = await getPullBranches(ownerName, repoName, number);
 		const section = select('.col-9.lh-condensed', pr);
-
-		if (!pull || !section) return;
-
-		section.appendChild(
-			<div class="mt-1 text-small text-gray">
-				Merge
-				<span class="commit-ref css-truncate user-select-contain">
-					<a title={pull.head.label} href={pull.head.url}>{pull.head.label}</a>
-				</span>
-				into
-				<span class="commit-ref css-truncate user-select-contain">
-					<a title={pull.base.label} href={pull.base.url}>{pull.base.label}</a>
-				</span>
-			</div>
-		);
+		if (pull && section) {
+			section.appendChild(
+				<div class="mt-1 text-small text-gray">
+					Merge
+					<span class="commit-ref css-truncate user-select-contain">
+						<a title={pull.head.label} href={pull.head.url}>{pull.head.label}</a>
+					</span>
+					into
+					<span class="commit-ref css-truncate user-select-contain">
+						<a title={pull.base.label} href={pull.base.url}>{pull.base.label}</a>
+					</span>
+				</div>
+			);
+		}
 	}
 }
