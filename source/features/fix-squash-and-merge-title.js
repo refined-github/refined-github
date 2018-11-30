ea2b17e295c6413b60ab04228f599f69159e1807
Laxman Damera <notlmn@outlook.com>
2018-12-01 00:37:45 +0530

    Update squash-and-merge title when PR title changes

--- source/features/fix-squash-and-merge-title.js
+++ source/features/fix-squash-and-merge-title.js
@@ -1,13 +1,20 @@
 import select from 'select-dom';
+import observeEl from '../libs/simplified-element-observer';
+
+function updateSquashAndMergeTitle() {
+	const title = select('.js-issue-title').textContent;
+	const number = select('.gh-header-number').textContent;
+	select('#merge_title_field').value = `${title.trim()} (${number})`;
+}
 
 export default function () {
 	const btn = select('.merge-message .btn-group-squash [type=submit]');
 	if (!btn) {
 		return;
 	}
-	btn.addEventListener('click', () => {
-		const title = select('.js-issue-title').textContent;
-		const number = select('.gh-header-number').textContent;
-		select('#merge_title_field').value = `${title.trim()} (${number})`;
-	});
+
+	btn.addEventListener('click', updateSquashAndMergeTitle);
+
+	// Watch for any changes to the PR title
+	observeEl('.js-issue-title', updateSquashAndMergeTitle);
 }
