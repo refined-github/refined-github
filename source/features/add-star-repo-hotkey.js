e3fe634faa64ed647fd3fe4cb2d1d154689809b3
Adam LaCombe <adam@edgeworkscreative.com>
2018-12-13 08:20:02 -0500

    lint :)

--- source/features/add-star-repo-hotkey.js
+++ source/features/add-star-repo-hotkey.js
@@ -9,16 +9,16 @@ export default function () {
 		starButtons[0].removeAttribute('data-hotkey');
 		starButtons[1].setAttribute('data-hotkey', 'g s');
 	};
-	
+
 	const addHotkeyToUnstarButton = () => {
 		starButtons[0].setAttribute('data-hotkey', 'g s');
 		starButtons[1].removeAttribute('data-hotkey');
 	};
 
-	if (!select.exists('.js-social-container.starring-container.on')) {
-		addHotkeyToStarButton();
-	} else {
+	if (select.exists('.js-social-container.starring-container.on')) {
 		addHotkeyToUnstarButton();
+	} else {
+		addHotkeyToStarButton();
 	}
 
 	starButtons[0].addEventListener('click', addHotkeyToStarButton);
