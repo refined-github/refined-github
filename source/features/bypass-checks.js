9bd6325a4ba81670743228c19f78f0dd46249678
Federico Brigante <github@bfred.it>
2019-01-07 10:28:50 +0700

    Cleanup
    - Drop old misplaced comment
    - Drop response check (if it's an error, let it bubble up)
    - Limit selector to checks area

--- source/features/bypass-checks.js
+++ source/features/bypass-checks.js
@@ -3,21 +3,12 @@ import domify from '../libs/domify';
 
 export default async function () {
 	// If anything errors, RGH will display the error next to the feature name
-	await Promise.all(select.all('[href^="/apps/"]').map(bypass));
+	await Promise.all(select.all('.merge-status-item [href^="/apps/"]').map(bypass));
 }
 
 async function bypass(check) {
-	// On the checks page, there is a link near the bottom that says 'View more details'.
-	// This code finds that link and replaces the `.href` of the 'details' link on the PR conversation page.
-
 	const details = select('.status-actions', check.parentNode);
-
 	const response = await fetch(details.href);
-
-	if (!response.ok) {
-		return;
-	}
-
 	const dom = domify(await response.text());
 	const directLink = select('a.text-small .octicon-link-external', dom);
 	details.href = directLink.parentNode.href;
