c7957e2c63b1161abc561c1edc8e01270ed71ab2
Dominic <8808097+doms@users.noreply.github.com>
2019-01-14 11:34:44 -0600

    Preserve Projects tab when current user is repo owner (#1711)
    This will remind the user to disable the Projects feature or will allow the feature to be used more easily without having to disable the feature.

--- source/features/remove-projects-tab.js
+++ source/features/remove-projects-tab.js
@@ -4,6 +4,12 @@ import onetime from 'onetime';
 import * as pageDetect from '../libs/page-detect';
 
 const removeProjectsTab = () => {
+	// Only those who can create a project will see the 'Settings' tab
+	// so, do not remove the 'Projects' tab if the 'Settings' tab exists
+	if (select.exists('.js-repo-nav [data-selected-links^="repo_settings"]')) {
+		return false;
+	}
+
 	const projectsTab = select('.js-repo-nav [data-selected-links^="repo_projects"]');
 	if (projectsTab && projectsTab.querySelector('.Counter, .counter').textContent === '0') {
 		projectsTab.remove();
