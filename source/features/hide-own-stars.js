3b2772e44ab7f899f620ab3a9aa0e0bc84dec0b8
Federico Brigante <github@bfred.it>
2019-01-15 13:55:42 +0700

    Fix hide-own-stars for new responsive dashbord

--- source/features/hide-own-stars.js
+++ source/features/hide-own-stars.js
@@ -1,15 +1,24 @@
+// Hide other users starring/forking your repos
+
 import select from 'select-dom';
-import observeEl from '../libs/simplified-element-observer';
 import {getUsername} from '../libs/utils';
 
-// Hide other users starring/forking your repos
-export default function () {
-	const username = getUsername();
-	observeEl('#dashboard .news', () => {
-		for (const item of select.all('#dashboard .news .watch_started, #dashboard .news .fork')) {
-			if (select(`a[href^="/${username}"]`, item)) {
-				item.style.display = 'none';
-			}
+const observer = new MutationObserver(([{addedNodes}]) => {
+	// Remove events
+	for (const item of select.all('#dashboard .news .watch_started, #dashboard .news .fork')) {
+		if (select(`a[href^="/${getUsername()}"]`, item)) {
+			item.style.display = 'none';
 		}
-	});
+	}
+
+	// Observe the new ajaxed-in containers
+	for (const node of addedNodes) {
+		if (node.tagName === 'DIV') {
+			observer.observe(node, {childList: true});
+		}
+	}
+});
+
+export default function () {
+	observer.observe(select('#dashboard .news'), {childList: true});
 }
