5488d65bdd840c6b340e1d0a9b36dcf1205befb9
Federico Brigante <github@bfred.it>
2019-01-21 17:19:29 +0700

    Drop commenters; shorten; use features.add

--- source/features/add-co-authored-by.tsx
+++ source/features/add-co-authored-by.tsx
@@ -1,19 +1,17 @@
-import {h} from 'dom-chef';
 import select from 'select-dom';
+import delegate from 'delegate';
 import * as api from '../libs/api';
-import {escapeForGql, getOwnerAndRepo, getDiscussionNumber} from '../libs/utils';
+import features from '../libs/features';
+import {getOwnerAndRepo, getDiscussionNumber} from '../libs/utils';
+import {getOP} from './op-labels';
 
-const coAuthorData = {};
+let coAuthors;
 
 async function fetchCoAuthoredData() {
-	const {ownerName, repoName} = getOwnerAndRepo();
 	const prNumber = getDiscussionNumber();
+	const {ownerName, repoName} = getOwnerAndRepo();
 
-	if (!ownerName || !repoName || !prNumber) {
-		return;
-	}
-
-	const contributorData = await api.v4(
+	const userInfo = await api.v4(
 		`{
 			repository(owner: "${ownerName}", name: "${repoName}") {
 				pullRequest(number: ${prNumber}) {
@@ -32,137 +30,43 @@ async function fetchCoAuthoredData() {
 							}
 						}
 					}
-					reviews(first: 100) {
-						nodes {
-							author {
-								login
-							}
-						}
-					}
-					comments(first: 100) {
-						nodes {
-							author {
-								login
-							}
-						}
-					}
 				}
 			}
 		}`
 	);
 
-	coAuthorData.userData = new Map();
-	coAuthorData.committers = new Set();
-
-	for (const commit of contributorData.repository.pullRequest.commits.nodes) {
-		const {email, user} = commit.commit.author;
-
-		coAuthorData.committers.add(user.login);
-
-		// If the commit had an email address attached, prefer that over the user email.
-		if (email) {
-			user.email = email;
-		}
-		coAuthorData.userData.set(user.login, user);
-	}
-
-	coAuthorData.reviewers = new Set();
-
-	for (const review of contributorData.repository.pullRequest.reviews.nodes) {
-		const {login} = review.author;
-
-		// Only store reviewers that aren't committers.
-		if (!coAuthorData.committers.has(login)) {
-			coAuthorData.reviewers.add(login);
-		}
-	}
-
-	coAuthorData.commenters = new Set();
-
-	for (const comment of contributorData.repository.pullRequest.comments.nodes) {
-		const {login} = comment.author;
-
-		// Only store commenters that aren't committers or reviewers.
-		if (!coAuthorData.committers.has(login) && !coAuthorData.reviewers.has(login)) {
-			coAuthorData.commenters.add(login);
-		}
-	}
-
-	// We already have user info for committers, we need to grab it for everyone else.
-	const userData = await api.v4(
-		'{' +
-			[...coAuthorData.reviewers, ...coAuthorData.commenters].map(user =>
-				escapeForGql(user) + `: user(login: "${user}") {databaseId, login, name, email}`
-			) +
-		'}'
-	);
-
-	for (const user of Object.values(userData)) {
-		coAuthorData.userData.set(user.login, user);
-	}
+	return userInfo.repository.pullRequest.commits.nodes.map(node => node.commit.author);
 }
 
-function addCoAuthoredBy(groups = ['committers']) {
-	if (coAuthorData.userData.size === 0) {
+function addCoAuthors() {
+	const field = select('#merge_message_field');
+	if (field.value.includes('Co-authored-by: ')) {
+		// Don't affect existing information
 		return;
 	}
 
-	const coAuthors = groups.map(group => {
-		// Skip an empty set of contributors.
-		if (coAuthorData[group].size === 0) {
-			return false;
-		}
+	const addendum = new Map();
+	for (const {email, user} of coAuthors) {
+		const commitEmail = email || `${user.databaseId}+${user.login}@users.noreply.github.com`;
+		addendum.set(user.login, `Co-authored-by: ${user.name} <${commitEmail}>`);
+	}
 
-		// Generate each Co-authored-by entry, and join them into a single string.
-		return [...coAuthorData[group]].map(username => {
-			const {name, databaseId, email} = coAuthorData.userData.get(username);
-			const commitEmail = email || `${databaseId}+${username}@users.noreply.github.com`;
-			return `Co-authored-by: ${name} <${commitEmail}>`;
-		}).join('\n');
-	}).filter(el => el).join('\n');
+	addendum.delete(getOP());
 
-	const messageEl = select('#merge_message_field');
-	const message = messageEl.value.replace(/^Co-authored-by:[\s\S]*/m, '').trim();
-	messageEl.value = message + '\n\n' + coAuthors;
+	field.value += '\n\n' + [...addendum.values()].join('\n');
 }
 
-function toggleAllContributors({target}) {
-	if (target.disabled) {
-		return;
-	}
+async function init() {
+	coAuthors = await fetchCoAuthoredData();
 
-	if (target.dataset.addAll === 'true') {
-		addCoAuthoredBy(['committers', 'reviewers', 'commenters']);
-		target.textContent = 'Remove Extra Co-Authors';
-		target.dataset.addAll = 'false';
-	} else {
-		addCoAuthoredBy(['committers']);
-		target.textContent = 'Add All Co-Authors';
-		target.dataset.addAll = 'true';
-	}
+	delegate('.discussion-timeline-actions', '.merge-message [type=submit]', 'click', addCoAuthors);
 }
 
-export default async function () {
-	const btn = select('.merge-message .btn-group-squash [type=submit]');
-	if (!btn) {
-		return;
-	}
-
-	await fetchCoAuthoredData();
-
-	btn.addEventListener('click', () => addCoAuthoredBy(['committers']));
-
-	const buttonGroup = select('.commit-form-actions .select-menu');
-
-	if (!buttonGroup) {
-		return;
-	}
-
-	// Insert our button before the Cancel button
-	buttonGroup.lastElementChild.before(
-		' ',
-		<button type="button" class="btn rgh-coauthor-button" onclick={toggleAllContributors} data-add-all={true}>
-			Add All Co-Authors
-		</button>
-	);
-}
+features.add({
+	id: 'add-co-authored-by',
+	include: [
+		features.isPRConversation
+	],
+	load: features.onAjaxedPages,
+	init
+});
--- source/features/op-labels.tsx
+++ source/features/op-labels.tsx
@@ -4,15 +4,18 @@ import features from '../libs/features';
 import {getUsername} from '../libs/utils';
 import {isPR, isPRFiles} from '../libs/page-detect';
 
-function init() {
-	let op;
+export function getOP(): string {
 	if (isPR()) {
-		const titleRegex = /^(?:.+) by (\S+) · Pull Request #(\d+)/;
-		[, op = ''] = titleRegex.exec(document.title) || [];
-	} else {
-		op = select('.timeline-comment-header-text .author').textContent;
+		const titleRegex = /^(.+) by (\S+) · Pull Request #(\d+)/;
+		const match = titleRegex.exec(document.title);
+		return match && match[2];
 	}
 
+	return select('.timeline-comment-header-text .author').textContent;
+}
+
+function init() {
+	const op = getOP();
 	let newComments = select.all('.js-comment:not(.refined-github-op)')
 		.filter(el => select(`strong .author[href="/${op}"]`, el));
 
