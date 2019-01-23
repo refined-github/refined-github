91ee11157340e95dceb57abb447edaaaa06ac4af
Federico Brigante <github@bfred.it>
2019-01-23 18:48:18 +0700

    Various fixes
    - Also apply to global search
    - Use native and lightweight count-getter
    - Correctly drop existing counters
    - Avoid innerHTML
    - Use URLSearchParams
    - Simplify and shorten code

--- source/features/search-results-filter.tsx
+++ source/features/search-results-filter.tsx
@@ -1,10 +1,9 @@
 import {React} from 'dom-chef/react';
 import select from 'select-dom';
 import features from '../libs/features';
-import domify from '../libs/domify';
 
-function cleanLinks(links) {
-	for (const link of links) {
+function cleanLinks() {
+	for (const link of select.all('.menu-item')) {
 		const searchParams = new URLSearchParams(link.search);
 		searchParams.set('q', cleanSearchQuery(searchParams.get('q')));
 		link.search = searchParams.toString();
@@ -12,84 +11,56 @@ function cleanLinks(links) {
 }
 
 function getSearchQuery() {
-	return new URL(location.href).searchParams.get('q');
+	return new URLSearchParams(location.search).get('q');
 }
 
-function cleanSearchQuery(query) {
-	return query.replace(/\s*\bis:(pr|issue)\b\s*/gi, '');
+function cleanSearchQuery(query, newType = '') {
+	return query
+		.replace(/\bis:(pr|issue)\b/gi, newType)
+		.replace(/\s{2,}/g, ' ');
 }
 
-function createUrl(type) {
-	const url = new URL(location.pathname, location.origin); // Create url without any search params.
-	url.searchParams.set('q', cleanSearchQuery(getSearchQuery()) + ` is:${type}`);
+function createUrl(type, pathname = location.pathname) {
+	const url = new URL(pathname, location.origin);
+	url.searchParams.set('q', cleanSearchQuery(getSearchQuery(), `is:${type}`));
 	url.searchParams.set('type', 'Issues');
 	return url.toString();
 }
 
-async function fetchCount(type) {
-	const response = await fetch(createUrl(type));
-	const text = await response.text();
-	const counter = domify(text).querySelector('.menu a.selected .Counter');
-	return counter ? parseInt(counter.textContent, 10) : 0; // When there are no results, the counter is absent.
-}
+function init() {
+	cleanLinks();
 
-function prIsActive() {
-	return getSearchQuery().split(' ').includes('is:pr');
-}
-
-async function init() {
-	const menu = select('nav.menu');
-
-	const issueLink = select('a[href*="&type=Issues"]', menu);
-	if (!issueLink) { // Stop when issues are not enabled for this repo.
-		return;
-	}
-
-	cleanLinks(select.all('a', menu).filter(item => item !== issueLink));
-
+	const issueLink = select('nav.menu a[href*="&type=Issues"]');
+	issueLink.textContent = 'Issues'; // Drops any possible counter
 	issueLink.href = createUrl('issue');
-
-	const prUrl = createUrl('pr');
-	const prHtml = (
-		<a class="menu-item" href={prUrl}>
-			Pull Requests
-			<span class="Counter ml-1 mt-1" data-search-type="PR" />
-		</a>
+	issueLink.append(
+		<include-fragment src={createUrl('issue', 'search/count')} />
 	);
-	issueLink.after(prHtml);
 
-	if (prIsActive()) {
-		select('.selected', menu).classList.remove('selected');
-		prHtml.classList.add('selected');
+	const prLink = issueLink.cloneNode(true);
+	prLink.textContent = 'Pull requests';
+	prLink.href = createUrl('pr');
+	prLink.append(
+		<include-fragment src={createUrl('pr', 'search/count')} />
+	);
 
-		const h3 = select('.codesearch-results h3');
-		h3.innerHTML = h3.innerHTML.replace(/\bissue(s)?\b/gi, 'pull request$1');
-	}
+	issueLink.after(prLink);
 
-	const [prCount, issueCount] = await Promise.all([fetchCount('pr'), fetchCount('issue')]);
-	const prCounter = select('.Counter', prHtml);
-	if (prCounter) { // When there are no results, the counter is absent.
-		if (prCount > 0) {
-			prCounter.textContent = prCount;
-		} else {
-			prCounter.remove();
-		}
-	}
+	// Update UI in PR searches
+	if (/\bis:pr\b/.test(getSearchQuery())) {
+		select('.menu-item.selected').classList.remove('selected');
+		prLink.classList.add('selected');
 
-	const issueCounter = select('.Counter', issueLink);
-	if (issueCounter) { // When there are no results, the counter is absent.
-		if (issueCount > 0) {
-			issueCounter.textContent = issueCount;
-		} else {
-			issueCounter.remove();
-		}
+		const title = select('.codesearch-results h3').firstChild;
+		title.textContent = title.textContent.replace('issue', 'pull request');
 	}
 }
 
 features.add({
 	id: 'search-results-filter',
 	include: [
-		features.isRepoSearch
+		features.isRepoSearch,
+		features.isGlobalSearchResults
 	],
 	load: features.onAjaxedPages,
 	init
