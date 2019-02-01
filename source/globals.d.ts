7fb3eb6c9d5139d9fb7f12266c220747104b1746
Jose Toledo <jotatoledon@gmail.com>
2019-02-01 13:32:17 +0100

    refactor: tweak local types
    * Tweak definition of select-dom.select in order
    to return an Element instance

--- source/globals.d.ts
+++ source/globals.d.ts
@@ -1,14 +1,24 @@
 declare const browser: AnyObject;
 
-type AnyObject = {[key: string]: any};
+type AnyObject = { [key: string]: any };
 
 declare module 'select-dom' {
 	import 'select-dom';
 
-	function select(selector: string, parent?: any): any | null;
+	function select(selector: string, parent?: any): Element | null;
 	namespace select {
 		export function exists(selector: string, parent?: Element): boolean;
-		export function all(selector: string, parent?: Element | Element[] | NodeList | DocumentFragment): any[];
+		export function all(
+			selector: string,
+			parent?: Element | Element[] | NodeList | DocumentFragment
+		): Element[];
 	}
 	export = select;
+}
+
+declare module 'webext-options-sync' {
+	export default class OptionsSync {
+		constructor(storageName?:string){}
+		getAll<TValue = Object>(): Promise<TValue>
+	}
 }
