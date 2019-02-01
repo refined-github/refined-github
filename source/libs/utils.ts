3dc8f25dfb73f899e48c199b7be905a8be3e2fc3
Jose Toledo <jotatoledon@gmail.com>
2019-02-01 11:59:36 +0100

    refactor: rework types in utils

--- source/libs/utils.ts
+++ source/libs/utils.ts
@@ -2,52 +2,39 @@ import select from 'select-dom';
 import onetime from 'onetime';
 import {isRepo, isPR, isIssue} from './page-detect';
 
-export const getUsername = onetime(() =>
-	select('meta[name="user-login"]').getAttribute('content')
-);
+export const getUsername = onetime(() => select('meta[name="user-login"]').getAttribute('content'));
 
-export const getDiscussionNumber = () =>
-	(isPR() || isIssue()) && getCleanPathname().split('/')[3];
+export const getDiscussionNumber = () => (isPR() || isIssue()) && getCleanPathname().split('/')[3];
 
 // Drops leading and trailing slash to avoid /\/?/ everywhere
-export const getCleanPathname = () =>
-	location.pathname.replace(/^[/]|[/]$/g, '');
+export const getCleanPathname = () => location.pathname.replace(/^[/]|[/]$/g, '');
 
 // Parses a repo's subpage, e.g.
 // '/user/repo/issues/' -> 'issues'
 // '/user/repo/' -> ''
-// returns false if the path is not a repo
+// returns undefined if the path is not a repo
 export const getRepoPath = () => {
 	if (isRepo()) {
-		return getCleanPathname()
-			.split('/')
-			.slice(2)
-			.join('/');
+		return getCleanPathname().split('/').slice(2).join('/');
 	}
 
-	return false;
+	return undefined;
 };
 
 export const getRepoBranch = () => {
-	const [type, branch] = getCleanPathname()
-		.split('/')
-		.slice(2);
+	const [type, branch] = getCleanPathname().split('/').slice(2);
 	if (isRepo() && type === 'tree') {
 		return branch;
 	}
 
-	return false;
+	return undefined;
 };
 
-export const getRepoURL = () =>
-	location.pathname
-		.slice(1)
-		.split('/', 2)
-		.join('/');
+export const getRepoURL = () => location.pathname.slice(1).split('/', 2).join('/');
 
 export const getOwnerAndRepo = () => {
 	const [, ownerName, repoName] = location.pathname.split('/', 3);
-	return { ownerName, repoName };
+	return {ownerName, repoName};
 };
 
 export const groupBy = <TValue>(iterable: Iterable<TValue>, grouper: (item: TValue) => string | number): {[TKey in string | number]: TValue[]} => {
@@ -64,7 +51,7 @@ export const groupBy = <TValue>(iterable: Iterable<TValue>, grouper: (item: TVal
 // Concats arrays but does so like a zipper instead of appending them
 // [[0, 1, 2], [0, 1]] => [0, 0, 1, 1, 2]
 // Like lodash.zip
-export const flatZip = (table, limit = Infinity) => {
+export const flatZip = <TValue>(table: TValue[][], limit = Infinity): TValue[] => {
 	const maxColumns = Math.max(...table.map(row => row.length));
 	const zipped = [];
 	for (let col = 0; col < maxColumns; col++) {
