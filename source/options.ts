60bbb89ae167076b4fd00123b78df6599bb53418
Nick Taylor <nick@iamdeveloper.com>
2019-02-04 01:19:59 -0500

    Reverted change from code review as it doesn't compile.

--- source/options.ts
+++ source/options.ts
@@ -8,7 +8,7 @@ textarea('textarea', {
 
 document.querySelector('[name="customCSS"]').addEventListener('keydown', (event: KeyboardEvent) => {
 	if (event.key === 'Tab' && !event.shiftKey) {
-		indentTextarea(event.target as HTMLTextAreaElement);
+		indentTextarea(event.target as HTMLInputElement);
 		event.preventDefault();
 	}
 });
