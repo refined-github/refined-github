6138334acebd3e6eb42b278f9f7dee2f34e7f580
Nick Taylor <nick@iamdeveloper.com>
2019-02-04 01:28:23 -0500

    getRepoPath explicitly cast to string to avoid strict issues.

--- source/libs/page-detect.ts
+++ source/libs/page-detect.ts
@@ -8,13 +8,13 @@ export const is404 = (): boolean => document.title === 'Page not found · GitHub
 
 export const is500 = (): boolean => document.title === 'Server Error · GitHub';
 
-export const isBlame = (): boolean => /^blame\//.test(getRepoPath());
+export const isBlame = (): boolean => /^blame\//.test(getRepoPath() as string);
 
 export const isCommit = (): boolean => isSingleCommit() || isPRCommit();
 
-export const isCommitList = (): boolean => /^commits\//.test(getRepoPath());
+export const isCommitList = (): boolean => /^commits\//.test(getRepoPath() as string);
 
-export const isCompare = (): boolean => /^compare/.test(getRepoPath());
+export const isCompare = (): boolean => /^compare/.test(getRepoPath() as string);
 
 export const isDashboard = (): boolean => /^$|^(orgs[/][^/]+[/])?dashboard([/]|$)/.test(getCleanPathname());
 
@@ -31,39 +31,39 @@ export const isGlobalPRSearch = (): boolean => location.pathname === '/pulls';
 
 export const isGlobalSearchResults = (): boolean => location.pathname === '/search' && new URLSearchParams(location.search).get('q') !== null;
 
-export const isIssue = (): boolean => /^issues\/\d+/.test(getRepoPath());
+export const isIssue = (): boolean => /^issues\/\d+/.test(getRepoPath() as string);
 
-export const isIssueList = (): boolean => /^(issues$|pulls$|labels\/)/.test(getRepoPath());
+export const isIssueList = (): boolean => /^(issues$|pulls$|labels\/)/.test(getRepoPath() as string);
 
-export const isLabel = (): boolean => /^labels\/\w+/.test(getRepoPath());
+export const isLabel = (): boolean => /^labels\/\w+/.test(getRepoPath() as string);
 
-export const isLabelList = (): boolean => /^labels\/?(((?=\?).*)|$)/.test(getRepoPath());
+export const isLabelList = (): boolean => /^labels\/?(((?=\?).*)|$)/.test(getRepoPath() as string);
 
-export const isMilestone = (): boolean => /^milestone\/\d+/.test(getRepoPath());
+export const isMilestone = (): boolean => /^milestone\/\d+/.test(getRepoPath() as string);
 
-export const isMilestoneList = (): boolean => getRepoPath() === 'milestones';
+export const isMilestoneList = (): boolean => getRepoPath() as string === 'milestones';
 
-export const isNewIssue = (): boolean => /^issues\/new/.test(getRepoPath());
+export const isNewIssue = (): boolean => /^issues\/new/.test(getRepoPath() as string);
 
 export const isNotifications = (): boolean => /^([^/]+[/][^/]+\/)?notifications/.test(getCleanPathname());
 
 export const isOwnUserProfile = (): boolean => isUserProfile() && getCleanPathname() === getUsername();
 
-export const isProject = (): boolean => /^projects\/\d+/.test(getRepoPath());
+export const isProject = (): boolean => /^projects\/\d+/.test(getRepoPath() as string);
 
-export const isPR = (): boolean => /^pull\/\d+/.test(getRepoPath());
+export const isPR = (): boolean => /^pull\/\d+/.test(getRepoPath() as string);
 
-export const isPRList = (): boolean => getRepoPath() === 'pulls';
+export const isPRList = (): boolean => getRepoPath() as string === 'pulls';
 
-export const isPRCommit = (): boolean => /^pull\/\d+\/commits\/[0-9a-f]{5,40}/.test(getRepoPath());
+export const isPRCommit = (): boolean => /^pull\/\d+\/commits\/[0-9a-f]{5,40}/.test(getRepoPath() as string);
 
-export const isPRConversation = (): boolean => /^pull\/\d+$/.test(getRepoPath());
+export const isPRConversation = (): boolean => /^pull\/\d+$/.test(getRepoPath() as string);
 
-export const isPRFiles = (): boolean => /^pull\/\d+\/files/.test(getRepoPath());
+export const isPRFiles = (): boolean => /^pull\/\d+\/files/.test(getRepoPath() as string);
 
 export const isQuickPR = (): boolean => isCompare() && /[?&]quick_pull=1(&|$)/.test(location.search);
 
-export const isReleasesOrTags = (): boolean => /^(releases|tags)/.test(getRepoPath());
+export const isReleasesOrTags = (): boolean => /^(releases|tags)/.test(getRepoPath() as string);
 
 export const isRepo = (): boolean => /^[^/]+\/[^/]+/.test(getCleanPathname()) &&
 	!isReserved(getOwnerAndRepo().ownerName) &&
@@ -72,17 +72,17 @@ export const isRepo = (): boolean => /^[^/]+\/[^/]+/.test(getCleanPathname()) &&
 	!isGist() &&
 	!isRepoSearch();
 
-export const isRepoRoot = (): boolean => /^(tree[/][^/]+)?$/.test(getRepoPath());
+export const isRepoRoot = (): boolean => /^(tree[/][^/]+)?$/.test(getRepoPath() as string);
 
 export const isRepoSearch = (): boolean => location.pathname.slice(1).split('/')[2] === 'search';
 
-export const isRepoSettings = (): boolean => /^settings/.test(getRepoPath());
+export const isRepoSettings = (): boolean => /^settings/.test(getRepoPath() as string);
 
-export const isRepoTree = (): boolean => isRepoRoot() || /^tree\//.test(getRepoPath());
+export const isRepoTree = (): boolean => isRepoRoot() || /^tree\//.test(getRepoPath() as string);
 
-export const isSingleCommit = (): boolean => /^commit\/[0-9a-f]{5,40}/.test(getRepoPath());
+export const isSingleCommit = (): boolean => /^commit\/[0-9a-f]{5,40}/.test(getRepoPath() as string);
 
-export const isSingleFile = (): boolean => /^blob\//.test(getRepoPath());
+export const isSingleFile = (): boolean => /^blob\//.test(getRepoPath() as string);
 
 export const isTrending = (): boolean => location.pathname === '/trending' || location.pathname.startsWith('/trending/');
 
