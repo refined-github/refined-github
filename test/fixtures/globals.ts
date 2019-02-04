fb6b1b1f11f2a12e22474fa6ab4242e7f2bd38b3
Nick Taylor <nick@iamdeveloper.com>
2019-02-03 22:57:53 -0500

    Fixed a lint issue.

--- test/fixtures/globals.ts
+++ test/fixtures/globals.ts
@@ -4,13 +4,15 @@ const {window} = new JSDOM('...');
 
 // Extending global to recognize the new properties we're adding on.
 declare global {
-  namespace NodeJS {
-    interface Global {
+	// Disabling this rule here, because this has to match exactly like in lib.dom.d.ts for merge declaration to happen.
+	// eslint-disable-next-line @typescript-eslint/no-namespace
+	namespace NodeJS {
+		interface Global {
 			navigator: typeof window.navigator;
 			document: typeof window.document;
 			location: URL;
-    }
-  }
+		}
+	}
 }
 
 global.navigator = window.navigator;
