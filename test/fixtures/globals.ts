347a46b056ca0b1a75ff57e5b63fa3b3fbe7bc50
Nick Taylor <nick@iamdeveloper.com>
2019-02-03 22:52:31 -0500

    Now the global object recognnizes our exxtra properties for tests.

--- test/fixtures/globals.ts
+++ test/fixtures/globals.ts
@@ -1,6 +1,18 @@
 import {JSDOM} from 'jsdom';
 
 const {window} = new JSDOM('...');
+
+// Extending global to recognize the new properties we're adding on.
+declare global {
+  namespace NodeJS {
+    interface Global {
+			navigator: typeof window.navigator;
+			document: typeof window.document;
+			location: URL;
+    }
+  }
+}
+
 global.navigator = window.navigator;
 global.document = window.document;
 global.location = new URL('https://github.com');
