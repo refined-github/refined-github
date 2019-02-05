3b8343299a3903ccadcd2f76f42924cbbb7dcd29
Nick Taylor <nick@iamdeveloper.com>
2019-02-04 20:42:10 -0500

    Removed types that are in their own packages now.

--- source/globals.d.ts
+++ source/globals.d.ts
@@ -13,35 +13,3 @@ declare module 'select-dom' {
 	}
 	export = select;
 }
-
-declare module 'webext-options-sync' {
-	interface DefineOptions {
-		defaults: {
-			disabledFeatures: string;
-			customCSS: string;
-			personalToken: string;
-			logging: boolean;
-		};
-		migrations: [(options: {disabledFeatures: string}) => void, () => void];
-	}
-
-	export default class OptionsSync {
-		static migrations: {
-			removeUnused: () => void;
-		}
-
-		getAll: <T>() => T;
-
-		syncForm: (selector: string) => void;
-
-		define: (options: DefineOptions) => void;
-	}
-}
-
-declare module 'webext-domain-permission-toggle' {
-	export function addContextMenu(): void;
-}
-
-declare module 'webext-dynamic-content-scripts' {
-	export function addToFutureTabs(): void;
-}
