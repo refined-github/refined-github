b8cd4b87496c902680d775c251ace9ac041300af
Nick Taylor <nick@iamdeveloper.com>
2019-02-05 18:40:59 -0500

    Installed latest select-dom.

--- source/globals.d.ts
+++ source/globals.d.ts
@@ -1,13 +1,2 @@
 // TODO: Type anything that is of type AnyObject
 type AnyObject = Record<string, any>;
-
-declare module 'select-dom' {
-	import 'select-dom';
-
-	function select<T extends HTMLElement = HTMLElement>(selector: string, parent?: string|Element|DocumentFragment): T | null;
-	namespace select {
-		export function exists(selector: string, parent?: Element): boolean;
-		export function all<T extends HTMLElement = HTMLElement>(selector: string, parent?: Element | Element[] | NodeList | DocumentFragment): T[];
-	}
-	export = select;
-}
