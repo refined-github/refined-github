2be76d8cd4cb9bb36f6be542eb1395d043e45ebf
Federico Brigante <github@bfred.it>
2019-02-09 20:28:18 +0800

    Shorten copy; copy GitHub style

--- source/features/warn-pr-from-master.tsx
+++ source/features/warn-pr-from-master.tsx
@@ -1,5 +1,5 @@
 /*
-Creating a PR from master branch is an anti-pattern. This feature produces a
+Creating a PR from the master branch is an anti-pattern. This feature produces a
 warning when a user attempts to create a PR from their fork's default branch.
 
 See:
@@ -15,18 +15,12 @@ import getDefaultBranch from '../libs/get-default-branch';
 async function init() {
 	const defaultBranch = await getDefaultBranch();
 	if (!location.pathname.endsWith(`${getUsername()}:${defaultBranch}`)) {
-		return;
+		return false;
 	}
 
 	select('.compare-pr').append(
-		<div class="warning">
-			<h3>DO NOT CREATE PULL REQUESTS FROM THE DEFAULT BRANCH</h3>
-			<p>
-				Pull requests (PRs) should not be created from the default
-				branch (e.g. the master branch). A separate PR-specific branch
-				should be created with your changes. Doing so will help prevent
-				unintentional changes from being included in your PR.
-			</p>
+		<div class="flash flash-error my-3">
+			<strong>Note:</strong> Creating a PR from the <code>master</code> branch is an [anti-pattern](https://blog.jasonmeridth.com/posts/do-not-issue-pull-requests-from-your-master-branch/).
 		</div>
 	);
 }
