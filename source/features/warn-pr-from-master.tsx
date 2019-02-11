6d75db7421fc8767cefd44d3f489600f2cd20fce
Federico Brigante <github@bfred.it>
2019-02-11 10:07:32 +0800

    Say "default branch" instead of "master branch"

--- source/features/warn-pr-from-master.tsx
+++ source/features/warn-pr-from-master.tsx
@@ -20,7 +20,7 @@ async function init() {
 
 	select('.compare-pr').append(
 		<div class="flash flash-error my-3">
-			<strong>Note:</strong> Creating a PR from the <code>master</code> branch is an <a href="https://blog.jasonmeridth.com/posts/do-not-issue-pull-requests-from-your-master-branch/" target="_blank">anti-pattern</a>.
+			<strong>Note:</strong> Creating a PR from the the default branch is an <a href="https://blog.jasonmeridth.com/posts/do-not-issue-pull-requests-from-your-master-branch/" target="_blank">anti-pattern</a>.
 		</div>
 	);
 }
