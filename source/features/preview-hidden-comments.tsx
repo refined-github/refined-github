db428d1089c926c7f59f08b37f63929553efd8ec
Federico Brigante <github@bfred.it>
2019-02-11 11:26:46 +0800

    Readability

--- source/features/preview-hidden-comments.tsx
+++ source/features/preview-hidden-comments.tsx
@@ -8,13 +8,15 @@ const init = () => {
 	for (const details of select.all('.minimized-comment:not(.d-none) > details:not(.rgh-preview-hidden-comments)')) {
 		details.classList.add('rgh-preview-hidden-comments');
 
-		const content = select('.review-comment-contents .comment-body', details);
-		const commentText = content.textContent.trim();
-		if (!commentText) {
+		const commentText = select('.comment-body', details).textContent.trim();
+		if (commentText.length === 0) {
 			continue;
 		}
 
-		const header = select('summary .timeline-comment-header-text, summary .discussion-item-copy', details);
+		const header = select(`
+			summary .timeline-comment-header-text,
+			summary .discussion-item-copy
+		`, details);
 
 		const headerLabel = header.textContent;
 		const [, reason]: string[] = headerLabel.trim().match(/was marked as ([^.]+)/);
