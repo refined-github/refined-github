046ab294c3627d35ab5e1c3e03cae7c21e809cc2
Federico Brigante <github@bfred.it>
2019-02-12 15:42:29 +0800

    Undo change on dropped feature

--- source/features/milestone-navigation.tsx
+++ source/features/milestone-navigation.tsx
@@ -1,4 +1,4 @@
-import React from 'dom-chef';
+import {React} from 'dom-chef/react';
 import select from 'select-dom';
 import features from '../libs/features';
 import {getRepoURL} from '../libs/utils';
