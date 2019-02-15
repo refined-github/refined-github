a3db811bbf0ba7ffb6cde4b596fe5ac5dd2c62da
Connor Brathwaite <connor.brathwaite@gmail.com>
2019-02-15 02:35:31 -0500

    (refactor): merge features

--- source/content.ts
+++ source/content.ts
@@ -76,7 +76,6 @@ import './features/extend-diff-expander';
 import './features/profile-gists-link';
 import './features/show-followers-you-know';
 import './features/show-user-top-repositories';
-import './features/show-user-updated-repositories';
 import './features/set-default-repositories-type-to-sources';
 import './features/user-profile-follower-badge';
 import './features/pr-branches';
--- source/features/show-user-top-repositories.tsx
+++ source/features/show-user-top-repositories.tsx
@@ -1,5 +1,5 @@
 /*
-Find a user’s most starred repositories in their profile.
+Find a user’s top repositories in their profile.
 https://user-images.githubusercontent.com/1402241/48474026-43e3ae80-e82c-11e8-93de-159ad4c6f283.png
 */
 
@@ -8,14 +8,24 @@ import select from 'select-dom';
 import features from '../libs/features';
 import {getCleanPathname} from '../libs/utils';
 
+const divider = ' / ';
+
+function buildUrl(queryField) {
+	return `/search?o=desc&q=user%3A${getCleanPathname()}&s=${queryField}&type=Repositories`;
+}
+
 function init() {
 	const showcaseTitle = select('.js-pinned-repos-reorder-container .text-normal');
 	if (!showcaseTitle) {
 		return false;
 	}
 
-	const url = `/search?o=desc&q=user%3A${getCleanPathname()}&s=stars&type=Repositories`;
-	showcaseTitle.firstChild.after(' / ', <a href={url}>Top repositories</a>);
+	showcaseTitle.firstChild.after(
+		divider,
+		<a href={buildUrl('stars')}>Top repositories</a>,
+		divider,
+		<a href={buildUrl('updated')}>Updated repositories</a>
+	);
 }
 
 features.add({
