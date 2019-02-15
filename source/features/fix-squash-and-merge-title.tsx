05dfe39703c2e98978de9103a06a5b5c3866e87e
Federico Brigante <github@bfred.it>
2019-02-16 00:35:12 +0800

    Re-apply

--- source/features/fix-squash-and-merge-title.tsx
+++ source/features/fix-squash-and-merge-title.tsx
@@ -1,5 +1,12 @@
 import select from 'select-dom';
 import features from '../libs/features';
+import observeEl from '../libs/simplified-element-observer';
+
+function updateSquashAndMergeTitle() {
+	const title = select('.js-issue-title').textContent;
+	const number = select('.gh-header-number').textContent;
+	select<HTMLTextAreaElement>('#merge_title_field').value = `${title.trim()} (${number})`;
+}
 
 function init() {
 	const btn = select('.merge-message .btn-group-squash [type=button]');
@@ -7,11 +14,8 @@ function init() {
 		return false;
 	}
 
-	btn.addEventListener('click', () => {
-		const title = select('.js-issue-title').textContent;
-		const number = select('.gh-header-number').textContent;
-		select<HTMLTextAreaElement>('#merge_title_field').value = `${title.trim()} (${number})`;
-	});
+	btn.addEventListener('click', updateSquashAndMergeTitle);
+	observeEl('.js-issue-title', updateSquashAndMergeTitle);
 }
 
 features.add({
