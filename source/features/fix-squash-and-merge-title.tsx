c9c165486feac06ae49971f9c3ba87c6c53cb55d
Federico Brigante <github@bfred.it>
2019-02-16 11:42:26 +0800

    Revert "Update squash-and-merge title when PR title changes (#1651)"
    This reverts commit 8f20ceeba0c61ca5c2d9ab3cff0d7f1d469efd7a.
    
    Fixes #1795

--- source/features/fix-squash-and-merge-title.tsx
+++ source/features/fix-squash-and-merge-title.tsx
@@ -1,12 +1,5 @@
 import select from 'select-dom';
 import features from '../libs/features';
-import observeEl from '../libs/simplified-element-observer';
-
-function updateSquashAndMergeTitle() {
-	const title = select('.js-issue-title').textContent;
-	const number = select('.gh-header-number').textContent;
-	select<HTMLTextAreaElement>('#merge_title_field').value = `${title.trim()} (${number})`;
-}
 
 function init() {
 	const btn = select('.merge-message .btn-group-squash [type=button]');
@@ -14,8 +7,11 @@ function init() {
 		return false;
 	}
 
-	btn.addEventListener('click', updateSquashAndMergeTitle);
-	observeEl('.js-issue-title', updateSquashAndMergeTitle);
+	btn.addEventListener('click', () => {
+		const title = select('.js-issue-title').textContent;
+		const number = select('.gh-header-number').textContent;
+		select<HTMLTextAreaElement>('#merge_title_field').value = `${title.trim()} (${number})`;
+	});
 }
 
 features.add({
