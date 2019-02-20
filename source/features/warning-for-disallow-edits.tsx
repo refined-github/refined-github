43f915028ec6dc579633d2ee7b554d9574218f43
Celestine Ekoh-Ordan <CEO.ehis@outlook.com>
2019-02-20 23:05:36 +0100

    show warning for disallow edits on conversation sidebar

--- source/features/warning-for-disallow-edits.tsx
+++ source/features/warning-for-disallow-edits.tsx
@@ -3,8 +3,9 @@ import select from 'select-dom';
 import features from '../libs/features';
 
 function init() {
-	const checkbox = select<HTMLInputElement>('.js-collab-option');
-	const container = select('.new-discussion-timeline .composer .timeline-comment');
+	const checkbox = select<HTMLInputElement>('input[type="checkbox"][class^="js-collab"]');
+	const container = select('.new-discussion-timeline .composer .timeline-comment')
+		|| select('.discussion-sidebar>.text-small');
 	const warning = (
 		<div class="flash flash-error" id="allow-edits-unchecked-warning">
 			<strong>Note:</strong> Maintainers may require changes. It’s easier and faster to allow them to make direct changes before merging.
@@ -23,7 +24,8 @@ function init() {
 features.add({
 	id: 'allow-edits-unchecked-warning',
 	include: [
-		features.isCompare
+		features.isCompare,
+		features.isPRConversation
 	],
 	load: features.onAjaxedPages,
 	init
