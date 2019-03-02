e065915ef07a0cab4994571097ccd839ea882f86
Federico Brigante <github@bfred.it>
2019-03-02 13:53:20 +0800

    Smartly wrap content in \n

--- source/features/collapsible-content-button.tsx
+++ source/features/collapsible-content-button.tsx
@@ -9,6 +9,26 @@ const addEvents = onetime(() => {
 	delegate('.rgh-collapsible-content-btn', 'click', addContentToDetails);
 });
 
+// Wraps string in at least 2 \n on each side,
+// as long as the field doesn't already have them.
+// Code adopted/adapted from GitHub.
+function smartBlockWrap(content: string, field: HTMLTextAreaElement) {
+	const before = field.value.slice(0, field.selectionStart);
+	const after = field.value.slice(field.selectionEnd);
+	const [whitespaceAtStart] = before.match(/\n*$/) || [''];
+	const [whitespaceAtEnd] = after.match(/^\n*/) || [''];
+	let newlinesToAppend = '';
+	let newlinesToPrepend = '';
+	if (before.match(/\S/) && whitespaceAtStart.length < 2) {
+		newlinesToPrepend = '\n'.repeat(2 - whitespaceAtStart.length);
+	}
+	if (after.match(/\S/) && whitespaceAtEnd.length < 2) {
+		newlinesToAppend = '\n'.repeat(2 - whitespaceAtEnd.length)
+	}
+
+	return newlinesToPrepend + content + newlinesToAppend;
+}
+
 function init() {
 	addEvents();
 	for (const anchor of select.all('md-ref')) {
@@ -21,6 +41,8 @@ function init() {
 }
 
 function addContentToDetails(event) {
+	const field = event.delegateTarget.form['comment[body]'];
+
 	// Don't indent <summary> because indentation will not be automatic on multi-line content
 	const newContent = `
 		<details>
@@ -32,11 +54,10 @@ function addContentToDetails(event) {
 	`.replace(/(\n|\b)\t+/g, '$1').trim();
 
 	// Inject new tags; it'll be undoable
-	document.execCommand('insertText', false, '\n\n' + newContent + '\n\n');
+	document.execCommand('insertText', false, smartBlockWrap(newContent, field));
 
 	// Restore selection.
 	// `selectionStart` will be right after the newly-inserted text
-	const field = event.currentTarget.form['comment[body]'];
 	field.setSelectionRange(
 		field.value.lastIndexOf('</summary>', field.selectionStart) + '</summary>'.length + 1,
 		field.value.lastIndexOf('</details>', field.selectionStart) - 1
