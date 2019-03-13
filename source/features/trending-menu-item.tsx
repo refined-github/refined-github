98b2a41febc3e3308aedb2d2e8d5f5aa22af18fe
Federico Brigante <github@bfred.it>
2019-03-13 17:46:44 +0800

    Update trending-menu-item.tsx

--- source/features/trending-menu-item.tsx
+++ source/features/trending-menu-item.tsx
@@ -5,20 +5,20 @@ import {safeElementReady} from '../libs/dom-utils';
 
 async function init() {
 	const selectedClass = isTrending() ? 'selected' : '';
-	const issuesLink = await safeElementReady('.HeaderNavlink[href="/issues"], .header-nav-link[href="/issues"]');
+	const issuesLink = await safeElementReady('.HeaderNavlink[href="/issues"]');
 	if (!issuesLink) {
 		return false;
 	}
 
 	issuesLink.parentNode.after(
-		<li class="header-nav-item">
-			<a href="/trending" class={`js-selected-navigation-item HeaderNavlink px-lg-2 py-2 py-lg-0 ${selectedClass}`} data-hotkey="g t">Trending</a>
+		<li>
+			<a href="/trending" class={`js-selected-navigation-item HeaderNavlink px-2 ${selectedClass}`} data-hotkey="g t">Trending</a>
 		</li>
 	);
 
 	// Explore link highlights /trending urls by default, remove that behavior
 	if (isTrending()) {
-		const exploreLink = await safeElementReady('a[href="/explore"]');
+		const exploreLink = await safeElementReady('.HeaderNavlink[href="/explore"]');
 		if (exploreLink) {
 			exploreLink.classList.remove('selected');
 		}
