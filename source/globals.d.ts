e054976d96a06d4405dac00863a9c369ffd4238e
Federico Brigante <github@bfred.it>
2019-03-14 17:16:21 +0800

    Load delegate-it@next and drop local types

--- source/globals.d.ts
+++ source/globals.d.ts
@@ -9,74 +9,9 @@ declare module 'github-reserved-names'; // TODO: PR types to that repo or DT
 
 // TODO: PR these types to the delegate repository.
 
-declare module 'delegate' {
+declare module 'delegate-it' {
 	// Custom event types used in RGH
 	type EventType = keyof GlobalEventHandlersEventMap | 'details:toggled';
-}
-
-// TODO: PR this into the delegate repo.
-declare module 'delegate' {
-	export type EventType = keyof GlobalEventHandlersEventMap;
-
-	export type DelegateEvent<T extends Event = Event> = T & {
-		delegateTarget: EventTarget;
-	}
-
-	export type DelegateEventHandler<T extends Event> = ((event: DelegateEvent<T>) => Promise<void>)| ((event: DelegateEvent<T>) => void);
-	export type DelegateSubscription = {
-		destroy: VoidFunction;
-	}
-
-	/**
-	 * Add event delegation with the default base (document)
-	 *
-	 * @param {string} selector A CSS selector
-	 * @param {EventType} eventType An event type, e.g. 'click'
-	 * @param {DelegateEventHandler<TEvent>} eventHandler An handler
-	 * @param {boolean} [useCapture=false] whether ot not to use capture
-	 *
-	 * @returns {DelegateSubscription} a delegate subscription
-	 */
-	export default function delegate<TEvent extends Event = Event>(selector: string, eventType: EventType, eventHandler: DelegateEventHandler<TEvent>, useCapture = false): DelegateSubscription;
-
-	/**
-	 * Add event delegation with an element as base
-	 *
-	 * @param {TElement} element A DOM element
-	 * @param {string} selector A CSS selector
-	 * @param {EventType} eventType An event type, e.g. 'click'
-	 * @param {DelegateEventHandler<TEvent>} eventHandler An event handler
-	 * @param {boolean} [useCapture=false] whether ot not to use capture
-	 *
-	 * @returns {DelegateSubscription} a delegate subscription
-	 */
-	export default function delegate<TEvent extends Event = Event, TElement extends HTMLElement = HTMLElement>(element: TElement, selector: string, eventType: EventType, eventHandler: DelegateEventHandler<TEvent>, useCapture = false): DelegateSubscription;
-
-	/**
-	 * Add event delegation with a selector (of existing elements) as base
-	 *
-	 * @param {string} containerSelector A CSS selector
-	 * @param {string} selector A CSS selector
-	 * @param {EventType} eventType An event type, e.g. 'click'
-	 * @param {DelegateEventHandler<TEvent>} eventHandler An event handler
-	 * @param {boolean} [useCapture=false] whether ot not to use capture
-	 *
-	 * @returns {DelegateSubscription} a delegate subscription
-	 */
-	export default function delegate<TEvent extends Event = Event>(containerSelector: string, selector: string, eventType: EventType, eventHandler: DelegateEventHandler<TEvent>, useCapture = false): DelegateSubscription;
-
-	/**
-	 * Add event delegation with an array/array-like of elements as base
-	 *
-	 * @param {IterableIterator<TElements>} elements A nodelist of DOM elements
-	 * @param {string} selector A CSS selector
-	 * @param {EventType} eventType An event type, e.g. 'click'
-	 * @param {DelegateEventHandler<TEvent>} eventHandler An event handler
-	 * @param {boolean} [useCapture=false] whether ot not to use capture
-	 *
-	 * @returns {DelegateSubscription} a delegate subscription
-	 */
-	export default function delegate<TEvent extends Event = Event, TElements extends HTMLElement = HTMLElement>(elements: IterableIterator<TElements>, selector: string, eventType: EventType, eventHandler: DelegateEventHandler<TEvent>, useCapture = false): DelegateSubscription;
 }
 
 // TODO: Add to dom-chef types
