76cc04d4b69974ce4aa8c02ed2afedb66ad66cd8
Federico Brigante <github@bfred.it>
2019-03-19 01:18:55 +0800

    Drop feature `make-headers-sticky`
    It was implemented by GitHub.
    
    Related: https://github.com/sindresorhus/refined-github/pull/647

--- source/content.ts
+++ source/content.ts
@@ -35,7 +35,6 @@ import './features/linkify-branch-refs';
 import './features/batch-open-issues';
 import './features/hide-useless-comments';
 import './features/navigate-pages-with-arrow-keys';
-import './features/make-headers-sticky';
 import './features/yours-menu-item';
 import './features/commented-menu-item';
 import './features/sort-issues-by-update-time'; // Must be after add-yours-menu-item + add-commented-menu-item
