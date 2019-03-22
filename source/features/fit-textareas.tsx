31f8c2f98f3cec2dd2e0bc2fb0afdeaf87893d49
Federico Brigante <github@bfred.it>
2019-03-22 14:51:06 +0800

    Only listen dynamically
    GH sometimes restores content to textareas without triggering an `input` event, the result is that `fit-textarea` won't actually resize the textarea, so it's best to just leave it on `focusin`

--- source/features/fit-textareas.tsx
+++ source/features/fit-textareas.tsx
@@ -3,12 +3,10 @@
 */
 
 import delegate from 'delegate-it';
-import features from '../libs/features';
 import fitTextarea from 'fit-textarea';
-import select from 'select-dom';
+import features from '../libs/features';
 
-function enableTextarea(textarea) {
-	console.log('enabling');
+function listener({ delegateTarget: textarea }) {
 	// `fit-textarea` adds only once listener
 	fitTextarea.watch(textarea);
 
@@ -16,21 +14,11 @@ function enableTextarea(textarea) {
 	textarea.classList.replace('js-size-to-fit', 'rgh-fit-textareas');
 }
 
-function listener({ delegateTarget: textarea }) {
-	enableTextarea(textarea);
-	console.log('listener')
-}
-
 function init(): void {
-	// Enable on existing textareas
-	select.all('textarea').forEach(enableTextarea);
-
-	// And on any dynamic ones
 	delegate('textarea', 'focusin', listener);
 }
 
 features.add({
 	id: 'fit-textareas',
-	load: features.onAjaxedPages,
 	init
 });
