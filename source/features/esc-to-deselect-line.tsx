7f0f65b95b14117295c4a44781d927dd940b55e3
Federico Brigante <github@bfred.it>
2019-03-23 03:29:03 +0800

    Lint and extract listener

--- source/features/esc-to-deselect-line.tsx
+++ source/features/esc-to-deselect-line.tsx
@@ -4,18 +4,19 @@ If you select a line in a file, pressing ESC will deselect it.
 
 import features from '../libs/features';
 
+function listener(event: KeyboardEvent) {
+	if (
+		event.key === 'Escape' && // Catch `Esc` key
+		location.hash.startsWith('#L') && // If a line is selected
+		!(event.target as Element).closest('textarea, input') // If a field isn’t focused
+	) {
+		location.hash = '#no-line'; // Update UI, without `scroll-to-top` behavior
+		history.replaceState({}, document.title, location.pathname); // Drop remaining # from url
+	}
+}
+
 function init() {
-	console.log('event')
-	document.body.addEventListener('keyup', event => {
-		if (
-			event.key === 'Escape' && // Catch `Esc` key
-			location.hash.startsWith('#L') && // If a line is selected
-			!(event.target as Element).closest('textarea, input') // If a field isn’t focused
-		) {
-			location.hash = '#no-line'; // Update UI, without `scroll-to-top` behavior
-			history.replaceState({}, document.title, location.pathname); // Drop remaining # from url
-		}
-	});
+	document.body.addEventListener('keyup', listener);
 }
 
 features.add({
