66f2bb3fdf50558476c81b1ea8989aa0a56d1b8e
Federico Brigante <github@bfred.it>
2019-03-22 18:17:43 +0800

    Truncate long "full names"
    Example: https://github.com/isaacs/github/issues/297
    
    Avoids this: https://user-images.githubusercontent.com/1402241/54816250-c266c600-4cce-11e9-9f23-82c575c93a05.png

--- source/features/show-names.tsx
+++ source/features/show-names.tsx
@@ -41,7 +41,15 @@ async function init() {
 			// If it's a regular comment author, add it outside <strong>
 			// otherwise it's something like "User added some commits"
 			const insertionPoint = usernameEl.parentElement.tagName === 'STRONG' ? usernameEl.parentElement : usernameEl;
-			insertionPoint.after(' (', <bdo>{name}</bdo>, ') ');
+			insertionPoint.after(
+				' (',
+				<bdo class="css-truncate">
+					<span class="css-truncate-target" style={{maxWidth: '200px'}}>
+						{name}
+					</span>
+				</bdo>,
+				') '
+			);
 		}
 	}
 }
