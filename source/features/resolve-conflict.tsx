9cc08db529e438b69cc73774f3c367ebc8d045dc
Ahad Birang <farnabaz@gmail.com>
2019-03-25 16:34:13 +0430

    improve CodeMirror detection

--- source/features/resolve-conflict.tsx
+++ source/features/resolve-conflict.tsx
@@ -1,10 +1,10 @@
 import React from 'dom-chef';
 import features from '../libs/features';
+import elementReady from 'element-ready';
 
-function init() {
-	setTimeout(() => {
-		document.head.append(<script src={browser.runtime.getURL('resolve-conflicts.js')}/>);
-	}, 2000);
+async function init() {
+	await elementReady('.CodeMirror'); // *Not* safeElementReady
+	document.head.append(<script src={browser.runtime.getURL('resolve-conflicts.js')}/>);
 }
 
 features.add({
