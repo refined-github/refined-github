aeefb1216a29232a7bbcfcdf2986c13ed6ecb8b3
Federico Brigante <github@bfred.it>
2019-03-28 12:04:10 +0800

    Add `follow-file-renames` feature (#1870)

--- source/content.ts
+++ source/content.ts
@@ -91,6 +91,7 @@ import './features/preview-hidden-comments';
 import './features/fit-textareas';
 import './features/collapsible-content-button';
 import './features/hide-watch-and-fork-count';
+import './features/follow-file-renames';
 
 // Add global for easier debugging
 (window as any).select = select;
