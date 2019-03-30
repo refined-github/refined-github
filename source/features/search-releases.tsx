691c285c5f6f9b3f2ba9a2db5c8da8b722de34b3
Hardik Modha <hardikmodha22@gmail.com>
2019-03-30 22:21:00 +0530

    Lint fixes. #1895

--- source/features/search-releases.tsx
+++ source/features/search-releases.tsx
@@ -1,12 +1,12 @@
-import React from "dom-chef";
-import select from "select-dom";
-import OptionsSync from "webext-options-sync";
+import React from 'dom-chef';
+import select from 'select-dom';
+import OptionsSync from 'webext-options-sync';
 
-import * as api from "../libs/api";
-import features from "../libs/features";
-import { getOwnerAndRepo } from "../libs/utils";
-import { isReleasesOrTags } from "../libs/page-detect";
-import { checkInline, x as closeIcon } from "../libs/icons";
+import * as api from '../libs/api';
+import features from '../libs/features';
+import {getOwnerAndRepo} from '../libs/utils';
+import {isReleasesOrTags} from '../libs/page-detect';
+import {checkInline, x as closeIcon} from '../libs/icons';
 
 type ReleasesResponseV4 = {
 	releases: {
@@ -21,10 +21,12 @@ type ReleasesResponseV4 = {
 	};
 };
 
+/* eslint-disable @typescript-eslint/camelcase */
 type ReleasesResponseV3 = {
 	tag_name: string;
 	html_url: string;
 };
+/* eslint-enable @typescript-eslint/camelcase */
 
 type Release = {
 	tag: string;
@@ -33,13 +35,13 @@ type Release = {
 
 async function init() {
 	if (!isReleasesOrTags()) {
-		console.debug("Not on releases or tag page");
+		console.debug('Not on releases or tag page');
 
 		return false;
 	}
 
 	if (!isReleasesTabSelected()) {
-		console.debug("Release tab is not selected.");
+		console.debug('Release tab is not selected.');
 
 		return false;
 	}
@@ -50,25 +52,26 @@ async function init() {
 		return false;
 	}
 
-	const { personalToken } = await new OptionsSync().getAll();
+	const {personalToken} = await new OptionsSync().getAll();
 
 	const releases = await fetchReleases(personalToken);
 	addSelectMenu(releases);
 	registerEventListener();
 }
 
-const isReleasesTabSelected = () => getSelectedTabText() === "Releases";
+const isReleasesTabSelected = () => getSelectedTabText() === 'Releases';
 
-const hasAnyReleases = () => select('.release-entry') !== null || select('.release') !== null;
+const hasAnyReleases = () =>
+	select('.release-entry') !== null || select('.release') !== null;
 
 const getSelectedTabText = () => {
-	const selectedTab = select(".subnav > .subnav-links > .selected");
+	const selectedTab = select('.subnav > .subnav-links > .selected');
 
 	return selectedTab ? selectedTab.innerText : null;
 };
 
 const fetchReleases = async (personalToken): Promise<Release[]> => {
-	const { ownerName, repoName } = getOwnerAndRepo();
+	const {ownerName, repoName} = getOwnerAndRepo();
 
 	if (personalToken && personalToken.trim().length > 0) {
 		return fetchReleasesV4(ownerName, repoName);
@@ -78,73 +81,64 @@ const fetchReleases = async (personalToken): Promise<Release[]> => {
 };
 
 const fetchReleasesV4 = async (ownerName, repoName): Promise<Release[]> => {
-	return new Promise(async (resolve, reject) => {
-		try {
-			const jsonResponse: Record<
-				"repository",
-				ReleasesResponseV4
-			> = await api.v4(
-				`{
-					repository(owner: ${ownerName}, name: ${repoName}) {
-						releases(first: 100, orderBy: { field: CREATED_AT, direction: DESC }) {
-							totalCount
-							edges {
-								node {
-									tagName
-								}
-							}
+	try {
+		const jsonResponse: Record<'repository', ReleasesResponseV4> = await api.v4(
+			`{
+			repository(owner: ${ownerName}, name: ${repoName}) {
+				releases(first: 100, orderBy: { field: CREATED_AT, direction: DESC }) {
+					totalCount
+					edges {
+						node {
+							tagName
 						}
 					}
-				}`
-			);
-
-			const releases: Release[] = jsonResponse.repository.releases.edges.map(
-				({ node: { tagName } }) => {
-					return {
-						tag: tagName,
-						htmlPage: `https://github.com/${ownerName}/${repoName}/releases/tag/${tagName}`
-					};
 				}
-			);
+			}
+		}`
+		);
 
-			resolve(releases);
-		} catch (error) {
-			reject(error);
-		}
-	});
+		const releases: Release[] = jsonResponse.repository.releases.edges.map(
+			({node: {tagName}}) => {
+				return {
+					tag: tagName,
+					htmlPage: `https://github.com/${ownerName}/${repoName}/releases/tag/${tagName}`
+				};
+			}
+		);
+
+		return releases;
+	} catch (error) {
+		return Promise.reject(error);
+	}
 };
 
 const fetchReleasesV3 = async (ownerName, repoName): Promise<Release[]> => {
-	return new Promise(async (resolve, reject) => {
-		try {
-			const jsonResponse = (await api.v3(
-				`repos/${ownerName}/${repoName}/releases?per_page=100`
-			)) as ReleasesResponseV3[];
+	try {
+		const jsonResponse = await api.v3(`repos/${ownerName}/${repoName}/releases?per_page=100`) as ReleasesResponseV3[];
 
-			const releases: Release[] = jsonResponse.map(item => {
-				return { tag: item.tag_name, htmlPage: item.html_url };
-			});
+		const releases: Release[] = jsonResponse.map(item => {
+			return {tag: item.tag_name, htmlPage: item.html_url};
+		});
 
-			resolve(releases);
-		} catch (error) {
-			reject(error);
-		}
-	});
+		return releases;
+	} catch (error) {
+		return Promise.reject(error);
+	}
 };
 
 const addSelectMenu = (releases: Release[]) => {
-	const container = select(".subnav");
+	const container = select('.subnav');
 
 	const child = (
 		<div class="float-right">
 			{getSelectMenu({
-				title: "Releases",
+				title: 'Releases',
 				children: getAllReleasesLink(releases)
 			})}
 		</div>
 	);
 
-	container.appendChild(child);
+	container.append(child);
 };
 
 const getAllReleasesLink = (releases: Release[]) => {
@@ -155,7 +149,7 @@ const getAllReleasesLink = (releases: Release[]) => {
 			<a
 				href={release.htmlPage}
 				class={`select-menu-item js-navigation-item ${
-					release.tag === selectedTag ? "selected" : ""
+					release.tag === selectedTag ? 'selected' : ''
 				}`}
 			>
 				{checkInline()}
@@ -167,7 +161,7 @@ const getAllReleasesLink = (releases: Release[]) => {
 	});
 };
 
-const getSelectMenu = ({ title, children }) => {
+const getSelectMenu = ({title, children}) => {
 	const selectedTag = getSelectedTagFromUrl();
 
 	// https://styleguide.github.com/primer/components/select-menu/#filter
@@ -222,43 +216,43 @@ const getSelectMenu = ({ title, children }) => {
 
 const getSelectedTagFromUrl = () => {
 	const currentUrl = location.href;
-	const index = currentUrl.indexOf("/releases/tag/");
+	const index = currentUrl.indexOf('/releases/tag/');
 
-	return index > -1
-		? currentUrl
-				.substr(index)
-				.split("/")
-				.pop()
-		: null;
+	return index > -1 ?
+		currentUrl
+			.substr(index)
+			.split('/')
+			.pop() :
+		null;
 };
 
 const getFilterInput = () =>
-	select(".refined-github-search-releases .js-filterable-field");
+	select('.refined-github-search-releases .js-filterable-field');
 
 const eventListener = e => {
 	if (e.which === 13) {
 		const selectedLink = select(
-			".refined-github-search-releases .select-menu-item.js-navigation-item.navigation-focus"
+			'.refined-github-search-releases .select-menu-item.js-navigation-item.navigation-focus'
 		);
 
-		window.location.assign(selectedLink.getAttribute("href"));
+		window.location.assign(selectedLink.getAttribute('href'));
 	}
 };
 
 const registerEventListener = () => {
 	const filterInput = getFilterInput();
 
-	return filterInput
-		? filterInput.addEventListener("keydown", eventListener)
-		: null;
+	return filterInput ?
+		filterInput.addEventListener('keydown', eventListener) :
+		null;
 };
 
 const deregisterEventListener = () => {
 	const filterInput = getFilterInput();
 
-	return filterInput
-		? filterInput.removeEventListener("keydown", eventListener)
-		: null;
+	return filterInput ?
+		filterInput.removeEventListener('keydown', eventListener) :
+		null;
 };
 
 function deinit() {
@@ -266,7 +260,7 @@ function deinit() {
 }
 
 features.add({
-	id: "search-releases",
+	id: 'search-releases',
 	include: [features.isReleasesOrTags],
 	load: features.onDomReady,
 	init,
--- source/libs/icons.tsx
+++ source/libs/icons.tsx
@@ -52,7 +52,7 @@ export const gear = (): SVGElement => <svg aria-hidden="true" class="octicon oct
 
 export const foldDown = (): SVGElement => <svg aria-hidden="true" class="octicon octicon-fold-down" width="14" height="16" ><path fill-rule="evenodd" d="M4 11l3 3 3-3H8V5H6v6H4zm-4 0c0 .55.45 1 1 1h2.5l-1-1h-1l2-2H5V8H3.5l-2-2H5V5H1c-.55 0-1 .45-1 1l2.5 2.5L0 11zm10.5-2H9V8h1.5l2-2H9V5h4c.55 0 1 .45 1 1l-2.5 2.5L14 11c0 .55-.45 1-1 1h-2.5l1-1h1l-2-2z"/></svg>;
 
-export const checkInline = (): SVGElement => <svg class="octicon octicon-check select-menu-item-icon" viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5L12 5z"></path></svg>
+export const checkInline = (): SVGElement => <svg class="octicon octicon-check select-menu-item-icon" viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5L12 5z"></path></svg>;
 
 export const privateLockFilled = (): SVGElement => (
 	<svg className="octicon octicon-lock" width="14" height="16" aria-hidden="true">
