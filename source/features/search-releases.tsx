400995ed2eb68b770f43d8e7a3eb325ff4fbdb04
Hardik Modha <hardikmodha22@gmail.com>
2019-04-02 01:19:24 +0530

    Not swallowing the errors and made code more concise with suggested changes. #1895

--- source/features/search-releases.tsx
+++ source/features/search-releases.tsx
@@ -2,16 +2,13 @@ import React from 'dom-chef';
 import select from 'select-dom';
 
 import features from '../libs/features';
-import {isReleasesOrTags} from '../libs/page-detect';
 import {getOwnerAndRepo} from '../libs/utils';
 import {octoface} from '../libs/icons';
 
 async function init() {
-	if (!isReleasesOrTags()) {
-		return false;
-	}
+	if (!hasAnyReleases()) {
+		console.debug('The repo does not have any releases.');
 
-	if (!isOnReleasePage()) {
 		return false;
 	}
 
@@ -19,20 +16,14 @@ async function init() {
 	registerEventListener();
 }
 
-const isOnReleasePage = () => select.exists('.release-entry') || select.exists('.release');
-
-const getSelectedTagFromUrl = () => {
-	const result = location.href.match(/releases\/tag\/([^>/]*)/) || [];
-
-	return (result && result.length > 0) ? result[1] : null;
-};
+const hasAnyReleases = () => select.exists('.release-entry') || select.exists('.release');
 
 const addSelectMenu = () => {
 	const container = select('.subnav');
 	const {ownerName, repoName} = getOwnerAndRepo();
 	const selectedTag = getSelectedTagFromUrl();
 
-	container.append((
+	container.append(
 		<div class="refined-github-search-releases float-right d-flex flex-shrink-0 flex-items-center mb-3">
 			<details class="details-reset details-overlay select-menu branch-select-menu">
 				<summary class="btn btn-sm select-menu-button css-truncate" data-hotkey="w" title="Filter releases" aria-haspopup="menu">
@@ -44,7 +35,7 @@ const addSelectMenu = () => {
 					src={`/${ownerName}/${repoName}/ref-list/master?source_action=disambiguate&source_controller=files`}
 					preload
 					role="menu"
-					style="z-index: 99;"
+					style={{ zIndex: 99 }}
 				>
 					<include-fragment class="select-menu-loading-overlay anim-pulse">
 						{octoface()}
@@ -52,7 +43,13 @@ const addSelectMenu = () => {
 				</details-menu>
 			</details>
 		</div>
-	));
+	);
+};
+
+const getSelectedTagFromUrl = () => {
+	const [tag = null] = location.href.match(/(?<=\/releases\/tag\/)([^\/]+)/) || [];
+
+	return tag;
 };
 
 const registerEventListener = () => {
@@ -62,7 +59,6 @@ const registerEventListener = () => {
 };
 
 const onFragmentLoaded = () => {
-	adjustMenuStyles();
 	adjustTextInputStyles();
 	removeBranches();
 	adjustListStyles();
@@ -71,85 +67,39 @@ const onFragmentLoaded = () => {
 	updateLinks();
 };
 
-// Needed because somehow style given at Line no 65 is getting removed.
-const adjustMenuStyles = () => {
-	const detailsMenu = select('.js-release-details');
-
-	if (!detailsMenu) {
-		return false;
-	}
-
-	detailsMenu.style.zIndex = '99';
-};
-
 const adjustTextInputStyles = () => {
 	const textInput = select('.js-release-details > tab-container > .select-menu-filters > .select-menu-text-filter > input');
 
-	if (!textInput) {
-		return false;
-	}
-
 	textInput.setAttribute('aria-label', 'Type to filter releases');
 	textInput.setAttribute('placeholder', 'Type to filter releases...');
-};
+}
 
-const removeBranches = () => {
-	const branchList = select('.js-release-details > tab-container > .select-menu-list');
+const removeBranches = () => select('.js-release-details > tab-container > .select-menu-list').remove();
 
-	return branchList ? branchList.remove() : false;
-};
+const adjustListStyles = () => select('.js-release-details > tab-container > .select-menu-list').removeAttribute('hidden');
 
-const adjustListStyles = () => {
-	const tagList = select('.js-release-details > tab-container > .select-menu-list');
+const removeHeader = () => select('.js-release-details > .select-menu-header').remove();
 
-	if (!tagList) {
-		return false;
-	}
-
-	tagList.removeAttribute('hidden');
-};
-
-const removeHeader = () => {
-	const header = select('.js-release-details > .select-menu-header');
-
-	return header ? header.remove() : false;
-};
-
-const removeTabs = () => {
-	const tabs = select('.js-release-details > tab-container > .select-menu-filters > .select-menu-tabs');
-
-	return tabs ? tabs.remove() : false;
-};
+const removeTabs = () => select('.js-release-details > tab-container > .select-menu-filters > .select-menu-tabs').remove();
 
 const updateLinks = () => {
 	const links = select.all('.js-release-details > tab-container > .select-menu-list > div .select-menu-item');
 	const selectedTag = getSelectedTagFromUrl();
 
-	links.forEach(item => {
+	for (const item of links) {
 		const link = item.getAttribute('href');
 
 		item.setAttribute('href', link.replace('/tree/', '/releases/tag/'));
 
-		if (item.innerText.trim() === selectedTag) {
+		if (item.textContent.trim() === selectedTag) {
 			item.classList.add('selected');
 		}
-	});
+	}
 };
-
-const deregisterEventListener = () => {
-	const includeFragment = select('.js-release-details > include-fragment');
-
-	return includeFragment ? includeFragment.removeEventListener('loadend', onFragmentLoaded) : null;
-};
-
-function deinit() {
-	deregisterEventListener();
-}
 
 features.add({
 	id: 'search-releases',
 	include: [features.isReleasesOrTags],
 	load: features.onAjaxedPages,
 	init,
-	deinit
 });
