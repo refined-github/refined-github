91dd15b828aa24d4b9c56e722f86b0d5ce92dfe1
Nick Taylor <nick@iamdeveloper.com>
2019-04-14 15:05:10 -0400

    Fixed some lint errors.

--- source/libs/fetch-dom.ts
+++ source/libs/fetch-dom.ts
@@ -1,7 +1,7 @@
 import domify from 'doma';
 
 async function fetchDom(url: string): Promise<DocumentFragment>;
-async function fetchDom(url: string, selector: string): Promise<Element>;
+async function fetchDom(url: string, selector: string): Promise<Element>; // eslint-disable-line @typescript-eslint/unified-signatures
 async function fetchDom(url: string, selector?: string): Promise<Node> {
 	const response = await fetch(url);
 	const dom = domify(await response.text());
