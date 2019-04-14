faa0e09909d944f19bff8510606eda8ddd24ed6f
Eduardo Fernandes <efernandes@MacBook-Pro-3.home>
2019-04-15 00:14:23 +0100

    Fix linting errors

--- source/features/default-to-rich-diff.tsx
+++ source/features/default-to-rich-diff.tsx
@@ -11,37 +11,37 @@ const fileExtensionRegExp = new RegExp('\\.(' + richDiffExtensions.join('|') + '
 const checkFileIsRichDiffable = (filename: string): boolean => fileExtensionRegExp.test(filename);
 
 const observer = new MutationObserver(([{addedNodes}]) => {
-    for (const node of addedNodes) {
-        const element = node as Element;
-        if (element.tagName === 'DIV') {
-            if (element.className === 'js-diff-progressive-container') {
-                observer.observe(element, childList);
-            } else {
-                setRichDiff(element);
-            }
-        }
-    }
+	for (const node of addedNodes) {
+		const element = node as Element;
+		if (element.tagName === 'DIV') {
+			if (element.className === 'js-diff-progressive-container') {
+				observer.observe(element, childList);
+			} else {
+				setRichDiff(element);
+			}
+		}
+	}
 });
 
 function setRichDiff(node: Element) {
-    for (const fileHeader of select.all('.file-header', node)) {
-        if (checkFileIsRichDiffable(select('.file-info > a', fileHeader).title)) {
-            select('[aria-label="Display the rich diff"]', fileHeader).click();
-        }
-    }
+	for (const fileHeader of select.all('.file-header', node)) {
+		if (checkFileIsRichDiffable(select('.file-info > a', fileHeader).title)) {
+			select('[aria-label="Display the rich diff"]', fileHeader).click();
+		}
+	}
 }
 
 function init() {
-    setRichDiff(select('.js-diff-progressive-container:first-child'));
-    observer.observe(select('.js-diff-progressive-container:last-child'), childList);
+	setRichDiff(select('.js-diff-progressive-container:first-child'));
+	observer.observe(select('.js-diff-progressive-container:last-child'), childList);
 }
 
 features.add({
-    id: 'default-to-rich-diff',
-    include: [
-        features.isCommit,
-        features.isPRFiles
-    ],
-    load: features.onAjaxedPages,
-    init
-})
+	id: 'default-to-rich-diff',
+	include: [
+		features.isCommit,
+		features.isPRFiles
+	],
+	load: features.onAjaxedPages,
+	init
+});
