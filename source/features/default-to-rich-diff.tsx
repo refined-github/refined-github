daff25a41f65217d5f4908da1834160bdeaa31fa
Eduardo Fernandes <efernandes@MacBook-Pro-3.home>
2019-04-15 10:58:35 +0100

    Address PR comments
    * Use selector to find SVG files for accessing the rich-diff button

--- source/features/default-to-rich-diff.tsx
+++ source/features/default-to-rich-diff.tsx
@@ -1,39 +1,27 @@
 import select from 'select-dom';
 import features from '../libs/features';
 
-const childList = {childList: true};
-
-// To push new extensions if we want more file types to have rich diff as default
-const richDiffExtensions = ['svg'];
-
-const fileExtensionRegExp = new RegExp('\\.(' + richDiffExtensions.join('|') + ')$');
-
-const checkFileIsRichDiffable = (filename: string): boolean => fileExtensionRegExp.test(filename);
-
 const observer = new MutationObserver(([{addedNodes}]) => {
 	for (const node of addedNodes) {
-		const element = node as Element;
-		if (element.tagName === 'DIV') {
-			if (element.className === 'js-diff-progressive-container') {
-				observer.observe(element, childList);
+		if (node instanceof Element) {
+			if (node.matches('.js-diff-progressive-container')) {
+				observer.observe(node, {childList: true});
 			} else {
-				setRichDiff(element);
+				setRichDiff(node);
 			}
 		}
 	}
 });
 
 function setRichDiff(node: Element) {
-	for (const fileHeader of select.all('.file-header', node)) {
-		if (checkFileIsRichDiffable(select('.file-info > a', fileHeader).title)) {
-			select('[aria-label="Display the rich diff"]', fileHeader).click();
-		}
+	for (const fileHeader of select.all('.file-header[data-file-type=".svg"]', node)) {
+		select('[aria-label="Display the rich diff"]', fileHeader).click();
 	}
 }
 
 function init() {
 	setRichDiff(select('.js-diff-progressive-container:first-child'));
-	observer.observe(select('.js-diff-progressive-container:last-child'), childList);
+	observer.observe(select('.js-diff-progressive-container:last-child'), {childList: true});
 }
 
 features.add({
