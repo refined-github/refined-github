d90f89087ea8fb0ad7db9338cbc1de043d6b434b
Hardik Modha <hardikmodha22@gmail.com>
2019-04-17 01:12:06 +0530

    Used cloneNode, removed unnecessary variables and minor enhancements.

--- source/features/filter-pr-by-build-status.tsx
+++ source/features/filter-pr-by-build-status.tsx
@@ -4,62 +4,44 @@ import {checkInline} from '../libs/icons';
 import features from '../libs/features';
 
 function init() {
-	const filterAssignee = select('.table-list-header-toggle > details:nth-last-child(2)');
+	const statusFilter = select('.table-list-header-toggle > details:nth-last-child(3)').cloneNode(true) as HTMLElement;
 
-	filterAssignee.before(
-		<details class="details-reset details-overlay float-left select-menu">
-			<summary class="btn-link select-menu-button" aria-haspopup="menu">
-				Status&nbsp;
-			</summary>
-			<details-menu class="select-menu-modal position-absolute right-0" style={{zIndex: 99}} role="menu">
-				<div class="select-menu-header">
-					<span class="select-menu-title">Filter by Status</span>
-				</div>
-				<div class="select-menu-list"></div>
-			</details-menu>
-		</details>
-	);
+	select('summary', statusFilter).textContent = 'Status\xA0';
+	select('.select-menu-title', statusFilter).textContent = 'Filter by build status';
+	select('.select-menu-list', statusFilter).innerHTML = '';
 
+	select('.table-list-header-toggle > details:nth-last-child(2)').before(statusFilter);
 	select('.table-list-header-toggle > details:nth-last-child(3)').addEventListener('toggle', populateDropDown, {once: true});
 }
 
 function populateDropDown() {
-	const dropDownItems = getStatusDropDownItems();
-	const menu = select('.table-list-header-toggle > details:nth-last-child(3) .select-menu-list');
-
-	for (const item of dropDownItems) {
-		menu.append(item);
-	}
-}
-
-const getStatusDropDownItems = () => {
 	const statusList = ['Success', 'Failure', 'Pending'];
 
 	const searchParam = new URLSearchParams(location.search);
-	let queryString = searchParam.get('q') || 'is:pr is:open';
+	let queryString = searchParam.get('q') || '';
 
-	const [currentStatus] = queryString.match(/(?<=status:)(success|failure|pending)/) || [undefined];
+	const [, currentStatus] = queryString.match(/status:(success|failure|pending)/) || [, ''];
 
 	if (currentStatus) {
 		queryString = queryString.replace(/status:(success|failure|pending)/g, '').trim();
 	}
 
-	return statusList.map(status => {
-		const isSelected = currentStatus ? currentStatus.toLowerCase() === status.toLowerCase() : false;
+	select('.table-list-header-toggle > details:nth-last-child(3) .select-menu-list').append(...statusList.map(status => {
+		const isSelected = currentStatus.toLowerCase() === status.toLowerCase();
 
 		return (
 			<a
-				href={`?${String(new URLSearchParams({q: queryString + ` status:${status.toLowerCase()}`}))}`}
+				href={`?${new URLSearchParams({q: `${queryString} status:${status.toLowerCase()}`})}`}
 				class={`select-menu-item ${isSelected ? 'selected' : ''}`}
-				aria-checked={isSelected ? 'true' : 'false'}
+				aria-checked={String(isSelected)}
 				role="menuitemradio"
 			>
 				{checkInline()}
 				<div class="select-menu-item-text">{status}</div>
 			</a>
 		);
-	});
-};
+	}));
+}
 
 features.add({
 	id: 'filter-pr-by-build-status',
