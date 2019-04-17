98710c6fd3957625eebbd4e1dd0133979e3e2f35
Hardik Modha <hardikmodha22@gmail.com>
2019-04-17 20:40:58 +0530

    Type fixes.

--- source/features/filter-pr-by-build-status.tsx
+++ source/features/filter-pr-by-build-status.tsx
@@ -4,14 +4,14 @@ import {checkInline} from '../libs/icons';
 import features from '../libs/features';
 
 function init() {
-	const statusFilter = select('.table-list-header-toggle > details:nth-last-child(3)').cloneNode(true) as HTMLElement;
+	const statusFilter = select('.table-list-header-toggle > details:nth-last-child(3)')!.cloneNode(true) as HTMLDetailsElement;
 
-	select('summary', statusFilter).textContent = 'Status\u00A0';
-	select('.select-menu-title', statusFilter).textContent = 'Filter by build status';
-	select('.select-menu-list', statusFilter).innerHTML = '';
+	select('summary', statusFilter)!.textContent = 'Status\u00A0';
+	select('.select-menu-title', statusFilter)!.textContent = 'Filter by build status';
+	select('.select-menu-list', statusFilter)!.innerHTML = '';
 
-	select('.table-list-header-toggle > details:nth-last-child(2)').before(statusFilter);
-	select('.table-list-header-toggle > details:nth-last-child(3)').addEventListener('toggle', populateDropDown, {once: true});
+	select('.table-list-header-toggle > details:nth-last-child(2)')!.before(statusFilter);
+	select('.table-list-header-toggle > details:nth-last-child(3)')!.addEventListener('toggle', populateDropDown, {once: true});
 }
 
 function populateDropDown() {
@@ -26,18 +26,18 @@ function populateDropDown() {
 		queryString = queryString.replace(/status:(success|failure|pending)/g, '').trim();
 	}
 
-	select('.table-list-header-toggle > details:nth-last-child(3) .select-menu-list').append(...statusList.map(status => {
+	select('.table-list-header-toggle > details:nth-last-child(3) .select-menu-list')!.append(...statusList.map(status => {
 		const isSelected = currentStatus.toLowerCase() === status.toLowerCase();
 
 		return (
 			<a
 				href={`?${new URLSearchParams({q: `${queryString} status:${status.toLowerCase()}`})}`}
-				class={`select-menu-item ${isSelected ? 'selected' : ''}`}
-				aria-checked={String(isSelected)}
+				className={`select-menu-item ${isSelected ? 'selected' : ''}`}
+				aria-checked={isSelected}
 				role="menuitemradio"
 			>
 				{checkInline()}
-				<div class="select-menu-item-text">{status}</div>
+				<div className="select-menu-item-text">{status}</div>
 			</a>
 		);
 	}));
