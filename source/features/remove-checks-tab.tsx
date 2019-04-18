73d23fd5dd346bcbd0b2cd83b81d9a98563580bd
janat08 <jey.and.key@gmail.com>
2019-04-19 02:26:11 +0600

    linted

--- source/features/remove-checks-tab.tsx
+++ source/features/remove-checks-tab.tsx
@@ -7,14 +7,19 @@ import features from '../libs/features';
 import {safeElementReady} from '../libs/dom-utils';
 
 async function init() {
-
 	const checksTab = await safeElementReady(`
 		[data-hotkey="g 3"]
 	`); // Wait for the tab to be loaded
 
-	if (!checksTab) return
-	const counter = select('.Counter', checksTab)
-	if (!counter || !counter.textContent) return
+	if (!checksTab) {
+		return;
+	}
+
+	const counter = select('.Counter', checksTab);
+	if (!counter || !counter.textContent) {
+		return;
+	}
+
 	// Only remove the tab if it's not the current page and if it has 0 checks
 	if (!checksTab.matches('.selected') && counter.textContent.trim() === '0') {
 		checksTab.remove();
