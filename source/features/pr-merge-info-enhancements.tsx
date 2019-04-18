6574cbf42bc916b5845790d622db8e073352e744
Federico Brigante <github@bfred.it>
2019-04-18 14:02:07 +0800

    Automatically update the PR title when merging

--- source/features/pr-merge-info-enhancements.tsx
+++ source/features/pr-merge-info-enhancements.tsx
@@ -1,35 +1,92 @@
+import React from 'dom-chef';
 import select from 'select-dom';
+import debounce from 'debounce-fn';
 import delegate from 'delegate-it';
 import features from '../libs/features';
 
-// Update the field and dispatches the right events (focus is also used by `fit-textareas`)
-function updateField(field: HTMLTextAreaElement | HTMLInputElement, value: string) {
+const inputMap = new WeakMap();
+
+const createCommitTitle = debounce((): string => {
+	const title = select('.js-issue-title')!.textContent!;
+	const number = getPRNumber();
+	return `${title.trim()} (${number})`;
+}, {
+	wait: 1000,
+	immediate: true
+}) as () => string;
+
+function getPRNumber(): string {
+	return select('.gh-header-number')!.textContent!;
+}
+
+// Updates the field and dispatches the right events (focus is also used by `fit-textareas`)
+function updateField(field: HTMLTextAreaElement | HTMLInputElement, value: string): void {
 	field.value = value;
 	field.focus();
 	field.dispatchEvent(new InputEvent('input'));
 }
 
-function update() {
-	const title = select('.js-issue-title')!.textContent!;
-	const number = select('.gh-header-number')!.textContent!;
-	const description = select('.comment-form-textarea[name=\'pull_request[body]\']')!.textContent!;
-
+function updateCommit(): void {
 	updateField(
 		select<HTMLInputElement>('#merge_title_field')!,
-		`${title.trim()} (${number})`
+		createCommitTitle()
 	);
 
 	updateField(
 		select<HTMLTextAreaElement>('#merge_message_field')!,
-		description.trim()
+		select('.comment-form-textarea[name=\'pull_request[body]\']')!.textContent!.trim()
 	);
 }
 
+function showNote(): void {
+	const inputField = select<HTMLInputElement>('#merge_title_field')!;
+	const needsUpdate = createCommitTitle() !== inputField.value;
+
+	const note: HTMLElement = inputMap.get(inputField) || (
+		<p className="note">
+			The title of this PR will be updated to match this title. <button type="button" className="btn-link muted-link text-underline" onClick={() => {
+				note.hidden = true;
+			}}>Cancel</button>
+		</p>
+	);
+	inputMap.set(inputField, note);
+
+	// The `hidden` attribute is used to remember the user's Cancellation
+	if (needsUpdate && !note.hidden) {
+		inputField.after(note)
+	} else {
+		note.remove();
+	}
+}
+
+function updatePR() {
+	const inputField = select<HTMLInputElement>('#merge_title_field')!;
+	const note = inputMap.get(inputField);
+	if (!note || !note.parentElement) {
+		return;
+	}
+
+	const prTitle = inputField.value.replace(new RegExp(`\\s*\\(${getPRNumber()}\\)$`), '');
+
+	// Fill and submit title-change form
+	select<HTMLInputElement>('.edit-issue-title')!.value = prTitle;
+	select('.js-issue-update [type="submit"]')!.click(); // `form.submit()` isn't sent via ajax
+}
+
+
+// GitHub automatically restores value from the previous session and opens the form
+function restoreSession() {
+	document.addEventListener('session:resume', debounce(showNote));
+}
+
 function init() {
-	delegate(`
+	delegate('#discussion_bucket', `
 		.js-merge-pr.is-squashing,
 		.js-merge-pr.is-merging
-	`, 'details:toggled', update);
+	`, 'details:toggled', updateCommit);
+
+	delegate('#discussion_bucket', '#merge_title_field', 'input', showNote);
+	delegate('#discussion_bucket', 'form.js-merge-pull-request', 'submit', updatePR);
 }
 
 features.add({
@@ -39,4 +96,12 @@ features.add({
 	],
 	load: features.onAjaxedPages,
 	init
+});
+
+features.add({
+	id: 'pr-merge-info-enhancements',
+	include: [
+		features.isPRConversation
+	],
+	init: restoreSession
 });
