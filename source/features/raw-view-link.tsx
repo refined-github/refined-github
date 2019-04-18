936b6a2f38f60f920384158fdaa6239ebc85a062
Eduardo Fernandes <efernandes@MacBook-Pro-3.home>
2019-04-18 10:45:49 +0100

    Change load mechanism based on a6a6e15

--- source/features/raw-view-link.tsx
+++ source/features/raw-view-link.tsx
@@ -8,38 +8,22 @@ const createRawUrl = (href: string): string => {
 	return url.join('/');
 };
 
-const observer = new MutationObserver(([{addedNodes}]) => {
-	for (const node of addedNodes) {
-		if (node instanceof Element) {
-			if (node.matches('js-diff-progressive-container')) {
-				observer.observe(node, {childList: true});
-			} else {
-				addRawBtn(node);
-			}
-		}
-	}
-});
-
-function addRawBtn(node: Element | null) {
-	if (node) {
-		for (const link of select.all('.file-header .file-actions > div.BtnGroup [href*=\'/blob/\']', node)) {
-			const href = link.getAttribute('href');
-			if (href) {
-				link.after(
-					<a href={createRawUrl(href)} className="btn btn-sm BtnGroup-item">
-						Raw
-					</a>
-				);
-			}
-		}
+function addRawButtons() {
+	for (const anchor of select.all<HTMLAnchorElement>('.file-header .file-actions > div.BtnGroup [href*=\'/blob/\']:last-child')) {
+		anchor.after(
+			<a href={createRawUrl(anchor.pathname)} className="btn btn-sm BtnGroup-item">
+				Raw
+			</a>
+		);
 	}
 }
 
 function init() {
-	addRawBtn(select('.js-diff-progressive-container:first-child'));
-	const lastProgressiveContainer = select('.js-diff-progressive-container:last-child');
-	if (lastProgressiveContainer) {
-		observer.observe(lastProgressiveContainer, {childList: true});
+	addRawButtons();
+
+	// Some files are loaded progressively later. On load, look for more buttons and more fragments
+	for (const fragment of select.all('include-fragment.diff-progressive-loader')) {
+		fragment.addEventListener('load', init);
 	}
 }
 
