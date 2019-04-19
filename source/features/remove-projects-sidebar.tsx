88915ba497e6761074a2b320039e2b19b0e0cf0a
Federico Brigante <github@bfred.it>
2019-04-19 20:32:32 +0800

    Apply review suggestions

--- source/features/remove-projects-sidebar.tsx
+++ source/features/remove-projects-sidebar.tsx
@@ -4,30 +4,29 @@ The `Projects` tab is hidden from repositories and profiles when there are no pr
 New projects can still be created via the [`Create new…` menu](https://user-images.githubusercontent.com/1402241/34909214-18b6fb2e-f8cf-11e7-8556-bed748596d3b.png).
 */
 
-import features from '../libs/features';
-import {safeElementReady} from '../libs/dom-utils';
+import select from 'select-dom';
 import * as api from '../libs/api';
+import features from '../libs/features';
 import {getOwnerAndRepo} from '../libs/utils';
 
-function buildQuery() {
+async function init(): Promise<false | void> {
+	const project = select('[aria-label="Select projects"]');
+	if (!project) {
+		return false;
+	}
+
 	const {ownerName, repoName} = getOwnerAndRepo();
-
-	return `{
-		repository(owner: "${ownerName}", name: "${repoName}") {
-			projects(states: [OPEN, CLOSED]){
-      			totalCount
-    		}
+	const {repository} = await api.v4(`
+		{
+			repository(owner: "${ownerName}", name: "${repoName}") {
+				projects(states: [OPEN, CLOSED]){
+	      				totalCount
+				}
+			}
 		}
-	}`;
-}
-
-async function init() {
-	const res = await api.v4(buildQuery());
-	const project = await safeElementReady(`
-		[aria-label="Select projects"]
 	`);
 
-	if (res.repository.projects.totalCount === 0 && project) {
+	if (repository.projects.totalCount === 0) {
 		project.parentElement!.remove();
 	}
 }
@@ -36,7 +35,7 @@ features.add({
 	id: 'remove-projects-sidebar',
 	include: [
 		features.isIssue,
-		features.isPR
+		features.isPRConversation
 	],
 	load: features.onAjaxedPages,
 	init
