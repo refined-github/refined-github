f1e2b2d67bb9a28647f95665522366be4434c5ea
janat08 <jey.and.key@gmail.com>
2019-04-20 13:15:53 +0600

    timely api call

--- source/features/remove-projects-sidebar.tsx
+++ source/features/remove-projects-sidebar.tsx
@@ -4,19 +4,19 @@ The sidebar can't be used to create new projects.
 New projects can still be created via the [`Create new…` menu](https://user-images.githubusercontent.com/1402241/34909214-18b6fb2e-f8cf-11e7-8556-bed748596d3b.png).
 */
 
-import select from 'select-dom';
 import * as api from '../libs/api';
 import features from '../libs/features';
 import {getOwnerAndRepo} from '../libs/utils';
+import { safeElementReady } from '../libs/dom-utils';
 
-async function init(): Promise<false | void> {
-	const project = select('[aria-label="Select projects"]');
-	if (!project) {
-		return false;
-	}
+var loading = false
+var res: Promise<any>
 
-	const {ownerName, repoName} = getOwnerAndRepo();
-	const {repository} = await api.v4(`
+function query (){
+	const { ownerName, repoName } = getOwnerAndRepo();
+	// return await onetime((){}())
+	loading = true
+	res = api.v4(`
 		{
 			repository(owner: "${ownerName}", name: "${repoName}") {
 				projects(states: [OPEN, CLOSED]){
@@ -25,18 +25,42 @@ async function init(): Promise<false | void> {
 			}
 		}
 	`);
+	loading = false
+}
 
-	if (repository.projects.totalCount === 0) {
-		project.parentElement!.remove();
+
+async function init(): Promise<false | void> {
+	if (!loading) {
+		query()
 	}
+	const project = await safeElementReady('[aria-label="Select projects"]')
+	if (!project) {
+		return false;
+	}
+	loading = false
+	res.then(x => {
+		if (x.repository.projects.totalCount === 0) {
+			project.parentElement!.remove();
+		}
+	})
 }
 
 features.add({
 	id: 'remove-projects-sidebar',
 	include: [
 		features.isIssue,
-		features.isPRConversation
+		features.isPR
 	],
 	load: features.onAjaxedPages,
 	init
 });
+
+features.add({
+	id: 'remove-projects-sidebar',
+	include: [
+		features.isIssue,
+		features.isPR
+	],
+	load: features.onNavigation,
+	init: query
+})
