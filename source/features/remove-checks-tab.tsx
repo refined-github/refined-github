d02a657fdb28a1cbb911427bd72006bbde1d5a38
Federico Brigante <github@bfred.it>
2019-04-20 15:16:30 +0800

    Return false if tab is't hidden

--- source/features/remove-checks-tab.tsx
+++ source/features/remove-checks-tab.tsx
@@ -5,14 +5,14 @@ The `checks` tab in PR is removed unless you're owner
 import select from 'select-dom';
 import features from '../libs/features';
 
-async function init() {
+function init(): false | void {
 	// If there's a settings tab, the current user can enable checks,
 	// so the tab should not be hidden
 	if (select.exists([
 		'.js-repo-nav [data-selected-links^="repo_settings"]', // In repos
 		'.pagehead-tabs-item[href$="/settings/profile"]' // In organizations
 	].join())) {
-		return;
+		return false;
 	}
 
 	// Only remove the tab if it's not the current page and if it has 0 checks
