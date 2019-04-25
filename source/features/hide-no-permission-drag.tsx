82cbe725fbb98f463eff8ac46b04e9dc4ce231c0
Kai Hao <kevin830726@gmail.com>
2019-04-25 17:21:59 +0800

    hide drag button without permission

---
+++ source/features/hide-no-permission-drag.tsx
@@ -0,0 +1,23 @@
+import select from 'select-dom';
+import features from '../libs/features';
+import observeEl from '../libs/simplified-element-observer';
+
+function init() {
+	observeEl(select('.js-milestone-issues-container')!, () => {
+		const noPermissionIcons = select.all(
+			'[aria-label="You do not have permission to edit this milestone."]'
+		)!;
+
+		noPermissionIcons.forEach(icon => {
+			const dragButton = icon.parentElement!;
+			dragButton.style.setProperty('display', 'none', 'important');
+		});
+	});
+}
+
+features.add({
+	id: 'hide-no-permission-drag',
+	include: [features.isMilestone],
+	load: features.onAjaxedPages,
+	init
+});
