8f3306a34aa446519be21a36717969d4e12f80e7
Federico Brigante <github@bfred.it>
2019-04-26 03:53:36 +0800

    Extract `view-markdown-source`'s CSS (#1986)

--- source/features/view-markdown-source.tsx
+++ source/features/view-markdown-source.tsx
@@ -2,6 +2,7 @@
 Add button to view the markdown source whereas GitHub only lets you see the rendered version.
 https://user-images.githubusercontent.com/1402241/54814836-7bc39c80-4ccb-11e9-8996-9ecf4f6036cb.png
 */
+import './view-markdown-source.css';
 import React from 'dom-chef';
 import select from 'select-dom';
 import delegate from 'delegate-it';
