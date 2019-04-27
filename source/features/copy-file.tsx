7043b9453a6916c64c52a799ceca9ba88ff61d70
Federico Brigante <github@bfred.it>
2019-04-28 02:41:39 +0800

    Drop unnecessary code

--- source/features/copy-file.tsx
+++ source/features/copy-file.tsx
@@ -7,24 +7,14 @@ import features from '../libs/features';
 function handleClick({currentTarget: button}: React.MouseEvent<HTMLButtonElement>): void {
 	const file = button.closest('.Box');
 	const content = select.all('.blob-code-inner', file!)
-		.map(blob => blob.innerText) // Must be `.innerText`
-		.map(line => line === '\n' ? '' : line)
+		.map(({innerText: line}) => line === '\n' ? '' : line) // Must be `.innerText`
 		.join('\n');
 	copyToClipboard(content);
 }
 
-async function init(): Promise<void> {
-	// This won't show button on markdown and binary files until the code is loaded
+function init(): void {
 	if (select.exists('.blob.instapaper_body')) {
-		delegate('.rgh-md-source', 'rgh:view-markdown-source', () => {
-			// Prevents several buttons
-			if (select.exists('.rgh-copy-file')) {
-				return;
-			}
-
-			renderButton();
-		});
-		// Removes button if non-raw content is displayed
+		delegate('.rgh-md-source', 'rgh:view-markdown-source', renderButton);
 		delegate('.rgh-md-source', 'rgh:view-markdown-rendered', () => {
 			const button = select('.rgh-copy-file');
 			if (button) {
