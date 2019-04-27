1458dc2e0544f7c278e12bf7f8c3d286878385b0
janat08 <jey.and.key@gmail.com>
2019-04-27 21:48:25 +0600

    refractor

--- source/features/copy-file.tsx
+++ source/features/copy-file.tsx
@@ -5,23 +5,25 @@ import delegate from 'delegate-it';
 import features from '../libs/features';
 
 function handleClick({currentTarget: button}: React.MouseEvent<HTMLButtonElement>): void {
-	if (features.isMarkDown()) {
-		const file = button.closest('.Box');
-		const content = select.all('.blame-hunk', file!)
+	const isMarkdown = features.isMarkDown()
+	const file = button.closest('.Box');
+	const content = select.all(isMarkdown ? '.blame-hunk': '.blob-code-inner', file!)
+	let parsed: string
+	if (isMarkdown) {
+		parsed = content
 			.map(line => select.all('.blob-code', line))
 			.map(line => {
 				return line.map(x => x.innerText).map(line => line === '\n' ? '' : line).join('\n');
 			})
 			.map(line => line === '\n' ? '' : line)
 			.join('\n');
-		copyToClipboard(content);
+		copyToClipboard(parsed);
 	} else {
-		const file = button.closest('.Box');
-		const content = select.all('.blob-code-inner', file!)
+		parsed = content
 			.map(blob => blob.innerText) // Must be `.innerText`
 			.map(line => line === '\n' ? '' : line)
 			.join('\n');
-		copyToClipboard(content);
+		copyToClipboard(parsed);
 	}
 }
 
