af548b26e31b24879c51ad602290706d6998dcc8
janat08 <jey.and.key@gmail.com>
2019-04-27 21:51:27 +0600

    linted

--- source/features/copy-file.tsx
+++ source/features/copy-file.tsx
@@ -5,20 +5,21 @@ import delegate from 'delegate-it';
 import features from '../libs/features';
 
 function handleClick({currentTarget: button}: React.MouseEvent<HTMLButtonElement>): void {
-	const isMarkdown = features.isMarkDown()
+	const isMarkdown = features.isMarkDown();
 	const file = button.closest('.Box');
-	const content = select.all(isMarkdown ? '.blame-hunk': '.blob-code-inner', file!)
-	let parsed: string | string[]
+	const content = select.all(isMarkdown ? '.blame-hunk' : '.blob-code-inner', file!);
+	let parsed: string | string[];
 	if (isMarkdown) {
 		parsed = content
 			.map(line => select.all('.blob-code', line))
 			.map(line => {
 				return line.map(x => x.innerText).map(line => line === '\n' ? '' : line).join('\n');
-			})
+			});
 	} else {
 		parsed = content
-			.map(blob => blob.innerText) // Must be `.innerText`
+			.map(blob => blob.innerText); // Must be `.innerText`
 	}
+
 	parsed = parsed
 		.map(line => line === '\n' ? '' : line)
 		.join('\n');
