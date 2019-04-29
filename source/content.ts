52fcec3f27caae5ad34e06eb145bc50f7a36ba3a
Kai Hao <kevin830726@gmail.com>
2019-04-30 01:28:03 +0800

    Add `hide-disabled-milestone-sorter` feature (#1980)

--- source/content.ts
+++ source/content.ts
@@ -97,6 +97,7 @@ import './features/raw-file-link';
 import './features/tags-dropdown';
 import './features/filter-pr-by-build-status';
 import './features/edit-files-faster';
+import './features/hide-disabled-milestone-sorter';
 
 import './features/scrollable-code-and-blockquote.css';
 import './features/center-reactions-popup.css';
