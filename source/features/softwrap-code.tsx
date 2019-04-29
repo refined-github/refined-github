198e2a5d1d6381db13adeafb486f06627df5b06f
Laxman <notlmn@outlook.com>
2019-04-29 23:36:34 +0530

    Minify code

--- source/features/softwrap-code.tsx
+++ source/features/softwrap-code.tsx
@@ -6,20 +6,15 @@ import select from 'select-dom';
 import features from '../libs/features';
 
 async function init(): Promise<void> {
-	// Classes:
-	// `.diff-table` - Split and unified diffs
-	// `.d-table` - Suggested changes
-	// `.js-file-line-container` - Embedded code blocks
-	const tables = select.all('.diff-table, .d-table, .js-file-line-container');
+	const tables = select.all([
+		'.diff-table:not(.rgh-softwrapped-code)', // Split and unified diffs
+		'.d-table:not(.rgh-softwrapped-code)', // "Suggested changes" in PRs
+		'.js-file-line-container:not(.rgh-softwrapped-code)' // Embedded code blocks
+	].join());
 
 	for (const table of tables) {
-		if (table.classList.contains('rgh-softwrapped-code')) {
-			continue;
-		}
-
 		table.classList.add('rgh-softwrapped-code');
 
-		const lines = select.all('.blob-code-inner', table);
 		for (const line of select.all('.blob-code-inner', table)) {
 			if (!line.firstChild || !line.firstChild.textContent) {
 				continue;
@@ -29,22 +24,23 @@ async function init(): Promise<void> {
 			if (!leadingSpaceCharacters) {
 				continue;
 			}
-				let spaceCount = 0;
-				let tabCount = 0;
-				for (const char of leadingSpaceCharacters[0]) {
-					if (char === ' ') {
-						spaceCount += 1;
-					} else if (char === '\t') {
-						tabCount += 1;
-					}
-				}
 
-				// Move the whole line where it is supposed to be, then unindent the
-				// start of the line to compensate for indentation, preserving spaces
-				// We might get `--tab-size` from compatible extensions like `github-custom-tab-size`
-				line.style.paddingLeft = `calc((var(--tab-size, 4) * ${tabCount}ch) + ${spaceCount}ch)`;
-				line.style.textIndent = `calc((var(--tab-size, 4) * -${tabCount}ch) - ${spaceCount}ch)`;
+			let spaceCount = 0;
+			let tabCount = 0;
+			for (const char of leadingSpaceCharacters[0]) {
+				if (char === '\t') {
+					tabCount += 1;
+				} else {
+					// Consider every other "space" character as one "blank space" character
+					spaceCount += 1;
+				}
 			}
+
+			// Move the whole line where it is supposed to be, then unindent the
+			// start of the line to compensate for indentation, preserving spaces
+			// We might get `--tab-size` from compatible extensions like `github-custom-tab-size`
+			line.style.paddingLeft = `calc((var(--tab-size, 4) * ${tabCount}ch) + ${spaceCount}ch)`;
+			line.style.textIndent = `calc((var(--tab-size, 4) * -${tabCount}ch) - ${spaceCount}ch)`;
 		}
 	}
 }
