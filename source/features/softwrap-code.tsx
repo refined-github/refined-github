045cd0052501447a858b1a666f1a93de4e58df31
Federico Brigante <github@bfred.it>
2019-04-29 23:28:54 +0530

    Apply suggestions from code review
    Co-Authored-By: notlmn <notlmn@outlook.com>

--- source/features/softwrap-code.tsx
+++ source/features/softwrap-code.tsx
@@ -20,13 +20,15 @@ async function init(): Promise<void> {
 		table.classList.add('rgh-softwrapped-code');
 
 		const lines = select.all('.blob-code-inner', table);
-		for (const line of lines) {
+		for (const line of select.all('.blob-code-inner', table)) {
 			if (!line.firstChild || !line.firstChild.textContent) {
 				continue;
 			}
 
-			const leadingSpaceCharacters = line.firstChild.textContent.match(/^\s+/);
-			if (leadingSpaceCharacters) {
+			const leadingSpaceCharacters = line.firstChild!.textContent!.match(/^\s+/);
+			if (!leadingSpaceCharacters) {
+				continue;
+			}
 				let spaceCount = 0;
 				let tabCount = 0;
 				for (const char of leadingSpaceCharacters[0]) {
@@ -39,7 +41,7 @@ async function init(): Promise<void> {
 
 				// Move the whole line where it is supposed to be, then unindent the
 				// start of the line to compensate for indentation, preserving spaces
-				// We get `--tab-size` from RGH or compatible extensions like GCTS
+				// We might get `--tab-size` from compatible extensions like `github-custom-tab-size`
 				line.style.paddingLeft = `calc((var(--tab-size, 4) * ${tabCount}ch) + ${spaceCount}ch)`;
 				line.style.textIndent = `calc((var(--tab-size, 4) * -${tabCount}ch) - ${spaceCount}ch)`;
 			}
