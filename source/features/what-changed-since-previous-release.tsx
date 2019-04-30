4ff927a6c1e8257f5f8c2d153351318cdf58b566
Hardik Modha <hardikmodha22@gmail.com>
2019-05-01 01:58:16 +0530

    Further simplify the code
    1. Removed conditional checks before looking for previous tags. All
    tags are now treated equally whether they are on releases, tag listing
    or single tag page.
    2. Use the single loop to go through the tags irrespective of their index.
    It also improved the probability of finding the previous tag to compare
    as we now look into the next page for all the tags.

--- source/features/what-changed-since-previous-release.tsx
+++ source/features/what-changed-since-previous-release.tsx
@@ -11,57 +11,42 @@ async function init(): Promise<void | false> {
 		return false;
 	}
 
-	const {ownerName, repoName} = getOwnerAndRepo();
-
 	// To extract the tag "v16.8.6" from links like "/facebook/react/releases/tag/v16.8.6"
 	const tagRegExp = /releases\/tag\/(.*)$/;
 
 	// To extract the commit hash from links like "/facebook/react/commit/92a1d8feac32d03ab5ea6ac13ae4941f6ae93b54"
 	const commitRegExp = /commit\/([0-9a-f]{40})$/;
 
-	const tagAnchorSelector = `a[href^="/${ownerName}/${repoName}/releases/tag"]`;
-	const tagSelector = `h4.commit-title ${tagAnchorSelector}, .commit .release-header ${tagAnchorSelector}`;
-
-	const commitAnchorSelector = `a[href^="/${ownerName}/${repoName}/commit/"]`;
-	const commitIdSelector = `.commit > ul ${commitAnchorSelector}, .release > div:first-child > ul ${commitAnchorSelector}`;
+	const tagSelector = 'h4.commit-title a[href*="/releases/tag"], .commit .release-header a[href*="/releases/tag"]';
+	const commitIdSelector = '.commit > ul a[href*="/commit/"], .release > div:first-child > ul a[href*="/commit/"]';
 
 	const allTags = extractValuesFromPathname(select.all<HTMLAnchorElement>(tagSelector), tagRegExp);
 	const allCommitIdsAnchor = select.all<HTMLAnchorElement>(commitIdSelector);
 	const allCommitIds = extractValuesFromPathname(allCommitIdsAnchor, commitRegExp);
 
-	for (let index = 0; index < allTags.length - 1; index++) {
+	const nextPageLink = select<HTMLAnchorElement>('.pagination a:last-child');
+	const nextPage = nextPageLink ? await fetchDom(nextPageLink.href) : await getNextPageForSinglePageTag(allTags[0]);
+
+	allTags.push(...extractValuesFromPathname(select.all<HTMLAnchorElement>(tagSelector, nextPage || document), tagRegExp));
+	allCommitIds.push(...extractValuesFromPathname(select.all<HTMLAnchorElement>(commitIdSelector, nextPage || document), commitRegExp));
+
+	for (let index = 0; index < allCommitIdsAnchor.length; index++) {
 		const previousCommitIndex = getPreviousTagIndex(index + 1, allCommitIds[index], allCommitIds, allTags[index], allTags);
 
 		if (previousCommitIndex !== -1) {
 			allCommitIdsAnchor[index].parentElement!.parentElement!.append(getTagComparisonLink(allTags[previousCommitIndex], allTags[index]));
 		}
 	}
+}
 
-	let nextPage;
-
-	if (isSingleTagPage()) {
-		nextPage = await fetchDom(`/${ownerName}/${repoName}/releases?after=${allTags[0]}`);
-	} else {
-		const nextPageLink = select<HTMLAnchorElement>('.pagination a:last-child');
-
-		if (!nextPageLink) {
-			return;
-		}
-
-		nextPage = await fetchDom(nextPageLink.href);
+const getNextPageForSinglePageTag = async (singleTag: string): Promise<void | DocumentFragment> => {
+	if (!isSingleTagPage()) {
+		return;
 	}
 
-	const nextPageAllTags = extractValuesFromPathname(select.all<HTMLAnchorElement>(tagSelector, nextPage), tagRegExp);
-	const nextPageAllCommitIds = extractValuesFromPathname(select.all<HTMLAnchorElement>(commitIdSelector, nextPage), commitRegExp);
+	const {ownerName, repoName} = getOwnerAndRepo();
 
-	for (let index = 0; index < nextPageAllTags.length; index++) {
-		const previousTagIndex = getPreviousTagIndex(index, allCommitIds[allCommitIds.length - 1], nextPageAllCommitIds, allTags[allTags.length - 1], nextPageAllTags);
-
-		if (previousTagIndex !== -1) {
-			allCommitIdsAnchor[allCommitIdsAnchor.length - 1].parentElement!.parentElement!.append(getTagComparisonLink(nextPageAllTags[previousTagIndex], allTags[allTags.length - 1]));
-			break;
-		}
-	}
+	return fetchDom(`/${ownerName}/${repoName}/releases?after=${singleTag}`);
 }
 
 const extractValuesFromPathname = (anchors: HTMLAnchorElement[], regexp: RegExp): string[] => {
