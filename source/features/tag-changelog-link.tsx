a069e9cff079143a24abad89e601e57497e92747
Federico Brigante <github@bfred.it>
2019-05-02 14:14:39 +0800

    Query once; merge functions; avoid indexes when possible

--- source/features/tag-changelog-link.tsx
+++ source/features/tag-changelog-link.tsx
@@ -1,4 +1,4 @@
-/**
+/*
 This feature adds a compare link on each releases/tags/single tag page so that you can see
 what has changed since the previous release. If the tags are namespaced then it tries to
 get the previous release of the same namespaced tag.
@@ -10,42 +10,45 @@ import React from 'dom-chef';
 import select from 'select-dom';
 import features from '../libs/features';
 import fetchDom from '../libs/fetch-dom';
-import {getOwnerAndRepo} from '../libs/utils';
-import {isSingleTagPage} from '../libs/page-detect';
 import {diff} from '../libs/icons';
+import {isSingleTagPage} from '../libs/page-detect';
+import {getRepoPath, getRepoURL} from '../libs/utils';
+
+async function getNextPage(): Promise<DocumentFragment> {
+	const nextPageLink = select<HTMLAnchorElement>('.pagination a:last-child');
+	if (nextPageLink) {
+		return fetchDom(nextPageLink.href);
+	}
+
+	if (isSingleTagPage()) {
+		const [, tag = ''] = getRepoPath()!.split('releases/tag/', 2); // Already URL-encoded
+		return fetchDom(`/${getRepoURL()}/tags?after=${tag}`);
+	}
+
+	return new DocumentFragment();
+}
 
 async function init(): Promise<void | false> {
 	if (select.exists('.blankslate')) {
 		return false;
 	}
 
-	const tagSelector = 'h4.commit-title a[href*="/releases/tag"], .commit .release-header a[href*="/releases/tag"]';
-	const commitIdSelector = '.commit > ul a[href*="/commit/"], .release > div:first-child > ul a[href*="/commit/"]';
+	const documents = [document, await getNextPage()] as any; // TODO: fix select-dom types to accept mixed arrays
+	const tagElements = select.all('[href*="/releases/tag"]', documents);
+	const commitElements = select.all('.muted-link[href*="/commit/"]', documents);
+	const tags = tagElements.map(anchor => anchor.textContent!.trim());
+	const commits = commitElements.map(anchor => anchor.textContent!.trim());
 
-	const allTags = extractTextFromAnchors(select.all<HTMLAnchorElement>(tagSelector));
-	const allCommitIdsAnchor = select.all<HTMLAnchorElement>(commitIdSelector);
-	const allCommitIds = extractTextFromAnchors(allCommitIdsAnchor);
+	for (const [index, commitElement] of commitElements.entries()) {
+		const previousTag = getPreviousTag(index, commits, tags);
 
-	const nextPageLink = select<HTMLAnchorElement>('.pagination a:last-child');
-	const nextPage = nextPageLink ? await fetchDom(nextPageLink.href) : await getNextPageForSinglePageTag(allTags[0]);
-
-	if (nextPage) {
-		allTags.push(...extractTextFromAnchors(select.all<HTMLAnchorElement>(tagSelector, nextPage)));
-		allCommitIds.push(...extractTextFromAnchors(select.all<HTMLAnchorElement>(commitIdSelector, nextPage)));
-	}
-
-	const {ownerName, repoName} = getOwnerAndRepo();
-
-	for (let index = 0; index < allCommitIdsAnchor.length; index++) {
-		const prevTagIndex = getPreviousTagIndex(index, allCommitIds, allTags);
-
-		if (prevTagIndex !== -1) {
-			allCommitIdsAnchor[index].closest('ul')!.append(
+		if (previousTag !== false) {
+			commitElement.closest('ul')!.append(
 				<li className="d-inline-block mb-1 mt-1 f6">
 					<a
 						className="muted-link text-mono tooltipped tooltipped-n"
-						aria-label={'See changes since ' + allTags[prevTagIndex]}
-						href={`/${ownerName}/${repoName}/compare/${allTags[prevTagIndex]}...${allTags[index]}`}
+						aria-label={'See changes since ' + previousTag}
+						href={`/${getRepoURL()}/compare/${previousTag}...${tags[index]}`}
 					>
 						{diff()} Changelog
 					</a>
@@ -55,43 +58,26 @@ async function init(): Promise<void | false> {
 	}
 }
 
-const extractTextFromAnchors = (anchors: HTMLAnchorElement[]): string[] =>
-	anchors.map((anchor: HTMLAnchorElement): string => anchor.textContent!.trim());
+const getPreviousTag = (index: number, commits: string[], tags: string[]): string | false => {
+	let previousTag: string | false = false;
 
-const getNextPageForSinglePageTag = async (tag: string): Promise<void | DocumentFragment> => {
-	if (!isSingleTagPage()) {
-		return;
-	}
-
-	const {ownerName, repoName} = getOwnerAndRepo();
-
-	// Firefox requires location.origin for fetch without that relative URLs will fail on Firefox.
-	// See: https://github.com/sindresorhus/refined-github/pull/1998#issuecomment-488231253
-	return fetchDom(`${location.origin}/${ownerName}/${repoName}/releases?after=${tag}`);
-};
-
-const getPreviousTagIndex = (index: number, allCommitIds: string[], allTags: string[]): number => {
-	let prevTagIndex = -1;
-
-	for (let i = index + 1; i < allCommitIds.length; i++) {
-		if (allCommitIds[i] === allCommitIds[index]) {
+	for (let i = index + 1; i < commits.length; i++) {
+		if (commits[i] === commits[index]) {
 			continue;
 		}
 
-		if (doesNamespaceMatch(allTags[i], allTags[index])) {
-			return i;
+		// Ensure that they have the same namespace. e.g. `parcel@1.2.4` and `parcel@1.2.3`
+		if (tags[i].split(/@[^@]+$/)[0] === tags[index].split(/@[^@]+$/)[0]) {
+			return tags[i];
 		}
 
-		if (prevTagIndex === -1) {
-			prevTagIndex = i;
+		if (previousTag === false) {
+			previousTag = tags[i];
 		}
 	}
 
-	return prevTagIndex;
+	return previousTag;
 };
-
-const doesNamespaceMatch = (tag1: string, tag2: string): boolean =>
-	tag1.split(/@[^@]+$/)[0] === tag2.split(/@[^@]+$/)[0];
 
 features.add({
 	id: 'tag-changelog-link',
