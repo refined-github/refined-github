eeb8c42e667239fa3f9d7c76b9039169cc50abc6
Federico Brigante <github@bfred.it>
2019-05-02 15:34:56 +0800

    Cache fetchDom when possible

--- source/libs/fetch-dom.ts
+++ source/libs/fetch-dom.ts
@@ -1,9 +1,12 @@
 import domify from 'doma';
+import pMemoize from 'p-memoize';
+
+const cachedFetch = pMemoize(fetch);
 
 async function fetchDom(url: string): Promise<DocumentFragment>;
 async function fetchDom(url: string, selector: string): Promise<Element>; // eslint-disable-line @typescript-eslint/unified-signatures
 async function fetchDom(url: string, selector?: string): Promise<Node> {
-	const response = await fetch(url);
+	const response = await cachedFetch(url);
 	const dom = domify(await response.text());
 	return selector ? dom.querySelector(selector)! : dom;
 }
