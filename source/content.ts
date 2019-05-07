0b6467669cda6e08f50375d1b8f84e7c6d1f8e15
Zachary Whaley <zachbwhaley@gmail.com>
2019-05-07 15:11:18 -0500

    Revert "Drop `fix-squash-and-merge-message`"
    This reverts commit 9be9440699d2b8040af0c7c26ae524169b3cd0b8.

--- source/content.ts
+++ source/content.ts
@@ -48,6 +48,7 @@ import './features/star-repo-hotkey';
 import './features/toggle-files-button';
 import './features/scroll-to-top-on-collapse';
 import './features/sync-pr-commit-title';
+import './features/fix-squash-and-merge-message';
 import './features/open-ci-details-in-new-tab';
 import './features/wait-for-build';
 import './features/hide-inactive-deployments';
