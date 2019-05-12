85c79c1f63bf83065561c7c831b5dd1e06b23cf7
Connor Love <connor@dotconnor.com>
2019-05-12 18:40:00 -0400

    enforce code style

--- source/features/open-issue-to-latest-comment.tsx
+++ source/features/open-issue-to-latest-comment.tsx
@@ -1,30 +1,28 @@
 import select from 'select-dom';
 import features from '../libs/features';
-import { getRepoPath, getOwnerAndRepo } from '../libs/utils';
+import {getRepoPath, getOwnerAndRepo} from '../libs/utils';
 
 function init(): void | false {
-  if (getRepoPath() === 'issues') {
-    const {ownerName, repoName} = getOwnerAndRepo();
-    select.all<HTMLAnchorElement>(`.js-issue-row a.muted-link[href^="/${ownerName}/${repoName}/issues/"]`).forEach((link) => {
-      link.href = `${link.href}#last`;
-    })
-  } else {
-    if (window.location.hash === '#last') {
-      setTimeout(() => {
-        const comments = select.all('.js-comment-container')
-        if (comments.length > 0) {
-          comments[comments.length - 1].scrollIntoView();
-        }
-      }, 100);
-    }
-  }
+	if (getRepoPath() === 'issues') {
+		const {ownerName, repoName} = getOwnerAndRepo();
+		select.all<HTMLAnchorElement>(`.js-issue-row a.muted-link[href^="/${ownerName}/${repoName}/issues/"]`).forEach(link => {
+			link.href = `${link.href}#last`;
+		});
+	} else if (window.location.hash === '#last') {
+		setTimeout(() => {
+			const comments = select.all('.js-comment-container');
+			if (comments.length > 0) {
+				comments[comments.length - 1].scrollIntoView();
+			}
+		}, 100);
+	}
 }
 
 features.add({
 	id: 'open-issue-to-latest-comment',
 	include: [
-    features.isIssue,
-    features.isIssueList
+		features.isIssue,
+		features.isIssueList
 	],
 	load: features.onAjaxedPages,
 	init
