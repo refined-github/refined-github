3fecccfb1b4bd24e210deb36484b350c37c5c4ff
Connor Love <connor@dotconnor.com>
2019-05-12 20:21:09 -0400

    update selector and use link to bottom of page element

--- source/features/open-issue-to-latest-comment.tsx
+++ source/features/open-issue-to-latest-comment.tsx
@@ -1,28 +1,16 @@
 import select from 'select-dom';
 import features from '../libs/features';
-import {getRepoPath, getOwnerAndRepo} from '../libs/utils';
 
 function init(): void | false {
-	if (getRepoPath() === 'issues') {
-		const {ownerName, repoName} = getOwnerAndRepo();
-		select.all<HTMLAnchorElement>(`.js-issue-row a.muted-link[href^="/${ownerName}/${repoName}/issues/"]`).forEach(link => {
-			link.href = `${link.href}#last`;
-		});
-	} else if (window.location.hash === '#last') {
-		setTimeout(() => {
-			const comments = select.all('.js-comment-container');
-			if (comments.length > 0) {
-				comments[comments.length - 1].scrollIntoView();
-			}
-		}, 100);
+	for (const link of select.all<HTMLAnchorElement>('a[aria-label*="comment"]')) {
+		link.href = `${link.href}#partial-timeline`;
 	}
 }
 
 features.add({
 	id: 'open-issue-to-latest-comment',
 	include: [
-		features.isIssue,
-		features.isIssueList
+		features.isDiscussionList
 	],
 	load: features.onAjaxedPages,
 	init
