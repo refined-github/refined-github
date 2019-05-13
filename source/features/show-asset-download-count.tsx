c34f36939f966a859fac0d841ee29902a7d0eacb
Connor Love <connor@dotconnor.com>
2019-05-12 21:51:59 -0400

    cleanup

--- source/features/show-asset-download-count.tsx
+++ source/features/show-asset-download-count.tsx
@@ -1,9 +1,9 @@
 import React from 'dom-chef';
 import select from 'select-dom';
 import features from '../libs/features';
-import {v4, escapeKey} from '../libs/api';
-import {cloudDownload} from '../libs/icons';
-import {getOwnerAndRepo, getRepoPath} from '../libs/utils';
+import * as api from '../libs/api';
+import * as icons from '../libs/icons';
+import {getOwnerAndRepo} from '../libs/utils';
 import {wrap} from '../libs/dom-utils';
 
 interface Asset {
@@ -15,45 +15,45 @@ interface Tag {
 }
 async function getAssetsForTag(tags: string[]): Promise<Tag> {
 	const {ownerName, repoName} = getOwnerAndRepo();
-	const data = await v4(
+	const {repository} = await api.v4(
 		'{' +
 			tags.map(tag =>
-				escapeKey(tag) + `: repository(owner: "${ownerName}", name: "${repoName}") { release(tagName:"${tag}") { releaseAssets(first: 100) { edges { node { name downloadCount } } } } }`
+				`repository(owner: "${ownerName}", name: "${repoName}") {
+					${api.escapeKey(tag)}: release(tagName:"${tag}") {
+						releaseAssets(first: 100) {
+							nodes {
+								name
+								downloadCount
+							}
+						}
+					}
+				}`
 			) +
 		'}'
 	);
 	const assets: Tag = {};
-	for (const [tag, repo] of Object.entries(data)) {
-		assets[tag] = repo.release.releaseAssets.edges.map((edge: any) => edge.node);
+	for (const [tag, release] of Object.entries(repository)) {
+		assets[tag] = (release as any).releaseAssets.nodes
 	}
 
 	return assets;
 }
 
 async function init(): Promise<void | false> {
-	if (getRepoPath() === null || !(getRepoPath() as string).startsWith('releases')) {
-		return;
-	}
-
 	const tags = select.all('svg.octicon-tag ~ span').map(tag => tag.textContent!);
+	if (tags.length === 0) {
+		return false;
+	}
 	const tagAssets = await getAssetsForTag(tags);
-	for (const release of select.all('.release-entry:not(.release-timeline-tags)')) {
-		const tagName = escapeKey(select('svg.octicon-tag ~ span', release)!.textContent!);
-		if (!tagAssets[tagName]) {
-			continue;
-		}
-
-		for (const assetTag of select.all('.release-main-section details.details-reset .Box-body.flex-justify-between', release)) {
+	for (const release of select.all('.release-entry .release')) {
+		const tagName = api.escapeKey(select('svg.octicon-tag ~ span', release)!.textContent!);
+		for (const assetTag of select.all('.release-main-section .Box-body.flex-justify-between', release)) {
 			const assetName = select('svg.octicon-package ~ span', assetTag)!.textContent!;
-			const asset = tagAssets[tagName].find((a: any) => a.name === assetName);
-			if (!asset) {
-				continue;
-			}
-
-			const right = select('small', assetTag)!;
-			wrap(right, <div className="flex-shrink-0">
+			const asset = tagAssets[tagName].find((a: any) => a.name === assetName)!;
+			const assetSize = select('small', assetTag)!;
+			wrap(assetSize, <div className="flex-shrink-0">
 				<span className="mr-2">
-					{cloudDownload()}
+					{icons.cloudDownload()}
 					<small className="ml-1">{asset.downloadCount}</small>
 				</span>
 			</div>);
