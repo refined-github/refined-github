5c2356154587cd6250a4d5f7cc5ae6da3f8d0383
Federico Brigante <github@bfred.it>
2019-05-28 16:19:25 +0800

    Show which features are disabled internally
    https://user-images.githubusercontent.com/1402241/58462364-4ff9e280-8164-11e9-8b7e-29c7e974a520.png

--- source/options.tsx
+++ source/options.tsx
@@ -23,15 +23,19 @@ function parseDescription(description: string): DocumentFragment {
 	return descriptionFragment;
 }
 
-function buildFeatureCheckbox([name, {description, screenshot}]: [string, FeatureDetails]): HTMLElement {
+function buildFeatureCheckbox([name, {description, screenshot, disabled}]: [string, FeatureDetails]): HTMLElement {
+	// `undefined` disconnects it from the options
+	const id = disabled ? undefined : `feature:${name}`;
+
 	const parsedDescription = parseDescription(
+		(disabled ? `Disabled because of ${disabled}. \n` : '') +
 		description.replace(/[^.]$/, '$&.') // Add period if missing
 	);
 
 	return (
 		<p>
-			<input type="checkbox" name={`feature:${name}`} id={`feature:${name}`} />
-			<label for={`feature:${name}`} className="info">
+			<input type="checkbox" name={id} id={id} disabled={Boolean(disabled)} />
+			<label for={id} className="info">
 				<span className="feature-name">{name}</span>
 				{' '}
 				<a href={`https://github.com/sindresorhus/refined-github/blob/master/source/features/${name}.tsx`}>
