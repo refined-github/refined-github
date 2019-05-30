2766b3a4105f3f491394b3bad71c38dc91a22c0a
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2019-05-30 12:05:41 +0100

    Fixed issue with raw-file-link not being added to the page

--- source/features/raw-file-link.tsx
+++ source/features/raw-file-link.tsx
@@ -10,17 +10,16 @@ const createRawUrl = (href: string): string => {
 };
 
 function addRawButtons(): void {
-	for (const anchor of select.all<HTMLAnchorElement>('.file-header .file-actions > div.BtnGroup [href*=\'/blob/\']:last-child')) {
+	for (const anchor of select.all<HTMLAnchorElement>('.file-header .file-actions > div > details > details-menu > a:nth-child(3)')) {
 		anchor.after(
-			<a href={createRawUrl(anchor.pathname)} className="btn btn-sm BtnGroup-item">
-				Raw
+			<a href={createRawUrl(anchor.pathname)} className="pl-5 dropdown-item btn-link">
+				View raw
 			</a>
 		);
 	}
 }
 
 function init(): void {
-	addRawButtons();
 	onPrFileLoad(addRawButtons);
 }
 
