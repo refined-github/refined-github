08f0fa901f8f6f323f2ae9f7f2f5913711653951
lubien <lubien1996@gmail.com>
2019-06-01 05:35:15 -0300

    Fix lint

--- source/features/highest-rated-comment.tsx
+++ source/features/highest-rated-comment.tsx
@@ -4,22 +4,22 @@ import features from '../libs/features';
 import './highest-rated-comment.css';
 
 type Option = {
-	$el: Element,
-	likes: number,
-	unlikes: number,
-	index: number
+	$el: Element;
+	likes: number;
+	unlikes: number;
+	index: number;
 }
 type Props = {
-	id: string,
-	username: string,
-	text: string,
-	avatar: string
+	id: string;
+	username: string;
+	text: string;
+	avatar: string;
 }
 
-const like = '👍'
-const unlike = '👎'
+const like = '👍';
+const unlike = '👎';
 
-const element = ({ id, username, text, avatar }: Props) => (
+const element = ({id, username, text, avatar}: Props): Node => (
 	<div className="timeline-comment-wrapper rgh-highest-rated-comment">
 		<div className="avatar-parent-child timeline-comment-avatar">
 			<a href={`https://github.com/${username}`} className="d-inline-block">
@@ -45,69 +45,69 @@ const element = ({ id, username, text, avatar }: Props) => (
 			</div>
 		</a>
 	</div>
-)
+);
 
 function init(): void {
-	const $comments: Element[] = select.all('.comment')
+	const $comments: Element[] = select.all('.comment');
 
-	const options: Option[] = select.all('.comment-reactions-options') // search for those with reactions
+	const options: Option[] = select.all('.comment-reactions-options') // Search for those with reactions
 		.map($reactions => {
-			const $el = $reactions.closest('.comment')!
-			const $buttons = select.all('button', $reactions)
+			const $el = $reactions.closest('.comment')!;
+			const $buttons = select.all('button', $reactions);
 			const reactions: {[key: string]: number} = $buttons.reduce((acc: {[s: string]: number}, $button) => {
 				try {
 					const [emoji, countStr] = $button.innerText
 						.split(' ')
-						.map(x => x.trim())
-					const count = parseInt(countStr, 10)
-					acc[emoji] = count
-					return acc
+						.map(x => x.trim());
+					const count = parseInt(countStr, 10);
+					acc[emoji] = count;
+					return acc;
 				} catch (error) {
-					return acc
+					return acc;
 				}
-			}, {})
+			}, {});
 
-			const likes = reactions[like] || 0
-			const unlikes = reactions[unlike] || 0
-			const index = $comments.indexOf($el)
+			const likes = reactions[like] || 0;
+			const unlikes = reactions[unlike] || 0;
+			const index = $comments.indexOf($el);
 
 			return {
 				$el,
 				likes,
 				unlikes,
 				index
-			}
+			};
 		})
-		.sort((a, b) => b.likes - a.likes)
+		.sort((a, b) => b.likes - a.likes);
 
-	const highestNumber = Math.max(...options.map(option => option.likes))
+	const highestNumber = Math.max(...options.map(option => option.likes));
 
-	function candidate (option: Option) {
-		// is the 5th or later comment (it doesn't make sense to highlight a comment that is right under the opening issue already)
-		const notClose = option.index >= 4
-		// has the most 👍 reactions
-		const mostLikes = option.likes >= highestNumber
-		// has at least 10 👍 reactions (or 👍.count > comments.length * 0.8)
-		const minimum = option.likes >= 10 || option.likes > $comments.length * 0.8
-		// controversial: 👎.count >= 👍.count / 2
-		const controversial = option.unlikes >= (option.likes / 2)
+	function candidate(option: Option): boolean {
+		// Is the 5th or later comment (it doesn't make sense to highlight a comment that is right under the opening issue already)
+		const notClose = option.index >= 4;
+		// Has the most 👍 reactions
+		const mostLikes = option.likes >= highestNumber;
+		// Has at least 10 👍 reactions (or 👍.count > comments.length * 0.8)
+		const minimum = option.likes >= 10 || option.likes > $comments.length * 0.8;
+		// Controversial: 👎.count >= 👍.count / 2
+		const controversial = option.unlikes >= (option.likes / 2);
 
-		return notClose && mostLikes && minimum && !controversial
+		return notClose && mostLikes && minimum && !controversial;
 	}
 
-	const comment = options.find(candidate)
+	const comment = options.find(candidate);
 
 	if (comment && comment.$el) {
-		const $parent = comment.$el.closest('.timeline-comment-group')!
-		const id = $parent.id
-		const username = select('.author', comment.$el)!.innerText
-		const text = select('.comment-body', comment.$el)!.innerText.substring(0, 100)
-		const $avatar = select('img.avatar', $parent)! as HTMLImageElement
-		const avatar = $avatar.src
-		const props: Props = { id, username, text, avatar }
+		const $parent = comment.$el.closest('.timeline-comment-group')!;
+		const {id} = $parent;
+		const username = select('.author', comment.$el)!.innerText;
+		const text = select('.comment-body', comment.$el)!.innerText.substring(0, 100);
+		const $avatar = select('img.avatar', $parent)! as HTMLImageElement;
+		const avatar = $avatar.src;
+		const props: Props = {id, username, text, avatar};
 
-		comment.$el.classList.add('rgh-highest-rated-comment')
-		select('.js-discussion')!.prepend(element(props))
+		comment.$el.classList.add('rgh-highest-rated-comment');
+		select('.js-discussion')!.prepend(element(props));
 	}
 }
 
