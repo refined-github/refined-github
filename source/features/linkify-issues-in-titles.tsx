c2b604613ef2c8e94d96a6fb0ce8a4716d7a8f43
Hardik Modha <hardikmodha22@gmail.com>
2019-06-02 15:18:17 +0530

    Format titles on single issue / PR pages

--- source/features/linkify-issues-in-titles.tsx
+++ source/features/linkify-issues-in-titles.tsx
@@ -1,16 +1,25 @@
 import select from 'select-dom';
+import zipTextNodes from 'zip-text-nodes';
 import features from '../libs/features';
 import observeEl from '../libs/simplified-element-observer';
+import parseBackticks from '../libs/parse-backticks';
 import {linkifyIssuesInDom} from './linkify-urls-in-code';
 
 function init(): void {
 	observeEl(
 		select('#partial-discussion-header')!.parentElement!,
 		() => {
-			const title = select('.js-issue-title:not(.refined-linkified-title)');
-			if (title) {
+			const title = select('.js-issue-title')!;
+
+			if (!title.classList.contains('refined-linkified-title')) {
 				title.classList.add('refined-linkified-title');
 				linkifyIssuesInDom(title);
+			}
+
+			const fragment = parseBackticks(title.textContent!);
+
+			if (fragment.children.length > 0) {
+				zipTextNodes(title, fragment);
 			}
 		});
 }
