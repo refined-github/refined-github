e53dfad221bbb4c3249e0b9799b110a7ea4cb7cf
Laxman Damera <notlmn@outlook.com>
2019-06-04 11:58:58 +0530

    Style inputs using :invalid

--- source/features/limit-commit-title-length.tsx
+++ source/features/limit-commit-title-length.tsx
@@ -1,11 +1,6 @@
-import React from 'dom-chef';
+import './limit-commit-title-length.css';
 import select from 'select-dom';
-import onetime from 'onetime';
 import features from '../libs/features';
-
-const getError = onetime<[], HTMLElement>((): HTMLElement =>
-	<p className="note text-red">Title of commit should be less than 70 characters.</p>
-);
 
 function init(): void {
 	const inputs = select.all<HTMLInputElement>([
@@ -15,20 +10,7 @@ function init(): void {
 	].join(','));
 
 	for (const inputField of inputs) {
-		const validityCallback = (): void => {
-			if (inputField.checkValidity()) {
-				getError().remove();
-				return;
-			}
-
-			inputField.after(getError());
-		};
-
 		inputField.setAttribute('maxlength', '70');
-
-		inputField.addEventListener('input', validityCallback);
-
-		validityCallback();
 	}
 }
 
