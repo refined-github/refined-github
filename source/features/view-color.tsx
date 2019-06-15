b876337462a142a4664b8cbaaddf47b597a32eee
sumanthratna <sumanthratna@gmail.com>
2019-06-15 10:52:42 -0400

    Use color-name-list

--- source/features/view-color.tsx
+++ source/features/view-color.tsx
@@ -1,40 +1,11 @@
 import features from '../libs/features';
+import namedColors from 'color-name-list';
 
 function init(): void {
 	addColors();
 }
-const namedColors: string = [
-	"aliceblue", "antiquewhite", "aqua", "aquamarine", "azure", "beige",
-	"bisque", "black", "blanchedalmond", "blue", "blueviolet", "brown",
-	"burlywood", "cadetblue", "chartreuse", "chocolate", "coral",
-	"cornflowerblue", "cornsilk", "crimson", "cyan", "darkblue", "darkcyan",
-	"darkgoldenrod", "darkgray", "darkgrey", "darkgreen", "darkkhaki",
-	"darkmagenta", "darkolivegreen", "darkorange", "darkorchid", "darkred",
-	"darksalmon", "darkseagreen", "darkslateblue", "darkslategray",
-	"darkslategrey", "darkturquoise", "darkviolet", "deeppink", "deepskyblue",
-	"dimgray", "dimgrey", "dodgerblue", "firebrick", "floralwhite",
-	"forestgreen", "fuchsia", "gainsboro", "ghostwhite", "gold", "goldenrod",
-	"gray", "grey", "green", "greenyellow", "honeydew", "hotpink",
-	"indianred", "indigo", "ivory", "khaki", "lavender", "lavenderblush",
-	"lawngreen", "lemonchiffon", "lightblue", "lightcoral", "lightcyan",
-	"lightgoldenrodyellow", "lightgray", "lightgrey", "lightgreen",
-	"lightpink", "lightsalmon", "lightseagreen", "lightskyblue",
-	"lightslategray", "lightslategrey", "lightsteelblue", "lightyellow",
-	"lime", "limegreen", "linen", "magenta", "maroon", "mediumaquamarine",
-	"mediumblue", "mediumorchid", "mediumpurple", "mediumseagreen",
-	"mediumslateblue", "mediumspringgreen", "mediumturquoise",
-	"mediumvioletred", "midnightblue", "mintcream", "mistyrose", "moccasin",
-	"navajowhite", "navy", "oldlace", "olive", "olivedrab", "orange",
-	"orangered", "orchid", "palegoldenrod", "palegreen", "paleturquoise",
-	"palevioletred", "papayawhip", "peachpuff", "peru", "pink", "plum",
-	"powderblue", "purple", "rebeccapurple", "red", "rosybrown", "royalblue",
-	"saddlebrown", "salmon", "sandybrown", "seagreen", "seashell", "sienna",
-	"silver", "skyblue", "slateblue", "slategray", "slategrey", "snow",
-	"springgreen", "steelblue", "tan", "teal", "thistle", "tomato",
-	"turquoise", "violet", "wheat", "white", "whitesmoke", "yellow",
-	"yellowgreen"
-].join("|");
-const regexNamed: RegExp = new RegExp("^(" + namedColors + ")$", "i");
+const colorNames: string = (namedColors as { name: string, hex: string }[]).map(color => color.name).join("|");
+const regexNamed: RegExp = new RegExp("^(" + colorNames + ")$", "i");
 // Ex: #123, #123456 or 0x123456 (unix style colors, used by three.js)
 const regexHex: RegExp = /^(#|0x)([0-9A-F]{6,8}|[0-9A-F]{3,4})$/i;
 // Ex: rgb(0,0,0) or rgba(0,0,0,0.2)
@@ -53,8 +24,8 @@ const regexPercent: RegExp = /%%/g;
 const block: HTMLElement = document.createElement("span");
 block.className = "rgh-color-block";
 
-function addNode(el: HTMLElement, val: string): void {
-	const node: HTMLElement = block.cloneNode();
+function addNode(el: Node, val: string): void {
+	const node: Node = block.cloneNode();
 	node.style.backgroundColor = val;
 	// Don't add node if color is invalid
 	if (node.style.backgroundColor !== "") {
@@ -62,11 +33,11 @@ function addNode(el: HTMLElement, val: string): void {
 	}
 }
 
-function getTextContent(el: HTMLElement): string | null {
+function getTextContent(el: Node): string | null {
 	return el ? el.textContent : "";
 }
 
-function rgb(els: Array<HTMLElement> | null, el: HTMLElement, txt: string | null): string {
+function rgb(els: Array<Node> | null, el: Node, txt: string | null): string {
 	// Color in a string contains everything
 	if (el.classList.contains("pl-s")) {
 		txt = txt.match(regexRGB)[0];
@@ -84,7 +55,7 @@ function rgb(els: Array<HTMLElement> | null, el: HTMLElement, txt: string | null
 	return els;
 }
 
-function hsl(els: Array<HTMLElement>, el: HTMLElement, txt: string) {
+function hsl(els: Array<Node>, el: Node, txt: string) {
 	const tmp: RegExp = /a$/i.test(txt);
 	if (el.classList.contains("pl-s")) {
 		// Color in a string contains everything
@@ -108,10 +79,10 @@ function hsl(els: Array<HTMLElement>, el: HTMLElement, txt: string) {
 }
 
 // Loop with delay to allow user interaction
-function* addBlock(els: Array<HTMLElement>) {
+function* addBlock(els: Array<Node>) {
 	let last: string = "";
 	while (els.length) {
-		let el: HTMLElement = els.shift();
+		let el: Node = els.shift();
 		let txt: string = el.textContent;
 		if (
 			// No swatch for JavaScript Math.tan
@@ -141,8 +112,8 @@ function addColors(): void {
 	if (document.querySelector(".highlight")) {
 		let status;
 		// .pl-c1 targets css hex colors, "rgb" and "hsl"
-		const els: Array<HTMLElement> = [...document.querySelectorAll(".pl-c1, .pl-s")];
-		const iter: IterableIterator<HTMLElement> = addBlock(els);
+		const els: Array<Node> = [...document.querySelectorAll(".pl-c1, .pl-s")];
+		const iter: IterableIterator<Node> = addBlock(els);
 		const loop: () => void = () => {
 			for (let i = 0; i < 40; i++) {
 				status = iter.next();
