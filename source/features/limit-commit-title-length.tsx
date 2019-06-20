dc59a585159b1a985795993f0cb8a049402a19b2
Laxman Damera <notlmn@outlook.com>
2019-06-20 13:33:19 +0530

    Lint fix

--- source/features/limit-commit-title-length.tsx
+++ source/features/limit-commit-title-length.tsx
@@ -2,16 +2,14 @@ import './limit-commit-title-length.css';
 import select from 'select-dom';
 import features from '../libs/features';
 
-
 function init(): void {
 	const inputs = select.all<HTMLInputElement>([
 		'#commit-summary-input', // Commit title on edit file page
 		'#merge_title_field' // PR merge message field
 	].join(','));
 
-
 	for (const inputField of inputs) {
-		function validityCallback(event: Event) {
+		const validityCallback = (event: Event): void => {
 			if (inputField.value.length > 72) {
 				inputField.setCustomValidity(`The title should be maximum 72 characters, but is ${inputField.value.length}`);
 				event.preventDefault();
@@ -19,7 +17,7 @@ function init(): void {
 			} else {
 				inputField.setCustomValidity('');
 			}
-		}
+		};
 
 		inputField.addEventListener('input', validityCallback);
 		inputField.form!.addEventListener('submit', validityCallback);
