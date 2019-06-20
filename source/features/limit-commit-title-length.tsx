749643fe6ef5a217033d3ea4fa0adcb66ee3d106
Laxman Damera <notlmn@outlook.com>
2019-06-20 13:28:04 +0530

    Drop check on PR title, validate on form submission

--- source/features/limit-commit-title-length.tsx
+++ source/features/limit-commit-title-length.tsx
@@ -2,38 +2,37 @@ import './limit-commit-title-length.css';
 import select from 'select-dom';
 import features from '../libs/features';
 
-const commitTitleLimit = 72;
 
 function init(): void {
 	const inputs = select.all<HTMLInputElement>([
 		'#commit-summary-input', // Commit title on edit file page
-		'#pull_request_title', // PR title while creating PR
 		'#merge_title_field' // PR merge message field
 	].join(','));
 
+
 	for (const inputField of inputs) {
-		inputField.setAttribute('maxlength', commitTitleLimit.toString());
-		inputField.addEventListener('input', () => {
-			const inputValueLength = inputField.value.length;
-			let customValidityMessage = '';
-
-			if (inputValueLength > commitTitleLimit) {
-				customValidityMessage = `The title should not be longer than 72 characters, but is currently ${inputValueLength} characters`;
+		function validityCallback(event: Event) {
+			if (inputField.value.length > 72) {
+				inputField.setCustomValidity(`The title should be maximum 72 characters, but is ${inputField.value.length}`);
+				event.preventDefault();
+				event.stopImmediatePropagation();
+			} else {
+				inputField.setCustomValidity('');
 			}
+		}
 
-			inputField.setCustomValidity(customValidityMessage);
-		});
+		inputField.addEventListener('input', validityCallback);
+		inputField.form!.addEventListener('submit', validityCallback);
 	}
 }
 
 features.add({
 	id: 'limit-commit-title-length',
-	description: 'Limits the length of commit fields to 70 characters',
+	description: 'Limits the length of commit fields to 72 characters',
 	init,
 	load: features.onAjaxedPages,
 	include: [
 		features.isPRConversation,
-		features.isCompare,
 		features.isEditingFile
 	]
 });
