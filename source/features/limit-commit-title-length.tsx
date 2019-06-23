7bd97575d060647dc669869ef56f620c4dfda8a6
Laxman <notlmn@outlook.com>
2019-06-23 13:29:24 +0530

    Drop loops, only one element exists on the page at any point of time

--- source/features/limit-commit-title-length.tsx
+++ source/features/limit-commit-title-length.tsx
@@ -4,23 +4,19 @@ import features from '../libs/features';
 import onPrMergePanelOpen from '../libs/on-pr-merge-panel-open';
 
 function init(): void {
-	const inputs = select.all<HTMLInputElement>([
+	const inputField = select<HTMLInputElement>([
 		'#commit-summary-input', // Commit title on edit file page
 		'#merge_title_field' // PR merge message field
 	].join(','));
 
-	for (const inputField of inputs) {
-		inputField.addEventListener('input', () => {
-			inputField.setCustomValidity(inputField.value.length > 72 ? `The title should be maximum 72 characters, but is ${inputField.value.length}` : '');
-		});
-	}
+	inputField.addEventListener('input', () => {
+		inputField.setCustomValidity(inputField.value.length > 72 ? `The title should be maximum 72 characters, but is ${inputField.value.length}` : '');
+	});
 
 	// For PR merges, GitHub restores any saved commit messages on page load
 	// Triggering input event for these fields immediately validates the form
 	onPrMergePanelOpen(() => {
-		for (const inputField of inputs) {
-			inputField.dispatchEvent(new Event('input'));
-		}
+		inputField.dispatchEvent(new Event('input'));
 	});
 }
 
