7c16b4e530e0e29ba4c87bceb327e542cf5383ac
Federico Brigante <github@bfred.it>
2019-06-28 21:19:02 +0800

    Slight code improvements

--- source/features/submit-review-as-single-comment.tsx
+++ source/features/submit-review-as-single-comment.tsx
@@ -14,72 +14,73 @@ const getButton = onetime(() => (
 	<button className="rgh-submit-single btn-link ml-2 text-gray text-small" type="button">Send now</button>
 ));
 
-async function handleReviewSubmission(event: DelegateEvent): Promise<void> {
-	const container = event.delegateTarget.closest('.line-comments')!;
-
-	await observeOneMutation(container, {
-		childList: true,
-		subtree: true
-	});
-
-	const pendingLabel = select(pendingSelector, container);
-	if (!pendingLabel) {
-		return; // "Add single comment" was clicked
-	}
-
+function updateUI(): void {
 	// The feature only works with one comment
-	if (select.all(pendingSelector).length === 1) {
-		pendingLabel.after(getButton());
+	const labels = select.all(pendingSelector);
+	if (labels.length === 1) {
+		labels[0].after(getButton());
 	} else {
 		getButton().remove();
 	}
 }
 
-async function sendNow(commentContainer: Element, delegateTarget: Element, commentText: string): Promise<void> {
-	const newCommentContainer = delegateTarget.closest('.js-resolvable-thread-contents')!;
-	const isReplyingToExistingThread = commentContainer.closest('.js-comments-holder')!.childElementCount > 1;
-	const lineBeingCommentedOn = newCommentContainer.closest('tr')!.previousElementSibling!;
-	const isRightSide = commentContainer.closest('.js-addition');
+async function handleReviewSubmission(event: DelegateEvent): Promise<void> {
+	const container = event.delegateTarget.closest('.line-comments')!;
+	await observeOneMutation(container);
+	if (select.exists(pendingSelector, container)) {
+		updateUI();
+	}
+	// If no label is found, "Add single comment" was clicked
+}
 
+async function sendNow(commentContainer: Element, commentText: string): Promise<void> {
+	// The following values need to be found *before* deleteLink.click()
+	const isReplyingToExistingThread = commentContainer.closest('.js-comments-holder')!.childElementCount > 1;
+	const isRightSide = commentContainer.closest('.js-addition');
+	const newCommentContainer = commentContainer.closest('.js-resolvable-thread-contents')!;
+	const lineBeingCommentedOn = commentContainer.closest('tr')!.previousElementSibling!; // The comments are in a <tr> right after the code
+	let formHolder;
+
+	// Delete comment without asking confirmation
 	const deleteLink = select<HTMLButtonElement>('[aria-label="Delete comment"]', commentContainer)!;
 	deleteLink.removeAttribute('data-confirm');
 	deleteLink.click();
-	let formHolder;
 
+	// Wait for the comment to be removed
+	await observeOneMutation(lineBeingCommentedOn.parentElement!);
+
+	// Use nearby comment box. "Open" it to make it functional but keep it hidden from the user
 	if (isReplyingToExistingThread) {
-		// Only one comment is being removed
-		await observeOneMutation(commentContainer.parentElement!);
-
-		// Open reply box, but keep it hidden
 		select('.review-thread-reply-button', newCommentContainer)!.click();
 		select('.open', newCommentContainer)!.classList.remove('open');
 		formHolder = newCommentContainer;
 	} else {
-		// The whole thread is being removed
-		await observeOneMutation(lineBeingCommentedOn.parentElement!);
+		// The whole comment area may have been removed.
 
-		// Open reply box, but keep it hidden
+		// Comment box is focused after being inserted/shown. This event helps us find its position in the dom
 		const listener = oneEvent(lineBeingCommentedOn.parentElement!, 'focusin');
 		(isRightSide ? select.last : select)<HTMLButtonElement>('.js-add-line-comment', lineBeingCommentedOn)!.click();
+
+		// Hide comment box
 		const {target} = await listener;
 		formHolder = (target as HTMLTextAreaElement).closest('.inline-comment-form-container') as HTMLElement;
 		formHolder.hidden = true;
 	}
 
+	// Add comment to new comment box
 	insertText(
 		select<HTMLTextAreaElement>('[name="comment[body]"]', formHolder)!,
 		commentText
 	);
+
 	const submitButton = select<HTMLButtonElement>('[name="single_comment"]', formHolder)!;
 	submitButton.disabled = false; // This should be enabled by GitHub, but sometimes the UI doesn't update in time
 	submitButton.click();
 }
 
-async function handleSubmitSingle({delegateTarget}: DelegateEvent): Promise<void> {
-	const commentContainer = delegateTarget.closest('.js-comment')!;
-
+async function handleSubmitSingle(event: DelegateEvent): Promise<void> {
+	const commentContainer = event.delegateTarget.closest('.js-comment')!;
 	const commentText = select<HTMLTextAreaElement>('[name="pull_request_review_comment[body]"]', commentContainer)!.value;
-
 	if (!commentText) {
 		reportBug(__featureName__, 'comment not found');
 		return;
@@ -88,8 +89,9 @@ async function handleSubmitSingle({delegateTarget}: DelegateEvent): Promise<void
 	// Place comment in console for safety
 	console.log(`Refined GitHub: \`${__featureName__}\` sending this comment:`);
 	console.log(commentText);
+
 	try {
-		await sendNow(commentContainer, delegateTarget, commentText);
+		await sendNow(commentContainer, commentText);
 	} catch (error) {
 		alert('There was an error sending the comment. If it was already deleted, you can find it in the browser console.');
 		reportBug(__featureName__, error.message);
@@ -98,24 +100,9 @@ async function handleSubmitSingle({delegateTarget}: DelegateEvent): Promise<void
 }
 
 function init(): void {
-	delegate(
-		'#files',
-		// TODO: change to `submit` event
-		'[action$="/review_comment/create"]',
-		'submit',
-		handleReviewSubmission
-	);
-	delegate(
-		'#files',
-		'.rgh-submit-single',
-		'click',
-		handleSubmitSingle
-	);
-
-	const labels = select.all(pendingSelector);
-	if (labels.length === 1) {
-		labels[0].after(getButton());
-	}
+	delegate('#files', '[action$="/review_comment/create"]', 'submit', handleReviewSubmission);
+	delegate('#files', '.rgh-submit-single', 'click', handleSubmitSingle);
+	updateUI();
 }
 
 features.add({
