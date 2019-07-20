119e2c16ef8010c6710aaddc9003a2c1c27abaad
Federico Brigante <github@bfred.it>
2019-07-20 17:16:09 +0800

    Keep `init` at the bottom

--- source/features/show-names.tsx
+++ source/features/show-names.tsx
@@ -19,15 +19,6 @@ async function updateAndWatch([firstChange]: MutationRecord[], observer: Mutatio
 	}
 }
 
-async function init(): Promise<false | void> {
-	if (features.isDashboard()) {
-		observeEl('.news', updateAndWatch);
-	} else {
-		const hasUsernames = await fetchAndAppendUsernames(); // This is called by observeEl, so it needs to be inside `else`
-		return hasUsernames;
-	}
-}
-
 async function fetchAndAppendUsernames(): Promise<false | void> {
 	const usernameElements = select.all([
 		'.js-discussion a.author:not(.rgh-fullname):not([href*="/apps/"]):not([href*="/marketplace/"]):not([data-hovercard-type="organization"])', // `a` selector needed to skip commits by non-GitHub users.
@@ -80,6 +71,15 @@ async function fetchAndAppendUsernames(): Promise<false | void> {
 				') '
 			);
 		}
+	}
+}
+
+async function init(): Promise<false | void> {
+	if (features.isDashboard()) {
+		observeEl('.news', updateAndWatch);
+	} else {
+		const hasUsernames = await fetchAndAppendUsernames();
+		return hasUsernames;
 	}
 }
 
