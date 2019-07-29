a1a7442a724499167ceee80c171a498c96bbd813
Federico Brigante <github@bfred.it>
2019-07-29 17:36:28 +0700

    Fix `edit-files-faster` feature (#2279)
    * Fix `edit-files-faster` feature
    
    * Lint

--- source/features/edit-files-faster.tsx
+++ source/features/edit-files-faster.tsx
@@ -4,8 +4,10 @@ import select from 'select-dom';
 import features from '../libs/features';
 import * as icons from '../libs/icons';
 import {wrap} from '../libs/dom-utils';
+import getDefaultBranch from '../libs/get-default-branch';
 
-function init(): void {
+async function init(): Promise<void> {
+	const defaultBranch = await getDefaultBranch();
 	for (const fileIcon of select.all('.files :not(a) > .octicon-file')) {
 		const pathnameParts = fileIcon
 			.closest('tr')!
@@ -14,6 +16,11 @@ function init(): void {
 			.split('/');
 
 		pathnameParts[3] = 'edit'; // Replaces `/blob/`
+
+		const isPermalink = /Tag|Tree/.test(select('.branch-select-menu i')!.textContent!);
+		if (isPermalink) {
+			pathnameParts[4] = defaultBranch; // Replaces /${tag|commit}/
+		}
 
 		wrap(fileIcon, <a href={pathnameParts.join('/')} className="rgh-edit-files-faster" />);
 		fileIcon.after(icons.edit());
