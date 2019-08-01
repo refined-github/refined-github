860015612a57c4eec0e8aa054d1a6b5c31619427
Federico Brigante <opensource@bfred.it>
2019-08-01 20:24:02 +0700

    Fix form submission
    I don't know how it worked before... maybe GitHub just made it mandatory

--- source/libs/post-form.ts
+++ source/libs/post-form.ts
@@ -2,7 +2,10 @@ export default async function postForm(form: HTMLFormElement): Promise<Response>
 	const response = await fetch(form.action, {
 		// `as` required until https://github.com/microsoft/TSJS-lib-generator/issues/741
 		body: new URLSearchParams(new FormData(form) as URLSearchParams),
-		method: 'POST'
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+		}
 	});
 
 	if (!response.ok) {
