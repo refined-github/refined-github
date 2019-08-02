refined-github/refined-github

commit 3fd193339a1b7869e4f43acba48f59907a766cff

Update username getter
Following #2193

source/features/minimize-user-comments.tsx
@@ -10,6 +10,10 @@ import anchorScroll from '../libs/anchor-scroll';
 
 const getLabel = (restore: boolean): string => `${restore ? 'Restore' : 'Minimize'} user’s comments`;
 
+function getUsernameFromComment(comment: Element): string {
+	return select<HTMLAnchorElement>('.author', comment)!.pathname.slice(1);
+}
+
 async function getMinimizedUsers(): Promise<string[]> {
 	return (await optionsStorage.getAll()).minimizedUsers.trim().split(/\s+/);
 }
@@ -30,7 +34,7 @@ function toggleComment(comment: HTMLElement, minimize: boolean): void {
 
 async function onButtonClick(event: React.MouseEvent<HTMLButtonElement>): Promise<void> {
 	const comment = event.currentTarget.closest('.js-targetable-comment')!;
-	const user = select('.author', comment)!.textContent!;
+	const user = getUsernameFromComment(comment);
 
 	let minimizedUsers = await getMinimizedUsers();
 	if (minimizedUsers.includes(user)) {
@@ -45,11 +49,7 @@ async function onButtonClick(event: React.MouseEvent<HTMLButtonElement>): Promis
 
 async function handleMenuOpening(event: DelegateEvent): Promise<void> {
 	const dropdown = event.delegateTarget.nextElementSibling!;
-	const user = dropdown
-		.closest('.js-targetable-comment')!
-		.querySelector('.author')!
-		.textContent!;
-
+	const user = getUsernameFromComment(dropdown.closest('.js-targetable-comment')!);
 	if (user === getUsername()) {
 		return;
 	}
@@ -79,8 +79,9 @@ async function minimizeMutedUserComments(): Promise<void> {
 	const minimizedUsers = await getMinimizedUsers();
 
 	for (const comment of select.all('.js-discussion .js-minimizable-comment-group')) {
-		const user = select('.author', comment)!.textContent!;
-		toggleComment(comment, minimizedUsers.includes(user));
+		if (minimizedUsers.includes(getUsernameFromComment(comment))) {
+			toggleComment(comment, true);
+		}
 	}
 }
 

commit eb02e48168c20238e10a110d9bea532968af5e5f

Restore "Restore user" button
Mistakenly broken by 3fd1933

source/features/minimize-user-comments.tsx
@@ -73,6 +73,7 @@ async function handleMenuOpening(event: DelegateEvent): Promise<void> {
 			{getLabel(shouldMinimizeComment)}
 		</button>
 	);
+
 	// The new button's label is too long for the dropdown
 	dropdown.style.width = '205px';
 }
@@ -81,9 +82,8 @@ async function minimizeMutedUserComments(): Promise<void> {
 	const minimizedUsers = await getMinimizedUsers();
 
 	for (const comment of select.all('.js-discussion .js-minimizable-comment-group')) {
-		if (minimizedUsers.includes(getUsernameFromComment(comment))) {
-			toggleComment(comment, true);
-		}
+		const user = getUsernameFromComment(comment);
+		toggleComment(comment, minimizedUsers.includes(user));
 	}
 }
 