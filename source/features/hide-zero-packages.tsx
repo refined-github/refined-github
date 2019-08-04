b5c1ea7c2059a1a3f25fe0e1ac924744dd29bd55
Han <yahyafarhan48@gmail.com>
2019-08-04 07:41:01 +0000

    screenshot added and 0 match updated

--- source/features/hide-zero-packages.tsx
+++ source/features/hide-zero-packages.tsx
@@ -4,15 +4,15 @@ import {getRepoURL} from '../libs/utils';
 
 function init(): void {
 	const packageElem: HTMLElement | null = select(`.numbers-summary a[href='/${getRepoURL()}/packages']`);
-	if (packageElem && packageElem.innerText.trim() === '0 packages') {
+	if (packageElem && packageElem.textContent!.trim().startsWith('0')) {
 		packageElem.parentElement!.remove();
 	}
 }
 
 features.add({
 	id: __featureName__,
-	description: 'Hides the `Packages` tab in repositories if it\'s empty.',
-	screenshot: false,
+	description: 'Hides the `Packages` tab in repositories if it’s empty.',
+	screenshot: 'https://user-images.githubusercontent.com/35382021/62420784-a6652f00-b687-11e9-82a5-eaf6a636f218.png',
 	include: [
 		features.isRepoRoot
 	],
