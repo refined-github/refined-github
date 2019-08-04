3e61a15435da85ef2d21346e04ad670eba5cdeb3
Han <yahyafarhan48@gmail.com>
2019-08-04 00:54:06 +0000

    code refined and description made clear

--- source/features/remove-zero-packages.tsx
+++ source/features/remove-zero-packages.tsx
@@ -4,17 +4,15 @@ import {getRepoURL} from '../libs/utils';
 
 function init(): void {
 	const packageElem: HTMLElement | null = select(`.numbers-summary a[href='/${getRepoURL()}/packages']`);
-	if (packageElem && packageElem.innerText.trim().startsWith('0')) {
+	if (packageElem && packageElem.innerText.trim() === '0 packages') {
 		const parentElem = packageElem.parentElement;
-		if (parentElem) {
-			parentElem.remove();
-		}
+		parentElem!.remove();
 	}
 }
 
 features.add({
 	id: __featureName__,
-	description: 'Removing 0 package',
+	description: 'Hides the packages tab, when no packages exist',
 	screenshot: false,
 	include: [
 		features.isRepo
