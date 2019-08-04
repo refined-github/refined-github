77acde61bd5e0f490e2a4184b9c1c61f99b40a5a
Han <yahyafarhan48@gmail.com>
2019-08-04 16:35:57 +0000

    screenshot added and reference to elem updated

--- source/features/hide-zero-packages.tsx
+++ source/features/hide-zero-packages.tsx
@@ -1,18 +1,17 @@
 import select from 'select-dom';
 import features from '../libs/features';
-import {getRepoURL} from '../libs/utils';
 
 function init(): void {
-	const packageElem: HTMLElement | null = select(`.numbers-summary a[href='/${getRepoURL()}/packages']`);
-	if (packageElem && packageElem.textContent!.trim().startsWith('0')) {
-		packageElem.parentElement!.remove();
+	const packagesLabel = select('.numbers-summary [href$="/packages"]');
+	if (packagesLabel && packagesLabel.textContent!.trim().startsWith('0')) {
+		packagesLabel.parentElement!.remove();
 	}
 }
 
 features.add({
 	id: __featureName__,
 	description: 'Hides the `Packages` tab in repositories if it’s empty.',
-	screenshot: 'https://user-images.githubusercontent.com/35382021/62420784-a6652f00-b687-11e9-82a5-eaf6a636f218.png',
+	screenshot: 'https://user-images.githubusercontent.com/35382021/62426530-688ef780-b6d5-11e9-93f2-515110aed1eb.jpg',
 	include: [
 		features.isRepoRoot
 	],
