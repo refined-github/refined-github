33c3d1101b46a24d5b5d7cc959fdae36b26b8ca5
Han <yahyafarhan48@gmail.com>
2019-08-04 07:18:06 +0000

    changed description and feature scope

--- source/features/remove-zero-packages.tsx
+++ source/features/remove-zero-packages.tsx
@@ -5,17 +5,16 @@ import {getRepoURL} from '../libs/utils';
 function init(): void {
 	const packageElem: HTMLElement | null = select(`.numbers-summary a[href='/${getRepoURL()}/packages']`);
 	if (packageElem && packageElem.innerText.trim() === '0 packages') {
-		const parentElem = packageElem.parentElement;
-		parentElem!.remove();
+		packageElem.parentElement!.remove();
 	}
 }
 
 features.add({
 	id: __featureName__,
-	description: 'Hides the packages tab, when no packages exist',
+	description: 'Hides the `Packages` tab in repositories if it\'s empty.',
 	screenshot: false,
 	include: [
-		features.isRepo
+		features.isRepoRoot
 	],
 	load: features.onAjaxedPages,
 	init
