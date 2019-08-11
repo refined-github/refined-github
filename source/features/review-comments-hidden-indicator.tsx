6858c4acd7a0fa33acdd266bd9217b6b44bff9a5
Sid Vishnoi <sidvishnoi8@gmail.com>
2019-08-11 20:34:27 +0530

    lint fix

--- source/features/review-comments-hidden-indicator.tsx
+++ source/features/review-comments-hidden-indicator.tsx
@@ -3,25 +3,27 @@ import React from 'dom-chef';
 import select from 'select-dom';
 import features from '../libs/features';
 import observeEl from '../libs/simplified-element-observer';
-import { comment as commentIcon } from '../libs/icons';
+import {comment as commentIcon} from '../libs/icons';
 
 const SELECTOR_COMMENT_CONTAINER = 'tr.inline-comments';
 const SELECTOR_CUSTOM_TOGGLE = 'tr.refined-toggle-comments';
 const SELECTOR_COMMENT = '.review-comment .js-comment';
 
-let isVisible = true; // are comments visible?
+let isVisible = true; // Are comments visible?
 
-const getOffsetY = (el: Element) =>
+const getOffsetY = (el: Element): number =>
 	el.getBoundingClientRect().top + window.pageYOffset;
 
-// toggle comments while maintaining scroll position
-const toggleComments = (target: HTMLElement) => {
+// Toggle comments while maintaining scroll position
+const toggleComments = (target: HTMLElement): void => {
 	const container = target.closest(SELECTOR_CUSTOM_TOGGLE)!;
 	const scrollOld = window.scrollY;
 	const offsetOld = getOffsetY(container);
 
 	select.all<HTMLInputElement>('input.js-toggle-file-notes').forEach(input => {
-		if (isVisible === input.checked) input.click();
+		if (isVisible === input.checked) {
+			input.click();
+		}
 	});
 	isVisible = !isVisible;
 
@@ -30,7 +32,7 @@ const toggleComments = (target: HTMLElement) => {
 	window.scrollTo(0, scrollY);
 };
 
-const removeToggles = () => {
+const removeToggles = (): void => {
 	const toRemove = select.all(SELECTOR_CUSTOM_TOGGLE).filter(el => {
 		const next = el.nextElementSibling;
 		return !next || !next.matches(SELECTOR_COMMENT_CONTAINER);
@@ -38,27 +40,34 @@ const removeToggles = () => {
 	toRemove.forEach(el => el.remove());
 };
 
-// watch for comment addition/deletions and update comment count
-const countListener = (mutations: MutationRecord[]) => {
+// Watch for comment addition/deletions and update comment count
+const countListener = (mutations: MutationRecord[]): void => {
 	const selectors = 'td.line-comments, .review-comment, .js-comments-holder';
 	for (const mutation of mutations) {
-		if (mutation.target.nodeType !== 1) continue;
+		if (mutation.target.nodeType !== 1) {
+			continue;
+		}
+
 		const target = mutation.target as HTMLElement;
-		if (!target.matches(selectors)) continue;
+		if (!target.matches(selectors)) {
+			continue;
+		}
+
 		const container = (mutation.target as HTMLElement).closest('tr')!;
 		const toggler = container.previousElementSibling;
 		if (toggler && toggler.matches(SELECTOR_CUSTOM_TOGGLE)) {
 			const comments = select.all(SELECTOR_COMMENT, container);
 			const counter = select('span', toggler)!;
 			counter.textContent = comments.length.toString();
-			return;
 		} else {
 			removeToggles();
 		}
+
+		break;
 	}
 };
 
-const CommentToggle = (count: number) => (
+const commentToggle = (count: number): JSX.Element => (
 	<tr className="refined-toggle-comments">
 		<td className="blob-num" colSpan={2}>
 			<button onClick={ev => toggleComments(ev.target as HTMLElement)}>
@@ -69,24 +78,33 @@ const CommentToggle = (count: number) => (
 	</tr>
 );
 
-const addToggle = (container: HTMLElement) => {
+const addToggle = (container: HTMLElement): void => {
 	const commentCount = select.all(SELECTOR_COMMENT, container).length;
-	if (!commentCount) return;
-	container.before(CommentToggle(commentCount));
-	observeEl(container, countListener, { childList: true, subtree: true });
+	if (!commentCount) {
+		return;
+	}
+
+	container.before(commentToggle(commentCount));
+	observeEl(container, countListener, {childList: true, subtree: true});
 };
 
-function init() {
+function init(): void {
 	select.all(SELECTOR_COMMENT_CONTAINER).forEach(el => addToggle(el));
 
-	// watching for addition/deletion of comment containers
-	const onTableMutation = (mutations: MutationRecord[]) => {
+	// Watch for addition/deletion of comment containers
+	const onTableMutation = (mutations: MutationRecord[]): void => {
 		for (const mutation of mutations) {
-			if (mutation.target.nodeType !== 1) continue;
+			if (mutation.target.nodeType !== 1) {
+				continue;
+			}
+
 			const target = mutation.target as HTMLElement;
-			if (!target.matches('td.line-comments')) continue;
+			if (!target.matches('td.line-comments')) {
+				continue;
+			}
+
 			const container = target.closest(
-				SELECTOR_COMMENT_CONTAINER,
+				SELECTOR_COMMENT_CONTAINER
 			) as HTMLElement;
 			const comments = select.all(SELECTOR_COMMENT, container);
 			if (comments.length === 0) {
@@ -96,8 +114,9 @@ function init() {
 			}
 		}
 	};
+
 	select.all('.diff-table tbody').forEach(tbody => {
-		observeEl(tbody, onTableMutation, { childList: true, subtree: true });
+		observeEl(tbody, onTableMutation, {childList: true, subtree: true});
 	});
 }
 
@@ -108,5 +127,5 @@ features.add({
 		'https://user-images.githubusercontent.com/1402241/35480123-68b9af1a-043a-11e8-8934-3ead3cff8328.gif', // TODO
 	include: [features.isPRFiles],
 	load: features.onAjaxedPages,
-	init,
+	init
 });
