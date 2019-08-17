30bd37b78c7c6563f9b28203f6cd9f2f6c9b0006
Kid <kidonng@gmail.com>
2019-08-17 22:06:18 +0800

    rebuild the feature

--- source/features/embed-gist-via-iframe.tsx
+++ source/features/embed-gist-via-iframe.tsx
@@ -1,34 +1,34 @@
-import React from 'dom-chef';
 import select from 'select-dom';
 import features from '../libs/features';
 
 function init(): void {
-	const embedMenuItem = select('.select-menu-item')!;
-	select('.select-menu-button')!.textContent = 'Embed via <script>';
-	select('.select-menu-item-heading', embedMenuItem)!.innerHTML = 'Embed via <code>&lt;script&gt;</code>';
-	select('.description', embedMenuItem)!.innerHTML = 'Embed this gist in your website via <code>&lt;script&gt;</code>.';
+	const embedViaScript = select('.select-menu-item')!;
+	const embedViaIframe = embedViaScript.cloneNode(true) as HTMLElement;
 
-	const iframe = `<iframe src="https://gist.github.com${location.pathname}.pibb"></iframe>`;
-	embedMenuItem.insertAdjacentElement(
-		'afterend',
-		<button name="button" type="button" className="select-menu-item width-full" aria-checked="false" role="menuitemradio" value={iframe}>
-			<svg className="octicon octicon-check select-menu-item-icon" viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5L12 5z" /></svg>
-			<div className="select-menu-item-text">
-				<span className="select-menu-item-heading" data-menu-button-text>
-					Embed via <code>&lt;iframe&gt;</code>
-				</span>
-				<span className="description">
-					Embed this gist in your website via <code>&lt;iframe&gt;</code>.
-				</span>
-			</div>
-		</button>
-	);
+	// Modify description to distinguish the two items
+	// This is also consistent with the existing "Clone via HTTPS" and "Clone via SSH" items
+	const viaScript = 'Embed via <script>';
+	select('[data-menu-button]')!.textContent = viaScript;
+	select('.select-menu-item-heading', embedViaScript)!.textContent = viaScript;
+	select('.description', embedViaScript)!.textContent = 'Embed this gist in your website via <script>.';
+
+	// Remove analytics attributes
+	embedViaIframe.removeAttribute('data-hydro-click');
+	embedViaIframe.removeAttribute('data-hydro-click-hmac');
+
+	// Set required content
+	embedViaIframe.setAttribute('aria-checked', 'false');
+	embedViaIframe.setAttribute('value', embedViaIframe.getAttribute('value')!.replace(/script/g, 'iframe').replace('.js', '.pibb'));
+	select('.select-menu-item-heading', embedViaIframe)!.textContent = 'Embed via <iframe>';
+	select('.description', embedViaIframe)!.textContent = 'Embed this gist in your website via <iframe>.';
+
+	embedViaScript.after(embedViaIframe);
 }
 
 features.add({
 	id: __featureName__,
-	description: 'Adds an option to embed gists via <iframe>.',
-	screenshot: 'https://user-images.githubusercontent.com/44045911/63209089-320e9f00-c10f-11e9-8878-4afbbff9ee83.png',
+	description: 'Adds a menu item to embed a gist via <iframe>.',
+	screenshot: 'https://user-images.githubusercontent.com/44045911/63212711-30f36700-c13b-11e9-945e-3b7946156fa8.png',
 	include: [
 		features.isGist
 	],
