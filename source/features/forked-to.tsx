f787a46bf1dcd3cbe9ab91f5db0fe89a481b4e7d
Jeroen van Warmerdam <jeronevw@hotmail.com>
2019-08-17 20:00:28 +0200

    Restore design 1

--- source/features/forked-to.tsx
+++ source/features/forked-to.tsx
@@ -7,6 +7,7 @@ import onetime from 'onetime';
 import features from '../libs/features';
 import {isRepoWithAccess} from '../libs/page-detect';
 import {getRepoURL, getUsername} from '../libs/utils';
+import * as icons from '../libs/icons';
 
 const getCacheKey = onetime((): string => `forked-to:${getUsername()}@${findForkedRepo() || getRepoURL()}`);
 
@@ -52,141 +53,49 @@ async function updateForks(forks: string[]): Promise<void> {
 	}
 }
 
-async function redesignForkDialog(this: HTMLElement): Promise<void> {
-	const forkDialog = select('details-dialog[src*="/fork"]')!;
-	console.log(forkDialog);
-
-	const detailsDialog = <details-menu
-		                      style={{ zIndex: 99 }}
-		                      className="select-menu-modal position-absolute right-0 mt-5">
-	                      </details-menu>;
-
-	const forks = (select.all('form button[aria-label^="View fork"]', forkDialog) || []).map(element => {
-		var fork = element.parentNode!.cloneNode(true) as HTMLElement;
-		fork.firstElementChild!.className = 'select-menu-item width-full'; // form>button
-		fork.firstElementChild!.firstElementChild!.className = 'select-menu-item-gravatar select-menu-item-icon'; // form>button>div
-		fork.firstElementChild!.firstElementChild!.after(
-			<span className="select-menu-item-text">
-				{fork.firstElementChild!.firstElementChild!.firstElementChild!.nextSibling! /* form>button>div>img+textnode*/}
-			</span>
-		);
-		return fork;
-	});
-	if (forks.length > 0) {
-		detailsDialog.append(
-			<div className="select-menu-header">
-				<span className="select-menu-title">Your forks</span>
-			</div>
-		);
-		detailsDialog.append(...forks);
-	}
-
-
-	const toBeForked = (select.all('form button[aria-label^="Will be created"]', forkDialog) || []).map(element => {
-		var fork = element.parentNode!.cloneNode(true) as HTMLElement;
-		fork.firstElementChild!.className = 'select-menu-item width-full'; // form>button
-		fork.firstElementChild!.firstElementChild!.className = 'select-menu-item-gravatar select-menu-item-icon'; // form>button>div
-		fork.firstElementChild!.firstElementChild!.after(
-			<span className="select-menu-item-text">
-				{fork.firstElementChild!.firstElementChild!.firstElementChild!.nextSibling! /* form>button>div>img+textnode*/}
-			</span>
-		);
-		return fork;
-
-		/*
-		ORIGINAL HTML:
-			<form class="button_to" method="post" action="/sindresorhus/refined-github/fork">
-				<button type="submit" name="organization" value="organization" tabindex="0"
-					class="btn-link f5 text-bold my-2 tooltipped tooltipped-se tooltipped-align-left-1" aria-label="Will be created as organization/refined-github." title="@organization">
-					<div class="d-flex flex-items-center">
-						<img class="avatar avatar-small mr-2" src="https://avatars2.githubusercontent.com/u/50828444?s=60&amp;v=4" alt="@organization" width="30" height="30">
-						organization
-					</div>
-				</button>
-				<input type="hidden" name="authenticity_token" value="sidifhfhsnklfdhvskdlfhkgdhgk">
-			</form>
-
-		NEEDED HTML:
-			<form method="post" action={fork.parentNode.href}>
-				<button type="submit" name="organization" value="organization" className="select-menu-item width-full"
-					aria-label="Will be created as organization/refined-github." title="@organization">
-
-					<span className="select-menu-item-gravatar select-menu-item-icon">
-						<img src="https://avatars1.githubusercontent.com/u/50828444?s=40&amp;v=4" alt="@organization" width="20" height="20" />
-					</span>
-					<span className="select-menu-item-text">
-						organization
-					</span>
-				</button>
-				<input type="hidden" name="authenticity_token" value="sidifhfhsnklfdhvskdlfhkgdhgk" />
-			</form>
-		*/
-	});
-	console.log(toBeForked);
-
-	for (const element of select.all('a.d-block', forkDialog)) {
-		const fork = element.cloneNode(true) as HTMLElement;
-		fork.className = 'select-menu-item';
-		fork.firstElementChild!.className = 'select-menu-item-gravatar select-menu-item-icon';
-		fork.firstElementChild!.after(
-			<span className="select-menu-item-text">
-				{fork.firstElementChild!!.nextSibling!}
-			</span>
-		);
-		toBeForked.push(fork as HTMLElement);
-		/*
-		ORIGINAL HTML:
-			<a class="d-block" href="/organization">
-				<img class="avatar" src="https://avatars3.githubusercontent.com/u/5607437?s=40&amp;v=4" alt="@organization" width="20" height="20">
-				organization
-			</a>
-
-		NEEDED HTML:
-			<a href="/organization"
-			   className="select-menu-item">
-				<span className="select-menu-item-gravatar select-menu-item-icon">
-					<img src="https://avatars1.githubusercontent.com/u/50828444?s=40&amp;v=4" alt="@organization" width="20" height="20" />
-				</span>
-				<span className="select-menu-item-text">
-					organization
-				</span>
-			</a>
-		*/
-	}
-	if (toBeForked.length > 0) {
-		detailsDialog.append(
-			<div className="select-menu-header">
-				<a className="select-menu-title" href={`/${getRepoURL()}/fork`}>Create fork</a>
-			</div>
-		);
-		detailsDialog.append(...toBeForked);
-	}
-
-
-	const forkButton = select('summary[title^="Fork your own copy of"]')!;
-	forkButton.after(detailsDialog);
-
-	forkDialog!.remove();
-}
-
 async function init(): Promise<void> {
-	const forkDialog = select('details-dialog[src*="/fork"] include-fragment')!;
-	forkDialog.addEventListener('load', saveAllForks);
+	select('details-dialog[src*="/fork"] include-fragment')!
+		.addEventListener('load', saveAllForks);
 
-	const forks = await cache.get<string[]>(getCacheKey()) || [];
+	const forks = await cache.get<string[]>(getCacheKey());
 
-	const forkButton = select('summary[title^="Fork your own copy of"]')!;
-	forkButton.parentElement!.classList.remove('details-overlay-dark');
-	forkButton.classList.add('select-menu-button');
-	forkButton.append(
-		<span>
-			<span className="Counter">{forks.length}</span>
-			<> </>
-		</span>
-	);
+	if (!forks) {
+		return;
+	}
 
-	forkDialog.addEventListener('load', redesignForkDialog);
-	forkDialog.click();
+	const forkCounter = select('.social-count[href$="/network/members"]')!;
+	if (forks.length === 1) {
+		forkCounter.before(
+			<a href={`/${forks[0]}`}
+				className="btn btn-sm float-left rgh-forked"
+				title={`Open your fork to ${forks[0]}`}>
+				{icons.externalLink()}
+			</a>
+		);
+	} else {
+		forkCounter.before(
+			<details className="details-reset details-overlay select-menu float-left">
+				<summary
+					className="select-menu-button float-left btn btn-sm btn-with-count rgh-forked"
+					title="Open any of your forks"></summary>
+				<details-menu
+					style={{zIndex: 99}}
+					className="select-menu-modal position-absolute right-0 mt-5">
+					<div className="select-menu-header">
+						<span className="select-menu-title">Your forks</span>
+					</div>
+					{...forks.map(fork =>
+						<a href={`/${fork}`}
+							className="select-menu-item"
+							title={`Open your fork to ${fork}`}>
+							{icons.fork()}
+							{fork}
+						</a>
+					)}
+				</details-menu>
+			</details>
+		);
+	}
 
 	// Validate cache after showing links once, to make it faster
 	await updateForks(forks);
