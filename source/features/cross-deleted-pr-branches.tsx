093ce2df3f312cbadb28bf99b11ed05d768b4536
Rahul Gurung <gurungrahul2@gmail.com>
2019-08-18 18:16:04 +0530

    feedback
    * used .last over .all
    * added suggested comments changes
    * merged conditions
    * remove the inPR() and directly used the code in init with early return if not PR
    * used property to make style changes in all code.

--- source/features/cross-deleted-pr-branches.tsx
+++ source/features/cross-deleted-pr-branches.tsx
@@ -2,57 +2,55 @@ import select from 'select-dom';
 import features from '../libs/features';
 import * as pageDetect from '../libs/page-detect';
 
-function inPR(): void {
+function init(): void {
+	if (!pageDetect.isPR()) {
+		return;
+	}
+
 	let deletedBranch: string | undefined;
-	const lastBranchAction = select.all(`
+	const lastBranchAction = select.last(`
 		.discussion-item-head_ref_deleted .commit-ref,
 		.discussion-item-head_ref_restored .commit-ref
-    `).pop();
+    `);
 
 	if (lastBranchAction && lastBranchAction.closest('.discussion-item-head_ref_deleted')) {
 		deletedBranch = lastBranchAction.textContent!.trim();
 	}
 
-	// Making deleted branch link to the default fork
-	for (const el of select.all('.commit-ref[title].head-ref[title]')) {
-		const [repo] = el.title.split(':', 1);
-		const branchName = el.textContent!.trim();
-		if (branchName !== 'unknown repository') {
-			if (branchName === deletedBranch) {
-				el.children[0].setAttribute('href', `${branchName}/${repo}`);
-			}
+	// Point the deleted branch link to the default branch
+	for (const element of select.all('.commit-ref[title].head-ref[title]')) {
+		const [repo] = element.title.split(':', 1);
+		const branchName = element.textContent!.trim();
+		if (branchName !== 'unknown repository' && branchName === deletedBranch) {
+			const deletedBranchHTMLElement = element.children[0] as HTMLLinkElement;
+			deletedBranchHTMLElement.href = `${branchName}/${repo}`;
 		}
 	}
 
 	// Adding a linethrough to the branch name
-	for (const el of select.all('.commit-ref')) {
-		const branchName = el.textContent!.trim();
+	for (const element of select.all('.commit-ref')) {
+		const branchName = element.textContent!.trim();
 
 		if (branchName !== 'unknown repository') {
 			if (branchName === deletedBranch) {
-				if (el.classList.contains('head-ref')) {
-					const deletedBranchElement = el.children[0].children;
+				if (element.classList.contains('head-ref')) {
+					const deletedBranchElement = element.children[0].children;
 					for (const item of deletedBranchElement) {
-						item.setAttribute('style', 'text-decoration: line-through;');
+						const itemHTMLElement = item as HTMLElement;
+						itemHTMLElement.style.textDecoration = 'line-through';
 					}
 				}
 
-				el.title = 'Deleted';
-				el.style.textDecoration = 'line-through';
+				element.title = 'Deleted';
+				element.style.textDecoration = 'line-through';
 			}
 		}
-	}
-}
-
-function init(): void {
-	if (pageDetect.isPR()) {
-		inPR();
 	}
 }
 
 features.add({
 	id: __featureName__,
-	description: 'Adds a line-through to the deleted branches',
+	description: 'Adds a line-through to the deleted branches in PRs',
 	screenshot: 'https://user-images.githubusercontent.com/30543444/63117366-480a4b80-bfb9-11e9-949f-f624c8f0485c.png',
 	include: [
 		features.isPR
