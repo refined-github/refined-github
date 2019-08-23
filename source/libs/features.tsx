06df91a883c299ff79e146d70d759074be5552ea
James George <jamesgeorge998001@gmail.com>
2019-08-23 18:20:48 +0530

    Minor tweak
    fix typo

--- source/libs/features.tsx
+++ source/libs/features.tsx
@@ -41,7 +41,7 @@ export interface FeatureDetails {
  * For this reason `onAjaxedPages` will only call its callback when a *new* page is loaded.
  *
  * Alternatively, use `onAjaxedPagesRaw` if your callback needs to be called at every page
- * change (e.g. to "unmount" a feature / listener) regardless of of *newness* of the page.
+ * change (e.g. to "unmount" a feature / listener) regardless of *newness* of the page.
  */
 async function onAjaxedPagesRaw(callback: () => void): Promise<void> {
 	await onDomReady;
