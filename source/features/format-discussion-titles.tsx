13def6ae80586341a485196a81f4cb3190f5d8cf
Federico Brigante <opensource@bfred.it>
2019-08-27 18:06:27 +0700

    Revert "cleanup duplicate code"
    This reverts commit 5a23c87ebdb48c0f7f70dc630a28500d249d194f.

--- source/features/format-discussion-titles.tsx
+++ source/features/format-discussion-titles.tsx
@@ -1,10 +1,9 @@
-import linkifyIssues from 'linkify-issues';
 import select from 'select-dom';
 import zipTextNodes from 'zip-text-nodes';
 import features from '../libs/features';
 import observeEl from '../libs/simplified-element-observer';
 import parseBackticks from '../libs/parse-backticks';
-import linkify from '../libs/linkify-text-nodes';
+import {linkifyIssuesInDom} from './linkify-urls-in-code';
 
 function init(): void {
 	observeEl(
@@ -13,7 +12,7 @@ function init(): void {
 			for (const title of select.all('.js-issue-title:not(.rgh-formatted-title)')) {
 				if (title) {
 					title.classList.add('rgh-formatted-title');
-					linkify(linkifyIssues, title);
+					linkifyIssuesInDom(title);
 
 					const fragment = parseBackticks(title.textContent!);
 
--- source/features/linkify-urls-in-code.tsx
+++ source/features/linkify-urls-in-code.tsx
@@ -1,11 +1,46 @@
 import select from 'select-dom';
 import linkifyUrls from 'linkify-urls';
+import zipTextNodes from 'zip-text-nodes';
 import linkifyIssues from 'linkify-issues';
 import features from '../libs/features';
-import linkify from '../libs/linkify-text-nodes';
+import {getOwnerAndRepo} from '../libs/utils';
+
+export function linkifyIssuesInDom(element: Element): void {
+	const linkified = linkifyIssues(element.textContent!, options);
+	if (linkified.children.length === 0) { // Children are <a>
+		return;
+	}
+
+	// Enable native issue title fetch
+	for (const link of (linkified.children as HTMLCollectionOf<HTMLAnchorElement>)) {
+		const issue = link.href.split('/').pop();
+		link.setAttribute('class', 'issue-link js-issue-link tooltipped tooltipped-ne');
+		link.setAttribute('data-error-text', 'Failed to load issue title');
+		link.setAttribute('data-permission-text', 'Issue title is private');
+		link.setAttribute('data-url', link.href);
+		link.setAttribute('data-id', `rgh-issue-${issue}`);
+	}
+
+	zipTextNodes(element, linkified);
+}
 
 // Shared class necessary to avoid also shortening the links
 export const linkifiedURLClass = 'rgh-linkified-code';
+
+// If we are not in a repo, relative issue references won't make sense
+// but `user`/`repo` need to be set to avoid breaking errors in `linkify-issues`
+// https://github.com/sindresorhus/refined-github/issues/1305
+const currentRepo = getOwnerAndRepo();
+const options = {
+	user: currentRepo.ownerName || '/',
+	repo: currentRepo.repoName || '/',
+	type: 'dom',
+	baseUrl: '',
+	attributes: {
+		rel: 'noreferrer noopener',
+		class: linkifiedURLClass // Necessary to avoid also shortening the links
+	}
+};
 
 function init(): false | void {
 	const wrappers = select.all(`
@@ -26,12 +61,17 @@ function init(): false | void {
 			continue;
 		}
 
-		linkify(linkifyUrls, element, element.textContent!);
+		const linkified = linkifyUrls(element.textContent!, options);
+		if (linkified.children.length === 0) { // Children are <a>
+			continue;
+		}
+
+		zipTextNodes(element, linkified);
 	}
 
 	// Linkify issue refs in comments
 	for (const element of select.all('span.pl-c', wrappers)) {
-		linkify(linkifyIssues, element);
+		linkifyIssuesInDom(element);
 	}
 
 	// Mark code block as touched
