908edda1416718a6e93bab8d7ba5062291de3d9c
Federico Brigante <opensource@bfred.it>
2019-09-17 12:23:57 +0700

    Merge `highlight-own-issues-and-prs` and `highlight-collaborators-in-lists`

--- source/features/highlight-collaborators-and-own-discussions.tsx
+++ source/features/highlight-collaborators-and-own-discussions.tsx
@@ -13,18 +13,22 @@ async function init(): Promise<false | void> {
 	const dom = await fetchDom(getRepoURL() + '/issues/show_menu_content?partial=issues/filters/authors_content');
 	const collaborators = select.all('.select-menu-item-text', dom).map(collaborator => {
 		return collaborator.firstChild!.textContent!.trim();
-	}).filter(collaborator => collaborator !== getUsername());
+	});
 
 	for (const author of authors) {
-		if (collaborators.includes(author.textContent!.trim())) {
+		const username = author.textContent!.trim();
+		if (collaborators.includes(username)) {
 			author.classList.add('rgh-collaborator');
+			if (username === getUsername()) {
+				author.style.fontWeight = 'bold';
+			}
 		}
 	}
 }
 
 features.add({
 	id: __featureName__,
-	description: 'Highlights discussions opened by the current repo’s collaborators.',
+	description: 'Highlights discussions opened by you or the current repo’s collaborators.',
 	screenshot: 'https://user-images.githubusercontent.com/55841/64478536-0a5ca500-d1aa-11e9-8284-a39114d37824.png',
 	include: [
 		features.isDiscussionList
