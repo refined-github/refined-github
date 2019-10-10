ec010808b2bb7c47a82702201aa40c93e73e7cd2
Flo Edelmann <florian-edelmann@online.de>
2019-10-11 00:05:03 +0200

    Add feature `separate-draft-pr-button`

---
+++ source/features/separate-draft-pr-button.tsx
@@ -0,0 +1,59 @@
+import React from 'dom-chef';
+import select from 'select-dom';
+import features from '../libs/features';
+
+async function init(): Promise<false | void> {
+	const previewForm = select('.new-pr-form')! as HTMLFormElement;
+
+	// move the "Allow edits from maintainers" to its own line if it exists
+	const allowEditsCheckbox = select('.float-left', previewForm);
+	if (allowEditsCheckbox) {
+		allowEditsCheckbox.classList.remove('float-left');
+	}
+
+	const buttonBar = select('.flex-justify-end', previewForm)!;
+	const defaultButton = select('.BtnGroup', buttonBar)!;
+
+	// hide default button with dropdown
+	defaultButton.hidden = true;
+
+	const menuItems = select.all('.select-menu-item', defaultButton);
+
+	for (const menuItem of menuItems) {
+		const title = select('.select-menu-item-heading', menuItem)!.textContent!.trim();
+		const description = select('.description', menuItem)!.textContent!.trim();
+		const radioButton = select('[type=radio]', menuItem)! as HTMLInputElement;
+
+		const classList = ['btn', 'ml-2', 'tooltipped', 'tooltipped-s'];
+		if (!title.toLowerCase().includes('draft')) {
+			classList.push('btn-primary');
+		}
+
+		const submitForm = () => {
+			radioButton.checked = true;
+			previewForm.submit();
+		};
+
+		buttonBar.prepend(
+			<button
+				className={classList.join(' ')}
+				aria-label={description}
+				type="button"
+				onClick={submitForm}
+			>
+				{title}
+			</button>
+		)
+	}
+}
+
+features.add({
+	id: __featureName__,
+	description: 'Separates `Create draft pull request` and `Create pull request` buttons.',
+	screenshot: 'https://user-images.githubusercontent.com/202916/66609826-4a081980-ebba-11e9-80d8-cdef503a8a80.png',
+	include: [
+		features.isCompare
+	],
+	load: features.onAjaxedPages,
+	init
+});
