00a04453ca0eb5eac30d1d800a8e92331dcc29c1
Kid <kidonng@gmail.com>
2019-10-27 20:51:34 +0800

    Add `preview-html-files` feature

---
+++ source/features/preview-html-files.tsx
@@ -0,0 +1,37 @@
+import React from 'dom-chef';
+import select from 'select-dom';
+import features from '../libs/features';
+import {groupSiblings} from '../libs/group-buttons';
+
+function init(): void {
+	const rawButton = select('#raw-url') as HTMLAnchorElement;
+	if (rawButton && /\.html?/.test(rawButton.pathname)) {
+		const link = rawButton.pathname.split('/')
+		// Remove /raw/
+		link.splice(3, 1)
+		rawButton
+			.parentElement! // `BtnGroup`
+			.prepend(
+				<a
+					className="btn btn-sm BtnGroup-item"
+				  href={`https://cdn.statically.io/gh${link.join('/')}`}>
+					Preview
+				</a>
+			);
+		groupSiblings(rawButton);
+	}
+}
+
+features.add({
+	id: __featureName__,
+	description: 'Adds a button to preview HTML files.',
+	screenshot: 'https://user-images.githubusercontent.com/44045911/67634792-48995980-f8fb-11e9-8b6a-7b57d5b12a2f.png',
+	include: [
+		features.isSingleFile
+	],
+	exclude: [
+		features.isEnterprise
+	],
+	load: features.onAjaxedPages,
+	init
+})
