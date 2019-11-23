a8e13ba98c234c4c31dbe0af4d23830148b9d284
Federico Brigante <opensource@bfred.it>
2019-11-23 16:38:37 +0700

    Return real `number` for `value`, not a string asserted as number

--- source/libs/time-ago.tsx
+++ source/libs/time-ago.tsx
@@ -4,7 +4,8 @@ export default function timeAgo(date: Date): {value: number; unit: string} {
 	const units = ['minute', 'hour', 'day', 'year'];
 
 	const time = (<time-ago datetime={date.toISOString()} format="micro"/>).textContent!;
-	const [value, short] = time.match(/[a-z]+|[^a-z]+/gi)! as [number, string];
+	const value = parseInt(time, 10);
+	const short = time.slice(-1);
 
 	let unit = units.find(unit => unit.startsWith(short))!;
 	unit = value > 1 ? unit + 's' : unit;
