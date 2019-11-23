a9aa6284f36441644ce7114a28a411b1abe5cf76
Federico Brigante <opensource@bfred.it>
2019-11-23 16:40:53 +0700

    Find unit with hash table instead of .find()

--- source/libs/time-ago.tsx
+++ source/libs/time-ago.tsx
@@ -1,14 +1,15 @@
 import React from 'dom-chef';
 
+const units = {
+	m: 'minute',
+	h: 'hour',
+	d: 'day',
+	y: 'year'
+};
 export default function timeAgo(date: Date): {value: number; unit: string} {
-	const units = ['minute', 'hour', 'day', 'year'];
-
 	const time = (<time-ago datetime={date.toISOString()} format="micro"/>).textContent!;
 	const value = parseInt(time, 10);
-	const short = time.slice(-1);
-
-	let unit = units.find(unit => unit.startsWith(short))!;
-	unit = value > 1 ? unit + 's' : unit;
-
+	const short = time.slice(-1) as 'm' | 'h' | 'd' | 'y';
+	const unit = units[short] + (value > 1 ? 's' : '');
 	return {value, unit};
 }
