e317e92a0131469d55969158a1a4309155ccf256
Karl Heitmann <khf1988@gmail.com>
2019-12-03 00:46:50 -0300

    some logging added

--- source/features/label-color-picker.tsx
+++ source/features/label-color-picker.tsx
@@ -1,5 +1,3 @@
-import features from '../libs/features';
-
 import React from 'dom-chef';
 import select from 'select-dom';
 import features from '../libs/features';
@@ -8,18 +6,28 @@ function log() {
 	console.log('✨', <div className="rgh-jsx-element"/>);
 }
 
-/*
-function init () {
-	console.log('My first contribution');
-}
-*/
-
 function init(): void {
-  console.log("asd")
-	select('.btn')!.addEventListener('click', log);
-}
-function deinit(): void {
-	select('.btn')!.removeEventListener('load', log);
+  console.log('init: label-color-picker')
+  // if (select.exists('.btn-link')) {
+  //   console.log('viento! detectado el boton')
+  // }
+	// // select('.btn-link')!.addEventListener('click', log);
+  // // );
+	// // select('.btn-link')!.addEventListener('click', log);
+  // select('.btn-link:not(.is-placeholder)')!.after(
+  select('.btn-link')!.after(
+    <div className='rgh-follower-badge'>Follows you</div>
+  );
+  console.log(select.all('.js-new-label-color-input'))
+  for (const field of select.all('.js-new-label-color-input')) {
+    console.log('begin iteration')
+    console.log(field)
+    field.type = 'color';
+    console.log('field type has changed')
+    console.log(field)
+    console.log('end iteration')
+  }
+  console.log("Ultimo")
 }
 
 features.add({
@@ -35,17 +43,9 @@ features.add({
 	exclude: [
 		features.isOwnUserProfile
 	],
-	load: features.onDomReady, // Wait for DOM ready
+	// load: features.onDomReady, // Wait for DOM ready
 	// load: features.onAjaxedPages, // Or: Wait for DOM ready AND run on all AJAXed loads
-	// load: features.onNewComments, // Or: Wait for DOM ready AND run on all AJAXed loads AND watch for new comments
-	deinit, // Rarely needed
+  // load: features.onNewComments, // Or: Wait for DOM ready AND run on all AJAXed loads AND watch for new comments
+	load: features.onAjaxedPagesRaw, // Or: Wait for DOM ready AND run on all AJAXed loads
 	init
 });
-/*
-features.add({
-	id: __featureName__,
-	description: 'Adds a color picker for labels',
-	screenshot: '',
-	init
-});
-*/
