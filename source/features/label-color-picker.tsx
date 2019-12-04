41905f383589c8ec48f020074edf650c1218ad93
Karl Heitmann <khf1988@gmail.com>
2019-12-04 02:32:33 -0300

    lint passed, some more comments have been deleted

--- source/features/label-color-picker.tsx
+++ source/features/label-color-picker.tsx
@@ -1,32 +1,33 @@
 import React from 'dom-chef';
 import select from 'select-dom';
-import delegate, { DelegateEvent } from 'delegate-it';
+import delegate, {DelegateEvent} from 'delegate-it';
 import features from '../libs/features';
 // FIXME: whenever color palette changes its value, the label color example above the edit form doesn't change it's value
 // FIXME: whenever the Save Changes button is pressed, if the web page is not reloaded and the "Edit" link is
 //        pressed again, when the new edit form will be prompted, the colorPalette would not appear.
 
 async function labelDelegateEvent(event: DelegateEvent<Event, HTMLButtonElement>): Promise<void> {
-  event.delegateTarget.nextElementSibling.value = event.delegateTarget.value
+	event.delegateTarget.nextElementSibling.value = event.delegateTarget.value;
 }
 
 async function colorPaletteDelegateEvent(event: DelegateEvent<Event, HTMLButtonElement>): Promise<void> {
-  event.delegateTarget.previousElementSibling.value = event.delegateTarget.value
-  event.delegateTarget.parentElement.previousElementSibling.style.backgroundColor = event.delegateTarget.value
+	event.delegateTarget.previousElementSibling.value = event.delegateTarget.value;
+	event.delegateTarget.parentElement.previousElementSibling.style.backgroundColor = event.delegateTarget.value;
 }
 
 async function testClickEvent(event: DelegateEvent<Event, HTMLButtonElement>): Promise<void> {
-  event.delegateTarget.nextElementSibling.children[1].value = event.delegateTarget.nextElementSibling.children[0].value
+	event.delegateTarget.nextElementSibling.children[1].value = event.delegateTarget.nextElementSibling.children[0].value;
 }
 
 function init(): void {
-  for (const field of select.all('.js-new-label-color-input')) {
-    const new_element=<input type="color" className={field.className} style={{width:'4em'}} value={field.value} />
-    field.after(new_element);
-  }
-  delegate('.js-new-label-color', 'click', testClickEvent);
-  delegate('.js-new-label-color-input', 'input', labelDelegateEvent);
-  delegate('.js-new-label-color-input + input', 'input', colorPaletteDelegateEvent);
+	for (const field of select.all('.js-new-label-color-input')) {
+		const new_element = <input type="color" className={field.className} style={{width: '4em'}} value={field.value} />;
+		field.after(new_element);
+	}
+
+	delegate('.js-new-label-color', 'click', testClickEvent);
+	delegate('.js-new-label-color-input', 'input', labelDelegateEvent);
+	delegate('.js-new-label-color-input + input', 'input', colorPaletteDelegateEvent);
 }
 
 features.add({
@@ -42,9 +43,6 @@ features.add({
 	exclude: [
 		features.isOwnUserProfile
 	],
-	// load: features.onDomReady, // Wait for DOM ready
-	// load: features.onAjaxedPages, // Or: Wait for DOM ready AND run on all AJAXed loads
-  // load: features.onNewComments, // Or: Wait for DOM ready AND run on all AJAXed loads AND watch for new comments
-	load: features.onAjaxedPagesRaw, // Or: Wait for DOM ready AND run on all AJAXed loads
+	load: features.onAjaxedPages, // Or: Wait for DOM ready AND run on all AJAXed loads
 	init
 });
