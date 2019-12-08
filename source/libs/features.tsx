f26644be7c8a9f8232ef2661c357624c5fe63181
Federico Brigante <github@bfred.it>
2019-12-08 17:12:06 +0700

    One more in features.tsx

--- source/libs/features.tsx
+++ source/libs/features.tsx
@@ -64,10 +64,7 @@ onAjaxedPages(async () => {
 	await globalReady; // Match `add()`
 	await Promise.resolve(); // Kicks it to the next tick, after the other features have `run()`
 
-	const ajaxContainer = select('#js-repo-pjax-container,#js-pjax-container');
-	if (ajaxContainer) {
-		ajaxContainer.append(<has-rgh/>);
-	}
+	select('#js-repo-pjax-container, #js-pjax-container')?.append(<has-rgh/>);
 });
 
 let log: typeof console.log;
