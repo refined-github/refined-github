126a5f90e52ef8649c97c6e78064aad152e14cb8
Federico Brigante <github@bfred.it>
2019-12-09 11:15:02 +0700

    Add `quick-mention` feature (#2556)

---
+++ source/features/quick-mention.tsx
@@ -0,0 +1,57 @@
+import './quick-mention.css';
+import React from 'dom-chef';
+import select from 'select-dom';
+import delegate, {DelegateEvent} from 'delegate-it';
+import insertText from 'insert-text-textarea';
+import features from '../libs/features';
+import * as icons from '../libs/icons';
+import {getUsername} from '../libs/utils';
+
+function mentionUser({delegateTarget: button}: DelegateEvent): void {
+	const userMention = button.parentElement!.querySelector('img')!.alt;
+	const newComment = select<HTMLTextAreaElement>('#new_comment_field')!;
+	newComment.focus();
+
+	// If the new comment field has selected text, don’t replace it
+	newComment.selectionStart = newComment.selectionEnd;
+
+	// If the cursor is preceded by a space (or is at place 0), don't add a space before the mention
+	const precedingCharacter = newComment.value.slice(newComment.selectionStart - 1, newComment.selectionStart);
+	const spacer = /\s|^$/.test(precedingCharacter) ? '' : ' ';
+
+	// The space after closes the autocomplete box and places the cursor where the user would start typing
+	insertText(newComment, `${spacer}${userMention} `);
+}
+
+function init(): void | false {
+	if (select.exists('.conversation-limited')) {
+		return false; // Discussion is locked
+	}
+
+	for (const avatar of select.all(`.TimelineItem-avatar > :not([href="/${getUsername()}"]):not(.rgh-quick-mention)`)) {
+		avatar.classList.add('rgh-quick-mention');
+		avatar.after(
+			<button
+				type="button"
+				className="rgh-quick-mention tooltipped tooltipped-e btn-link"
+				aria-label="Mention user in a new comment"
+			>
+				{icons.reply()}
+			</button>
+		);
+	}
+
+	delegate('#discussion_bucket', 'button.rgh-quick-mention', 'click', mentionUser);
+}
+
+features.add({
+	id: __featureName__,
+	description: 'Adds a button to @mention a user in discussions.',
+	screenshot: 'https://user-images.githubusercontent.com/1402241/70406615-f445d580-1a73-11ea-9ab1-bf6bd9aa70a3.gif',
+	include: [
+		features.isIssue,
+		features.isPRConversation
+	],
+	load: features.onNewComments,
+	init
+});
--- source/libs/icons.tsx
+++ source/libs/icons.tsx
@@ -77,3 +77,5 @@ export const cloudDownload = (): SVGElement => <svg width="16" height="16" class
 export const arrowDown = (): SVGElement => <svg aria-hidden="true" className="octicon octicon-arrow-down" width="16" height="16"><path d="M7 7V3H3v4H0l5 6 5-6H7z" /></svg>;
 
 export const fork = (): SVGElement => <svg aria-hidden="true" className="octicon octicon-repo-forked select-menu-item-icon" width="10" height="16"><path fill-rule="evenodd" d="M8 1a1.993 1.993 0 0 0-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 0 0 0-1 3.72V6.5l3 3v1.78A1.993 1.993 0 0 0 5 15a1.993 1.993 0 0 0 1-3.72V9.5l3-3V4.72A1.993 1.993 0 0 0 8 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"></path></svg>;
+
+export const reply = (): SVGElement => <svg aria-hidden="true" className="octicon octicon-reply" width="16" height="14"><path d="M6 3.5c3.92.44 8 3.125 8 10-2.312-5.062-4.75-6-8-6V11L.5 5.5 6 0v3.5z"/></svg>;
