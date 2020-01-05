c2d27d0fd45ff9556ee28034b258e7f22685e899
Samuel Meuli <me@samuelmeuli.com>
2020-01-05 19:01:57 +0100

    Improve docs in `parse-backticks.tsx`

--- source/features/parse-backticks.tsx
+++ source/features/parse-backticks.tsx
@@ -4,13 +4,13 @@ import features from '../libs/features';
 import {parseBackticks} from '../libs/dom-formatters';
 
 function init(): void {
-	for (const title of select.all(`
-		[aria-label="Issues"][role="group"] .js-navigation-open,
-		.message,
-		.commit-title,
-		.commit-desc,
-		.Box--condensed .link-gray[href*="/commit/"]
-	`)) {
+	for (const title of select.all([
+		'.commit-title', // `isCommit`
+		'.commit-desc', // `isCommit`, `isCommitList`, `isRepoTree`
+		'.message', // `isCommitList`, `isRepoTree`
+		'.Box--condensed .link-gray[href*="/commit/"]', // `isSingleFile`
+		'[aria-label="Issues"][role="group"] .js-navigation-open' // `isDiscussionList`
+	])) {
 		parseBackticks(title);
 	}
 }
@@ -20,21 +20,10 @@ features.add({
 	description: 'Renders text in `backticks` in issue titles and commit titles/descriptions.',
 	screenshot: 'https://user-images.githubusercontent.com/170270/55060505-31179b00-50a4-11e9-99a9-c3691ba38d66.png',
 	include: [
-		// Issue/PR list: "[aria-label="Issues"][role="group"] .js-navigation-open" selector
-		features.isDiscussionList,
-
-		// Repository start page:
-		// - Preview of latest commit: ".message" and ".commit-desc" selectors
-		// - Commit messages in file tree: ".message" selector
-		features.isRepoTree,
-
-		// Commit list: ".message" and ".commit-desc" selectors
-		features.isCommitList,
-
-		// Single commit view: ".commit-title" and ".commit-desc" selectors
 		features.isCommit,
-
-		// Single file view: '.Box--condensed .link-gray[href*="/commit/"]' selector
+		features.isCommitList,
+		features.isDiscussionList,
+		features.isRepoTree,
 		features.isSingleFile
 	],
 	load: features.onAjaxedPages,
