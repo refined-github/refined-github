c0238b7514ed4dff00658a669e3566b1af518ddc
Adk9p <Adk9p@users.noreply.github.com>
2020-01-06 00:36:22 -0800

    Changed variable container to pagination

--- source/features/infinite-commit-list-scroll.tsx
+++ source/features/infinite-commit-list-scroll.tsx
@@ -6,7 +6,7 @@ import features from '../libs/features';
 // Button from: https://primer.style/css/components/loaders
 const githubLoadingButton = <button className="btn mn-3" disabled><span>Loading</span><span className="AnimatedEllipsis"></span></button>;
 
-let container: HTMLElement;
+let pagination: HTMLElement;
 let link: HTMLAnchorElement | undefined;
 
 let lastDate: string;
@@ -22,8 +22,8 @@ function setLastDate(document_ = select('.commits-listing')!): void {
 
 async function appendOlder(): Promise<void> {
 	// Replace buttons with loading button
-	select('*', container)!.remove();
-	container.append(githubLoadingButton);
+	select('*', pagination)!.remove();
+	pagination.append(githubLoadingButton);
 
 	// Fetch older content
 	const olderContent = await fetchDom(link!.href, '.repository-content')!;
@@ -40,12 +40,12 @@ async function appendOlder(): Promise<void> {
 
 	// Add border to differentiate appended content
 	olderList.classList.add('border-top', 'border-gray-dark');
-	container.before(olderList);
+	pagination.before(olderList);
 
 	// Set loading button to Newer and Older links
-	select('*', container)!.remove();
-	container.append(select('.paginate-container > .BtnGroup', olderContent)!);
-	link = select<HTMLAnchorElement>('div > a:last-child', container)!;
+	select('*', pagination)!.remove();
+	pagination.append(select('.paginate-container > .BtnGroup', olderContent)!);
+	link = select<HTMLAnchorElement>('div > a:last-child', pagination)!;
 }
 
 const inView = new IntersectionObserver(([{isIntersecting}]) => {
@@ -59,11 +59,11 @@ const inView = new IntersectionObserver(([{isIntersecting}]) => {
 function init(): void {
 	setLastDate();
 
-	container = select('.paginate-container')!;
-	link = select<HTMLAnchorElement>('div > a:last-child', container)!;
+	pagination = select('.paginate-container')!;
+	link = select<HTMLAnchorElement>('div > a:last-child', pagination)!;
 
 	if (link) {
-		inView.observe(container);
+		inView.observe(pagination);
 	}
 }
 
