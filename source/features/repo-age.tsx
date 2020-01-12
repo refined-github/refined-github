c1e1473e5f96e28b6701c81b7da26effff4e6dcb
Hardik Modha <hardikmodha22@gmail.com>
2020-01-12 20:03:55 +0530

    Improvements
    1. Do not wait for DOM for fetching commit details.
    2. Remove unnecessary function
    3. Edit content of repo-age item on summary row

--- source/features/repo-age.tsx
+++ source/features/repo-age.tsx
@@ -41,50 +41,33 @@ const getOldestCommitDetails = cache.function(async (): Promise<OldestCommitDeta
 	cacheKey: () => __featureName__ + ':' + getRepoURL()
 });
 
-const addRepoAgeItem = (element: Element): void => {
-	const license = select('.numbers-summary .octicon-law');
-
-	if (license) {
-		license.closest('li')!.before(element);
-	} else {
-		select('.numbers-summary')!.append(element);
-	}
-};
-
 async function init(): Promise<void> {
+	const detailsPromise = getOldestCommitDetails();
+
 	const placeholder = (
 		<li className="text-gray">
-			{repoIcon()} <span className="text-emphasized js_repo_age_placeholder">Loading...</span>
+			{repoIcon()} <span className="text-emphasized">Loading...</span>
 		</li>
 	);
 
 	await elementReady('.overall-summary + *');
+	const license = select('.numbers-summary .octicon-law');
+	if (license) {
+		license.closest('li')!.before(placeholder);
+	} else {
+		select('.numbers-summary')!.append(placeholder);
+	}
 
-	addRepoAgeItem(placeholder);
-
-	const details = await getOldestCommitDetails();
+	const details = await detailsPromise;
 
 	if (!details) {
-		select('.js_repo_age_placeholder', placeholder)!.textContent = '-';
+		select('span', placeholder)!.textContent = '-';
 		placeholder.title = 'Cannot determine repo age from the oldest commit';
 
 		return;
 	}
 
-	// Older cached values are date strings so to handle it for the older
-	// cached values we are checking for the type of the returned values.
-	// This check is supposed to be dropped soon.
-	let oldestCommitUrl;
-	let repoCreationDate;
-
-	if (typeof details === 'object') {
-		oldestCommitUrl = details.url;
-		repoCreationDate = details.datetime;
-	} else {
-		repoCreationDate = details;
-	}
-
-	const date = new Date(repoCreationDate);
+	const date = new Date(details.datetime);
 
 	// `twas` could also return `an hour ago` or `just now`
 	const [value, unit] = twas(date.getTime())
@@ -92,21 +75,9 @@ async function init(): Promise<void> {
 		.replace(/^an?/, '1')
 		.split(' ');
 
-	const repoAge = <>{repoIcon()} <span className="num text-emphasized">{value}</span> {unit} old</>;
-
-	// Commit url won't be present for the existing cached values
-	const element = oldestCommitUrl ? (
-		<li className="text-gray" title={`First commit dated ${dateFormatter.format(date)}`}>
-			<a href={oldestCommitUrl}>{repoAge}</a>
-		</li>
-	) : (
-		<li className="text-gray" title={`Repository created on ${dateFormatter.format(date)}`}>
-			{repoAge}
-		</li>
-	);
-
-	placeholder.remove();
-	addRepoAgeItem(element);
+	placeholder.title = `First commit dated ${dateFormatter.format(date)}`
+	placeholder.innerHTML = '';
+	placeholder.append(<a href={details.url}>{repoIcon()} <span className="num text-emphasized">{value}</span> {unit} old</a>);
 }
 
 features.add({
