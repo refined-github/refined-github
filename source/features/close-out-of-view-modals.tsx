bf0c1a2dacb1a0b83aa7ae82a76afc0e8833cb6f
Federico Brigante <github@bfred.it>
2020-01-15 01:18:26 +0700

    Temporarily disable `close-out-of-view-modals`
    Due to https://github.com/sindresorhus/refined-github/issues/2701

--- source/features/close-out-of-view-modals.tsx
+++ source/features/close-out-of-view-modals.tsx
@@ -16,6 +16,7 @@ function init(): void {
 }
 
 features.add({
+	disabled: '#2701',
 	id: __featureName__,
 	description: 'Automatically closes dropdown menus when they’re no longer visible.',
 	screenshot: 'https://user-images.githubusercontent.com/1402241/37022353-531c676e-2155-11e8-96cc-80d934bb22e0.gif',
