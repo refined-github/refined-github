a6276a90fbaad15f54b4b8e4fe5c6528f1547411
Federico Brigante <opensource@bfred.it>
2020-01-16 19:15:08 +0700

    Restore `close-out-of-view-modals`
    Fixes #2701 and makes it more resilient. This issue happened a couple of times before

--- source/features/close-out-of-view-modals.tsx
+++ source/features/close-out-of-view-modals.tsx
@@ -11,12 +11,20 @@ const observer = new IntersectionObserver(([{intersectionRatio, target}]) => {
 function init(): void {
 	// The `open` attribute is added after this handler is run, so the selector is inverted
 	delegate('.details-overlay:not([open]) > summary', 'click', ({delegateTarget: summary}) => {
-		observer.observe(summary.parentElement!.querySelector('.dropdown-menu')!);
+		// The timeout gives the element time to "open"
+		setTimeout(() => {
+			const modalBox = summary.parentElement!.querySelector('.dropdown-menu')!;
+			if (modalBox.getBoundingClientRect().width === 0) {
+				console.error(__featureName__, 'Modal element was not correctly detected for', summary);
+				return;
+			}
+
+			observer.observe(modalBox);
+		}, 100);
 	});
 }
 
 features.add({
-	disabled: '#2701',
 	id: __featureName__,
 	description: 'Automatically closes dropdown menus when they’re no longer visible.',
 	screenshot: 'https://user-images.githubusercontent.com/1402241/37022353-531c676e-2155-11e8-96cc-80d934bb22e0.gif',
