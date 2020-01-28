b266354a7f2cda82b01160da60337e16dd515a02
Bert Blommers <bblommers@users.noreply.github.com>
2020-01-28 10:17:42 +0000

    Extend `hide-zero-packages` to user profiles (#2735)
    Co-authored-by: Federico Brigante <github@bfred.it>

--- source/features/hide-zero-packages.tsx
+++ source/features/hide-zero-packages.tsx
@@ -1,20 +1,25 @@
-import select from 'select-dom';
+import elementReady from 'element-ready';
 import features from '../libs/features';
 
-function init(): void {
-	const packagesLabel = select('.numbers-summary [href$="/packages"]');
-	if (packagesLabel?.textContent!.trim().startsWith('0')) {
-		packagesLabel!.parentElement!.remove();
+async function init(): Promise<void> {
+	const packagesCounter = await elementReady([
+		'.numbers-summary [href$="/packages"] .num', // `isRepoRoot`
+		'.UnderlineNav-item[href$="?tab=packages"] .Counter' // `isUserProfile`
+	].join(','));
+
+	if (packagesCounter?.textContent!.trim() === '0') {
+		packagesCounter!.closest('li, .UnderlineNav-item')!.remove();
 	}
 }
 
 features.add({
 	id: __featureName__,
-	description: 'Hides the `Packages` tab in repositories if it’s empty.',
+	description: 'Hides the `Packages` tab if it’s empty (in repositories and user profiles).',
 	screenshot: 'https://user-images.githubusercontent.com/35382021/62426530-688ef780-b6d5-11e9-93f2-515110aed1eb.jpg',
 	include: [
-		features.isRepoRoot
+		features.isRepoRoot,
+		features.isUserProfile
 	],
-	load: features.onAjaxedPages,
+	load: features.nowAndOnAjaxedPages,
 	init
 });
