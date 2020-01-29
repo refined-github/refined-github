e9be50e8c1a45e5d7c1ee35603a5678e3385cd8e
Karl Heitmann <khf1988@gmail.com>
2020-01-29 17:51:54 -0300

    lint passed

--- source/features/remove-is-open-is-closed.tsx
+++ source/features/remove-is-open-is-closed.tsx
@@ -29,11 +29,12 @@ function init(): void {
 		console.log(link);
 		console.log(link.children);
 		link.firstElementChild.remove();
-		if (link.classList.contains("selected")) {
+		if (link.classList.contains('selected')) {
 			link.prepend(<svg className="octicon octicon-check" viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true">
-				<path fill-rule="evenodd" d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5L12 5z"></path>
-			</svg>)
+				<path fill-rule="evenodd" d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5L12 5z"/>
+			</svg>);
 		}
+
 		const linkSearchParameters = new URLSearchParams(link.search);
 		const linkQuery = linkSearchParameters.get('q');
 		console.log(linkSearchParameters);
