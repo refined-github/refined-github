bac196b114a751f86089ba4bcc4269bb20d6da49
Federico Brigante <opensource@bfred.it>
2020-02-02 16:31:24 +0700

    Meta: improve error reporting

--- source/libs/utils.ts
+++ source/libs/utils.ts
@@ -3,10 +3,11 @@ import onetime from 'onetime';
 import stripIndent from 'strip-indent';
 import {isRepo, isPR, isIssue} from './page-detect';
 
-export function logError(featureName: typeof __featureName__, message: string, ...extras: unknown[]): void {
+export function logError(featureName: typeof __featureName__, error: Error | string, ...extras: unknown[]): void {
+	const message = typeof error === 'string' ? error : error.message;
 	// Don't change this to `throw Error` because Firefox doesn't show extensions' errors in the console.
 	// Use `return` after calling this function.
-	console.error(`❌ Refined GitHub: feature \`${featureName}\`\n\n${message}`, ...extras, stripIndent(`
+	console.error(`❌ Refined GitHub: feature \`${featureName}\`\n\n`, error, ...extras, stripIndent(`
 	
 		Search for open issues:
 		https://github.com/sindresorhus/refined-github/issues?q=is%3Aissue+${encodeURIComponent(message)}
