e1c37007f0be92b76a608c0de4476aec70ae65e5
Federico Brigante <opensource@bfred.it>
2020-02-04 18:52:18 +0700

    Improve GitHub search query manipulation /2
    Add tests
    Add defaults when the q parameter is missing
    Update features to use this and the new `user:@me` keyword instead of `user:${getUsername()}`

--- test/fixtures/globals.ts
+++ test/fixtures/globals.ts
@@ -6,3 +6,5 @@ const {window} = new JSDOM('...');
 (global as any).navigator = window.navigator;
 (global as any).document = window.document;
 (global as any).location = new URL('https://github.com');
+(global as any).HTMLAnchorElement = window.HTMLAnchorElement;
+(global as any).Location = window.Location;
