6716e226b51b5d6934b771e49266b2f001257f7e
Kai Hao <kevin830726@gmail.com>
2020-02-05 20:55:12 +0800

    Refactor

--- source/features/expand-all-hidden-comments.ts
+++ source/features/expand-all-hidden-comments.ts
@@ -1,34 +1,29 @@
-import select from 'select-dom';
 import delegate, {DelegateEvent} from 'delegate-it';
 import features from '../libs/features';
 
-function handleAltClick(event: DelegateEvent<MouseEvent, HTMLFormElement>): void {
+/**
+As per #2625:
+Construct the new URL. This is undocumented, and may break at any time.
+As of 12/08/2019, URLs look like this:
+`/_render_node/MDExOlB1bGxSZXF1ZXN0MjE2MDA0MzU5/timeline/more_items?variables%5Bafter%5D=Y3Vyc29yOnYyOpPPAAABZemjg2AAqTQyMjE5MTk1MQ%3D%3D&variables%5Bbefore%5D=Y3Vyc29yOnYyOpPPAAABaENrVHAAqTQ1Mzc3MjMzNg%3D%3D&variables%5Bfirst%5D=60&variables%5BhasFocusedReviewComment%5D=false&variables%5BhasFocusedReviewThread%5D=false`
+The key "variables[first]" in the URL query parameters appears to control how many additional comments are fetched.
+Github appears to always set this to 60, but it can be increased up to the total number of hidden items.
+By setting "variables[first]" to total number of hidden items (extracted from the button tex), we can fetch all hidden comments at once.
+*/
+function handleAltClick(event: DelegateEvent<MouseEvent, HTMLButtonElement>): void {
 	if (!event.altKey) {
 		return;
 	}
 
-	const form = event.delegateTarget;
-
-	const buttons = select.all('button', form);
-	const remainCommentsButton = buttons.find(button => /\s*\d+ hidden items\s*/.test(button.textContent!))!;
-	const remainComments = remainCommentsButton.textContent!.replace(/\D+/g, '');
-
-	/**
-	As per #2625:
-	Construct the new URL. This is undocumented, and may break at any time.
-	As of 12/08/2019, URLs look like this:
-	`/_render_node/MDExOlB1bGxSZXF1ZXN0MjE2MDA0MzU5/timeline/more_items?variables%5Bafter%5D=Y3Vyc29yOnYyOpPPAAABZemjg2AAqTQyMjE5MTk1MQ%3D%3D&variables%5Bbefore%5D=Y3Vyc29yOnYyOpPPAAABaENrVHAAqTQ1Mzc3MjMzNg%3D%3D&variables%5Bfirst%5D=60&variables%5BhasFocusedReviewComment%5D=false&variables%5BhasFocusedReviewThread%5D=false`
-	The key "variables[first]" in the URL query parameters appears to control how many additional comments are fetched.
-	Github appears to always set this to 60, but it can be increased up to the total number of hidden items.
-	By setting "variables[first]" to total number of hidden items (extracted from the button tex), we can fetch all hidden comments at once.
-	 */
+	const form = event.delegateTarget.form!;
+	const remainComments = form.textContent!.replace(/\D+/g, '');
 	const url = new URL(form.action);
 	url.searchParams.set('variables[first]', remainComments);
 	form.action = url.href;
 }
 
 function init(): void {
-	delegate('.ajax-pagination-form', 'click', handleAltClick);
+	delegate('.ajax-pagination-form button[type="submit"]', 'click', handleAltClick);
 }
 
 features.add({
