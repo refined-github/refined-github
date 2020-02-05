16e05b43398f5b7c354a9f9a3a91587bcb43fc1f
Kai Hao <kevin830726@gmail.com>
2020-02-05 17:43:13 +0800

    Switch to use undocumented API

--- source/features/expand-all-hidden-comments.ts
+++ source/features/expand-all-hidden-comments.ts
@@ -2,41 +2,37 @@ import select from 'select-dom';
 import delegate, {DelegateEvent} from 'delegate-it';
 import features from '../libs/features';
 
-const expanderSelector = '.ajax-pagination-form';
-const expanderButtonSelector = `${expanderSelector} .ajax-pagination-btn`;
-const containerSelector = '#js-progressive-timeline-item-container';
+const EXPANDER_SELECTOR = '.ajax-pagination-form';
+const REMAIN_COMMENTS_REGEX = /\s*(\d+) hidden items\s*/;
 
-function observe(expanderElement: HTMLElement): void {
-	// Waits for the next loaded comments and clicks on any additional "Load more..." buttons it finds
-	const expandingCodeObserver = new MutationObserver(([mutation]) => {
-		const expandButton = select(expanderButtonSelector, mutation.target as Element);
-
-		if (expandButton) {
-			expandButton.click();
-
-			// Recursively observe the returned list
-			observe(expandButton);
-		}
-
-		expandingCodeObserver.disconnect();
-	});
-
-	expandingCodeObserver.observe(
-		expanderElement.closest(containerSelector)!,
-		{childList: true}
-	);
-}
-
-function handleAltClick(event: DelegateEvent<MouseEvent, HTMLButtonElement>): void {
+function handleAltClick(event: DelegateEvent<MouseEvent, HTMLFormElement>): void {
 	if (!event.altKey) {
 		return;
 	}
 
-	observe(event.delegateTarget);
+	const form = event.delegateTarget;
+
+	const buttons = select.all('button', form);
+	const remainCommentsButton = buttons.find(button => REMAIN_COMMENTS_REGEX.test(button.textContent!))!;
+	const [remainComments] = REMAIN_COMMENTS_REGEX.exec(remainCommentsButton.textContent!)!;
+
+	/**
+	As per #2625:
+	Construct the new URL. This is undocumented, and may break at any time.
+	As of 12/08/2019, URLs look like this:
+	`/_render_node/MDExOlB1bGxSZXF1ZXN0MjE2MDA0MzU5/timeline/more_items?variables%5Bafter%5D=Y3Vyc29yOnYyOpPPAAABZemjg2AAqTQyMjE5MTk1MQ%3D%3D&variables%5Bbefore%5D=Y3Vyc29yOnYyOpPPAAABaENrVHAAqTQ1Mzc3MjMzNg%3D%3D&variables%5Bfirst%5D=60&variables%5BhasFocusedReviewComment%5D=false&variables%5BhasFocusedReviewThread%5D=false`
+	The key "variables[first]" in the URL query parameters appears to control how many additional comments are fetched.
+	Github appears to always set this to 60, but it can be increased up to the total number of hidden items.
+	By setting "variables[first]" to total number of hidden items (extracted from the button tex), we can fetch all hidden comments at once.
+	 */
+	const actionPath = form.getAttribute('action')!;
+	const url = new URL(actionPath, location.origin);
+	url.searchParams.set('variables[first]', remainComments);
+	form.setAttribute('action', url.pathname + url.search);
 }
 
 function init(): void {
-	delegate(expanderSelector, 'click', handleAltClick);
+	delegate(EXPANDER_SELECTOR, 'click', handleAltClick);
 }
 
 features.add({
