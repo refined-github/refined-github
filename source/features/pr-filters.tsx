7051c85b0820469059724277eefa92c4573937b8
Flo Edelmann <florian-edelmann@online.de>
2020-02-13 14:46:32 +0100

    Fix some spacing again, because GitHub likes to change stuff around

--- source/features/pr-filters.tsx
+++ source/features/pr-filters.tsx
@@ -108,7 +108,7 @@ async function addChecksFilter(): Promise<void> {
 		addDropdownItem(dropdown, status, 'status', status.toLowerCase());
 	}
 
-	reviewsFilter.after(' ', checksFilter);
+	reviewsFilter.after(checksFilter);
 }
 
 function init(): void {
