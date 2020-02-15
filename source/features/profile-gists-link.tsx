0a425e39654ec80576ed8813eca44c39b4e5b987
Federico Brigante <opensource@bfred.it>
2020-02-16 02:54:13 +0800

    Faster `profile-gists-link`

--- source/features/profile-gists-link.tsx
+++ source/features/profile-gists-link.tsx
@@ -1,13 +1,27 @@
 import './profile-gists-link.css';
 import React from 'dom-chef';
-import select from 'select-dom';
+import elementReady from 'element-ready';
+import cache from 'webext-storage-cache';
 import * as api from '../libs/api';
 import features from '../libs/features';
 import {getCleanPathname} from '../libs/utils';
 import {isEnterprise} from '../libs/page-detect';
 
+const getGistCount = cache.function(async (username: string): Promise<number> => {
+	const {user} = await api.v4(`
+		user(login: "${username}") {
+			gists(first: 0) {
+				totalCount
+			}
+		}
+	`);
+	return user.gists.totalCount;
+}, {
+	cacheKey: ([username]) => 'gist-count:' + username
+});
+
 async function init(): Promise<false | void> {
-	const container = select('body.page-profile .UnderlineNav-body');
+	const container = await elementReady('body.page-profile .UnderlineNav-body');
 
 	if (!container) {
 		return false;
@@ -18,10 +32,11 @@ async function init(): Promise<false | void> {
 	const link = <a href={href} className="UnderlineNav-item" role="tab" aria-selected="false">Gists </a>;
 	container.append(link);
 
-	const userData = await api.v3(`users/${username}`);
-	if (userData.public_gists) {
-		link.append(<span className="Counter hide-lg hide-md hide-sm">{userData.public_gists}</span>);
-	}
+	link.append(
+		<span className="Counter hide-lg hide-md hide-sm">
+			{await getGistCount(username)}
+		</span>
+	);
 }
 
 features.add({
@@ -31,6 +46,6 @@ features.add({
 	include: [
 		features.isUserProfile
 	],
-	load: features.onAjaxedPages,
+	load: features.nowAndOnAjaxedPages,
 	init
 });
