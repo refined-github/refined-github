f44be0966bce140bead44284f9de1fa00190cf3b
Yakov Kantor <16872793+yakov116@users.noreply.github.com>
2020-02-17 23:16:48 -0500

    Updates

--- source/features/pinned-issues-update-time.tsx
+++ source/features/pinned-issues-update-time.tsx
@@ -1,60 +1,45 @@
-import clockIcon from 'octicon/clock.svg';
-import features from '../libs/features';
-import * as api from '../libs/api';
-import {getRepoGQL} from '../libs/utils';
 import React from 'dom-chef';
 import select from 'select-dom';
+import clockIcon from 'octicon/clock.svg';
+import * as api from '../libs/api';
+import features from '../libs/features';
+import { getRepoGQL } from '../libs/utils';
 
-async function init(): Promise<false | void | any> {
-	for (const link of select.all<HTMLAnchorElement | any>('.pinned-issue-item')) {
-		const issueNUmber: any = Number((select<any>('a.link-gray-dark.no-underline.h4[href]', link)).href.split('/').slice(-1).pop());
-		const [lastCommented, lastUpdated] = await getLastCommented(issueNUmber);
-		if (select.exists('a[aria-label*="comment"]', link)) {
-			const now = new Intl.DateTimeFormat('default', {
-				month: 'numeric',
-				day: 'numeric',
-				year: 'numeric'
-			}).format(new Date(lastCommented));
-			const updatedTitle = `Last Commented on ${now}`;
-			select<HTMLAnchorElement | any>('a[aria-label*="comment"]', link).title = updatedTitle;
-		}
+async function init(): Promise<false | void> {
+	for (const pinnedIssue of select.all('.pinned-issue-item')) {
+		const issueNumber = select('.opened-by', pinnedIssue)!.firstChild!.textContent!.replace(/\D/g, '');
+		const lastUpdated = await getLastUpdated(issueNumber);
+		const lastUpdatedTitle = new Intl.DateTimeFormat('default', {
+			month: 'short',
+			day: 'numeric',
+			year: 'numeric',
+			hour: '2-digit',
+			minute: 'numeric',
+			timeZoneName: 'short'
+		}).format(new Date(lastUpdated));
 
-		const bottomBar: any = (select.all(['.open', '.closed'], link))[0].parentElement;
+		const bottomBar = select.all(['.open', '.closed'], pinnedIssue)[0].parentElement!;
 
 		bottomBar.append(
 			<span className="d-none d-md-inline">
 				<span className="issue-meta-section ml-2">
-					{clockIcon()} updated <relative-time datetime={lastUpdated} title={lastUpdated}/>
+					{clockIcon()} updated <relative-time datetime={lastUpdated} title={lastUpdatedTitle}/>
 				</span>
 			</span>
 		);
 	}
 }
 
-const getLastCommented = async (issue: number): Promise<string | false | any> => {
+const getLastUpdated = async (issue: string): Promise<string | false | any> => {
 	const {repository} = await api.v4(`
 		repository(${getRepoGQL()}) {
-			    issue (number:${issue}){
-      updatedAt
-      comments(last: 1) {
-        nodes {
-          updatedAt
-        }
-      }
-    }
+			issue (number:${issue}){
+    			updatedAt
+    		}
 		}
 	`);
-	const {nodes} = repository.issue.comments;
-	let lastCommented;
-	if (nodes.length > 0) {
-		lastCommented = nodes[0].updatedAt;
-	} else {
-		lastCommented = [];
-	}
 
-	const lastUpdated = repository.issue.updatedAt;
-	// Otherwise just use the latest
-	return [lastCommented, lastUpdated];
+	return repository.issue.updatedAt;
 };
 
 features.add({
