2ca745f0380a9dcb4f605d6bce228020eb4ab871
Yakov Kantor <16872793+yakov116@users.noreply.github.com>
2020-02-22 23:14:49 -0500

    Fix usercard hover.
    The CSS selector is no longer there

--- source/features/user-local-time.tsx
+++ source/features/user-local-time.tsx
@@ -101,7 +101,13 @@ function init(): void {
 
 		// Adding the time element might change the height of the hovercard and thus break its positioning
 		const hovercardHeight = hovercard.offsetHeight;
-		select('div.d-flex.mt-3 > div.overflow-hidden.ml-3:not([data-hovercard-type="contributors"]', hovercard)!.append(container);
+
+		const userHovercard = select('div.d-flex.mt-3 > div.overflow-hidden.ml-3', hovercard)!;
+		if (!userHovercard) {
+			return;
+		}
+
+		userHovercard.append(container);
 
 		if (hovercard.matches('.Popover-message--bottom-right, .Popover-message--bottom-left')) {
 			const diff = hovercard.offsetHeight - hovercardHeight;
