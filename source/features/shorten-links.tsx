062e3ca8ec5d7f4e6a07d2776fb50230f02354fb
Kid <44045911+kidonng@users.noreply.github.com>
2020-02-24 14:37:21 +0800

    Update shorten-links.tsx

--- source/features/shorten-links.tsx
+++ source/features/shorten-links.tsx
@@ -2,15 +2,8 @@ import select from 'select-dom';
 import {applyToLink} from 'shorten-repo-url';
 import features from '../libs/features';
 import {linkifiedURLClass} from '../libs/dom-formatters';
-import {isGlobalSearchResults} from '../libs/page-detect'
 
 function init(): void {
-  if (isGlobalSearchResults()) {
-		for (const a of select.all('.repo-list-item p a')) {
-			a.classList.add(linkifiedURLClass)
-		}
-	}
-
 	for (const a of select.all<HTMLAnchorElement>(`a[href]:not(.${linkifiedURLClass})`)) {
 		applyToLink(a, location.href);
 	}
@@ -21,5 +14,9 @@ features.add({
 	description: 'Shortens URLs and repo URLs to readable references like "_user/repo/.file@`d71718d`".',
 	screenshot: 'https://user-images.githubusercontent.com/1402241/27252232-8fdf8ed0-538b-11e7-8f19-12d317c9cd32.png',
 	load: features.onAjaxedPages,
+	exclude: [
+		// Due to GitHub’s bug: #2828
+		features.isGlobalSearchResults
+	],
 	init
 });
