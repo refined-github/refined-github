d4410607df2c7b6c70abf9b68de39a1e2b3f5412
Dane David <dane@surveysparrow.com>
2020-02-24 10:56:16 +0530

    Remove keydown handler in favor of beforeunload & pjax:start event handlers

--- source/features/preserve-file-finder-term.tsx
+++ source/features/preserve-file-finder-term.tsx
@@ -1,18 +1,14 @@
 import features from '../libs/features';
 import select from 'select-dom';
-import {poll} from '../libs/utils';
-import delegate, {DelegateEvent, DelegateSubscription} from 'delegate-it';
+import elementReady from 'element-ready';
 
-let delegated: DelegateSubscription | null = null;
 const FILE_FINDER_INPUT_SELECTOR = '.js-tree-finder > .breadcrumb > #tree-finder-field';
 
-const persistText = (value: string): void => {
-	sessionStorage.setItem('rgh-file-finder-term', value);
-};
-
-const inputHandler = (event: DelegateEvent<InputEvent, HTMLInputElement>): void => {
-	const value: string = (event.delegateTarget).value;
-	persistText(value);
+const unloadHandler = (): void => {
+	const inputElement = select<HTMLInputElement>(FILE_FINDER_INPUT_SELECTOR);
+	if (inputElement) {
+		sessionStorage.setItem('rgh-file-finder-term', inputElement.value);
+	}
 };
 
 // Set the input field value & trigger event
@@ -20,10 +16,8 @@ const setValueInField = async (): Promise<void> => {
 	const value: string = sessionStorage.getItem('rgh-file-finder-term') ?? '';
 	const inputElement = select<HTMLInputElement>(FILE_FINDER_INPUT_SELECTOR);
 	if (inputElement) {
-		if (inputElement.value) {		// Handle case where text is present in buffer
-			persistText(inputElement.value);
-		} else if (value) {
-			await poll(() => select('.js-tree-browser-results > li'), 100);	// For search to work
+		if (!inputElement.value && value) {
+			await elementReady('.js-tree-browser-results > li', {stopOnDomReady: false});	// For search to work
 			inputElement.value = value;
 			inputElement.dispatchEvent(new Event('input')); // Manually trigger event to trigger search
 		}
@@ -32,14 +26,13 @@ const setValueInField = async (): Promise<void> => {
 
 async function init(): Promise<void> {
 	await setValueInField();
-	delegated = delegate(FILE_FINDER_INPUT_SELECTOR, 'input', inputHandler);
+	window.addEventListener('beforeunload', unloadHandler);
+	window.addEventListener('pjax:start', unloadHandler);
 }
 
 function deinit(): void {
-	if (delegated) {
-		delegated.destroy();
-		delegated = null;
-	}
+	window.removeEventListener('beforeunload', unloadHandler);
+	window.removeEventListener('pjax:start', unloadHandler);
 }
 
 features.add({
