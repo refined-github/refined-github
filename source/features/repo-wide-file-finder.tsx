99e3f23de5dae683efa5e365a69d11a95ef568c9
Dane David <dane@surveysparrow.com>
2020-02-24 10:09:30 +0530

    Remove unnecessary variables, excludes & deinit from repo-wide-file-finder

--- source/features/repo-wide-file-finder.tsx
+++ source/features/repo-wide-file-finder.tsx
@@ -5,29 +5,15 @@ import {getRepoURL} from '../libs/utils';
 import getDefaultBranch from '../libs/get-default-branch';
 
 async function init(): Promise<void> {
-	const defaultBranch = await getDefaultBranch();
-	const pjaxContainer = select('#js-repo-pjax-container');
-	const fileFinderButtonExists = select.exists('[data-hotkey="t"]');
-	const hiddenButton: HTMLElement = (
-		<a
-			hidden
-			id="rgh-file-finder-hidden-btn"
-			data-hotkey="t"
-			data-pjax="true"
-			href={`/${getRepoURL()}/find/${defaultBranch}`}
-		/>
-	);
-	if (!fileFinderButtonExists) {
-    pjaxContainer?.appendChild(hiddenButton);
-	}
-}
-
-function deinit(): void {
-	const pjaxContainer = select('#js-repo-pjax-container');
-	const hiddenButton = select<HTMLAnchorElement>('#rgh-file-finder-hidden-btn');
-
-	if (hiddenButton) {
-    pjaxContainer?.removeChild(hiddenButton);
+	if (!select.exists('[data-hotkey="t"]')) {
+    select('#js-repo-pjax-container')?.appendChild(
+	<a
+		hidden
+		data-hotkey="t"
+		data-pjax="true"
+		href={`/${getRepoURL()}/find/${await getDefaultBranch()}`}
+	/>
+		);
 	}
 }
 
@@ -40,13 +26,6 @@ features.add({
 		features.isPR,
 		features.isIssue
 	],
-	exclude: [
-		features.isRepoRoot,
-		features.isRepoTree,
-		features.isSingleFile,
-		features.isFileFinder
-	],
 	load: features.onAjaxedPages,
-	init,
-	deinit
+	init
 });
