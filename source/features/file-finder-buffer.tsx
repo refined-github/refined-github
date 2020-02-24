9d204e273febeb67d19ed850622bc1b408f8b152
Dane David <dane@surveysparrow.com>
2020-02-24 13:35:28 +0530

    Remove keydown handler in favor of hidden input in file-finder-buffer

--- source/features/file-finder-buffer.tsx
+++ source/features/file-finder-buffer.tsx
@@ -1,47 +1,33 @@
+import './file-finder-buffer.css';
+import React from 'dom-chef';
 import features from '../libs/features';
 import select from 'select-dom';
 
 const FILE_FINDER_INPUT_SELECTOR = '.js-tree-finder > .breadcrumb > #tree-finder-field';
 
-let fileFinderBuffer = '';
-
-const isValidCharacter = (char: string): boolean => (
-	char.length === 1
-) && ((
-	char >= 'a' &&
-  char <= 'z'
-) || (
-	char >= 'A' &&
-  char <= 'Z'
-) || (
-	char >= '0' &&
-  char <= '9'
-));
-
-const keyDownHandler = ({key, target}: KeyboardEvent): void => {
-	const nodeName = (target as Element).nodeName;
-	if (nodeName !== 'INPUT' && nodeName !== 'TEXTAREA' && isValidCharacter(key)) {
-		fileFinderBuffer = `${fileFinderBuffer}${key}`;
-	}
-};
-
 const pjaxStartHandler = ((event: CustomEvent): void => {
 	const destinationURL: string = event.detail?.url || '';
 	if (destinationURL.includes('/find/')) {
-		window.addEventListener('keydown', keyDownHandler);
+		const hiddenInput = select<HTMLInputElement>('#rgh-file-finder-buffer') ?? document.body.insertBefore(
+			<input
+				type="text"
+				className="visuallyhidden"
+				id="rgh-file-finder-buffer"
+			/>,
+			document.body.firstChild
+		);
+		hiddenInput.focus();
 	}
 }) as EventListener; // Explicit type cast. See https://github.com/microsoft/TypeScript/issues/28357#issuecomment-436484705
 
 const pjaxCompleteHandler = (): void => {
-	const input = select<HTMLInputElement>(FILE_FINDER_INPUT_SELECTOR);
-	if (input) {
-		input.value = fileFinderBuffer.slice(1); // First character is the 't' pressed
-		input.dispatchEvent(new Event('input')); // Manually trigger event to trigger search
+	const fileFinderInput = select<HTMLInputElement>(FILE_FINDER_INPUT_SELECTOR);
+	const hiddenInput = select<HTMLInputElement>('#rgh-file-finder-buffer');
+	if (hiddenInput && fileFinderInput) {
+		fileFinderInput.value = hiddenInput.value;
+		fileFinderInput.dispatchEvent(new Event('input')); // Manually trigger event to trigger search
+		hiddenInput.value = '';
 	}
-
-	fileFinderBuffer = '';
-
-	window.removeEventListener('keydown', keyDownHandler);
 };
 
 function init(): void {
@@ -59,12 +45,7 @@ features.add({
 	description: 'Buffering for search term pressed after `t`',
 	screenshot: false,
 	include: [
-		features.isRepoDiscussionList,
-		features.isPR,
-		features.isIssue,
-		features.isRepoRoot,
-		features.isRepoTree,
-		features.isSingleFile
+		features.isRepo
 	],
 	load: features.onAjaxedPages,
 	init,
