b388d93562784f9c73f718b7a32085f8b448dc77
Yakov Kantor <16872793+yakov116@users.noreply.github.com>
2020-02-25 23:35:54 -0500

    add back mark-merge-commits-in-list

---
+++ source/features/mark-merge-commits-in-list.tsx
@@ -0,0 +1,50 @@
+import './mark-merge-commits-in-list.css';
+import select from 'select-dom';
+import * as api from '../libs/api';
+import features from '../libs/features';
+import {getRepoGQL} from '../libs/utils';
+
+const getLastUpdated = async (commits: any): any => {
+	const {repository} = await api.v4(`
+		repository(${getRepoGQL()}) {
+			${commits.map((commit: string) => `
+				${api.escapeKey(commit)}: object(expression: "${commit}") {
+						... on Commit {
+							parents{
+								totalCount
+							}
+						}
+					}
+			`).join('\n')}
+		}
+	`);
+
+	return repository;
+};
+
+function getCommitHash(commit: any): any {
+	return commit.dataset.channel.split(':')[3];
+}
+
+async function init(): Promise<void | false> {
+	const pageCommits = select.all('li.commit');
+	const parentCommitCount = await getLastUpdated(pageCommits.map(getCommitHash));
+	for (const commit of pageCommits) {
+		const commitHash = getCommitHash(commit);
+		const {totalCount} = parentCommitCount[api.escapeKey(commitHash)].parents;
+		if (totalCount === 2) {
+			commit.classList.add('refined-github-merge-commit');
+		}
+	}
+}
+
+features.add({
+	id: __featureName__,
+	description: 'Dims merge commits in commits list.',
+	screenshot: false,
+	include: [
+		features.isCommitList
+	],
+	load: features.onAjaxedPages,
+	init
+});
