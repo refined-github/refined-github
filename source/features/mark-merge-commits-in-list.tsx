b58ea2b2533aae1b9c7f1381fc36d0b722f56649
Yakov Kantor <16872793+yakov116@users.noreply.github.com>
2020-02-27 09:07:52 -0500

    Feedback

--- source/features/mark-merge-commits-in-list.tsx
+++ source/features/mark-merge-commits-in-list.tsx
@@ -1,6 +1,6 @@
 import './mark-merge-commits-in-list.css';
 import select from 'select-dom';
-import gitmerge from 'octicon/git-merge.svg';
+import gitPullRequest from 'octicon/git-pull-request.svg';
 import * as api from '../libs/api';
 import features from '../libs/features';
 import {getRepoGQL} from '../libs/utils';
@@ -10,29 +10,26 @@ const getMergeCommits = async (commits: string[]): Promise<string[]> => {
 		repository(${getRepoGQL()}) {
 			${commits.map((commit: string) => `
 				${api.escapeKey(commit)}: object(expression: "${commit}") {
-					... on Commit {
-							parents{
-								totalCount
-							}
+				... on Commit {
+						parents {
+							totalCount
 						}
 					}
+				}
 			`).join('\n')}
 		}
 	`);
 
-	return filterCommits(repository);
-};
-
-function filterCommits(commits: object): string[] {
 	const mergeCommits = [];
-	for (const commit of Object.entries(commits)) {
-		if (commit[1].parents.totalCount === 2) {
-			mergeCommits.push(commit[0]);
+	// @ts-ignore
+	for (const [commit, {parents}] of Object.entries(repository)) {
+		if (parents.totalCount === 2) {
+			mergeCommits.push(commit.slice(1));
 		}
 	}
 
 	return mergeCommits;
-}
+};
 
 function getCommitHash(commit: HTMLElement): string {
 	return (commit.dataset.channel as string).split(':')[3];
@@ -42,11 +39,9 @@ async function init(): Promise<void | false> {
 	const pageCommits = select.all('li.commit');
 	const mergeCommits = await getMergeCommits(pageCommits.map(getCommitHash));
 	for (const commit of pageCommits) {
-		const commitHash = api.escapeKey(getCommitHash(commit));
-
-		if (mergeCommits.includes(commitHash)) {
+		if (mergeCommits.includes(getCommitHash(commit))) {
 			commit.classList.add('rgh-merge-commit');
-			select('.commit-title', commit)!.prepend(gitmerge());
+			select('.commit-title', commit)!.prepend(gitPullRequest());
 		}
 	}
 }
