2bb813a22dcdad1b5472ebd7520f556ed3767b3b
Kid <44045911+kidonng@users.noreply.github.com>
2020-02-28 18:50:47 +0800

    Don't apply `shorten-links` on search results (#2828)

--- source/features/shorten-links.tsx
+++ source/features/shorten-links.tsx
@@ -14,5 +14,9 @@ features.add({
 	description: 'Shortens URLs and repo URLs to readable references like "_user/repo/.file@`d71718d`".',
 	screenshot: 'https://user-images.githubusercontent.com/1402241/27252232-8fdf8ed0-538b-11e7-8f19-12d317c9cd32.png',
 	load: features.onAjaxedPages,
+	exclude: [
+		// Due to GitHub’s bug: #2828
+		features.isGlobalSearchResults
+	],
 	init
 });
