5652e7b4ce548941fe75bf52a96a62117699e8fa
Federico Brigante <opensource@bfred.it>
2020-02-28 17:40:54 +0800

    Lint `preserve-file-finder-term`

--- source/features/preserve-file-finder-term.tsx
+++ source/features/preserve-file-finder-term.tsx
@@ -1,24 +1,24 @@
-import features from '../libs/features';
 import select from 'select-dom';
 import elementReady from 'element-ready';
+import features from '../libs/features';
 
-const unloadHandler = (): void => {
+function unloadHandler(): void {
 	const inputElement = select<HTMLInputElement>('#tree-finder-field');
 	if (inputElement) {
 		sessionStorage.setItem('rgh-file-finder-term', inputElement.value);
 	}
-};
+}
 
 // Set the input field value & trigger event
-const setValueInField = async (): Promise<void> => {
-	const value = sessionStorage.getItem('rgh-file-finder-term');
+async function setValueInField(): Promise<void> {
+	const preservedValue = sessionStorage.getItem('rgh-file-finder-term');
 	const inputElement = select<HTMLInputElement>('#tree-finder-field');
-	if (inputElement && !inputElement.value && value) {
+	if (inputElement && !inputElement.value && preservedValue) {
 		await elementReady('.js-tree-browser-results > li', {stopOnDomReady: false});	// For search to work
-		inputElement.value = value;
-		inputElement.dispatchEvent(new Event('input')); // Manually trigger event to trigger search
+		inputElement.value = preservedValue;
+		inputElement.dispatchEvent(new Event('input')); // Trigger search
 	}
-};
+}
 
 function init(): void {
 	setValueInField();
