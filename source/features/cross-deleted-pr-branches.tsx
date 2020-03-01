3aa538b6f15223796add22c72936878a2c024d73
Yakov Kantor <16872793+yakov116@users.noreply.github.com>
2020-03-01 09:14:33 -0500

    Feedback
    Also added that the colon should have a strike through

--- source/features/cross-deleted-pr-branches.tsx
+++ source/features/cross-deleted-pr-branches.tsx
@@ -1,20 +1,36 @@
+import React from 'dom-chef';
 import select from 'select-dom';
 import features from '../libs/features';
+import {wrap} from '../libs/dom-utils';
 
 function init(): void {
-	let deletedBranch: string | undefined;
 	const lastBranchAction = select.last('.user-select-contain > span:not(.base-ref)');
-
-	if (lastBranchAction?.closest('.TimelineItem-body')?.textContent?.includes('deleted')) {
-		deletedBranch = lastBranchAction.textContent!.trim();
+	if (!lastBranchAction || select.exists('.post-merge-message')) {
+		return;
 	}
+
+	if (!lastBranchAction.closest('.TimelineItem-body')!.textContent!.includes('deleted')) {
+		return;
+	}
+
+	const deletedBranchName = lastBranchAction.textContent!.trim();
+	const repoRoot = (select('.head-ref a')! as HTMLAnchorElement).href.replace(/\/tree.*$/, '');
 
 	for (const element of select.all('.commit-ref')) {
 		const branchName = element.textContent!.trim();
-		if (branchName !== 'unknown repository' && branchName === deletedBranch) {
+		if (branchName !== 'unknown repository' && branchName === deletedBranchName) {
 			for (const deletedBranchElement of select.all('span', element)) {
 				deletedBranchElement.title = 'Deleted';
 				deletedBranchElement.style.textDecoration = 'line-through';
+				// The head-ref already has an a element
+				if (element.classList.contains('head-ref')) {
+					const linkElement = select('a', element)!;
+					linkElement.href = repoRoot;
+					linkElement.style.textDecoration = 'line-through';
+					linkElement.classList.remove('no-underline');
+				} else {
+					wrap(element, <a href={repoRoot}/>);
+				}
 			}
 		}
 	}
