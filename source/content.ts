206a1a338074f8c4ad6abfa2319bb7d0ecdee3c1
yakov116 <16872793+yakov116@users.noreply.github.com>
2020-03-03 05:06:53 -0500

    Add `pinned-issues-update-time` feature (#2795)
    Co-Authored-By: Federico Brigante <opensource@bfred.it>

--- source/content.ts
+++ source/content.ts
@@ -65,6 +65,7 @@ import './features/navigate-pages-with-arrow-keys';
 import './features/global-discussion-list-filters';
 import './features/filter-comments-by-you';
 import './features/sort-issues-by-update-time'; // Must be after global-discussion-list-filters and filter-comments-by-you
+import './features/pinned-issues-update-time';
 import './features/latest-tag-button';
 import './features/default-branch-button';
 import './features/faster-pr-diff-options';
---
+++ source/features/pinned-issues-update-time.tsx
@@ -0,0 +1,60 @@
+import React from 'dom-chef';
+import cache from 'webext-storage-cache';
+import select from 'select-dom';
+import clockIcon from 'octicon/clock.svg';
+import * as api from '../libs/api';
+import features from '../libs/features';
+import {getRepoGQL, getRepoURL} from '../libs/utils';
+
+interface IssueInfo {
+	updatedAt: string;
+}
+
+const getLastUpdated = cache.function(async (issueNumbers: string[]): Promise<Record<string, IssueInfo>> => {
+	const {repository} = await api.v4(`
+		repository(${getRepoGQL()}) {
+			${issueNumbers.map(number => `
+				${api.escapeKey(number)}: issue(number: ${number}) {
+					updatedAt
+				}
+			`).join('\n')}
+		}
+	`);
+
+	return repository;
+}, {
+	expiration: 1,
+	cacheKey: () => __featureName__ + ':' + getRepoURL()
+});
+
+function getPinnedIssueNumber(pinnedIssue: HTMLElement): string {
+	return select('.opened-by', pinnedIssue)!.firstChild!.textContent!.replace(/\D/g, '');
+}
+
+async function init(): Promise<void | false> {
+	const pinnedIssues = select.all('.pinned-issue-item');
+	if (pinnedIssues.length === 0) {
+		return false;
+	}
+
+	const lastUpdated: Record<string, IssueInfo> = await getLastUpdated(pinnedIssues.map(getPinnedIssueNumber));
+	for (const pinnedIssue of pinnedIssues) {
+		const issueNumber = getPinnedIssueNumber(pinnedIssue);
+		const {updatedAt} = lastUpdated[api.escapeKey(issueNumber)];
+		pinnedIssue.lastElementChild!.append(
+			<span className="ml-3 text-gray">{clockIcon()}</span>,
+			<span className="text-gray text-small"> Updated <relative-time datetime={updatedAt}/></span>
+		);
+	}
+}
+
+features.add({
+	id: __featureName__,
+	description: 'Adds the updated time to pinned issues.',
+	screenshot: 'https://user-images.githubusercontent.com/1402241/75525936-bb524700-5a4b-11ea-9225-466bda58b7de.png',
+	include: [
+		features.isRepoIssueList
+	],
+	load: features.onAjaxedPages,
+	init
+});
