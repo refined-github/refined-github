d6f7f056bcce687bc3d00c5824b6d7683695c4d4
Yakov Kantor <16872793+yakov116@users.noreply.github.com>
2020-03-04 19:26:23 -0500

    Actually implement what was asked

--- source/features/lines-changed-per-commit.tsx
+++ source/features/lines-changed-per-commit.tsx
@@ -3,51 +3,50 @@ import * as api from '../libs/api';
 import features from '../libs/features';
 import {getRepoGQL} from '../libs/utils';
 import React from 'dom-chef';
+import elementReady from 'element-ready';
 
-const getCommitChanges = async (commits: string[]): Promise<string[]> => {
+const getCommitChanges = async (commit: string): Promise<number[]> => {
 	const {repository} = await api.v4(`
 		repository(${getRepoGQL()}) {
-			${commits.map((commit: string) => `
-				${api.escapeKey(commit)}: object(expression: "${commit}") {
-				... on Commit {
+            object(expression: "${commit}") {
+                ... on Commit {
                     additions
                     deletions
-					}
-				}
-			`).join('\n')}
+                    }
+                }
 		}
-	`);
-
-	return repository;
+    `);
+	// @ts-ignore
+	const {additions, deletions} = repository.object;
+	return [additions, deletions];
 };
 
-function getCommitHash(commit: HTMLElement): string {
-	return (commit.dataset.channel as string).split(':')[3];
-}
-
 async function init(): Promise<void | false> {
-	const pageCommits = select.all('li.commit:not(.rgh-merge-commit)');
-	const commitChanges = await getCommitChanges(pageCommits.map(getCommitHash));
-	for (const commit of pageCommits) {
-		// @ts-ignore
-		const {additions, deletions} = commitChanges[api.escapeKey(getCommitHash(commit))];
-		const totalLinesChanged = `${Number(additions) + Number(deletions)} Lines Changed`;
-		(select('.commit-indicator', commit)! ?? select('relative-time', commit)).append(
-			<span className="ml-2 diffstat tooltipped tooltipped-s" aria-label={totalLinesChanged}>
-				<span className="text-green">+{additions}</span>
-				<span className="ml-1 mr-2 text-red">−{deletions}</span>
+	const pageCommit = (await elementReady('.sha.user-select-contain'))!;
+	const commitSha = String(pageCommit.textContent);
+	const [additions, deletions] = await getCommitChanges(commitSha);
+	const totalLinesChanged = additions + deletions;
+	const totalLinesChangedText = `${totalLinesChanged} Line${totalLinesChanged > 1 ? 's' : ''} Changed`;
+	select('.diffstat')!.replaceWith(
+		<span className="ml-2 diffstat tooltipped tooltipped-s" aria-label={totalLinesChangedText}>
+			<span className="text-green">+{additions}</span>
+			<span className="ml-1 mr-2 text-red">−{deletions}</span>
+			<span className="diffstat-block-neutral"/>
+			<span className="diffstat-block-neutral"/>
+			<span className="diffstat-block-neutral"/>
+			<span className="diffstat-block-neutral"/>
+			<span className="diffstat-block-neutral"/>
 
-			</span>);
-	}
+		</span>);
 }
 
 features.add({
 	id: __featureName__,
-	description: 'Add line changes per commit',
+	description: 'Add line changes on pr commits',
 	screenshot: 'https://user-images.githubusercontent.com/16872793/75835025-cb3f9300-5d8b-11ea-8126-d8778c71e467.png',
 	include: [
-		features.isCommitList
+		features.isPRCommit
 	],
-	load: features.onAjaxedPages,
+	load: features.nowAndOnAjaxedPages,
 	init
 });
