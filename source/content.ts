5e0603d1c12ed72b9ff79fb0fb7dfd0ae4c0c375
yakov116 <16872793+yakov116@users.noreply.github.com>
2020-03-06 08:27:40 -0500

    Update source/content.ts
    Co-Authored-By: Federico Brigante <github@bfred.it>

--- source/content.ts
+++ source/content.ts
@@ -164,7 +164,7 @@ import './features/bugs-tab';
 import './features/repo-wide-file-finder';
 import './features/preserve-file-finder-term';
 import './features/file-finder-buffer';
-import './features/lines-changed-per-commit';
+import './features/pr-commit-lines-changed';
 
 // Add global for easier debugging
 (window as any).select = select;
