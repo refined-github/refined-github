9cf076a16f9c3ca5eb748079439371a806835251
Federico Brigante <github@bfred.it>
2020-03-08 11:45:04 +0300

    Add screenshot to feature

--- source/features/align-issue-labels.tsx
+++ source/features/align-issue-labels.tsx
@@ -8,6 +8,6 @@ function init(): void {
 features.add({
 	id: __featureName__,
 	description: 'Aligns labels in lists to the left.',
-	screenshot: false,
+	screenshot: 'https://user-images.githubusercontent.com/1402241/28006237-070b8214-6581-11e7-94bc-2b01a007d00b.png',
 	init
 });
