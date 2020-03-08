9457eff9c6ec62ac13c853ba16184801258478cc
yakov116 <16872793+yakov116@users.noreply.github.com>
2020-03-08 04:46:32 -0400

    Allow `align-issue-labels` to be disabled (#2872)

---
+++ source/features/align-issue-labels.tsx
@@ -0,0 +1,13 @@
+import './align-issue-labels.css';
+import features from '../libs/features';
+
+function init(): void {
+	document.body.classList.add('rgh-align-issue-labels');
+}
+
+features.add({
+	id: __featureName__,
+	description: 'Aligns labels in lists to the left.',
+	screenshot: 'https://user-images.githubusercontent.com/1402241/28006237-070b8214-6581-11e7-94bc-2b01a007d00b.png',
+	init
+});
