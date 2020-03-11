a9ba465d4bcd9541a6ce34b41dfce7c45b654f52
Yakov Kantor <16872793+yakov116@users.noreply.github.com>
2020-03-11 09:21:07 -0400

    WIP

--- source/content.ts
+++ source/content.ts
@@ -123,6 +123,7 @@ import './features/update-pr-from-base-branch';
 import './features/hide-disabled-milestone-sorter';
 import './features/tag-changelog-link';
 import './features/link-to-file-in-file-history';
+import './features/deep-blame';
 import './features/clean-sidebar';
 import './features/release-download-count';
 import './features/open-issue-to-latest-comment';
---
+++ source/features/deep-blame.tsx
@@ -0,0 +1,69 @@
+import select from 'select-dom';
+import * as api from '../libs/api';
+import features from '../libs/features';
+import {getRepoGQL} from '../libs/utils';
+
+const filterMergeCommits = async (commits: string[]): Promise<AnyObject> => {
+	const {repository} = await api.v4(`
+		repository(${getRepoGQL()}) {
+			${commits.map((commit: string) => `
+				${api.escapeKey(commit)}: object(expression: "${commit}") {
+					... on Commit {
+						associatedPullRequests(last: 1) {
+							nodes {
+								mergeCommit {
+									oid
+								}
+								commits(last: 1) {
+									nodes {
+										commit {
+											oid
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			`).join('\n')}
+		}
+    `);
+
+	const lastCommits = {};
+	for (const [key, commit] of Object.entries<AnyObject>(repository)) {
+		const mergeCommit = (commit.associatedPullRequests.nodes[0].mergeCommit.oid as string);
+		const lastCommit = commit.associatedPullRequests.nodes[0].commits.nodes[0].commit.oid;
+		if (mergeCommit === key.slice(1)) {
+			lastCommits[mergeCommit] = lastCommit;
+		}
+	}
+
+	return lastCommits;
+};
+
+function getCommitHash(commit: HTMLElement): string {
+	return (commit.nextElementSibling! as HTMLAnchorElement).href.split('/').slice(-1)[0];
+}
+
+async function init(): Promise<void | false> {
+	const pullRequests = select.all('[data-hovercard-type="pull_request"]');
+	const commits = await filterMergeCommits([...new Set(pullRequests.map(getCommitHash))]);
+
+	for (const pullRequest of pullRequests) {
+		const test = commits[getCommitHash(pullRequest)];
+		const a = pullRequest.closest('.blame-commit.flex-self-stretch.mr-1');
+		console.log(a!.closest('.octicon.octicon-versions'));
+		console.log(pullRequest.parentElement?.closest('.octicon.octicon-versions'));
+	}
+}
+
+features.add({
+	id: __featureName__,
+	description: '',
+	screenshot: false,
+	include: [
+		features.isBlame
+	],
+	load: features.onAjaxedPages,
+	init
+});
