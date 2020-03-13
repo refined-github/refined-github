d7d94027e1b656304c4946c32ee53c196cb8ed2b
Yakov Kantor <16872793+yakov116@users.noreply.github.com>
2020-03-13 17:23:43 -0400

    Correctly match PR Number

--- source/features/deep-blame.tsx
+++ source/features/deep-blame.tsx
@@ -1,4 +1,5 @@
 import './deep-blame.css';
+import blurAccessibly from 'blur-accessibly';
 import cache from 'webext-storage-cache';
 import delegate, {DelegateEvent} from 'delegate-it';
 import React from 'dom-chef';
@@ -8,13 +9,14 @@ import * as api from '../libs/api';
 import features from '../libs/features';
 import {getRepoGQL, getReference} from '../libs/utils';
 
-const getPullRequestBlameCommit = cache.function(async (commit: string): Promise<string | false> => {
+const getPullRequestBlameCommit = cache.function(async (commit: string, prNumber: number): Promise<string | false> => {
 	const {repository} = await api.v4(`
 		repository(${getRepoGQL()}) {
 			object(expression: "${commit}") {
 				... on Commit {
 					associatedPullRequests(last: 1) {
 						nodes {
+							number
 							mergeCommit {
 								oid
 							}
@@ -34,8 +36,9 @@ const getPullRequestBlameCommit = cache.function(async (commit: string): Promise
 
 	const {nodes} = repository.object.associatedPullRequests;
 	const mergeCommit = nodes[0].mergeCommit.oid;
+	const pullRequestNumber = nodes[0].number;
 
-	if (mergeCommit === commit) {
+	if (pullRequestNumber === prNumber && mergeCommit === commit) {
 		return nodes[0].commits.nodes[0].commit.oid;
 	}
 
@@ -59,21 +62,31 @@ const githubSpinner = (
 
 async function redirectToBlameCommit(event: DelegateEvent<MouseEvent, HTMLLinkElement>): Promise<void | false> {
 	const blameLink = event.delegateTarget;
-	const {lineNumber, commit: pullRequestCommit} = blameLink.dataset;
+	const {lineNumber, commit: pullRequestCommit, prNumber} = blameLink.dataset;
 	if (blameLink.href && event.altKey) {
 		event.preventDefault();
 	}
 
 	blameLink.firstElementChild!.replaceWith(githubSpinner);
 
-	const prBlameCommit = await getPullRequestBlameCommit(String(pullRequestCommit));
+	const prBlameCommit = await getPullRequestBlameCommit(pullRequestCommit!, Number(prNumber));
 	if (!prBlameCommit) {
-		blameLink.firstElementChild!.remove();
+		if (blameLink.href) {
+			blameLink.setAttribute('aria-label', 'View blame prior to this change.');
+			blameLink.classList.remove('rgh-deep-blame');
+			blameLink.firstElementChild!.replaceWith(versionIcon());
+			// Hide tooltip after click, it’s shown on :focus
+			blurAccessibly(blameLink);
+		} else {
+			blameLink.firstElementChild!.remove();
+		}
+
+		alert('Unable to find linked Pull Request');
 		return;
 	}
 
-	const href = new URL(window.location.href.replace(String(getReference()), prBlameCommit));
-	href.hash = 'L' + String(lineNumber);
+	const href = new URL(location.href.replace(getReference()!, prBlameCommit));
+	href.hash = 'L' + lineNumber!;
 	location.href = String(href);
 }
 
@@ -87,18 +100,22 @@ async function init(): Promise<void | false> {
 		const currentParentElement = pullRequest.closest('.blame-hunk')!;
 		const versionsParent = select('.blob-reblame', currentParentElement);
 		const currentLineNumber = select('.js-line-number', currentParentElement)!.textContent!;
+		const prNumber = select('.issue-link', currentParentElement)!.textContent!.replace('#', '');
+		const commitHash = getCommitHash(pullRequest);
 
 		if (select.exists('a', versionsParent!)) {
 			const versionsElement = select('a', versionsParent!)!;
 			versionsElement.setAttribute('aria-label', 'View blame prior to this change. Press Alt to view the `deep-blame`');
 			versionsElement.classList.add('rgh-deep-blame');
-			versionsElement.dataset.commit = getCommitHash(pullRequest);
+			versionsElement.dataset.commit = commitHash;
 			versionsElement.dataset.lineNumber = currentLineNumber;
+			versionsElement.dataset.prNumber = prNumber;
 		} else {
 			versionsParent!.append(
 				<a
-					data-commit={getCommitHash(pullRequest)}
+					data-commit={commitHash}
 					data-line-number={currentLineNumber}
+					data-pr-number={prNumber}
 					aria-label="View `deep-blame` prior to this change"
 					className="reblame-link link-hover-blue no-underline tooltipped tooltipped-e d-inline-block pr-1 rgh-deep-blame"
 				> {versionIcon()}
