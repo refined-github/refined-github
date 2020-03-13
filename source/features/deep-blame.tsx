bf263c1fb73ceadbbd43b1c7c0c2e395b9ceed6c
Yakov Kantor <16872793+yakov116@users.noreply.github.com>
2020-03-12 23:27:17 -0400

    WIP add load on click only, very messy code
    Good Night

--- source/features/deep-blame.tsx
+++ source/features/deep-blame.tsx
@@ -1,68 +1,72 @@
 import './deep-blame.css';
 import select from 'select-dom';
 import * as api from '../libs/api';
-import elementReady from 'element-ready';
 import React from 'dom-chef';
 import features from '../libs/features';
 import {getRepoGQL, getReference} from '../libs/utils';
+import delegate from 'delegate-it';
 import versionIcon from 'octicon/versions.svg';
 
-const filterMergeCommits = async (commits: string[]): Promise<AnyObject> => {
+const filterMergeCommits = async (commit: string): Promise<AnyObject | undefined> => {
 	const {repository} = await api.v4(`
 		repository(${getRepoGQL()}) {
-			${commits.map((commit: string) => `
-				${api.escapeKey(commit)}: object(expression: "${commit}") {
-					... on Commit {
-						associatedPullRequests(last: 1) {
-							nodes {
-								mergeCommit {
-									oid
-								}
-								commits(last: 1) {
-									nodes {
-										commit {
-											oid
-										}
+			${api.escapeKey(commit)}: object(expression: "${commit}") {
+				... on Commit {
+					associatedPullRequests(last: 1) {
+						nodes {
+							mergeCommit {
+								oid
+							}
+							commits(last: 1) {
+								nodes {
+									commit {
+										oid
 									}
 								}
 							}
 						}
 					}
 				}
-			`).join('\n')}
+			}
 		}
 	`);
 
-	const lastCommits = new Map();
-	for (const [key, commit] of Object.entries<AnyObject>(repository)) {
-		const {nodes} = commit.associatedPullRequests;
-		const mergeCommit = nodes[0].mergeCommit.oid;
-		const lastCommit = nodes[0].commits.nodes[0].commit.oid;
-		if (mergeCommit === key.slice(1)) {
-			lastCommits.set(mergeCommit, lastCommit);
-		}
+	const {nodes} = repository[api.escapeKey(commit)].associatedPullRequests;
+	const mergeCommit = nodes[0].mergeCommit.oid;
+
+	if (mergeCommit === commit) {
+		return nodes[0].commits.nodes[0].commit.oid;
 	}
 
-	return lastCommits;
+	return undefined;
 };
 
-async function getDeepBlame(): Promise<void | false> {
+function getCommitHash(commit: HTMLElement): string {
+	return (commit.nextElementSibling! as HTMLAnchorElement).href.split('/').slice(-1)[0];
+}
+
+async function getDeepBlame(event): Promise<void | false> {
+	const currentParentElement = event.target.closest('.blame-hunk');
+	const currentLineNumber = select('.js-line-number', currentParentElement)!.textContent!;
+	const pullRequestCommit = event.target.parentElement.dataset.commit;
+	const prBlameCommit = await filterMergeCommits(pullRequestCommit);
+
+	const href = new URL(window.location.href.replace(String(getReference()), prBlameCommit));
+	href.hash = 'L' + currentLineNumber;
+	location.href = String(href);
+}
+
+async function init(): Promise<void | false> {
 	const pullRequests = select.all('[data-hovercard-type="pull_request"]');
-	const commits = await filterMergeCommits([...new Set(pullRequests.map(getCommitHash))]);
+	if (pullRequests.length === 0) {
+		return;
+	}
 
 	for (const pullRequest of pullRequests) {
-		const prBlameCommit = commits.get(getCommitHash(pullRequest));
 		// If a pull request number was associated with a random commit.
-		if (!prBlameCommit) {
-			return;
-		}
 
 		const currentParentElement = pullRequest.closest('.blame-hunk');
 		const versionsParent = select('.blob-reblame', currentParentElement!);
-		const currentLineNumber = select('.js-line-number', currentParentElement!)!.textContent!;
-
-		const href = new URL(window.location.href.replace(String(getReference()), prBlameCommit));
-		href.hash = 'L' + currentLineNumber;
 
 		if (select.exists('a', versionsParent!)) {
 			const versionsElement = select('a', versionsParent!);
@@ -71,13 +75,13 @@ async function getDeepBlame(): Promise<void | false> {
 			versionsElement!.addEventListener('click', event => {
 				if (event.altKey) {
 					event.preventDefault();
-					location.href = String(href);
+					location.href = String('');
 				}
 			});
 		} else {
 			versionsParent!.append(
 				<a
-					href={String(href)}
+					data-commit={getCommitHash(pullRequest)}
 					aria-label="View `deep-blame` prior to this change"
 					className="reblame-link link-hover-blue no-underline tooltipped tooltipped-e d-inline-block pr-1 rgh-deep-blame"
 				> {versionIcon()}
@@ -86,29 +90,7 @@ async function getDeepBlame(): Promise<void | false> {
 		}
 	}
 
-	select('.rgh-deep-blame-button')!.remove();
-}
-
-function getCommitHash(commit: HTMLElement): string {
-	return (commit.nextElementSibling! as HTMLAnchorElement).href.split('/').slice(-1)[0];
-}
-
-async function init(): Promise<void | false> {
-	const buttonGroup = await elementReady('.file-actions .BtnGroup');
-	if (!select.exists('[data-hovercard-type="pull_request"]')) {
-		return;
-	}
-
-	buttonGroup!.append(
-		<a
-			className="btn btn-sm BtnGroup-item tooltipped tooltipped tooltipped-n rgh-md-source rgh-deep-blame-button"
-			type="button"
-			aria-label="Get the deep blame"
-			onClick={getDeepBlame}
-		>
-			{versionIcon()}
-		</a>
-	);
+	delegate('.rgh-deep-blame', 'click', getDeepBlame);
 }
 
 features.add({
@@ -118,6 +100,6 @@ features.add({
 	include: [
 		features.isBlame
 	],
-	load: features.nowAndOnAjaxedPages,
+	load: features.onAjaxedPages,
 	init
 });
