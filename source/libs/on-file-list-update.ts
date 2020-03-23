7d0b33b8304923737dd92b51912bbcb5f6213fb9
Federico Brigante <opensource@bfred.it>
2020-03-23 22:02:28 +0100

    Revert some changes to special listeners

--- source/libs/on-file-list-update.ts
+++ source/libs/on-file-list-update.ts
@@ -1,31 +1,8 @@
-let target = new EventTarget(); // Using a replaceable target is an easy way to discard all listeners at once
-const observer = new MutationObserver(dispatch);
-
-function dispatch(): void {
-	target.dispatchEvent(new CustomEvent('rgh:file-list-update'));
-}
-
-function observe(): void {
+export default async function (callback: VoidFunction): Promise<void> {
 	const ajaxFiles = document.querySelector('include-fragment.file-wrap');
 	if (ajaxFiles) {
-		observer.observe(ajaxFiles.parentNode!, {
+		new MutationObserver(callback).observe(ajaxFiles.parentNode!, {
 			childList: true
 		});
 	}
-}
-
-function connect(): void {
-	observe();
-	target.addEventListener('rgh:file-list-update', observe);
-}
-
-function disconnect(): void {
-	observer.disconnect();
-	target = new EventTarget();
-}
-
-export default async function onFileListUpdate(callback: VoidFunction): Promise<void> {
-	connect();
-	document.addEventListener('pjax:end', disconnect);
-	target.addEventListener('rgh:file-list-update', callback);
 }
--- source/libs/on-new-comments.ts
+++ source/libs/on-new-comments.ts
@@ -1,30 +1,37 @@
 import select from 'select-dom';
 import delegate, {DelegateSubscription} from 'delegate-it';
 
+const discussionsWithListeners = new WeakSet();
+const handlers = new Set<VoidFunction>();
 const delegates = new Set<DelegateSubscription>();
-const observer = new MutationObserver(newCommentsEvent);
+const observer = new MutationObserver(run);
 
-let target: EventTarget | undefined;
-
-function newCommentsEvent(): void {
-	target!.dispatchEvent(new CustomEvent('new-comments'));
+function run(): void {
+	// Run all callbacks without letting an error stop the loop and without silencing it
+	handlers.forEach(async callback => callback());
 }
 
 function removeListeners(): void {
-	target = undefined;
 	for (const subscription of delegates) {
 		subscription.destroy();
 	}
 
 	delegates.clear();
+	handlers.clear();
 	observer.disconnect();
 }
 
 function addListeners(): void {
 	const discussion = select('.js-discussion');
-	if (!discussion) {
+	if (!discussion || discussionsWithListeners.has(discussion)) {
 		return;
 	}
+
+	// Ensure listeners are only ever added once
+	discussionsWithListeners.add(discussion);
+
+	// Remember to remove all listeners when a new page is loaded
+	document.addEventListener('pjax:beforeReplace', removeListeners);
 
 	// When new comments come in via AJAX
 	observer.observe(discussion, {
@@ -32,19 +39,13 @@ function addListeners(): void {
 	});
 
 	// When hidden comments are loaded by clicking "Load more..."
-	delegates.add(delegate('.js-ajax-pagination', 'page:loaded', newCommentsEvent));
+	delegates.add(delegate('.js-ajax-pagination', 'page:loaded', run));
 
 	// Outdated comment are loaded later using an include-fragment element
-	delegates.add(delegate('details.outdated-comment > include-fragment', 'load', newCommentsEvent, true));
+	delegates.add(delegate('details.outdated-comment > include-fragment', 'load', run, true));
 }
 
 export default function (callback: VoidFunction): void {
-	target = new EventTarget();
 	addListeners();
-
-	target.addEventListener('new-comments', callback);
-
-	document.addEventListener('pjax:beforeReplace', removeListeners, {
-		once: true
-	});
+	handlers.add(callback);
 }
