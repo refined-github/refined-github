9c0d1294d6b11184b0644b20d0c03300b0c9eb0d
Yakov Kantor <16872793+yakov116@users.noreply.github.com>
2020-04-03 08:15:40 -0400

    Apply suggested code
    Co-Authored-By: Federico Brigante <github@bfred.it>

--- source/features/bypass-checks.tsx
+++ source/features/bypass-checks.tsx
@@ -9,6 +9,10 @@ async function init(): Promise<void> {
 
 async function bypass(check: HTMLElement): Promise<void> {
 	const details = select<HTMLAnchorElement>('.status-actions', check.parentElement!)!;
+	if (details.origin !== location.origin) {
+		return;
+	}
+
 	const directLink = await fetchDom(details.href, 'a.text-small .octicon-link-external');
 	details.href = (directLink!.parentElement as HTMLAnchorElement).href;
 }
