d15036496b0040c95801ef3030325e29680458d0
Yakov Kantor <16872793+yakov116@users.noreply.github.com>
2020-04-03 15:01:55 -0400

    ...

--- source/features/deep-reblame.tsx
+++ source/features/deep-reblame.tsx
@@ -74,7 +74,6 @@ async function redirectToBlameCommit(event: DelegateEvent<MouseEvent, HTMLAnchor
 	blameElement.blur(); // Hide tooltip after click, it’s shown on :focus
 
 	const currentFilename = getCleanPathname().split('/').slice(-3).join('/');
-	const currentFilename = splitPath.slice(Math.max(splitPath.length - 3, 1)).join('/');
 
 	const prBlameCommit = await getPullRequestBlameCommit(prCommit, prNumber, currentFilename);
 	if (prBlameCommit) {
