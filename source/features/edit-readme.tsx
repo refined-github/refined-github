211cd6b2733fb69f962ab7908873a43e06fff3f8
Yakov Kantor <16872793+yakov116@users.noreply.github.com>
2020-04-12 22:32:21 -0400

    Fix GitHub’s own "Edit readme" link on tag pages
    By extending `edit-readme`

--- source/features/edit-readme.tsx
+++ source/features/edit-readme.tsx
@@ -10,11 +10,6 @@ async function init(): Promise<void | false> {
 		return false;
 	}
 
-	// The button already exists on repos you can push to.
-	if (select.exists('a[aria-label="Edit this file"]')) {
-		return false;
-	}
-
 	const isPermalink = /Tag|Tree/.test(select('.branch-select-menu i')!.textContent!);
 
 	const filename = readmeHeader.textContent!.trim();
@@ -22,6 +17,17 @@ async function init(): Promise<void | false> {
 	pathnameParts[3] = 'edit'; // Replaces /blob/
 	if (isPermalink) {
 		pathnameParts[4] = await getDefaultBranch(); // Replaces /${tag|commit}/
+	}
+
+	// The button already exists on repos you can push to.
+	const buttonExists = select<HTMLAnchorElement>('a[aria-label="Edit this file"]');
+	if (buttonExists) {
+		// See https://github.com/sindresorhus/refined-github/issues/2997: Fixes GitHub’s own "Edit readme" link on tag pages
+		if (isPermalink) {
+			buttonExists.href = pathnameParts.join('/');
+		}
+
+		return false;
 	}
 
 	readmeHeader.after(
@@ -37,7 +43,7 @@ async function init(): Promise<void | false> {
 
 features.add({
 	id: __featureName__,
-	description: 'Adds an Edit button on previewed Readmes in folders, even if you have to make a fork.',
+	description: 'Adds an Edit button on previewed Readmes in folders, even if you have to make a fork. Also fixes GitHub’s own "Edit readme" link on tag pages',
 	screenshot: 'https://user-images.githubusercontent.com/1402241/62073307-a8378880-b26a-11e9-9e31-be6525d989d2.png'
 }, {
 	include: [
