361a29a48f00e89b7df33e71d0c89f11950086e6
Federico Brigante <opensource@bfred.it>
2020-04-15 00:40:56 +0200

    Use new loader format

--- source/features/clear-pr-merge-commit-message.tsx
+++ source/features/clear-pr-merge-commit-message.tsx
@@ -2,19 +2,13 @@ import select from 'select-dom';
 import features from '../libs/features';
 import onPrMergePanelOpen from '../libs/on-pr-merge-panel-open';
 
-function clearField(): void {
+function init(): void {
 	const messageField = select<HTMLTextAreaElement>('#merge_message_field')!;
 
 	const coAuthorsMatches = messageField.value.matchAll(/Co-Authored-By: [^\n]+/g);
-
-	// Deduplicate Co-Authors
-	const coAuthors = [...new Set([...coAuthorsMatches].flat())];
+	const coAuthors = [...new Set([...coAuthorsMatches].flat())]; // Deduplicate Co-Authors
 
 	messageField.value = coAuthors.join('\n');
-}
-
-function init(): void {
-	onPrMergePanelOpen(clearField);
 }
 
 features.add({
@@ -25,6 +19,10 @@ features.add({
 	include: [
 		features.isPRConversation
 	],
-	load: features.onDocumentStart,
+	additionalListeners: [
+		onPrMergePanelOpen
+	],
+	onlyAdditionalListeners: true,
+	waitForDomReady: false,
 	init
 });
