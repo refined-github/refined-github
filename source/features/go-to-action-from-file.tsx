096d39ffb21e691289c09d8850a8cab191486c5d
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-04-16 19:56:12 +0200

    rename

--- source/features/go-to-action-from-file.tsx
+++ source/features/go-to-action-from-file.tsx
@@ -1,25 +1,23 @@
 import React from 'dom-chef';
 import select from 'select-dom';
 import features from '../libs/features';
+import getRepoPath from './utils';
 
 const isWorkflowFile = (): boolean => /\/\.github\/workflows\/.+\.ya?ml$/.test(getRepoPath()!);
 
 function init(): void {
-  
-  const actionName = 
+	const actionName =
 	select.all('.blob-code-inner')
 		.find(line => line.textContent!.startsWith('name'))
 		.textContent
 		.replace(/^name:\s+/, '')
 		.replace(/["']/g, '')
 		.trim();
-	const rawButton = select<HTMLAnchorElement>('#raw-url')!;
-	const link = location.pathname.split('/', 7);
-	
-	const actionURL = new URL(`${location.origin}/${getRepoURL()}/actions`);
-	actionURL.searchParams.set('query', `workflow:"${actionName}"`)
 
-	select('#raw-url')!
+	const actionURL = new URL(`${location.origin}/${getRepoURL()}/actions`);
+	actionURL.searchParams.set('query', `workflow:"${actionName}"`);
+
+	select<HTMLAnchorElement>('#raw-url')!
 		.parentElement! // `BtnGroup`
 		.prepend(
 			<a
@@ -33,14 +31,11 @@ function init(): void {
 
 features.add({
 	id: __featureName__,
-	description: 'Adds a link to view Action workflow in action.',
+	description: 'Adds a link to access runs of a GitHub Action workflow from the workflow configuration file.',
 	screenshot: 'https://user-images.githubusercontent.com/44045911/67634792-48995980-f8fb-11e9-8b6a-7b57d5b12a2f.png'
 }, {
 	include: [
 	  isWorkflowFile
-	],
-	exclude: [
-		features.isEnterprise
 	],
 	init
 });
