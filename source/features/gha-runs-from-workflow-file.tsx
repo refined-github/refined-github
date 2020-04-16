ed7ff45916c5b4527fb93683196ada95b0f2a21a
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-04-16 15:52:52 +0200

    aha

--- source/features/gha-runs-from-workflow-file.tsx
+++ source/features/gha-runs-from-workflow-file.tsx
@@ -1,32 +1,17 @@
-import React from 'dom-chef';
-import select from 'select-dom';
+//import React from 'dom-chef';
+//import select from 'select-dom';
 import features from '../libs/features';
 
 const isWorkflowFile = (): boolean => (location.pathname.endsWith('.yaml') && location.pathname.split('/', 7)[5] == ".github"  && location.pathname.split('/', 7)[6] == "workflows");
 
-	console.log(location.pathname)
 function init(): void {
-  const actionName =
+  const actionName = 
   	document.body('.blob-code-inner')
   		.find(line => line.textContent.startsWith('name'))
   		.textContent
   		.replace(/^name:\s+/, '')
   		.trim();
-	const runButton = select<HTMLAnchorElement>('#workflow-in-action')!;
-	const link = runButton.pathname.split('/');
-	// Remove /raw/
-	link.splice(3, 1);
 	console.log(actionName)
-	runButton
-		.parentElement! // `BtnGroup`
-		.prepend(
-			<a
-				className="btn btn-sm BtnGroup-item"
-				href={`https://github.com/r-hub/docs/actions?query={actionName}`}
-			>
-				Workflow in Action
-			</a>
-		);
 }
 
 features.add({
@@ -35,7 +20,8 @@ features.add({
 	screenshot: 'https://user-images.githubusercontent.com/44045911/67634792-48995980-f8fb-11e9-8b6a-7b57d5b12a2f.png'
 }, {
 	include: [
-//		isWorkflowFile
+	  features.isSingleFile,
+	  isWorkflowFile
 	],
 	exclude: [
 		features.isEnterprise
