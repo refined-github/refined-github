75f4cc8b5fa9b75aa315a9b9a036e60d5abbadd0
yakov116 <16872793+yakov116@users.noreply.github.com>
2020-04-21 12:24:15 -0400

    Keep `clone-branch` after a search (#3023)
    * Keep clone-branch after a search
    
    * Drop `return false`
    
    In the other feature I had suggested `return false` because there was already an `init` on the same page
    
    Co-authored-by: Federico Brigante <opensource@bfred.it>

--- source/features/clone-branch.tsx
+++ source/features/clone-branch.tsx
@@ -8,6 +8,7 @@ import features from '../libs/features';
 import * as pageDetect from '../libs/page-detect';
 import loadingIcon from '../libs/icon-loading';
 import {getRepoURL, getRepoGQL} from '../libs/utils';
+import observeElement from '../libs/simplified-element-observer';
 
 const getBranchBaseSha = async (branchName: string): Promise<string> => {
 	const {repository} = await api.v4(`
@@ -99,5 +100,7 @@ features.add({
 	include: [
 		pageDetect.isBranches
 	],
-	init
+	init: () => {
+		observeElement('[data-target="branch-filter-controller.results"]', init);
+	}
 });
