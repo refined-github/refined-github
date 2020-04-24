f8036106e2e3f71b97c3c95dd45ab7ff9147cf96
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-04-24 16:00:34 +0200

    yes

--- source/features/action-used-by-link.tsx
+++ source/features/action-used-by-link.tsx
@@ -6,9 +6,9 @@ export const isActionPage = (): boolean => location.pathname.split('/', 3).join(
 
 async function init(): Promise<void> {
 	const actionRepo = select('.d-block.mb-2')!
-		.getAttribute("href")!
+		.getAttribute('href')!
 		.replace(location.origin, '')
-		.replace("/", "");
+		.replace('/', '');
 
 	const actionURL = new URL('https://github.com/search');
 	actionURL.searchParams.set('q', `${actionRepo} path:.github/workflows/ language:YAML`);
@@ -19,8 +19,8 @@ async function init(): Promise<void> {
 	select('.d-block.mb-2')!
 		.parentElement!
 		.append(
-			<a href={String(actionURL)} class="d-block mb-2">
-				<svg class="octicon octicon-search text-gray-dark mr-2" width="14" height="16" viewBox="0 0 16 16" version="1.1" aria-hidden="true"><path fill-rule="evenodd" d="M15.7 13.3l-3.81-3.83A5.93 5.93 0 0013 6c0-3.31-2.69-6-6-6S1 2.69 1 6s2.69 6 6 6c1.3 0 2.48-.41 3.47-1.11l3.83 3.81c.19.2.45.3.7.3.25 0 .52-.09.7-.3a.996.996 0 000-1.41v.01zM7 10.7c-2.59 0-4.7-2.11-4.7-4.7 0-2.59 2.11-4.7 4.7-4.7 2.59 0 4.7 2.11 4.7 4.7 0 2.59-2.11 4.7-4.7 4.7z"/></svg>Used by (find workflows)
+			<a href={String(actionURL)} className="d-block mb-2">
+				<svg className="octicon octicon-search text-gray-dark mr-2" width="14" height="16" viewBox="0 0 16 16" version="1.1" aria-hidden="true"><path fill-rule="evenodd" d="M15.7 13.3l-3.81-3.83A5.93 5.93 0 0013 6c0-3.31-2.69-6-6-6S1 2.69 1 6s2.69 6 6 6c1.3 0 2.48-.41 3.47-1.11l3.83 3.81c.19.2.45.3.7.3.25 0 .52-.09.7-.3a.996.996 0 000-1.41v.01zM7 10.7c-2.59 0-4.7-2.11-4.7-4.7 0-2.59 2.11-4.7 4.7-4.7 2.59 0 4.7 2.11 4.7 4.7 0 2.59-2.11 4.7-4.7 4.7z"/></svg>Used by (find workflows)
 			</a>
 		);
 }
