fe12cf99eb5a3beee343e464510675a9b728a154
Federico Brigante <opensource@bfred.it>
2020-05-02 10:08:01 +0200

    Wrap textNode only

--- source/features/linkify-notification-repository-header.tsx
+++ source/features/linkify-notification-repository-header.tsx
@@ -3,16 +3,14 @@ import React from 'dom-chef';
 import select from 'select-dom';
 import features from '../libs/features';
 import * as pageDetect from '../libs/page-detect';
-import {wrap} from '../libs/dom-utils';
 
 function init(): void {
 	for (const header of select.all('.js-notifications-group h6')) {
-		wrap(
-			header,
-			<a
-				className={header.className + ' text-inherit'}
-				href={'/' + header.textContent!.trim()}
-			/>);
+		header.append(
+			<a className="text-inherit" href={'/' + header.textContent!.trim()}>
+				{header.firstChild}
+			</a>
+		);
 	}
 }
 
