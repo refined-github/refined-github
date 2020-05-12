a7b1a8bd85c230ff9ff8e407ca8f6d3646c90eb6
Max Arias <max@max-arias.com>
2020-05-12 20:35:41 -0300

    Added feature to stop GH from replacing Pr links to commit links

---
+++ source/features/stop-redirecting-commit-pull-url-to-commit-url.tsx
@@ -0,0 +1,43 @@
+import select from 'select-dom';
+import * as pageDetect from 'github-url-detection';
+
+import features from '../libs/features';
+
+function init(): void {
+    for (const form of select.all(['form#new_issue', 'form.js-new-comment-form'])) {
+        form.addEventListener('submit', () => {
+            for (const textarea of select.all('textarea', form)) {
+                if (textarea.value) {
+                    textarea.value = textarea.value.replace(/\bhttps?:\/\/github.com\/.*\/pull\/.*\b/gi, (match): string => {
+                        try {
+                            if (match) {
+                                const parts = match.split('/') || [];
+                                if (parts && parts.length) {
+                                    const sha = parts[parts.length - 1].substring(0, 7);
+                                    return `[${sha}](${match})`;
+                                }
+                            }
+                        } catch (error) {
+                            console.log(error);
+                            return match;
+                        }
+
+                        return match;
+                    })
+                }
+            }
+        });
+    }
+}
+
+features.add({
+	id: __filebasename,
+	description: 'Prevents Github from converting PR links into commit links',
+	screenshot: false
+}, {
+	include: [
+        pageDetect.isIssue,
+		pageDetect.isPRConversation
+	],
+	init
+});
