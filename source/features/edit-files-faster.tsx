f1059daab4bf8f1aa4340223a85976ba18ecde43
Yakov Kantor <16872793+yakov116@users.noreply.github.com>
2020-05-11 22:23:20 -0400

    Switch `edit-files-faster` to `parseRoute`

--- source/features/edit-files-faster.tsx
+++ source/features/edit-files-faster.tsx
@@ -6,21 +6,17 @@ import * as pageDetect from 'github-url-detection';
 
 import {wrap} from '../libs/dom-utils';
 import features from '../libs/features';
+import {parseRoute} from '../libs/utils';
 import getDefaultBranch from '../libs/get-default-branch';
 import onFileListUpdate from '../libs/on-file-list-update';
 
 async function init(): Promise<void> {
 	const defaultBranch = await getDefaultBranch();
+	const isPermalink = /Tag|Tree/.test(select('.branch-select-menu i')!.textContent!);
 	for (const fileIcon of select.all('.files :not(a) > .octicon-file')) {
-		const pathnameParts = fileIcon
-			.closest('tr')!
-			.querySelector<HTMLAnchorElement>('.js-navigation-open')!
-			.pathname
-			.split('/');
-
-		pathnameParts[3] = 'edit'; // Replaces `/blob/`
-
-		const isPermalink = /Tag|Tree/.test(select('.branch-select-menu i')!.textContent!);
+		const {pathname} = fileIcon.closest('tr')!.querySelector<HTMLAnchorElement>('.js-navigation-open')!;
+		const pathnameParts = parseRoute(pathname);
+		pathnameParts[3] = 'edit'; // Replaces /blob/
 		if (isPermalink) {
 			pathnameParts[4] = defaultBranch; // Replaces /${tag|commit}/
 		}
