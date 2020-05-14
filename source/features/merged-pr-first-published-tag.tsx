4c4efcda0e3015890f75b0d8f8d843a61c0fb40b
Yakov Kantor <16872793+yakov116@users.noreply.github.com>
2020-05-14 11:32:13 -0400

    Fix Selector

--- source/features/merged-pr-first-published-tag.tsx
+++ source/features/merged-pr-first-published-tag.tsx
@@ -17,7 +17,7 @@ const getFirstTag = cache.function(async (commit: string): Promise<[string, stri
 });
 
 async function addTag(discussionHeader: HTMLElement): Promise<void | false> {
-	const mergeCommit = select('div.TimelineItem-body > a[href*="commit"] > code.link-gray-dark')!.textContent!;
+	const mergeCommit = select(`.TimelineItem.js-details-container.Details a[href^="/${getRepoURL()}/commit/"] > code.link-gray-dark`)!.textContent!;
 	const [tagUrl, tagName] = await getFirstTag(mergeCommit) ?? [];
 
 	if (!tagUrl) {
