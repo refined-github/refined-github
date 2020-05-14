657f9c1136f6be4a96f386fb6eebb910060a8287
Yakov Kantor <16872793+yakov116@users.noreply.github.com>
2020-05-14 10:01:39 -0400

    Add comments, Update Picture and update description
    Also move the title to tag link

--- source/features/pr-earliest-tag-after-merge.tsx
+++ source/features/pr-earliest-tag-after-merge.tsx
@@ -4,8 +4,8 @@ import select from 'select-dom';
 import TagIcon from 'octicon/tag.svg';
 import * as pageDetect from 'github-url-detection';
 
-import fetchDom from '../libs/fetch-dom';
 import features from '../libs/features';
+import fetchDom from '../libs/fetch-dom';
 import {getRepoURL} from '../libs/utils';
 import observeElement from '../libs/simplified-element-observer';
 
@@ -20,7 +20,7 @@ const getEarliestTag = cache.function(async (commit: string): Promise<[string, s
 	cacheKey: ([commit]) => `earliest-tag:${getRepoURL()}:${commit}`
 });
 
-async function applyTagToHeaders(discussionHeader: Element): Promise<void | false> {
+async function applyTagToHeaders(discussionHeader: HTMLElement): Promise<void | false> {
 	const mergeCommit = select('div.TimelineItem-body > a[href*="commit"] > code.link-gray-dark')!.textContent!;
 	const [tagUrl, tagName] = await getEarliestTag(mergeCommit) ?? [];
 
@@ -29,9 +29,13 @@ async function applyTagToHeaders(discussionHeader: Element): Promise<void | fals
 	}
 
 	discussionHeader.parentElement!.append(
-		<span title={`${tagName!} was the earliest tag after this PR was merged`}>
+		<span>
 			• <TagIcon width={14} className="mx-1 text-gray-light"/>
-			<a href={tagUrl} className="commit-ref">
+			<a
+				href={tagUrl}
+				className="commit-ref"
+				title={`${tagName!} was the earliest tag after this PR was merged`}
+			>
 				{tagName}
 			</a>
 		</span>
@@ -39,12 +43,14 @@ async function applyTagToHeaders(discussionHeader: Element): Promise<void | fals
 }
 
 function init(): void | false {
+	// The state will be purple on a merged PR
 	if (!select.exists('.State.State--purple')) {
 		return false;
 	}
 
 	const ajaxedTitleArea = select('#partial-discussion-header')!.parentElement!;
 	observeElement(ajaxedTitleArea, () => {
+		// Select the PR header and sticky header
 		for (const discussionHeader of select.all('#partial-discussion-header relative-time:not(.rgh-earliest-tag)')) {
 			discussionHeader.classList.add('rgh-earliest-tag');
 			applyTagToHeaders(discussionHeader);
@@ -54,8 +60,8 @@ function init(): void | false {
 
 features.add({
 	id: __filebasename,
-	description: 'Show the earliest tag after a merged PR.',
-	screenshot: 'https://user-images.githubusercontent.com/16872793/81870251-7c5c6980-9543-11ea-836d-abfbce92c74d.png'
+	description: 'Shows the earliest published tag on a merged PR.',
+	screenshot: 'https://user-images.githubusercontent.com/16872793/81943321-38ac4300-95c9-11ea-8543-0f4858174e1e.png'
 }, {
 	include: [
 		pageDetect.isPRConversation
