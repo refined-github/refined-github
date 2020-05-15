95810c66cae0ebbc85b538aea28fa3f586813a5c
Fregante <opensource@bfred.it>
2020-05-15 02:28:56 +0200

    Revert "Shorten selector"
    This reverts commit 031a49d1265a85bb3a0c6a061fac35c402632024.

--- source/features/merged-pr-first-published-tag.tsx
+++ source/features/merged-pr-first-published-tag.tsx
@@ -21,7 +21,7 @@ const getFirstTag = cache.function(async (commit: string): Promise<string | unde
 });
 
 async function init(): Promise<void> {
-	const mergeCommit = select(`.TimelineItem a[href^="/${getRepoURL()}/commit/" i] > code.link-gray-dark`)!.textContent!;
+	const mergeCommit = select(`.TimelineItem.js-details-container.Details a[href^="/${getRepoURL()}/commit/" i] > code.link-gray-dark`)!.textContent!;
 	const tagName = await getFirstTag(mergeCommit);
 
 	if (!tagName) {
