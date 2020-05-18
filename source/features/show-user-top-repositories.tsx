refined-github/refined-github

commit 4224b5b20c019ace98af86604b14824352cf009f

Use Optional chaining where possible (#3100)

source/features/show-user-top-repositories.tsx
@@ -14,13 +14,9 @@ function buildUrl(queryField: string): URL {
 	return url;
 }
 
-function init(): false | void {
-	const showcaseTitle = select('.js-pinned-items-reorder-container .text-normal');
-	if (!showcaseTitle) {
-		return false;
-	}
-
-	showcaseTitle.firstChild!.after(
+function init(): void {
+	// Showcase title
+	select('.js-pinned-items-reorder-container .text-normal')?.firstChild!.after(
 		' / ',
 		<a href={String(buildUrl('stars'))}>Top repositories</a>
 	);

source/features/download-folder-button.tsx
@@ -5,17 +5,14 @@ import * as pageDetect from 'github-url-detection';
 import features from '../libs/features';
 
 function init(): void {
-	const buttonGroup = select('.file-navigation .BtnGroup.float-right');
-	if (buttonGroup) {
-		buttonGroup.prepend(
-			<a
-				className="btn btn-sm BtnGroup-item"
-				href={`https://download-directory.github.io/?url=${location.href}`}
-			>
-				Download
-			</a>
-		);
-	}
+	select('.file-navigation .BtnGroup.float-right')?.prepend(
+		<a
+			className="btn btn-sm BtnGroup-item"
+			href={`https://download-directory.github.io/?url=${location.href}`}
+		>
+			Download
+		</a>
+	);
 }
 
 features.add({

source/features/linkify-symbolic-links.tsx
@@ -6,8 +6,7 @@ import {wrap} from '../libs/dom-utils';
 import features from '../libs/features';
 
 function init(): void | false {
-	const mode = select('.file-mode');
-	if (mode && mode.textContent === 'symbolic link') {
+	if (select('.file-mode')?.textContent === 'symbolic link') {
 		const line = select('.js-file-line')!;
 		wrap(line.firstChild!, <a href={line.textContent!}/>);
 	}

source/features/remove-upload-files-button.tsx
@@ -4,13 +4,8 @@ import * as pageDetect from 'github-url-detection';
 import features from '../libs/features';
 import {getRepoURL} from '../libs/utils';
 
-function init(): false | void {
-	const uploadFilesButton = select(`.file-navigation a[href^="/${getRepoURL()}/upload"]`);
-	if (!uploadFilesButton) {
-		return false;
-	}
-
-	uploadFilesButton.remove();
+function init(): void {
+	select(`.file-navigation a[href^="/${getRepoURL()}/upload"]`)?.remove();
 }
 
 features.add({

source/features/show-names.tsx
@@ -24,8 +24,8 @@ async function init(): Promise<false | void> {
 
 		// Drop 'commented' label to shorten the copy
 		const commentedNode = element.parentNode!.nextSibling;
-		if (commentedNode && commentedNode.textContent!.includes('commented')) {
-			commentedNode.remove();
+		if (commentedNode?.textContent!.includes('commented')) {
+			commentedNode!.remove();
 		}
 	}
 
@@ -44,7 +44,7 @@ async function init(): Promise<false | void> {
 		const userKey = api.escapeKey(username);
 
 		// For the currently logged in user, `names[userKey]` would not be present.
-		if (names[userKey] && names[userKey].name) {
+		if (names[userKey]?.name) {
 			// If it's a regular comment author, add it outside <strong>
 			// otherwise it's something like "User added some commits"
 			if (compareNames(username, names[userKey].name)) {

commit 2b46ce664bd44ce7d4108eafd858d2e09c031495

Don't run `show-user-top-repositories` on any profile tabs
Fix console error caused by #3100

source/features/show-user-top-repositories.tsx
@@ -34,5 +34,8 @@ features.add({
 	include: [
 		pageDetect.isUserProfile
 	],
+	exclude: [
+		() => Boolean(new URLSearchParams(location.search).get('tab'))
+	],
 	init
 });