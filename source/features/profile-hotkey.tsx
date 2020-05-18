82bd9c3a49527a897dee1431830d3923e439d8bd
yakov116 <16872793+yakov116@users.noreply.github.com>
2020-05-18 09:39:44 -0400

    Revert profile-hotkey.tsx

--- source/features/profile-hotkey.tsx
+++ source/features/profile-hotkey.tsx
@@ -3,11 +3,13 @@ import select from 'select-dom';
 import features from '../libs/features';
 import {getUsername} from '../libs/utils';
 
-function init(): void {
+function init(): false | void {
 	const menuItem = select(`a[href="/${getUsername()}"]`);
 
 	if (menuItem) {
 		menuItem.dataset.hotkey = 'g m';
+	} else {
+		return false;
 	}
 }
 
