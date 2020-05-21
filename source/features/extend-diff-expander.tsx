8df5b2f190f564be432e6acdd8f6f5497c667fe7
Yakov Kantor <16872793+yakov116@users.noreply.github.com>
2020-05-21 07:58:50 -0400

    Move delegate to init
    Not sure about this one

--- source/features/extend-diff-expander.tsx
+++ source/features/extend-diff-expander.tsx
@@ -5,15 +5,11 @@ import * as pageDetect from 'github-url-detection';
 
 import features from '.';
 
-function expandDiff(event: delegate.Event): void {
+function init(event: delegate.Event): void {
 	// Skip if the user clicked directly on the icon
 	if (!(event.target as Element).closest('.js-expand')) {
 		select<HTMLAnchorElement>('.js-expand', event.delegateTarget)!.click();
 	}
-}
-
-function init(): void {
-	delegate(document, '.diff-view .js-expandable-line', 'click', expandDiff);
 }
 
 features.add({
@@ -25,5 +21,7 @@ features.add({
 		pageDetect.isPRFiles,
 		pageDetect.isCommit
 	],
-	init
+	init: () => {
+		delegate(document, '.diff-view .js-expandable-line', 'click', init);
+	}
 });
