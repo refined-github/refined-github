94828f5ed562cb02d0373a926200d9a8237a3335
Yakov Kantor <16872793+yakov116@users.noreply.github.com>
2020-05-21 09:48:17 -0400

    Fix import

--- source/features/bugs-tab.tsx
+++ source/features/bugs-tab.tsx
@@ -1,7 +1,7 @@
 import React from 'dom-chef';
 import cache from 'webext-storage-cache';
 import select from 'select-dom';
-import BugIcon from 'octicons/bug.svg';
+import BugIcon from 'octicon/bug.svg';
 import elementReady from 'element-ready';
 import * as pageDetect from 'github-url-detection';
 
