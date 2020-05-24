b5f7489f8aa90788b3fd4d5b15baaa623f2cd007
Yakov Kantor <16872793+yakov116@users.noreply.github.com>
2020-05-24 08:14:07 -0400

    Use `getRepoURL` instead of  `getOwnerandRepo` for `add-tag-to-commits`

--- source/features/add-tag-to-commits.tsx
+++ source/features/add-tag-to-commits.tsx
@@ -6,7 +6,7 @@ import * as pageDetect from 'github-url-detection';
 
 import features from '.';
 import * as api from '../github-helpers/api';
-import {getOwnerAndRepo, getRepoURL, getRepoGQL} from '../github-helpers';
+import {getRepoURL, getRepoGQL} from '../github-helpers';
 
 interface CommitTags {
 	[name: string]: string[];
@@ -31,9 +31,6 @@ interface TagNode {
 	name: string;
 	target: CommonTarget;
 }
-
-const {ownerName, repoName} = getOwnerAndRepo();
-const cacheKey = `tags:${ownerName!}/${repoName!}`;
 
 function mergeTags(oldTags: CommitTags, newTags: CommitTags): CommitTags {
 	const result: CommitTags = {...oldTags};
@@ -159,6 +156,7 @@ async function init(): Promise<void | false> {
 		}
 	}
 
+	const cacheKey = `tags:${getRepoURL()}`;
 	await cache.set(cacheKey, cached, 1);
 }
 
