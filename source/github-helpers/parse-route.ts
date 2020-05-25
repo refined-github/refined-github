636a80189c3632f4401e14968694b74c01a2e5e4
Yakov Kantor <16872793+yakov116@users.noreply.github.com>
2020-05-25 11:30:47 -0400

    Support timestamps in `parseRoute`

--- source/github-helpers/parse-route.ts
+++ source/github-helpers/parse-route.ts
@@ -8,10 +8,17 @@ interface Pathname {
 	filePath: string;
 	toString: () => string;
 }
+
+const timeStampRegex = /.*@{(\d{4}([.\-/ ])\d{2}\2\d{2}T\d\d:\d\d:\d\dZ)}/;
+function isTimeStamp(part1: string): boolean {
+	return Boolean(timeStampRegex.exec(decodeURIComponent(part1)));
+}
+
 export default function parseRoute(pathname: string): Pathname {
 	const [user, repository, route, ...next] = pathname.replace(/^\/|\/$/g, '').split('/');
-	const parts = next.join('/');
 	const branch = getCurrentBranch();
+	next[0] = isTimeStamp(next[0]) ? branch + '/' : next[0];
+	const parts = next.join('/');
 	if (parts !== branch && !parts.startsWith(branch + '/')) {
 		throw new Error('The branch of the current page must match the branch in the `pathname` parameter');
 	}
