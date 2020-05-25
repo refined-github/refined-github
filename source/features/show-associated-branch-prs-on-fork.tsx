d8ed0ceab81884e2496f851a06792f4263bb5688
Fregante <opensource@bfred.it>
2020-05-25 12:15:17 +0200

    .

--- source/features/show-associated-branch-prs-on-fork.tsx
+++ source/features/show-associated-branch-prs-on-fork.tsx
@@ -105,7 +105,7 @@ async function init(): Promise<void> {
 
 features.add({
 	id: __filebasename,
-	description: 'Shows the associated pull requests on branches for forked repository’s',
+	description: 'Shows the associated pull requests on branches for forked repository’s.',
 	screenshot: 'https://user-images.githubusercontent.com/16872793/81504659-7e5ec800-92b8-11ea-9ee6-924110e8cca1.png'
 }, {
 	include: [
