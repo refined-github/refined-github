9c715e81ef44ed9d87e013a9f74a8647431ead8c
Yakov <16872793+yakov116@users.noreply.github.com>
2020-05-31 17:46:53 -0400

    '

--- source/features/enable-file-links-in-compare-view.tsx
+++ source/features/enable-file-links-in-compare-view.tsx
@@ -70,7 +70,7 @@ features.add({
 		pageDetect.isCompare
 	],
 	exclude: [
-		// The following selectors will only exist if you can create a PR. If you cant create a PR you are probably looking at a permalink.
+		// The following selectors will only exist if you can create a PR. If you can't create a PR you are probably looking at a permalink.
 		// It will also exclude repositories you don't have access to.
 		() => !select.exists([
 			'[name="collab_privs"]', // Allow edits by maintainers
