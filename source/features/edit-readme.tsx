8eaee4bc97b35594a75da7bd84babb364be9d4c8
Flo Edelmann <florian-edelmann@online.de>
2020-06-06 14:40:33 +0200

    Simplify selectors
    Co-authored-by: Fregante <opensource@bfred.it>

--- source/features/edit-readme.tsx
+++ source/features/edit-readme.tsx
@@ -13,10 +13,9 @@ async function init(): Promise<void | false> {
 		return false;
 	}
 
-	const isPermalink = /Tag|Tree/.test(select('.branch-select-menu i, #branch-select-menu i')!.textContent!);
+	const isPermalink = /Tag|Tree/.test(select('[data-hotkey="w"] i')!.textContent!);
 	const filename = readmeHeader.textContent!.trim();
-	const filesList = select('.files, #files ~ .Details')!;
-	const fileLink = select<HTMLAnchorElement>(`[title="${filename}"]`, filesList)!;
+	const fileLink = select<HTMLAnchorElement>(`.js-navigation-open[title="${filename}"]`)!;
 
 	const url = new GitHubURL(fileLink.href).assign({
 		route: 'edit'
