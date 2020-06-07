5947c0cb4d75004176aa3c2aa874dace49188d7a
Fregante <github@bfred.it>
2020-06-08 01:30:10 +0200

    Drop/remove comment

--- source/features/stop-redirecting-in-notification-bar.tsx
+++ source/features/stop-redirecting-in-notification-bar.tsx
@@ -8,16 +8,13 @@ const hasNotificationBar = (): boolean =>
 
 function handleClick(event: delegate.Event<MouseEvent, HTMLButtonElement>): void {
 	// Disable the redirect to the Notifications inbox if either:
-	// 1. The alt key was held down during click
-	// 2. The notification has been opened in a new tab
+	// 1. The alt key was held down during click (user choice)
+	// 2. The notification has been opened in a new tab (the inbox is still open in the previous tab)
 	const redirectDisabled = event.altKey || sessionStorage.rghIsNewTab === 'true';
 	event.delegateTarget.form!.toggleAttribute('data-redirect-to-inbox-on-submit', !redirectDisabled);
 }
 
 async function init(): Promise<void> {
-	// If the history length is `1` on init, that means there are no
-	// older history states (this means the notification has been opened
-	// in a new browser tab)
 	sessionStorage.rghIsNewTab = history.length === 1;
 	delegate(document, '.notification-shelf .js-notification-action button', 'click', handleClick);
 }
