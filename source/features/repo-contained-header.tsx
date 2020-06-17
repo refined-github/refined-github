0cd5d8cae5789c1cc6883e6f9d8f09982a709ddc
Adam LaCombe <adam@edgeworkscreative.com>
2020-06-18 06:58:04 +0800

    Add `repo-contained-header` feature

---
+++ source/features/repo-contained-header.tsx
@@ -0,0 +1,33 @@
+import './repo-contained-header.css';
+import * as pageDetect from 'github-url-detection';
+import select from 'select-dom';
+import features from '.';
+
+function init(): void {
+  const actionsContainer = select('.pagehead > div');
+  const pageHead = select('.pagehead');
+  const nav = select('.repohead nav');
+
+  // detect repository refresh
+  if (!pageHead?.classList.contains('mb-5')) {
+    return;
+  }
+
+  actionsContainer?.classList.add('container-xl');
+  pageHead?.classList.remove('border-0');
+  pageHead?.classList.remove('mb-5');
+  pageHead?.classList.add('rg-repo-contained-header');
+  nav?.classList.add('container-xl');
+  nav?.classList.remove('UnderlineNav');
+}
+
+void features.add({
+  id: __filebasename,
+  description: 'Places the repo header in a container (requires "repository refresh" to be enabled)',
+  screenshot: false
+}, {
+  include: [
+    pageDetect.isRepo
+  ],
+  init
+});
