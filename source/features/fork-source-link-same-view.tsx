b7461a59a1be0be78f3372241d923ff7d445eeac
Fregante <opensource@bfred.it>
2020-06-18 14:15:02 +0200

    Use forked repository again

--- source/features/fork-source-link-same-view.tsx
+++ source/features/fork-source-link-same-view.tsx
@@ -5,14 +5,14 @@ import features from '.';
 import GitHubURL from '../github-helpers/github-url';
 import doesFileExist from '../github-helpers/does-file-exist';
 import getDefaultBranch from '../github-helpers/get-default-branch';
-import {getRepositoryInfo} from '../github-helpers';
+import {getRepositoryInfo, getForkedRepo} from '../github-helpers';
 
 async function init(): Promise<void> {
-	const currentRepository = getRepositoryInfo();
+	const forkedRepository = getRepositoryInfo(getForkedRepo());
 	const sameViewUrl = new GitHubURL(location.href).assign({
-		user: currentRepository.owner,
-		repository: currentRepository.name,
-		branch: await getDefaultBranch(currentRepository)
+		user: forkedRepository.owner,
+		repository: forkedRepository.name,
+		branch: await getDefaultBranch(forkedRepository)
 	});
 
 	if (await doesFileExist(sameViewUrl)) {
