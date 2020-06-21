9d2640075b73f7dc8995c2d6afbe5d6436a293e5
Yakov <16872793+yakov116@users.noreply.github.com>
2020-06-21 10:12:54 -0400

    Add `pr-jump-to-first-unviewed-file` feature

---
+++ source/features/pr-jump-to-first-unveiwed-file.tsx
@@ -0,0 +1,33 @@
+import select from 'select-dom';
+import delegate from 'delegate-it';
+import * as pageDetect from 'github-url-detection';
+
+import features from '.';
+
+function jumpToFirstUnviewed(): void {
+	const firstNotView = select('.js-reviewed-checkbox:not([checked])')!;
+	if (!firstNotView) {
+		window.scrollTo(0, document.body.scrollHeight);
+		return;
+	}
+
+	const fileID = 	firstNotView.closest<HTMLDivElement>('[data-anchor]')!.dataset.anchor!;
+	// Focus file and dont put into history
+	location.replace('#' + fileID);
+}
+
+function init(): void {
+	delegate(document, 'progress-bar', 'click', jumpToFirstUnviewed);
+	select('progress-bar')!.style.cursor = 'pointer';
+}
+
+void features.add({
+	id: __filebasename,
+	description: 'Jump to first unviewed pr file.',
+	screenshot: 'https://user-images.githubusercontent.com/16872793/85226580-3bf3d500-b3a6-11ea-8494-3d9b6280d033.gif'
+}, {
+	include: [
+		pageDetect.isPRFiles
+	],
+	init
+});
