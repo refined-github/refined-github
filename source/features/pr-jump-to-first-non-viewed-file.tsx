70c8eb0403d922db10e07c20a8e1a2c898d23e5e
Yakov <16872793+yakov116@users.noreply.github.com>
2020-06-22 05:29:13 -0400

    Rename

--- source/features/pr-jump-to-first-non-viewed-file.tsx
+++ source/features/pr-jump-to-first-non-viewed-file.tsx
@@ -4,27 +4,27 @@ import * as pageDetect from 'github-url-detection';
 
 import features from '.';
 
-function jumpToFirstUnviewed(): void {
+function jumpToFirstNonViewed(): void {
 	const firstNonViewedFile = select('.js-reviewed-checkbox:not([checked])')!;
-	if (!firstNotView) {
+	if (!firstNonViewedFile) {
 		// Scroll to page end
 		window.scrollTo(window.scrollX, document.body.scrollHeight);
 		return;
 	}
 
-	const fileID = 	firstNotView.closest('[id]')!.id;
+	const fileID = 	firstNonViewedFile.closest('[id]')!.id;
 	// Scroll to file without pushing to history
 	location.replace('#' + fileID);
 }
 
 function init(): void {
-	delegate(document, '.diffbar-item progress-bar', 'click', jumpToFirstUnviewed);
+	delegate(document, '.diffbar-item progress-bar', 'click', jumpToFirstNonViewed);
 	select('.diffbar-item progress-bar')!.style.cursor = 'pointer';
 }
 
 void features.add({
 	id: __filebasename,
-	description: 'Jumps to first unviewed file in a pull request when clicking on the progress bar.',
+	description: 'Jumps to first non-viewed file in a pull request when clicking on the progress bar.',
 	screenshot: 'https://user-images.githubusercontent.com/16872793/85226580-3bf3d500-b3a6-11ea-8494-3d9b6280d033.gif'
 }, {
 	include: [
