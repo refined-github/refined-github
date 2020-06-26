83fef37f9d932a3bb920868d9d5f1aa2491395e0
yakov116 <16872793+yakov116@users.noreply.github.com>
2020-06-26 04:35:54 -0400

    Don't run `comments-time-machine-links` if comment is after latest commit (#3251)

--- source/features/comments-time-machine-links.tsx
+++ source/features/comments-time-machine-links.tsx
@@ -65,9 +65,17 @@ async function showTimemachineBar(): Promise<void | false> {
 		url.pathname = pathnameParts.join('/');
 	} else {
 		// This feature only makes sense if the URL points to a non-permalink
-		const branchSelector = await elementReady('.branch-select-menu i');
+		const branchSelector = await elementReady('[data-hotkey="w"] i');
 		const isPermalink = /Tag|Tree/.test(branchSelector!.textContent!);
 		if (isPermalink) {
+			return false;
+		}
+
+		const lastCommitDate = await elementReady([
+			'.repository-content .Box.Box--condensed relative-time',
+			'[itemprop="dateModified"] relative-time' // "Repository refresh" layout
+		].join());
+		if (date > lastCommitDate?.attributes.datetime.value!) {
 			return false;
 		}
 
