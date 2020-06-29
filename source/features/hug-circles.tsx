7cdb4345e86041ef5351c3fea954b6746ca6958d
Jacob Hrbek <kreyren@rixotstudio.cz>
2020-06-30 01:24:14 +0200

    hug-circles: Added screenshot

--- source/features/hug-circles.tsx
+++ source/features/hug-circles.tsx
@@ -8,7 +8,7 @@ function init(): void {
 void features.add({
 	id: __filebasename,
 	description: 'Removes the new-UI circles and turns them into squares(old UI).',
-	screenshot: false
+	screenshot: 'https://user-images.githubusercontent.com/11302521/86065591-42eaa980-ba70-11ea-8171-6d919a237d6e.png'
 }, {
 	waitForDomReady: false,
 	repeatOnAjax: false,
