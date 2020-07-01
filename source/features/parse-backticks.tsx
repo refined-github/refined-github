abe91b13aac8cbd0fc07199a14e289672f479e6e
Kid <kidonng@gmail.com>
2020-07-01 20:59:06 +0800

    Parentheses may read clearer

--- source/features/parse-backticks.tsx
+++ source/features/parse-backticks.tsx
@@ -25,10 +25,10 @@ function init(): void {
 		'.Box-row.js-navigation-item a.link-gray-dark', // `isGlobalDiscussionList`
 		'[data-channel^="check_suites"] a', // `isActions`
 		'.repository-content .pr-toolbar h2', // `isActions` run
-		'.Details[data-issue-and-pr-hovercards-enabled] .Details-content--hidden a:first-child', // `isRepoRoot` "Repository refresh" layout
-		'.Details[data-issue-and-pr-hovercards-enabled] .Details-content--hidden pre', // `isRepoRoot` "Repository refresh" layout
+		'.Details[data-issue-and-pr-hovercards-enabled] .Details-content--hidden a:first-child', // `isRepoRoot` ("Repository refresh" layout)
+		'.Details[data-issue-and-pr-hovercards-enabled] .Details-content--hidden pre', // `isRepoRoot` ("Repository refresh" layout)
 		'.js-issue-title', // `isIssue` "Repository refresh" layout
-		'.existing-pull-contents .list-group-item-link', // `isCompare` existing PR "Repository refresh" layout
+		'.existing-pull-contents .list-group-item-link', // `isCompare` existing PR ("Repository refresh" layout)
 		'#wiki-wrapper h1' // `isWiki`
 	].map(selector => selector + ':not(.rgh-backticks-already-parsed)'))) {
 		title.classList.add('rgh-backticks-already-parsed');
