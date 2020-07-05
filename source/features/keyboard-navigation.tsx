7770108adfd41dde2e3097eb68bad19791c5af93
Yakov <16872793+yakov116@users.noreply.github.com>
2020-07-05 17:05:12 -0400

    Exclude pull request reviews

--- source/features/keyboard-navigation.tsx
+++ source/features/keyboard-navigation.tsx
@@ -14,7 +14,7 @@ function runShortcuts(event: KeyboardEvent): void {
 	if (['j', 'k'].includes(event.key)) {
 		event.preventDefault();
 
-		const items = select.all('.js-targetable-element')
+		const items = select.all('.js-targetable-element:not([id^="pullrequestreview"]')
 			.filter(comment => !comment.querySelector('.minimized-comment:not(.d-none)'));
 		// `j` goes to the next comment, `k` goes back a comment
 		const direction = event.key === 'j' ? 1 : -1;
