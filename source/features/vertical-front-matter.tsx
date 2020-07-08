c950057fc586e1a2ee257b65e6e19e32e100fdc3
Kid <kidonng@gmail.com>
2020-07-08 23:37:50 +0800

    Lint

--- source/features/vertical-front-matter.tsx
+++ source/features/vertical-front-matter.tsx
@@ -11,20 +11,21 @@ function init(): void {
 		return;
 	}
 
-	// table > thead
+	// Table > thead
 	const thead = table.firstElementChild!;
-	// table > thead > tr
+	// Table > thead > tr
 	const theadTr = thead.firstElementChild!;
 	const count = theadTr.childElementCount;
 	if (count <= 4) {
-    return;
+		return;
 	}
 
-	// table > tbody
+	// Table > tbody
 	const tbody = table.lastElementChild!;
-	// table > tbody > tr
+	// Table > tbody > tr
 	const tbodyTr = tbody.firstElementChild!;
 	for (let i = 0; i < count; i++) {
+		// Table > thead > tr > th
 		const th = theadTr.firstElementChild!;
 		th.classList.add('rgh-front-matter-name');
 		tbody.append(
