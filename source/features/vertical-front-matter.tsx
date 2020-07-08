2cdfe815717cc9372112d56bbdeb78931ac86f70
Kid <kidonng@gmail.com>
2020-07-08 23:00:51 +0800

    Update style and screenshots

--- source/features/vertical-front-matter.tsx
+++ source/features/vertical-front-matter.tsx
@@ -1,3 +1,5 @@
+import './vertical-front-matter.css';
+import React from 'dom-chef';
 import select from 'select-dom';
 import * as pageDetect from 'github-url-detection';
 
@@ -12,17 +14,21 @@ function init(): void {
 	const thead = select('thead', table)!;
 	const tbody = select('tbody', table)!;
 	while (select.exists('th', thead)) {
-		const tr = document.createElement('tr');
-		tr.append(select('th', thead)!);
-		tr.append(select('td', tbody)!);
-		tbody.append(tr);
+		const th = select('th', thead)!;
+		th.classList.add('rgh-front-matter-name');
+		tbody.append(
+			<tr>
+				{th}
+				{select('td', tbody)!}
+			</tr>
+		);
 	}
 }
 
 void features.add({
 	id: __filebasename,
 	description: 'Show Markdown front matter as vertical table.',
-	screenshot: 'https://user-images.githubusercontent.com/184316/86918308-c9655200-c126-11ea-8e10-31013667dd76.png'
+	screenshot: 'https://user-images.githubusercontent.com/44045911/86934729-a068af80-c16e-11ea-9d73-7b2efee652d1.png'
 }, {
 	include: [
 		pageDetect.isSingleFile
