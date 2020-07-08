bfa9332996ac68bc1122124870f8919007ca49a3
Kid <kidonng@gmail.com>
2020-07-08 20:30:05 +0800

    Refactor
    These selectors are on completely different pages

--- source/features/parse-backticks.tsx
+++ source/features/parse-backticks.tsx
@@ -5,8 +5,15 @@ import * as pageDetect from 'github-url-detection';
 import features from '.';
 import {parseBackticks} from '../github-helpers/dom-formatters';
 
-function init(): void {
-	for (const title of select.all([
+function parse(selectors: string[]): void {
+	for (const element of select.all(selectors.map(selector => selector + ':not(.rgh-backticks-already-parsed)'))) {
+		element.classList.add('rgh-backticks-already-parsed');
+		parseBackticks(element);
+	}
+}
+
+function repoInit(): void {
+	parse([
 		'.commit-title', // `isCommit`
 		'.commit-desc', // `isCommit`, `isCommitList`, `isRepoTree`
 		'.commit-message', // Pushed commits in `isPRConversation`, `isCompare`, `isReleasesOrTags`
@@ -19,8 +26,6 @@ function init(): void {
 		'.TimelineItem-body > del, .TimelineItem-body > ins', // Title edits in `isIssue`, `isPRConversation`
 		'.js-pinned-issue-list-item > .d-block', // Pinned Issues
 		'.pulse-section li', // `isPulse`
-		'.js-recent-activity-container .text-bold', // `isDashboard`"Recent activity" titles
-		'.commits blockquote', // Newsfeed commits
 		'.release-header', // `isReleasesOrTags` Headers
 		'[id^="check_suite"] a.link-gray-dark', // `isActions`
 		'.repository-content .pr-toolbar h2', // `isActions` run
@@ -28,13 +33,22 @@ function init(): void {
 		'.Details[data-issue-and-pr-hovercards-enabled] .Details-content--hidden pre', // `isRepoRoot`
 		'.existing-pull-contents .list-group-item-link', // `isCompare` with existing PR
 		'[aria-label="Link issues"] a', // "Linked issues" in `isIssue`, `isPRConversation`
-		'.notifications-list-item p.text-normal', // `isNotifications`
 		'.js-wiki-sidebar-toggle-display a', // `isWiki`
 		'.gh-header-title' // `isWiki`
-	].map(selector => selector + ':not(.rgh-backticks-already-parsed)'))) {
-		title.classList.add('rgh-backticks-already-parsed');
-		parseBackticks(title);
-	}
+	]);
+}
+
+function dashboardInit(): void {
+	parse([
+    '.js-recent-activity-container .text-bold', // `isDashboard`"Recent activity" titles
+		'.commits blockquote', // Newsfeed commits
+	]);
+}
+
+function notificationsInit(): void {
+	parse([
+		'.notifications-list-item p.text-normal', // `isNotifications`
+	]);
 }
 
 void features.add({
@@ -43,14 +57,18 @@ void features.add({
 	screenshot: 'https://user-images.githubusercontent.com/170270/55060505-31179b00-50a4-11e9-99a9-c3691ba38d66.png'
 }, {
 	include: [
-		pageDetect.isRepo,
-		pageDetect.isNotifications
+		pageDetect.isRepo
 	],
-	init
+	init: repoInit
 }, {
 	include: [
 		pageDetect.isDashboard
 	],
 	onlyAdditionalListeners: true,
-	init
+	init: dashboardInit
+}, {
+	include: [
+		pageDetect.isNotifications
+	],
+	init: notificationsInit
 });
