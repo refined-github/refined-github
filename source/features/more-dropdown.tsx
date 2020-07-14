b2d50018c41d042558e7009c6e7eb20ae383c526
Federico Brigante <opensource@bfred.it>
2020-07-14 18:19:16 +0200

    Create "refreshed" `more-dropdown` feature

--- source/features/more-dropdown.tsx
+++ source/features/more-dropdown.tsx
@@ -8,6 +8,7 @@ import DiffIcon from 'octicon/diff.svg';
 import BranchIcon from 'octicon/git-branch.svg';
 import HistoryIcon from 'octicon/history.svg';
 import PackageIcon from 'octicon/package.svg';
+import KebabHorizontalIcon from 'octicon/kebab-horizontal.svg';
 
 import features from '.';
 import {appendBefore} from '../helpers/dom-utils';
@@ -15,7 +16,8 @@ import {getRepoURL, getCurrentBranch} from '../github-helpers';
 
 const repoUrl = getRepoURL();
 
-function createDropdown(): void {
+// Pre "Repository refresh" layout
+function createLegacyDropdown(): void {
 	// Markup copied from native GHE dropdown
 	appendBefore(
 		// GHE doesn't have `reponav > ul`
@@ -31,10 +33,80 @@ function createDropdown(): void {
 	);
 }
 
+/* eslint-disable-next-line import/prefer-default-export */
+export function createDropdownItem(label: string, url: string, overflow: boolean): Element {
+	const id = `rgh-${label.toLowerCase()}-item`;
+	const item = overflow ?
+		<li data-menu-item={id}/> :
+		<li data-tab-item={id} className="js-responsive-underlinenav-item"/>;
+	item.append(
+		<a role="menuitem" className="dropdown-item" href={url}>
+			{label}
+		</a>
+	);
+	return item;
+}
+
+function createDropdown(): Element {
+	const reference = getCurrentBranch();
+	const compareUrl = `/${repoUrl}/compare/${reference}`;
+	const commitsUrl = `/${repoUrl}/commits/${reference}`;
+	const dependenciesUrl = `/${repoUrl}/network/dependencies`;
+	return (
+		<li className="d-flex js-responsive-underlinenav-item" data-tab-item="rgh-more-dropdown">
+			<details className="details-overlay details-reset position-relative">
+				<summary role="button" aria-haspopup="menu">
+					<div className="UnderlineNav-item mr-0 border-0">
+						<KebabHorizontalIcon/>
+						<span className="sr-only">More</span>
+					</div>
+				</summary>
+				<details-menu className="dropdown-menu dropdown-menu-sw" role="menu">
+					<ul>
+						{createDropdownItem('Compare', compareUrl, false)}
+						{pageDetect.isEnterprise() && createDropdownItem('Dependencies', dependenciesUrl, false)}
+						{createDropdownItem('Commits', commitsUrl, false)}
+						{createDropdownItem('Branches', `/${repoUrl}/branches`, false)}
+					</ul>
+				</details-menu>
+			</details>
+		</li>
+	);
+}
+
 async function init(): Promise<void> {
 	await elementReady('.pagehead + *'); // Wait for the tab bar to be loaded
+
+	const nav = select('.js-responsive-underlinenav .UnderlineNav-body');
+	if (nav) {
+		// "Repository refresh" layout
+		nav.parentElement!.classList.add('rgh-has-more-dropdown');
+		const settingsTab = select('[data-tab-item="settings-tab"]');
+		if (settingsTab) {
+			settingsTab.parentElement!.after(createDropdown());
+		} else {
+			nav.append(createDropdown());
+		}
+
+		const reference = getCurrentBranch();
+		const compareUrl = `/${repoUrl}/compare/${reference}`;
+		const commitsUrl = `/${repoUrl}/commits/${reference}`;
+		const dependenciesUrl = `/${repoUrl}/network/dependencies`;
+		const menu = select('.js-responsive-underlinenav-overflow ul')!;
+
+		menu.append(
+			<div data-menu-item="rgh-more-dropdown"/>,
+			createDropdownItem('Compare', compareUrl, true),
+			pageDetect.isEnterprise() ? '' : createDropdownItem('Dependencies', dependenciesUrl, true),
+			createDropdownItem('Commits', commitsUrl, true),
+			createDropdownItem('Branches', `/${repoUrl}/branches`, true)
+		);
+
+		return;
+	}
+
 	if (!select.exists('.reponav-dropdown')) {
-		createDropdown();
+		createLegacyDropdown();
 	}
 
 	const reference = getCurrentBranch();
