b8628a40970583d5daa58b90c18012fb64afcfde
Kid <kidonng@gmail.com>
2020-07-16 23:07:09 +0800

    Add extension whitelist

--- source/features/vertical-front-matter.tsx
+++ source/features/vertical-front-matter.tsx
@@ -5,6 +5,9 @@ import * as pageDetect from 'github-url-detection';
 
 import features from '.';
 
+// https://github.com/github/markup/blob/cd01f9ec87c86ce5a7c70188a74ef40fc4669c5b/lib/github/markup/markdown.rb#L34
+const hasFrontMatter = () => pageDetect.isSingleFile() && /\.(mdx?|mkdn?|mdwn|mdown|markdown|litcoffee)$/.test(location.pathname)
+
 function init(): void | false {
 	const table = select('[data-table-type="yaml-metadata"]')!;
 	const headers = select.all(':scope > thead th', table);
@@ -33,7 +36,7 @@ void features.add({
 	screenshot: 'https://user-images.githubusercontent.com/44045911/87251695-26069b00-c4a0-11ea-9077-53ce366490ed.png'
 }, {
 	include: [
-		pageDetect.isSingleFile
+		hasFrontMatter
 	],
 	init
 });
