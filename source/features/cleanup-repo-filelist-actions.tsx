622be856552159710e14c04c5a7621bf08313118
Kid <kidonng@gmail.com>
2020-07-28 03:56:26 +0800

    Meta

--- source/features/cleanup-repo-filelist-actions.tsx
+++ source/features/cleanup-repo-filelist-actions.tsx
@@ -27,7 +27,7 @@ function init(): void {
 
 void features.add({
 	id: __filebasename,
-	description: 'Replace some button texts with icons.',
+	description: 'Replaces the labels of some simple buttons on repository filelists with icons, making them take less space.',
 	screenshot: 'https://user-images.githubusercontent.com/44045911/88551471-7a3f7c80-d055-11ea-82f1-c558b7871824.png'
 }, {
 	include: [
