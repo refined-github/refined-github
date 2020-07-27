117415717ef200f3f495373b4d0f886fb1f62a84
Kid <kidonng@gmail.com>
2020-07-27 19:27:19 +0800

    Group it

--- source/features/repo-icon-buttons.tsx
+++ source/features/repo-icon-buttons.tsx
@@ -5,12 +5,19 @@ import SearchIcon from 'octicon/search.svg';
 import * as pageDetect from 'github-url-detection';
 
 import features from '.';
+import {groupButtons} from '../github-helpers/group-buttons';
 
 function init(): void {
-	select('.file-navigation a.btn.d-md-block')!.childNodes[0].replaceWith(<SearchIcon/>);
+	const searchButton = select('.file-navigation a.btn.d-md-block')!;
+	searchButton.childNodes[0].replaceWith(<SearchIcon/>);
+
 	const addButton = select('.file-navigation .d-md-flex.flex-items-center')!;
 	addButton.classList.replace('d-md-flex', 'd-md-block');
+	addButton.classList.remove('ml-2');
 	addButton.childNodes[0].replaceWith(<PlusIcon/>);
+
+	groupButtons([searchButton, addButton]);
+
 	select('.file-navigation .btn.btn-primary')!.childNodes[2].remove();
 }
 
