e4a24d14e408c758f21be129c5814c0d883b7603
Kid <kidonng@gmail.com>
2020-07-28 02:13:07 +0800

    Finish

--- source/features/clean-conversation-headers.tsx
+++ source/features/clean-conversation-headers.tsx
@@ -4,13 +4,31 @@ import * as pageDetect from 'github-url-detection';
 
 import features from '.';
 
-function init(): void {
+function initIssue(): void {
 	const header = select('.gh-header-meta .TableObject-item--primary')!;
 	const childNodes = [...header.childNodes].slice(4);
 	childNodes[0].textContent = childNodes[0].textContent!.replace('·', '');
-	header.replaceWith(<div className="TableObject-item TableObject-item--primary">
-		{...childNodes}
-	</div>);
+	header.replaceWith(
+		<div className="TableObject-item TableObject-item--primary">
+			{...childNodes}
+		</div>
+	);
+}
+
+function initPR(): void {
+	const header = select('.gh-header-meta .TableObject-item--primary')!;
+	const isMerged = (header.previousElementSibling!.firstElementChild! as HTMLElement).title === 'Status: Merged';
+	const isSameAuthor = select('.js-discussion > .TimelineItem:first-child .author')!.textContent === header.firstElementChild!.textContent;
+	const baseBranch = header.childNodes[isMerged ? 3 : 5];
+	const isDefaultBranch = (baseBranch.firstChild! as HTMLAnchorElement).pathname.split('/').length === 3;
+	const childNodes = [...header.childNodes].slice(isMerged ? 5 : 9);
+	header.replaceWith(
+		<div className="TableObject-item TableObject-item--primary">
+			{!isSameAuthor && <>by {header.firstElementChild} </>}
+			{!isDefaultBranch && <>into {baseBranch}</>}
+			{...childNodes}
+		</div>
+	);
 }
 
 void features.add({
@@ -21,5 +39,10 @@ void features.add({
 	include: [
 		pageDetect.isIssue
 	],
-	init
+	init: initIssue
+}, {
+	include: [
+		pageDetect.isPR
+	],
+	init: initPR
 });
