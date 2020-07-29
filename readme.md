9751c74328ae78917efce87225b7ac51ce510dde
Yakov <16872793+yakov116@users.noreply.github.com>
2020-07-29 09:15:22 -0400

    Add `linkify-user-edit-history-popup` feature

--- readme.md
+++ readme.md
@@ -220,6 +220,7 @@ Thanks for contributing! 🦋🙌
 - [](# "bugs-tab") [Adds a "Bugs" tab to repos, if there are any open issues with the "bug" label.](https://user-images.githubusercontent.com/1402241/73720910-a688d900-4755-11ea-9c8d-70e5ddb3bfe5.png)
 - [](# "pinned-issue-update-time") [Adds the updated time to pinned issues.](https://user-images.githubusercontent.com/1402241/75525936-bb524700-5a4b-11ea-9225-466bda58b7de.png)
 - [](# "clean-pinned-issues") [Changes the layout of pinned issues from side-by-side to a standard list.](https://user-images.githubusercontent.com/1402241/84509958-c82a3c00-acc4-11ea-8399-eaf06a59e9e4.png)
+- [](# "linkify-user-edit-history-popup") [Linkifies the username in the edit history popup.](https://user-images.githubusercontent.com/16872793/88803491-85d89380-d17a-11ea-9aad-53635ad7b699.png)
 
 <!-- Refer to style guide above. Keep this message between sections. -->
 
---
+++ source/features/linkify-user-edit-history-popup.tsx
@@ -0,0 +1,38 @@
+import React from 'dom-chef';
+import {observe} from 'selector-observer';
+import * as pageDetect from 'github-url-detection';
+
+import features from '.';
+
+function init(): void {
+	observe('details-dialog .Box-header .mr-3 img', {
+		add(element) {
+			const userName = (element as HTMLImageElement).alt.slice(1);
+			if (userName.includes('[bot]')) {
+				return;
+			}
+
+			element.nextElementSibling!.replaceWith(
+				<a
+					className="link-gray-dark css-truncate-target v-align-middle text-bold text-small"
+					href={`/${userName}`}
+				>
+					{userName}
+				</a>
+			);
+		}
+	});
+}
+
+void features.add({
+	id: __filebasename,
+	description: 'Linkifies the username in the edit history popup.',
+	screenshot: 'https://user-images.githubusercontent.com/16872793/88803491-85d89380-d17a-11ea-9aad-53635ad7b699.png'
+}, {
+	init,
+	include: [
+		pageDetect.isIssue,
+		pageDetect.isPRConversation
+	],
+	repeatOnAjax: false
+});
