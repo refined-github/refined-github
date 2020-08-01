be7c091c7ab53c05437b886b7f3f99a48a580e26
Federico <opensource@bfred.it>
2020-08-01 02:02:57 +0100

    Enable `tab-to-indent` on more pages
    Fixes https://github.com/sindresorhus/refined-github/issues/3414
    Closes #3419

--- source/features/tab-to-indent.tsx
+++ source/features/tab-to-indent.tsx
@@ -14,7 +14,7 @@ void features.add({
 	screenshot: 'https://user-images.githubusercontent.com/1402241/33802977-beb8497c-ddbf-11e7-899c-698d89298de4.gif'
 }, {
 	include: [
-		pageDetect.hasCode
+		pageDetect.hasRichTextEditor
 	],
 	waitForDomReady: false,
 	repeatOnAjax: false,
