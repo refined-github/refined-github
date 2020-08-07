dbdc7d60e612c1c05442c1fcd609716148d0a0e3
Yakov <16872793+yakov116@users.noreply.github.com>
2020-08-07 09:37:06 -0400

    Force local load

--- source/features/show-pr-file-state.tsx
+++ source/features/show-pr-file-state.tsx
@@ -1,37 +1,37 @@
-import React from 'dom-chef';
 import select from 'select-dom';
 import {observe} from 'selector-observer';
 import elementReady from 'element-ready';
 import * as pageDetect from 'github-url-detection';
 
-import DiffAddedIcon from 'octicon/diff-added.svg';
-import DiffRemovedIcon from 'octicon/diff-removed.svg';
-import DiffModifiedIcon from 'octicon/diff-modified.svg';
-
 import features from '.';
-import fetchDom from '../helpers/fetch-dom';
+import {observeOneMutation} from '../helpers/simplified-element-observer';
+
+function loadJumpList(jumpList: Element): void {
+	jumpList.dispatchEvent(new MouseEvent('mouseover'));
+}
 
 async function init(): Promise<false | void> {
-	const jumpList = await elementReady('details [src*="show_toc"]')!;
-	const src = jumpList!.getAttribute('src')!;
-	const fileList = await fetchDom(src, '[data-filterable-for]');
-
-	const list = new Map();
-	for (const file of select.all('a', fileList)) {
-		const fileStatus = [...file.firstElementChild!.classList].find(iconClass => iconClass.includes('diff'));
-		list.set(file.hash, fileStatus);
-	}
+	const jumpList = await elementReady('details.toc-select')!;
+	loadJumpList(jumpList!);
+	setTimeout(loadJumpList, 1000, jumpList!);
+	await observeOneMutation(jumpList!.querySelector('details-menu')!);
 
 	observe('.file-info [href]', {
 		constructor: HTMLAnchorElement,
 		add(element) {
-			const icon: Record<string, Element> = {
-				'octicon-diff-added': <DiffAddedIcon color="#28a745" className="d-inline-block mx-1"/>,
-				'octicon-diff-removed': <DiffRemovedIcon color="#cb2431" className="d-inline-block mx-1"/>,
-				'octicon-diff-modified': <DiffModifiedIcon color="#dbab09" className="d-inline-block mx-1"/>
-			};
+			const icon = select(`[href="${element.hash}"] svg`, jumpList)!.cloneNode(true);
+			const iconTitle = icon.getAttribute('title')!;
+			if (iconTitle === 'added') {
+				icon.classList.add('text-green');
+			} else if (iconTitle === 'removed') {
+				icon.classList.add('text-red');
+			} else {
+				return;
+			}
 
-			element.before(icon[list.get(element.hash)]);
+			icon.classList.remove('select-menu-item-icon');
+			icon.classList.add('d-inline-block', 'mx-1');
+			element.before(icon);
 		}
 	});
 }
