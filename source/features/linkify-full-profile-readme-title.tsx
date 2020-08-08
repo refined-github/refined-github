e9125f7f9287540a162f5a67a1ee29562322361b
Richie Bendall <richiebendall@gmail.com>
2020-08-09 03:04:55 +1200

    Revert prettifier changes
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- source/features/linkify-full-profile-readme-title.tsx
+++ source/features/linkify-full-profile-readme-title.tsx
@@ -14,12 +14,7 @@ async function init(): Promise<void> {
 		const owner = location.pathname.slice(1);
 		const {innerText: repository} = titleNodes[3] as HTMLAnchorElement;
 
-		titleNodes[5].replaceWith(
-			<a
-				href={`/${owner}/${repository}#readme`}
-				className="no-underline link-gray-dark readme-text"
-			>README</a>
-		);
+		titleNodes[5].replaceWith(<a href={`/${owner}/${repository}#readme`} className="no-underline link-gray-dark readme-text">README</a>);
 		select('.readme-text')!.append(
 			titleNodes[6]
 		);
@@ -29,13 +24,13 @@ async function init(): Promise<void> {
 void features.add(
 	{
 		id: __filebasename,
-		description:
-			'Linkify the readme text on profile pages.',
-		screenshot:
-			'https://user-images.githubusercontent.com/29491356/89711998-094b7d80-d9e2-11ea-8ae8-2957960d2308.png'
+		description: 'Linkify the readme text on profile pages.',
+		screenshot: 'https://user-images.githubusercontent.com/29491356/89711998-094b7d80-d9e2-11ea-8ae8-2957960d2308.png'
 	},
 	{
-		include: [pageDetect.isUserProfile],
+		include: [
+			pageDetect.isUserProfile
+		],
 		repeatOnAjax: false,
 		init
 	}
