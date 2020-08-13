4d85461953e19e4951c1e5d943874fa72771ac92
Federico <opensource@bfred.it>
2020-08-13 23:58:32 +0100

    Avoid repetition, use existing button, change copy, button is <button>

--- source/features/convert-release-to-draft.tsx
+++ source/features/convert-release-to-draft.tsx
@@ -10,21 +10,20 @@ import {getRepoURL} from '../github-helpers';
 
 async function convertToDraft({delegateTarget: draftButton}: delegate.Event<MouseEvent, HTMLButtonElement>): Promise<void> {
 	try {
-		draftButton.textContent = 'Converting…';
 		draftButton.append(<LoadingIcon className="ml-2" width={16}/>);
 
 		const tagName = location.pathname.split('/').pop()!;
-		const {id: releaseID} = await api.v3(`repos/${getRepoURL()}/releases/tags/${tagName}`);
-		const response = await api.v3(`repos/${getRepoURL()}/releases/${releaseID as string}`, {
+		const release = await api.v3(`repos/${getRepoURL()}/releases/tags/${tagName}`);
+		await api.v3(release.url, {
 			method: 'PATCH',
 			body: {
 				draft: true
 			}
 		});
 
-		location.pathname = [getRepoURL(), 'releases', 'edit', response.tag_name].join('/');
+		draftButton.previousElementSibling!.click(); // Visit "Edit release" page
 	} catch (error) {
-		draftButton.textContent = 'Error. Open console or retry';
+		draftButton.textContent = 'Error. Check console or retry';
 		features.error(__filebasename, error);
 	}
 }
@@ -37,7 +36,7 @@ async function init(): Promise<void | false> {
 		return false;
 	}
 
-	editButton.after(<a className="btn BtnGroup-item rgh-convert-draft">Convert to draft</a>);
+	editButton.after(<button type="button" className="btn BtnGroup-item rgh-convert-draft">Convert to draft</button>);
 	delegate(document, '.rgh-convert-draft', 'click', convertToDraft);
 }
 
