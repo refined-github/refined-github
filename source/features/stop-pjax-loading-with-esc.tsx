229e85e313e04c59d29bbef6e3ed931c6fe1bdff
artusm <36174850+artusm@users.noreply.github.com>
2020-08-16 11:09:54 +0300

    Fix browser forward button

--- source/features/stop-pjax-loading-with-esc.tsx
+++ source/features/stop-pjax-loading-with-esc.tsx
@@ -12,11 +12,33 @@ function fixProfileNavAndTimeline() {
 	}
 }
 
+function getPjaxContainerId(): string | boolean {
+	const pjaxContainer = select('#js-repo-pjax-container, #js-pjax-container, #gist-pjax-container');
+
+	if (pjaxContainer) {
+		return `#${pjaxContainer.id}`;
+	}
+
+	features.error(__filebasename, 'Pjax container id not found.');
+	return false;
+}
+
 function init() {
 	const progressLoader = select('.progress-pjax-loader')!;
 
 	window.addEventListener('keydown', event => {
 		if (event.key === 'Escape' && progressLoader.classList.contains(progressLoaderLoadingClass)) {
+			const pjaxContainerId = getPjaxContainerId();
+
+			if (pjaxContainerId && history.state && '_id' in history.state) {
+				history.replaceState({
+					url: location.href,
+					title: '',
+					container: pjaxContainerId,
+					...history.state
+				}, '', location.href);
+			}
+
 			history.back();
 			progressLoader.classList.remove(progressLoaderLoadingClass);
 		}
