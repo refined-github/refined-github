3f0f5a1c3bfc07701cb7c95bebcc5940e42050a4
Andrew Luca <thendrluca@gmail.com>
2020-08-18 22:37:26 +0300

    remove offset functions, convert inline

--- source/features/conversation-navigation.tsx
+++ source/features/conversation-navigation.tsx
@@ -2,6 +2,7 @@ import React from 'dom-chef';
 import * as pageDetect from 'github-url-detection';
 import ChevronLeftIcon from 'octicon/chevron-left.svg';
 import ChevronRightIcon from 'octicon/chevron-right.svg';
+import onetime from 'onetime'
 import select from 'select-dom';
 import cache from 'webext-storage-cache';
 
@@ -27,13 +28,13 @@ async function init() {
 	setButtonHref(
 		previousButton,
 		listQuery,
-		list.find(item => item.cursor === alterCursor(conversation!.cursor, -1))
+		list.find(item => item.cursor === conversation!.cursor - 1)
 	);
 
 	setButtonHref(
 		nextButton,
 		listQuery,
-		list.find(item => item.cursor === alterCursor(conversation!.cursor, +1))
+		list.find(item => item.cursor === conversation!.cursor + 1)
 	);
 }
 
@@ -58,11 +59,8 @@ const getButton = (ariaLabel: string, Icon: () => JSX.Element): HTMLAnchorElemen
 	</a>
 ) as any;
 
-// Current default number of items in a conversation list
-const ITEMS_PER_PAGE = 25;
-
 type Conversation = {
-	cursor: string;
+	cursor: number;
 	number: number;
 	url: string;
 };
@@ -73,7 +71,11 @@ const fetchConversationList = cache.function(
 
 		return api.v4(searchQuery).then(({search}) => search.edges.map((edge: any) => ({
 			...edge.node,
-			cursor: edge.cursor
+			// GitHub GraphQL cursor is an offset number starting at 1, with prefix `cursor:` and converted to base64.
+			// e.g. offset 42 is `cursor:42` and converted to base64 is `Y3Vyc29yOjQy`
+			// Always query considering items offsets starting at 1, otherwise unexpected results may happen.
+			// Convert cursor into a number to make operations easier
+			cursor: Number.parseInt(atob(edge.cursor).replace('cursor:', ''), 10)
 		})));
 	},
 	{
@@ -82,13 +84,16 @@ const fetchConversationList = cache.function(
 	}
 );
 
+// Current default number of items in a conversation list
+const ITEMS_PER_PAGE = 25;
+
 async function getConversationList() {
 	const listQuery = getListQuery();
 	const list = await fetchConversationList(listQuery);
 	const currentNumber = getConversationNumber();
 	const conversation = list.find(item => item.number === currentNumber);
-	const after = getCursor((listQuery.page - 1) * ITEMS_PER_PAGE);
-	const before = getCursor((listQuery.page * ITEMS_PER_PAGE) + 1);
+	const after = (listQuery.page - 1) * ITEMS_PER_PAGE;
+	const before = (listQuery.page * ITEMS_PER_PAGE) + 1;
 
 	if (conversation!.cursor !== after && conversation!.cursor !== before) {
 		return {
@@ -97,9 +102,9 @@ async function getConversationList() {
 		};
 	}
 
-	if(conversation!.cursor === after) {
+	if (conversation!.cursor === after) {
 		// `currentConversation` is from previous page, we need to fetch previous page items
-		listQuery.page -= 1
+		listQuery.page -= 1;
 	} else if (conversation!.cursor === before) {
 		// `currentConversation` is from next page, we need to fetch next page items
 		listQuery.page += 1;
@@ -132,18 +137,15 @@ function getListQuery() {
 	};
 }
 
-/**
- * When fetching a page of conversations we want to also fetch last item from previous page, and first item from next page.
- * This is needed for cases when we are on a conversation that is last in current query page, and we need to know if items exist on next page.
- * To take into consideration also, than when we are on first item on first page, we don't have a previous item, and we don't want to fetch using a negative cursor.
- * Read more on {@link getCursor} function
- */
+// When fetching a page of conversations we want to also fetch last item from previous page, and first item from next page.
+// This is needed for cases when conversation is first/last in list, and we need to know if items exist on previous/next page.
+// To take into consideration also, than when we are on first item on first page, we don't have a previous item, and we don't want to fetch using a negative cursor.
 function getConversationSearchQuery({page, query}: ReturnType<typeof getListQuery>): string {
 	return `
 search(
 	type: ISSUE,
 	first: ${page === 1 ? ITEMS_PER_PAGE + 1 : ITEMS_PER_PAGE + 2},
-	after: "${getCursor(page === 1 ? 0 : ((page - 1) * ITEMS_PER_PAGE) - 1)}",
+	after: "${btoa(`cursor:${page === 1 ? 0 : ((page - 1) * ITEMS_PER_PAGE) - 1}`)}",
 	query: "${query}"
 ) {
 	edges {
@@ -157,27 +159,10 @@ search(
 	`;
 }
 
-function getConversationNumber(): number {
+const getConversationNumber = onetime(() => {
 	const conversationHashtag = select('.gh-header-number')?.textContent ?? '#';
 	return Number.parseInt(conversationHashtag.replace('#', ''), 10);
-}
-
-/**
- * Get a GitHub GraphQL cursor based on offset number
- * GitHub items cursors have this format e.g. `cursor:1` and are in base64 format
- * First cursor has offset 1, if offset bellow 1 will be used, still correct first items will be in response but with cursor based on given negative offset
- *
- * @param {number} offset
- * @returns {string}
- */
-function getCursor(offset: number): string {
-	return btoa(`cursor:${offset}`);
-}
-
-function alterCursor(cursor: string, diff: number): string {
-	const index = Number.parseInt(atob(cursor).replace('cursor:', ''), 10);
-	return getCursor(index + diff);
-}
+})
 
 void features.add({
 	id: __filebasename,
