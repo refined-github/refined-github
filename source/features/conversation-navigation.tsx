532e23818ba62115ba7af42c87aa3231ea0fa9aa
Andrew Luca <thendrluca@gmail.com>
2020-08-18 22:37:48 +0300

    make linter happy

--- source/features/conversation-navigation.tsx
+++ source/features/conversation-navigation.tsx
@@ -2,7 +2,7 @@ import React from 'dom-chef';
 import * as pageDetect from 'github-url-detection';
 import ChevronLeftIcon from 'octicon/chevron-left.svg';
 import ChevronRightIcon from 'octicon/chevron-right.svg';
-import onetime from 'onetime'
+import onetime from 'onetime';
 import select from 'select-dom';
 import cache from 'webext-storage-cache';
 
@@ -162,7 +162,7 @@ search(
 const getConversationNumber = onetime(() => {
 	const conversationHashtag = select('.gh-header-number')?.textContent ?? '#';
 	return Number.parseInt(conversationHashtag.replace('#', ''), 10);
-})
+});
 
 void features.add({
 	id: __filebasename,
