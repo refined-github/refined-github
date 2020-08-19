3d5aa63cbe4d67d4b4d7aeb7f305be359831997d
Federico <opensource@bfred.it>
2020-08-19 08:49:25 +0100

    1/*

--- source/features/conversation-navigation.tsx
+++ source/features/conversation-navigation.tsx
@@ -28,7 +28,7 @@ async function init() {
 	const previousButton = getButton('previous');
 	const nextButton = getButton('next');
 
-	select('.gh-header-meta')?.append(
+	select('.gh-header-meta')!.append(
 		<div className="BtnGroup ml-2">
 			{previousButton}
 			{nextButton}
@@ -36,27 +36,29 @@ async function init() {
 	);
 
 	const conversationNumber = looseParseInt(select('.gh-header-number')?.textContent ?? '');
-	const {list, listQuery} = await getConversationList(conversationNumber);
+	const {list, query, page} = await getConversationList(conversationNumber);
 	const conversation = list.find(item => item.number === conversationNumber);
 
 	setButtonHref(
 		previousButton,
-		listQuery,
+		query,
+		page,
 		list.find(item => item.cursor === conversation!.cursor - 1)
 	);
 
 	setButtonHref(
 		nextButton,
-		listQuery,
+		query,
+		page,
 		list.find(item => item.cursor === conversation!.cursor + 1)
 	);
 }
 
-function setButtonHref(button: HTMLAnchorElement, listQuery: ReturnType<typeof getListQuery>, conversation?: Conversation) {
+function setButtonHref(button: HTMLAnchorElement, query: string, page: number, conversation?: Conversation) {
 	if (conversation) {
 		const url = new URL(conversation.url);
-		url.searchParams.set('q', listQuery.query);
-		url.searchParams.set('page', listQuery.page);
+		url.searchParams.set('q', query);
+		url.searchParams.set('page', page);
 		button.setAttribute('aria-disabled', 'false');
 		button.href = url.href;
 	}
@@ -68,82 +70,11 @@ type Conversation = {
 	url: string;
 };
 
-const fetchConversationList = cache.function(
-	async (listQuery: ReturnType<typeof getListQuery>): Promise<Conversation[]> => {
-		const searchQuery = getConversationSearchQuery(listQuery);
-
-		const {search} = await api.v4(searchQuery)
-		return search.edges.map((edge: any) => ({
-			...edge.node,
-			// GitHub GraphQL cursor is an offset number starting at 1, with prefix `cursor:` and converted to base64.
-			// e.g. offset 42 is `cursor:42` and converted to base64 is `Y3Vyc29yOjQy`
-			// Always query considering items offsets starting at 1, otherwise unexpected results may happen.
-			cursor: looseParseInt(atob(edge.cursor))
-		})));
-	},
-	{
-		cacheKey: ([listQuery]) => `${__filebasename}:${JSON.stringify(listQuery)}`,
-		maxAge: 1 / 24 // 1 hour from a day is enough
-	}
-);
-
-// Current default number of items in a conversation list
-const ITEMS_PER_PAGE = 25;
-
-async function getConversationList(conversationNumber: number) {
-	const listQuery = getListQuery();
-	const list = await fetchConversationList(listQuery);
-	const conversation = list.find(item => item.number === conversationNumber);
-	const after = (listQuery.page - 1) * ITEMS_PER_PAGE;
-	const before = (listQuery.page * ITEMS_PER_PAGE) + 1;
-
-	if (conversation!.cursor !== after && conversation!.cursor !== before) {
-		return {
-			list,
-			listQuery
-		};
-	}
-
-	if (conversation!.cursor === after) {
-		// `currentConversation` is from previous page, we need to fetch previous page items
-		listQuery.page -= 1;
-	} else if (conversation!.cursor === before) {
-		// `currentConversation` is from next page, we need to fetch next page items
-		listQuery.page += 1;
-	}
-
-	return {
-		list: await fetchConversationList(listQuery),
-		listQuery
-	};
-}
-
-const noListQuery = (): boolean => !(
-	// Allowed also from global conversation list
-	pageDetect.isConversationList(new URL(document.referrer)) ||
-	new URL(location.href).searchParams.has('q')
-);
-
-function getListQuery() {
-	const referrerUrl = new URL(document.referrer);
-	const url = pageDetect.isConversationList(referrerUrl) ? referrerUrl : new URL(location.href);
-	const searchQuery = new SearchQuery(url);
-
-	if (pageDetect.isRepoConversationList(referrerUrl)) {
-		searchQuery.add(`repo:${getRepoURL()}`);
-	}
-
-	return {
-		page: Number(url.searchParams.get('page') ?? 1),
-		query: searchQuery.get()
-	};
-}
-
-// When fetching a page of conversations we want to also fetch last item from previous page, and first item from next page.
-// This is needed for cases when conversation is first/last in list, and we need to know if items exist on previous/next page.
-// To take into consideration also, than when we are on first item on first page, we don't have a previous item, and we don't want to fetch using a negative cursor.
-function getConversationSearchQuery({page, query}: ReturnType<typeof getListQuery>): string {
-	return `
+const fetchConversationList = cache.function(async (query: string, page: number): Promise<Conversation[]> => {
+	// When fetching a page of conversations we want to also fetch last item from previous page, and first item from next page.
+	// This is needed for cases when conversation is first/last in list, and we need to know if items exist on previous/next page.
+	// To take into consideration also, that when we are on first item on first page, we don't have a previous item, and we don't want to fetch using a negative cursor.
+	const {search} = await api.v4(`
 		search(
 			type: ISSUE,
 			first: ${page === 1 ? ITEMS_PER_PAGE + 1 : ITEMS_PER_PAGE + 2},
@@ -158,7 +89,65 @@ function getConversationSearchQuery({page, query}: ReturnType<typeof getListQuer
 				}
 			}
 		}
-	`;
+	`);
+
+	return search.edges.map((edge: any) => ({
+		...edge.node,
+		// GitHub GraphQL cursor is an offset number starting at 1, with prefix `cursor:` and converted to base64.
+		// e.g. offset 42 is `cursor:42` and converted to base64 is `Y3Vyc29yOjQy`
+		// Always query considering items offsets starting at 1, otherwise unexpected results may happen.
+		cursor: looseParseInt(atob(edge.cursor))
+	}));
+},
+{
+	cacheKey: ([query, page]) => `${__filebasename}:${query}/${page}`,
+	maxAge: 1 / 24
+});
+
+// Current default number of items in a conversation list
+const ITEMS_PER_PAGE = 25;
+
+async function getConversationList(conversationNumber: number) {
+	const listQuery = getConversationListQuery();
+	let page = Number(listQuery.searchParams.get('page') ?? 1);
+	const query = listQuery.get();
+
+	const list = await fetchConversationList(query, page);
+	const conversation = list.find(item => item.number === conversationNumber);
+
+	if (conversation!.cursor === (page - 1) * ITEMS_PER_PAGE) {
+		// `currentConversation` is from previous page, we need to fetch previous page items
+		page -= 1;
+	} else if (conversation!.cursor === (page * ITEMS_PER_PAGE) + 1) {
+		// `currentConversation` is from next page, we need to fetch next page items
+		page += 1;
+	} else {
+		return {query, page, list};
+	}
+
+	return {
+		query,
+		page,
+		list: await fetchConversationList(query, page)
+	};
+}
+
+const noListQuery = (): boolean => !(
+	// Allowed also from global conversation list
+	pageDetect.isConversationList(new URL(document.referrer)) ||
+	new URL(location.href).searchParams.has('q')
+);
+
+function getConversationListQuery(): SearchQuery {
+	const referrerUrl = new URL(document.referrer);
+	const url = pageDetect.isConversationList(referrerUrl) ? referrerUrl : new URL(location.href);
+	const searchQuery = new SearchQuery(url);
+
+	if (pageDetect.isRepoConversationList(url)) {
+		searchQuery.add(`repo:${getRepoURL()}`);
+	}
+
+	return listQuery;
 }
 
 void features.add({
