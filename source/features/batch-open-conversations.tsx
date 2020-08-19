58a99ba46dd3aa018674a56fbf71b23b2bd05b3b
Muescha <184316+muescha@users.noreply.github.com>
2020-08-20 00:43:48 +0200

    show `batch-open-conversations` on all repos

--- source/features/batch-open-conversations.tsx
+++ source/features/batch-open-conversations.tsx
@@ -34,6 +34,21 @@ function openIssues(): void {
 	});
 }
 
+interface IssueConfig {
+	number: string;
+	prependAt: Element;
+}
+
+function getIssueConfig(prRow: Element): IssueConfig {
+	let match = prRow.id.match(/^issue_(\d*)$/);
+	const number: string = match?.[1] ?? '';
+	return {
+		number,
+		prependAt: prRow.children[0]!,
+	};
+}
+
+
 async function init(): Promise<void | false> {
 	if (!await elementReady('.js-issue-row + .js-issue-row')) {
 		return false;
@@ -63,12 +78,44 @@ async function init(): Promise<void | false> {
 				Open selected
 			</button>
 		);
+	} else {
+		select('#js-issues-toolbar')?.prepend(
+			<div className="mr-3 d-none d-md-block">
+  			<input type="checkbox" data-check-all="" aria-label="Select all issues" autoComplete="off"/>
+		  </div>
+		);
+		select('#js-issues-toolbar')?.append(
+			<div className="table-list-triage flex-auto js-issues-toolbar-triage">
+				<span className="text-gray table-list-header-toggle">
+					<span data-check-all-count="">1</span> selected
+					<button type="button" className="btn-link rgh-batch-open-issues pl-3">Open selected</button>
+				</span>
+			</div>
+		);
+
+		const prElements = select.all('.js-issue-row');
+		const prs = prElements.map(getIssueConfig);
+
+		for (const pr of prs) {
+			pr.prependAt.prepend(
+				<label class="flex-shrink-0 py-2 pl-3  d-none d-md-block">
+					<input
+						type="checkbox"
+						data-check-all-item=""
+						className="js-issues-list-check"
+						name="issues[]"
+						value={pr.number}
+						aria-labelledby={`issue_${pr.number}_link`}
+						autoComplete="off" />
+				</label>
+			);
+		}
 	}
 }
 
 void features.add({
 	id: __filebasename,
-	description: 'Adds a button to open multiple conversations at once in your repos.',
+	description: 'Adds a button to open multiple conversations at once.',
 	screenshot: 'https://user-images.githubusercontent.com/1402241/38084752-4820b0d8-3378-11e8-868c-a1582b16f915.gif'
 }, {
 	include: [
