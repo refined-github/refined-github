7db2c3f7bdebd7e069badd5fb7fc1d74e41589f9
Yakov <16872793+yakov116@users.noreply.github.com>
2020-08-21 11:50:21 -0400

    Use Suggestion

--- source/features/linkify-full-profile-readme-title.tsx
+++ source/features/linkify-full-profile-readme-title.tsx
@@ -1,33 +1,22 @@
-import React from 'dom-chef';
 import select from 'select-dom';
 import * as pageDetect from 'github-url-detection';
 
 import features from '.';
 
 function init(): void {
-	const title = select('.user-profile-nav + div .text-mono');
-	if (title) {
-		const {childNodes: titleNodes} = title;
-
-		const owner = location.pathname.slice(1);
-		const {innerText: repository} = titleNodes[3] as HTMLAnchorElement;
-
-		titleNodes[5].replaceWith(<a href={`/${owner}/${repository}#readme`} className="no-underline link-gray-dark readme-text">README</a>);
-		select('.readme-text')!.append(titleNodes[6]);
+	const link = select('.user-profile-nav + div .octicon-smiley + a');
+	while (link?.nextSibling) {
+		link.append(link.nextSibling);
 	}
 }
 
-void features.add(
-	{
-		id: __filebasename,
-		description: 'Linkify the readme text on profile pages.',
-		screenshot: 'https://user-images.githubusercontent.com/29491356/89711998-094b7d80-d9e2-11ea-8ae8-2957960d2308.png'
-	},
-	{
-		include: [
-			pageDetect.isUserProfile
-		],
-		repeatOnAjax: false,
-		init
-	}
-);
+void features.add({
+	id: __filebasename,
+	description: 'Linkify the readme text on profile pages.',
+	screenshot: 'https://user-images.githubusercontent.com/29491356/89711998-094b7d80-d9e2-11ea-8ae8-2957960d2308.png'
+}, {
+	include: [
+		pageDetect.isUserProfileMainTab
+	],
+	init
+});
