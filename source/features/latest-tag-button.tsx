898a69b85ce80fe7c02714236c21e9c1a17489fd
Kid <44045911+kidonng@users.noreply.github.com>
2020-08-26 18:27:42 +0800

    Fix `latest-tag-button` position (#3497)

--- source/features/latest-tag-button.tsx
+++ source/features/latest-tag-button.tsx
@@ -3,7 +3,7 @@ import React from 'dom-chef';
 import cache from 'webext-storage-cache';
 import TagIcon from 'octicon/tag.svg';
 import DiffIcon from 'octicon/diff.svg';
-import elementReady from 'element-ready';
+import select from 'select-dom';
 import * as pageDetect from 'github-url-detection';
 
 import features from '.';
@@ -88,11 +88,6 @@ async function init(): Promise<false | void> {
 		return false;
 	}
 
-	const breadcrumb = await elementReady('#branch-select-menu');
-	if (!breadcrumb) {
-		return;
-	}
-
 	const currentBranch = getCurrentBranch();
 	const url = new GitHubURL(location.href);
 	url.assign({
@@ -106,7 +101,7 @@ async function init(): Promise<false | void> {
 		</a>
 	);
 
-	breadcrumb.after(link);
+	select('#branch-select-menu')!.parentElement!.after(link);
 	if (currentBranch !== latestTag) {
 		link.append(' ', <span className="css-truncate-target">{latestTag}</span>);
 	}
@@ -143,7 +138,7 @@ async function init(): Promise<false | void> {
 		link.setAttribute('aria-label', 'Visit the latest release');
 	}
 
-	link.classList.add('tooltipped', 'tooltipped-ne');
+	link.classList.add('tooltipped', 'tooltipped-ne', 'rgh-latest-tag-button');
 }
 
 void features.add({
