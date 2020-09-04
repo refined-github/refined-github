cc74f0e1e49fad4cc095d5d1abe5ecdc34ec8a2a
Federico Brigante <opensource@bfred.it>
2020-09-04 13:04:11 -0500

    Revert "Improve `cleanup-repo-filelist-actions`’s reliability (#3517)"
    This reverts commit b09477f0cdb0de44810f7b5eb372d331deb161c0.
    
    Due to #3527

--- source/features/cleanup-repo-filelist-actions.tsx
+++ source/features/cleanup-repo-filelist-actions.tsx
@@ -1,7 +1,5 @@
 import React from 'dom-chef';
 import select from 'select-dom';
-import onetime from 'onetime';
-import {observe} from 'selector-observer';
 import PlusIcon from 'octicon/plus.svg';
 import SearchIcon from 'octicon/search.svg';
 import * as pageDetect from 'github-url-detection';
@@ -10,34 +8,30 @@ import features from '.';
 import {groupButtons} from '../github-helpers/group-buttons';
 
 function init(): void {
-	observe('.btn[data-hotkey="t"]', {
-		add(searchButton) {
-			searchButton.classList.add('tooltipped', 'tooltipped-ne');
-			searchButton.setAttribute('aria-label', 'Go to file');
-			searchButton.firstChild!.replaceWith(<SearchIcon/>);
+	const searchButton = select('.btn[data-hotkey="t"]')!;
+	searchButton.classList.add('tooltipped', 'tooltipped-ne');
+	searchButton.setAttribute('aria-label', 'Go to file');
+	searchButton.firstChild!.replaceWith(<SearchIcon/>);
 
-			const addButtonWrapper = searchButton.nextElementSibling!;
-			const addButton = select('.dropdown-caret', addButtonWrapper)?.parentElement;
-			if (addButton) {
-				addButton.classList.add('d-md-block', 'tooltipped', 'tooltipped-ne');
-				addButton.classList.remove('d-md-flex', 'ml-2');
-				addButton.setAttribute('aria-label', 'Add file');
-				addButton.textContent = '';
-				addButton.append(<PlusIcon/>);
+	const addButtonWrapper = searchButton.nextElementSibling!;
+	const addButton = select('.dropdown-caret', addButtonWrapper)?.parentElement;
+	if (addButton) {
+		addButton.classList.add('d-md-block', 'tooltipped', 'tooltipped-ne');
+		addButton.classList.remove('d-md-flex', 'ml-2');
+		addButton.setAttribute('aria-label', 'Add file');
+		addButton.textContent = '';
+		addButton.append(<PlusIcon/>);
 
-				searchButton.classList.remove('mr-2');
-				groupButtons([searchButton, addButtonWrapper]);
-			}
-		}
-	});
+		searchButton.classList.remove('mr-2');
+		groupButtons([searchButton, addButtonWrapper]);
+	}
 
-	observe('get-repo details', {
-		add(downloadButton) {
-			downloadButton.classList.add('tooltipped', 'tooltipped-ne');
-			downloadButton.setAttribute('aria-label', 'Clone or download');
-			select('.octicon-download', downloadButton)!.nextSibling!.remove();
-		}
-	});
+	const downloadButton = select('get-repo details');
+	if (downloadButton) {
+		downloadButton.classList.add('tooltipped', 'tooltipped-ne');
+		downloadButton.setAttribute('aria-label', 'Clone or download');
+		select('.octicon-download', downloadButton)!.nextSibling!.remove();
+	}
 }
 
 void features.add({
@@ -49,5 +43,5 @@ void features.add({
 		pageDetect.isRepoTree,
 		pageDetect.isSingleFile
 	],
-	init: onetime(init)
+	init
 });
