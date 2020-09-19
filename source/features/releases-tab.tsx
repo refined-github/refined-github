175e48aa5b643d49601c738d79bc05850f1bac23
shine <4771718+shinenelson@users.noreply.github.com>
2020-09-20 01:42:21 +0530

    Hide redundant 'Releases' section from repo sidebar (#3566)

--- source/features/releases-tab.tsx
+++ source/features/releases-tab.tsx
@@ -110,6 +110,12 @@ async function init(): Promise<false | void> {
 			})
 		);
 
+		// Hide redundant 'Releases' section from repo sidebar
+		if (pageDetect.isRepoRoot()) {
+			const sidebarReleases = await elementReady('.BorderGrid-cell a[href$="/releases"]');
+			sidebarReleases!.closest('.BorderGrid-row')!.setAttribute('hidden', '');
+		}
+
 		return;
 	}
 
