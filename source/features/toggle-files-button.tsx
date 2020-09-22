refined-github/refined-github

commit 8b3b90b83290294117fe0fc518452a6297db719f

Apply suggestions from code review
Co-authored-by: Federico <me@fregante.com>

source/features/toggle-files-button.tsx
@@ -35,12 +35,13 @@ function addButton(): void {
 }
 
 async function init(): Promise<void> {
-	const cacheKey = 'files-list-toggled-off';
+	const cacheKey = 'hide-file-list';
 	const repoContent = (await elementReady('.repository-content'))!;
 	observeElement(repoContent, addButton);
 
 	delegate(document, '.rgh-toggle-files', 'click', async () => {
-		await cache.set(cacheKey, repoContent.classList.toggle('rgh-files-hidden'));
+		// Persist a closed list or remove from cache
+		await cache.set(cacheKey, repoContent.classList.toggle('rgh-files-hidden') || undefined);
 	});
 
 	if (await cache.get<boolean>(cacheKey)) {

commit a93b3a5a5d95e81b1a38fbca3db68dc95f33d626

Fix bug introduced by 8b3b90b

source/features/toggle-files-button.tsx
@@ -40,12 +40,11 @@ async function init(): Promise<void> {
 	observeElement(repoContent, addButton);
 
 	delegate(document, '.rgh-toggle-files', 'click', async () => {
-		// Persist a closed list or remove from cache
-		await cache.set(cacheKey, (repoContent.classList.toggle('rgh-files-hidden') || undefined)!);
+		await cache.set(cacheKey, repoContent.classList.toggle('rgh-files-hidden'));
 	});
 
 	if (await cache.get<boolean>(cacheKey)) {
-		repoContent.classList.toggle('rgh-files-hidden');
+		select('.rgh-toggle-files')!.click();
 	}
 }
 