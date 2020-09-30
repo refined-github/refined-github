b747dd37d99a850595d56025f0b13aaaa33fc228
cheap-glitch <cheap.glitch@gmail.com>
2020-09-30 15:15:49 +0200

    Show next scheduled time of GitHub Actions

---
+++ source/features/show-github-action-next-time.tsx
@@ -0,0 +1,74 @@
+import './show-github-action-next-time.css';
+import React from 'dom-chef';
+import cache from 'webext-storage-cache';
+import select from 'select-dom';
+import elementReady from 'element-ready';
+import * as pageDetect from 'github-url-detection';
+import {parseCron} from '@cheap-glitch/mi-cron';
+
+import features from '.';
+import * as api from '../github-helpers/api';
+import {getRepoURL, getRepoGQL, getCurrentBranch} from '../github-helpers';
+
+const getWorkflows = cache.function(async (): Promise<Array<{[index: string]: {[index: string]: string}}>> => {
+	const {repository: {object: {entries: workflows}}} = await api.v4(
+		`repository(${getRepoGQL()}) {
+			object(expression: "${getCurrentBranch()}:.github/workflows") {
+				... on Tree {
+					entries {
+						object { ... on Blob { text } }
+					}
+				}
+			}
+		}
+	`);
+
+	return workflows;
+}, {
+	maxAge: {
+		days: 1
+	},
+	cacheKey: () => __filebasename + ':' + getRepoURL()
+});
+
+function parseYamls(yamls: string[]): {[index: string]: string} {
+	return Object.fromEntries(yamls
+		.map(yaml => [(/^name:\s+(\S+)$/m.exec(yaml) ?? [])[1], (/^\s*-\scron:\s+['"](.+)['"]/m.exec(yaml) ?? [])[1]])
+		.filter(([name, cron]) => name !== undefined && cron !== undefined)
+	);
+}
+
+async function init(): Promise<void> {
+	const actionsSidebar = (await elementReady('.hx_actions-sidebar'))!;
+
+	if (actionsSidebar) {
+		const currentDate = new Date();
+		const workflows = await getWorkflows();
+
+		if (workflows) {
+			const actionsSchedules = parseYamls(workflows.map(workflow => workflow.object.text));
+
+			for (const actionListItem of select.all('li > a', actionsSidebar).slice(1)) {
+				const actionName = actionListItem.textContent!.trim();
+				if (actionName in actionsSchedules) {
+					const nextTime = parseCron.nextDate(actionsSchedules[actionName], currentDate);
+					if (nextTime) {
+						actionListItem.append(<span className="rgh-github-action-next-time">(next <relative-time datetime={nextTime.toString()}/>)</span>);
+					}
+				}
+			}
+		}
+	}
+}
+
+void features.add({
+	id: __filebasename,
+	description: '',
+	screenshot: 'TODO'
+}, {
+	include: [
+		pageDetect.isRepo
+	],
+	awaitDomReady: false,
+	init
+});
