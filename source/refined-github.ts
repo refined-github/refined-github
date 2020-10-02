bef4b211ea364bf6cc5f5a24bf7651b6ffb2fc1c
cheap glitch <cheap.glitch@gmail.com>
2020-10-02 06:09:21 +0200

    Add `next-scheduled-github-action` feature (#3604)
    Co-authored-by: Federico <me@fregante.com>

--- source/refined-github.ts
+++ source/refined-github.ts
@@ -198,6 +198,7 @@ import './features/same-page-definition-jump';
 import './features/new-repo-disable-projects-and-wikis';
 import './features/table-input';
 import './features/link-to-gihub-io';
+import './features/next-scheduled-github-action';
 
 // Add global for easier debugging
 (window as any).select = select;
