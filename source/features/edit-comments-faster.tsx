fb893fd37cc4d58bccf18e2d84b6524b3cb98cfa
Federico Brigante <opensource@bfred.it>
2020-10-07 17:29:47 -0500

    Lint and comment

--- source/features/edit-comments-faster.tsx
+++ source/features/edit-comments-faster.tsx
@@ -7,12 +7,18 @@ import * as pageDetect from 'github-url-detection';
 import features from '.';
 
 function init(): void {
+	// Find editable comments first, then traverse to the correct position
 	observe('.js-comment .js-comment-update:not(.rgh-edit-comment)', {
 		add(comment) {
 			comment.classList.add('rgh-edit-comment');
 
 			comment.closest('.js-comment')!.querySelector('.js-comment-header-reaction-button')!.after(
-				<button type="button" role="menuitem" className="timeline-comment-action btn-link js-comment-edit-button" aria-label="Edit comment">
+				<button
+					type="button"
+					role="menuitem"
+					className="timeline-comment-action btn-link js-comment-edit-button"
+					aria-label="Edit comment"
+				>
 					<PencilIcon/>
 				</button>
 			);
