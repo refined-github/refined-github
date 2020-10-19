d9e41186485836e72ccbfedce6a1f535c574887a
Yakov <16872793+yakov116@users.noreply.github.com>
2020-10-19 09:12:56 -0400

    Avoid recreating the line-actions in `view-markdown-source`

--- source/features/view-markdown-source.tsx
+++ source/features/view-markdown-source.tsx
@@ -7,73 +7,19 @@ import * as pageDetect from 'github-url-detection';
 
 import FileIcon from 'octicon/file.svg';
 import CodeIcon from 'octicon/code.svg';
-import KebabHorizontalIcon from 'octicon/kebab-horizontal.svg';
 
 import features from '.';
 import fetchDom from '../helpers/fetch-dom';
 import GitHubURL from '../github-helpers/github-url';
 import {buildRepoURL} from '../github-helpers';
 
-const lineActions = onetime(() => (
-	<details
-		className="details-reset details-overlay BlobToolbar position-absolute js-file-line-actions dropdown d-none"
-		aria-hidden="true"
-	>
-		<summary
-			className="btn-octicon ml-0 px-2 p-0 bg-white border border-gray-dark rounded-1"
-			aria-label="Inline file action toolbar"
-			aria-haspopup="menu"
-			role="button"
-		>
-			<KebabHorizontalIcon/>
-		</summary>
-		<details-menu role="menu">
-			<ul
-				className="BlobToolbar-dropdown dropdown-menu dropdown-menu-se mt-2"
-				style={{width: '185px'}}
-			>
-				<li>
-					<clipboard-copy
-						role="menuitem"
-						className="dropdown-item zeroclipboard-link"
-						id="js-copy-lines"
-					>
-						Copy line
-					</clipboard-copy>
-				</li>
-				<li>
-					<clipboard-copy
-						role="menuitem"
-						className="dropdown-item zeroclipboard-link"
-						id="js-copy-permalink"
-					>
-						Copy permalink
-					</clipboard-copy>
-				</li>
-				<li>
-					<a
-						className="dropdown-item js-update-url-with-hash"
-						id="js-view-git-blame"
-						role="menuitem"
-						href={new GitHubURL(location.href).assign({route: 'blame'}).href}
-					>
-						View git blame
-					</a>
-				</li>
-				<li>
-					<a
-						className="dropdown-item"
-						id="js-new-issue"
-						role="menuitem"
-						href={buildRepoURL('issues/new')}
-					>
-						Reference in new issue
-					</a>
-				</li>
-			</ul>
-		</details-menu>
-	</details>
-));
+const lineActions = onetime(async () => {
+	// Avoid having to create the entire 70 lines of JSX. The URL is hardcoded to support enterprise
+	const blobToolbar = await fetchDom('https://github.com/sindresorhus/refined-github/blob/b1229bbaeb8cf071f0711bc2ed1b40dd96cd7a05/.editorconfig', '.BlobToolbar');
+	select<HTMLAnchorElement>('#js-view-git-blame', blobToolbar)!.href = new GitHubURL(location.href).assign({route: 'blame'}).href;
+	select<HTMLAnchorElement>('#js-new-issue', blobToolbar)!.href = buildRepoURL('issues/new');
+	return blobToolbar;
+});
 
 const buttonBodyMap = new WeakMap<Element, Element | Promise<Element>>();
 
@@ -119,7 +65,7 @@ async function showSource(): Promise<void> {
 	sourceButton.classList.add('selected');
 	renderedButton.classList.remove('selected');
 	blurButton(sourceButton);
-	(await source).before(lineActions());
+	(await source).before(await lineActions() as unknown as HTMLDetailsElement);
 
 	dispatchEvent(sourceButton, 'rgh:view-markdown-source');
 }
@@ -137,7 +83,7 @@ async function showRendered(): Promise<void> {
 	sourceButton.classList.remove('selected');
 	renderedButton.classList.add('selected');
 	blurButton(renderedButton);
-	lineActions().remove();
+	(lineActions() as unknown as HTMLDetailsElement).remove();
 
 	dispatchEvent(sourceButton, 'rgh:view-markdown-rendered');
 }
