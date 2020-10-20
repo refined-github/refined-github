7551cbc2588f7c2d28f0c35ab34383627f743b02
Federico <me@fregante.com>
2020-10-20 00:42:36 -0500

    Restore fetch-dom.ts

--- source/helpers/fetch-dom.ts
+++ source/helpers/fetch-dom.ts
@@ -2,11 +2,11 @@ import mem from 'mem';
 import domify from 'doma';
 
 async function fetchDom(url: string): Promise<DocumentFragment>;
-async function fetchDom<TElement extends Element>(url: string, selector: string, backgroundRequest?: boolean): Promise<TElement | undefined>;
-async function fetchDom(url: string, selector?: string, backgroundRequest?: boolean): Promise<Node | undefined> {
+async function fetchDom<TElement extends Element>(url: string, selector: string): Promise<TElement | undefined>;
+async function fetchDom(url: string, selector?: string): Promise<Node | undefined> {
 	const absoluteURL = new URL(url, location.origin).toString(); // Firefox `fetch`es from the content script, so relative URLs fail
-	const response = backgroundRequest ? await browser.runtime.sendMessage({request: absoluteURL}) : await fetch(absoluteURL);
-	const dom = domify(await response.text?.() ?? response);
+	const response = await fetch(absoluteURL);
+	const dom = domify(await response.text());
 	if (selector) {
 		return dom.querySelector(selector) ?? undefined;
 	}
