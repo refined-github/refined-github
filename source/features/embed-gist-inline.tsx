42ce2dffb59dff541232ed60fcddb8ece9a1e91c
yakov116 <16872793+yakov116@users.noreply.github.com>
2020-10-20 21:12:46 -0400

    Fetch widget for `view-markdown-source` instead of maintaining a copy of it (#3670)
    Co-authored-by: Federico <me@fregante.com>

--- source/features/embed-gist-inline.tsx
+++ source/features/embed-gist-inline.tsx
@@ -22,7 +22,7 @@ async function embedGist(link: HTMLAnchorElement): Promise<void> {
 		// Get the gist via background.js due to CORB policies introduced in Chrome 73
 		const gistData = await browser.runtime.sendMessage({request: `${link.href}.json`});
 
-		const files = domify.one(gistData.div)!;
+		const files = domify.one(JSON.parse(gistData).div)!;
 		const fileCount = files.children.length;
 
 		if (fileCount > 1) {
