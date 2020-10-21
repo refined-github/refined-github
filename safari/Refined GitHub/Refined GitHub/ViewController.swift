6a90fa757db809c2e5d9a33274990521a3197200
Sindre Sorhus <sindresorhus@gmail.com>
2020-10-22 01:53:06 +0200

    Update ViewController.swift

--- safari/Refined GitHub/Refined GitHub/ViewController.swift
+++ safari/Refined GitHub/Refined GitHub/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  Refined GitHub
-//
-//  Created by Rico on 21/10/20.
-//
-
 import Cocoa
 import SafariServices.SFSafariApplication
 import SafariServices.SFSafariExtensionManager
@@ -12,40 +5,44 @@ import SafariServices.SFSafariExtensionManager
 let appName = "Refined GitHub"
 let extensionBundleIdentifier = "com.sindresorhus.Refined-GitHub-Extension"
 
-class ViewController: NSViewController {
+final class ViewController: NSViewController {
+	@IBOutlet var appNameLabel: NSTextField!
 
-    @IBOutlet var appNameLabel: NSTextField!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.appNameLabel.stringValue = appName
-        SFSafariExtensionManager.getStateOfSafariExtension(withIdentifier: extensionBundleIdentifier) { (state, error) in
-            guard let state = state, error == nil else {
-                // Insert code to inform the user that something went wrong.
-                return
-            }
+	override func viewDidLoad() {
+		super.viewDidLoad()
 
-            DispatchQueue.main.async {
-                if (state.isEnabled) {
-                    self.appNameLabel.stringValue = "\(appName)'s extension is currently on."
-                } else {
-                    self.appNameLabel.stringValue = "\(appName)'s extension is currently off. You can turn it on in Safari Extensions preferences."
-                }
-            }
-        }
-    }
-    
-    @IBAction func openSafariExtensionPreferences(_ sender: AnyObject?) {
-        SFSafariApplication.showPreferencesForExtension(withIdentifier: extensionBundleIdentifier) { error in
-            guard error == nil else {
-                // Insert code to inform the user that something went wrong.
-                return
-            }
+		appNameLabel.stringValue = appName
 
-            DispatchQueue.main.async {
-                NSApplication.shared.terminate(nil)
-            }
-        }
-    }
+		SFSafariExtensionManager.getStateOfSafariExtension(withIdentifier: extensionBundleIdentifier) { state, error in
+			guard
+				let state = state,
+				error == nil
+			else {
+				// Insert code to inform the user that something went wrong.
+				return
+			}
 
+			DispatchQueue.main.async {
+				if state.isEnabled {
+					self.appNameLabel.stringValue = "\(appName)'s extension is currently on."
+				} else {
+					self.appNameLabel.stringValue = "\(appName)'s extension is currently off. You can turn it on in Safari Extensions preferences."
+				}
+			}
+		}
+	}
+
+	@IBAction
+	func openSafariExtensionPreferences(_ sender: AnyObject?) {
+		SFSafariApplication.showPreferencesForExtension(withIdentifier: extensionBundleIdentifier) { error in
+			guard error == nil else {
+				// Insert code to inform the user that something went wrong.
+				return
+			}
+
+			DispatchQueue.main.async {
+				NSApp.terminate(nil)
+			}
+		}
+	}
 }
