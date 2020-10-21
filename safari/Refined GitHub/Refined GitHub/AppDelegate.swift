ab47b3ae45a989786baa132998e5e964f4526a88
Sindre Sorhus <sindresorhus@gmail.com>
2020-10-22 01:54:51 +0200

    Update AppDelegate.swift

--- safari/Refined GitHub/Refined GitHub/AppDelegate.swift
+++ safari/Refined GitHub/Refined GitHub/AppDelegate.swift
@@ -1,21 +1,6 @@
-//
-//  AppDelegate.swift
-//  Refined GitHub
-//
-//  Created by Rico on 21/10/20.
-//
-
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
-    
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
-    
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-    
+final class AppDelegate: NSObject, NSApplicationDelegate {
+	func applicationDidFinishLaunching(_ notification: Notification) {}
 }
