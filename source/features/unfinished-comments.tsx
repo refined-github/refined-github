de97d767d4a7ae51b5b69bf38ee946191a1adb1f
cheap-glitch <cheap.glitch@gmail.com>
2020-10-31 19:53:57 +0100

    Move listener to top level to avoid dataset hack

--- source/features/unfinished-comments.tsx
+++ source/features/unfinished-comments.tsx
@@ -3,28 +3,23 @@ import * as pageDetect from 'github-url-detection';
 
 import features from '.';
 
-let documentTitle = document.title;
+let documentTitle = '';
+
+async function updateDocumentTitle(): Promise<void> {
+	if (document.visibilityState === 'hidden') {
+		if (select.all('textarea').some(textarea => textarea.value.length > 0 && (textarea.offsetWidth > 0 || textarea.offsetHeight > 0))) {
+			console.log('changing title');
+			documentTitle = document.title;
+			document.title = '(Draft comment) ' + document.title;
+		}
+	} else {
+		document.title = documentTitle;
+	}
+}
 
 function init(): void {
-	if (document.body.dataset.visibilityListenerAdded) {
-		return;
-	}
-
-	const textareas = select.all('textarea');
-
-	document.addEventListener('visibilitychange', async () => {
-		if (document.visibilityState === 'hidden') {
-			if (textareas.some(textarea => (textarea.offsetWidth > 0 || textarea.offsetHeight > 0) && textarea.value.length > 0)) {
-				documentTitle = document.title;
-				document.title = '(Draft comment) ' + document.title;
-			}
-
-			return;
-		}
-
-		document.title = documentTitle;
-	});
-	document.body.dataset.visibilityListenerAdded = String(true);
+	documentTitle = document.title;
+	document.addEventListener('visibilitychange', updateDocumentTitle);
 }
 
 void features.add(__filebasename, {
