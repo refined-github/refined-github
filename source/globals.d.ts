3c1eb788d14305595a90fe91c5198ccc03287819
Federico <me@fregante.com>
2020-11-02 23:08:31 -0600

    Mention https://github.com/DefinitelyTyped/DefinitelyTyped/issues/48806

--- source/globals.d.ts
+++ source/globals.d.ts
@@ -25,6 +25,7 @@ declare module 'markdown-wasm/dist/markdown.node';
 
 declare module 'size-plugin';
 
+// TODO: until https://github.com/DefinitelyTyped/DefinitelyTyped/issues/48806
 declare module 'terser-webpack-plugin';
 
 // Custom UI events specific to RGH
