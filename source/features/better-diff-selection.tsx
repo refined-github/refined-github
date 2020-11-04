a33851465d292da20c43e63a713b8074e8749c2f
cheap-glitch <cheap.glitch@gmail.com>
2020-11-04 20:43:43 +0100

    Use CSS and data attributes to simplify logic

--- source/features/better-diff-selection.tsx
+++ source/features/better-diff-selection.tsx
@@ -1,33 +1,24 @@
+import './better-diff-selection.css';
+
 import select from 'select-dom';
 import delegate from 'delegate-it';
 import * as pageDetect from 'github-url-detection';
 
 import features from '.';
 
-let disabledDiffTable: HTMLElement | undefined;
-
-const leftDiffLines = '.diff-table tr:not(.js-expandable-line) td:nth-child(2)';
-const rightDiffLines = '.diff-table tr:not(.js-expandable-line) td:nth-child(4)';
-
-function disableDiffSelection(diffLines: string, clickedElement: HTMLElement): void {
-	disabledDiffTable = clickedElement.closest('tbody') as HTMLElement;
-
-	for (const diffLine of select.all(diffLines, disabledDiffTable)) {
-		diffLine.style.userSelect = 'none';
-	}
-}
+let selectedDiffTableBody: HTMLElement | undefined;
 
 function init(): void {
-	delegate(document.body, leftDiffLines, 'mousedown', event => disableDiffSelection(rightDiffLines, event.target as HTMLElement));
-	delegate(document.body, rightDiffLines, 'mousedown', event => disableDiffSelection(leftDiffLines, event.target as HTMLElement));
+	delegate(document.body, '.diff-table tr:not(.js-expandable-line) td:nth-child(even)', 'mousedown', event => {
+		const target = event.target as HTMLElement;
+		selectedDiffTableBody = target.closest('tbody')!;
+		selectedDiffTableBody.dataset.rghSelect = target.closest('td') === target.closest('tr')!.children[1] ? 'left' : 'right';
+	});
 
 	document.body.addEventListener('mouseup', () => {
-		if (disabledDiffTable) {
-			for (const diffLine of select.all('tr:not(.js-expandable-line) td:nth-child(even)', disabledDiffTable)) {
-				diffLine.style.userSelect = 'auto';
-			}
-
-			disabledDiffTable = undefined;
+		if (selectedDiffTableBody) {
+			selectedDiffTableBody.removeAttribute('data-rgh-select');
+			selectedDiffTableBody = undefined;
 		}
 	});
 }
