30637160d10855a90d56594b221c6089a47cef44
cheap-glitch <cheap.glitch@gmail.com>
2020-11-04 18:14:10 +0100

    Rewrite logic to target on diff table at a time

--- source/features/better-diff-selection.tsx
+++ source/features/better-diff-selection.tsx
@@ -4,22 +4,39 @@ import * as pageDetect from 'github-url-detection';
 
 import features from '.';
 
+let disabledDiffTable: HTMLElement | undefined;
+
 const leftDiffLines = '.diff-table tr:not(.js-expandable-line) td:nth-child(2)';
 const rightDiffLines = '.diff-table tr:not(.js-expandable-line) td:nth-child(4)';
 
-function disableDiffSelection(diffLines: string): void {
-	for (const diffLine of select.all(diffLines)) {
+function getTableBody(element: HTMLElement): HTMLElement {
+	let parent = element;
+	do {
+		parent = parent.parentElement!;
+	} while (parent.tagName !== 'TBODY');
+
+	return parent;
+}
+
+function disableDiffSelection(diffLines: string, clickedElement: HTMLElement): void {
+	disabledDiffTable = getTableBody(clickedElement);
+
+	for (const diffLine of select.all(diffLines, disabledDiffTable)) {
 		diffLine.style.userSelect = 'none';
 	}
 }
 
 function init(): void {
-	delegate(document.body, leftDiffLines, 'mousedown', () => disableDiffSelection(rightDiffLines));
-	delegate(document.body, rightDiffLines, 'mousedown', () => disableDiffSelection(leftDiffLines));
+	delegate(document.body, leftDiffLines, 'mousedown', event => disableDiffSelection(rightDiffLines, event.target as HTMLElement));
+	delegate(document.body, rightDiffLines, 'mousedown', event => disableDiffSelection(leftDiffLines, event.target as HTMLElement));
 
 	document.body.addEventListener('mouseup', () => {
-		for (const diffLine of select.all('.diff-table tr:not(.js-expandable-line) td:nth-child(even)')) {
-			diffLine.style.userSelect = 'auto';
+		if (disabledDiffTable) {
+			for (const diffLine of select.all('tr:not(.js-expandable-line) td:nth-child(even)', disabledDiffTable)) {
+				diffLine.style.userSelect = 'auto';
+			}
+
+			disabledDiffTable = undefined;
 		}
 	});
 }
