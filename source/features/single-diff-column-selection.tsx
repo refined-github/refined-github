15fe3c6fafa55f618d283809367fcc23387915a6
cheap-glitch <cheap.glitch@gmail.com>
2020-11-07 14:27:28 +0100

    Simplify logic

--- source/features/single-diff-column-selection.tsx
+++ source/features/single-diff-column-selection.tsx
@@ -6,18 +6,15 @@ import delegate from 'delegate-it';
 import features from '.';
 
 function disableDiffSelection(event: delegate.Event<MouseEvent, HTMLElement>): void {
-	event.delegateTarget.closest('tbody')!.dataset.rghSelect = event.delegateTarget.closest('td:last-child') ? 'right' : 'left';
-}
-
-function restoreDiffSelection(): void {
-	if (document.getSelection()!.isCollapsed) {
+	if (event.button === 0) {
+		document.getSelection()!.empty();
 		select('[data-rgh-select]')?.removeAttribute('data-rgh-select');
+		event.delegateTarget.closest('tbody')!.dataset.rghSelect = event.delegateTarget.closest('td:last-child') ? 'right' : 'left';
 	}
 }
 
 function init(): void {
 	delegate(document.body, '.blob-code', 'mousedown', disableDiffSelection);
-	document.body.addEventListener('selectionchange', restoreDiffSelection);
 }
 
 void features.add(__filebasename, {
