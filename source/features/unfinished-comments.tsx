7282f020c6121e08d7b071fa543f705e34b56466
cheap-glitch <cheap.glitch@gmail.com>
2020-11-08 15:45:57 +0100

    Tag forms sending comment with custom class

--- source/features/unfinished-comments.tsx
+++ source/features/unfinished-comments.tsx
@@ -7,22 +7,27 @@ import features from '.';
 let documentTitle: string | undefined;
 
 function hasDraftComments(): boolean {
-	return select.all('textarea').some(textarea => textarea.value.length > 0 && textarea.offsetWidth > 0);
+	return select.all('textarea').some(textarea => textarea.value.length > 0 && textarea.offsetWidth > 0 && !textarea.closest('.rgh-is-sending-comment'));
 }
 
 function updateDocumentTitle(): void {
-	if (document.visibilityState === 'hidden' && hasDraftComments() && !documentTitle) {
+	if (document.visibilityState === 'hidden' && hasDraftComments()) {
 		documentTitle = document.title;
 		document.title = '(Draft comment) ' + document.title;
 	} else if (documentTitle) {
 		document.title = documentTitle;
 		documentTitle = undefined;
 	}
+	select('.rgh-is-sending-comment')?.classList.remove('rgh-is-sending-comment');
+}
+
+function tagSendingForm({delegateTarget}: delegate.Event<Event, HTMLElement>): void {
+	delegateTarget.classList.add('rgh-is-sending-comment');
 }
 
 function init(): void {
 	document.addEventListener('visibilitychange', updateDocumentTitle);
-	delegate(document.body, 'form', 'submit', updateDocumentTitle);
+	delegate(document.body, 'form', 'submit', tagSendingForm);
 }
 
 void features.add(__filebasename, {
