6a76dd7e3f98e11364439976315455a623321704
Federico Brigante <me@fregante.com>
2020-11-09 17:54:08 -0600

    works

--- source/features/single-diff-column-selection.tsx
+++ source/features/single-diff-column-selection.tsx
@@ -5,16 +5,43 @@ import delegate from 'delegate-it';
 
 import features from '.';
 
-function disableDiffSelection(event: delegate.Event<MouseEvent, HTMLElement>): void {
-	if (event.button === 0) {
-		document.getSelection()!.empty();
+function getSide(element: Element): 'right' | 'left' {
+	return element.closest('td:last-child') ? 'right' : 'left';
+}
+
+function pickSelectionSide(event: delegate.Event<MouseEvent, HTMLElement>): void {
+}
+
+function unpickSelectionSide(event: delegate.Event<MouseEvent, HTMLElement>): void {
+	const currentSide = event.delegateTarget.closest('tbody')!.dataset.rghSelect;
+	if (getSide(event.delegateTarget) !== currentSide) {
+		getSelection()!.removeAllRanges();
 		select('[data-rgh-select]')?.removeAttribute('data-rgh-select');
-		event.delegateTarget.closest('tbody')!.dataset.rghSelect = event.delegateTarget.closest('td:last-child') ? 'right' : 'left';
+	}
+}
+
+function resetClassesWhenEmpty(): void {
+	const selection = getSelection()!;
+
+	if (selection.isCollapsed) {
+		select('[data-rgh-select]')?.removeAttribute('data-rgh-select');
+		return;
+	}
+
+	const range = selection.getRangeAt(0);
+
+	if (!['TR', 'TBODY'].includes((range.commonAncestorContainer as Element).tagName)) {
+		// Ignore
+
+	} else if (range.startContainer.parentElement!.closest('.blob-code')) {
+		range.startContainer.parentElement!.closest('tbody')!.dataset.rghSelect = getSide(range.startContainer.parentElement!);
 	}
 }
 
 function init(): void {
-	delegate(document.body, '.blob-code', 'mousedown', disableDiffSelection);
+	delegate(document.body, '.blob-code', 'selectstart', pickSelectionSide);
+	delegate(document.body, '[data-rgh-select] .blob-code', 'mousedown', unpickSelectionSide);
+	document.addEventListener('selectionchange', resetClassesWhenEmpty);
 }
 
 void features.add(__filebasename, {
