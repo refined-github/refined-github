f4fd74f1c5a59be03d43308ea6e6dae2d5975f45
Christian Jerson Atilano <xtian.atilano@gmail.com>
2020-11-10 03:22:27 +0800

    converted to selector-observer

--- source/features/link-to-requested-change-comment.tsx
+++ source/features/link-to-requested-change-comment.tsx
@@ -1,21 +1,27 @@
 import React from 'dom-chef';
 import select from 'select-dom';
+import onetime from 'onetime';
+import {observe} from 'selector-observer';
 import * as pageDetect from 'github-url-detection';
 
 import {wrap} from '../helpers/dom-utils';
 import features from '.';
 
 function init(): void {
-	for (const reviewItem of select.all('.merge-status-item.review-item')) {
-		const comment = select<HTMLAnchorElement>('.review-status-item details details-menu a', reviewItem)!;
-		wrap(reviewItem, <a href={comment.href}/>);
-	}
+	observe('div.js-merge-message-container', {
+		constructor: HTMLDivElement,
+		add(messageContainer) {
+			for (const reviewItem of select.all('.merge-status-item.review-item', messageContainer)) {
+				const comment = select<HTMLAnchorElement>('.review-status-item details details-menu a', reviewItem)!;
+				wrap(reviewItem, <a href={comment.href}/>);
+			}
+		}
+	});
 }
 
 void features.add(__filebasename, {
 	include: [
 		pageDetect.isPRConversation
 	],
-	awaitDomReady: false,
-	init
+	init: onetime(init)
 });
