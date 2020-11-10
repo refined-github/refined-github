4c9fabb7d38b1748da3661dfc837e0c3ec7ee85d
Federico Brigante <me@fregante.com>
2020-11-09 18:42:55 -0600

    Comment and reset table with a click anywhere on the page

--- source/features/single-diff-column-selection.tsx
+++ source/features/single-diff-column-selection.tsx
@@ -10,22 +10,33 @@ function getSide(element: Element): 'right' | 'left' {
 }
 
 function pickSelectionSide(event: delegate.Event): void {
-	const table = event.delegateTarget.closest('tbody')!;
+	const table = event.delegateTarget.closest('table')!;
 	table.dataset.rghSelect = getSide(event.delegateTarget);
-	table.addEventListener('mousedown', unpickSelectionSide);
+
+	// Selection events are disabled on `user-select:none`, so `mousedown` is what unsets it before they can happen (in the same "mouse click + drag" event)
+	document.addEventListener('mousedown', unpickSelectionSide);
 }
 
 function unpickSelectionSide(event: Event): void {
-	const table = event.currentTarget as HTMLElement;
-	if (getSide(event.target as Element) !== table.dataset.rghSelect) {
-		getSelection()!.removeAllRanges();
-		delete table.dataset.rghSelect;
-		table.removeEventListener('mousedown', unpickSelectionSide);
+	// Note: The table definitely exists or else this listener wouldn't be called
+	const table = select('[data-rgh-select]')!;
+	const clickedElement = event.target as Element;
+
+	// No changes are required if the user is clicking the same side
+	if (table.contains(clickedElement) && getSide(clickedElement) === table.dataset.rghSelect) {
+		return;
 	}
+
+	document.removeEventListener('mousedown', unpickSelectionSide);
+	delete table.dataset.rghSelect;
+
+	// At this point, without `user-select: none`, Chrome will immediately consider both sides selected, so it will flash the other side’s selection *and* start dragging that text, if the mouse is trying to selec it: https://user-images.githubusercontent.com/46634000/98528355-79390080-227c-11eb-9083-d046a7a61f13.gif
+	// This line resets the selection to avoid this behavior
+	getSelection()!.removeAllRanges();
 }
 
 function init(): void {
-	delegate(document.body, '.blob-code', 'selectstart', pickSelectionSide);
+	delegate(document, '.blob-code', 'selectstart', pickSelectionSide);
 }
 
 void features.add(__filebasename, {
