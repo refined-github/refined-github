583afe7f94b9e71c45eed5d7d216a904ac54eefb
Christian Jerson Atilano <xtian.atilano@gmail.com>
2020-11-12 13:49:44 +0800

    updated feature name and added rgh class

--- source/features/jump-to-change-requested-comment.tsx
+++ source/features/jump-to-change-requested-comment.tsx
@@ -8,9 +8,10 @@ import {wrap} from '../helpers/dom-utils';
 import features from '.';
 
 function init(): void {
-	observe('.review-item .dropdown-item[href^="#pullrequestreview-"]', {
+	observe('.review-item .dropdown-item[href^="#pullrequestreview-"]:not(.rgh-jump-to-change-requested-comment)', {
 		constructor: HTMLAnchorElement,
 		add(messageContainer) {
+			messageContainer.classList.add('rgh-jump-to-change-requested-comment');
 			const element = select('.review-status-item div[title*="requested changes"]')?.lastChild;
 
 			if (element) {
