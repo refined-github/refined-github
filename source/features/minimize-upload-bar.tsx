6ac6bddf2c106a51829cbc35f7c9b8673872df19
Kenneth <kennethtsang9286@gmail.com>
2020-11-15 08:47:35 +1100

    feature/show-upload-image: Put classname in body

--- source/features/minimize-upload-bar.tsx
+++ source/features/minimize-upload-bar.tsx
@@ -1,13 +1,10 @@
 import './minimize-upload-bar.css';
-import select from 'select-dom';
 import * as pageDetect from 'github-url-detection';
 
 import features from '.';
 
 function init(): void {
-	for (const commentForm of select.all('.js-previewable-comment-form')) {
-		commentForm.classList.add('rgh-minimize-upload-bar');
-	}
+	document.body.classList.add('rgh-minimize-upload-bar');
 }
 
 void features.add(__filebasename, {
