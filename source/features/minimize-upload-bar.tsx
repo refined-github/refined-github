a5391e28000d43ee372b2894352baaf7a8803f9f
Kenneth <kennethtsang9286@gmail.com>
2020-11-14 16:10:33 +1100

    feature/show-upload-image: Fix implementation for 'minimize-upload-bar'

--- source/features/minimize-upload-bar.tsx
+++ source/features/minimize-upload-bar.tsx
@@ -1,33 +1,17 @@
 import './minimize-upload-bar.css';
-import React from 'dom-chef';
 import select from 'select-dom';
-import delegate from 'delegate-it';
-import UploadIcon from 'octicon/upload.svg';
 import * as pageDetect from 'github-url-detection';
 
 import features from '.';
 
-function addButton(): void {
+function addMinimizeUploadBarClassname(): void {
 	for (const toolbarButton of select.all('md-ref')) {
-		toolbarButton.after(
-			<button type="button" className="toolbar-item tooltipped tooltipped-n rgh-upload-btn" aria-label="Attach files">
-				<UploadIcon/>
-			</button>
-		);
 		toolbarButton.closest('form')!.classList.add('rgh-minimize-upload-bar');
 	}
 }
 
-function triggerUpload(event: delegate.Event<Event, HTMLButtonElement>): void {
-	event.delegateTarget
-		.form!
-		.querySelector<HTMLInputElement>('[type="file"]')!
-		.click(); // Open UI
-}
-
 function init(): void {
-	addButton();
-	delegate(document, '.rgh-upload-btn', 'click', triggerUpload);
+	addMinimizeUploadBarClassname();
 }
 
 void features.add(__filebasename, {
