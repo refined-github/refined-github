6492b9902ea4a5f520386becb9841b3065f34b5f
Yakov <16872793+yakov116@users.noreply.github.com>
2020-11-15 08:45:35 -0500

    Only replace query if it exists

--- source/github-helpers/api.ts
+++ source/github-helpers/api.ts
@@ -161,7 +161,9 @@ export const v4 = mem(async (
 		throw new TypeError('`query` should only be what’s inside \'query {...}\', like \'user(login: "foo") { name }\', but is \n' + query);
 	}
 
-	query = query.replace('repository() {', `repository(owner: "${getRepo()!.owner}", name: "${getRepo()!.name}") {`);
+	if (query.includes('repository() {')) {
+		query = query.replace('repository() {', `repository(owner: "${getRepo()!.owner}", name: "${getRepo()!.name}") {`);
+	}
 
 	const response = await fetch(api4, {
 		headers: {
