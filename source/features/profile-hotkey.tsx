34afca4609f7e40acc4a004aea81e64228225076
Yakov <16872793+yakov116@users.noreply.github.com>
2020-11-15 05:34:31 -0500

    feedback and make the feature work

--- source/features/profile-hotkey.tsx
+++ source/features/profile-hotkey.tsx
@@ -1,18 +1,19 @@
 import React from 'dom-chef';
-import select from 'select-dom';
 import onetime from 'onetime';
+import {isEnterprise} from 'github-url-detection';
 
 import features from '.';
+import {getUsername} from '../github-helpers';
 
 function init(): void {
-	// This exists on all pages even gists
-	const {origin} = new URL(select('.js-notification-shelf-include-fragment')!.dataset.baseSrc!);
-	document.body.append(<a className="d-none" data-hotkey="g m" href={origin + '/profile'}/>);
+	const profileLink = (isEnterprise() ? location.origin : 'https://github.com') + '/' + getUsername();
+	document.body.append(<a hidden data-hotkey="g m" href={profileLink}/>);
 }
 
 void features.add(__filebasename, {
 	shortcuts: {
 		'g m': 'Go to Profile'
 	},
+	awaitDomReady: false,
 	init: onetime(init)
 });
