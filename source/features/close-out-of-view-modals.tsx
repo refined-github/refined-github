70285cfd31a44fe261ef2151cd68c44a41f1bdfe
Federico Brigante <me@fregante.com>
2020-11-20 14:10:58 -0600

    Consider ALL IntersectionObserver entries

--- source/features/close-out-of-view-modals.tsx
+++ source/features/close-out-of-view-modals.tsx
@@ -1,58 +1,58 @@
-import debounceFn from 'debounce-fn';
+import select from 'select-dom';
+import onetime from 'onetime';
+import delegate from 'delegate-it';
+
 import features from '.';
 
-const observer = new IntersectionObserver(([{intersectionRatio, target}]) => {
-	if (intersectionRatio === 0) {
-		observer.unobserve(target);
-		shouldClose(target);
+const visible = new Set();
+const observer = new IntersectionObserver(entries => {
+	let lastModal: Element;
+	for (const {intersectionRatio, target: modal} of entries) {
+		if (intersectionRatio > 0) {
+			visible.add(modal);
+		} else {
+			visible.delete(modal);
+		}
+
+		lastModal = modal;
+	}
+
+	if (visible.size === 0) {
+		observer.disconnect();
+		lastModal!.closest('details')!.open = false;
 	}
 });
 
-const shouldClose = debounceFn((target: Element) => {
-	const details = target.closest('details')!
-	const visibleChildren = getVisibleChildren(details);
-	if (visibleChildren.length === 0) {
-		details.open = false;
-	} else {
-		observeAll(visibleChildren);
-	}
-}, {wait: 100});
-
-function observeAll(targets: Element[]): void {
-	for (const child of targets) {
-		observer.observe(child);
-	}
-}
-
-function getVisibleChildren(details: HTMLDetailsElement): Element[] {
-	const visible = [];
-	for (const possibleModal of details.children) {
-		if (possibleModal.tagName !== 'SUMMARY') {
-			const rect = possibleModal.getBoundingClientRect();
-			if (rect.width + rect.height > 0 && rect.bottom > 0 && rect.top < window.innerHeight) {
-				visible.push(possibleModal);
-			}
-		}
-	}
-
-	return visible;
-}
-
+let lastOpen: number;
+let delegation: delegate.Subscription;
 function menuActivatedHandler(event: CustomEvent): void {
 	const details = event.target as HTMLDetailsElement;
-	const visibleChildren = getVisibleChildren(details);
-	if (visibleChildren.length === 0) {
-		features.error(__filebasename, 'Modal element was not correctly detected for', details);
-	} else {
-		observeAll(visibleChildren);
+
+	// Safety check #3742
+	if (!details.open && lastOpen > Date.now() - 1000) {
+		delegation!.destroy();
+		features.error(__filebasename, 'Modal was closed too quickly. Disabling feature');
+		return;
+	}
+
+	lastOpen = Date.now();
+
+	const modals = select.all([
+		':scope > details-menu', // "Watch repo" dropdown
+		':scope > details-dialog', // "Watch repo" dropdown
+		':scope > div > .dropdown-menu' // "Clone or download" and "Repo nav overflow"
+	], details);
+
+	for (const modal of modals) {
+		observer.observe(modal);
 	}
 }
 
 function init(): void {
-	document.addEventListener('menu:activated', menuActivatedHandler);
+	delegation = delegate(document, '.details-overlay', 'toggle', menuActivatedHandler, true);
 }
 
 void features.add(__filebasename, {
 	awaitDomReady: false,
-	init
+	init: onetime(init)
 });
