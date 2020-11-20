cc7e991598eaf612e6b3d87263cedb1ec8a39aaa
cheap-glitch <cheap.glitch@gmail.com>
2020-11-20 22:20:16 +0100

    Fix `set-default-repositories-type-to-sources`

--- source/features/set-default-repositories-type-to-sources.tsx
+++ source/features/set-default-repositories-type-to-sources.tsx
@@ -1,25 +1,35 @@
 import select from 'select-dom';
+import {observe} from 'selector-observer';
 
 import features from '.';
+
+function addSourceTypeToLink(link: HTMLAnchorElement): void {
+	const search = new URLSearchParams(link.search);
+	search.set('type', 'source');
+	link.search = String(search);
+}
 
 function init(): void {
 	const links = select.all<HTMLAnchorElement>([
 		// Pre "Repository refresh" layout
-		'.user-profile-nav [href$="tab=repositories"]', // "Your repositories" in header dropdown
 		'#user-links [href$="tab=repositories"]', // "Repositories" tab on user profile
 		'.orgnav > .pagehead-tabs-item:first-child', // "Repositories" tab on organization profile
 		// "Repository refresh" layout
-		'.header-nav-current-user ~ [href$="tab=repositories"]', // "Your repositories" in header dropdown
 		'[aria-label="User profile"] [href$="tab=repositories"]', // "Repositories" tab on user profile
 		'[aria-label="Organization"] .UnderlineNav-item:first-child', // "Repositories" tab on organization profile
 		'[data-hovercard-type="organization"]' // Organization name on repo header + organization list on user profile
 	]);
 
 	for (const link of links) {
-		const search = new URLSearchParams(link.search);
-		search.set('type', 'source');
-		link.search = String(search);
+		addSourceTypeToLink(link);
 	}
+
+	observe('.header-nav-current-user ~ [href$="tab=repositories"]', { // "Your repositories" in header dropdown
+		constructor: HTMLAnchorElement,
+		add(link) {
+			addSourceTypeToLink(link);
+		}
+	});
 }
 
 void features.add(__filebasename, {
