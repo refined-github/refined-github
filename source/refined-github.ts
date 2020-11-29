dd783c921528d9db05935a779af1762f50bfe165
yakov116 <16872793+yakov116@users.noreply.github.com>
2020-11-29 18:48:13 -0500

    Drop `cycle-lists-with-keyboard-shortcuts` feature (#3777)

--- source/refined-github.ts
+++ source/refined-github.ts
@@ -128,7 +128,6 @@ import './features/suggest-commit-title-limit';
 import './features/highest-rated-comment';
 import './features/clean-conversation-filters';
 import './features/minimize-upload-bar';
-import './features/cycle-lists-with-keyboard-shortcuts';
 import './features/tags-on-commits-list';
 import './features/forked-to';
 import './features/submit-review-as-single-comment';
