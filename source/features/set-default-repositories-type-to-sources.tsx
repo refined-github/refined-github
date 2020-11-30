0b70251be8b77742f8d8709be9f9ce22dd2afb87
cheap-glitch <cheap.glitch@gmail.com>
2020-11-30 14:48:26 +0100

    Fix lint errors

--- source/features/set-default-repositories-type-to-sources.tsx
+++ source/features/set-default-repositories-type-to-sources.tsx
@@ -1,7 +1,7 @@
 import select from 'select-dom';
+import oneEvent from 'one-event';
 
 import features from '.';
-import oneEvent from 'one-event';
 
 function addSourceTypeToLink(link: HTMLAnchorElement): void {
 	const search = new URLSearchParams(link.search);
