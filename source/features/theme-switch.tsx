c3aa967699df03d5fc3bf8cfd48058f66c22305a
Kid <kidonng@gmail.com>
2020-12-09 19:54:09 +0800

    Prevent breakage

--- source/features/theme-switch.tsx
+++ source/features/theme-switch.tsx
@@ -9,19 +9,20 @@ import features from '.';
 import fetchDom from '../helpers/fetch-dom';
 
 async function init(): Promise<void> {
-	const dom = (await fetchDom('https://github.com/settings/appearance', '.js-color-mode-settings'))!;
-	dom.classList.add('rgh-theme-switch', 'mt-1', 'ml-1');
-	select('.flex-column', dom)!.classList.remove('flex-lg-row');
-	for (const radio of select.all('.position-relative', dom)) {
+	const form = (await fetchDom('https://github.com/settings/appearance', '.js-color-mode-settings'))!;
+	form.classList.add('rgh-theme-switch-form', 'mt-1', 'ml-1');
+	select('.flex-column', form)!.classList.remove('flex-lg-row');
+	for (const radio of select.all('.position-relative', form)) {
 		radio.classList.remove('mb-4');
 	}
 
-	observe('.dropdown-item[href="https://gist.github.com/mine"]', {
+	observe('.dropdown-item[href="https://gist.github.com/mine"]:not(.rgh-theme-switch)', {
 		add(item) {
+			item.classList.add('rgh-theme-switch');
 			item.after(
 				<>
 					<span role="menuitem" className="dropdown-item rgh-theme-switch-title">Theme preference</span>
-					{dom}
+					{form}
 				</>
 			);
 		}
