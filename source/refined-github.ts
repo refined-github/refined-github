1acbc03369165d0f91ddab70d69ea646578597eb
cheap-glitch <cheap.glitch@gmail.com>
2020-12-11 10:01:20 +0100

    Rename feature to `easy-toggle-files` and update readme description

--- source/refined-github.ts
+++ source/refined-github.ts
@@ -202,7 +202,7 @@ import './features/single-diff-column-selection';
 import './features/jump-to-change-requested-comment';
 import './features/esc-to-cancel';
 import './features/quick-fork-deletion';
-import './features/pr-easy-toggle-files';
+import './features/easy-toggle-files';
 
 // Add global for easier debugging
 (window as any).select = select;
