08c5741c6601d93c51385b15b528373c5e98f166
fbfdestro <fbfdestro@gmail.com>
2020-12-13 00:46:14 -0300

    added class rgh-clean-pinned-issue directly to body

--- source/features/clean-pinned-issues.tsx
+++ source/features/clean-pinned-issues.tsx
@@ -1,14 +1,10 @@
 import './clean-pinned-issues.css';
-import select from 'select-dom';
 import * as pageDetect from 'github-url-detection';
 
 import features from '.';
 
 function init(): void {
-	const pinned_issues_container = select('.js-pinned-issues-reorder-container');
-	if (pinned_issues_container) {
-		pinned_issues_container.classList.add('clean-pinned-issue');
-	}
+	document.body.classList.add('rgh-clean-pinned-issue');
 }
 
 void features.add(__filebasename, {
