85ca667695bdea4fff882bf12d7c3ee176f4c485
cheap-glitch <cheap.glitch@gmail.com>
2020-12-22 11:41:09 +0100

    Hide empty meta at the end

--- source/features/clean-repo-sidebar.tsx
+++ source/features/clean-repo-sidebar.tsx
@@ -9,12 +9,6 @@ import features from '.';
 async function init(): Promise<void> {
 	document.body.classList.add('rgh-clean-repo-sidebar');
 
-	// Hide empty meta if it’s not editable by the current user
-	// TODO: once it's fixed, use https://github.com/fregante/github-url-detection/blob/4840d85d31d59bfe71e884ef42c482fbfff2d955/index.ts#L490
-	if (!select.exists('.repository-content [aria-label="Edit repository metadata"]')) {
-		select('.repository-content .BorderGrid-cell:first-child > .text-italic')?.remove();
-	}
-
 	// Hide redundant "Releases" section
 	const sidebarReleases = await elementReady('.BorderGrid-cell a[href$="/releases"]');
 	sidebarReleases!.closest<HTMLElement>('.BorderGrid-row')!.hidden = true;
@@ -23,6 +17,12 @@ async function init(): Promise<void> {
 	const packagesCounter = await elementReady('.BorderGrid-cell a[href*="/packages?"] .Counter')!;
 	if (packagesCounter && packagesCounter.textContent === '0') {
 		packagesCounter.closest<HTMLElement>('.BorderGrid-row')!.hidden = true;
+	}
+
+	// Hide empty meta if it’s not editable by the current user
+	// TODO: once it's fixed, use https://github.com/fregante/github-url-detection/blob/4840d85d31d59bfe71e884ef42c482fbfff2d955/index.ts#L490
+	if (!select.exists('.repository-content [aria-label="Edit repository metadata"]')) {
+		select('.repository-content .BorderGrid-cell:first-child > .text-italic')?.remove();
 	}
 }
 
