71e3c4ab0f81650e1cbfb8c86007bbe8b3bda531
Federico <me@fregante.com>
2020-12-27 01:02:23 -0600

    Restore selector, the bug was fixed

--- source/github-helpers/index.ts
+++ source/github-helpers/index.ts
@@ -23,7 +23,7 @@ Example tag content on private repositories https://github.com/private/private/c
 */
 export const getCurrentBranch = (): string | undefined => {
 	// .last needed for #2799
-	const feedLink = select.last('link[type^="application/atom"]');
+	const feedLink = select.last('link[type="application/atom+xml"]');
 	// The feedLink is not available on `isIssue` #3641
 	if (!feedLink) {
 		return;
