2d629cd55ba800bc1048cab0b69eeda97bd2ca78
Philipp Beck <dertieran@gmail.com>
2021-01-11 15:44:14 +0100

    Use query object in search query tests

--- test/search-query.ts
+++ test/search-query.ts
@@ -4,34 +4,34 @@ import './fixtures/globals';
 import SearchQuery from '../source/github-helpers/search-query';
 
 test('.get', t => {
-	const query = new SearchQuery('?q=wow');
+	const query = new SearchQuery({q: 'wow'});
 	t.is(query.get(), 'wow');
 });
 
 test('.getQueryParts', t => {
-	const query = new SearchQuery('?q=cool+is%3Aissue');
+	const query = new SearchQuery({q: 'cool is:issue'});
 	t.deepEqual(query.getQueryParts(), ['cool', 'is:issue']);
 });
 
 test('getQueryParts with spaces support', t => {
-	const query = new SearchQuery('?q=please+label%3A"under+discussion"');
+	const query = new SearchQuery({q: 'please label:"under discussion"'});
 	t.deepEqual(query.getQueryParts(), ['please', 'label:"under discussion"']);
 });
 
 test('.set', t => {
-	const query = new SearchQuery('?q=wow');
+	const query = new SearchQuery({q: 'wow'});
 	query.set('lol');
 	t.is(query.get(), 'lol');
 });
 
 test('.edit', t => {
-	const query = new SearchQuery('?q=gone+fishing');
+	const query = new SearchQuery({q: 'gone fishing'});
 	query.edit(query => query.split(' ')[0]);
 	t.is(query.get(), 'gone');
 });
 
 test('.replace', t => {
-	const query = new SearchQuery('?q=404+error');
+	const query = new SearchQuery({q: '404 error'});
 	query.replace('error', 'failure');
 	t.is(query.get(), '404 failure');
 
@@ -40,25 +40,25 @@ test('.replace', t => {
 });
 
 test('.remove', t => {
-	const query = new SearchQuery('?q=is%3Aissue+dog+is%3Aopen');
+	const query = new SearchQuery({q: 'is:issue dog is:open'});
 	query.remove('is:issue', 'is:open');
 	t.is(query.get(), 'dog');
 });
 
 test('.add', t => {
-	const query = new SearchQuery('?q=is%3Apr+birds+everywhere');
+	const query = new SearchQuery({q: 'is:pr birds everywhere'});
 	query.add('and', 'aliens');
 	t.is(query.get(), 'is:pr birds everywhere and aliens');
 });
 
 test('.includes', t => {
-	const query = new SearchQuery('?q=label:nonsense');
+	const query = new SearchQuery({q: 'label:nonsense'});
 	t.false(query.includes('nonsense'));
 	t.true(query.includes('label:nonsense'));
 });
 
 test('defaults', t => {
-	const query = new SearchQuery('?q=');
+	const query = new SearchQuery({q: ''});
 	t.is(query.get(), '');
 
 	const link = document.createElement('a');
@@ -68,7 +68,7 @@ test('defaults', t => {
 });
 
 test('deduplicate is:pr/issue', t => {
-	const query = new SearchQuery('?q=refined+github+is%3Apr');
+	const query = new SearchQuery({q: 'refined github is:pr'});
 	query.add('is:issue');
 
 	t.false(query.includes('is:pr'));
@@ -76,6 +76,6 @@ test('deduplicate is:pr/issue', t => {
 });
 
 test('remove additional spaces', t => {
-	const query = new SearchQuery('?q=+refined+++github+');
+	const query = new SearchQuery({q: ' refined   github '});
 	t.is(query.get(), 'refined github');
 });
