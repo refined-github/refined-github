2792d0fcf3f92794b21853b31de01d66b5b66937
Federico Brigante <me@fregante.com>
2021-01-14 15:47:47 -0600

    Drop intermediary handlers

--- source/features/wait-for-attachments.ts
+++ source/features/wait-for-attachments.ts
@@ -3,29 +3,21 @@ import * as pageDetect from 'github-url-detection';
 
 import features from '.';
 
-function toggleSubmitButtons(target: HTMLElement, disabled: boolean): void {
+function toggleSubmitButtons({target, type}: Event): void {
 	// `data-required-trimmed` overrides the `disabled` state of the form
-	const textarea = select('textarea', target)!;
-	if (disabled) {
+	const textarea = select('textarea', target as HTMLFormElement)!;
+	if (type === 'upload:setup') {
 		delete textarea.dataset.requiredTrimmed;
 	} else {
 		textarea.dataset.requiredTrimmed = 'Text field is empty';
 	}
 }
 
-function handleUploadStart(event: Event): void {
-	toggleSubmitButtons(event.target as HTMLElement, true);
-}
-
-function handleUploadStop(event: Event): void {
-	toggleSubmitButtons(event.target as HTMLElement, false);
-}
-
 function init(): void {
-	document.addEventListener('upload:setup', handleUploadStart, true);
-	document.addEventListener('upload:complete', handleUploadStop);
-	document.addEventListener('upload:error', handleUploadStop);
-	document.addEventListener('upload:invalid', handleUploadStop);
+	document.addEventListener('upload:setup', toggleSubmitButtons, true);
+	document.addEventListener('upload:complete', toggleSubmitButtons);
+	document.addEventListener('upload:error', toggleSubmitButtons);
+	document.addEventListener('upload:invalid', toggleSubmitButtons);
 }
 
 void features.add(__filebasename, {
