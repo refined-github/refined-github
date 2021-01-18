933d63a82070679fbea962635b65ad04935b7c03
shoter <lubiebardzozelki@gmail.com>
2021-01-18 02:03:03 +0100

    fix: fix lint

--- source/features/conversation-timeline-filter.tsx
+++ source/features/conversation-timeline-filter.tsx
@@ -4,23 +4,22 @@ import elementReady from 'element-ready';
 import {observe} from 'selector-observer';
 import * as pageDetect from 'github-url-detection';
 
-import features from '.';
 import delay from 'delay';
 
-enum FilterSettings
-{
+import features from '.';
+
+enum FilterSettings {
 	ShowAll = 1,
 	ShowOnlyComments = 2,
 	ShowOnlyUnresolvedComments = 3,
 	ShowOnlyUnresolvedReviews = 4
 }
 
-let CurrentSettings : FilterSettings = FilterSettings.ShowAll;
-let autoLoadEnabled : boolean = false;
+let currentSettings: FilterSettings = FilterSettings.ShowAll;
+let autoLoadEnabled = false;
 
-const showFilterId = "rgh-show-filter";
+const showFilterId = 'rgh-show-filter';
 const autoLoadHiddenSelectorId = 'auto-load-hidden';
-
 
 // Every element on the timeline that is recognizable by this feature will be marked with tis class.
 const timelineFiltersSelectorId = 'timeline-filters';
@@ -32,19 +31,22 @@ const loadMoreSelector = '.ajax-pagination-btn:not([disabled])';
 function regenerateFilterSummary(): void {
 	const timelineFilter = select(`#${timelineFiltersSelectorId}`)!;
 
-	let text = "";
-	switch(CurrentSettings) {
+	let text = '';
+	switch (currentSettings) {
 		case FilterSettings.ShowAll:
-			text = "Show All";
+			text = 'Show All';
 			break;
 		case FilterSettings.ShowOnlyComments:
-			text = "Show only comments";
+			text = 'Show only comments';
 			break;
 		case FilterSettings.ShowOnlyUnresolvedReviews:
-			text = "Show only unresolved reviews";
+			text = 'Show only unresolved reviews';
 			break;
 		case FilterSettings.ShowOnlyUnresolvedComments:
-			text = "Show unresolved comments";
+			text = 'Show unresolved comments';
+			break;
+		default:
+			text = '';
 			break;
 	}
 
@@ -59,8 +61,7 @@ function regenerateFilterSummary(): void {
 
 async function saveSettings(): Promise<any> {
 	autoLoadEnabled = (select<HTMLInputElement>(`#${autoLoadHiddenSelectorId}`))!.checked;
-	CurrentSettings = parseInt((select<HTMLInputElement>(`#${showFilterId}:checked`))!.value);
-
+	currentSettings = Number.parseInt((select<HTMLInputElement>(`#${showFilterId}:checked`))!.value, 10);
 
 	// Close window
 	select(detailsSelector)!.removeAttribute('open');
@@ -76,17 +77,19 @@ async function saveSettings(): Promise<any> {
 	}
 }
 
-
 function reapplySettings(): void {
 	select
 		.all(timelineItemSelector)
-		.forEach(element => processTimelineItem(element));
+		.forEach(element => {
+			processTimelineItem(element);
+		});
 }
 
-
 function restoreSettings(): void {
-	select.all<HTMLInputElement>(`#${showFilterId}`).forEach(el => el.checked = false);
-	select<HTMLInputElement>(`#${showFilterId}[value="${CurrentSettings.valueOf()}"]`)!.checked = true;
+	select.all<HTMLInputElement>(`#${showFilterId}`).forEach(element => {
+		element.checked = false;
+	});
+	select<HTMLInputElement>(`#${showFilterId}[value="${currentSettings.valueOf()}"]`)!.checked = true;
 	select<HTMLInputElement>(`#${autoLoadHiddenSelectorId}`)!.checked = autoLoadEnabled;
 }
 
@@ -109,7 +112,7 @@ function createRadio(form: JSX.Element, id: string, title: string, summary: stri
 	const element = (
 		<label className={'d-block p-3 ' + (hasTopBorder ? 'border-top' : '')}>
 			<div className="form-checkbox my-0">
-				<input id={id} type="radio" name="id" value={value} checked={value===CurrentSettings}/> {title}
+				<input id={id} type="radio" name="id" value={value} checked={value === currentSettings}/> {title}
 				<p className="note">
 					{summary}
 				</p>
@@ -133,7 +136,7 @@ async function addTimelineItemsFilter(): Promise<void> {
 	select('form.thread-subscribe-form', timelineFilter)!.remove();
 	const summary = select('summary', timelineFilter)!;
 	summary.setAttribute('aria-label', 'Customize timeline filters');
-	summary.addEventListener("click", restoreSettings);
+	summary.addEventListener('click', restoreSettings);
 	select('div.text-bold', summary)!.textContent = 'Filters';
 	createDetailsDialog(timelineFilter);
 	notifications.after(timelineFilter);
@@ -153,7 +156,7 @@ function createDetailsDialog(timelineFilter: Element): void {
 	createRadio(form, showFilterId, 'Show all', '', FilterSettings.ShowAll, true);
 	createRadio(form, showFilterId, 'Show only comments', 'Hides commits and events', FilterSettings.ShowOnlyComments, true);
 	createRadio(form, showFilterId, 'Show only unresolved comments', 'Also hides resolved reviews and hidden comments', FilterSettings.ShowOnlyUnresolvedComments, true);
-	if(pageDetect.isPRConversation()) {
+	if (pageDetect.isPRConversation()) {
 		createRadio(form, showFilterId, 'Show only unresolved reviews', 'Also hides regular comments (PR only)', FilterSettings.ShowOnlyUnresolvedReviews, true);
 	}
 
@@ -184,7 +187,6 @@ async function tryClickLoadMore(item: HTMLElement): Promise<any> {
 		item.click();
 	}
 }
-
 
 function processTimelineItem(item: HTMLElement): void {
 	const pr = select('.js-comment[id^=pullrequestreview]', item);
@@ -220,17 +222,15 @@ function processPR(item: HTMLElement): void {
 		hasVisibleElement = hasVisibleElement || threadContainer.style.display === '';
 	}
 
-	if(hasVisibleElement)
-	{
+	if (hasVisibleElement) {
 		item.style.display = '';
 	} else {
 		item.style.display = 'none';
-
 	}
 }
 
-function applyDisplay(element: HTMLElement, ...displaySettings : FilterSettings[]): void {
-	if (displaySettings.includes(CurrentSettings) || CurrentSettings == FilterSettings.ShowAll) {
+function applyDisplay(element: HTMLElement, ...displaySettings: FilterSettings[]): void {
+	if (displaySettings.includes(currentSettings) || currentSettings === FilterSettings.ShowAll) {
 		element.style.display = '';
 	} else {
 		element.style.display = 'none';
