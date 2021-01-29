0c788e20b51b347eb62652c1d5beb8ab8dc03dea
cheap glitch <cheap.glitch@gmail.com>
2021-01-29 15:20:19 +0100

    Apply suggestion
    Co-authored-by: Federico <me@fregante.com>

--- source/github-events/on-profile-dropdown-load.ts
+++ source/github-events/on-profile-dropdown-load.ts
@@ -2,7 +2,9 @@ import oneMutation from 'one-mutation';
 import elementReady from 'element-ready';
 
 export default async function onProfileDropdownLoad(): Promise<Element> {
-	const dropdown = await elementReady('.Header-item:last-child .dropdown-menu', {waitForChildren: false});
-	await oneMutation(dropdown!, {childList: true});
+	const dropdown = await elementReady('.Header-item:last-child .dropdown-menu');
+	if (!exists()) {
+		await oneMutation(dropdown!, {childList: true, filter: exists});		
+	}
 	return dropdown!;
 }
