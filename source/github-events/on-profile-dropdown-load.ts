99d3104ec82c5ebeb3517ae0880a74d7beccdf43
cheap-glitch <cheap.glitch@gmail.com>
2021-01-29 15:37:35 +0100

    Add helper function

--- source/github-events/on-profile-dropdown-load.ts
+++ source/github-events/on-profile-dropdown-load.ts
@@ -1,10 +1,15 @@
+import select from 'select-dom';
 import oneMutation from 'one-mutation';
 import elementReady from 'element-ready';
 
 export default async function onProfileDropdownLoad(): Promise<Element> {
 	const dropdown = await elementReady('.Header-item:last-child .dropdown-menu');
-	if (!exists()) {
-		await oneMutation(dropdown!, {childList: true, filter: exists});		
+	if (!dropdownContentExists()) {
+		await oneMutation(dropdown!, {childList: true, filter: dropdownContentExists});
 	}
 	return dropdown!;
+}
+
+function dropdownContentExists() {
+	return select.exists('.Header-item:last-child .dropdown-menu .dropdown-item')
 }
