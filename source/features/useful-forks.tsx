bac4262cf404808d769dc4a9294c2c71c15463a1
payne911 <payne911@users.noreply.github.com>
2021-02-08 14:01:43 -0500

    Lint fixes
    Signed-off-by: payne911 <payne911@users.noreply.github.com>

--- source/features/useful-forks.tsx
+++ source/features/useful-forks.tsx
@@ -1,6 +1,6 @@
 import React from 'dom-chef';
 import select from 'select-dom';
-import { LinkExternalIcon } from '@primer/octicons-react';
+import {LinkExternalIcon} from '@primer/octicons-react';
 import * as pageDetect from 'github-url-detection';
 
 import features from '.';
@@ -9,14 +9,15 @@ function init(): void {
 	const downloadUrl = new URL('https://useful-forks.github.io/?repo=' + pageDetect.utils.getRepoURL());
 
 	select('#network')!.prepend(
-			<a
-					className="btn mt-2 mb-2"
-					href={downloadUrl.href}
-					target="_blank"
-			>
-				<LinkExternalIcon className="mr-2"/>
-				Find useful forks
-			</a>
+		<a
+			className="btn mt-2 mb-2"
+			href={downloadUrl.href}
+			target="_blank"
+			rel="noreferrer"
+		>
+			<LinkExternalIcon className="mr-2"/>
+			Find useful forks
+		</a>
 	);
 }
 
