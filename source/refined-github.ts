05631469583bd33f8e88cdb3f4c57704ae6874d2
payne911 <payne911@users.noreply.github.com>
2021-02-08 12:26:50 -0500

    Integrating `useful-forks` (fixes https://github.com/sindresorhus/refined-github/issues/3682)

--- source/refined-github.ts
+++ source/refined-github.ts
@@ -207,6 +207,7 @@ import './features/easy-toggle-files';
 import './features/quick-repo-deletion';
 import './features/clean-repo-sidebar';
 import './features/rgh-feature-descriptions';
+import './features/useful-forks';
 
 // Add global for easier debugging
 (window as any).select = select;
