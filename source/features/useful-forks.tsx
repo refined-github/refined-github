bb7d38282eae9532e1c7e3d06ef1d229efb73477
payne911 <payne911@users.noreply.github.com>
2021-02-08 13:23:15 -0500

    Add `useful-forks` feature (fixes https://github.com/sindresorhus/refined-github/issues/3682)
    Signed-off-by: payne911 <payne911@users.noreply.github.com>

---
+++ source/features/useful-forks.tsx
@@ -0,0 +1,31 @@
+import React from 'dom-chef';
+import select from 'select-dom';
+import { LinkExternalIcon } from '@primer/octicons-react';
+import * as pageDetect from 'github-url-detection';
+
+import features from '.';
+
+function init(): void {
+	const downloadUrl = new URL('https://useful-forks.github.io/?repo=' + pageDetect.utils.getRepoURL());
+
+	select('#network')!.prepend(
+			<a
+					className="btn mt-2 mb-2"
+					href={downloadUrl.href}
+					target="_blank"
+			>
+				<LinkExternalIcon className="mr-2"/>
+				Find useful forks
+			</a>
+	);
+}
+
+const isForksPage = (url: URL | HTMLAnchorElement | Location = location): boolean => pageDetect.utils.getRepoPath(url) === 'network/members';
+const isNetworkPage = (url: URL | HTMLAnchorElement | Location = location): boolean => pageDetect.utils.getRepoPath(url) === 'network';
+void features.add(__filebasename, {
+	include: [
+		isForksPage,
+		isNetworkPage
+	],
+	init
+});
