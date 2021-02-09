70154599c1997c7346e8159648eb62a0242f2c50
payne911 <payne911@users.noreply.github.com>
2021-02-09 00:39:52 -0500

    Not displaying when there are no forks
    Signed-off-by: payne911 <payne911@users.noreply.github.com>

--- source/features/useful-forks.tsx
+++ source/features/useful-forks.tsx
@@ -5,8 +5,14 @@ import {RepoForkedIcon} from '@primer/octicons-react';
 
 import features from '.';
 import {getRepo} from '../github-helpers';
+import looseParseInt from "../helpers/loose-parse-int";
 
 function init(): void {
+
+	if (hasNoForks()) {
+		return;
+	}
+
 	const downloadUrl = new URL('https://useful-forks.github.io');
 	downloadUrl.searchParams.set('repo', getRepo()!.nameWithOwner);
 
@@ -16,6 +22,11 @@ function init(): void {
 			Find useful forks
 		</a>
 	);
+}
+
+function hasNoForks(): boolean {
+	const forksAmount = looseParseInt(select(".social-count[href$='/network/members']")!);
+	return forksAmount === 0;
 }
 
 const isForksPage = (url: URL | HTMLAnchorElement | Location = location): boolean => pageDetect.utils.getRepoPath(url) === 'network/members';
