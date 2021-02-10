3b529a127de7c7b3434042239a6306abfe332f33
Yakov <16872793+yakov116@users.noreply.github.com>
2021-02-10 00:20:52 -0500

    Return early on no forks

--- source/features/useful-forks.tsx
+++ source/features/useful-forks.tsx
@@ -7,19 +7,22 @@ import features from '.';
 import {getRepo} from '../github-helpers';
 import looseParseInt from '../helpers/loose-parse-int';
 
-async function init(): Promise<void> {
-	if (looseParseInt((await elementReady('.social-count[href$="/network/members"]', {waitForChildren: false}))!) > 0) {
-		const downloadUrl = new URL('https://useful-forks.github.io');
-		downloadUrl.searchParams.set('repo', getRepo()!.nameWithOwner);
-
-		const selector = isForksPage() ? '#network' : '#repo-content-pjax-container h2';
-		(await elementReady(selector, {waitForChildren: false}))!.prepend(
-			<a className="btn mb-2 float-right" href={downloadUrl.href}>
-				<RepoForkedIcon className="mr-2"/>
-				Find useful forks
-			</a>
-		);
+async function init(): Promise<void | false> {
+	const forkCount = looseParseInt((await elementReady('.social-count[href$="/network/members"]', {waitForChildren: false}))!);
+	if (forkCount === 0) {
+		return false;
 	}
+
+	const downloadUrl = new URL('https://useful-forks.github.io');
+	downloadUrl.searchParams.set('repo', getRepo()!.nameWithOwner);
+
+	const selector = isForksPage() ? '#network' : '#repo-content-pjax-container h2';
+	(await elementReady(selector, {waitForChildren: false}))!.prepend(
+		<a className="btn mb-2 float-right" href={downloadUrl.href}>
+			<RepoForkedIcon className="mr-2"/>
+			Find useful forks
+		</a>
+	);
 }
 
 const isForksPage = (url: URL | HTMLAnchorElement | Location = location): boolean => pageDetect.utils.getRepoPath(url) === 'network/members';
