dabdfcb5d60973874d75f5d2aaa8197f3ed04ec1
yakov116 <16872793+yakov116@users.noreply.github.com>
2021-02-14 17:23:44 -0500

    Restore embed-gist-inline.tsx

--- source/features/embed-gist-inline.tsx
+++ source/features/embed-gist-inline.tsx
@@ -5,14 +5,14 @@ import * as pageDetect from 'github-url-detection';
 
 import features from '.';
 
-const isGist = ({pathname, hostname}: HTMLAnchorElement): boolean =>
-	!pathname.includes('.') && // Exclude links to embed files
+const isGist = (link: HTMLAnchorElement): boolean =>
+	!link.pathname.includes('.') && // Exclude links to embed files
 	(
-		(hostname.startsWith('gist.') && pathname.includes('/', 1)) || // Exclude user links
-		pathname.startsWith('gist/')
+		(link.hostname.startsWith('gist.') && link.pathname.includes('/', 1)) || // Exclude user links
+		link.pathname.startsWith('gist/')
 	);
 
-const isOnlyChild = ({textContent, parentNode}: HTMLAnchorElement): boolean => textContent!.trim() === parentNode!.textContent!.trim();
+const isOnlyChild = (link: HTMLAnchorElement): boolean => link.textContent!.trim() === link.parentNode!.textContent!.trim();
 
 async function embedGist(link: HTMLAnchorElement): Promise<void> {
 	const info = <em> (loading)</em>;
