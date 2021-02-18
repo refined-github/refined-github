a1a18843de1b3e5f8ed1597c1c897ae76312ed6f
cheap-glitch <cheap.glitch@gmail.com>
2021-02-18 16:24:10 +0100

    Remove unnecessary comment

--- source/helpers/on-element-removal.ts
+++ source/helpers/on-element-removal.ts
@@ -3,7 +3,6 @@ import mem from 'mem';
 const onElementRemoval = mem(
 	async (element: Element): Promise<void> => (
 		new Promise(resolve => {
-			// @ts-expect-error until https://github.com/microsoft/TypeScript/issues/37861
 			new ResizeObserver(([{target}], observer) => {
 				if (!target.isConnected) {
 					observer.disconnect();
