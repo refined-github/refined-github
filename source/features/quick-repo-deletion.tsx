1d95444e24312cb540cff2e295b3e5c6a629f5db
Federico Brigante <me@fregante.com>
2021-02-19 16:38:22 -0600

    Restore previous "Delete fork" button copy

--- source/features/quick-repo-deletion.tsx
+++ source/features/quick-repo-deletion.tsx
@@ -132,7 +132,7 @@ async function init(): Promise<void | false> {
 			<details className="details-reset details-overlay select-menu rgh-quick-repo-deletion">
 				<summary aria-haspopup="menu" role="button">
 					{/* This extra element is needed to keep the button above the <summary>’s lightbox */}
-					<span className="btn btn-sm btn-danger">Delete repo</span>
+					<span className="btn btn-sm btn-danger">Delete fork</span>
 				</summary>
 			</details>
 		</li>
