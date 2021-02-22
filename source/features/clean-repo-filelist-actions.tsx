b208fb2df454442a93d47ff82abfb9ccd501d65e
Federico Brigante <me@fregante.com>
2021-02-22 12:49:51 -0600

    Use single observer and add comments

--- source/features/clean-repo-filelist-actions.tsx
+++ source/features/clean-repo-filelist-actions.tsx
@@ -1,4 +1,5 @@
 import React from 'dom-chef';
+import select from 'select-dom';
 import onetime from 'onetime';
 import {observe} from 'selector-observer';
 import * as pageDetect from 'github-url-detection';
@@ -8,31 +9,38 @@ import {wrap} from '../helpers/dom-utils';
 import features from '.';
 
 function init(): void {
-	// Only select desktop version of the button
-	observe('.d-flex > [data-hotkey="t"]:not(.rgh-clean-actions)', {
+	// `.btn` selects the desktop version
+	observe('.btn[data-hotkey="t"]:not(.rgh-repo-filelist-actions)', {
 		add(searchButton) {
-			searchButton.classList.add('tooltipped', 'tooltipped-ne', 'rgh-clean-actions');
+			searchButton.classList.add('tooltipped', 'tooltipped-ne', 'rgh-repo-filelist-actions');
 			searchButton.setAttribute('aria-label', 'Go to file');
+
+			// Replace "Go to file" with  icon
 			searchButton.firstChild!.replaceWith(<SearchIcon/>);
 
-			// Exclude logged out, mobile or file pages
-			const addButtonText = searchButton.nextElementSibling?.querySelector('.d-md-flex');
-			if (addButtonText) {
-				addButtonText.classList.replace('d-md-flex', 'd-md-block');
-				addButtonText.firstChild!.replaceWith(<PlusIcon/>);
-				// Add tooltip on a wrapper to avoid breaking dropdown functionality
-				wrap(addButtonText.closest('details')!, <div className="tooltipped tooltipped-ne" aria-label="Add file"/>);
-			}
-		}
-	});
+			// This button doesn't appear on `isSingleFile`
+			const addFileDropdown = searchButton.nextElementSibling!.querySelector('.dropdown-caret');
+			if (addFileDropdown) {
+				addFileDropdown.parentElement!.classList.replace('d-md-flex', 'd-md-block');
 
-	observe('get-repo summary:not(.rgh-clean-actions)', {
-		add(button) {
-			button.classList.add('rgh-clean-actions');
-			// Remove "Code" text in dropdown button
-			button.firstElementChild!.nextSibling!.remove();
-			// Add tooltip on a wrapper to avoid breaking dropdown functionality
-			wrap(button.closest('details')!, <div className="tooltipped tooltipped-ne" aria-label="Clone, open or download"/>);
+				// Replace "Add file" with icon
+				addFileDropdown.previousSibling!.replaceWith(<PlusIcon/>);
+
+				// Add tooltip on a wrapper to avoid breaking dropdown functionality
+				wrap(addFileDropdown.closest('details')!, <div className="tooltipped tooltipped-ne" aria-label="Add file"/>);
+			}
+
+			// This dropdown doesn't appear on `isSingleFile`
+			const downloadIcon = select('get-repo .octicon-download');
+			if (downloadIcon) {
+				downloadIcon.classList.add('rgh-repo-filelist-actions');
+
+				// Remove "Code" text next to it
+				downloadIcon.nextSibling!.remove();
+
+				// Add tooltip on a wrapper to avoid breaking dropdown functionality
+				wrap(downloadIcon.closest('details')!, <div className="tooltipped tooltipped-ne" aria-label="Clone, open or download"/>);
+			}
 		}
 	});
 }
