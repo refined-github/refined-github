8c46db3bd2a8732a48ca77de3ac24df824371dd2
cheap-glitch <cheap.glitch@gmail.com>
2021-02-22 10:28:49 +0100

    Extract async operations

--- source/features/clean-repo-sidebar.tsx
+++ source/features/clean-repo-sidebar.tsx
@@ -1,57 +1,74 @@
 import './clean-repo-sidebar.css';
 import select from 'select-dom';
+import domLoaded from 'dom-loaded';
 import elementReady from 'element-ready';
 import * as pageDetect from 'github-url-detection';
 
 import features from '.';
 
-async function init(): Promise<void> {
-	document.body.classList.add('rgh-clean-repo-sidebar');
-
+async function removeReadmeLink(): Promise<void> {
 	// Hide "Readme" link made unnecessary by toggle-files-button #3580
 	(await elementReady('.muted-link[href="#readme"]'))?.parentElement!.remove();
+}
 
+async function cleanLicenseText(): Promise<void> {
 	// Remove whitespace in license link to fix alignment of icons https://github.com/sindresorhus/refined-github/pull/3974#issuecomment-780213892
 	const licenseLink = await elementReady('.repository-content .octicon-law');
 	if (licenseLink) {
 		licenseLink.nextSibling!.textContent = licenseLink.nextSibling!.textContent!.trim();
 	}
+}
 
-	// Clean up "Releases" section
+async function cleanReleases(): Promise<void> {
 	const sidebarReleases = await elementReady('.BorderGrid-cell a[href$="/releases"]', {waitForChildren: false});
-	if (sidebarReleases) {
-		const releasesSection = sidebarReleases.closest<HTMLElement>('.BorderGrid-cell')!;
-		if (select.exists('.octicon-tag', releasesSection)) {
-			// Collapse "Releases" section into previous section
-			releasesSection.classList.add('border-0', 'pt-3');
-			sidebarReleases.closest('.BorderGrid-row')!
-				.previousElementSibling! // About’s .BorderGrid-row
-				.firstElementChild! // About’s .BorderGrid-cell
-				.classList.add('border-0', 'pb-0');
-
-			// Hide header and footer
-			for (const uselessInformation of select.all(':scope > :not(a)', releasesSection)) {
-				uselessInformation.hidden = true;
-			}
-
-			// Align latest tag icon with the icons of other meta links
-			const tagIcon = select('.octicon-tag:not(.text-green)', releasesSection)!;
-			if (tagIcon) {
-				tagIcon.classList.add('mr-2');
-				// Remove whitespace node
-				tagIcon.nextSibling!.remove();
-			}
-		} else {
-			// Hide the whole section if there's no releases
-			releasesSection.hidden = true;
-		}
+	if (!sidebarReleases) {
+		return;
 	}
 
-	// Hide empty "Packages" section
+	const releasesSection = sidebarReleases.closest<HTMLElement>('.BorderGrid-cell')!;
+	if (!select.exists('.octicon-tag', releasesSection)) {
+		// Hide the whole section if there's no releases
+		releasesSection.hidden = true;
+		return;
+	}
+
+	// Collapse "Releases" section into previous section
+	releasesSection.classList.add('border-0', 'pt-3');
+	sidebarReleases.closest('.BorderGrid-row')!
+		.previousElementSibling! // About’s .BorderGrid-row
+		.firstElementChild! // About’s .BorderGrid-cell
+		.classList.add('border-0', 'pb-0');
+
+	// Hide header and footer
+	for (const uselessInformation of select.all(':scope > :not(a)', releasesSection)) {
+		uselessInformation.hidden = true;
+	}
+
+	// Align latest tag icon with the icons of other meta links
+	const tagIcon = select('.octicon-tag:not(.text-green)', releasesSection)!;
+	if (tagIcon) {
+		tagIcon.classList.add('mr-2');
+		// Remove whitespace node
+		tagIcon.nextSibling!.remove();
+	}
+}
+
+async function hideEmptyPackages(): Promise<void> {
 	const packagesCounter = await elementReady('.BorderGrid-cell a[href*="/packages?"] .Counter', {waitForChildren: false})!;
 	if (packagesCounter && packagesCounter.textContent === '0') {
 		packagesCounter.closest<HTMLElement>('.BorderGrid-row')!.hidden = true;
 	}
+}
+
+async function init(): Promise<void> {
+	document.body.classList.add('rgh-clean-repo-sidebar');
+
+	void removeReadmeLink();
+	void cleanLicenseText();
+	void cleanReleases();
+	void hideEmptyPackages();
+
+	await domLoaded;
 
 	// Hide empty meta if it’s not editable by the current user
 	if (!pageDetect.canUserEditRepo()) {
