64366c32c6fa21827c882ed0712b3927436931be
Federico <me@fregante.com>
2021-02-22 21:53:12 -0600

    Meta: Linkify RG’s own features on this repo (#4009)

---
+++ source/features/rgh-linkify-features.tsx
@@ -0,0 +1,31 @@
+import React from 'dom-chef';
+import select from 'select-dom';
+import * as pageDetect from 'github-url-detection';
+
+import {wrap} from '../helpers/dom-utils';
+import features from '.';
+import {isNotRefinedGitHubRepo} from '../github-helpers';
+
+// eslint-disable-next-line import/prefer-default-export
+export function linkifyFeature(codeElement: HTMLElement): void {
+	const id = codeElement.textContent as FeatureID;
+	if (features.list.includes(id) && !codeElement.closest('a')) {
+		wrap(codeElement, <a href={`/sindresorhus/refined-github/blob/main/source/features/${id}.tsx`}/>);
+	}
+}
+
+function init(): void {
+	for (const possibleMention of select.all('.js-comment-body code')) {
+		linkifyFeature(possibleMention);
+	}
+}
+
+void features.add(__filebasename, {
+	include: [
+		pageDetect.hasComments
+	],
+	exclude: [
+		isNotRefinedGitHubRepo
+	],
+	init
+});
