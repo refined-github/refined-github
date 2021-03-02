a19275bbcecc5a549fbcca93ab6b21093cf16a9d
Yakov <16872793+yakov116@users.noreply.github.com>
2021-03-01 23:17:00 -0500

    Cleanup x 4

--- source/github-helpers/toast.tsx
+++ source/github-helpers/toast.tsx
@@ -5,13 +5,9 @@ import {CheckIcon} from '@primer/octicons-react';
 import {ToastSpinner} from '../helpers/icons';
 
 export default class Toast {
-	toastContent: string;
+	constructor() {}
 
-	constructor(toastContent: string) {
-		this.toastContent = toastContent;
-	}
-
-	baseElement(backgroundClass: string, icon: JSX.Element): JSX.Element {
+	baseElement(backgroundClass: string, icon: JSX.Element, toastContent: string): JSX.Element {
 		return (
 			<div
 				role="log"
@@ -21,19 +17,19 @@ export default class Toast {
 				<span className="Toast-icon">
 					{icon}
 				</span>
-				<span className="Toast-content">{this.toastContent}</span>
+				<span className="Toast-content">{toastContent}</span>
 			</div>
 		);
 	}
 
-	show(): void {
+	show(textContent = 'Bulk actions currently being processed.'): void {
 		this.destroy();
-		document.body.append(this.baseElement('Toast--loading', <ToastSpinner/>));
+		document.body.append(this.baseElement('Toast--loading', <ToastSpinner/>, textContent));
 	}
 
-	done(): void {
+	done(textContent = 'Bulk action processing complete.'): void {
 		this.destroy();
-		document.body.append(this.baseElement('Toast--success', <CheckIcon/>));
+		document.body.append(this.baseElement('Toast--success', <CheckIcon/>, textContent));
 		setTimeout(() => {
 			this.destroy();
 		}, 3000);
