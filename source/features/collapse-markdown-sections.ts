c9f859bdc0dbab4ee6a50d49d22444ed0bd4e3ae
cheap-glitch <cheap.glitch@gmail.com>
2021-03-10 12:08:40 +0100

    Rename state variable

--- source/features/collapse-markdown-sections.ts
+++ source/features/collapse-markdown-sections.ts
@@ -5,7 +5,7 @@ import * as pageDetect from 'github-url-detection';
 import features from '.';
 
 function toggleSection(sectionHeading: HTMLElement): void {
-	const sectionState = sectionHeading.classList.toggle('rgh-markdown-section-collapsed');
+	const isSectionHidden = sectionHeading.classList.toggle('rgh-markdown-section-collapsed');
 	let element = sectionHeading.nextElementSibling as HTMLElement;
 	while (element) {
 		if (/^H\d$/.test(element.tagName)) {
@@ -13,9 +13,9 @@ function toggleSection(sectionHeading: HTMLElement): void {
 				return;
 			}
 
-			element.classList.toggle('rgh-markdown-section-collapsed', sectionState);
+			element.classList.toggle('rgh-markdown-section-collapsed', isSectionHidden);
 		} else {
-			element.hidden = sectionState;
+			element.hidden = isSectionHidden;
 		}
 
 		element = element.nextElementSibling as HTMLElement;
