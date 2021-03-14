c7b8114b8d1a45048347918b8ac846a67f0a5973
Kid <kidonng@gmail.com>
2021-03-14 20:11:56 +0800

    Simplify  symbol name

--- source/features/ci-link.tsx
+++ source/features/ci-link.tsx
@@ -8,25 +8,25 @@ import fetchDom from '../helpers/fetch-dom';
 import {buildRepoURL, getPRNumber} from '../github-helpers';
 
 // Look for the CI icon in the latest 2 days of commits #2990
-const getRepoCIIcon = onetime(async () => fetchDom(
+const getRepoIcon = onetime(async () => fetchDom(
 	buildRepoURL('commits'), [
 		'.commit-group:nth-of-type(-n+2) .commit-build-statuses', // Pre "Repository refresh" layout
 		'.TimelineItem--condensed:nth-of-type(-n+2) .commit-build-statuses'
 	].join()
 ));
 
-const getPRCIIcon = onetime(async () => fetchDom(
+const getPRIcon = onetime(async () => fetchDom(
 	buildRepoURL('pull', getPRNumber().slice(1), 'commits'), '.js-commits-list-item:last-of-type .commit-build-statuses'
 ));
 
 async function initRepo(): Promise<false | void> {
-	const icon = await getRepoCIIcon() as HTMLElement | undefined;
+	const icon = await getRepoIcon() as HTMLElement | undefined;
 	if (!icon) {
 		return false;
 	}
 
 	icon.classList.add('rgh-ci-link');
-	if (onetime.callCount(getRepoCIIcon) > 1) {
+	if (onetime.callCount(getRepoIcon) > 1) {
 		icon.style.animation = 'none';
 	}
 
@@ -35,13 +35,13 @@ async function initRepo(): Promise<false | void> {
 }
 
 async function initPR(): Promise<false | void> {
-	const icon = await getPRCIIcon() as HTMLElement | undefined;
+	const icon = await getPRIcon() as HTMLElement | undefined;
 	if (!icon) {
 		return false;
 	}
 
 	icon.classList.add('rgh-ci-link');
-	if (onetime.callCount(getPRCIIcon) > 1) {
+	if (onetime.callCount(getPRIcon) > 1) {
 		icon.style.animation = 'none';
 	}
 
