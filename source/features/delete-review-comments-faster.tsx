dd756ee7fab69f5b0dbccf50f5ac305b16c4a2d7
cheap-glitch <cheap.glitch@gmail.com>
2021-03-14 13:19:32 +0100

    Load dropdown if fragment is not loaded yet

--- source/features/delete-review-comments-faster.tsx
+++ source/features/delete-review-comments-faster.tsx
@@ -6,13 +6,26 @@ import * as pageDetect from 'github-url-detection';
 
 import features from '.';
 
+function deleteComment(comment: HTMLElement): void {
+	select('.dropdown-menu .js-comment-delete > button', comment)!.click();
+}
+
 function onButtonClick(event: delegate.Event): void {
-	select('.dropdown-menu .js-comment-delete > button', (event.target as HTMLElement).closest('.js-comment')!)!.click();
+	const comment = (event.target as HTMLElement).closest<HTMLElement>('.js-comment')!;
+	const dropdownFragment = select('include-fragment.SelectMenu-loading', comment);
+	if (dropdownFragment) {
+		select('.timeline-comment-actions > details:last-of-type', comment)!.dispatchEvent(new Event('mouseover'));
+		dropdownFragment.addEventListener('load', () => {
+			deleteComment(comment);
+		});
+		return;
+	}
+
+	deleteComment(comment);
 }
 
 function init(): void {
 	delegate(document, '.rgh-review-comment-delete-button', 'click', onButtonClick);
-
 	observe('.review-comment > .unminimized-comment .btn-primary[type="submit"]', {
 		add(submitButton) {
 			if (submitButton.classList.contains('rgh-delete-button-added')) {
