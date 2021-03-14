2c3e232fc6d395ec1bca4a5a034a2187f45c7aa6
Kid <kidonng@gmail.com>
2021-03-14 18:57:12 +0800

    Simplify

--- source/features/show-forks-in-search.tsx
+++ source/features/show-forks-in-search.tsx
@@ -7,15 +7,14 @@ import features from '.';
 import SearchQuery from '../github-helpers/search-query';
 
 function init(): void {
-	let includeForks = new SearchQuery(location.search).includes('fork:true');
+	const includeForks = new SearchQuery(location.search).includes('fork:true');
 	const checkbox = <label className="float-right"><input className="rgh-show-forks-in-search-checkbox mr-1" type="checkbox" checked={includeForks}/>Include forks</label>;
 	checkbox.addEventListener('click', () => {
-		includeForks = !includeForks;
 		for (const link of select.all<HTMLAnchorElement>('.codesearch-results .select-menu-item')) {
 			if (includeForks) {
-				new SearchQuery(link).add('fork:true');
-			} else {
 				new SearchQuery(link).remove('fork:true');
+			} else {
+				new SearchQuery(link).add('fork:true');
 			}
 
 			if (link.getAttribute('aria-checked') === 'true') {
