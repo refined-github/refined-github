d8ae541af84411bca25992f0e5422f4cab4118b1
Kid <kidonng@gmail.com>
2021-03-15 17:57:11 +0800

    Resolve reviews

--- source/features/show-forks-in-search.tsx
+++ source/features/show-forks-in-search.tsx
@@ -1,6 +1,6 @@
-import './show-forks-in-search.css';
 import React from 'dom-chef';
 import select from 'select-dom';
+import delegate from 'delegate-it';
 import * as pageDetect from 'github-url-detection';
 
 import features from '.';
@@ -8,21 +8,25 @@ import SearchQuery from '../github-helpers/search-query';
 
 function init(): void {
 	const includeForks = new SearchQuery(location.search).includes('fork:true');
-	const checkbox = <label className="float-right"><input className="rgh-show-forks-in-search-checkbox mr-1" type="checkbox" checked={includeForks}/>Include forks</label>;
-	checkbox.addEventListener('click', () => {
-		for (const link of select.all<HTMLAnchorElement>('.codesearch-results .select-menu-item')) {
-			if (includeForks) {
-				new SearchQuery(link).remove('fork:true');
-			} else {
-				new SearchQuery(link).add('fork:true');
-			}
 
-			if (link.getAttribute('aria-checked') === 'true') {
-				link.click();
-			}
+	const checkbox = <input className="rgh-show-forks-in-search v-align-middle mr-1" type="checkbox" checked={includeForks}/>;
+	delegate(document, '.rgh-show-forks-in-search', 'change', () => {
+		const link = select<HTMLAnchorElement>('.codesearch-results .select-menu-item[aria-checked="true"]')!;
+
+		if (includeForks) {
+			new SearchQuery(link).remove('fork:true');
+		} else {
+			new SearchQuery(link).add('fork:true');
 		}
+
+		link.click();
 	});
-	select('.codesearch-results .select-menu-title')!.after(checkbox);
+
+	select('.codesearch-results .select-menu-title')!.after(
+		<label className="float-right">
+			{checkbox}Include forks
+		</label>
+	);
 }
 
 void features.add(__filebasename, {
