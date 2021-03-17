f37ff27db1ddab541a1a53a029e289b5c07fe4f4
Yakov <16872793+yakov116@users.noreply.github.com>
2021-03-16 23:20:46 -0400

    Enable `one-key-formatting` on title fields

---
+++ source/github-events/on-field-keydown.tsx
@@ -0,0 +1,29 @@
+import select from 'select-dom';
+import delegate from 'delegate-it';
+
+function onFieldKeydown(selector: string, callback: delegate.EventHandler<KeyboardEvent, HTMLTextAreaElement | HTMLInputElement>): void {
+	delegate<HTMLTextAreaElement, KeyboardEvent>(document, selector, 'keydown', event => {
+		const field = event.delegateTarget;
+
+		// Don't do anything if the autocomplete helper is shown or if non-latin input is being used
+		if (select.exists('.suggester', field.form!) || event.isComposing) {
+			return;
+		}
+
+		callback(event);
+	}, {
+		// Adds support for `esc` key; GitHub seems to use `stopPropagation` on it
+		capture: true
+	});
+}
+
+const onCommentFieldKeydown = (callback: delegate.EventHandler<KeyboardEvent, HTMLTextAreaElement>) => {
+	onFieldKeydown('.js-comment-field, #commit-description-textarea', callback);
+};
+
+const onTitleFieldKeydown = (callback: delegate.EventHandler<KeyboardEvent, HTMLInputElement>) => {
+	onFieldKeydown('#issue_title', callback);
+};
+
+export {onCommentFieldKeydown, onTitleFieldKeydown};
+
