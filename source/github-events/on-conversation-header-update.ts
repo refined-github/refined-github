8af2f77ea59ece8657e4d46bdcb18a843a823845
Yakov <16872793+yakov116@users.noreply.github.com>
2021-03-19 14:19:38 -0400

    Remove options

--- source/github-events/on-conversation-header-update.ts
+++ source/github-events/on-conversation-header-update.ts
@@ -2,12 +2,11 @@ import select from 'select-dom';
 
 import observeElement from '../helpers/simplified-element-observer';
 
-export default function onConversationHeaderUpdate(callback: VoidFunction, options?: MutationObserverInit): void {
+export default function onConversationHeaderUpdate(callback: VoidFunction): void {
 	const conversationHeader = select('#partial-discussion-header');
 	if (conversationHeader) {
 		observeElement(conversationHeader.parentElement!, callback, {
-			childList: true,
-			...options
+			childList: true
 		});
 	}
 }
