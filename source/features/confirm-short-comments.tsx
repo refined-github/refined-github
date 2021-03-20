b592931df517d2a0dca4a8b07cd43cb132ee4afb
darkred <rick3162@gmail.com>
2021-03-20 17:15:10 +0200

    Update confirm-short-comments.tsx
    - Now there's only one confirm() message for all cases
    - Added non-working  workaround for form submission not working after the first blocking.

--- source/features/confirm-short-comments.tsx
+++ source/features/confirm-short-comments.tsx
@@ -4,24 +4,39 @@ import * as pageDetect from 'github-url-detection';
 
 import features from '.';
 
-
 function handleIssueComment(event: delegate.Event<MouseEvent, KeyboardEvent, HTMLAnchorElement>): void {
 	const textareas = select('textarea[name="comment[body]"], textarea#issue_body')!.value.length;
-	const title = select('input#issue_title')?.value.length;
-	if ( (textareas && textareas < 3) && !confirm('Body is less than 3 characters, are you sure?') ) {
-		event.preventDefault();
-		event.stopImmediatePropagation();
+	const title = select('form.new_issue input#issue_title')?.value.length;
+	
+	let message = '';
+	if ( (title && title < 3) && (textareas < 3)) {
+		message = 'Body, title are';
+	} else if (title && title < 3) {
+		message = message + 'Title is';
+	} else if (textareas < 3) {
+		message = message + 'Body is';
 	}
+	message = message + ' less than 3 characters, are you sure?';
 
-	if ( pageDetect.isNewIssue() && (title && title < 3) && !confirm('Title is less than 3 characters, are you sure?') ) {
-		event.preventDefault();
+	if ( ( (title && title < 3) || (textareas < 3) ) && !confirm(message) ) {
+		event.preventDefault(); 
+
+		if ( (textareas < 3 ) && pageDetect.isIssue() ) {
+			event.stopImmediatePropagation();
+		} 
+
+		const element = document.activeElement;
+		element?.blur();
+		element?.focus();
 	}
 
 }
+
 
 function init(): void {
 	delegate(document, 'form.js-new-comment-form, form.new_issue', 'submit', handleIssueComment)
 }
+
 
 void features.add(__filebasename, {
 	include: [
