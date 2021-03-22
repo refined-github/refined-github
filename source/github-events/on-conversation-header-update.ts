46f45a28790562ccd3c588f9b39056c230b2f49a
Yakov <16872793+yakov116@users.noreply.github.com>
2021-03-21 22:27:31 -0400

    Use simple observer

--- source/github-events/on-conversation-header-update.ts
+++ source/github-events/on-conversation-header-update.ts
@@ -1,11 +1,9 @@
 import select from 'select-dom';
 
-import observeElement from '../helpers/simplified-element-observer';
-
 export default function onConversationHeaderUpdate(callback: VoidFunction): void {
 	const conversationHeader = select('#partial-discussion-header');
 	if (conversationHeader) {
-		observeElement(conversationHeader.parentElement!, callback, {
+		new MutationObserver(callback).observe(conversationHeader.parentElement!, {
 			childList: true
 		});
 	}
