5b850ff7472cf6ecaa1b45a5eb0b38ef85d1d37a
cheap-glitch <cheap.glitch@gmail.com>
2021-03-31 11:10:15 +0200

    Move bisect logic to separate function

--- source/features/index.tsx
+++ source/features/index.tsx
@@ -94,7 +94,7 @@ const globalReady: Promise<RGHOptions> = new Promise(async resolve => {
 	document.documentElement.classList.add('refined-github');
 
 	// Options defaults
-	let options = await optionsStorage.getAll();
+	const options = await optionsStorage.getAll();
 	const hotfix = browser.runtime.getManifest().version === '0.0.0' || await cache.get('hotfix'); // Ignores the cache when loaded locally
 
 	// If features are remotely marked as "seriously breaking" by the maintainers, disable them without having to wait for proper updates to propagate #3529
@@ -105,37 +105,36 @@ const globalReady: Promise<RGHOptions> = new Promise(async resolve => {
 		document.head.append(<style>{options.customCSS}</style>);
 	}
 
-	const testedFeatures: [string[], string[]] | false | undefined = await cache.get('bisect');
-	if (testedFeatures !== undefined) {
-		if (testedFeatures === false) {
-			createMessageBox('Every feature has been disabled. If you still see the bug, try disabling the whole extension.', false);
-			return;
-		}
-
-		const [firstHalf] = testedFeatures;
-		options = {
-			...options,
-			...Object.fromEntries(__features__.map(feature => [`feature:${feature}`, firstHalf.includes(feature)]))
-		};
-
-		if (firstHalf.length === 0) {
-			createMessageBox('Every feature has been disabled. Can you see the bug?');
-			return;
-		}
-
-		if (firstHalf.length === 1) {
-			createMessageBox(<span>The bug is caused by <a href={'https://github.com/sindresorhus/refined-github/blob/main/source/features/' + firstHalf[0] + '.tsx'}><code>{firstHalf[0]}</code></a>.</span>, false);
-			return;
-		}
-
-		createMessageBox(`${Math.ceil(Math.log2(firstHalf.length))} steps remaining. Can you see the bug?`);
-	}
+	Object.assign(options, await bisectFeatures());
 
 	// Create logging function
 	log = options.logging ? console.log : () => {/* No logging */};
 
 	resolve(options);
 });
+
+async function bisectFeatures(): Promise<Record<string, boolean>> {
+	const bisectedFeatures: string[] | false | undefined = await cache.get('bisect');
+	if (bisectedFeatures === undefined) {
+		return {};
+	}
+
+	if (bisectedFeatures === false) {
+		createMessageBox('Every feature has been disabled. If you still see the bug, try disabling the whole extension.', false);
+		return {};
+	}
+
+	const [firstHalf] = splitArray(bisectedFeatures);
+	if (firstHalf.length === 0) {
+		createMessageBox('Every feature has been disabled. Can you see the bug?');
+	} else if (firstHalf.length === 1) {
+		createMessageBox(<span>The bug is caused by <a href={'https://github.com/sindresorhus/refined-github/blob/main/source/features/' + firstHalf[0] + '.tsx'}><code>{firstHalf[0]}</code></a>.</span>, false);
+	} else {
+		createMessageBox(`${Math.ceil(Math.log2(firstHalf.length))} steps remaining. Can you see the bug?`);
+	}
+
+	return Object.fromEntries(features.list.map(feature => [`feature:${feature}`, firstHalf.includes(feature)]));
+}
 
 function createMessageBox(message: string | Element, yesNoButtons = true): void {
 	delegate(document, '.rgh-bisect-button-choice', 'click', onBisectButtonChoiceClick);
