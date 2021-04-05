1c517a7211d43a850aa10a253184ee5fa17a8b69
Federico <me@fregante.com>
2021-04-05 15:09:07 +0700

    Refactor
    - Starts from all the features, ends on no features
    - changes steps copy
    - simplifies createMessageBox to accept a string string buttons only when needed, instead of using a yesNoButtons boolean
    - avoid refresh on last step
    - clean cache on last step, without having to click exit
    - avoid nested conditions

--- source/helpers/bisect.tsx
+++ source/helpers/bisect.tsx
@@ -3,6 +3,9 @@ import cache from 'webext-storage-cache';
 import select from 'select-dom';
 
 import features from '../features';
+import pluralize from './pluralize';
+
+const getMiddleStep = (list: any[]): number => Math.floor(list.length / 2);
 
 export default async function bisectFeatures(): Promise<Record<string, boolean> | void> {
 	const bisectedFeatures = await cache.get<FeatureID[]>('bisect');
@@ -10,19 +13,29 @@ export default async function bisectFeatures(): Promise<Record<string, boolean> 
 		return;
 	}
 
-	const enabledFeatures = new Set(bisectedFeatures.slice(0, Math.ceil(bisectedFeatures.length / 2)));
-	if (bisectedFeatures.length === 0) {
-		createMessageBox(<p>Every feature has been disabled. Do you still see the change or issue?</p>);
-	} else if (bisectedFeatures.length === 1) {
-		createMessageBox(<p>The change or issue is caused by <a href={'https://github.com/sindresorhus/refined-github/blob/main/source/features/' + bisectedFeatures[0] + '.tsx'}><code>{bisectedFeatures[0]}</code></a>.</p>, false);
-	} else {
-		createMessageBox(<p>Can you see the change or issue? ({Math.ceil(Math.log2(bisectedFeatures.length))} steps remaining.)</p>);
-	}
 
-	return Object.fromEntries(features.list.map(feature => [`feature:${feature}`, enabledFeatures.has(feature)]));
+	const steps = Math.ceil(Math.log2(Math.max(bisectedFeatures.length))) + 1;
+	createMessageBox(
+		`Do you see the change or issue? (${pluralize(steps, 'last step', '$$ steps remaining')})`,
+		<div>
+			<button type="button" className="btn btn-danger mr-2" value="no" onClick={onChoiceButtonClick}>No</button>
+			<button type="button" className="btn btn-primary" value="yes" onClick={onChoiceButtonClick}>Yes</button>
+		</div>
+	);
+
+	const half = getMiddleStep(bisectedFeatures);
+	const temporaryOptionsPairs = features.list.map(feature => {
+		const index = bisectedFeatures.indexOf(feature);
+		return [
+			`feature:${feature}`,
+			index > -1 && index < half
+		];
+	});
+	const temporaryOptions = Object.fromEntries(temporaryOptionsPairs);
+	return temporaryOptions;
 }
 
-function createMessageBox(message: Element, yesNoButtons = true): void {
+function createMessageBox(message: Element | string, extraButtons?: Element): void {
 	select('#rgh-bisect-dialog')?.remove();
 	document.body.append(
 		<div
@@ -38,17 +51,10 @@ function createMessageBox(message: Element, yesNoButtons = true): void {
 				zIndex: 2147483647
 			}}
 		>
-			{message}
+			<p>{message}</p>
 			<div className="d-flex flex-justify-between">
-				<div>
-					<button type="button" className="btn" onClick={onEndButtonClick}>Exit</button>
-				</div>
-				{yesNoButtons ?
-					<div>
-						<button type="button" className="btn btn-danger mr-2" value="no" onClick={onChoiceButtonClick}>No</button>
-						<button type="button" className="btn btn-primary" value="yes" onClick={onChoiceButtonClick}>Yes</button>
-					</div> :
-					undefined}
+				<button type="button" className="btn" onClick={onEndButtonClick}>Exit</button>
+				{extraButtons}
 			</div>
 		</div>
 	);
@@ -57,26 +63,31 @@ function createMessageBox(message: Element, yesNoButtons = true): void {
 async function onChoiceButtonClick({currentTarget}: React.MouseEvent<HTMLButtonElement>): Promise<void> {
 	const answer = currentTarget.value;
 	const bisectedFeatures = (await cache.get<FeatureID[]>('bisect'))!;
-	if (!bisectedFeatures) {
-		return;
-	}
 
-	if (bisectedFeatures.length === 0) {
-		if (answer === 'yes') {
-			createMessageBox(<p>Every feature has been disabled. If you still see the change or issue, try disabling the whole extension.</p>, false);
-			return;
-		}
-
-		await cache.set('bisect', features.list);
+	if (bisectedFeatures.length > 1) {
+		await cache.set('bisect', answer === 'yes' ?
+			bisectedFeatures.slice(0, getMiddleStep(bisectedFeatures)) :
+			bisectedFeatures.slice(getMiddleStep(bisectedFeatures))
+		);
 
 		location.reload();
 		return;
 	}
 
-	const half = Math.ceil(bisectedFeatures.length / 2);
-	await cache.set('bisect', answer === 'yes' ? bisectedFeatures.slice(0, half) : bisectedFeatures.slice(half));
+	// Last step, no JS feature was enabled
+	if (answer === 'yes') {
+		createMessageBox('No features were enabled on this page. Try disabling Refined GitHub to see if it belongs to it at all.');
+	} else {
+		const feature = (
+			<a href={'https://github.com/sindresorhus/refined-github/blob/main/source/features/' + bisectedFeatures[0] + '.tsx'}>
+				<code>{bisectedFeatures[0]}</code>
+			</a>
+		);
 
-	location.reload();
+		createMessageBox(<>The change or issue is caused by {feature}.</>);
+	}
+
+	await cache.delete('bisect');
 }
 
 async function onEndButtonClick(): Promise<void> {
