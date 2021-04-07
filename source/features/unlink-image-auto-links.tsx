ae821c78c9e7eecf667524da2441d00ad7654653
Kid <kidonng@gmail.com>
2021-04-07 22:32:23 +0800

    Add more file types

--- source/features/unlink-image-auto-links.tsx
+++ source/features/unlink-image-auto-links.tsx
@@ -5,6 +5,19 @@ import features from '.';
 import {isMarkdownFile} from '../github-helpers';
 import GitHubURL from '../github-helpers/github-url';
 
+function isMarkupFile(): boolean {
+	if (isMarkdownFile()) {
+		return true;
+	}
+
+	// https://github.com/github/markup/blob/46908504f5099b15a88030749f0aeb52cd2f6799/README.md#markups
+	if (/\.(textile|rdoc|org|creole|(media)?wiki|rst|a(scii)?doc|asc|pod)$/.test(location.pathname)) {
+		return true;
+	}
+
+	return false;
+}
+
 function getRawUrl(link: HTMLAnchorElement): string {
 	if (pageDetect.isSingleFile(link)) {
 		const rawLink = new GitHubURL(link.href).assign({route: 'raw'});
@@ -27,7 +40,7 @@ function init(): void {
 void features.add(__filebasename, {
 	include: [
 		pageDetect.isRepoTree,
-		() => pageDetect.isSingleFile() && isMarkdownFile()
+		() => pageDetect.isSingleFile() && isMarkupFile()
 	],
 	init
 });
