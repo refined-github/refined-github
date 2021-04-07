22a97b79da8b9460b5f12470674025c7479afc5e
yakov116 <16872793+yakov116@users.noreply.github.com>
2021-04-07 01:20:26 -0400

    Update wording in `enable-file-links-in-compare-view.`
    Co-authored-by: Federico <me@fregante.com>

--- source/features/enable-file-links-in-compare-view.tsx
+++ source/features/enable-file-links-in-compare-view.tsx
@@ -60,7 +60,7 @@ void features.add(__filebasename, {
 		pageDetect.isPRCommit
 	],
 	exclude: [
-		// Only enabled on Open/Draft PRs. Editing files doesn't make sense after a PR is closed/merged.
+		// Editing files doesn't make sense after a PR is closed/merged
 		pageDetect.isClosedPR,
 		// If you're viewing changes from partial commits, ensure you're on the latest one.
 		() => select.exists('.js-commits-filtered') && !select.exists('[aria-label="You are viewing the latest commit"]')
