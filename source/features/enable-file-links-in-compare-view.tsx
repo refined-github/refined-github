a596b1f88d5ed15e6e3c795c8ab54547ec6da93c
Yakov <16872793+yakov116@users.noreply.github.com>
2021-04-07 01:01:32 -0400

    Use `pageDetect` to exclude in `enable-file-links-in-compare-view`

--- source/features/enable-file-links-in-compare-view.tsx
+++ source/features/enable-file-links-in-compare-view.tsx
@@ -61,7 +61,7 @@ void features.add(__filebasename, {
 	],
 	exclude: [
 		// Only enabled on Open/Draft PRs. Editing files doesn't make sense after a PR is closed/merged.
-		() => !select.exists('.gh-header-meta [title$="Open"], .gh-header-meta [title$="Draft"]'),
+		pageDetect.isClosedPR,
 		// If you're viewing changes from partial commits, ensure you're on the latest one.
 		() => select.exists('.js-commits-filtered') && !select.exists('[aria-label="You are viewing the latest commit"]')
 	],
