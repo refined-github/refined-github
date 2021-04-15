76211a459f33395e941bc0df683d6b7cd7dacd2b
Federico Brigante <me@fregante.com>
2021-04-15 23:32:03 +0700

    Restore vertical-front-matter.tsx

--- source/features/vertical-front-matter.tsx
+++ source/features/vertical-front-matter.tsx
@@ -4,7 +4,9 @@ import select from 'select-dom';
 import * as pageDetect from 'github-url-detection';
 
 import features from '.';
-import {isMarkdownFile} from '../github-helpers';
+
+// https://github.com/github/markup/blob/cd01f9ec87c86ce5a7c70188a74ef40fc4669c5b/lib/github/markup/markdown.rb#L34
+const hasFrontMatter = (): boolean => pageDetect.isSingleFile() && /\.(mdx?|mkdn?|mdwn|mdown|markdown|litcoffee)$/.test(location.pathname);
 
 function init(): void | false {
 	const table = select('[data-table-type="yaml-metadata"]')!;
@@ -30,7 +32,7 @@ function init(): void | false {
 
 void features.add(__filebasename, {
 	include: [
-		() => pageDetect.isSingleFile() && isMarkdownFile()
+		hasFrontMatter
 	],
 	init
 });
