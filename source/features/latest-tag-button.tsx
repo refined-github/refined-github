44dabf2758d06507d5b3219005c78eca9216619d
cheap-glitch <cheap.glitch@gmail.com>
2021-04-15 19:00:33 +0200

    Remove unnecessary deduplication

--- source/features/latest-tag-button.tsx
+++ source/features/latest-tag-button.tsx
@@ -20,12 +20,8 @@ export async function addAfterBranchSelector(button: Element): Promise<void> {
 	const wrapper = branchSelector.closest('.position-relative')!;
 	button.classList.add('mt-md-2', 'mt-lg-0');
 	wrapper.append(button);
-	if (wrapper.classList.contains('rgh-button-added')) {
-		return;
-	}
-
 	branchSelector.classList.add('mr-2');
-	wrapper.classList.add('d-flex', 'd-md-block', 'd-lg-flex', 'rgh-button-added');
+	wrapper.classList.add('d-flex', 'd-md-block', 'd-lg-flex');
 	select('.breadcrumb')!.classList.add('flex-md-self-baseline', 'flex-lg-self-center');
 }
 
