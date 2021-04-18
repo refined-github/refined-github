773c390c2cced011ac3ffdf8ce20d624b184b334
Yakov <16872793+yakov116@users.noreply.github.com>
2021-04-17 23:23:52 -0400

    Revert "Fix visual issue in `quick-repo-deletion`"
    This reverts commit 0ad953e1f16d369aa917a36ae86d3011685adea7.

--- source/features/quick-repo-deletion.tsx
+++ source/features/quick-repo-deletion.tsx
@@ -15,7 +15,6 @@ import looseParseInt from '../helpers/loose-parse-int';
 import parseBackticks from '../github-helpers/parse-backticks';
 
 function handleToggle(event: delegate.Event<Event, HTMLDetailsElement>): void {
-	document.body.classList.add('rgh-quick-repo-deletion');
 	const hasContent = select.exists([
 		'[data-hotkey="g i"] .Counter:not([hidden])', // Open issues
 		'[data-hotkey="g p"] .Counter:not([hidden])', // Open PRs
@@ -69,7 +68,6 @@ async function buttonTimeout(buttonContainer: HTMLDetailsElement): Promise<boole
 			await delay(1000, {signal: abortController.signal}); // eslint-disable-line no-await-in-loop
 		} while (--secondsLeft);
 	} catch {
-		document.body.classList.remove('rgh-quick-repo-deletion');
 		button.textContent = 'Delete repo';
 		button.style.transform = '';
 	}
