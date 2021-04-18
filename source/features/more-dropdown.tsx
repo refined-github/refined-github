146190aa74dc6bbf99b7a8199e185ce76e397cc8
Federico Brigante <me@fregante.com>
2021-04-19 01:38:20 +0700

    Undo TS change

--- source/features/more-dropdown.tsx
+++ source/features/more-dropdown.tsx
@@ -59,8 +59,8 @@ async function init(): Promise<void> {
 	].join());
 
 	const reference = await getCurrentCommittishAnywhere() ?? await getDefaultBranch();
-	const compareUrl = buildRepoURL('compare', reference!);
-	const commitsUrl = buildRepoURL('commits', reference!);
+	const compareUrl = buildRepoURL('compare', reference);
+	const commitsUrl = buildRepoURL('commits', reference);
 	const branchesUrl = buildRepoURL('branches');
 	const dependenciesUrl = buildRepoURL('network/dependencies');
 
