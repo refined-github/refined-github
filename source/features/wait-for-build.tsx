405d809752b17607fa826d665f823b3a4ace8294
yakov116 <16872793+yakov116@users.noreply.github.com>
2021-04-19 22:56:10 -0400

    Restore wait-for-build.tsx

--- source/features/wait-for-build.tsx
+++ source/features/wait-for-build.tsx
@@ -41,7 +41,8 @@ function showCheckboxIfNecessary(): void {
 function disableForm(disabled = true): void {
 	for (const field of select.all(`
 		textarea[name="commit_message"],
-		input:is([name="commit_title"], [name="rgh-pr-check-waiter"]),
+		input[name="commit_title"],
+		input[name="rgh-pr-check-waiter"],
 		button.js-merge-commit-button
 	`)) {
 		field.disabled = disabled;
