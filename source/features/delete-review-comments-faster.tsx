963e0af80f15ad936727d5bbe90e74f991ef84b4
cheap-glitch <cheap.glitch@gmail.com>
2021-04-20 20:19:52 +0200

    Change design to single icon button

--- source/features/delete-review-comments-faster.tsx
+++ source/features/delete-review-comments-faster.tsx
@@ -1,10 +1,9 @@
-import './delete-review-comments-faster.css';
-
 import React from 'dom-chef';
 import select from 'select-dom';
 import delegate from 'delegate-it';
 import {observe} from 'selector-observer';
 import * as pageDetect from 'github-url-detection';
+import {TrashIcon} from '@primer/octicons-react';
 
 import features from '.';
 import onFragmentLoad from '../github-events/on-fragment-load';
@@ -18,19 +17,18 @@ async function onButtonClick(event: delegate.Event): Promise<void> {
 	select('.dropdown-menu .js-comment-delete > button', comment)!.click();
 }
 
-function addDeleteButton(submitButton: Element): void {
-	submitButton.classList.add('rgh-delete-button-added');
-	submitButton.parentElement!.classList.add('d-flex', 'flex-row-reverse', 'flex-justify-between', 'rgh-delete-button-form');
-	submitButton.after(
-		<button className="btn btn-danger rgh-review-comment-delete-button" type="button">
-			Delete comment
+function addDeleteButton(cancelButton: Element): void {
+	cancelButton.classList.add('rgh-delete-button-added');
+	cancelButton.after(
+		<button className="btn btn-danger float-left rgh-review-comment-delete-button" type="button">
+			<TrashIcon/>
 		</button>
 	);
 }
 
 function init(): void {
 	const listener = delegate(document, '.rgh-review-comment-delete-button', 'click', onButtonClick);
-	const observer = observe('.review-comment > .unminimized-comment form:not(.js-single-suggested-change-form) .btn-primary[type="submit"]:not(.rgh-delete-button-added)', {
+	const observer = observe('.review-comment > .unminimized-comment form:not(.js-single-suggested-change-form) .js-comment-cancel-button:not(.rgh-delete-button-added)', {
 		add: addDeleteButton
 	});
 	deinit.push(listener.destroy, observer.abort);
