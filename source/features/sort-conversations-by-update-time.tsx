2f48d46783c2a9c975e613b660d9d220b0bf1b5d
yakov116 <16872793+yakov116@users.noreply.github.com>
2021-04-19 22:57:59 -0400

    Update source/features/sort-conversations-by-update-time.tsx

--- source/features/sort-conversations-by-update-time.tsx
+++ source/features/sort-conversations-by-update-time.tsx
@@ -17,7 +17,7 @@ function init(): void {
 	}
 
 	// Extra nicety: Avoid GitHub's unnecessary redirect, this is their own bug
-	for (const link of select.all('a[href*="/issues"][href*="is%3Apr"]:not([href*="is%3Aprivate"])')) {
+	for (const link of select.all('a[href*="/issues"][href*="is%3Apr"]:not([href*="is%3Aprivate"])')) { // #4161
 		link.pathname = link.pathname.replace(/issues\/?$/, 'pulls');
 	}
 }
