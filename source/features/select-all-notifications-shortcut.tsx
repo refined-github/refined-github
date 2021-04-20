8879ebefcbabc9df2eff41a1864ccfff568040c0
Federico Brigante <me@fregante.com>
2021-04-20 16:34:44 +0700

    `fit-textareas`: Improve performance (#4222)

--- source/features/select-all-notifications-shortcut.tsx
+++ source/features/select-all-notifications-shortcut.tsx
@@ -5,7 +5,7 @@ import features from '.';
 import onElementRemoval from '../helpers/on-element-removal';
 
 async function init(): Promise<void> {
-	const selectAllNotifications = await elementReady<HTMLElement>('.js-notifications-mark-all-prompt');
+	const selectAllNotifications = await elementReady('.js-notifications-mark-all-prompt');
 	if (selectAllNotifications) { // Notifications page may be empty
 		selectAllNotifications.dataset.hotkey = 'a';
 		await onElementRemoval(selectAllNotifications); // "Select all" checkbox will be replaced if there's more notifications to load #4199
