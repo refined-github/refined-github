e3645e92814c6aa2874e8871f09faebd7239708c
darkred <rick3162@gmail.com>
2021-04-22 20:25:57 +0300

    Use addQuickSubmit

--- source/features/avoid-accidental-submissions.tsx
+++ source/features/avoid-accidental-submissions.tsx
@@ -11,16 +11,12 @@ function onKeyDownEnter(event: delegate.Event<KeyboardEvent, HTMLFormElement>): 
 	}
 }
 
-// Add ability to submit via Ctrl+Enter in isNewFile and isEditingFile pages (was not available before)
-function onKeyDownCtrlEnter(event: delegate.Event<KeyboardEvent, HTMLFormElement>): void {
-	if (event.key === 'Enter' && event.ctrlKey) {
-		event.delegateTarget.form.submit();
-	}
-}
-
 function init(): void {
 	delegate(document, 'input#issue_title, input#pull_request_title, input#commit-summary-input', 'keydown', onKeyDownEnter);
-	delegate(document, 'input#commit-summary-input', 'keydown', onKeyDownCtrlEnter);
+}
+
+function addQuickSubmit(): void {
+	select('input#commit-summary-input')?.classList.add('js-quick-submit');
 }
 
 void features.add(__filebasename, {
@@ -31,4 +27,10 @@ void features.add(__filebasename, {
 		pageDetect.isEditingFile
 	],
 	init
+}, {
+	include: [
+		pageDetect.isNewFile,
+		pageDetect.isEditingFile
+	],
+	init: addQuickSubmit
 });
