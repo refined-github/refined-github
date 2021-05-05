37b5734d86e006f9ee73ac496f31dad89f9d8b22
yakov116 <16872793+yakov116@users.noreply.github.com>
2021-05-05 05:31:31 -0400

    Restore globals.d.ts

--- source/globals.d.ts
+++ source/globals.d.ts
@@ -74,11 +74,3 @@ declare module 'react' {
 interface Node extends EventTarget {
 	cloneNode(deep?: boolean): this;
 }
-
-interface HTMLCollectionOf<T extends HTMLElement> {
-	[Symbol.iterator](): Iterator<T>;
-}
-
-interface NodeListOf<T extends ChildNode> {
-	[Symbol.iterator](): Iterator<T>;
-}
