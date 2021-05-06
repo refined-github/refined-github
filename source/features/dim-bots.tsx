254730a8064f1a0a33c2982852b8bacc11cc98b7
cheap-glitch <cheap.glitch@gmail.com>
2021-05-06 11:17:57 +0200

    Don't dim tagged commits in `dim-bots`

--- source/features/dim-bots.tsx
+++ source/features/dim-bots.tsx
@@ -1,5 +1,6 @@
 import './dim-bots.css';
 import select from 'select-dom';
+import oneMutation from 'one-mutation';
 import * as pageDetect from 'github-url-detection';
 
 import features from '.';
@@ -15,12 +16,24 @@ const botSelector = [
 	'.labels [href$="label%3Abot"]'
 ].join();
 
-function init(): void {
+function tagsLoaded(): boolean {
+	return select.exists('.js-navigation-container .octicon-tag');
+}
+
+async function init(): Promise<void> {
 	for (const bot of select.all(botSelector)) {
 		// Exclude co-authored commits
 		if (select.all('a', bot.parentElement!).every(link => link.matches(botSelector))) {
 			bot.closest('.commit, .Box-row')!.classList.add('rgh-dim-bot');
 		}
+	}
+
+	if (!tagsLoaded()) {
+		await oneMutation(document.body, {subtree: true, childList: true, filter: tagsLoaded});
+	}
+
+	for (const tag of select.all('.js-navigation-container .octicon-tag')) {
+		tag.closest('.commit, .Box-row')!.classList.remove('rgh-dim-bot');
 	}
 }
 
