074b2b8c2f0a12d82e6b528db1b6366f017148fa
Max Elkins <max@maxelkins.me>
2021-05-11 15:54:15 +0100

    Fix `remove-label-faster` hover color (#4346)

--- source/features/remove-label-faster.tsx
+++ source/features/remove-label-faster.tsx
@@ -47,9 +47,6 @@ async function init(): Promise<void> {
 					aria-label="Remove this label"
 					className="btn-link tooltipped tooltipped-nw rgh-remove-label-faster"
 					data-name={label.dataset.name}
-					style={{
-						'--rgh-remove-label-faster-color': label.style.backgroundColor
-					} as React.CSSProperties}
 				>
 					<XIcon/>
 				</button>
