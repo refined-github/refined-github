47c409ebc9dfa510398ce6e662e81e3864cd589e
Flo Edelmann <florian-edelmann@online.de>
2021-05-23 01:08:16 +0200

    Prevent discussion link loss

--- source/features/prevent-link-loss.tsx
+++ source/features/prevent-link-loss.tsx
@@ -7,27 +7,38 @@ import * as pageDetect from 'github-url-detection';
 import * as textFieldEdit from 'text-field-edit';
 
 import features from '.';
-import {prCommitUrlRegex, preventPrCommitLinkLoss, prCompareUrlRegex, preventPrCompareLinkLoss} from '../github-helpers';
+import {
+	prCommitUrlRegex, preventPrCommitLinkLoss,
+	prCompareUrlRegex, preventPrCompareLinkLoss,
+	discussionUrlRegex, preventDiscussionLinkLoss
+} from '../github-helpers';
 
 function handleButtonClick({delegateTarget: fixButton}: delegate.Event<MouseEvent, HTMLButtonElement>): void {
 	const field = fixButton.form!.querySelector('textarea')!;
 	textFieldEdit.replace(field, prCommitUrlRegex, preventPrCommitLinkLoss);
 	textFieldEdit.replace(field, prCompareUrlRegex, preventPrCompareLinkLoss);
+	textFieldEdit.replace(field, discussionUrlRegex, preventDiscussionLinkLoss);
 	fixButton.parentElement!.remove();
 }
 
 function getUI(field: HTMLTextAreaElement): HTMLElement {
 	return select('.rgh-prevent-link-loss-container', field.form!) ?? (
 		<div className="flash flash-warn mb-2 rgh-prevent-link-loss-container">
-			<AlertIcon/> Your PR Commit link may be <a target="_blank" rel="noopener noreferrer" href="https://github.com/sindresorhus/refined-github/issues/2327">misinterpreted by GitHub.</a>
+			<AlertIcon/> Your link may be misinterpreted by GitHub (see <a target="_blank" rel="noopener noreferrer" href="https://github.com/sindresorhus/refined-github/issues/2327">#2327</a>, <a target="_blank" rel="noopener noreferrer" href="https://github.com/sindresorhus/refined-github/issues/3528">#3528</a>, <a target="_blank" rel="noopener noreferrer" href="https://github.com/sindresorhus/refined-github/issues/4357">#4357</a>).
 			<button type="button" className="btn btn-sm primary flash-action rgh-prevent-link-loss">Fix link</button>
 		</div>
 	);
 }
 
-const updateUI = debounceFn(({delegateTarget: field}: delegate.Event<Event, HTMLTextAreaElement>): void => {
+function isVulnerableToLinkLoss(value: string) {
 	// The replacement logic is not just in the regex, so it alone can't be used to detect the need for the replacement
-	if (field.value === field.value.replace(prCommitUrlRegex, preventPrCommitLinkLoss) && field.value === field.value.replace(prCompareUrlRegex, preventPrCompareLinkLoss)) {
+	return value !== value.replace(prCommitUrlRegex, preventPrCommitLinkLoss) ||
+		value !== value.replace(prCompareUrlRegex, preventPrCompareLinkLoss) ||
+		value !== value.replace(discussionUrlRegex, preventDiscussionLinkLoss);
+}
+
+const updateUI = debounceFn(({delegateTarget: field}: delegate.Event<Event, HTMLTextAreaElement>): void => {
+	if (!isVulnerableToLinkLoss(field.value)) {
 		getUI(field).remove();
 	} else if (pageDetect.isNewIssue() || pageDetect.isCompare()) {
 		select('file-attachment', field.form!)!.append(
