d3add0bf16a41dc91dcd6e20ae8c43896793fa2d
shine <4771718+shinenelson@users.noreply.github.com>
2021-05-25 23:42:31 +0530

    cache workflows count
    https://github.com/sindresorhus/refined-github/pull/4395#issuecomment-847483998

--- source/features/remove-actions-tab.tsx
+++ source/features/remove-actions-tab.tsx
@@ -1,17 +1,26 @@
+import cache from 'webext-storage-cache';
 import elementReady from 'element-ready';
 import * as pageDetect from 'github-url-detection';
 
 import features from '.';
+import {getRepo} from '../github-helpers';
 import * as api from '../github-helpers/api';
+
+const getWorkflowsCount = cache.function(async (): Promise<number> => {
+	const {workflows} = await api.v3('actions/workflows');
+	return workflows.length;
+}, {
+	maxAge: {days: 1},
+	cacheKey: (): string => __filebasename + ':' + getRepo()!.nameWithOwner
+});
 
 async function removeActionsTab(): Promise<void | false> {
 	const actionsTab = await elementReady('[data-hotkey="g a"]');
-	const {workflows} = await api.v3('actions/workflows');
 
 	if (
 		!actionsTab || // Actions Tab does not exist 🎉
 		actionsTab.matches('.selected') || // User is on Actions tab 👀
-		workflows.length > 0 // Actions Workflows are configured for the repository
+		await getWorkflowsCount() > 0 // Actions Workflows are configured for the repository
 	) {
 		return false;
 	}
