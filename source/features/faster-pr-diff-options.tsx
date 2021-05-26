4600b4e7b8fc2e75c03e4a69d4a22a8122df18e7
l198881 <82917144+l198881@users.noreply.github.com>
2021-05-26 09:35:45 -0300

    Update and rename faster-pr-diff-options.ts to faster-pr-diff-options.tsx

--- source/features/faster-pr-diff-options.tsx
+++ source/features/faster-pr-diff-options.tsx
@@ -1,18 +1,18 @@
-import React from 'dom-chef';
-import select from 'select-dom';
-import * as pageDetect from 'github-url-detection';
-import {BookIcon, CheckIcon, DiffIcon, DiffModifiedIcon} from '@primer/octicons-react';
+ React 'dom-chef';
+ select 'select-dom';
+ * pageDetect 'github-url-detection';
+ {BookIcon, CheckIcon, DiffIcon, DiffModifiedIcon} from '@primer/octicons-react';
 
-import features from '.';
+ features '.';
 
-function makeLink(type: string, icon: Element, selected: boolean): JSX.Element {
-	const url = new URL(location.href);
+ makeLink(type: string, icon: Element, selected: boolean): JSX.Element {
+	 url URL(location.href);
 	url.searchParams.set('diff', type);
-	const classes = pageDetect.isPR() ?
+        classes pageDetect.isPR() ?
 		'tooltipped tooltipped-s d-none d-lg-block ml-2 color-icon-secondary' :
 		'tooltipped tooltipped-s btn btn-sm BtnGroup-item ' + (selected ? 'selected' : '');
 
-	return (
+	 (
 		<a
 			className={classes}
 			aria-label={`Switch to the ${type} diff view`}
@@ -23,19 +23,19 @@ function makeLink(type: string, icon: Element, selected: boolean): JSX.Element {
 	);
 }
 
-function createDiffStyleToggle(): DocumentFragment {
-	const isUnified = select.exists([
+createDiffStyleToggle(): DocumentFragment {
+	 isUnified = .exists([
 		'[value="unified"][checked]', // Form in PR
-		'.table-of-contents .selected[href*="diff=unified"]' // Link in single commit
+		'.table-of-contents .[href*="diff=unified"]' // Link in single commit
 	]);
 
-	if (pageDetect.isPR()) {
-		return isUnified ?
+	(pageDetect.isPR()) {
+	            isUnified ?
 			makeLink('split', <BookIcon/>, false) :
 			makeLink('unified', <DiffIcon/>, false);
 	}
 
-	return (
+	 (
 		<>
 			{makeLink('unified', <DiffIcon/>, isUnified)}
 			{makeLink('split', <BookIcon/>, !isUnified)}
@@ -43,83 +43,83 @@ function createDiffStyleToggle(): DocumentFragment {
 	);
 }
 
-function createWhitespaceButton(): HTMLElement {
-	const url = new URL(location.href);
-	const isHidingWhitespace = url.searchParams.get('w') === '1';
+ createWhitespaceButton(): HTMLElement {
+	 url URL(location.href);
+	 isHidingWhitespace url.searchParams.get('w') === '1';
 
-	if (isHidingWhitespace) {
-		url.searchParams.delete('w');
-	} else {
-		url.searchParams.set('w', '1');
+	 (isHidingWhitespace) {
+		url.searchParams.('w');
+	}  {
+		url.searchParams.('w', '1');
 	}
 
-	const classes = pageDetect.isPR() ?
-		'tooltipped tooltipped-s d-none d-lg-block color-icon-secondary ' + (isHidingWhitespace ? '' : 'color-icon-info') :
-		'tooltipped tooltipped-s btn btn-sm btn-outline tooltipped ' + (isHidingWhitespace ? 'bg-gray-light text-gray-light color-text-tertiary' : '');
+	 classes pageDetect.isPR() ?
+		'tooltipped tooltipped-s d-none d-lg-block color-icon-secondary ' (isHidingWhitespace ? '' : 'color-icon-info') :
+		'tooltipped tooltipped-s btn btn-sm btn-outline tooltipped ' (isHidingWhitespace ? 'bg-gray-light text-gray-light color-text-tertiary' : '');
 
-	return (
+	 (
 		<a
-			href={url.href}
-			data-hotkey="d w"
-			className={classes}
-			aria-label={`${isHidingWhitespace ? 'Show' : 'Hide'} whitespace changes`}
+			href{url.href}
+			-hotkey"d w"
+			className{classes}
+			-label{`${isHidingWhitespace ? 'Show' : 'Hide'} whitespace changes`}
 		>
 			{pageDetect.isPR() ? <DiffModifiedIcon/> : <>{isHidingWhitespace && <CheckIcon/>} No Whitespace</>}
 		</a>
 	);
 }
 
-function initPR(): false | void {
-	select('.js-file-filter')!.closest('.flex-auto')!.append(
-		<div className="diffbar-item d-flex">{createDiffStyleToggle()}</div>,
-		<div className="diffbar-item d-flex">{createWhitespaceButton()}</div>
+ initPR(): false {
+	('.js-file-filter')!.closest('.flex-auto')!.append(
+		className"diffbar-item d-flex"{createDiffStyleToggle()},
+		className"diffbar-item d-flex"{createWhitespaceButton()},
 	);
 
 	// Trim title
-	const prTitle = select('.pr-toolbar .js-issue-title');
-	if (prTitle && select.exists('.pr-toolbar progress-bar')) { // Only review view has progress-bar
-		prTitle.style.maxWidth = '24em';
-		prTitle.title = prTitle.textContent!;
+	 prTitle select('.pr-toolbar .js-issue-title');
+	(prTitle && select.exists('.pr-toolbar progress-bar')) { // Only review view has progress-bar
+		prTitle.style.maxWidth '24em';
+		prTitle.title prTitle.textContent!;
 	}
 
 	// Only show the native dropdown on medium and small screens #2597
-	select('.js-diff-settings')!.closest('details')!.classList.add('d-lg-none');
+	('.js-diff-settings')!.closest('details')!.classList.add('d-lg-none');
 
 	// Make space for the new button by removing "Changes from" #655
-	select('[data-hotkey="c"] strong')!.previousSibling!.remove();
+	('[data-hotkey="c"] strong')!.previousSibling!.remove();
 
 	// Remove extraneous padding around "Clear filters" button
-	select('.subset-files-tab')?.classList.replace('px-sm-3', 'ml-sm-2');
+	('.subset-files-tab')?.classList.replace('px-sm-3', 'ml-sm-2');
 }
 
-function initCommitAndCompare(): false | void {
-	select('#toc')!.prepend(
-		<div className="float-right d-flex">
-			<div className="d-flex ml-3 BtnGroup">{createDiffStyleToggle()}</div>
-			<div className="d-flex ml-3 BtnGroup">{createWhitespaceButton()}</div>
-		</div>
+      initCommitAndCompare(): false {
+	('#toc')!.prepend(
+		className"float-right d-flex">
+			className"d-flex ml-3 BtnGroup"{createDiffStyleToggle()}
+			className"d-flex ml-3 BtnGroup"{createWhitespaceButton()}
+		
 	);
 
 	// Remove previous options UI
-	select('[data-ga-load^="Diff, view"]')!.remove();
+	('[data-ga-load^="Diff, view"]')!.remove();
 }
 
-void features.add(__filebasename, {
-	include: [
+.add(__filebasename, {
+	[
 		pageDetect.isPRFiles,
 		pageDetect.isPRCommit
 	],
-	shortcuts: {
+	{
 		'd w': 'Show/hide whitespaces in diffs'
 	},
 	init: initPR
 }, {
-	include: [
+	[
 		pageDetect.isSingleCommit,
 		pageDetect.isCompare
 	],
-	shortcuts: {
-		'd w': 'Show/hide whitespaces in diffs'
+        {
+		'd w': 'Show/hide whitespaces diffs'
 	},
 	init: initCommitAndCompare
 });
