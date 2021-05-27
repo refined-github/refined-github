9f9634eb3c53093274039a8dbe6ff67d72686031
Yakov <16872793+yakov116@users.noreply.github.com>
2021-05-26 23:13:43 -0400

    Update picture and wording

--- readme.md
+++ readme.md
@@ -170,7 +170,7 @@ Thanks for contributing! 🦋🙌
 - [](# "refined-github.css") [Reduces tabs’ size to 4 spaces instead of 8.](https://cloud.githubusercontent.com/assets/170270/14170088/d3be931e-f755-11e5-8edf-c5f864336382.png)
 - [](# "esc-to-deselect-line") [Adds a keyboard shortcut to deselect the current line: <kbd>esc</kbd>.](https://github.com/sindresorhus/refined-github/issues/1590)
 - [](# "vertical-front-matter") [Shows Markdown front matter as vertical table.](https://user-images.githubusercontent.com/44045911/87251695-26069b00-c4a0-11ea-9077-53ce366490ed.png)
-- [](# "list-pr-for-branch") [Shows PRs that touch the current branch.](https://user-images.githubusercontent.com/55841/60622834-879e1f00-9de1-11e9-9a9e-bae5ec0b3728.png)
+- [](# "list-pr-for-branch") [On branch commit lists, shows the PR that touch the current branch.](https://user-images.githubusercontent.com/16872793/119760295-b8751a80-be77-11eb-87da-91d0c403bb49.png)
 
 <!-- Refer to style guide above. Keep this message between sections. -->
 
--- source/features/list-pr-for-branch.tsx
+++ source/features/list-pr-for-branch.tsx
@@ -8,6 +8,14 @@ import getDefaultBranch from '../github-helpers/get-default-branch';
 import {addAfterBranchSelector} from './latest-tag-button';
 import {getPullRequestsAssociatedWithBranch} from './show-associated-branch-prs-on-fork';
 
+// Taken from https://github.com/fregante/github-issue-link-status/blob/98792f2837352bacbf80664f3edbcec8e579ed17/source/github-issue-link-status.js#L10
+const stateColorMap = {
+	OPEN: 'text-green color-text-success',
+	CLOSED: 'text-red color-text-danger',
+	MERGED: 'text-purple color-purple-5',
+	DRAFT: ''
+};
+
 // TODO remove this after #4196 is fixed
 function getCurrentBranch(): string {
 	const feedLink = select.last('link[type="application/atom+xml"]')!;
@@ -19,13 +27,6 @@ function getCurrentBranch(): string {
 		.replace(/\.atom$/, '');
 }
 
-// Taken from https://github.com/fregante/github-issue-link-status/blob/98792f2837352bacbf80664f3edbcec8e579ed17/source/github-issue-link-status.js#L10
-const stateColorMap = {
-	OPEN: 'text-green color-text-success',
-	CLOSED: 'text-red color-text-danger',
-	MERGED: 'text-purple color-purple-5',
-	DRAFT: ''
-};
 async function init(): Promise<void | false> {
 	const currentBranch = getCurrentBranch();
 
@@ -44,7 +45,7 @@ async function init(): Promise<void | false> {
 		<a
 			data-issue-and-pr-hovercards-enabled
 			href={prInfo.url}
-			className="btn btn-outline flex-self-center rgh-list-pr-for-branch"
+			className="btn flex-self-center rgh-list-pr-for-branch"
 			data-hovercard-type="pull_request"
 			data-hovercard-url={prInfo.url + '/hovercard'}
 		>
