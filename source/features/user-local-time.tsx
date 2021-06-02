4442fb2b7019338b5aa395c2e04eed6b6d7ef3df
cheap-glitch <cheap.glitch@gmail.com>
2021-06-02 14:16:29 +0200

    Use `selector-observer` to de-duplicate handler calls

--- source/features/user-local-time.tsx
+++ source/features/user-local-time.tsx
@@ -5,12 +5,12 @@ import cache from 'webext-storage-cache';
 import delay from 'delay';
 import select from 'select-dom';
 import onetime from 'onetime';
+import {observe} from 'selector-observer';
 import {ClockIcon} from '@primer/octicons-react';
 
 import features from '.';
 import * as api from '../github-helpers/api';
 import {getUsername} from '../github-helpers';
-import observeElement from '../helpers/simplified-element-observer';
 
 interface Commit {
 	url: string;
@@ -78,67 +78,68 @@ function parseOffset(date: string): number {
 }
 
 function init(): void {
-	const hovercard = select('.js-hovercard-content > .Popover-message')!;
+	observe('.js-hovercard-content div.d-flex.mt-3 > div.overflow-hidden.ml-3:not(.rgh-user-local-time-added)', {
+		add: async hovercardContainer => {
+			hovercardContainer.classList.add('rgh-user-local-time-added');
 
-	observeElement(hovercard, async ([mutation]) => {
-		if (
-			mutation?.addedNodes?.length !== 1 || // Avoid duplicated time element issue #3634
-			!select.exists('[data-hydro-view*="user-hovercard-hover"]', hovercard) // It's not the hovercard type we expect
-		) {
-			return;
-		}
-
-		const login = select('a[data-octo-dimensions="link_type:profile"]', hovercard)?.pathname.slice(1);
-		if (!login || login === getUsername()) {
-			return;
-		}
-
-		const datePromise = getLastCommitDate(login);
-		const race = await Promise.race([delay(300), datePromise]);
-		if (race === false) {
-			// The timezone was undeterminable and this resolved "immediately" (or was cached), so don't add the icon at all
-			return;
-		}
-
-		const placeholder = <span>Guessing local time…</span>;
-		const container = (
-			<div className="rgh-local-user-time mt-2 text-gray color-text-secondary text-small">
-				<ClockIcon/> {placeholder}
-			</div>
-		);
-
-		// Adding the time element might change the height of the hovercard and thus break its positioning
-		const hovercardHeight = hovercard.offsetHeight;
-
-		select('div.d-flex.mt-3 > div.overflow-hidden.ml-3', hovercard)!.append(container);
-
-		if (hovercard.matches('.Popover-message--bottom-right, .Popover-message--bottom-left')) {
-			const diff = hovercard.offsetHeight - hovercardHeight;
-			if (diff > 0) {
-				const parent = hovercard.parentElement!;
-				const top = Number.parseInt(parent.style.top, 10);
-				parent.style.top = `${top - diff}px`;
+			const hovercard = hovercardContainer.closest<HTMLElement>('.Popover-message')!;
+			if (!select.exists('[data-hydro-view*="user-hovercard-hover"]', hovercard)) {
+				// It's not the hovercard type we expect
+				return;
 			}
+
+			const login = select('a[data-octo-dimensions="link_type:profile"]', hovercard)?.pathname.slice(1);
+			if (!login || login === getUsername()) {
+				return;
+			}
+
+			const datePromise = getLastCommitDate(login);
+			const race = await Promise.race([delay(300), datePromise]);
+			if (race === false) {
+				// The timezone was undeterminable and this resolved "immediately" (or was cached), so don't add the icon at all
+				return;
+			}
+
+			const placeholder = <span>Guessing local time…</span>;
+			const container = (
+				<div className="mt-2 text-gray color-text-secondary text-small">
+					<ClockIcon/> {placeholder}
+				</div>
+			);
+
+			// Adding the time element might change the height of the hovercard and thus break its positioning
+			const hovercardHeight = hovercard.offsetHeight;
+
+			hovercardContainer.append(container);
+
+			if (hovercard.matches('.Popover-message--bottom-right, .Popover-message--bottom-left')) {
+				const diff = hovercard.offsetHeight - hovercardHeight;
+				if (diff > 0) {
+					const parent = hovercard.parentElement!;
+					const top = Number.parseInt(parent.style.top, 10);
+					parent.style.top = `${top - diff}px`;
+				}
+			}
+
+			const date = await datePromise;
+			if (!date) {
+				placeholder.textContent = 'Timezone unknown';
+				container.title = 'Timezone couldn’t be determined from their last commits';
+				return;
+			}
+
+			const userTime = new Date();
+			userTime.setMinutes(parseOffset(date) + userTime.getTimezoneOffset() + userTime.getMinutes());
+
+			const timeFormatter = new Intl.DateTimeFormat(undefined, {
+				hour: 'numeric',
+				minute: 'numeric',
+				weekday: userTime.getDay() === new Date().getDay() ? undefined : 'long'
+			});
+
+			placeholder.textContent = timeFormatter.format(userTime);
+			container.title = `Timezone guessed from their last commit: ${date}`;
 		}
-
-		const date = await datePromise;
-		if (!date) {
-			placeholder.textContent = 'Timezone unknown';
-			container.title = 'Timezone couldn’t be determined from their last commits';
-			return;
-		}
-
-		const userTime = new Date();
-		userTime.setMinutes(parseOffset(date) + userTime.getTimezoneOffset() + userTime.getMinutes());
-
-		const timeFormatter = new Intl.DateTimeFormat(undefined, {
-			hour: 'numeric',
-			minute: 'numeric',
-			weekday: userTime.getDay() === new Date().getDay() ? undefined : 'long'
-		});
-
-		placeholder.textContent = timeFormatter.format(userTime);
-		container.title = `Timezone guessed from their last commit: ${date}`;
 	});
 }
 
