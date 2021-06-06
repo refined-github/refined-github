5238c275f725c0f1320c409285f01bc27eed70a0
Matt Dziuban <mrdziuban@gmail.com>
2021-06-06 10:40:38 -0400

    Update source/features/repo-wide-file-finder.tsx
    Co-authored-by: yakov116 <16872793+yakov116@users.noreply.github.com>

--- source/features/repo-wide-file-finder.tsx
+++ source/features/repo-wide-file-finder.tsx
@@ -3,8 +3,8 @@ import select from 'select-dom';
 import * as pageDetect from 'github-url-detection';
 
 import features from '.';
-import {buildRepoURL, getCurrentCommittish} from '../github-helpers';
 import getDefaultBranch from '../github-helpers/get-default-branch';
+import {buildRepoURL, getCurrentCommittish} from '../github-helpers';
 
 async function init(): Promise<void> {
 	document.body.append(
