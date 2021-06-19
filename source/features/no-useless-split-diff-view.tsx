40474d614f6502da3fbb39e39721be9f3a70413d
Yufan You <ouuansteve@gmail.com>
2021-06-19 23:36:07 +0800

    Use `rgh-no-useless-split-diff-view-visited` to prevent duplicate visits

--- source/features/no-useless-split-diff-view.tsx
+++ source/features/no-useless-split-diff-view.tsx
@@ -13,7 +13,8 @@ function isUnifiedDiff(): boolean {
 }
 
 function init(): void {
-	for (const diffTable of select.all('.js-diff-table')) {
+	for (const diffTable of select.all('.js-diff-table:not(.rgh-no-useless-split-diff-view-visited)')) {
+		diffTable.classList.add('rgh-no-useless-split-diff-view-visited');
 		for (const side of ['left', 'right']) {
 			if (!select.exists(`[data-split-side="${side}"]:is(.blob-code-addition, .blob-code-deletion)`, diffTable)) {
 				// eslint-disable-next-line unicorn/prefer-dom-node-dataset -- CSS file has the same selector, this can be grepped
