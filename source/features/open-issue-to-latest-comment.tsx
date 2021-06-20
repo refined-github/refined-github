944144c18c312698be9c53156e55075b57d314ea
yakov116 <16872793+yakov116@users.noreply.github.com>
2021-06-20 10:51:20 -0400

    Remove `open-issue-to-latest-comment` from the dashboard

--- source/features/open-issue-to-latest-comment.tsx
+++ source/features/open-issue-to-latest-comment.tsx
@@ -1,11 +1,7 @@
-import React from 'dom-chef';
 import select from 'select-dom';
-import onetime from 'onetime';
-import {observe} from 'selector-observer';
 import * as pageDetect from 'github-url-detection';
 
 import features from '.';
-import {wrapAll} from '../helpers/dom-utils';
 
 function init(): void {
 	for (const link of select.all('.js-issue-row a[aria-label*="comment"], .js-pinned-issue-list-item a[aria-label*="comment"]')) {
@@ -13,24 +9,9 @@ function init(): void {
 	}
 }
 
-function initDashboard(): void {
-	observe('.js-recent-activity-container :not(a) > div > .octicon-comment', {
-		add(icon) {
-			const url = icon.closest('li')!.querySelector('a')!.pathname + '#partial-timeline';
-			icon.parentElement!.classList.remove('col-1'); // Also fix extra space added by GitHub #3174
-			wrapAll([icon, icon.nextSibling!], <a className="muted-link Link--muted" href={url}/>);
-		}
-	});
-}
-
 void features.add(__filebasename, {
 	include: [
 		pageDetect.isConversationList
 	],
 	init
-}, {
-	include: [
-		pageDetect.isDashboard
-	],
-	init: onetime(initDashboard)
 });
