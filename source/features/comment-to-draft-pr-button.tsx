e4cc15406a5fb727b76ccbc4d405cab3ab73405a
darkred <rick3162@gmail.com>
2021-06-27 19:54:30 +0300

    Replace `innerHTML` with `textContent`

--- source/features/comment-to-draft-pr-button.tsx
+++ source/features/comment-to-draft-pr-button.tsx
@@ -11,12 +11,12 @@ function handleClicks(): void {
 		'table:not([style="display: none"]) .add-comment-label' // For when adding a single comment after having started a review
 	]);
 	for (const element of buttonsSingleComment) {
-		element.innerHTML = ' Add single comment to Draft PR ';
+		element.textContent = 'Add single comment to Draft PR';
 	}
 
 	const buttonsStartReview = select.all('table:not([style="display: none"]) .start-review-label');
 	for (const element of buttonsStartReview) {
-		element.innerHTML = ' Start a review to Draft PR ';
+		element.textContent = 'Start a review to Draft PR';
 	}
 }
 
@@ -24,7 +24,7 @@ function init(): void {
 	let commentButton;
 	if (pageDetect.isPRConversation()) {
 		commentButton = select('#partial-new-comment-form-actions .btn-primary');
-		commentButton!.innerHTML = 'Comment to Draft PR';
+		commentButton!.textContent = 'Comment to Draft PR';
 	} else if (pageDetect.isPRFiles()) {
 		delegate('.diff-table', 'button', 'click', handleClicks);
 	}
