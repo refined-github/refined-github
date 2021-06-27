be36f7f56187475de06ac3fec9e068ef6f56d946
darkred <rick3162@gmail.com>
2021-06-27 18:23:30 +0300

    Followed all suggestions + updated screenshot
    - Followed all suggestions by yakov116 and fregante
    - Updated the feature screenshot to include the Files tab

--- source/features/comment-to-draft-pr-button.tsx
+++ source/features/comment-to-draft-pr-button.tsx
@@ -1,19 +1,42 @@
 import select from 'select-dom';
+import delegate from 'delegate-it';
 import * as pageDetect from 'github-url-detection';
 
 import features from '.';
 import onNewComments from '../github-events/on-new-comments';
 
+function handleClicks(event: delegate.Event<MouseEvent, HTMLButtonElement>): void {
+	const buttonsSingleComment = select.all([
+		'table:not([style="display: none"]) .review-simple-reply-button',
+		'table:not([style="display: none"]) .add-comment-label' // For when adding a single comment after having started a review
+	]);
+	for (const element of buttonsSingleComment) {
+		element.innerHTML = ' Add single comment to Draft PR ';
+	}
+
+	const buttonsStartReview = select.all('table:not([style="display: none"]) .start-review-label');
+	for (const element of buttonsStartReview) {
+		element.innerHTML = ' Start a review to Draft PR ';
+	}
+}
+
 function init(): void {
-	if (select.exists('#partial-discussion-header span[title="Status: Draft"]')) {
-		const commentButton = select('#partial-new-comment-form-actions > div > div.color-bg-secondary.ml-1 > button');
+	let commentButton;
+	if (pageDetect.isPRConversation()) {
+		commentButton = select('#partial-new-comment-form-actions .btn-primary');
 		commentButton!.innerHTML = 'Comment to Draft PR';
+	} else if (pageDetect.isPRFiles()) {
+		delegate('.diff-table', 'button', 'click', handleClicks);
 	}
 }
 
 void features.add(__filebasename, {
 	include: [
-		pageDetect.isPRConversation
+		pageDetect.isPRConversation,
+		pageDetect.isPRFiles
+	],
+	exclude: [
+		() => !pageDetect.isDraftPR
 	],
 	additionalListeners: [
 		onNewComments
