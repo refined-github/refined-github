d272354ea1f72642103366cb6a05bed627431764
Mahdyar Hasanpour <hi@mahdyar.me>
2021-07-03 18:22:44 +0430

    feat: use url and show selected mode

--- source/features/view-markdown-source.tsx
+++ source/features/view-markdown-source.tsx
@@ -6,17 +6,30 @@ import {CodeIcon, FileIcon} from '@primer/octicons-react';
 import features from '.';
 
 async function init(): Promise<void> {
-	const URLWithoutSearchParameters = window.location.href.split('?')[0];
 	select('#raw-url')!.closest('.d-flex')!.prepend(
 		<div className="BtnGroup">
-			<a href="?plain=1" className="btn btn-sm BtnGroup-item tooltipped tooltipped-nw rgh-md-source" type="button" aria-label="Display the source blob">
+			<a href="" className="btn btn-sm BtnGroup-item tooltipped tooltipped-nw rgh-md-source" type="button" aria-label="Display the source blob">
 				<CodeIcon/>
 			</a>
-			<a href={URLWithoutSearchParameters} className="btn btn-sm BtnGroup-item tooltipped tooltipped-nw rgh-md-rendered" type="button" aria-label="Display the rendered blob">
+			<a href="" className="btn btn-sm BtnGroup-item tooltipped tooltipped-nw rgh-md-rendered" type="button" aria-label="Display the rendered blob">
 				<FileIcon/>
 			</a>
 		</div>
 	);
+	const currentURL = new URL(window.location.href);
+	const sourceButton = select('a.rgh-md-source')!;
+	const renderedButton = select('a.rgh-md-rendered')!;
+	if (currentURL.searchParams.has('plain') && currentURL.searchParams.get('plain') === '1') {
+		sourceButton.href = currentURL.toString();
+		currentURL.searchParams.delete('plain');
+		renderedButton.href = currentURL.toString();
+		sourceButton.className += ' selected';
+	} else {
+		renderedButton.href = currentURL.toString();
+		currentURL.searchParams.append('plain', '1');
+		sourceButton.href = currentURL.toString();
+		renderedButton.className += ' selected';
+	}
 }
 
 void features.add(__filebasename, {
