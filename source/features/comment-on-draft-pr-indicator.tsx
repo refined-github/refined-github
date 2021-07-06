b385a8473a9c3eba90dbcb8538e00e3777b9541e
Federico <me@fregante.com>
2021-07-07 00:04:30 +0700

    Do what the reviewers say

--- source/features/comment-on-draft-pr-indicator.tsx
+++ source/features/comment-on-draft-pr-indicator.tsx
@@ -1,45 +1,40 @@
 import select from 'select-dom';
-import delegate from 'delegate-it';
 import * as pageDetect from 'github-url-detection';
 
 import features from '.';
-import onNewComments from '../github-events/on-new-comments';
 
-function handleClicks(): void {
+function initPRFiles(): void {
 	const buttonsSingleComment = select.all([
 		'.review-simple-reply-button',
 		'.add-comment-label' // For when adding a single comment after having started a review
 	]);
 	for (const element of buttonsSingleComment) {
-		element.textContent = 'Add single comment to draft PR';
+		element.textContent += ' to draft PR';
 	}
 
-	const buttonsStartReview = select.all('.start-review-label');
-	for (const element of buttonsStartReview) {
-		element.textContent = 'Start a review on draft PR';
+	for (const element of select.all('.start-review-label')) {
+		element.textContent += ' on draft PR';
 	}
 }
 
-function init(): void {
-	let commentButton;
-	if (pageDetect.isPRConversation()) {
-		commentButton = select('#partial-new-comment-form-actions .btn-primary');
-		commentButton!.textContent = 'Comment on draft PR';
-	} else if (pageDetect.isPRFiles()) {
-		delegate('.diff-table', 'button', 'click', handleClicks);
-	}
+function initConversation(): void {
+	select('#partial-new-comment-form-actions .btn-primary')!.textContent = 'Comment on draft PR';
 }
 
 void features.add(__filebasename, {
 	include: [
-		pageDetect.isPRConversation,
 		pageDetect.isPRFiles
 	],
 	exclude: [
 		() => !pageDetect.isDraftPR()
 	],
-	additionalListeners: [
-		onNewComments
+	init: initPRFiles
+}, {
+	include: [
+		pageDetect.isPRConversation,
 	],
-	init
+	exclude: [
+		() => !pageDetect.isDraftPR()
+	],
+	init: initConversation
 });
