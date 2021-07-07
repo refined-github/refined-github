3c7788adae1f3d9bbfaa33f22808673d9aabcd97
Federico <me@fregante.com>
2021-07-07 17:12:18 +0700

    Also support review threads on the Conversation tab

--- source/features/comment-on-draft-pr-indicator.tsx
+++ source/features/comment-on-draft-pr-indicator.tsx
@@ -4,12 +4,12 @@ import * as pageDetect from 'github-url-detection';
 import features from '.';
 import onReplacedElement from '../helpers/on-replaced-element';
 
-function addIndicator(button: HTMLElement) {
+function addIndicator(button: HTMLElement): void {
 	const preposition = button.textContent!.includes('Add') ? ' to ' : ' on ';
 	button.textContent += preposition + 'draft PR';
 }
 
-function initPRFiles(): void {
+function init(): void {
 	const buttons = select.all([
 		'.review-simple-reply-button', // "Add single comment" button
 		'.add-comment-label', // "Add review comment" button
@@ -18,27 +18,20 @@ function initPRFiles(): void {
 	for (const button of buttons) {
 		addIndicator(button);
 	}
-}
 
-function initConversation(): void {
-	// The button is part of a .js-updatable-content partial
-	void onReplacedElement('#partial-new-comment-form-actions .btn-primary', addIndicator, true);
+	if (pageDetect.isConversation()) {
+		// The button is part of a .js-updatable-content partial
+		void onReplacedElement('#partial-new-comment-form-actions .btn-primary', addIndicator, true);
+	}
 }
 
 void features.add(__filebasename, {
 	include: [
+		pageDetect.isPRConversation,
 		pageDetect.isPRFiles
 	],
 	exclude: [
 		() => !pageDetect.isDraftPR()
 	],
-	init: initPRFiles
-}, {
-	include: [
-		pageDetect.isPRConversation
-	],
-	exclude: [
-		() => !pageDetect.isDraftPR()
-	],
-	init: initConversation
+	init
 });
