30ecddfc72a0eca5d3d67481addf91001f5105af
Federico Brigante <me@fregante.com>
2021-07-09 23:57:28 +0700

    Revert "Review"

--- source/features/remove-unused-repo-tabs.tsx
+++ source/features/remove-unused-repo-tabs.tsx
@@ -12,10 +12,6 @@ import abbreviateNumber from '../helpers/abbreviate-number';
 import {onlyShowInDropdown} from './more-dropdown';
 import {buildRepoURL, getRepo} from '../github-helpers';
 
-async function canUserEditOrganization(): Promise<boolean> {
-	return Boolean(await elementReady('.btn-primary[href$="repositories/new"]'));
-}
-
 function mustKeepTab(tab: HTMLElement | undefined): boolean {
 	return (
 		!tab // Tab disabled 🎉
@@ -80,8 +76,8 @@ async function initProjects(): Promise<void | false> {
 		return;
 	}
 
-	if (await canUserEditOrganization()) {	
-		// Leave Project tab visible to those who can create a new project
+ 	// .isOrganizationProfile
+ 	if (await elementReady('a.btn-primary[href$="repositories/new"]')) {
 		return;
 	}
 
