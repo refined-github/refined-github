76117c6de6f9b875e768fbd51964d4d057d45c1a
yakov116 <16872793+yakov116@users.noreply.github.com>
2021-07-11 14:26:24 -0400

    Use PageDetect

--- source/features/remove-unused-repo-tabs.tsx
+++ source/features/remove-unused-repo-tabs.tsx
@@ -12,10 +12,6 @@ import abbreviateNumber from '../helpers/abbreviate-number';
 import {onlyShowInDropdown} from './more-dropdown';
 import {buildRepoURL, getRepo} from '../github-helpers';
 
-async function canUserEditOrganization(): Promise<boolean> {
-	return Boolean(await elementReady('.btn-primary[href$="repositories/new"]'));
-}
-
 function mustKeepTab(tab: HTMLElement | undefined): boolean {
 	return (
 		!tab // Tab disabled 🎉
@@ -79,7 +75,7 @@ async function initProjects(): Promise<void | false> {
 		return;
 	}
 
-	if (await canUserEditOrganization()) {
+	if (pageDetect.canUserEditOrganization()) {
 		// Leave Project tab visible to those who can create a new project
 		return;
 	}
