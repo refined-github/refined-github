176c6198dbb39cfb596bc059ab55b85785fd3a2b
Federico Brigante <me@fregante.com>
2021-07-26 23:44:14 +0700

    No 160-char long lines

--- source/features/bugs-tab.tsx
+++ source/features/bugs-tab.tsx
@@ -130,7 +130,8 @@ async function init(): Promise<void | false> {
 	bugsCounter.title = '';
 
 	// Update Bugs’ link
-	new SearchQuery(bugsTab).add(`label:${await getBugLabel() ?? 'bug'}`); // TODO[2021-8-15] Drop `?? 'bug' its only needed until `countPromise` refreshed one time
+	// TODO[2021-8-15] Drop `?? 'bug'`, it's only needed until `countPromise` refreshes one time
+	new SearchQuery(bugsTab).add(`label:${await getBugLabel() ?? 'bug'}`);
 
 	// In case GitHub changes its layout again #4166
 	if (issuesTab.parentElement!.tagName === 'LI') {
