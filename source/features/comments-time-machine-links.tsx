179014950266cb0ed9d41e748dae1db59a8b185d
Kid <kidonng@gmail.com>
2021-08-04 21:56:09 +0800

    Lint

--- source/features/comments-time-machine-links.tsx
+++ source/features/comments-time-machine-links.tsx
@@ -105,7 +105,18 @@ async function showTimemachineBar(): Promise<void | false> {
 	}
 
 	addNotice(
-		<>You can also <a className="rgh-link-date" href={String(url)} data-pjax="#repo-content-pjax-container">view this object as it appeared at the time of the comment</a> (<relative-time datetime={date}/>)</>,
+		<>
+			You can also
+			{' '}
+			<a
+				className="rgh-link-date"
+				href={String(url)}
+				data-pjax="#repo-content-pjax-container"
+			>
+				view this object as it appeared at the time of the comment
+			</a>
+			(<relative-time datetime={date}/>)
+		</>,
 	);
 }
 
