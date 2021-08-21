1fe90da1761a61301f134bfc36d5e4a878fe5af8
Vedant K <gamemaker0042@gmail.com>
2021-08-21 12:59:37 +0530

    refactor: remove duplicate condition

--- source/features/follow-file-renames.tsx
+++ source/features/follow-file-renames.tsx
@@ -46,30 +46,28 @@ async function linkify(button: HTMLButtonElement, filePath: string): Promise<voi
 	const sha = (isNewer ? select : select.last)('clipboard-copy[aria-label="Copy the full SHA"]')!;
 
 	const fileChanges = await getChangesToFileInCommit(sha.getAttribute('value')!, filePath);
-	if (fileChanges?.file?.status !== 'renamed') {
+	if (fileChanges?.file.status !== 'renamed') {
 		return;
 	}
 
-	if (fileChanges.file.status === 'renamed') {
-		const linkifiedURL = new GitHubURL(location.href);
-		linkifiedURL.assign({
-			route: 'commits',
-			filePath: fileChanges.file[toKey],
-			// Clear the search from the url, so it does not get passed to the rename link
-			search: '',
-		});
-		button.replaceWith(
-			<a
-				href={String(linkifiedURL)}
-				aria-label={`Renamed ${isNewer ? 'to' : 'from'} ${fileChanges.file[toKey]!}`}
-				className="btn btn-outline BtnGroup-item tooltipped tooltipped-n tooltipped-no-delay"
-			>
-				{isNewer && <DiffRenamedIcon className="mr-1" style={{transform: 'rotate(180deg)'}}/>}
-				{button.textContent}
-				{!isNewer && <DiffRenamedIcon className="ml-1"/>}
-			</a>,
-		);
-	}
+	const linkifiedURL = new GitHubURL(location.href);
+	linkifiedURL.assign({
+		route: 'commits',
+		filePath: fileChanges.file[toKey],
+		// Clear the search from the url, so it does not get passed to the rename link
+		search: '',
+	});
+	button.replaceWith(
+		<a
+			href={String(linkifiedURL)}
+			aria-label={`Renamed ${isNewer ? 'to' : 'from'} ${fileChanges.file[toKey]!}`}
+			className="btn btn-outline BtnGroup-item tooltipped tooltipped-n tooltipped-no-delay"
+		>
+			{isNewer && <DiffRenamedIcon className="mr-1" style={{transform: 'rotate(180deg)'}}/>}
+			{button.textContent}
+			{!isNewer && <DiffRenamedIcon className="ml-1"/>}
+		</a>,
+	);
 }
 
 function init(): void | false {
