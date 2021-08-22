d9621aaf54aae286b45b8805d34d9354224412ca
Vedant K <gamemaker0042@gmail.com>
2021-08-22 21:19:59 +0530

    feat: add `verify-features` script to replace the features CI action

---
+++ build/verify-features.ts
@@ -0,0 +1,47 @@
+import path from 'node:path'
+import {readdirSync as readDirSync, readFileSync} from 'node:fs'
+import { getFeaturesMeta } from './readme-parser'
+
+function getFileExtension(fileName: string): string | void {
+	const splitFileName = fileName.split('.')
+	if (splitFileName.length == 1) return
+	
+	return splitFileName[splitFileName.length - 1]
+}
+
+const featuresDirContents = readDirSync(path.join(__dirname, '../source/features/'));
+const refinedGithubTs = readFileSync(path.join(__dirname, '../source/refined-github.ts')).toString('utf-8');
+const featuresInReadme = getFeaturesMeta()
+
+let errors: string[] = []
+
+for (let fileName of featuresDirContents) {
+	const fileExt = getFileExtension(fileName)
+	if (fileExt === 'css' || fileName === 'index.tsx' || fileName.includes('rgh')) {
+		continue;
+	}
+	fileName = fileName.replace('.tsx', '').replace('.ts', '')
+	
+	const featureMeta = featuresInReadme.find((feature) => feature.id === fileName)
+	if (!featureMeta) {
+		errors.push(`readme: The feature ${fileName} is not included in the readme.`)
+		continue
+	}
+
+	if (featureMeta!.description.length < 20) {
+		errors.push(`desc: The description for ${featureMeta!.id} is less than 20 characters. Try explaining it better!`)
+	}
+
+	if (!refinedGithubTs.includes(featureMeta!.id)) {
+		errors.push(`import: The feature ${featureMeta!.id} has not been imported in \`/sources/refined-github.ts\`.`)
+	}
+}
+
+if (errors.length > 0) {
+	for (const error of errors) {
+		console.error('ERR:', error)
+	}
+	process.exit(1)
+} else {
+	console.info('All features verified!')
+}
