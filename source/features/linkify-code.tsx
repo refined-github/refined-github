57be5769d3f72373bd05f3fdfa5f6c225896fc7e
Vedant K <gamemaker0042@gmail.com>
2021-08-24 18:51:20 +0530

    fix: linkify URLs in markdown code blocks as well

--- source/features/linkify-code.tsx
+++ source/features/linkify-code.tsx
@@ -18,7 +18,8 @@ function initTitle(): void {
 function init(): void {
 	const selectors = [
 		'.js-blob-wrapper tr:not(.inline-comments)', // File blocks in pages like `isPRFiles`, `isSingleFile`
-		'.blob-wrapper tr', // Every other code blocks
+		'.blob-wrapper tr', // File blocks in blob URLs
+		'.comment-body' // Markdown code blocks
 	].map(selector => selector + `:not(.${linkifiedURLClass})`).join(',');
 
 	observe(selectors, {
