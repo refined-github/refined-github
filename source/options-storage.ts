488c63e3a796beb51cb7598c6276cf5fe3ad4a9b
darkred <rick3162@gmail.com>
2021-08-26 23:22:07 +0300

    Add `// Merged in August`

--- source/options-storage.ts
+++ source/options-storage.ts
@@ -28,10 +28,10 @@ const migrations = [
 	featureWasRenamed('more-dropdown', 'clean-repo-tabs'), // Merged in July
 	featureWasRenamed('remove-diff-signs', 'hide-diff-signs'), // Merged in August
 	featureWasRenamed('remove-label-faster', 'hide-label-faster'), // Merged in August
-	featureWasRenamed('hide-useless-comments', 'hide-noise-comments'),
-	featureWasRenamed('hide-useless-newsfeed-events', 'hide-noise-newsfeed-events'),
-	featureWasRenamed('no-useless-split-diff-view', 'no-noise-split-diff-view'),
-	featureWasRenamed('unwrap-useless-dropdowns', 'unwrap-twofold-dropdowns'),
+	featureWasRenamed('hide-useless-comments', 'hide-noise-comments'), // Merged in August
+	featureWasRenamed('hide-useless-newsfeed-events', 'hide-noise-newsfeed-events'), // Merged in August
+	featureWasRenamed('no-useless-split-diff-view', 'no-noise-split-diff-view'), // Merged in August
+	featureWasRenamed('unwrap-useless-dropdowns', 'unwrap-twofold-dropdowns'), // Merged in August
 
 	// Removed features will be automatically removed from the options as well
 	OptionsSyncPerDomain.migrations.removeUnused,
