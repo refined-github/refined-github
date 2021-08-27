d51f0d693397288c4664dbd26ea5f605cc76feb9
Federico Brigante <me@fregante.com>
2021-08-27 17:38:18 +0700

    Use map callback instead

--- build/verify-features.ts
+++ build/verify-features.ts
@@ -10,64 +10,45 @@ const entryPointSource = readFileSync(entryPoint);
 const importedFeatures = getFeatures();
 const featuresInReadme = getFeaturesMeta();
 
-const errors: string[] = [];
-
-function checkIfCssFileIsImported(fileName: string): string | null {
-	if (!entryPointSource.includes(`import './features/${fileName}';`)) {
-		const correspondingTsxFile = `source/features/${fileName.replace(/.css$/, '.tsx')}`;
-
-		if (existsSync(correspondingTsxFile)) {
-			if (!readFileSync(correspondingTsxFile).includes(`import './${fileName}';`)) {
-				return `ERR: The file \`${fileName}\` is not imported \`${correspondingTsxFile}\``;
-			}
-		} else {
-			return `ERR: The file \`${fileName}\` is not imported in \`${entryPoint}\``;
-		}
-	}
-
-	return null;
-}
-
-for (const fileName of featuresDirContents) {
+function findError(fileName): string | void {
 	if (fileName === 'index.tsx') {
 		continue;
 	}
 
 	if (fileName.endsWith('.css')) {
-		const error = checkIfCssFileIsImported(fileName);
-
-		if (error) {
-			errors.push(error);
+		const correspondingTsxFile = `source/features/${fileName.replace(/.css$/, '.tsx')}`;
+		if (existsSync(correspondingTsxFile) && !readFileSync(correspondingTsxFile).includes(`import './${fileName}';`)) {
+			return `ERR: \`${fileName}\` should be imported by \`${correspondingTsxFile}\``;
 		}
-
-		continue;
+		
+		if (!entryPointSource.includes(`import './features/${fileName}';`)) {
+			return `ERR: \`${fileName}\` should be imported by \`${entryPoint}\``;
+		}
 	}
 
 	if (!fileName.endsWith('.tsx')) {
-		errors.push(`ERR: The \`/source/features\` folder should only contain .css and .tsx files. File \`${fileName}\` violates that rule`);
-		continue;
+		return `ERR: The \`/source/features\` folder should only contain .css and .tsx files. File \`${fileName}\` violates that rule`;
 	}
 
 	const featureId = fileName.replace('.tsx', '');
 	if (!importedFeatures.includes(featureId as FeatureID)) {
-		errors.push(`ERR: ${featureId} should be imported by \`${entryPoint}\``);
+		return `ERR: ${featureId} should be imported by \`${entryPoint}\``;
 	}
 
 	if (fileName.startsWith('rgh-')) {
-		continue;
+		return;
 	}
 
 	const featureMeta = featuresInReadme.find(feature => feature.id === featureId);
 	if (!featureMeta) {
-		errors.push(`ERR: The feature ${featureId} should be described in the readme`);
-		continue;
+		return `ERR: The feature ${featureId} should be described in the readme`;
 	}
 
 	if (featureMeta.description.length < 20) {
-		errors.push(`ERR: ${featureId} should be described better in the readme (at least 20 characters)`);
+		return `ERR: ${featureId} should be described better in the readme (at least 20 characters)`;
 	}
 }
 
+const errors = featuresDirContents.map(name => findError(name)).filter(Boolean);
 console.error(errors.join('\n'));
-
 process.exitCode = errors.length;
