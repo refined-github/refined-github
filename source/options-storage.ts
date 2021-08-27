e80eafd85c0a7acf65d509f04fc2f66cab8702aa
Federico Brigante <me@fregante.com>
2021-08-27 16:46:24 +0700

    Drop `collapse-wiki-sections` migration

--- source/options-storage.ts
+++ source/options-storage.ts
@@ -20,7 +20,6 @@ const defaults = Object.assign({
 
 // TODO[2021-10-01]: Drop classes `muted-link`, `link-gray`, `link-gray-dark`, `text-gray`, `text-gray-light`, `text-gray-dark`, `text-green`, `text-red` `text-blue` #4021
 const migrations = [
-	featureWasRenamed('collapse-markdown-sections', 'collapse-wiki-sections'), // Merged in May
 	featureWasRenamed('separate-draft-pr-button', 'one-click-pr-or-gist'), // Merged in May
 	featureWasRenamed('prevent-pr-commit-link-loss', 'prevent-link-loss'), // Merged in May
 	featureWasRenamed('remove-projects-tab', 'remove-unused-repo-tabs'), // Merged in July
