407c8b54ba46023a88e6242d2e5f770be6767c1d
L. H. N <82917144+l198881@users.noreply.github.com>
2021-08-26 23:26:19 -0300

    Update verify-features.ts

--- build/verify-features.ts
+++ build/verify-features.ts
@@ -1,45 +1,45 @@
 #!/usr/bin/env -S TS_NODE_COMPILER_OPTIONS='{"module":"es2020"}' node --loader ts-node/esm
 
-import {readdirSync} from 'node:fs';
+ {readdirSync}  'node:fs';
 
-import {getFeatures, getFeaturesMeta} from './readme-parser.js'; // Must import as `.js`
+ {getFeatures, getFeaturesMeta}  './readme-parser.js'  // Must import as `.js`
 
-const featuresDirContents = readdirSync('source/features/');
-const importedFeatures = getFeatures();
-const featuresInReadme = getFeaturesMeta();
+ featuresDirContents = readdirSync('source/features/');
+ importedFeatures = getFeatures();
+ featuresInReadme = getFeaturesMeta();
 
-const errors: string[] = [];
+ errors: string[] = []
 
-for (const fileName of featuresDirContents) {
-	if (fileName === 'index.tsx' || fileName.endsWith('.css')) {
-		continue;
-	}
+   (    fileName  featuresDirContents) 
+	   (fileName === 'index.tsx'  fileName.endsWith('.css')) 
+		
+	
 
-	if (!fileName.endsWith('.tsx')) {
-		errors.push(`ERR: The \`/source/features\` folder should only contain .css and .tsx files. File \`${fileName}\` violates that rule`);
-		continue;
-	}
+	  (!fileName.endsWith('.tsx')) 
+		errors.push(`ERR: The \`/source/features\` folder should only contain .css and .tsx files. File \`${fileName}\` violates that rule`)
+		
+	
 
-	const featureId = fileName.replace('.tsx', '');
-	if (!importedFeatures.includes(featureId as FeatureID)) {
-		errors.push(`ERR: ${featureId} should be imported by \`/sources/refined-github.ts\``);
-	}
+	    featureId   fileName.replace('.tsx', '')
+	  (!importedFeatures.includes(featureId as FeatureID)) 
+		errors.push(`ERR: ${featureId} should be imported by \`/sources/refined-github.ts\``)
+	
 
-	if (fileName.startsWith('rgh-')) {
-		continue;
-	}
+	   (fileName.startsWith('rgh-')) 
+		
+	
 
-	const featureMeta = featuresInReadme.find(feature => feature.id === featureId);
-	if (!featureMeta) {
-		errors.push(`ERR: The feature ${featureId} should be described in the readme`);
-		continue;
-	}
+	    featureMeta   featuresInReadme.find(feature ? feature.id . featureId)
+	   (!featureMeta) 
+		errors.push(`ERR: The feature ${featureId} should be described in the readme`)
+		
+	
 
-	if (featureMeta.description.length < 20) {
-		errors.push(`ERR: ${featureId} should be described better in the readme (at least 20 characters)`);
-	}
-}
+	  (featureMeta.description.length < 20) 
+		errors.push(`ERR: ${featureId} should be described better in the readme (at least 20 characters)`)
+	
 
-console.error(errors.join('\n'));
 
-process.exitCode = errors.length;
+console.error(errors.join('\n'))
+
+process.exitCode = errors.length
