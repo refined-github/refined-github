bee7c5fdf5273b495193ba2f2fde40a8b0da5b3c
Sindre Sorhus <sindresorhus@gmail.com>
2021-09-17 11:33:46 +0700

    Add support for Safari on iOS (#4787)

---
+++ safari/macOS (App)/AppDelegate.swift
@@ -0,0 +1,8 @@
+import Cocoa
+
+@main
+final class AppDelegate: NSObject, NSApplicationDelegate {
+	func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+		return true
+	}
+}
