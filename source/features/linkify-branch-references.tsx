395862b22d89f70c766709993681468149d2e75f
Kid <kidonng@gmail.com>
2021-09-25 21:03:19 +0800

    Extend `linkify-branch-references` to `isEditingFile` page

--- source/features/linkify-branch-references.tsx
+++ source/features/linkify-branch-references.tsx
@@ -2,23 +2,29 @@ import React from 'dom-chef';
 import elementReady from 'element-ready';
 import * as pageDetect from 'github-url-detection';
 
-import {wrap} from '../helpers/dom-utils';
 import features from '.';
 import {buildRepoURL} from '../github-helpers';
 
 async function init(): Promise<void | false> {
-	const element = await elementReady('.branch-name');
+	const element = await elementReady(pageDetect.isQuickPR() ? '.branch-name' : '.commit-form .branch-name');
 	if (!element) {
 		return false;
 	}
 
 	const branchUrl = buildRepoURL('tree', element.textContent!);
-	wrap(element.closest('.branch-name')!, <a href={branchUrl} data-pjax="#repo-content-pjax-container"/>);
+	element.replaceWith(
+		<span className="commit-ref">
+			<a className="no-underline " href={branchUrl} data-pjax="#repo-content-pjax-container">
+				{element.textContent}
+			</a>
+		</span>,
+	);
 }
 
 void features.add(__filebasename, {
 	include: [
 		pageDetect.isQuickPR,
+		pageDetect.isEditingFile,
 	],
 	init,
 });
