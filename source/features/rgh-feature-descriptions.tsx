57e93dc49b2af9d302203aa8bbdece9731cf5d65
Federico Brigante <me@fregante.com>
2021-10-01 22:51:50 +0200

    Explain selector + drop unnecessary one

--- source/features/rgh-feature-descriptions.tsx
+++ source/features/rgh-feature-descriptions.tsx
@@ -13,13 +13,13 @@ async function init(): Promise<void | false> {
 
 	const conversationsUrl = '/sindresorhus/refined-github/issues?q=' + encodeURIComponent(`"${feature.id}" sort:updated-desc`);
 
-	const commitInfoBox = (await elementReady([
-		'.hx_commit-tease', // Selector for commit message
-		'.Box-header.Details', // Generic selector for entire commit box
-		'include-fragment.commit-loader', // Required according to 2683e30
-	].join(',')))!.parentElement!;
-	commitInfoBox.classList.add('width-fit', 'min-width-0', 'flex-auto', 'mb-lg-0', 'mr-lg-3');
-	commitInfoBox.classList.remove('flex-shrink-0');
+	const commit = await elementReady([
+		'.Box-header.Details', // Already loaded
+		'include-fragment.commit-loader', // Deferred loading
+	].join(','));
+	
+	commit!.parentElement!.classList.add('width-fit', 'min-width-0', 'flex-auto', 'mb-lg-0', 'mr-lg-3');
+	commit!.parentElement!.classList.remove('flex-shrink-0');
 
 	const featureInfoBox = (
 		<div className="Box rgh-feature-description" style={{flex: '0 1 544px'}}>
