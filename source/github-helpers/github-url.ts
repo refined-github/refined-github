ca2348d14680fc11edf7d1950c075d2594d99797
Kid <kidonng@gmail.com>
2021-10-07 17:48:08 +0800

    Remove most `toString()`
    Use `href` instead

--- source/github-helpers/github-url.ts
+++ source/github-helpers/github-url.ts
@@ -20,10 +20,6 @@ export default class GitHubURL {
 		this.pathname = this.internalUrl.pathname;
 	}
 
-	toString(): string {
-		return this.href;
-	}
-
 	assign(...replacements: Array<Partial<GitHubURL>>): this {
 		Object.assign(this, ...replacements);
 		return this;
