4ad5e6e576f12d090c2e389e6978f1371bf0f307
Florent <cheap.glitch@gmail.com>
2021-10-23 12:52:51 +0200

    Fix `show-names` on PR files tab (#4938)

--- source/features/show-names.tsx
+++ source/features/show-names.tsx
@@ -11,7 +11,8 @@ import {getUsername, compareNames} from '../github-helpers';
 
 async function init(): Promise<false | void> {
 	const usernameElements = select.all([
-		'.js-discussion a.author:not(.rgh-fullname, [href*="/apps/"], [href*="/marketplace/"], [data-hovercard-type="organization"])', // `a` selector needed to skip commits by non-GitHub users.
+		// `a` selector needed to skip commits by non-GitHub users.
+		':is(.js-discussion, .inline-comments) a.author:not(.rgh-fullname, [href*="/apps/"], [href*="/marketplace/"], [data-hovercard-type="organization"])',
 		'#dashboard a.text-bold[data-hovercard-type="user"]:not(.rgh-fullname)', // On dashboard `.text-bold` is required to not fetch avatars.
 	]);
 
