719b1597c875bcfa6d5f1572b5b5ec08015039b9
Florent <cheap.glitch@gmail.com>
2021-10-24 11:08:47 +0200

    Fix `quick-pr-diff-options` feature (#4971)

--- source/features/quick-pr-diff-options.tsx
+++ source/features/quick-pr-diff-options.tsx
@@ -83,7 +83,8 @@ function initPR(): false | void {
 	}
 
 	// Only show the native dropdown on medium and small screens #2597
-	select('.js-diff-settings')!.closest('details')!.classList.add('d-lg-none');
+	// TODO [2022-05-01]: Remove `.js-diff-settings` from the selector (kept for GHE)
+	select('.js-diff-settings, [aria-label="Diff settings"]')!.closest('details')!.classList.add('d-lg-none');
 
 	// Make space for the new button by removing "Changes from" #655
 	select('[data-hotkey="c"] strong')!.previousSibling!.remove();
