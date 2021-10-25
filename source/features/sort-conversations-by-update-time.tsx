dc6073483562fb4d0faf40cde4c5139c93702ae5
Kid <kidonng@gmail.com>
2021-10-26 01:29:32 +0800

    Sort projects by update time

--- source/features/sort-conversations-by-update-time.tsx
+++ source/features/sort-conversations-by-update-time.tsx
@@ -6,13 +6,20 @@ import SearchQuery from '../github-helpers/search-query';
 
 function init(): void {
 	// Get issues links that don't already have a specific sorting applied
-	const issueLinks = select.all('a:is([href*="/issues"], [href*="/pulls"]):not([href*="sort%3A"], .issues-reset-query)');
+	const issueLinks = select.all('a:is([href*="/issues"], [href*="/pulls"], [href*="/projects"]):not([href*="sort%3A"], .issues-reset-query)');
 	for (const link of issueLinks) {
 		// Pick only links to lists, not single issues
 		// + skip pagination links
 		// + skip pr/issue filter dropdowns (some are lazyloaded)
 		if (pageDetect.isConversationList(link) && link.host === location.host && !link.closest('.pagination, .table-list-filters')) {
 			new SearchQuery(link).add('sort:updated-desc');
+		}
+
+		if (pageDetect.utils.getRepositoryInfo(link)?.path === 'projects' && link.host === location.host && !link.closest('.table-list-header-toggle')) {
+			const search = new URLSearchParams(link.search);
+			const query = search.get('query') ?? 'is:open';
+			search.set('query', `${query} sort:updated-desc`);
+			link.search = search.toString();
 		}
 	}
 }
