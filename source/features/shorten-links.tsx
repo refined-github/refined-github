30a63d27d64bf678bb9199693766c2ce5c9e74bc
Kid <kidonng@gmail.com>
2021-10-26 02:44:11 +0800

    Shorten GitHub redirect domains

--- source/features/shorten-links.tsx
+++ source/features/shorten-links.tsx
@@ -1,16 +1,35 @@
 import onetime from 'onetime';
 import {observe} from 'selector-observer';
-import {applyToLink} from 'shorten-repo-url';
+import shortenURL from 'shorten-repo-url';
 import * as pageDetect from 'github-url-detection';
 
 import features from '.';
 import {linkifiedURLClass} from '../github-helpers/dom-formatters';
 
+const redirectDomains = new Set([
+	'github-redirect.dependabot.com', // Dependabot
+	'togithub.com', // Renovate
+]);
+
 function init(): void {
 	observe(`a[href]:not(.${linkifiedURLClass})`, {
 		constructor: HTMLAnchorElement,
 		add(link) {
-			applyToLink(link, location.href);
+			if (redirectDomains.has(link.hostname)) {
+				const {href} = link;
+				link.hostname = 'github.com';
+
+				if (
+					(href === link.textContent!.trim() || href === `${link.textContent!}/`)
+					&& !link.firstElementChild
+				) {
+					const shortened = shortenURL(link.href, location.href);
+					link.innerHTML = shortened;
+					return;
+				}
+			}
+
+			shortenURL.applyToLink(link, location.href);
 		},
 	});
 }
