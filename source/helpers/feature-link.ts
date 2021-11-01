1fa828cd168786e59ee86ab5e646d24d25bee8cb
Kid <kidonng@gmail.com>
2021-11-02 00:48:09 +0800

    Lint

--- source/helpers/feature-link.ts
+++ source/helpers/feature-link.ts
@@ -1,3 +1,3 @@
 export default function featureLink(id: string): string {
-	return `https://github.com/refined-github/refined-github/blob/main/source/features/${id}.tsx`
+	return `https://github.com/refined-github/refined-github/blob/main/source/features/${id}.tsx`;
 }
