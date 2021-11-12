4f270c4f50e0a2a20085a6e92095117f10340322
Florent <cheap.glitch@gmail.com>
2021-11-12 15:55:26 +0100

    Fix `highest-rated-comment` checkmark misalignment (#5065)

--- source/features/highest-rated-comment.tsx
+++ source/features/highest-rated-comment.tsx
@@ -51,7 +51,7 @@ function highlightBestComment(bestComment: Element): void {
 	select('.unminimized-comment', bestComment)!.classList.add('rgh-highest-rated-comment');
 	select('.unminimized-comment .timeline-comment-header-text', bestComment)!.before(
 		<span
-			className="d-flex flex-items-center color-text-success color-fg-success mr-1 tooltipped tooltipped-n"
+			className="d-inline-block color-text-success color-fg-success mr-1 tooltipped tooltipped-n"
 			aria-label="This comment has the most positive reactions on this issue."
 		>
 			<CheckCircleFillIcon/>
