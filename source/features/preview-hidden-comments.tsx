634201cfa6eca66a7067c72ef213b75332d21731
cheap-glitch <cheap.glitch@gmail.com>
2021-11-16 14:35:28 +0100

    Fix: `preview-hidden-comments` shows "Nothing to preview" after hiding a comment

--- source/features/preview-hidden-comments.tsx
+++ source/features/preview-hidden-comments.tsx
@@ -29,10 +29,11 @@ function init(): void {
 		}
 
 		// Hidden review comments that have been preloaded have their header text wrapped in an extra <div>
-		const headerTextWrapper = header.tagName === 'H3' ? select(':scope > .d-inline-block:last-child', header) : undefined;
+		// Comments that have just been hidden already have a `.Details-content--open` element #4983
+		const headerText = select(':scope > :is(.d-inline-block:last-child, .Details-content--open)', header) ?? header.firstChild;
 		header.append(
-			<span className="Details-content--open">{headerTextWrapper ?? header.firstChild}</span>,
-			<span className="Details-content--closed">{`${upperCaseFirst(reason)} — ${commentText.slice(0, 100)}`}</span>,
+			<span className="Details-content--open">{headerText}</span>,
+			<span className="Details-content--closed rgh-preview-hidden-comments-details">{`${upperCaseFirst(reason)} — ${commentText.slice(0, 100)}`}</span>,
 		);
 	}
 }
