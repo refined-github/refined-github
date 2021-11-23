59925d58c1ab5a69494b8e1e069d0f47ddd247f7
cheap-glitch <cheap.glitch@gmail.com>
2021-11-23 12:37:23 +0100

    Memoize and debounce listener

--- source/github-events/on-new-review.ts
+++ source/github-events/on-new-review.ts
@@ -1,13 +1,11 @@
+import mem from 'mem';
+import debounce from 'debounce-fn';
 import delegate from 'delegate-it';
 
-function callbackWrapper(callback: VoidFunction): (event: CustomEvent) => void {
-	return function (event) {
-		if (/^pull request #\d+ updated$/.test(event.detail.data.reason)) {
-			callback();
-		}
-	};
-}
+const getDeduplicatedHandler = mem((callback: EventListener): delegate.EventHandler => debounce((event: delegate.Event) => {
+	callback(event);
+}, {wait: 200}));
 
-export default function onNewReviewComment(callback: VoidFunction): void {
-	delegate(document, '.js-pull-refresh-on-pjax', 'socket:message', callbackWrapper(callback), {capture: true});
+export default function onNewReviewComment(callback: EventListener): void {
+	delegate(document, '.js-pull-refresh-on-pjax', 'socket:message', getDeduplicatedHandler(callback), {capture: true});
 }
