a408ddf284c7066708c0ec43b7b51e1422cf1f11
Kid <kidonng@gmail.com>
2021-12-01 22:53:30 +0800

    Make it work with `infinite-scroll`

--- source/features/deprioritize-marketplace-link.tsx
+++ source/features/deprioritize-marketplace-link.tsx
@@ -15,14 +15,18 @@ async function init(): Promise<void> {
 
 	const link = <a href="/marketplace">Marketplace</a>;
 	const item = <li>{link}</li>;
+	const footers = [elementReady('.footer[role="contentinfo"] ul:last-of-type')]; // Native
 
 	if (pageDetect.isDashboard()) {
 		link.classList.add('Link--secondary');
+		footers.push(elementReady('[aria-label="Explore"] .footer ul:last-of-type')); // Added by `infinite-scroll`
 	} else {
 		item.classList.add('ml-3', 'ml-lg-0');
 	}
 
-	(await elementReady('.footer ul:last-of-type'))!.append(item);
+	for await (const footer of footers) {
+		footer?.append(item.cloneNode(true));
+	}
 }
 
 void features.add(import.meta.url, {
