351340895ae49ed00e3e22b2caa11dd21c1e138f
Federico Brigante <me@fregante.com>
2021-12-10 13:45:38 +0700

    Extend comments

--- source/features/rgh-linkify-welcome-issue.tsx
+++ source/features/rgh-linkify-welcome-issue.tsx
@@ -4,11 +4,16 @@ import delegate from 'delegate-it';
 import features from '.';
 
 /**
-This page has specific hidden links to wrap the text and then this feature creates a visible link only when the extension is installed. It looks like:
+@file This issue has specific hidden links to wrap the text and then this feature creates a visible link only when the extension is installed.
+
+@example
 
 ```md
 ...in the [](#rgh-linkify-welcome-issue)extension’s options[](#rgh-linkify-welcome-issue) to...
 ```
+
+This is done so that when editing that issue we're aware that something is up with that piece of text. Without these hidden links we might forget about this feature and break it.
+
 */
 const issueUrl = 'https://github.com/refined-github/refined-github/issues/3543';
 const placeholdersSelector = 'a[href="#rgh-linkify-welcome-issue"]';
