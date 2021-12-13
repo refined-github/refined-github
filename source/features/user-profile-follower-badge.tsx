0ba166ce2abe00e8bfede2f644e2530c298e7524
Florent <cheap.glitch@gmail.com>
2021-12-13 10:04:56 +0100

    Fix `user-profile-follower-badge` feature (#5195)

--- source/features/user-profile-follower-badge.tsx
+++ source/features/user-profile-follower-badge.tsx
@@ -21,7 +21,7 @@ const doesUserFollow = cache.function(async (userA: string, userB: string): Prom
 
 async function init(): Promise<void> {
 	if (await doesUserFollow(getCleanPathname(), getUsername()!)) {
-		select('.js-profile-editable-area .octicon-star')!.closest('.mb-3')!.append(
+		select('.js-profile-editable-area [href$="?tab=following"]')!.after(
 			<span className="color-text-secondary color-fg-muted"> · Follows you</span>,
 		);
 	}
