e106e249e54a106bf5ee21e41018465bf066630f
Florent <cheap.glitch@gmail.com>
2022-01-04 13:18:48 +0100

    Fix: `profile-gists-link` is not responsive (#5252)

--- source/features/profile-gists-link.tsx
+++ source/features/profile-gists-link.tsx
@@ -1,5 +1,6 @@
 import React from 'dom-chef';
 import cache from 'webext-storage-cache';
+import select from 'select-dom';
 import elementReady from 'element-ready';
 import * as pageDetect from 'github-url-detection';
 import {CodeSquareIcon} from '@primer/octicons-react';
@@ -7,6 +8,7 @@ import {CodeSquareIcon} from '@primer/octicons-react';
 import features from '.';
 import * as api from '../github-helpers/api';
 import {getCleanPathname} from '../github-helpers';
+import {createDropdownItem} from './more-dropdown-links';
 
 const getGistCount = cache.function(async (username: string): Promise<number> => {
 	const {user} = await api.v4(`
@@ -27,13 +29,25 @@ async function init(): Promise<void> {
 	const username = getCleanPathname();
 	const href = pageDetect.isEnterprise() ? `/gist/${username}` : `https://gist.github.com/${username}`;
 	const link = (
-		<a href={href} className="UnderlineNav-item" role="tab" aria-selected="false">
+		<a
+			href={href}
+			className="UnderlineNav-item js-responsive-underlinenav-item"
+			role="tab"
+			aria-selected="false"
+			data-tab-item="rgh-gists-item"
+		>
 			<CodeSquareIcon className="UnderlineNav-octicon hide-sm"/>
 			{' Gists '}
 		</a>
 	);
 
-	(await elementReady('.UnderlineNav-body'))!.append(link);
+	const navigationBar = (await elementReady('.UnderlineNav-body'))!;
+	navigationBar.append(link);
+	navigationBar.replaceWith(navigationBar);
+
+	select('.js-responsive-underlinenav .dropdown-menu ul')!.append(
+		createDropdownItem('Gists', href),
+	);
 
 	const count = await getGistCount(username);
 	if (count > 0) {
