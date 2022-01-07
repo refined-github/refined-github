c3d39518ffe3807739f4cee5b5f6c88e3936be3d
Kid <kidonng@gmail.com>
2022-01-07 23:49:18 +0800

    Rewrite into a feature

---
+++ source/features/dim-visited-conversations.tsx
@@ -0,0 +1,25 @@
+import './dim-visited-conversations.css';
+import select from 'select-dom';
+import * as pageDetect from 'github-url-detection';
+
+import features from '.';
+
+function init(): void {
+	document.body.classList.add('rgh-dim-visited-conversations');
+
+	for (const row of select.all('.js-issue-row')) {
+		const count = select('.octicon-comment + .text-small', row)?.textContent;
+
+		if (count) {
+			select('a.h4', row)!.hash = 'rgh-dim-visited-conversations-' + count;
+		}
+	}
+}
+
+void features.add(import.meta.url, {
+	include: [
+		pageDetect.isConversationList,
+	],
+	deduplicate: 'has-rgh-inner',
+	init,
+});
