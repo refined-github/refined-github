723b10e427e3b780a45943c7f46aed371f32e14a
Kid <kidonng@gmail.com>
2022-01-14 00:32:19 +0800

    Input version automatically

--- source/features/index.tsx
+++ source/features/index.tsx
@@ -70,6 +70,7 @@ const logError = (url: string, error: unknown): void => {
 	newIssueUrl.searchParams.set('template', '1_bug_report.yml');
 	newIssueUrl.searchParams.set('title', `\`${id}\`: ${message}`);
 	newIssueUrl.searchParams.set('example_url', location.href);
+	newIssueUrl.searchParams.set('extension_version', version);
 	newIssueUrl.searchParams.set('description', [
 		'```',
 		String(error instanceof Error ? error.stack! : error).trim(),
