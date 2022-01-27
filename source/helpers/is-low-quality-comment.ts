61b0490e3ad149be7fc69327633f4d72ec58bc4d
Ambar Mutha <ambar.ytl@gmail.com>
2022-01-27 16:50:02 +0530

    Add "bump" to low quality comment regex

--- source/helpers/is-low-quality-comment.ts
+++ source/helpers/is-low-quality-comment.ts
@@ -1,4 +1,4 @@
 export default function isLowQualityComment(text: string): boolean {
 	// Note: the unicode range targets skin color modifiers for the hand emojis
-	return text.replace(/[\s,.!?👍👎👌🙏]+|[\u{1F3FB}-\u{1F3FF}]|[+-]\d+|⬆️|ditt?o|me|too|here|there|on|same|this|issue?s?|please|pl[sz]|any|updates?|question|solution|th[ae]nks?|thnx|you|followin?g/gui, '') === '';
+	return text.replace(/[\s,.!?👍👎👌🙏]+|[\u{1F3FB}-\u{1F3FF}]|[+-]\d+|⬆️|ditt?o|me|too|here|there|on|same|this|issue?s?|please|pl[sz]|any|updates?|bump|question|solution|th[ae]nks?|thnx|you|followin?g/gui, '') === '';
 }
