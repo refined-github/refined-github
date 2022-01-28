c6c52256be10c07045dd305fde33ca49a6e0afa3
Ambar Mutha <44284655+supercoww@users.noreply.github.com>
2022-01-28 16:59:31 +0530

    Update source/helpers/is-low-quality-comment.ts
    Co-authored-by: Federico Brigante <me@fregante.com>

--- source/helpers/is-low-quality-comment.ts
+++ source/helpers/is-low-quality-comment.ts
@@ -1,4 +1,4 @@
 export default function isLowQualityComment(text: string): boolean {
 	// Note: the unicode range targets skin color modifiers for the hand emojis
-	return text.replace(/[\s,.!?👍👎👌🙏]+|[\u{1F3FB}-\u{1F3FF}]|[+-]\d+|⬆️|ditt?o|me|too|here|there|on|same|this|issue?s?|please|pl[sz]|any|updates?|bump|question|solution|th[ae]nks?|thnx|you|followin?g/gui, '') === '';
+	return text.replace(/[\s,.!?👍👎👌🙏]+|[\u{1F3FB}-\u{1F3FF}]|[+-]\d+|⬆️|ditt?o|me|too|t?here|on|same|this|issues?|please|pl[sz]|any|updates?|bump|question|solution|following/gui, '') === '';
 }
