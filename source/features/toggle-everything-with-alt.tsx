7eb2ad42ed9cc027f4b4c9a4ca7ddb6c3d77f202
Kid <hi@xuann.wang>
2022-01-30 00:55:52 +0800

    Refactor stuff a bit

--- source/features/toggle-everything-with-alt.tsx
+++ source/features/toggle-everything-with-alt.tsx
@@ -4,10 +4,6 @@ import * as pageDetect from 'github-url-detection';
 import features from '.';
 import clickAll from '../helpers/click-all';
 
-function diffsSelector(): string {
-	return '.js-file .js-diff-load';
-}
-
 function minimizedCommentsSelector(clickedItem: HTMLElement): string {
 	if ((clickedItem.parentElement as HTMLDetailsElement).open) {
 		return '.minimized-comment > details[open] > summary';
@@ -16,17 +12,15 @@ function minimizedCommentsSelector(clickedItem: HTMLElement): string {
 	return '.minimized-comment > details:not([open]) > summary';
 }
 
+const diffsSelector = '.js-file .js-diff-load';
+
 function resolvedCommentsSelector(clickedItem: HTMLElement): string {
 	return `.js-resolvable-thread-toggler[aria-expanded="${clickedItem.getAttribute('aria-expanded')!}"]:not(.d-none)`;
 }
 
-function expandSelector(): string {
-	return '.js-file .js-expand-full';
-}
+const expandSelector = '.js-file .js-expand-full';
 
-function collapseSelector(): string {
-	return '.js-file .js-collapse-diff';
-}
+const collapseSelector = '.js-file .js-collapse-diff';
 
 const commitMessageSelector = '.TimelineItem .ellipsis-expander';
 
@@ -35,14 +29,14 @@ function init(): void {
 	delegate(document, '.minimized-comment details summary', 'click', clickAll(minimizedCommentsSelector));
 
 	// "Load diff" buttons in PR files
-	delegate(document, '.js-file .js-diff-load', 'click', clickAll(diffsSelector));
+	delegate(document, diffsSelector, 'click', clickAll(() => diffsSelector));
 
 	// Review comments in PR
 	delegate(document, '.js-file .js-resolvable-thread-toggler', 'click', clickAll(resolvedCommentsSelector));
 
 	// "Expand all" and "Collapse expanded lines" buttons in commit files
-	delegate(document, '.js-file .js-expand-full', 'click', clickAll(expandSelector));
-	delegate(document, '.js-file .js-collapse-diff', 'click', clickAll(collapseSelector));
+	delegate(document, expandSelector, 'click', clickAll(() => expandSelector));
+	delegate(document, collapseSelector, 'click', clickAll(() => collapseSelector));
 
 	// Commit message buttons in commit lists and PR conversations
 	delegate(document, commitMessageSelector, 'click', clickAll(() => commitMessageSelector));
