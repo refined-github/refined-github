e157427f8f97977c49c74743647699aee540895b
cheap-glitch <cheap.glitch@gmail.com>
2022-01-29 16:42:05 +0100

    Add `easy-toggle-commit-messages` feature'

---
+++ source/features/easy-toggle-commit-messages.tsx
@@ -0,0 +1,28 @@
+import select from 'select-dom';
+import * as pageDetect from 'github-url-detection';
+
+import features from '.';
+
+function toggleCommitMessage(event: MouseEvent): void {
+	const elementClicked = event.target as HTMLElement;
+	const commitBox = elementClicked.closest('.js-commits-list-item')!;
+
+	// The clicked element is not a button or a link
+	if (elementClicked === commitBox || elementClicked.matches('div, p, pre')) {
+		select('.ellipsis-expander', commitBox)!
+			.dispatchEvent(new MouseEvent('click', {bubbles: true, altKey: event.altKey}));
+	}
+}
+
+async function init(): Promise<void> {
+	document.body.addEventListener('click', toggleCommitMessage);
+}
+
+void features.add(import.meta.url, {
+	include: [
+		pageDetect.isCommitList,
+	],
+	awaitDomReady: false,
+	deduplicate: 'has-rgh-inner',
+	init,
+});
