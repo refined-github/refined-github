fc184c55bb4bf730f0adcb0cd3d5a4bb39e96590
Kid <hi@xuann.wang>
2022-01-30 20:40:54 +0800

    Revert to functions

--- source/features/toggle-everything-with-alt.tsx
+++ source/features/toggle-everything-with-alt.tsx
@@ -12,34 +12,42 @@ function minimizedCommentsSelector(clickedItem: HTMLElement): string {
 	return '.minimized-comment > details:not([open]) > summary';
 }
 
-const diffsSelector = '.js-file .js-diff-load';
+function diffsSelector(): string {
+	return '.js-file .js-diff-load';
+}
 
 function resolvedCommentsSelector(clickedItem: HTMLElement): string {
 	return `.js-resolvable-thread-toggler[aria-expanded="${clickedItem.getAttribute('aria-expanded')!}"]:not(.d-none)`;
 }
 
-const expandSelector = '.js-file .js-expand-full';
+function expandSelector(): string {
+	return '.js-file .js-expand-full';
+}
 
-const collapseSelector = '.js-file .js-collapse-diff';
+function collapseSelector(): string {
+	return '.js-file .js-collapse-diff';
+}
 
-const commitMessageSelector = '.TimelineItem .ellipsis-expander';
+function commitMessageSelector(): string {
+	return '.TimelineItem .ellipsis-expander';
+}
 
 function init(): void {
 	// Collapsed comments in PR conversations and files
 	delegate(document, '.minimized-comment details summary', 'click', clickAll(minimizedCommentsSelector));
 
 	// "Load diff" buttons in PR files
-	delegate(document, diffsSelector, 'click', clickAll(() => diffsSelector));
+	delegate(document, diffsSelector(), 'click', clickAll(diffsSelector));
 
 	// Review comments in PR
 	delegate(document, '.js-file .js-resolvable-thread-toggler', 'click', clickAll(resolvedCommentsSelector));
 
 	// "Expand all" and "Collapse expanded lines" buttons in commit files
-	delegate(document, expandSelector, 'click', clickAll(() => expandSelector));
-	delegate(document, collapseSelector, 'click', clickAll(() => collapseSelector));
+	delegate(document, expandSelector(), 'click', clickAll(expandSelector));
+	delegate(document, collapseSelector(), 'click', clickAll(collapseSelector));
 
 	// Commit message buttons in commit lists and PR conversations
-	delegate(document, commitMessageSelector, 'click', clickAll(() => commitMessageSelector));
+	delegate(document, commitMessageSelector(), 'click', clickAll(commitMessageSelector));
 }
 
 void features.add(import.meta.url, {
