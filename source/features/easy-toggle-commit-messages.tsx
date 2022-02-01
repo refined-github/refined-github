9fe6db084f6fdbe7bbba8b1fa9a324fc5f7672ac
cheap-glitch <cheap.glitch@gmail.com>
2022-02-01 17:43:19 +0100

    Exclude popups & dropdowns too

--- source/features/easy-toggle-commit-messages.tsx
+++ source/features/easy-toggle-commit-messages.tsx
@@ -6,8 +6,8 @@ import features from '.';
 
 function toggleCommitMessage(event: delegate.Event<MouseEvent>): void {
 	const elementClicked = event.target as HTMLElement;
-	// The clicked element is not a button or a link
-	if (!elementClicked.closest('a, button, clipboard-copy')) {
+	// The clicked element is not a button, a link or a popup ("Verified" badge, CI details, etc.)
+	if (!elementClicked.closest('a, button, clipboard-copy, details')) {
 		select('.ellipsis-expander', event.delegateTarget)?.dispatchEvent(
 			new MouseEvent('click', {
 				bubbles: true,
