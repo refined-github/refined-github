f45048df40c611d5517899707ee483dbf9bcbd60
Florent <cheap.glitch@gmail.com>
2022-02-13 11:08:35 +0100

    Improve reliability of `wait-for-build` (#5393)

--- source/options-storage.ts
+++ source/options-storage.ts
@@ -40,6 +40,7 @@ export const renamedFeatures = new Map<string, string>([
 	['conversation-filters', 'more-conversation-filters'],
 	['quick-pr-diff-options', 'one-click-diff-options'],
 	['quick-review-buttons', 'one-click-review-submission'],
+	['wait-for-build', 'wait-for-checks'],
 ]);
 
 export function getNewFeatureName(possibleFeatureName: string): FeatureID | undefined {
