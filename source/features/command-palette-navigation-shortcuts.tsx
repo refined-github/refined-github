7a996ed7f604319e0120805ab556e789208eaca0
Ken Powers <ken@kenpowers.net>
2022-02-27 21:01:08 -0600

    Review feedback

--- source/features/command-palette-navigation-shortcuts.tsx
+++ source/features/command-palette-navigation-shortcuts.tsx
@@ -2,6 +2,7 @@ import delegate from 'delegate-it';
 import onetime from 'onetime';
 
 import features from '.';
+import {isMac} from '../github-helpers';
 
 function commandPaletteKeydown(event: delegate.Event<KeyboardEvent>): void {
 	const {key, ctrlKey, target} = event;
@@ -20,15 +21,14 @@ function init(): void {
 	delegate(document, 'command-palette', 'keydown', commandPaletteKeydown);
 }
 
-const onMacOs = !!navigator.userAgent.match(/Macintosh/);
-
-if (onMacOs) {
-	void features.add(import.meta.url, {
-		shortcuts: {
-			'ctrl n': 'Select next item in command palette',
-			'ctrl p': 'Select previous item in command palette',
-		},
-		awaitDomReady: false,
-		init: onetime(init),
-	});
-}
+void features.add(import.meta.url, {
+	include: [
+		() => isMac,
+	],
+	shortcuts: {
+		'ctrl n': 'Select next item in command palette',
+		'ctrl p': 'Select previous item in command palette',
+	},
+	awaitDomReady: false,
+	init: onetime(init),
+});
