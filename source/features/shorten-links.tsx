85882b7aa09c8906e20fbed6d30d0d12b1e809e1
Dave Nicolson <david.nicolson@gmail.com>
2022-03-04 21:53:40 +0100

    Shorten tree links

--- source/features/shorten-links.tsx
+++ source/features/shorten-links.tsx
@@ -10,16 +10,24 @@ function init(): void {
 	observe(`a[href]:not(.${linkifiedURLClass})`, {
 		constructor: HTMLAnchorElement,
 		add(link) {
-			// Don't shorten links in code or code suggestions (but shorten them in review comments)
-			// Code explanation: Exclude links if the closest element found is not `.comment-body`
-			// https://github.com/refined-github/refined-github/pull/4759#discussion_r702460890
-			if (link.closest('.blob-code, .comment-body, .js-suggested-changes-blob')?.matches('.blob-code, .js-suggested-changes-blob')) {
-				return;
+			if (!isLinkInCode(link)) {
+				applyToLink(link, location.href);
 			}
-
-			applyToLink(link, location.href);
 		},
 	});
+
+	for (const link of document.querySelectorAll('.comment-body a[href*="/tree/"]')) {
+		if (!isLinkInCode(link)) {
+			applyToLink(link, location.href);
+		}
+	}
+}
+
+function isLinkInCode(link: HTMLAnchorElement): boolean {
+	// Don't shorten links in code or code suggestions (but shorten them in review comments)
+	// Code explanation: Exclude links if the closest element found is not `.comment-body`
+	// https://github.com/refined-github/refined-github/pull/4759#discussion_r702460890
+	return Boolean(link.closest('.blob-code, .comment-body, .js-suggested-changes-blob')?.matches('.blob-code'));
 }
 
 void features.add(import.meta.url, {
