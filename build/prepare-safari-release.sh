0e66690e7f5bc4e37a1eec7c8fcb9257a42dfb09
Federico Brigante <me@fregante.com>
2022-03-13 16:31:04 +0800

    Add script to automatically bump Safari’s version

---
+++ build/prepare-safari-release.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# Automatically exit on error
+set -e
+
+CONFIG_FILE=./safari/LocalOverrides.xcconfig
+
+TAG=$(git describe --tags --abbrev=0)
+PROJECT_VERSION=$(sed -n 's/^CURRENT_PROJECT_VERSION = \(.*\)/\1/p' < ./safari/LocalOverrides.xcconfig)
+NEXT_PROJECT_VERSION=$((PROJECT_VERSION + 1))
+
+echo "Will bump the project version" "$PROJECT_VERSION"
+
+trash distribution
+npm run build
+npx dot-json distribution/manifest.json version "$TAG"
+
+sed -i '' '/MARKETING_VERSION/d' $CONFIG_FILE
+sed -i '' '/CURRENT_PROJECT_VERSION/d' $CONFIG_FILE
+
+echo "MARKETING_VERSION = $TAG" >> $CONFIG_FILE
+echo "CURRENT_PROJECT_VERSION = $NEXT_PROJECT_VERSION" >> $CONFIG_FILE
