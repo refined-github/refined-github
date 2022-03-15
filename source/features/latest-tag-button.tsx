77d1842f1a2e5554573df6fbaa0cfd35a5a345d3
cheap-glitch <cheap.glitch@gmail.com>
2022-03-15 08:31:19 +0100

    Use `MAX_SAFE_INTEGER` to encode +20 commits difference

--- source/features/latest-tag-button.tsx
+++ source/features/latest-tag-button.tsx
@@ -15,7 +15,7 @@ import {buildRepoURL, getCurrentCommittish, getLatestVersionTag, getRepo} from '
 
 interface RepoPublishState {
 	latestTag: string | false;
-	aheadBy?: number;
+	aheadBy: number;
 }
 
 interface Tags {
@@ -27,6 +27,8 @@ interface Tags {
 		};
 	};
 }
+
+const undeterminableAheadBy = Number.MAX_SAFE_INTEGER; // For when the branch is ahead by more than 20 commits #5505
 
 const getRepoPublishState = cache.function(async (): Promise<RepoPublishState> => {
 	const {repository} = await api.v4(`
@@ -64,6 +66,7 @@ const getRepoPublishState = cache.function(async (): Promise<RepoPublishState> =
 	if (repository.refs.nodes.length === 0) {
 		return {
 			latestTag: false,
+			aheadBy: 0,
 		};
 	}
 
@@ -76,11 +79,10 @@ const getRepoPublishState = cache.function(async (): Promise<RepoPublishState> =
 	const latestTagOid = tags.get(latestTag)!;
 	const aheadBy = repository.defaultBranchRef.target.history.nodes.findIndex((node: AnyObject) => node.oid === latestTagOid);
 
-	if (aheadBy < 0) {
-		return {latestTag};
-	}
-
-	return {latestTag, aheadBy};
+	return {
+		latestTag,
+		aheadBy: aheadBy === -1 ? undeterminableAheadBy : aheadBy,
+	};
 }, {
 	maxAge: {hours: 1},
 	staleWhileRevalidate: {days: 2},
@@ -118,7 +120,7 @@ async function init(): Promise<false | void> {
 	const defaultBranch = await getDefaultBranch();
 	const onLatestTag = currentBranch === latestTag;
 	const onDefaultBranch = !currentBranch || currentBranch === defaultBranch; // `getCurrentCommittish` returns `undefined` when at the repo root on the default branch #5446
-	const isAhead = aheadBy === undefined || aheadBy > 0;
+	const isAhead = aheadBy > 0;
 
 	if (onLatestTag || (onDefaultBranch && !isAhead)) {
 		link.setAttribute('aria-label', 'You’re on the latest version');
@@ -131,7 +133,7 @@ async function init(): Promise<false | void> {
 		link.setAttribute(
 			'aria-label',
 			isAhead
-				? `${defaultBranch} is ${aheadBy === undefined ? 'more than 20 commits' : pluralize(aheadBy, '1 commit', '$$ commits')} ahead of the latest version`
+				? `${defaultBranch} is ${aheadBy === undeterminableAheadBy ? 'more than 20 commits' : pluralize(aheadBy, '1 commit', '$$ commits')} ahead of the latest version`
 				: `The HEAD of ${defaultBranch} isn’t tagged`,
 		);
 
