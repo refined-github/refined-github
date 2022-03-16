d230d21e84c88e22e40bb536fe39cf120e1ce1d7
Florent <cheap.glitch@gmail.com>
2022-03-16 15:11:26 +0100

    Dim more PRs opened by bots in `dim-bots` (#5512)

--- source/features/dim-bots.tsx
+++ source/features/dim-bots.tsx
@@ -4,7 +4,7 @@ import * as pageDetect from 'github-url-detection';
 
 import features from '.';
 
-const commitSelectors = [
+const botNames = [
 	'actions-user',
 	'bors',
 	'ImgBotApp',
@@ -15,16 +15,18 @@ const commitSelectors = [
 	'scala-steward',
 	'snyk-bot',
 	'web-flow',
-].map(bot => `.commit-author[href$="?author=${bot}"]`);
+];
 
-commitSelectors.push('.commit-author[href$="%5Bbot%5D"]'); // Generic [bot] label
-
+const commitSelectors = botNames.map(bot => `.commit-author[href$="?author=${bot}"]`);
+commitSelectors.push('.commit-author[href$="%5Bbot%5D"]'); // Generic `[bot]` label in author name
 const commitSelector = commitSelectors.join(',');
 
-const prSelector = [
-	'.opened-by [href*="author%3Aapp%2F"]',
-	'.labels [href$="label%3Abot"]',
-];
+const prSelectors = botNames.map(bot => `.opened-by [title="pull requests opened by ${bot}"]`);
+prSelectors.push(
+	'.opened-by [href*="author%3Aapp%2F"]', // Search query `is:pr+author:app/*`
+	'.labels [href$="label%3Abot"]', // PR tagged with `bot` label
+);
+const prSelector = prSelectors.join(',');
 
 function init(): void {
 	for (const bot of select.all(commitSelector)) {
