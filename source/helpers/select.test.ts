77325c5ad14360e5bedff81fa7ca4808c83d5310
Federico Brigante <me@fregante.com>
2022-03-17 15:20:41 +0800

    Add `has()` selector helper

---
+++ source/helpers/select.test.ts
@@ -0,0 +1,69 @@
+import jsdom from 'jsdom';
+import test from 'ava';
+
+import select from './select';
+
+const {JSDOM} = jsdom;
+
+test('basic :has() support', t => {
+	const {window: {document}} = new JSDOM(`
+		<a>Home</a>
+		<a><strong>Contacts</a>
+	`);
+
+	t.like(select('a:has(strong)', document), {textContent: 'Contacts'});
+});
+
+test('returns undefined if not found', t => {
+	const {window: {document}} = new JSDOM(`
+		<a>Home</a>
+		<a><strong>Contacts</strong></a>
+	`);
+
+	t.is(select('a:has(em)', document), undefined);
+});
+
+test('supports looking for children in base element', t => {
+	const {window: {document}} = new JSDOM(`
+		<a>Home</a>
+		<a><em>Contacts</em> <i>icon</i></a>
+	`);
+
+	t.like(select('a:has(em) i', document), {textContent: 'icon'});
+});
+
+test('throws error when there’s a space before :has()', t => {
+	const {window: {document}} = new JSDOM(`
+		<a>Home</a>
+	`);
+
+	t.throws(() => {
+		select('a :has(em)', document);
+	}, {
+		message: 'No spaces before :has() supported',
+	});
+});
+
+test('throws error when there is more than one :has()', t => {
+	const {window: {document}} = new JSDOM(`
+		<a>Home</a>
+	`);
+
+	t.throws(() => {
+		select('a:has(em) b:has(strong)', document);
+	}, {
+		message: 'Only one `:has()` required/allowed, found 2',
+	});
+});
+
+test('throws on sibling selectors', t => {
+	const {window: {document}} = new JSDOM(`
+		<a>Home</a>
+	`);
+
+	t.throws(() => {
+		select('a:has(+a)', document);
+	}, {
+		message: 'This polyfill only supports looking into the children of the base element',
+	});
+});
---
+++ source/helpers/select.ts
@@ -0,0 +1,29 @@
+// Adapted from https://stackoverflow.com/a/35271017/288906
+const hasSelectorRegex = /:has\(((?:[^)(]+|\((?:[^)(]+|\([^)(]*\))*\))*)\)/;
+
+export default function select<ExpectedElement extends HTMLElement>(selectors: string | string[], baseElement: HTMLElement | Document = document): ExpectedElement | void {
+	const count = [...String(selectors).matchAll(/has\(/g)].length;
+	if (count !== 1) { // Only one :has allowed. KISS
+		throw new Error(`Only one \`:has()\` required/allowed, found ${count}`);
+	}
+
+	const parts = String(selectors).split(hasSelectorRegex);
+
+	const [baseSelector, hasSelector, finalSelector] = parts;
+	if (/\s$/.test(baseSelector)) {
+		throw new Error('No spaces before :has() supported');
+	}
+
+	if (/^[+~]/.test(hasSelector.trim())) {
+		throw new Error('This polyfill only supports looking into the children of the base element');
+	}
+
+	for (const base of baseElement.querySelectorAll(baseSelector)) {
+		if (base.querySelector(':scope ' + hasSelector)) {
+			const finalElement = finalSelector.trim() ? base.querySelector(finalSelector) : base;
+			if (finalElement) {
+				return finalElement as ExpectedElement;
+			}
+		}
+	}
+}
