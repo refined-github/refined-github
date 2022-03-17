1157c6c2e8e39f02de7ed50ad159d323e9a632aa
Federico Brigante <me@fregante.com>
2022-03-17 15:49:16 +0800

    Query :has content first

--- source/helpers/selectHas.ts
+++ source/helpers/selectHas.ts
@@ -18,8 +18,9 @@ export default function selectHas<ExpectedElement extends HTMLElement>(selectors
 		throw new Error('This polyfill only supports looking into the children of the base element');
 	}
 
-	for (const base of baseElement.querySelectorAll(baseSelector)) {
-		if (base.querySelector(':scope ' + hasSelector)) {
+	for (const expectedChild of baseElement.querySelectorAll(hasSelector)) {
+		const base = expectedChild.closest(baseSelector);
+		if (base) {
 			const finalElement = finalSelector.trim() ? base.querySelector(finalSelector) : base;
 			if (finalElement) {
 				return finalElement as ExpectedElement;
