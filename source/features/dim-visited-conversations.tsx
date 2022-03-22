01e7e7e499d2fef5bb5756679a8729d243f76158
Federico Brigante <me@fregante.com>
2022-03-22 23:38:45 +0800

    Delete dim-visited-conversations.tsx

--- source/features/dim-visited-conversations.tsx
+++
@@ -1,6 +0,0 @@
-import './dim-visited-conversations.css';
-import * as pageDetect from 'github-url-detection';
-
-import features from '.';
-
-void features.addCssFeature(import.meta.url, [pageDetect.isConversationList]);
