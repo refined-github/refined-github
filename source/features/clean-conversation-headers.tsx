4f6f39181cf2dc9c977caa7475a5d4938d110a82
cheap-glitch <cheap.glitch@gmail.com>
2022-04-07 16:53:20 +0200

    Revert "Drop `elementReady`"
    This reverts commit 9a36d83a0dad9d15d87fbdaf2b22e153fb587220.

--- source/features/clean-conversation-headers.tsx
+++ source/features/clean-conversation-headers.tsx
@@ -1,6 +1,7 @@
 import './clean-conversation-headers.css';
 import React from 'dom-chef';
 import select from 'select-dom';
+import elementReady from 'element-ready';
 import {ArrowLeftIcon} from '@primer/octicons-react';
 import * as pageDetect from 'github-url-detection';
 
@@ -8,8 +9,12 @@ import features from '.';
 import getDefaultBranch from '../github-helpers/get-default-branch';
 import onConversationHeaderUpdate from '../github-events/on-conversation-header-update';
 
-function initIssue(): void {
-	const byline = select('.gh-header-meta .flex-auto:not(.rgh-clean-conversation-headers)')!;
+async function initIssue(): Promise<void> {
+	const byline = await elementReady('.gh-header-meta .flex-auto:not(.rgh-clean-conversation-headers)');
+	if (!byline) {
+		return;
+	}
+
 	byline.classList.add('rgh-clean-conversation-headers', 'rgh-clean-conversation-headers-hide-author');
 
 	// Shows on issues: octocat opened this issue on 1 Jan · [1 comments]
@@ -19,12 +24,16 @@ function initIssue(): void {
 }
 
 async function initPR(): Promise<void> {
-	const byline = select('.gh-header-meta .flex-auto:not(.rgh-clean-conversation-headers)')!;
+	const byline = await elementReady('.gh-header-meta .flex-auto:not(.rgh-clean-conversation-headers)');
+	if (!byline) {
+		return;
+	}
+
 	byline.classList.add('rgh-clean-conversation-headers');
 
 	// Extra author name is only shown on `isPRConversation`
 	// Hide if it's the same as the opener (always) or merger
-	const shouldHideAuthor = pageDetect.isPRConversation() && select('.author', byline)!.textContent === select('.TimelineItem .author')!.textContent;
+	const shouldHideAuthor = pageDetect.isPRConversation() && select('.author', byline)!.textContent === (await elementReady('.TimelineItem .author'))!.textContent;
 	if (shouldHideAuthor) {
 		byline.classList.add('rgh-clean-conversation-headers-hide-author');
 	}
@@ -49,6 +58,7 @@ void features.add(import.meta.url, {
 	additionalListeners: [
 		onConversationHeaderUpdate,
 	],
+	awaitDomReady: false,
 	deduplicate: 'has-rgh-inner',
 	init: initIssue,
 }, {
@@ -58,6 +68,7 @@ void features.add(import.meta.url, {
 	additionalListeners: [
 		onConversationHeaderUpdate,
 	],
+	awaitDomReady: false,
 	deduplicate: 'has-rgh-inner',
 	init: initPR,
 });
