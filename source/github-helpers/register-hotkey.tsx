45ef8ced12d9df6582d530ce0488a8ff38c0fcb7
cheap-glitch <cheap.glitch@gmail.com>
2022-04-13 10:27:16 +0200

    Use named type

--- source/github-helpers/register-hotkey.tsx
+++ source/github-helpers/register-hotkey.tsx
@@ -1,6 +1,6 @@
 import React from 'dom-chef';
 
-export default function registerHotkey(hotkey: string, handler: () => void): Deinit {
+export default function registerHotkey(hotkey: string, handler: VoidFunction): Deinit {
 	const hotkeyButton = <button hidden type="button" data-hotkey={hotkey} onClick={handler}/>;
 	document.body.append(hotkeyButton);
 
