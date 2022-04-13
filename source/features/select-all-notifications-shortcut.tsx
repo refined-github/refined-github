d11f49bc2ffe559f319be6a659ca453dae1d4ba9
cheap-glitch <cheap.glitch@gmail.com>
2022-04-13 10:23:57 +0200

    Use button with native `data-hotkey` attribute

--- source/features/select-all-notifications-shortcut.tsx
+++ source/features/select-all-notifications-shortcut.tsx
@@ -2,22 +2,14 @@ import select from 'select-dom';
 import * as pageDetect from 'github-url-detection';
 
 import features from '.';
-import {isEditable} from '../helpers/dom-utils';
+import registerHotkey from '../github-helpers/register-hotkey';
 
-function runShortcut(event: KeyboardEvent): void {
-	if (
-		event.key === 'a'
-		&& !event.ctrlKey
-		&& !event.metaKey
-		&& !isEditable(event.target)
-	) {
-		select('.js-notifications-mark-all-prompt')!.click();
-	}
+function selectAllNotifications(): void {
+	select('.js-notifications-mark-all-prompt')!.click();
 }
 
-function init(signal: AbortSignal): void {
-	// Listen to the shortcut ourselves instead of attaching it to the checkbox #5569
-	document.body.addEventListener('keypress', runShortcut, {signal});
+function init(): Deinit {
+	return registerHotkey('a', selectAllNotifications);
 }
 
 void features.add(import.meta.url, {
---
+++ source/github-helpers/register-hotkey.tsx
@@ -0,0 +1,12 @@
+import React from 'dom-chef';
+
+export default function registerHotkey(hotkey: string, handler: () => void): Deinit {
+	const hotkeyButton = <button hidden type="button" data-hotkey={hotkey} onClick={handler}/>;
+	document.body.append(hotkeyButton);
+
+	return () => {
+		if (hotkeyButton.isConnected) {
+			hotkeyButton.remove();
+		}
+	};
+}
