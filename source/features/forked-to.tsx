10ab22ad9f8b602c14d419200a6b01c525a62c0d
cheap-glitch <cheap.glitch@gmail.com>
2022-04-14 17:55:40 +0200

    Enable feature for every user

--- source/features/forked-to.tsx
+++ source/features/forked-to.tsx
@@ -116,12 +116,9 @@ async function init(): Promise<void | false> {
 		await updateUI(forks);
 	}
 
-	// This feature only applies to users that have multiple organizations, because that makes a fork picker modal appear when clicking on "Fork"
-	const hasOrganizations = select.exists('.pagehead-actions [data-ga-click*="show fork modal"]');
-
 	// Only fetch/update forks when we see a fork (on the current page or in the cache).
 	// This avoids having to `updateCache` for every single repo you visit.
-	if (forks || (hasOrganizations && pageDetect.isForkedRepo())) {
+	if (forks || pageDetect.isForkedRepo()) {
 		await updateCache();
 	} else {
 		return false;
