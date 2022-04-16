4109c741f3a4206013d4b82e76d02fc59ec873f7
Gyubong Lee <jopemachine@naver.com>
2022-04-16 14:07:55 +0900

    Revert "-1 +1 = 0"
    This reverts commit c0749df5f12f0269ad2da7f86b0ca0a267492f02.

--- source/helpers/get-items-between.ts
+++ source/helpers/get-items-between.ts
@@ -3,9 +3,11 @@ Get the items between previous and current, both ends included. If `previous` is
 */
 export default function getItemsBetween<T>(items: T[], previous: T | undefined, current: T): T[] {
 	const selections = [
-		items.indexOf(previous) + 1,
-		items.indexOf(current) + 1,
+		previous ? items.indexOf(previous) : 0,
+		items.indexOf(current),
 	].sort((a, b) => a - b);
+
+	selections[1] += 1;
 
 	return items.slice(...selections);
 }
