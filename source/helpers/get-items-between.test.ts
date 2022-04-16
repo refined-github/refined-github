4247d03546762b62e15eb950bddca1fa50a01233
cheap-glitch <cheap.glitch@gmail.com>
2022-04-16 13:36:20 +0200

    Lint /2

--- source/helpers/get-items-between.test.ts
+++ source/helpers/get-items-between.test.ts
@@ -5,11 +5,11 @@ import getItemsBetween from './get-items-between';
 test('getItemsBetween', t => {
 	const list = ['❤️', '💛', '💚', '💙'];
 
-	t.deepEqual(getItemsBetween<string>(list, '💛', '💚'), ['💛', '💚']);
-	t.deepEqual(getItemsBetween<string>(list, '💚', '💛'), ['💛', '💚']);
-	t.deepEqual(getItemsBetween<string>(list, '❤️', '💙'), ['❤️', '💛', '💚', '💙']);
-	t.deepEqual(getItemsBetween<string>(list, '💙', '❤️'), ['❤️', '💛', '💚', '💙']);
-	t.deepEqual(getItemsBetween<string>(list, undefined, '❤️'), ['❤️']);
-	t.deepEqual(getItemsBetween<string>(list, undefined, '💚'), ['❤️', '💛', '💚']);
-	t.deepEqual(getItemsBetween<string>(list, undefined, '💙'), ['❤️', '💛', '💚', '💙']);
+	t.deepEqual(getItemsBetween(list, '💛', '💚'), ['💛', '💚']);
+	t.deepEqual(getItemsBetween(list, '💚', '💛'), ['💛', '💚']);
+	t.deepEqual(getItemsBetween(list, '❤️', '💙'), ['❤️', '💛', '💚', '💙']);
+	t.deepEqual(getItemsBetween(list, '💙', '❤️'), ['❤️', '💛', '💚', '💙']);
+	t.deepEqual(getItemsBetween(list, undefined, '❤️'), ['❤️']);
+	t.deepEqual(getItemsBetween(list, undefined, '💚'), ['❤️', '💛', '💚']);
+	t.deepEqual(getItemsBetween(list, undefined, '💙'), ['❤️', '💛', '💚', '💙']);
 });
