029b02ec4f446c4305dbe341af929ca9664dcd12
Federico Brigante <me@fregante.com>
2022-06-19 18:19:56 +0700

    Experiment with automatic class generation

--- source/helpers/attach-element.ts
+++ source/helpers/attach-element.ts
@@ -1,20 +1,27 @@
 import select from 'select-dom';
 
+import hashString from './hash-string';
+
 type Position = 'append' | 'prepend' | 'before' | 'after';
 
 interface Attachment<NewElement extends Element> {
 	// eslint-disable-next-line @typescript-eslint/ban-types --  Allows dom traversing without requiring `!`
 	anchor: Element | string | undefined | null;
 	getNewElement: () => NewElement;
-	className: string;
+	className?: string;
 	position: Position;
 	allowMissingAnchor?: boolean;
+}
+
+// Get a unique position in the code
+function getSnapshotUUID(index = 3): string {
+	return hashString(new Error('Get stack').stack!.split('\n')[index]);
 }
 
 export default function attach<NewElement extends Element>({
 	anchor,
 	getNewElement,
-	className,
+	className = 'rgh-' + getSnapshotUUID(),
 	position,
 	allowMissingAnchor = false,
 }: Attachment<NewElement>): NewElement | void {
