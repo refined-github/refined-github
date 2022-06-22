d3c896a231c83fadab66d92a2253b2ddae17e870
Flo Edelmann <florian-edelmann@online.de>
2022-06-22 09:34:40 +0200

    Reuse class name variable

--- source/features/open-all-notifications.tsx
+++ source/features/open-all-notifications.tsx
@@ -8,6 +8,15 @@ import {LinkExternalIcon} from '@primer/octicons-react';
 import features from '.';
 import openTabs from '../helpers/open-tabs';
 
+// Selector works on:
+// https://github.com/notifications (Grouped by date)
+// https://github.com/notifications (Grouped by repo)
+// https://github.com/notifications?query=reason%3Acomment (which is an unsaved filter)
+const notificationHeaderSelector = '.js-check-all-container .js-bulk-action-toasts ~ div .Box-header';
+
+const openUnreadButtonClass = 'rgh-open-notifications-button';
+const openSelectedButtonClass = 'rgh-open-selected-button';
+
 function getUnreadNotifications(container: ParentNode = document): HTMLElement[] {
 	return select.all('.notification-unread', container);
 }
@@ -33,7 +42,7 @@ function openNotifications(notifications: Element[], markAsDone = false): void {
 }
 
 function removeOpenAllButtons(container: ParentNode = document): void {
-	for (const button of select.all('.rgh-open-notifications-button', container)) {
+	for (const button of select.all('.' + openUnreadButtonClass, container)) {
 		button.remove();
 	}
 }
@@ -60,21 +69,12 @@ function addOpenRepoButtons(): void {
 		}
 
 		select('.js-grouped-notifications-mark-all-read-button', repository)!.before(
-			<button type="button" className="btn btn-sm mr-2 tooltipped tooltipped-w rgh-open-notifications-button" aria-label="Open all unread notifications from this repo">
+			<button type="button" className={'btn btn-sm mr-2 tooltipped tooltipped-w ' + openUnreadButtonClass} aria-label="Open all unread notifications from this repo">
 				<LinkExternalIcon width={16}/> Open unread
 			</button>,
 		);
 	}
 }
-
-// Selector works on:
-// https://github.com/notifications (Grouped by date)
-// https://github.com/notifications (Grouped by repo)
-// https://github.com/notifications?query=reason%3Acomment (which is an unsaved filter)
-const notificationHeaderSelector = '.js-check-all-container .js-bulk-action-toasts ~ div .Box-header';
-
-const openUnreadButtonClass = 'rgh-open-notifications-button';
-const openSelectedButtonClass = 'rgh-open-selected-button';
 
 function init(): Deinit {
 	const deinit = [delegate(document, '.' + openSelectedButtonClass, 'click', openSelectedNotifications)];
