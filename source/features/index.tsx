8ba236cf8f025badc0bcd90e8a041564fed57bbc
Federico Brigante <me@fregante.com>
2022-07-02 14:32:49 +0700

    Revert "strings.json demo"
    This reverts commit 6d63f33e978f05feba74e03e21726674c3ffc927.

--- source/features/index.tsx
+++ source/features/index.tsx
@@ -95,8 +95,6 @@ const log = {
 
 // eslint-disable-next-line no-async-promise-executor -- Rule assumes we don't want to leave it pending
 const globalReady: Promise<RGHOptions> = new Promise(async resolve => {
-	console.log({value: _`Original`});
-
 	const [options, localHotfixes, hotfixCSS, bisectedFeatures] = await Promise.all([
 		optionsStorage.getAll(),
 		getLocalHotfixesAsOptions(),
@@ -104,8 +102,6 @@ const globalReady: Promise<RGHOptions> = new Promise(async resolve => {
 		bisectFeatures(),
 		getLocalStrings(),
 	]);
-
-	console.log({value: _`Original`});
 
 	await waitFor(() => document.body);
 
--- source/helpers/is-development-version.ts
+++ source/helpers/is-development-version.ts
@@ -1,6 +1,5 @@
 const {version} = browser.runtime.getManifest();
 
 export default function isDevelopmentVersion(): boolean {
-	return false;
 	return version === '0.0.0';
 }
