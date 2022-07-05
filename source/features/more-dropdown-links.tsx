c05563c36804b7c7f9f118a722ef5ca0c31ffd18
Federico Brigante <me@fregante.com>
2022-07-05 17:42:27 +0700

    Lint

--- source/features/more-dropdown-links.tsx
+++ source/features/more-dropdown-links.tsx
@@ -14,7 +14,7 @@ export async function unhideOverflowDropdown(): Promise<boolean> {
 	// Wait for the tab bar to be loaded
 	const repoNavigationBar = await elementReady('.UnderlineNav-body');
 
-	// No dropdown on mobile https://github.com/refined-github/refined-github/issues/5781
+	// No dropdown on mobile #5781
 	if (!select.exists('.js-responsive-underlinenav')) {
 		return false;
 	}
@@ -49,7 +49,7 @@ void features.add(import.meta.url, {
 	exclude: [
 		pageDetect.isEmptyRepo,
 
-		// No dropdown on mobile https://github.com/refined-github/refined-github/issues/5781
+		// No dropdown on mobile #5781
 		() => !select.exists('.js-responsive-underlinenav'),
 	],
 	awaitDomReady: false,
