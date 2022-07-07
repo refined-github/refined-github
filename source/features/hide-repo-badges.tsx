0a6a8465211e81a18502a5c05ed32ce896994022
Federico Brigante <me@fregante.com>
2022-07-07 21:09:48 +0700

    Delete hide-repo-badges.tsx

--- source/features/hide-repo-badges.tsx
+++
@@ -1,9 +0,0 @@
-import './hide-repo-badges.css';
-import * as pageDetect from 'github-url-detection';
-
-import features from '.';
-
-void features.addCssFeature(import.meta.url, [
-	pageDetect.isRepo,
-	pageDetect.isUserProfile,
-]);
