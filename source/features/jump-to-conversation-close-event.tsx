0b41ec40786f4b4eb75ba5ba84bead0b332cf20f
yakov116 <16872793+yakov116@users.noreply.github.com>
2022-07-07 13:35:11 -0400

    Update var

--- source/features/jump-to-conversation-close-event.tsx
+++ source/features/jump-to-conversation-close-event.tsx
@@ -9,14 +9,14 @@ import features from '.';
 function init(): Deinit {
 	return observe('#partial-discussion-header :is([title="Status: Closed"], [title="Status: Merged"], [title="Status: Closed as not planned"]):not(.rgh-jump-to-conversation-close-event)', {
 		constructor: HTMLSpanElement,
-		add(messageContainer) {
-			messageContainer.classList.add('rgh-jump-to-conversation-close-event');
+		add(discussionHeader) {
+			discussionHeader.classList.add('rgh-jump-to-conversation-close-event');
 			// Hide the native title
-			messageContainer.style.pointerEvents = 'none';
-			messageContainer.style.cursor = 'default';
+			discussionHeader.style.pointerEvents = 'none';
+			discussionHeader.style.cursor = 'default';
 
 			const lastCloseEvent = select.last('.TimelineItem-badge :is(.octicon-issue-closed, .octicon-git-merge, .octicon-git-pull-request-closed, .octicon-skip)')!.closest('.TimelineItem')!;
-			wrap(messageContainer,
+			wrap(discussionHeader,
 				<a
 					aria-label="Scroll to most recent close event."
 					className="tooltipped tooltipped-s"
