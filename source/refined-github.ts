3996cc453ca530c441bda87235b4dda383b46cbb
Federico Brigante <me@fregante.com>
2022-07-08 14:17:58 +0700

    Improve `readable-title-change-events` order (#5795)

--- source/refined-github.ts
+++ source/refined-github.ts
@@ -21,6 +21,7 @@ import './features/night-not-found.css';
 import './features/hide-watch-and-fork-count.css';
 import './features/clean-commit-form.css';
 import './features/sticky-file-header.css';
+import './features/readable-title-change-events.css';
 
 // DO NOT add CSS files here if they are part of a JavaScript feature.
 // Import the `.css` file from the `.tsx` instead.
