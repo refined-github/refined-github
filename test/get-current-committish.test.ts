05d9fc0c5411cbdd107e1e71166a41ae6d9118dd
Federico Brigante <me@fregante.com>
2022-07-09 14:47:13 +0700

    Meta: AVA -> Vitest (#5801)

--- test/get-current-committish.test.ts
+++ test/get-current-committish.test.ts
@@ -1,107 +1,107 @@
-import test from 'ava';
+import {assert, test} from 'vitest';
 
 import './fixtures/globals';
 import {getCurrentCommittish} from '../source/github-helpers';
 
 // The titles supplied here listed here are real, not guessed, except the error tester
-test('getCurrentCommittish', t => {
+test('getCurrentCommittish', () => {
 	// Error testing
-	t.is(getCurrentCommittish(
+	assert.equal(getCurrentCommittish(
 		'/',
 		'some page title',
 	), undefined, 'It should never throw with valid input');
-	t.throws(() => getCurrentCommittish(
+	assert.throws(() => getCurrentCommittish(
 		'https://github.com',
 		'github.com',
 	));
 
 	// Root
-	t.is(getCurrentCommittish(
+	assert.equal(getCurrentCommittish(
 		'/typescript-eslint/typescript-eslint',
 		'typescript-eslint/typescript-eslint: Monorepo for all the tooling which enables ESLint to support TypeScript',
 	), undefined);
-	t.is(getCurrentCommittish(
+	assert.equal(getCurrentCommittish(
 		'/typescript-eslint/typescript-eslint/tree/chore/lerna-4',
 		'typescript-eslint/typescript-eslint at chore/lerna-4',
 	), 'chore/lerna-4');
 
 	// Sub folder
-	t.is(getCurrentCommittish(
+	assert.equal(getCurrentCommittish(
 		'/typescript-eslint/typescript-eslint/tree/master/docs',
 		'typescript-eslint/docs at master · typescript-eslint/typescript-eslint',
 	), 'master');
-	t.is(getCurrentCommittish(
+	assert.equal(getCurrentCommittish(
 		'/typescript-eslint/typescript-eslint/tree/chore/lerna-4/docs',
 		'typescript-eslint/docs at chore/lerna-4 · typescript-eslint/typescript-eslint',
 	), 'chore/lerna-4');
 
 	// Sub sub folder
-	t.is(getCurrentCommittish(
+	assert.equal(getCurrentCommittish(
 		'/typescript-eslint/typescript-eslint/tree/master/docs/getting-started',
 		'typescript-eslint/docs/getting-started at master · typescript-eslint/typescript-eslint',
 	), 'master');
-	t.is(getCurrentCommittish(
+	assert.equal(getCurrentCommittish(
 		'/typescript-eslint/typescript-eslint/tree/chore/lerna-4/docs/getting-started',
 		'typescript-eslint/docs/getting-started at chore/lerna-4 · typescript-eslint/typescript-eslint',
 	), 'chore/lerna-4');
 
 	// File
-	t.is(getCurrentCommittish(
+	assert.equal(getCurrentCommittish(
 		'/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/README.md',
 		'typescript-eslint/README.md at master · typescript-eslint/typescript-eslint',
 	), 'master');
-	t.is(getCurrentCommittish(
+	assert.equal(getCurrentCommittish(
 		'/typescript-eslint/typescript-eslint/blob/chore/lerna-4/docs/getting-started/README.md',
 		'typescript-eslint/README.md at chore/lerna-4 · typescript-eslint/typescript-eslint',
 	), 'chore/lerna-4');
 
 	// Editing file
-	t.is(getCurrentCommittish(
+	assert.equal(getCurrentCommittish(
 		'/typescript-eslint/typescript-eslint/edit/master/docs/getting-started/README.md',
 		'Editing typescript-eslint/README.md at master · typescript-eslint/typescript-eslint',
 	), 'master');
-	t.is(getCurrentCommittish(
+	assert.equal(getCurrentCommittish(
 		'/typescript-eslint/typescript-eslint/edit/chore/lerna-4/docs/getting-started/README.md',
 		'Editing typescript-eslint/README.md at chore/lerna-4 · typescript-eslint/typescript-eslint',
 	), 'chore/lerna-4');
 
 	// Blame
-	t.is(getCurrentCommittish(
+	assert.equal(getCurrentCommittish(
 		'/typescript-eslint/typescript-eslint/blame/master/docs/getting-started/README.md',
 		'typescript-eslint/docs/getting-started/README.md at master · typescript-eslint/typescript-eslint',
 	), 'master');
-	t.is(getCurrentCommittish(
+	assert.equal(getCurrentCommittish(
 		'/typescript-eslint/typescript-eslint/blame/chore/lerna-4/docs/getting-started/README.md',
 		'typescript-eslint/docs/getting-started/README.md at chore/lerna-4 · typescript-eslint/typescript-eslint',
 	), 'chore/lerna-4');
 
 	// Commits
-	t.is(getCurrentCommittish(
+	assert.equal(getCurrentCommittish(
 		'/typescript-eslint/typescript-eslint/commits/master/docs/getting-started/README.md',
 		'History for docs/getting-started/README.md - typescript-eslint/typescript-eslint',
 	), 'master');
-	t.is(getCurrentCommittish(
+	assert.equal(getCurrentCommittish(
 		'/typescript-eslint/typescript-eslint/commits/chore/lerna-4/docs/getting-started/README.md',
 		'History for docs/getting-started/README.md - typescript-eslint/typescript-eslint',
 	), 'chore/lerna-4');
-	t.is(getCurrentCommittish(
+	assert.equal(getCurrentCommittish(
 		'/yakov116/TestR/commits/this/branch/has/many/slashes',
 		'Commits · yakov116/TestR',
 	), 'this/branch/has/many/slashes');
 
 	// Single commit
-	t.is(getCurrentCommittish(
+	assert.equal(getCurrentCommittish(
 		'/typescript-eslint/typescript-eslint/commit/795fd1c529ee58e97283c9ddf8463703517b50ab',
 		'chore: add markdownlint (#1889) · typescript-eslint/typescript-eslint@795fd1c',
 	), '795fd1c529ee58e97283c9ddf8463703517b50ab');
 
 	// Branch includes period
-	t.is(getCurrentCommittish(
+	assert.equal(getCurrentCommittish(
 		'/anggrayudi/SimpleStorage/tree/release/0.8.0',
 		'anggrayudi/SimpleStorage at release/0.8.0',
 	), 'release/0.8.0');
 
-	t.is(getCurrentCommittish(
+	assert.equal(getCurrentCommittish(
 		'/ksh-code/repository/tree/h.l.o.o',
 		'ksh-code/repository at h.l.o.o',
 	), 'h.l.o.o');
