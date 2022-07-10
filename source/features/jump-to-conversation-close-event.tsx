031ab23ee432e1459a8281d94f3d9e7328cb0966
Yakov <16872793+yakov116@users.noreply.github.com>
2022-07-10 10:38:31 -0400

    Extract function

--- source/features/jump-to-conversation-close-event.tsx
+++ source/features/jump-to-conversation-close-event.tsx
@@ -6,23 +6,25 @@ import * as pageDetect from 'github-url-detection';
 import {wrap} from '../helpers/dom-utils';
 import features from '.';
 
+function addToConversation(discussionHeader: HTMLElement): void {
+	discussionHeader.classList.add('rgh-jump-to-conversation-close-event');
+	// Hide the native title
+	discussionHeader.style.pointerEvents = 'none';
+
+	const lastCloseEvent = select.last('.TimelineItem-badge :is(.octicon-issue-closed, .octicon-git-merge, .octicon-git-pull-request-closed, .octicon-skip)')!.closest('.TimelineItem')!;
+	wrap(discussionHeader,
+		<a
+			aria-label="Scroll to most recent close event"
+			className="tooltipped tooltipped-s"
+			href={'#' + lastCloseEvent.id}
+		/>,
+	);
+}
+
 function init(): Deinit {
 	return observe('#partial-discussion-header :is([title="Status: Closed"], [title="Status: Merged"], [title="Status: Closed as not planned"]):not(.rgh-jump-to-conversation-close-event)', {
 		constructor: HTMLElement,
-		add(discussionHeader) {
-			discussionHeader.classList.add('rgh-jump-to-conversation-close-event');
-			// Hide the native title
-			discussionHeader.style.pointerEvents = 'none';
-
-			const lastCloseEvent = select.last('.TimelineItem-badge :is(.octicon-issue-closed, .octicon-git-merge, .octicon-git-pull-request-closed, .octicon-skip)')!.closest('.TimelineItem')!;
-			wrap(discussionHeader,
-				<a
-					aria-label="Scroll to most recent close event"
-					className="tooltipped tooltipped-s"
-					href={'#' + lastCloseEvent.id}
-				/>,
-			);
-		},
+		add: addToConversation,
 	});
 }
 
