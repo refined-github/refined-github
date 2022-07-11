74d52adf2437df31356f3397af4c0e9287c04b0e
Federico Brigante <me@fregante.com>
2022-07-11 20:03:47 +0700

    Meta: Improve new bug template (#5808)

--- source/features/rgh-improve-new-issue-form.tsx
+++ source/features/rgh-improve-new-issue-form.tsx
@@ -11,7 +11,7 @@ import {isRefinedGitHubRepo} from '../github-helpers';
 function init(): void {
 	const {version} = browser.runtime.getManifest();
 	select('input#issue_form_extension_version')!.value = version;
-	select('input[id*="extension_cache"]')!.parentElement!.after(
+	select('[href="#clear-cache"]')!.replaceWith(
 		<button
 			className="btn"
 			type="button"
@@ -33,9 +33,8 @@ function init(): void {
 void features.add(import.meta.url, {
 	asLongAs: [
 		isRefinedGitHubRepo,
-	],
-	include: [
-		() => pageDetect.isNewIssue() && new URL(location.href).searchParams.get('template') === '1_bug_report.yml',
+		pageDetect.isNewIssue,
+		() => new URL(location.href).searchParams.get('template') === '1_bug_report.yml',
 	],
 	deduplicate: 'has-rgh-inner',
 	init,
