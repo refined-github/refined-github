21b27756923211cf1914a55676aade0aaa7ff646
yakov116 <16872793+yakov116@users.noreply.github.com>
2022-07-11 15:12:41 -0400

    Update source/features/first-published-tag-for-merged-pr.tsx

--- source/features/first-published-tag-for-merged-pr.tsx
+++ source/features/first-published-tag-for-merged-pr.tsx
@@ -11,8 +11,8 @@ import createBanner from '../github-helpers/banner';
 import TimelineItem from '../github-helpers/timeline-item';
 import attachElement from '../helpers/attach-element';
 import {canEditEveryComment} from './quick-comment-edit';
-import {buildRepoURL, getRepo, isRefinedGitHubRepo} from '../github-helpers';
 import onConversationHeaderUpdate from '../github-events/on-conversation-header-update';
+import {buildRepoURL, getRepo, isRefinedGitHubRepo} from '../github-helpers';
 
 // TODO: Not an exact match; Moderators can edit comments but not create releases
 const canCreateRelease = canEditEveryComment;
