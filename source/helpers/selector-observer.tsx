42358cedbdf70ae3b936595326bed638593a43f8
Federico Brigante <me@fregante.com>
2022-08-05 14:03:33 +0700

    Remove `animation` after being seen so future selector matches can see it

--- source/helpers/selector-observer.tsx
+++ source/helpers/selector-observer.tsx
@@ -4,19 +4,24 @@ import {css} from 'code-tag';
 import onetime from 'onetime';
 import {ParseSelector} from 'typed-query-selector/parser';
 
+import {getSnapshotUUID} from './attach-element';
+
 const animation = 'rgh-selector-observer';
-const tracked = new Map<string, WeakSet<EventTarget>>();
-const getListener = mem(<ExpectedElement extends HTMLElement>(selector: string, callback: (element: ExpectedElement) => void) => function (event: AnimationEvent) {
+const seen = new Map<string, WeakSet<EventTarget>>();
+const getListener = mem(<ExpectedElement extends HTMLElement>(seenMark: string, selector: string, callback: (element: ExpectedElement) => void) => function (event: AnimationEvent) {
 	const target = event.target as ExpectedElement;
-	if (tracked.get(selector)?.has(target) || !target.matches(selector)) {
+	if (seen.get(selector)?.has(target) || !target.matches(selector)) {
 		return;
 	}
+
+	// Removes this specific selector’s animation once it was seen
+	target.classList.add(seenMark);
 
 	if (!/shortenLink|bypass/.test(callback.toString())) {
 		console.log('selector-observer', target, callback);
 	}
 
-	tracked.get(selector)!.add(target);
+	seen.get(selector)!.add(target);
 
 	callback(target);
 });
@@ -40,15 +45,16 @@ export default function observe<
 	}
 
 	const selector = String(selectors); // Array#toString() creates a comma-separated string
+	const seenMark = 'rgh-' + getSnapshotUUID();
 	const rule = new Text(css`
-		:where(${String(selector)}) {
+		:where(${String(selector)}):not(.${seenMark}) {
 			animation: 1ms ${animation};
 		}
 	`);
-	tracked.set(String(selector), new WeakSet());
+	seen.set(String(selector), new WeakSet());
 	getTag().append(rule);
 	signal?.addEventListener('abort', () => {
 		rule.remove();
 	});
-	window.addEventListener('animationstart', getListener(selector, listener), {signal});
+	window.addEventListener('animationstart', getListener(seenMark, selector, listener), {signal});
 }
