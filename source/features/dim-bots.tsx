cef44ba7baac0185cdb84be33f32030e9b49a662
Federico Brigante <me@fregante.com>
2022-09-08 16:56:16 +0700

    Restore dim-bots.tsx

--- source/features/dim-bots.tsx
+++ source/features/dim-bots.tsx
@@ -20,12 +20,11 @@ const commitSelectors = botNames.map(bot => `.commit-author[href$="?author=${bot
 commitSelectors.push('.commit-author[href$="%5Bbot%5D"]'); // Generic `[bot]` label in author name
 const commitSelector = commitSelectors.join(',');
 
-const prSelectors = [
-	...botNames.map(bot => `.opened-by [title*="pull requests opened by ${bot}"]`),
-	...botNames.map(bot => `.opened-by [title*="pull requests created by ${bot}"]`), // PRs in https://github.com/pulls page
+const prSelectors = botNames.map(bot => `.opened-by [title="pull requests opened by ${bot}"]`);
+prSelectors.push(
 	'.opened-by [href*="author%3Aapp%2F"]', // Search query `is:pr+author:app/*`
 	'.labels [href$="label%3Abot"]', // PR tagged with `bot` label
-];
+);
 const prSelector = prSelectors.join(',');
 
 function init(): void {
