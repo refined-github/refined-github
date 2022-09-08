a592b94090fbff5b09386d8eef7f711f8dc87129
Federico Brigante <me@fregante.com>
2022-09-08 21:11:20 +0700

    Update source/features/dim-bots.tsx
    Co-authored-by: Gaëtan Maisse <gaetanmaisse@gmail.com>

--- source/features/dim-bots.tsx
+++ source/features/dim-bots.tsx
@@ -20,7 +20,7 @@ const commitSelectors = botNames.map(bot => `.commit-author[href$="?author=${bot
 commitSelectors.push('.commit-author[href$="%5Bbot%5D"]'); // Generic `[bot]` label in author name
 const commitSelector = commitSelectors.join(',');
 
-const prSelectors = botNames.map(bot => `.opened-by [title="pull requests created by ${bot}"]`);
+const prSelectors = botNames.map(bot => `.opened-by [title*="pull requests created by ${bot}"]`);
 prSelectors.push(
 	'.opened-by [href*="author%3Aapp%2F"]', // Search query `is:pr+author:app/*`
 	'.labels [href$="label%3Abot"]', // PR tagged with `bot` label
