653552a654b843d2eac91b0364d7e12436ba5ac7
Christian Avendaño <chcamiloam@gmail.com>
2022-09-09 19:46:06 +0200

    Restore `hidden-review-comments-indicator` (#5950)
    Co-authored-by: Federico Brigante <me@fregante.com>

--- source/features/hidden-review-comments-indicator.tsx
+++ source/features/hidden-review-comments-indicator.tsx
@@ -25,8 +25,7 @@ const handleIndicatorClick = ({delegateTarget}: DelegateEvent): void => {
 
 // `mem` avoids adding the indicator twice to the same thread
 const addIndicator = mem((commentThread: HTMLElement): void => {
-	const commentCount = commentThread.querySelectorAll('.review-comment .js-comment').length;
-
+	const commentCount = commentThread.querySelectorAll('.review-comment.js-comment').length;
 	commentThread.before(
 		<tr>
 			<td className="rgh-comments-indicator blob-num" colSpan={2}>
