9138a68f03fdcae753ea5dbcb3343da6ad33f97f
Federico Brigante <me@fregante.com>
2022-09-10 00:13:59 +0700

    Restore vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import {defineConfig} from 'vitest/config';
 export default defineConfig({
 	test: {
 		setupFiles: [
-			'./test/fixtures/globals.js',
+			'./test/fixtures/globals.ts',
 		],
 	},
 });
