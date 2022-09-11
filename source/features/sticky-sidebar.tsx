df33c0babc0cd83c1a78ce06e16f393d4d332ad2
Federico Brigante <me@fregante.com>
2022-09-11 19:08:39 +0700

    Attempt for #5939
    Issue: when resizing the window, you're most likely hovering the sidebar as well, so the whole feature breaks 50% of the time

--- source/features/sticky-sidebar.tsx
+++ source/features/sticky-sidebar.tsx
@@ -1,35 +1,59 @@
 import './sticky-sidebar.css';
-import select from 'select-dom';
 import debounce from 'debounce-fn';
-import {observe} from 'selector-observer';
 import * as pageDetect from 'github-url-detection';
 
 import features from '.';
+import observe from '../helpers/selector-observer';
 import onAbort from '../helpers/abort-controller';
 
-// The first selector in the parentheses is for the repo root, the second one for conversation pages
-const sidebarSelector = '.Layout-sidebar :is(.BorderGrid, #partial-discussion-sidebar)';
+let sidebar: HTMLElement;
+let isHovered = false;
+const onResize = debounce(updateStickiness, {wait: 100});
+const resizeObserver = new ResizeObserver(onResize);
 
-function updateStickiness(): void {
-	const sidebar = select(sidebarSelector)!;
-	const margin = pageDetect.isConversation() ? 60 : 0; // 60 matches sticky header's height
-	sidebar.classList.toggle('rgh-sticky-sidebar', sidebar.offsetHeight + margin < window.innerHeight);
+function toggleHoverState(event: MouseEvent): void {
+	isHovered = event.type === 'mouseenter';
 }
 
-const onResize = debounce(updateStickiness, {wait: 100});
+// Can't use delegate because it's not efficient to track mouse events across the document
+function trackSidebar(foundSidebar: HTMLElement, signal: AbortSignal): void {
+	sidebar = foundSidebar;
+	resizeObserver.observe(sidebar, {box: 'border-box'});
+
+	sidebar.addEventListener('mouseenter', toggleHoverState, {signal});
+	sidebar.addEventListener('mouseleave', toggleHoverState, {signal});
+}
+
+function updateStickiness(): void {
+	if (isHovered) {
+		return;
+	}
+
+	const margin = pageDetect.isConversation() ? 60 : 0; // 60 matches sticky header's height
+	sidebar.classList.toggle(
+		'rgh-sticky-sidebar',
+		sidebar.offsetHeight + margin < window.innerHeight,
+	);
+}
+
 
 function init(signal: AbortSignal): void {
+	isHovered = false;
 	document.documentElement.classList.add('rgh-sticky-sidebar-enabled');
 
-	const resizeObserver = new ResizeObserver(onResize);
-	const selectObserver = observe(sidebarSelector, {
-		add(sidebar) {
-			resizeObserver.observe(sidebar, {box: 'border-box'});
-		},
-	});
 	window.addEventListener('resize', onResize, {signal});
+	window.addEventListener('load', onResize, {signal});
 
-	onAbort(signal, resizeObserver, selectObserver);
+	observe(
+		// The first selector in the parentheses is for the repo root, the second one for conversation pages
+		'.Layout-sidebar :is(.BorderGrid, #partial-discussion-sidebar)',
+		element => {
+			trackSidebar(element, signal);
+		},
+		{signal},
+	);
+	updateStickiness();
+	onAbort(signal, resizeObserver);
 }
 
 void features.add(import.meta.url, {
@@ -40,6 +64,6 @@ void features.add(import.meta.url, {
 	exclude: [
 		pageDetect.isEmptyRepoRoot,
 	],
-	deduplicate: 'has-rgh-inner',
+	deduplicate: false,
 	init,
 });
