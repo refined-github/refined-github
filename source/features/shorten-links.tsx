5bbdaef2702bbe667a373fe91ddb99b73d7223a6
Federico Brigante <me@fregante.com>
2022-09-12 16:36:14 +0700

    Limit `shorten-links` to only Markdown links (#5962)

--- source/features/shorten-links.tsx
+++ source/features/shorten-links.tsx
@@ -1,5 +1,4 @@
 import onetime from 'onetime';
-import * as pageDetect from 'github-url-detection';
 
 import features from '.';
 import {linkifiedURLClass, shortenLink} from '../github-helpers/dom-formatters';
@@ -7,21 +6,16 @@ import observe from '../helpers/selector-observer';
 
 /* This feature is currently so broad that it's not de-inited via signal, it's just run once for all pageloads #5889 */
 function init(): void {
-	observe(`a[href]:not(.${linkifiedURLClass})`, shortenLink);
+	observe(`.comment-body a[href]:not(.${linkifiedURLClass})`, shortenLink);
 }
 
 void features.add(import.meta.url, {
-	exclude: [
-		// Due to GitHub’s bug: #2828
-		pageDetect.isGlobalSearchResults,
-	],
 	init: onetime(init),
 });
 
 /*
-
 ## Test URLs
 
 https://github.com/refined-github/sandbox/pull/14
-
+https://github.com/refined-github/refined-github/pull/473
 */
