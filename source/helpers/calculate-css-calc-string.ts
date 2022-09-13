49d7007b4797dc1b74b7aca49b3684e750345228
Federico Brigante <me@fregante.com>
2022-09-13 18:05:38 +0700

    Don't change `sticky-sidebar` state while interacting with it (#5961)

---
+++ source/helpers/calculate-css-calc-string.ts
@@ -0,0 +1,12 @@
+import looseParseInt from './loose-parse-int';
+
+/**
+Compute the sum in a `calc()`. Only works with very simple sums of px values.
+When used with custom properties, `calc()` are not evaluated when retrieved via `getComputedStyle()`
+
+@example calculateCssCalcString('calc(1px + 2px)') => 3;
+*/
+export default function calculateCssCalcString(string: string): number {
+	const addends = string.split('+').map(part => looseParseInt(part));
+	return addends.reduce((a, b) => a + b);
+}
