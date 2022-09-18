00bd4452997187e475a9c1720d232fead5c4d754
Federico Brigante <me@fregante.com>
2022-09-19 00:51:23 +0700

    Make `show-whitespace` more reliable (#5994)

--- source/features/show-whitespace.tsx
+++ source/features/show-whitespace.tsx
@@ -1,14 +1,11 @@
 import './show-whitespace.css';
-import select from 'select-dom';
-import delegate from 'delegate-it';
 import * as pageDetect from 'github-url-detection';
 
 import features from '.';
-import onNewComments from '../github-events/on-new-comments';
-import {onDiffFileLoad} from '../github-events/on-fragment-load';
 import {codeElementsSelector} from '../github-helpers/dom-formatters';
 import showWhiteSpacesOnLine from '../helpers/show-whitespace-on-line';
 import onAbort from '../helpers/abort-controller';
+import observe from '../helpers/selector-observer';
 
 const viewportObserver = new IntersectionObserver(changes => {
 	for (const {target: line, isIntersecting} of changes) {
@@ -20,19 +17,12 @@ const viewportObserver = new IntersectionObserver(changes => {
 	}
 });
 
-function observeWhiteSpace(): void {
-	for (const line of select.all(`:is(${codeElementsSelector}):not(.rgh-observing-whitespace, .blob-code-hunk)`)) {
-		line.classList.add('rgh-observing-whitespace');
-		viewportObserver.observe(line);
-	}
+function showWhitespaceWhenInViewport(line: HTMLElement): void {
+	viewportObserver.observe(line);
 }
 
 function init(signal: AbortSignal): void {
-	observeWhiteSpace();
-
-	// Show whitespace on new review suggestions #2852
-	// This event is not very reliable as it also triggers when review comments are edited or deleted
-	delegate(document, '.js-pull-refresh-on-pjax', 'socket:message', observeWhiteSpace, {capture: true, signal});
+	observe(`:is(${codeElementsSelector}):not(.blob-code-hunk)`, showWhitespaceWhenInViewport, {signal});
 	onAbort(signal, viewportObserver);
 }
 
@@ -40,10 +30,7 @@ void features.add(import.meta.url, {
 	include: [
 		pageDetect.hasCode,
 	],
-	additionalListeners: [
-		onNewComments,
-		onDiffFileLoad,
-	],
+	awaitDomReady: false,
 	deduplicate: false,
 	init,
 });
