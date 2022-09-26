5e5c87a479dbdb531fb79788d77ebe098c4a2766
Federico Brigante <me@fregante.com>
2022-09-26 17:19:49 +0700

    Cleanup `no-unnecessary-split-diff-view`

--- source/features/no-unnecessary-split-diff-view.tsx
+++ source/features/no-unnecessary-split-diff-view.tsx
@@ -3,41 +3,71 @@ import select from 'select-dom';
 import * as pageDetect from 'github-url-detection';
 
 import features from '../feature-manager';
-import {onDiffFileLoad} from '../github-events/on-fragment-load';
+import observe from '../helpers/selector-observer';
 
 function isUnifiedDiff(): boolean {
+	// TODO: Maybe use this when available on isCompare
+	// select.exists('meta[name="diff-view"][content="split"]'),
 	return select.exists([
 		'[value="unified"][checked]', // Form in PR
 		'.table-of-contents .selected[href*="diff=unified"]', // Link in single commit
 	]);
 }
 
+// TODO: Replace with CSS-only :has?
 function init(): void {
-	for (const diffTable of select.all('.js-diff-table:not(.rgh-no-unnecessary-split-diff-view-visited)')) {
-		diffTable.classList.add('rgh-no-unnecessary-split-diff-view-visited');
+	observe('.js-diff-table', diffTable => {
 		for (const side of ['left', 'right']) {
 			if (!select.exists(`[data-split-side="${side}"]:is(.blob-code-addition, .blob-code-deletion)`, diffTable)) {
 				diffTable.setAttribute('data-rgh-hide-empty-split-diff-side', side);
 				break;
 			}
 		}
-	}
+	});
 }
+
+// Make sure the class names we need exist on the page #4483
+const hasRequiredClasses = (): boolean => select.exists(`
+	.js-diff-table
+	:is(
+		[data-split-side="left"],
+		[data-split-side="right"]
+	):is(
+		.blob-code-addition,
+		.blob-code-deletion
+	)
+`);
 
 void features.add(import.meta.url, {
 	asLongAs: [
-		// Make sure the class names we need exist on the page #4483
-		() => select.exists('.js-diff-table :is([data-split-side="left"], [data-split-side="right"]):is(.blob-code-addition, .blob-code-deletion)'),
-	],
-	include: [
+		hasRequiredClasses,
+		isUnifiedDiff,
 		pageDetect.hasFiles,
 	],
-	exclude: [
-		isUnifiedDiff,
-	],
-	additionalListeners: [
-		onDiffFileLoad,
-	],
-	deduplicate: 'has-rgh-inner',
 	init,
 });
+
+/*
+
+## Test URLs
+### Right side only
+
+https://github.com/sindresorhus/refined-github/pull/4296/files?diff=split
+
+### Left side only
+
+https://github.com/sindresorhus/refined-github/pull/3637/files?diff=split
+
+### Single-side split diffs & regular split diffs
+
+https://github.com/sindresorhus/refined-github/pull/4382/files?diff=split
+
+### Compare page
+
+https://github.com/sindresorhus/refined-github/compare/main...cheap-glitch:add-hide-empty-split-diff-side?diff=split
+
+### Single commit
+
+https://github.com/sindresorhus/refined-github/commit/3b1359ea465ff5c4d3f0f79e2d6781c7ce9a8283?diff=split
+
+*/
