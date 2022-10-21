52f92c118ffa18360ebc33668e8b30cab805500f
Momin Khan <foolmoron@gmail.com>
2022-10-21 13:44:49 -0400

    Clean up and document math

--- source/helpers/math.ts
+++ source/helpers/math.ts
@@ -1,28 +1,50 @@
+/**
+ * Linear interpolation. Given a range and a value, returns the
+ * percentage position in the range for the value.
+ *
+ * @param min Lowest value in the range
+ * @param max Highest value in the range
+ * @param t Interpolation amount from 0.0 to 1.0 (or beyond to get a value outside the range [min, max])
+ * @returns Interpolated value
+ */
 export function lerp(min: number, max: number, t: number): number {
 	return min + ((max - min) * t);
 }
 
+/**
+ * Inverse operation of lerp. Given a range and a value, returns the
+ * interpolation amount to get that value.
+ *
+ * @param min Lowest value in the range
+ * @param max Highest value in the range
+ * @param x Value in the range
+ * @returns Interpolation amount from 0.0 to 1.0 (or beyond if `x` is outside the range [min, max])
+ */
 export function invlerp(min: number, max: number, x: number): number {
 	return (x - min) / (max - min);
 }
 
-export function calculateMinMax(values: number[]): {min: number; max: number} {
-	return {
-		min: Math.min(...values),
-		max: Math.max(...values),
-	};
-}
-
-export function createHeatIndexFunc(values: number[]): (value: number) => number {
+/**
+ * Create a function that calculates a GitHub heat index based on a list of numbers.
+ * The list is used to find the min and max of the range. The function can then
+ * calculate a heat index for a given value based on that range.
+ *
+ * GitHub has 10 heat values, and uses a lower heat index for a higher value. So the
+ * highest heat index that will be returned is 1, and the lowest is 10.
+ *
+ * @param numbers values to use to calculate the min and max of the heat range
+ * @returns function to calculate heat index of a given number
+ */
+export function createHeatIndexFunc(numbers: number[]): (value: number) => number {
 	const steps = 10; // GH has 10 heat colors
-	const {min, max} = calculateMinMax(values);
+	const min = Math.min(...numbers);
+	const max = Math.max(...numbers);
 
 	return (value: number) => {
 		// Inverse lerp figures out how far the value is between min & max
-		const interp = invlerp(min, max, value);
+		const interp = Math.max(0, Math.min(1, invlerp(min, max, value)));
 
-		// Higher heat values have a lower index, 1 is highest and 10 lowest.
-		// This maps the [0.0, 1.0] value to [10, 1].
+		// Maps the [0.0, 1.0] value to [steps, 1]
 		const floored = Math.floor(interp * steps);
 		const heatIndex = Math.max(1, steps - floored);
 
