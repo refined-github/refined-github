62f1ac3160d40dd86940c9d8eac8e50c138cd8a4
Federico Brigante <me@fregante.com>
2022-10-26 15:09:17 +0700

    Open RGH links in current tab unless we're on a form (#6082)

--- source/features/view-last-pr-deployment.tsx
+++ source/features/view-last-pr-deployment.tsx
@@ -1,7 +1,7 @@
 import React from 'dom-chef';
 import select from 'select-dom';
 import * as pageDetect from 'github-url-detection';
-import {LinkExternalIcon} from '@primer/octicons-react';
+import {GlobeIcon} from '@primer/octicons-react';
 
 import features from '../feature-manager';
 import onConversationHeaderUpdate from '../github-events/on-conversation-header-update';
@@ -18,10 +18,8 @@ function init(): void {
 		<a
 			className="rgh-last-deployment btn btn-sm d-none d-md-block mr-1"
 			href={href}
-			target="_blank"
-			rel="noreferrer"
 		>
-			<LinkExternalIcon className="mr-1"/> View deployment
+			<GlobeIcon className="mr-1"/> View deployment
 		</a>,
 	);
 }
