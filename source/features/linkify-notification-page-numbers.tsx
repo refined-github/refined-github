77a0942fe7177b258baf44e81c409745c8514740
yakov116 <16872793+yakov116@users.noreply.github.com>
2022-11-03 19:55:55 +0000

    Only linkify the number

--- source/features/linkify-notification-page-numbers.tsx
+++ source/features/linkify-notification-page-numbers.tsx
@@ -2,19 +2,18 @@ import React from 'dom-chef';
 import select from 'select-dom';
 import * as pageDetect from 'github-url-detection';
 
-import {wrap} from '../helpers/dom-utils';
 import features from '../feature-manager';
 import looseParseInt from '../helpers/loose-parse-int';
 
 function init(): void {
 	const nextButtonURL = new URL(select('a[data-hotkey="ArrowRight"]')!.href);
 	const nextButtonSearch = new URLSearchParams(nextButtonURL.search);
-	const lastNotificationPage = select('.js-notifications-list-paginator-counts')!.lastChild!;
-	const lastNotificationPageNumber = looseParseInt(lastNotificationPage);
+	const lastNotificationPageNode = select('.js-notifications-list-paginator-counts')!.lastChild!;
+	const lastNotificationPageNumber = looseParseInt(lastNotificationPageNode);
 	const lastCursor = Math.floor(lastNotificationPageNumber / 50) * 50;
 	nextButtonSearch.set('after', btoa('cursor:' + String(lastCursor)));
 	nextButtonURL.search = nextButtonSearch.toString();
-	wrap(lastNotificationPage, <a href={nextButtonURL.href}/>);
+	lastNotificationPageNode.replaceWith(<> of <a className="rgh-link" href={nextButtonURL.href}>{lastNotificationPageNumber}</a></>);
 }
 
 void features.add(import.meta.url, {
@@ -24,6 +23,8 @@ void features.add(import.meta.url, {
 	include: [
 		pageDetect.isNotifications,
 	],
-	deduplicate: 'has-rgh',
+	exclude: [
+		() => select.exists('.rgh-link'),
+	],
 	init,
 });
