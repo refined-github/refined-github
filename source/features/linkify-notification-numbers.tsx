5409cf7e0ae624c83fe730db66d714798e33c625
yakov116 <16872793+yakov116@users.noreply.github.com>
2022-11-03 16:57:02 +0000

    Fix up

--- source/features/linkify-notification-numbers.tsx
+++ source/features/linkify-notification-numbers.tsx
@@ -7,17 +7,23 @@ import looseParseInt from '../helpers/loose-parse-int';
 import {wrap} from '../helpers/dom-utils';
 
 function init(): void {
-	const nextButtonSearch = new URLSearchParams(select('a[aria-label="Next"][data-hotkey="ArrowRight"]')!.search);
+	const nextButtonURL = new URL(select('a[data-hotkey="ArrowRight"]')!.href);
+	const nextButtonSearch = new URLSearchParams(nextButtonURL.search);
 	const lastNotificationPage = select('.js-notifications-list-paginator-counts')!.lastChild!;
 	const lastNotificationPageNumber = looseParseInt(lastNotificationPage);
 	const lastCursor = Math.floor(lastNotificationPageNumber / 50) * 50;
 	nextButtonSearch.set('after', btoa('cursor:' + String(lastCursor)));
-	wrap(lastNotificationPage, <a href={nextButtonSearch}/>);
+	nextButtonURL.search = nextButtonSearch.toString();
+	wrap(lastNotificationPage, <a href={nextButtonURL.href}/>);
 }
 
 void features.add(import.meta.url, {
 	include: [
 		pageDetect.isNotifications,
+	],
+	exclude: [
+		pageDetect.isBlank, // Empty notification list
+		() => !select.exists('[disabled="disabled"][data-hotkey="ArrowRight"]'), // No next page
 	],
 	deduplicate: 'has-rgh',
 	init,
