df46c6b64ef57c46de3b956a9c05e21e7b6b7ae1
Yakov <16872793+yakov116@users.noreply.github.com>
2022-11-03 21:57:29 -0400

    Clean up

--- source/features/last-notification-page-button.tsx
+++ source/features/last-notification-page-button.tsx
@@ -5,19 +5,23 @@ import * as pageDetect from 'github-url-detection';
 import features from '../feature-manager';
 import looseParseInt from '../helpers/loose-parse-int';
 
-function init(): void {
-	const nextButtonURL = new URL(select('a[data-hotkey="ArrowRight"]')!.href);
-	const nextButtonSearch = new URLSearchParams(nextButtonURL.search);
+function init(): void | false {
+	const nextButton = select('a[data-hotkey="ArrowRight"]');
+	if (!nextButton) {
+		return false;
+	}
+
 	const lastNotificationPageNode = select('.js-notifications-list-paginator-counts')!.lastChild!;
 	const lastNotificationPageNumber = looseParseInt(lastNotificationPageNode);
 	const lastCursor = Math.floor(lastNotificationPageNumber / 50) * 50;
+	const nextButtonSearch = new URLSearchParams(nextButton.search);
 	nextButtonSearch.set('after', btoa('cursor:' + String(lastCursor)));
-	nextButtonURL.search = nextButtonSearch.toString();
+	nextButton.search = nextButtonSearch.toString();
 	lastNotificationPageNode.replaceWith(' of ',
 		<a
 			style={{textDecoration: 'underline'}}
 			className="rgh-last-notification-page-button"
-			href={nextButtonURL.href}
+			href={nextButton.href}
 		>
 			{lastNotificationPageNumber}
 		</a>,
@@ -25,9 +29,6 @@ function init(): void {
 }
 
 void features.add(import.meta.url, {
-	asLongAs: [
-		() => select.exists('[data-hotkey="ArrowRight"]:not([disabled])'),
-	],
 	include: [
 		pageDetect.isNotifications,
 	],
