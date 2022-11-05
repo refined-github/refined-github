ea84f3045e6b52ff3b150ec30e881bc90f929476
Federico Brigante <me@fregante.com>
2022-11-05 23:11:38 +0700

    Use observer instead

--- source/features/view-last-pr-deployment.tsx
+++ source/features/view-last-pr-deployment.tsx
@@ -4,37 +4,35 @@ import * as pageDetect from 'github-url-detection';
 import {LinkExternalIcon} from '@primer/octicons-react';
 
 import features from '../feature-manager';
-import onConversationHeaderUpdate from '../github-events/on-conversation-header-update';
-import attachElement from '../helpers/attach-element';
+import observe from '../helpers/selector-observer';
 
-function init(): void | false {
-	const lastDeployment = select.last<HTMLAnchorElement>('.js-timeline-item a[title="Deployment has completed"]');
+function addLink(header: HTMLElement): void {
+	const lastDeployment = select.last('.js-timeline-item a[title="Deployment has completed"]');
 	if (!lastDeployment) {
-		return false;
+		return;
 	}
 
-	attachElement('.gh-header-actions', {
-		prepend: () => (
-			<a
-				className="rgh-last-deployment btn btn-sm d-none d-md-block mr-1"
-				target="_blank" // Matches GitHub’s own behavior
-				rel="noopener noreferrer"
-				href={lastDeployment.href}
-			>
-				<LinkExternalIcon className="mr-1"/> Last deployment
-			</a>
-		),
-	});
+	header.prepend(
+		<a
+			className="rgh-last-deployment btn btn-sm d-none d-md-block mr-1"
+			target="_blank" // Matches GitHub’s own behavior
+			rel="noopener noreferrer"
+			href={lastDeployment.href}
+		>
+			<LinkExternalIcon className="mr-1"/>
+			Last deployment
+		</a>
+	);
+}
+
+function init(signal: AbortSignal): void {
+	observe('.gh-header-actions', addLink, {signal});
 }
 
 void features.add(import.meta.url, {
 	include: [
 		pageDetect.isPRConversation,
 	],
-	additionalListeners: [
-		onConversationHeaderUpdate,
-	],
-	deduplicate: '.rgh-last-deployment',
 	init,
 });
 
