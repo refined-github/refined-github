f73801dd75b2e45f30a1324e3101c068c8e39fe7
yakov116 <16872793+yakov116@users.noreply.github.com>
2022-11-06 16:38:32 +0000

    Support up to 4 months (125 days)

--- source/features/file-age-color.tsx
+++ source/features/file-age-color.tsx
@@ -3,19 +3,6 @@ import * as pageDetect from 'github-url-detection';
 import observe from '../helpers/selector-observer';
 import features from '../feature-manager';
 
-const heatMap = new Map([
-	['3', '1'],
-	['6', '2'],
-	['15', '3'],
-	['24', '4'],
-	['40', '5'],
-	['70', '6'],
-	['100', '7'],
-	['160', '8'],
-	['275', '9'],
-	['30000', '10'],
-]);
-
 function daysDiff(date1: Date, date2 = new Date()): number {
 	const diffTime = Math.abs(date2.getTime() - date1.getTime());
 	const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
@@ -24,15 +11,7 @@ function daysDiff(date1: Date, date2 = new Date()): number {
 
 function addHeatIndex(timeAgo: HTMLElement): void {
 	const dateDiff = daysDiff(new Date(timeAgo.attributes.datetime.value));
-	let heatIndex = '0';
-	for (const [days, heat] of heatMap) {
-		if (dateDiff <= Number(days)) {
-			heatIndex = heat;
-			return;
-		}
-	}
-
-	timeAgo.dataset.rghHeat = heatIndex;
+	timeAgo.dataset.rghHeat = String(Math.sqrt(dateDiff / 1).toFixed(0));
 }
 
 function init(signal: AbortSignal): void {
