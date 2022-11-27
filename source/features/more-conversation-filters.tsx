1b9fb8622180dda62577d7c66b2b8b6e7c4987a6
Federico Brigante <me@fregante.com>
2022-11-27 16:31:28 +0800

    Replace `commented:` filters with `involved:`

--- source/features/more-conversation-filters.tsx
+++ source/features/more-conversation-filters.tsx
@@ -7,16 +7,16 @@ import SearchQuery from '../github-helpers/search-query';
 function init(): void {
 	const sourceItem = select('#filters-select-menu a:nth-last-child(2)')!;
 
-	// Add "Everything commented by you" filter
+	// "Involved" filter
 	const commentsLink = sourceItem.cloneNode(true);
-	commentsLink.lastChild!.textContent = 'Everything commented by you';
+	commentsLink.lastChild!.textContent = 'Everything you’re involved in';
 	commentsLink.removeAttribute('target');
-	commentsLink.href = SearchQuery.from(commentsLink).set('is:open commenter:@me').href;
+	commentsLink.href = SearchQuery.from(commentsLink).set('is:open involved:@me').href;
 	commentsLink.setAttribute('aria-checked', String(commentsLink.href === location.href)); // #4589
 
 	sourceItem.after(commentsLink);
 
-	// Add "Everything you subscribed to" link
+	// "Subscribed" link
 	const subscriptionsLink = commentsLink.cloneNode(true);
 	subscriptionsLink.lastChild!.textContent = 'Everything you subscribed to';
 	subscriptionsLink.setAttribute('aria-checked', 'false'); // #4589
