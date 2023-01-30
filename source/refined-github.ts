3ed10d90435905d5414d3cf1de431eafca58b213
Federico Brigante <me@fregante.com>
2023-01-30 16:02:55 +0800

    Drop `global-search-filters` feature

--- source/refined-github.ts
+++ source/refined-github.ts
@@ -198,7 +198,6 @@ import './features/conversation-activity-filter';
 import './features/select-all-notifications-shortcut';
 import './features/no-duplicate-list-update-time';
 import './features/view-last-pr-deployment';
-import './features/global-search-filters';
 import './features/clean-header-search-field';
 import './features/avoid-accidental-submissions';
 import './features/quick-review-comment-deletion';
