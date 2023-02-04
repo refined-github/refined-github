17e82d6c8d39e93ca7f585042450dbe2762d240a
ambujsahu81 <Ambuj.sahu1998@gmail.com>
2023-02-04 22:06:12 +0530

    replace select with observe

--- source/features/rgh-linkify-yolo-issues.tsx
+++ source/features/rgh-linkify-yolo-issues.tsx
@@ -1,17 +1,19 @@
 import React from 'dom-chef';
-import select from 'select-dom';
 import * as pageDetect from 'github-url-detection';
 
 import {wrap} from '../helpers/dom-utils';
 import features from '../feature-manager';
 import {getRghIssueUrl} from '../helpers/rgh-issue-link';
 import {isRefinedGitHubYoloRepo} from '../github-helpers';
+import observe from '../helpers/selector-observer';
 
-function init(): void {
+function linkify(issueCell: HTMLElement): void {
+	wrap(issueCell.firstChild!, <a href={getRghIssueUrl(issueCell.textContent!)}/>);
+}
+
+function init(signal: AbortSignal): void {
 	// .js-csv-data is the old selector
-	for (const issueCell of select.all(':is(.js-csv-data, .react-csv-row) td:nth-child(3)')) {
-		wrap(issueCell.firstChild!, <a href={getRghIssueUrl(issueCell.textContent!)}/>);
-	}
+	observe(':is(.js-csv-data, .react-csv-row) td:nth-child(3)', linkify, {signal});
 }
 
 void features.add(import.meta.url, {
@@ -20,5 +22,6 @@ void features.add(import.meta.url, {
 		pageDetect.isSingleFile,
 		() => location.pathname.endsWith('broken-features.csv'),
 	],
+	awaitDomReady: false,
 	init,
 });
