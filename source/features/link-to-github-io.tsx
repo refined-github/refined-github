2feeb1d5a9844ee2025b850e0b9daa2e34387359
ambujsahu81 <Ambuj.sahu1998@gmail.com>
2023-02-06 16:03:39 +0530

    fix lint issues

--- source/features/link-to-github-io.tsx
+++ source/features/link-to-github-io.tsx
@@ -9,7 +9,7 @@ import observe from '../helpers/selector-observer';
 function getLinkToGitHubIo(repoTitle: HTMLElement, className?: string): JSX.Element {
 	return (
 		<a
-			href={`https://${repoTitle.textContent!.trim().replace('.com','.io')}`}
+			href={`https://${repoTitle.textContent!.trim().replace('.com', '.io')}`}
 			className={className}
 		>
 			<LinkIcon className="v-align-middle"/>
