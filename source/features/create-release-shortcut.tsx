b67667dcb69ca779689b0cb0bd053479197da48b
Federico Brigante <me@fregante.com>
2023-02-11 17:43:38 +0800

    Restore

---
+++ source/features/create-release-shortcut.tsx
@@ -0,0 +1,30 @@
+import * as pageDetect from 'github-url-detection';
+
+import features from '../feature-manager';
+import {registerHotkey} from '../github-helpers/hotkey';
+import {buildRepoURL} from '../github-helpers';
+
+function init(): void {
+	// Reasoning for this feature: #1254
+	registerHotkey('c', buildRepoURL('releases/new'));
+}
+
+void features.add(import.meta.url, {
+	shortcuts: {
+		c: 'Create a new release',
+	},
+	include: [
+		pageDetect.isReleasesOrTags,
+	],
+	init,
+});
+
+/*
+
+Test URLs
+
+https://github.com/refined-github/refined-github/releases
+https://github.com/refined-github/sandbox/releases/new
+https://github.com/refined-github/sandbox/releases/tag/cool
+
+*/
--- source/refined-github.ts
+++ source/refined-github.ts
@@ -82,6 +82,7 @@ import './features/embed-gist-inline';
 import './features/comments-time-machine-links';
 import './features/hide-issue-list-autocomplete';
 import './features/esc-to-deselect-line';
+import './features/create-release-shortcut';
 import './features/patch-diff-links';
 import './features/parse-backticks';
 import './features/mark-merge-commits-in-list';
