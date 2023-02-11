da05ce30d3cc172a2a34ba1d32cab3e5b97c3b1d
Federico Brigante <me@fregante.com>
2023-02-11 17:52:43 +0800

    Make `create-release-shortcut` more resilient (#6324)

--- source/features/create-release-shortcut.tsx
+++ source/features/create-release-shortcut.tsx
@@ -1,27 +1,34 @@
-import select from 'select-dom';
 import * as pageDetect from 'github-url-detection';
 
 import features from '../feature-manager';
-import {addHotkey} from '../github-helpers/hotkey';
-import addQuickSubmit from './submission-via-ctrl-enter-everywhere';
+import {registerHotkey} from '../github-helpers/hotkey';
+import {buildRepoURL} from '../github-helpers';
 
 function init(): void {
-	addHotkey(select('a[href$="/releases/new"]'), 'c');
+	// Reasoning for this feature: #1254
+	registerHotkey('c', buildRepoURL('releases/new'));
 }
 
 void features.add(import.meta.url, {
 	shortcuts: {
 		c: 'Create a new release',
-		'ctrl enter': 'Publish a release',
 	},
 	include: [
 		pageDetect.isReleasesOrTags,
 	],
-	init,
-}, {
-	include: [
+	exclude: [
 		pageDetect.isNewRelease,
 		pageDetect.isEditingRelease,
 	],
-	init: addQuickSubmit,
+	init,
 });
+
+/*
+
+Test URLs
+
+https://github.com/refined-github/refined-github/releases
+https://github.com/refined-github/sandbox/releases/new
+https://github.com/refined-github/sandbox/releases/tag/cool
+
+*/
