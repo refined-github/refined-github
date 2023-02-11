b2f0f5e153a830b6f5ce191d2aa34b717e713d87
Federico Brigante <me@fregante.com>
2023-02-11 17:29:41 +0800

    Add `quick-new-issue` feature

---
+++ source/features/quick-new-issue.tsx
@@ -0,0 +1,34 @@
+import React from 'dom-chef';
+import * as pageDetect from 'github-url-detection';
+
+import features from '../feature-manager';
+import {buildRepoURL, getRepo, isArchivedRepoAsync} from '../github-helpers';
+import observe from '../helpers/selector-observer';
+
+function add(dropdownMenu: HTMLElement): void {
+	dropdownMenu.append(
+		<div role="none" className="dropdown-divider"/>,
+		<div className="dropdown-header">
+			<span title={getRepo()?.name}>This repository</span>
+		</div>,
+		<a role="menuitem" className="dropdown-item" href={buildRepoURL('issues/new/choose')}>
+			New issue
+		</a>,
+	);
+}
+
+async function init(signal: AbortSignal): Promise<void | false> {
+	if (await isArchivedRepoAsync()) {
+		return false;
+	}
+
+	observe('.Header-item .dropdown-menu:has(> [data-ga-click="Header, create new repository"])', add, {signal});
+}
+
+void features.add(import.meta.url, {
+	include: [
+		pageDetect.isRepo,
+	],
+	awaitDomReady: false,
+	init,
+});
