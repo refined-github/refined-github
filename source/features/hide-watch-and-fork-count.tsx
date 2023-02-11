d7fa28a2999269131c89a3962be3334bf566816c
Federico Brigante <me@fregante.com>
2023-02-11 18:00:14 +0800

    Let user disable 2 more CSS features

---
+++ source/features/hide-watch-and-fork-count.tsx
@@ -0,0 +1,5 @@
+import './hide-watch-and-fork-count.css';
+
+import features from '../feature-manager';
+
+void features.addCssFeature(import.meta.url);
---
+++ source/features/scrollable-code-and-blockquote.tsx
@@ -0,0 +1,5 @@
+import './scrollable-code-and-blockquote.css';
+
+import features from '../feature-manager';
+
+void features.addCssFeature(import.meta.url);
