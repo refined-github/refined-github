77ba29b2be8cde3ab57dcb7ca857c5815046c418
Federico Brigante <me@fregante.com>
2023-02-12 21:17:08 +0800

    skrt skrttt

--- source/features/file-age-color.tsx
+++ source/features/file-age-color.tsx
@@ -3,15 +3,11 @@ import * as pageDetect from 'github-url-detection';
 import observe from '../helpers/selector-observer';
 import features from '../feature-manager';
 
-function daysDiff(date1: Date, date2 = new Date()): number {
-	const diffTime = Math.abs(date2.getTime() - date1.getTime());
-	const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-	return diffDays;
-}
-
 function addHeatIndex(timeAgo: HTMLElement): void {
-	const dateDiff = daysDiff(new Date(timeAgo.attributes.datetime.value));
-	timeAgo.dataset.rghHeat = String(Math.sqrt(dateDiff / 2.5).toFixed(0));
+	const diff = Date.now() - new Date(timeAgo.attributes.datetime.value).getTime();
+
+	// Create heat square root curve
+	timeAgo.dataset.rghHeat = String(Math.max(0, Math.min(10, Math.sqrt(diff / 400_000_000))));
 }
 
 function init(signal: AbortSignal): void {
@@ -22,5 +18,6 @@ void features.add(import.meta.url, {
 	include: [
 		pageDetect.isRepoTree,
 	],
+	awaitDomReady: false,
 	init,
 });
