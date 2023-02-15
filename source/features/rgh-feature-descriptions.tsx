3238584a860796e323d43b357dde81f5ead440b6
Amandeep Singh Malhotra <amandeep.malhotra.894@gmail.com>
2023-02-15 14:21:08 +0530

    Fixed feature description overlapping with first line of code

--- source/features/rgh-feature-descriptions.tsx
+++ source/features/rgh-feature-descriptions.tsx
@@ -22,7 +22,7 @@ async function add(infoBanner: HTMLElement): Promise<void> {
 	conversationsUrl.searchParams.set('q', `sort:updated-desc "${feature.id}"`);
 
 	infoBanner.before(
-		<div className="Box mb-3">
+		<div className="Box mb-3 d-inline-block width-full">
 			<div className="Box-row d-flex gap-3 flex-wrap">
 				<div className="rgh-feature-description">
 					<h3 className="mb-2"><code>{feature.id}</code>
