e1e1638464f9f1ee3a9591cc683877a8eca07737
Federico Brigante <me@fregante.com>
2023-02-22 18:57:38 +0800

    Improve UX of `dim-bots` by disabling the feature on click

--- source/features/dim-bots.tsx
+++ source/features/dim-bots.tsx
@@ -1,6 +1,7 @@
 import './dim-bots.css';
 import select from 'select-dom';
 import * as pageDetect from 'github-url-detection';
+import delegate from 'delegate-it';
 
 import features from '../feature-manager';
 
@@ -31,22 +32,31 @@ const prSelectors = [
 	'.labels [href$="label%3Abot"]', // PR tagged with `bot` label
 ];
 
-function init(): void {
+const dimBots = features.getIdentifiers(import.meta.url);
+
+function undimBots(): void {
+	for (const bot of select.all(dimBots.selector)) {
+		bot.classList.remove(dimBots.class);
+	}
+}
+
+function init(signal: AbortSignal): void {
 	for (const bot of select.all(commitSelectors)) {
 		// Exclude co-authored commits
 		if (select.all('a', bot.parentElement!).every(link => link.matches(commitSelectors))) {
-			bot.closest('.commit, .Box-row')!.classList.add('rgh-dim-bot');
+			bot.closest('.commit, .Box-row')!.classList.add(dimBots.class);
 		}
 	}
 
 	for (const bot of select.all(prSelectors)) {
-		bot.closest('.commit, .Box-row')!.classList.add('rgh-dim-bot');
+		bot.closest('.commit, .Box-row')!.classList.add(dimBots.class);
 	}
 
-	// Delay collapsing, but only after they're collapsed on load #5158
-	requestAnimationFrame(() => {
-		select('#repo-content-turbo-frame .js-navigation-container')!.classList.add('rgh-dim-bots--after-hover');
-	});
+	// Undim on mouse focus
+	delegate(document, dimBots.selector, 'click', undimBots, {signal});
+
+	// Undim on keyboard focus
+	document.documentElement.addEventListener('navigation:keydown', undimBots, {once: true, signal});
 }
 
 void features.add(import.meta.url, {
@@ -57,7 +67,18 @@ void features.add(import.meta.url, {
 	exclude: [
 		pageDetect.isBlank, // Prevent error on empty lists #5544
 	],
-	awaitDomReady: true, // TODO: Feature needs a rewrite
+	awaitDomReady: true, // TODO: Rewrite with :has()
 	deduplicate: 'has-rgh-inner',
 	init,
 });
+
+/*
+
+Test URLs
+
+
+https://github.com/typed-ember/ember-cli-typescript/commits/master
+https://github.com/OctoLinker/OctoLinker/commits/master
+https://github.com/OctoLinker/OctoLinker/pulls?q=is%3Apr+is%3Aclosed
+
+*/
