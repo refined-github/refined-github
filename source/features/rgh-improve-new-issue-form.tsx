3b03ea14981cf4fd2f86ee58061b0b3372ffa28d
Federico Brigante <me@fregante.com>
2023-02-23 19:04:56 +0800

    Restore `rgh-improve-new-issue-form`

--- source/features/rgh-improve-new-issue-form.tsx
+++ source/features/rgh-improve-new-issue-form.tsx
@@ -5,12 +5,42 @@ import * as pageDetect from 'github-url-detection';
 import features from '../feature-manager';
 import openOptions from '../helpers/open-options';
 import clearCacheHandler from '../helpers/clear-cache-handler';
-import {expectTokenScope} from '../github-helpers/api';
+import {expectToken, expectTokenScope} from '../github-helpers/api';
 import {isRefinedGitHubRepo} from '../github-helpers';
+import {assertNodeContent} from '../helpers/dom-utils';
 
-function init(): void {
+function addNotice(adjective: JSX.Element | string): void {
+	select('#issue_body_template_name')!.before(
+		<div className="flash flash-warn m-2">{}
+			Your Personal Access Token is {adjective}. Some Refined GitHub features will not work without it.
+			You can update it <button className="btn-link" type="button" onClick={openOptions as unknown as React.MouseEventHandler}>in the options</button>.
+		</div>,
+	);
+}
+
+async function checkToken(): Promise<void> {
+	try {
+		await expectToken();
+	} catch {
+		addNotice('missing');
+		return;
+	}
+
+	try {
+		await expectTokenScope('repo');
+	} catch {
+		addNotice('invalid or expired');
+	}
+}
+
+async function setVersion(): Promise<void> {
 	const {version} = browser.runtime.getManifest();
-	select('input#issue_form_extension_version')!.value = version;
+	const versionFieldId = 'issue_form_4ceb28848d92a2c2094093043f57e5e4cb152518a3c2b3b950d1273f795dc13c';
+	assertNodeContent(select(`label[for="${versionFieldId}"`)!.firstChild, 'Extension version');
+	select(`input#${versionFieldId}`)!.value = version;
+}
+
+async function linkifyCacheRefresh(): Promise<void> {
 	select('[href="#clear-cache"]')!.replaceWith(
 		<button
 			className="btn"
@@ -20,14 +50,15 @@ function init(): void {
 			Clear cache
 		</button>,
 	);
-	void expectTokenScope('repo').catch(() => {
-		select('#issue_body_template_name')!.before(
-			<div className="flash flash-warn m-2">
-				Your Personal Access Token is either missing, incorrect or expired. Some Refined GitHub features will not work without it.<br/>
-				You can update it <a href="#" onClick={openOptions as unknown as React.MouseEventHandler}>in the options</a>.
-			</div>,
-		);
-	});
+}
+
+async function init(): Promise<void> {
+// Asyn functions so they're independent
+	await Promise.all([
+		linkifyCacheRefresh(),
+		checkToken(),
+		setVersion(),
+	]);
 }
 
 void features.add(import.meta.url, {
