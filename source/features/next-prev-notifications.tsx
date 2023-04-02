1cfb3a47f439a5b2b4712ce0a742cb2c3a3da2eb
Himanshu Gohil <himanshugohil234@gmail.com>
2023-04-02 12:09:02 +0530

    fixed all linting errors in next-prev-notifications feature

--- source/features/next-prev-notifications.tsx
+++ source/features/next-prev-notifications.tsx
@@ -1,24 +1,26 @@
-import "./next-prev-notifications.css";
+import './next-prev-notifications.css';
 import * as pageDetect from 'github-url-detection';
 
 import features from '../feature-manager';
 import observe from '../helpers/selector-observer';
 
-function addNextPrevBtn(notificationsHeader:Element):void {
-    notificationsHeader.parentElement?.parentElement?.classList.add('flex-items-between')
-    const nextPrevBtns = document.querySelector(".js-notifications-list-paginator-buttons")?.cloneNode(true);
-    nextPrevBtns?.classList.add('d-none', 'd-lg-flex');
-    nextPrevBtns && notificationsHeader.parentElement?.parentElement?.appendChild(nextPrevBtns);
+function addNextPreviousBtn(notificationsHeader: Element): void {
+	notificationsHeader.parentElement?.parentElement?.classList.add('flex-items-between');
+	const nextPreviousBtns = document.querySelector('.js-notifications-list-paginator-buttons')?.cloneNode(true);
+	nextPreviousBtns?.classList.add('d-none', 'd-lg-flex');
+	if (nextPreviousBtns) {
+		notificationsHeader.parentElement?.parentElement?.appendChild(nextPreviousBtns);
+	}
 }
+
 async function init(signal: AbortSignal): Promise<void> {
-    observe('notification-filter',addNextPrevBtn, {signal})
-    
+	observe('notification-filter', addNextPreviousBtn, {signal});
 }
 
 void features.add(import.meta.url, {
 	include: [
 		pageDetect.isNotifications,
 	],
-    awaitDomReady: true,
+	awaitDomReady: true,
 	init,
 });
