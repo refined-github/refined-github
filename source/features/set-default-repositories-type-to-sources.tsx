cb09ce7b863d0f6584cd0b08157eb89c172ba4df
Kid <hi@xuann.wang>
2023-04-09 16:49:12 +0800

    Match org repo everwhere

--- source/features/set-default-repositories-type-to-sources.tsx
+++ source/features/set-default-repositories-type-to-sources.tsx
@@ -11,10 +11,9 @@ function addSourceTypeToLink(link: HTMLAnchorElement): void {
 function init(): void {
 	observe([
 		`a:is([href^="/"], [href^="${location.origin}"])[href$="?tab=repositories"]`, // "Repositories" tab on user profile, only matches relative or same-origin links
+		`a:is([href^="/orgs/"], [href^="${location.origin}/orgs/"])[href$="/repositories"]`, // "Repositories" tab on organization profile, only matches relative or same-origin links
 		'[aria-label="Organization"] [data-tab-item="org-header-overview-tab"] a', // "Overview" tab on organization profile
-		'[aria-label="Organization"] [data-tab-item="org-header-repositories-tab"] a', // "Repositories" tab on organization profile
 		'[aria-label="Organization"] a[data-tab-item="i0overview-tab"]', // "Overview" tab on organization profile (Global navigation update)
-		'[aria-label="Organization"] a[data-tab-item="i1repositories-tab"]', // "Repositories" tab on organization profile (Global navigation update)
 		'a[data-hovercard-type="organization"]', // Organization name on repo header + organization list on user profile
 	], addSourceTypeToLink);
 }
