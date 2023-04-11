0bdf28976dfb95e99653b9195fb2ab03456b1482
Kid <hi@xuann.wang>
2023-04-12 01:58:43 +0800

    Wrap `tags-on-commits-list`

--- source/features/tags-on-commits-list.tsx
+++ source/features/tags-on-commits-list.tsx
@@ -136,7 +136,9 @@ async function init(): Promise<void | false> {
 			// There was no tags for this commit, save that info to the cache
 			commitsWithNoTags.push(targetCommit);
 		} else if (targetTags.length > 0) {
-			select('.flex-auto .d-flex.mt-1', commit)!.append(
+			const meta = select('.flex-auto .d-flex.mt-1', commit)!;
+			meta.classList.add('flex-wrap');
+			meta.append(
 				<span>
 					<TagIcon className="ml-1"/>
 					{...targetTags.map(tag => (
