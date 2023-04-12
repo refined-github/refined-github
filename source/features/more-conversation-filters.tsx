dcbec6371cf9882446cb201417b09d01bc96fc6a
Kid <hi@xuann.wang>
2023-04-12 22:51:15 +0800

    Oops

--- source/features/more-conversation-filters.tsx
+++ source/features/more-conversation-filters.tsx
@@ -22,7 +22,7 @@ function init(): void {
 
 	const subscriptionsUrl = new URL('https://github.com/notifications/subscriptions');
 	const repositoryId
-		= select('meta[name="octolytics-dimension-repository_id"]')!.content
+		= select('meta[name="octolytics-dimension-repository_id"]')?.content
 		?? select('input[name="repository_id"]')!.value;
 	subscriptionsUrl.searchParams.set('repository', btoa(`010:Repository${repositoryId}`));
 	subscriptionsLink.href = subscriptionsUrl.href;
