77e2609244cf74c815a0d1d8d2ea9f9252ee1246
Kid <hi@xuann.wang>
2023-04-12 23:18:48 +0800

    Convert `small-user-avatars` to observer

--- source/features/small-user-avatars.tsx
+++ source/features/small-user-avatars.tsx
@@ -1,36 +1,35 @@
 import './small-user-avatars.css';
 import React from 'dom-chef';
-import select from 'select-dom';
 import * as pageDetect from 'github-url-detection';
 
 import features from '../feature-manager';
+import observe from '../helpers/selector-observer';
 import getUserAvatarURL from '../github-helpers/get-user-avatar';
 
-function init(): void {
-	for (const link of select.all(
-		':is(.js-issue-row, .js-pinned-issue-list-item) [data-hovercard-type="user"]', // Excludes bots
-	)) {
-		const username = link.textContent!;
-		const size = 14;
+function addAvatar(link: HTMLElement): void {
+	const username = link.textContent!;
+	const size = 14;
 
-		link.prepend(
-			<img
-				className="avatar avatar-user v-align-text-bottom mr-1 rgh-small-user-avatars"
-				src={getUserAvatarURL(username, size)!}
-				width={size}
-				height={size}
-				loading="lazy"
-			/>,
-		);
-	}
+	link.prepend(
+		<img
+			className="avatar avatar-user v-align-text-bottom mr-1 rgh-small-user-avatars"
+			src={getUserAvatarURL(username, size)!}
+			width={size}
+			height={size}
+			loading="lazy"
+		/>,
+	);
+}
+
+function init(signal: AbortSignal): void {
+	// Excludes bots
+	observe(':is(.js-issue-row, .js-pinned-issue-list-item) [data-hovercard-type="user"]', addAvatar, {signal});
 }
 
 void features.add(import.meta.url, {
 	include: [
 		pageDetect.isIssueOrPRList,
 	],
-	awaitDomReady: true,
-	deduplicate: 'has-rgh-inner',
 	init,
 });
 
