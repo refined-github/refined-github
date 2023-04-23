c4cbb62c0da5045c582c1e4c98f770a8f1d95f08
Cooper Oh <cooper@chequer.io>
2023-04-23 17:48:19 +0900

    Lint

--- source/features/previous-version.tsx
+++ source/features/previous-version.tsx
@@ -1,12 +1,16 @@
-import features from "../feature-manager";
-import * as pageDetect from "github-url-detection";
-import * as api from '../github-helpers/api';
-import observe from "../helpers/selector-observer";
-import React from "dom-chef";
-import GitHubURL from "../github-helpers/github-url";
-import cache from "webext-storage-cache";
+import * as pageDetect from 'github-url-detection';
 
-const getHistoryOids = cache.function('file-history',async (branch: string, filePath: string): Promise<string[] | false> => {
+import React from 'dom-chef';
+
+import cache from 'webext-storage-cache';
+
+import features from '../feature-manager';
+import * as api from '../github-helpers/api';
+import observe from '../helpers/selector-observer';
+
+import GitHubURL from '../github-helpers/github-url';
+
+const getHistoryOids = cache.function('file-history', async (branch: string, filePath: string): Promise<string[] | false> => {
 	const {resource: {history}} = await api.v4(`
 		resource(url: "/refined-github/refined-github/commit/${branch}") {
 			... on Commit {
@@ -28,7 +32,7 @@ const getHistoryOids = cache.function('file-history',async (branch: string, file
 	return nodes.map<string>(n => n.oid);
 });
 
-const add = async (actionButtons: HTMLElement) => {
+const add = async (actionButtons: HTMLElement): Promise<void> => {
 	const githubUrl = new GitHubURL(location.href);
 	const historyOids = await getHistoryOids(githubUrl.branch, githubUrl.filePath);
 
@@ -36,67 +40,75 @@ const add = async (actionButtons: HTMLElement) => {
 		return;
 	}
 
-	const button = <div className="BtnGroup ml-1">
-		<div className="BtnGroup-parent">
-			{(() => {
-				const button = <div className="btn-sm BtnGroup-item btn">
-					Previous
-				</div>
+	const button = (
+		<div className="BtnGroup ml-1">
+			<div className="BtnGroup-parent">
+				{(() => {
+					const button = (
+						<div className="btn-sm BtnGroup-item btn">
+							Previous
+						</div>
+					);
 
-				button.addEventListener('click', () => {
-					const url = new GitHubURL(location.href);
-					url.branch = historyOids[1];
-					location.href = url.toString();
-				})
+					button.addEventListener('click', () => {
+						const url = new GitHubURL(location.href);
+						url.branch = historyOids[1];
+						location.href = url.toString();
+					});
 
-				return button;
-			})()}
-		</div>
+					return button;
+				})()}
+			</div>
 
-		<details className="details-reset details-overlay select-menu BtnGroup-parent d-inline-block position-relative" open={false}>
-			<summary data-disable-invalid="" data-disable-with=""
-							 data-dropdown-tracking="{&quot;type&quot;:&quot;blob_edit_dropdown.more_options_click&quot;,&quot;context&quot;:{&quot;repository_id&quot;:51769689,&quot;actor_id&quot;:null,&quot;github_dev_enabled&quot;:false,&quot;edit_enabled&quot;:false,&quot;small_screen&quot;:false}}"
-							 aria-label="Select additional options" data-view-component="true"
-							 className="js-blob-dropdown-click select-menu-button btn-sm btn BtnGroup-item float-none px-2">
-			</summary>
-			<div className="SelectMenu right-0">
-				<div className="SelectMenu-modal width-full">
-					<div className="SelectMenu-list SelectMenu-list--borderless py-2">
-						{historyOids.slice(2).map((e, i) => {
-							const item = <div className="SelectMenu-item no-wrap text-normal f5">
-								<div className="d-flex width-full gap-4">
-									<div className="color-fg-default flex-auto">{i + 2} commits ago</div>
-									<div className="color-fg-muted flex-shrink-0">{e.slice(0, 7)}</div>
-								</div>
-							</div>
+			<details className="details-reset details-overlay select-menu BtnGroup-parent d-inline-block position-relative" open={false}>
+				<summary
+					data-disable-invalid="" data-disable-with=""
+					data-dropdown-tracking="{&quot;type&quot;:&quot;blob_edit_dropdown.more_options_click&quot;,&quot;context&quot;:{&quot;repository_id&quot;:51769689,&quot;actor_id&quot;:null,&quot;github_dev_enabled&quot;:false,&quot;edit_enabled&quot;:false,&quot;small_screen&quot;:false}}"
+					aria-label="Select additional options" data-view-component="true"
+					className="js-blob-dropdown-click select-menu-button btn-sm btn BtnGroup-item float-none px-2"
+				/>
+				<div className="SelectMenu right-0">
+					<div className="SelectMenu-modal width-full">
+						<div className="SelectMenu-list SelectMenu-list--borderless py-2">
+							{historyOids.slice(2).map((element, i) => {
+								const item = (
+									<div className="SelectMenu-item no-wrap text-normal f5">
+										<div className="d-flex width-full gap-4">
+											<div className="color-fg-default flex-auto">{i + 2} commits ago</div>
+											<div className="color-fg-muted flex-shrink-0">{element.slice(0, 7)}</div>
+										</div>
+									</div>
+								);
 
-							item.addEventListener('click', () => {
-								const url = new GitHubURL(location.href);
-								url.branch = e;
-								location.href = url.toString();
-							});
+								item.addEventListener('click', () => {
+									const url = new GitHubURL(location.href);
+									url.branch = element;
+									location.href = url.toString();
+								});
 
-							return item;
-						})}
+								return item;
+							})}
+						</div>
 					</div>
 				</div>
-			</div>
-		</details>
-	</div>;
+			</details>
+		</div>
+	);
 
 	actionButtons.prepend(button);
 };
+
 async function init(signal: AbortSignal): Promise<false | void> {
 	const githubUrl = new GitHubURL(location.href);
 	const historyOids = await getHistoryOids(githubUrl.branch, githubUrl.filePath);
 
-	if(!historyOids) {
+	if (!historyOids) {
 		return false;
 	}
 
 	observe([
 		'#repos-sticky-header .react-blob-header-edit-and-raw-actions',	// For signed in
-		'readme-toc .Box-header.js-blob-header div:has(.BtnGroup)'			// For not signed in
+		'readme-toc .Box-header.js-blob-header div:has(.BtnGroup)',			// For not signed in
 	], add, {signal});
 }
 
