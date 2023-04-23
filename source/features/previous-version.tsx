bc5f1755eab2efbe6b7ea7df8cc0a5c3e5f9b2ba
Cooper Oh (오지환) <cooper@chequer.io>
2023-04-24 00:42:42 +0900

    Nit pick just fixing the spacing
    Co-authored-by: yakov116 <16872793+yakov116@users.noreply.github.com>

--- source/features/previous-version.tsx
+++ source/features/previous-version.tsx
@@ -1,14 +1,12 @@
-import * as pageDetect from 'github-url-detection';
-import React from 'dom-chef';
 import cache from 'webext-storage-cache';
-
+import React from 'dom-chef';
 import delegate from 'delegate-it';
+import * as pageDetect from 'github-url-detection';
 
 import features from '../feature-manager';
-import * as api from '../github-helpers/api';
 import observe from '../helpers/selector-observer';
-
-import GitHubURL from '../github-helpers/github-url';
+import * as api from '../github-helpers/api';
+import GitHubURL from '../github-helpers/github-url'; 
 
 const getPastCommits = cache.function('previous-version', async (currHref: string): Promise<string[] | false> => {
 	const githubUrl = new GitHubURL(currHref);
