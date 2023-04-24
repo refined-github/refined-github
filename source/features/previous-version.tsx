4abec33b26cba49814ecb2c0ddf03678883d431b
Federico Brigante <me@fregante.com>
2023-04-24 23:40:07 +0800

    Drop dropdown

--- source/features/previous-version.tsx
+++ source/features/previous-version.tsx
@@ -1,4 +1,3 @@
-import cache from 'webext-storage-cache';
 import React from 'dom-chef';
 import * as pageDetect from 'github-url-detection';
 
@@ -7,12 +6,12 @@ import observe from '../helpers/selector-observer';
 import * as api from '../github-helpers/api';
 import GitHubURL from '../github-helpers/github-url';
 
-const getPastCommits = cache.function('previous-version', async (pathname: string): Promise<string[]> => {
+async function getPreviousCommitForFile(pathname: string): Promise<string> {
 	const {user, repository, branch, filePath} = new GitHubURL(pathname);
 	const {resource} = await api.v4(`
 		resource(url: "/${user}/${repository}/commit/${branch}") {
 			... on Commit {
-				history(path: "${filePath}", first: 5) {
+				history(path: "${filePath}", first: 2) {
 					nodes {
 						oid
 					}
@@ -21,58 +20,24 @@ const getPastCommits = cache.function('previous-version', async (pathname: strin
 		}
 	`);
 
-	// The first commit refers to the current one, so we drop it
-	const nodes = resource.history.nodes.slice(1) as any[];
-	return nodes.map(node => node.oid);
-}, {
-	maxAge: {hours: 1},
-	staleWhileRevalidate: {days: 1},
-});
-
-function createDetailsButton(pastCommits: string[], url: GitHubURL): Element | void {
-	return (
-		<details
-			className="details-reset details-overlay select-menu BtnGroup-parent d-inline-block position-relative"
-		>
-			<summary className="js-blob-dropdown-click select-menu-button btn-sm btn BtnGroup-item float-none px-2"/>
-			<div className="SelectMenu right-0">
-				<div className="SelectMenu-modal width-full">
-					<div className="SelectMenu-list SelectMenu-list--borderless py-2">
-						{pastCommits.map((branch, i) => (
-							<a href={url.assign({branch}).href} className="SelectMenu-item no-wrap text-normal f5">
-								<div className="d-flex width-full gap-4">
-									<div className="color-fg-default flex-auto">{i + 2} commits ago</div>
-									<div className="color-fg-muted flex-shrink-0">{branch.slice(0, 7)}</div>
-								</div>
-							</a>
-						))}
-					</div>
-				</div>
-			</div>
-		</details>
-	);
+	// The first commit refers to the current one, so we skip it
+	return resource.history.nodes[1].oid;
 }
 
 async function add(actionButtons: HTMLElement): Promise<void> {
-	const pastCommits = await getPastCommits(location.href);
+	const previousCommit = await getPreviousCommitForFile(location.href);
 
-	if (pastCommits.length === 0) {
+	if (!previousCommit) {
 		return;
 	}
 
-	const url = new GitHubURL(location.href);
-	url.branch = pastCommits[0];
+	const url = new GitHubURL(location.href)
+		.assign({branch: previousCommit});
 
 	actionButtons.prepend(
-		<div className="BtnGroup ml-1">
-			<div className="BtnGroup-parent tooltipped tooltipped-n" aria-label="View previous version of this file">
-				<a href={url.href} className="btn-sm BtnGroup-item btn">
-					Previous
-				</a>
-			</div>
-
-			{pastCommits.length > 1 && createDetailsButton(pastCommits.slice(1), url)}
-		</div>,
+		<a href={url.href} className="btn btn-sm tooltipped tooltipped-n ml-1" aria-label="View previous version of this file">
+			Previous
+		</a>,
 	);
 }
 
