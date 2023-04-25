cedc7e5fdd9a89a3270d709ce0a6d290a36f2d1d
Federico Brigante <me@fregante.com>
2023-04-25 23:42:49 +0800

    Comment

--- source/github-events/on-pr-merge.ts
+++ source/github-events/on-pr-merge.ts
@@ -2,6 +2,7 @@ import {oneEvent} from 'delegate-it';
 
 import observe from '../helpers/selector-observer';
 
+// This event ensures that the callback appears exclusively to the person that merged the PR and not anyone who was on the page at the time of the merge
 export default async function onPrMerge(callback: VoidFunction, signal: AbortSignal): Promise<void> {
 	await oneEvent('.js-merge-commit-button', 'click', {signal});
 	observe('.TimelineItem-badge .octicon-git-merge', callback, {signal});
