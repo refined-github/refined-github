d955f3cc239d78827b7ccafdc6d3bf6235afa9e5
Federico Brigante <me@fregante.com>
2023-04-25 23:31:15 +0800

    Signal

--- source/github-events/on-pr-merge.ts
+++ source/github-events/on-pr-merge.ts
@@ -3,6 +3,6 @@ import {oneEvent} from 'delegate-it';
 import observe from '../helpers/selector-observer';
 
 export default async function onPrMerge(callback: VoidFunction, signal: AbortSignal): Promise<void> {
-	await oneEvent('.js-merge-commit-button', 'click');
+	await oneEvent('.js-merge-commit-button', 'click', {signal});
 	observe('.TimelineItem-badge .octicon-git-merge', callback, {signal});
 }
