834882a7088babdbd641be9278ea687e51005a93
Cooper Oh <cooper@chequer.io>
2023-05-07 13:40:12 +0900

    Design fix

--- source/features/previous-version.tsx
+++ source/features/previous-version.tsx
@@ -1,6 +1,8 @@
 import React from 'dom-chef';
 import * as pageDetect from 'github-url-detection';
 
+import {VersionsIcon} from '@primer/octicons-react';
+
 import features from '../feature-manager';
 import observe from '../helpers/selector-observer';
 import * as api from '../github-helpers/api';
@@ -24,7 +26,7 @@ async function getPreviousCommitForFile(pathname: string): Promise<string> {
 	return resource.history.nodes[1].oid;
 }
 
-async function add(actionButtons: HTMLElement): Promise<void> {
+async function add(historyButton: HTMLElement): Promise<void> {
 	const previousCommit = await getPreviousCommitForFile(location.href);
 
 	if (!previousCommit) {
@@ -34,15 +36,17 @@ async function add(actionButtons: HTMLElement): Promise<void> {
 	const url = new GitHubURL(location.href)
 		.assign({branch: previousCommit});
 
-	actionButtons.prepend(
-		<a href={url.href} className="btn btn-sm tooltipped tooltipped-n ml-1" aria-label="View previous version of this file">
-			Previous
+	const historyButtonContainer = historyButton.parentElement!;
+	historyButtonContainer.insertBefore(
+		<a href={url.href} className="UnderlineNav-item no-wrap tooltipped tooltipped-n ml-2" aria-label="View prior to this change">
+			<VersionsIcon className="UnderlineNav-octicon" style={{marginRight: 0}}/>
 		</a>,
+		historyButton,
 	);
 }
 
 async function init(signal: AbortSignal): Promise<void> {
-	observe('#repos-sticky-header .react-blob-header-edit-and-raw-actions', add, {signal});
+	observe('a[aria-label=History].react-last-commit-history-group', add, {signal});
 }
 
 void features.add(import.meta.url, {
