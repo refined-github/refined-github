3dd29a47b6b2b3bf4ca9f103f359fdbfeb39f8d6
Federico Brigante <me@fregante.com>
2023-05-07 18:09:59 +0800

    Some fixes around `quick-file-edit` (#6627)

--- source/github-helpers/selectors.ts
+++ source/github-helpers/selectors.ts
@@ -3,3 +3,20 @@ export const underlineNavDropdownUl_ = [
 	'https://github.com/refined-github/refined-github',
 	'https://github.com/refined-github/refined-github/releases',
 ];
+
+export const branchSelector = '[data-hotkey="w"]';
+export const branchSelector_ = [
+	'https://github.com/refined-github/refined-github',
+	'https://github.com/refined-github/refined-github/blob/main/readme.md',
+	'https://github.com/refined-github/refined-github/tree/main/source',
+];
+
+export const directoryListingFileIcon = [
+	// .color-fg-muted selects only files; some icon extensions use `img` tags
+	'.react-directory-filename-column > :is(svg, img).color-fg-muted',
+	'.js-navigation-container .octicon-file',
+];
+export const directoryListingFileIcon_ = [
+	'https://github.com/refined-github/refined-github',
+	'https://github.com/refined-github/refined-github/tree/main/.github',
+];
