2c1e99a36d7213219bd4966b036094bdeaa95dd6
Federico Brigante <me@fregante.com>
2023-05-07 21:49:45 +0800

    URLs

--- source/features/clean-repo-filelist-actions.tsx
+++ source/features/clean-repo-filelist-actions.tsx
@@ -62,3 +62,11 @@ void features.add(import.meta.url, {
 	],
 	init,
 });
+
+/*
+
+Test URLs
+https://github.com/refined-github/sandbox
+https://github.com/refined-github/sandbox/tree/branch/with/slashes
+
+*/
