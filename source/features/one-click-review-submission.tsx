0971327bfbf6509d7c2b8b9abb7868228c43f844
Federico Brigante <me@fregante.com>
2023-05-09 14:42:42 +0800

    Fix helpers logic

--- source/features/one-click-review-submission.tsx
+++ source/features/one-click-review-submission.tsx
@@ -9,7 +9,7 @@ import looseParseInt from '../helpers/loose-parse-int.js';
 
 function addButtons(radios: HTMLInputElement[]): void {
 	const form = radios[0].form!;
-	const container
+	const actionsRow
 		= select('.form-actions', form) // TODO: Drop after September 2023
 		?? form.closest('.SelectMenu')!.querySelector('.form-actions')!;
 
@@ -28,7 +28,7 @@ function addButtons(radios: HTMLInputElement[]): void {
 	for (const radio of radios) {
 		const tooltip = radio.parentElement!.getAttribute('aria-label');
 
-		const classes = ['btn btn-sm'];
+		const classes = ['btn btn-sm rgh-one-click-review-submission'];
 		if (radio.value === 'comment') {
 			classes.push('btn-primary');
 		}
@@ -42,12 +42,11 @@ function addButtons(radios: HTMLInputElement[]): void {
 				type="submit"
 				name="pull_request_review[event]"
 				// The buttons are no longer inside the form itself; this links the form
-				form={form.contains(container) ? undefined : form.id} // TODO: Drop condition after September 2023
+				form={form.contains(actionsRow) ? undefined : form.id} // TODO: Drop condition after September 2023
 				value={radio.value}
 				className={classes.join(' ')}
 				aria-label={tooltip!}
 				disabled={radio.disabled}
-				data-disable-with=""
 			>
 				{radio.nextSibling}
 			</button>
@@ -59,7 +58,7 @@ function addButtons(radios: HTMLInputElement[]): void {
 			button.prepend(<FileDiffIcon className="color-fg-danger"/>);
 		}
 
-		container.append(button);
+		actionsRow.append(button);
 	}
 
 	// Remove original fields at last to avoid leaving a broken form
@@ -68,24 +67,26 @@ function addButtons(radios: HTMLInputElement[]): void {
 	}
 
 	// The selector excludes the "Cancel" button
-	select('[type="submit"]:not([name])', container)!.remove();
+	select('[type="submit"]:not([name])', actionsRow)!.remove();
 }
 
-function handleSubmission(event: DelegateEvent): void {
-	// Delay disabling the fields to let them be submitted first
-	setTimeout(() => {
-		for (const control of select.all('button, textarea', event.delegateTarget)) {
-			control.disabled = true;
-		}
-	});
+let lastSubmission: number | undefined;
+function blockDuplicateSubmissions(event: DelegateEvent): void {
+	if (lastSubmission && Date.now() - lastSubmission < 1000) {
+		event.preventDefault();
+		console.log('Duplicate submission prevented');
+		return;
+	}
+
+	lastSubmission = Date.now();
 }
 
 function init(signal: AbortSignal): false | void {
 	// Freeze form to avoid duplicate submissions
-	delegate('[action$="/reviews"]', 'submit', handleSubmission, {signal});
+	delegate('[action$="/reviews"]', 'submit', blockDuplicateSubmissions, {signal});
 
 	// This will prevent submission when clicking "Comment" and "Request changes" without entering a comment and no other review comments are pending
-	delegate('[action$="/reviews"] button', 'click', ({delegateTarget: {value, form}}) => {
+	delegate('button.rgh-one-click-review-submission', 'click', ({delegateTarget: {value, form}}) => {
 		const pendingComments = looseParseInt(select('.js-reviews-toggle .js-pending-review-comment-count'));
 		const submissionRequiresComment = pendingComments === 0 && (value === 'reject' || value === 'comment');
 		select('#pull_request_review_body', form!)!.toggleAttribute('required', submissionRequiresComment);
