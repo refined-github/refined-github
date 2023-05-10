3f1fcc0620644d3d011e63b4b6ac2ce571c90ff7
Federico Brigante <me@fregante.com>
2023-05-11 05:26:09 +0800

    Lint

--- source/features/list-prs-for-branch.tsx
+++ source/features/list-prs-for-branch.tsx
@@ -58,3 +58,12 @@ void features.add(import.meta.url, {
 	],
 	init,
 });
+
+/*
+
+Test URLs
+
+https://github.com/refined-github/sandbox/commits/4679-1
+https://github.com/refined-github/sandbox/commits/branch/with/slashes
+
+*/
--- source/helpers/add-after-branch-selector.ts
+++
@@ -1,5 +0,0 @@
-export default function addAfterBranchSelector(branchSelectorParent: HTMLDetailsElement, sibling: HTMLElement): void {
-	const row = branchSelectorParent.closest('.position-relative')!;
-	row.classList.add('d-flex', 'flex-shrink-0', 'gap-2');
-	row.append(sibling);
-}
