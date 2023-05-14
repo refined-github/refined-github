25a4778edbf7ea84369e914f150c9353847e0405
Federico Brigante <me@fregante.com>
2023-05-14 20:21:55 +0800

    Oh wow, so easy 🥹

--- source/features/status-subscription.tsx
+++ source/features/status-subscription.tsx
@@ -2,36 +2,86 @@ import React from 'dom-chef';
 import * as pageDetect from 'github-url-detection';
 import {BellIcon, BellSlashIcon, IssueReopenedIcon} from '@primer/octicons-react';
 
-import features from '../feature-manager';
-import observe from '../helpers/selector-observer';
+import features from '../feature-manager.js';
+import observe from '../helpers/selector-observer.js';
+
+const disableAttrs = {
+	'aria-selected': true,
+	className: 'selected',
+	style: {pointerEvents: 'none'},
+} as const satisfies React.HTMLAttributes<HTMLButtonElement>;
+
+function SubButton(): JSX.Element {
+	return (
+		<button
+			data-disable-with
+			name="id"
+			type="submit"
+			className="btn btn-sm flex-grow-1 BtnGroup-item tooltipped tooltipped-sw"
+		/>
+	);
+}
+
+function getCurrentStatus(subscriptionButton: HTMLButtonElement): 'none' | 'all' | 'status' {
+	const reason = subscriptionButton
+		.closest('.thread-subscription-status')!
+		.querySelector('.reason')!
+		.textContent!;
+
+	// You’re receiving notifications because you chose custom settings for this thread.
+	if (reason.includes('custom settings')) {
+		return 'status';
+	}
+
+	// You’re not receiving notifications from this thread.
+	if (reason.includes('not receiving')) {
+		return 'none';
+	}
+
+	return 'all';
+}
 
 function addButton(subscriptionButton: HTMLButtonElement): void {
-	subscriptionButton.before(
+	const status = getCurrentStatus(subscriptionButton);
+	// Save first
+	const originalId = subscriptionButton.form!.elements.id;
+
+	subscriptionButton.after(
 		<div className="BtnGroup d-flex width-full">
-			<button
-				type="button"
-				className="btn btn-sm flex-grow-1 BtnGroup-item tooltipped tooltipped-s"
+			<SubButton
+				// @ts-expect-error I don't remember how to fix this
+				value="unsubscribe"
 				aria-label="Unsubscribe"
+				{...(status === 'none' && disableAttrs)}
 			>
 				<BellSlashIcon/> None
-			</button>
-			<button
-				aria-selected
-				type="button"
-				className="btn btn-sm flex-grow-1 BtnGroup-item tooltipped tooltipped-s selected"
-				aria-label="Subscribe"
+			</SubButton>
+			<SubButton
+				// @ts-expect-error I don't remember how to fix this
+				value="subscribe"
+				aria-label="Subscribe to all events"
+				{...(status === 'all' && disableAttrs)}
 			>
 				<BellIcon/> All
-			</button>
-			<button
-				type="button"
-				className="btn btn-sm flex-grow-1 BtnGroup-item tooltipped tooltipped-s"
-				aria-label="Subscribe just to close event"
+			</SubButton>
+
+			{/* Always submitted, but ignored unless the value is `subscribe_to_custom_notifications` */}
+			<input type="hidden" name="events[]" value="merged"/>
+			<input type="hidden" name="events[]" value="closed"/>
+			<input type="hidden" name="events[]" value="reopened"/>
+			<SubButton
+				// @ts-expect-error I don't remember how to fix this
+				value="subscribe_to_custom_notifications"
+				aria-label="Subscribe just to the close event"
+				{...(status === 'status' && disableAttrs)}
 			>
 				<IssueReopenedIcon/> Status
-			</button>
+			</SubButton>
 		</div>,
 	);
+
+	// Remove it only if the form was successfully added
+	originalId.remove();
 	subscriptionButton.hidden = true;
 }
 
