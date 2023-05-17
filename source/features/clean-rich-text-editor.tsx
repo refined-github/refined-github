7056ef2bf279497bb4d4837a53b893f6bd0e2564
Amal Chandran M V <amaltathva@gmail.com>
2023-05-17 10:51:41 +0530

    hide unnecessary tooltip on required text inputs

--- source/features/clean-rich-text-editor.tsx
+++ source/features/clean-rich-text-editor.tsx
@@ -2,5 +2,25 @@ import './clean-rich-text-editor.css';
 import * as pageDetect from 'github-url-detection';
 
 import features from '../feature-manager.js';
+import select from 'select-dom';
+
+function hideToolTip(): void {
+    for (const textarea of select.all(`
+        input[id="issue_title"], 
+        textarea[id="issue_form_repro"], 
+        textarea[id="issue_form_description"], 
+        input[id="issue_form_browser", 
+        textarea[id="new_comment_field"]
+    `)) {
+        textarea.title = '';
+    }
+}
 
 void features.addCssFeature(import.meta.url, [pageDetect.hasRichTextEditor]);
+
+void features.add(import.meta.url, {
+    include: [
+        pageDetect.hasRichTextEditor,
+    ],
+    init: hideToolTip
+})
