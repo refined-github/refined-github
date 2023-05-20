1abfe978f5f14766aae5e88e889b88686be2d159
Federico Brigante <me@fregante.com>
2023-05-20 13:50:35 +0800

    Use correct file after merges in in `restore-file`

--- source/github-helpers/get-pr-info.ts
+++ source/github-helpers/get-pr-info.ts
@@ -1,8 +1,9 @@
-import * as api from './api.js';
+import api from './api.js';
 import {getConversationNumber} from './index.js';
 
 export type PullRequestInfo = {
 	baseRefOid: string;
+	headRefOid: string;
 	// https://docs.github.com/en/graphql/reference/enums#mergeablestate
 	mergeable: 'CONFLICTING' | 'MERGEABLE' | 'UNKNOWN';
 	viewerCanEditFiles: boolean;
@@ -13,8 +14,10 @@ export type PullRequestInfo = {
 export default async function getPrInfo(base: string, number = getConversationNumber()!): Promise<PullRequestInfo> {
 	const {repository} = await api.v4(`
 		repository() {
+			# Cache buster ${Math.random()}
 			pullRequest(number: ${number}) {
 				baseRefOid
+				headRefOid
 				mergeable
 				viewerCanEditFiles
 				headRef {
@@ -29,12 +32,14 @@ export default async function getPrInfo(base: string, number = getConversationNu
 
 	const {
 		baseRefOid,
+		headRefOid,
 		mergeable,
 		viewerCanEditFiles,
 		headRef,
 	} = repository.pullRequest;
 	return {
 		baseRefOid,
+		headRefOid,
 		mergeable,
 		viewerCanEditFiles,
 		// The comparison in the API is base -> head, so it must be flipped
