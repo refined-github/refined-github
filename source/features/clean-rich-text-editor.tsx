1d5b18f287f229f2c2d68b947eed168fbc4b07dd
Federico Brigante <me@fregante.com>
2023-05-21 02:09:03 +0800

    Revert

--- source/features/clean-rich-text-editor.tsx
+++ source/features/clean-rich-text-editor.tsx
@@ -1,31 +1,9 @@
-import select from 'select-dom';
 import './clean-rich-text-editor.css';
 import * as pageDetect from 'github-url-detection';
 
 import features from '../feature-manager.js';
 
-const textInputSelectors = [
-	'#issue_title',
-	'#issue_form_repo',
-	'#issue_form_description',
-	'#issue_form_browser', // Id's for text input fields inside issues
-	'#new_comment_field', // Id for text input field in comments
-];
-
-async function init(): Promise<void> {
-	document.documentElement.classList.add('rgh-clean-rich-text-editor');
-	// Hide unnecessary tooltips
-	for (const textarea of select.all(textInputSelectors)) {
-		textarea.title = '';
-	}
-}
-
-void features.add(import.meta.url, {
-	include: [
-		pageDetect.hasRichTextEditor,
-	],
-	init,
-});
+void features.addCssFeature(import.meta.url, [pageDetect.hasRichTextEditor]);
 
 /*
 
