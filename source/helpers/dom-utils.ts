466b06e591a8158be44b68a49069e0aca1f5b689
Flo Edelmann <git@flo-edelmann.de>
2023-05-21 21:32:11 +0200

    Add comment
    Co-authored-by: Federico Brigante <me@fregante.com>

--- source/helpers/dom-utils.ts
+++ source/helpers/dom-utils.ts
@@ -86,6 +86,7 @@ const isTextNode = (node: Text | ChildNode): boolean =>
 
 // eslint-disable-next-line @typescript-eslint/ban-types -- Nodes may be exactly `null`
 export const assertNodeContent = <N extends Text | ChildNode>(node: N | null, expectation: RegExp | string): N => {
+	// Make sure only text is being considered, not links, icons, etc
 	if (!node || !isTextNode(node)) {
 		console.warn('TypeError', node);
 		throw new TypeError(`Expected Text node, received ${String(node?.nodeName)}`);
