dfa3951efae21bdaad65ee275d51637755dcab6a
Cooper Oh <cooper@chequer.io>
2023-05-29 12:50:42 +0900

    Seperate graphql query file

--- source/features/previous-version.tsx
+++ source/features/previous-version.tsx
@@ -1,28 +1,16 @@
 import React from 'dom-chef';
 import * as pageDetect from 'github-url-detection';
-
 import {VersionsIcon} from '@primer/octicons-react';
 
 import features from '../feature-manager.js';
 import observe from '../helpers/selector-observer.js';
 import api from '../github-helpers/api.js';
 import GitHubURL from '../github-helpers/github-url.js';
+import previousVersionQuery from './previous-version.gql';
 
 async function getPreviousCommitForFile(pathname: string): Promise<string | false> {
 	const {user, repository, branch, filePath} = new GitHubURL(pathname);
-	const {resource} = await api.v4(`
-		query getPreviousCommitForFile($resource: URI!, $filePath: String!) {
-			resource(url: $resource) {
-				... on Commit {
-					history(path: $filePath, first: 2) {
-						nodes {
-							oid
-						}
-					}
-				}
-			}
-		}
-	`, {
+	const {resource} = await api.v4(previousVersionQuery, {
 		variables: {
 			filePath,
 			resource: `/${user}/${repository}/commit/${branch}`,
