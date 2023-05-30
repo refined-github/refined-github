dc7a02e44378079f7da4f8122722aa0fdf87c947
Cooper Oh (오지환) <cooper@chequer.io>
2023-05-31 02:00:58 +0900

    Fix `previous-version` throws error on brand-new files (#6690)

--- source/features/previous-version.tsx
+++ source/features/previous-version.tsx
@@ -1,28 +1,16 @@
 import React from 'dom-chef';
 import * as pageDetect from 'github-url-detection';
-
 import {VersionsIcon} from '@primer/octicons-react';
 
 import features from '../feature-manager.js';
 import observe from '../helpers/selector-observer.js';
 import api from '../github-helpers/api.js';
 import GitHubURL from '../github-helpers/github-url.js';
+import previousVersionQuery from './previous-version.gql';
 
-async function getPreviousCommitForFile(pathname: string): Promise<string> {
+async function getPreviousCommitForFile(pathname: string): Promise<string | undefined> {
 	const {user, repository, branch, filePath} = new GitHubURL(pathname);
-	const {resource} = await api.v4(`
-		query getPreviousCommitForFile($resource: URI!, $filePath: String!) {
-			resource(url: $resource) {
-				... on Commit {
-					history(path: $filePath, first: 2) {
-						nodes {
-							oid
-						}
-					}
-				}
-			}
-		}
-	`, {
+	const {resource} = await api.v4(previousVersionQuery, {
 		variables: {
 			filePath,
 			resource: `/${user}/${repository}/commit/${branch}`,
@@ -30,12 +18,11 @@ async function getPreviousCommitForFile(pathname: string): Promise<string> {
 	});
 
 	// The first commit refers to the current one, so we skip it
-	return resource.history.nodes[1].oid;
+	return resource.history.nodes[1]?.oid;
 }
 
 async function add(historyButton: HTMLElement): Promise<void> {
 	const previousCommit = await getPreviousCommitForFile(location.href);
-
 	if (!previousCommit) {
 		return;
 	}
