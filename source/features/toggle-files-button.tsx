f3ff0f82bf43be14244f711f2ba86d10ab39a002
Federico Brigante <me@fregante.com>
2023-06-01 00:32:36 +0900

    Use native file toggle in `toggle-files-button`

--- source/features/toggle-files-button.tsx
+++ source/features/toggle-files-button.tsx
@@ -1,81 +1,64 @@
 import './toggle-files-button.css';
+import select from 'select-dom';
 import cache from 'webext-storage-cache';
 import React from 'dom-chef';
-import select from 'select-dom';
 import delegate from 'delegate-it';
 import * as pageDetect from 'github-url-detection';
-import {FoldIcon, UnfoldIcon, ArrowUpIcon} from '@primer/octicons-react';
+import {ChevronDownIcon, FoldIcon, UnfoldIcon} from '@primer/octicons-react';
 
 import features from '../feature-manager.js';
 import selectHas from '../helpers/select-has.js';
-import attachElement from '../helpers/attach-element.js';
 import observe from '../helpers/selector-observer.js';
 
 const cacheKey = 'files-hidden';
-const hiddenFilesClass = 'rgh-files-hidden';
 const toggleButtonClass = 'rgh-toggle-files';
-const noticeClass = 'rgh-files-hidden-notice';
-
-// 19px align this icon with the <UnfoldIcon/> above it
-const noticeStyle = {paddingRight: '19px'};
 
 function addButton(filesBox: HTMLElement): void {
-	attachElement(selectHas('ul:has(.octicon-history)', filesBox)!, {
-		allowMissingAnchor: true,
-		className: toggleButtonClass,
-		append: () => (
-			<button
-				type="button"
-				className="btn-octicon"
-				aria-label="Toggle files section"
-			>
-				<FoldIcon/>
-				<UnfoldIcon/>
-			</button>
-		),
-	});
-}
-
-function addFilesHiddenNotice(fileBox: Element): void {
-	// Add notice so the user knows that the list was collapsed #5524
-	attachElement(fileBox, {
-		className: noticeClass,
-		after: () => (
-			<div
-				className="mb-3 mt-n3 py-1 text-right text-small color-fg-subtle"
-				style={noticeStyle}
-			>
-				The file list was collapsed via Refined GitHub <ArrowUpIcon className="v-align-middle"/>
-			</div>
-		),
-	});
+	selectHas('ul:has(.octicon-history)', filesBox)?.append(
+		<button
+			type="button"
+			className={`btn-octicon ${toggleButtonClass}`}
+			aria-label="Hide files"
+		>
+			<ChevronDownIcon/>
+		</button>,
+	);
 }
 
 function toggle(toggle?: boolean): boolean {
-	return document.body.classList.toggle(hiddenFilesClass, toggle);
+	const fileList = select('[aria-labelledby="files"]')!;
+	const button = fileList.nextElementSibling!;
+	const isHidden = fileList.classList.toggle('d-md-block', toggle);
+	button.classList.toggle('d-md-none', isHidden);
+	return isHidden;
 }
 
-async function toggleHandler(): Promise<void> {
-	// Remove notice after the first click
-	select(`.${noticeClass}`)?.remove();
+async function toggleList(): Promise<void> {
+	const fileList = select('[aria-labelledby="files"]')!;
+	const button = fileList.nextElementSibling!;
 
-	const isHidden = toggle();
-	await cache.set(cacheKey, isHidden);
+	if (fileList.classList.contains('d-md-block')) {
+		// Desktop, collapse list and enable native toggling
+		fileList.classList.remove('d-md-block');
+		button.classList.remove('d-md-none');
+	} else {
+		// Toggle file list via native button
+		(button.firstElementChild as HTMLButtonElement).click()
+	}
 }
 
 async function updateView(anchor: HTMLHeadingElement): Promise<void> {
 	const filesBox = anchor.parentElement!;
 	addButton(filesBox);
 	if (await cache.get<boolean>(cacheKey)) {
-		toggle(true);
-		addFilesHiddenNotice(filesBox);
+		// toggle(true);
 	}
 }
 
 async function init(signal: AbortSignal): Promise<void> {
 	// TODO: Use `.Box:has(> #files)` instead
 	observe('.Box h2#files', updateView, {signal});
-	delegate(`.${toggleButtonClass}, .${noticeClass}`, 'click', toggleHandler, {signal});
+	delegate(`.${toggleButtonClass}`, 'click', toggleList, {signal});
 }
 
 void features.add(import.meta.url, {
