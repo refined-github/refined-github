125fbde6bd87aa3192b3a0f9428b5f96d60b8a65
Federico Brigante <me@fregante.com>
2023-06-02 20:33:26 +0900

    A little more spacing

--- source/features/mobile-tabs.tsx
+++ source/features/mobile-tabs.tsx
@@ -3,4 +3,13 @@ import * as pageDetect from 'github-url-detection';
 
 import features from '../feature-manager.js';
 
+// TODO: Enable on profiles after the new header migration has completed. December 2023?
 void features.addCssFeature(import.meta.url, [pageDetect.hasRepoHeader]);
+
+/*
+
+Test URLs:
+
+https://github.com/refined-github/refined-github
+
+*/
