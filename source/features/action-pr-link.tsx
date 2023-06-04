563d153f24f0fac8a00d0afda560e6d2bf398e51
Cooper Oh <cooper@chequer.io>
2023-06-04 16:38:46 +0900

    select directly

--- source/features/action-pr-link.tsx
+++ source/features/action-pr-link.tsx
@@ -1,24 +1,18 @@
 import * as pageDetect from 'github-url-detection';
-import select from 'select-dom';
-
 import features from '../feature-manager.js';
 import observe from '../helpers/selector-observer.js';
 
-async function add(row: HTMLDivElement): Promise<void> {
-	const prNumber = select('a[data-hovercard-type="pull_request"]', row)?.textContent!.slice(1);
+async function add(prLink: HTMLAnchorElement): Promise<void> {
+	const prNumber = prLink.textContent!.slice(1);
 
-	if (prNumber === undefined) {
-		return;
-	}
-
-	const runLink = select('a.Link--primary', row)!;
+	const runLink = prLink.closest('.Box-row')?.querySelector('a.Link--primary')!;
 	const parameters = new URLSearchParams(runLink.search);
 	parameters.set('pr', prNumber);
 	runLink.search = String(parameters);
 }
 
 async function init(signal: AbortSignal): Promise<void> {
-	observe('div.Box-row[id^=check_suite_]', add, {signal});
+	observe('div.Box-row[id^=check_suite_] a[data-hovercard-type="pull_request"]', add, {signal});
 }
 
 void features.add(import.meta.url, {
