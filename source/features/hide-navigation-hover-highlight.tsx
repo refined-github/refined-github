82c9eb50bd532dd59d2afcea47aa5b3ba5e665e6
Federico Brigante <me@fregante.com>
2023-07-21 12:36:48 +0200

    More style + cleanup + hide style longer

--- source/features/hide-navigation-hover-highlight.tsx
+++ source/features/hide-navigation-hover-highlight.tsx
@@ -4,11 +4,12 @@ import onetime from 'onetime';
 import features from '../feature-manager.js';
 
 const className = 'rgh-no-navigation-highlight';
+const html = document.documentElement;
 
 function init(): void {
-	document.documentElement.classList.add(className);
-	document.documentElement.addEventListener('navigation:keydown', () => {
-		document.documentElement.classList.remove(className);
+	html.classList.add(className);
+	html.addEventListener('navigation:focus', () => {
+		html.classList.remove(className);
 	}, {once: true});
 }
 
@@ -23,6 +24,6 @@ Test URLs
 - Notifications list: https://github.com/notifications
 - Issue list: https://github.com/refined-github/refined-github/issues
 - React file list: https://github.com/refined-github/refined-github/tree/main/.github
-- File list: File list
+- File list: https://github.com/refined-github/refined-github
 
 */
