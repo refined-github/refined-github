b38923f4dbe4bcb99a4a1374b24ae0a8c430e273
Federico Brigante <me@fregante.com>
2023-07-22 10:53:19 +0200

    Restore `hide-navigation-hover-highlight` on files/notifications (#6779)

--- source/features/hide-navigation-hover-highlight.tsx
+++ source/features/hide-navigation-hover-highlight.tsx
@@ -1,17 +1,28 @@
 import './hide-navigation-hover-highlight.css';
-import onetime from 'onetime';
 
 import features from '../feature-manager.js';
 
 const className = 'rgh-no-navigation-highlight';
+const html = document.documentElement;
 
 function init(): void {
-	document.documentElement.classList.add(className);
-	document.documentElement.addEventListener('navigation:keydown', () => {
-		document.documentElement.classList.remove(className);
+	html.classList.add(className);
+	html.addEventListener('navigation:focus', () => {
+		html.classList.remove(className);
 	}, {once: true});
 }
 
 void features.add(import.meta.url, {
-	init: onetime(init),
+	init,
 });
+
+/*
+
+Test URLs
+
+- Notifications list: https://github.com/notifications
+- Issue list: https://github.com/refined-github/refined-github/issues
+- React file list: https://github.com/refined-github/refined-github/tree/main/.github
+- File list: https://github.com/refined-github/refined-github
+
+*/
