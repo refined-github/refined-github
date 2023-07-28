44ca8e9e2eed9a6506513e87b47f9828f67159e3
Cooper Oh <cooper@chequer.io>
2023-07-28 20:45:37 +0900

    Extend for on pr details page

--- source/features/action-pr-link.tsx
+++ source/features/action-pr-link.tsx
@@ -2,25 +2,51 @@ import * as pageDetect from 'github-url-detection';
 
 import features from '../feature-manager.js';
 import observe from '../helpers/selector-observer.js';
+import select from "select-dom";
+import GitHubURL from "../github-helpers/github-url.js";
 
-async function add(prLink: HTMLAnchorElement): Promise<void> {
+function setSearchParam(anchorElement: HTMLAnchorElement, name: string, value: string) {
+	const params = new URLSearchParams(anchorElement.search);
+	params.set(name, value)
+	anchorElement.search = String(params)
+}
+
+async function addForRepositoryActions(prLink: HTMLAnchorElement): Promise<void> {
 	const prNumber = prLink.textContent!.slice(1);
 
 	const runLink = prLink.closest('.Box-row')!.querySelector('a.Link--primary')!;
-	const parameters = new URLSearchParams(runLink.search);
-	parameters.set('pr', prNumber);
-	runLink.search = String(parameters);
+	setSearchParam(runLink, 'pr', prNumber)
 }
 
-async function init(signal: AbortSignal): Promise<void> {
-	observe('div.Box-row[id^=check_suite_] a[data-hovercard-type="pull_request"]', add, {signal});
+async function addForPR(dropdown: HTMLDivElement): Promise<void> {
+	const {branch: prNumber} = new GitHubURL(location.href);
+	const actions = select.all('div.merge-status-item:has(a[href="/apps/github-actions"]) a.status-actions', dropdown)
+
+	// TODO: The actions is empty when ajax response is slow
+
+	for(const action of actions) {
+		setSearchParam(action, 'pr', prNumber);
+	}
+}
+
+async function initForRepositoryActionsPage(signal: AbortSignal): Promise<void> {
+	observe('div.Box-row[id^=check_suite_] a[data-hovercard-type="pull_request"]', addForRepositoryActions, {signal});
+}
+
+async function initForPRPage(signal: AbortSignal): Promise<void> {
+	observe('div.js-timeline-item details.commit-build-statuses > div.dropdown-menu', addForPR, {signal});
 }
 
 void features.add(import.meta.url, {
 	include: [
 		pageDetect.isRepositoryActions,
 	],
-	init,
+	init: initForRepositoryActionsPage,
+}, {
+	include: [
+		pageDetect.isPR,
+	],
+	init: initForPRPage
 });
 
 /*
