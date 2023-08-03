e6b17929bfd01c88a98021cf29ad1f57027a5877
오지환 (Cooper) <cooper@chequer.io>
2023-08-03 21:27:47 +0900

    Extend `action-pr-link` to links in PRs (#6797)
    Co-authored-by: Federico Brigante <me@fregante.com>

--- source/features/action-pr-link.tsx
+++ source/features/action-pr-link.tsx
@@ -2,25 +2,44 @@ import * as pageDetect from 'github-url-detection';
 
 import features from '../feature-manager.js';
 import observe from '../helpers/selector-observer.js';
+import {getConversationNumber} from '../github-helpers/index.js';
 
-async function add(prLink: HTMLAnchorElement): Promise<void> {
+function setSearchParameter(anchorElement: HTMLAnchorElement, name: string, value: string): void {
+	const parameters = new URLSearchParams(anchorElement.search);
+	parameters.set(name, value);
+	anchorElement.search = String(parameters);
+}
+
+async function addForRepositoryActions(prLink: HTMLAnchorElement): Promise<void> {
 	const prNumber = prLink.textContent!.slice(1);
 
 	const runLink = prLink.closest('.Box-row')!.querySelector('a.Link--primary')!;
-	const parameters = new URLSearchParams(runLink.search);
-	parameters.set('pr', prNumber);
-	runLink.search = String(parameters);
+	setSearchParameter(runLink, 'pr', prNumber);
 }
 
-async function init(signal: AbortSignal): Promise<void> {
-	observe('div.Box-row[id^=check_suite_] a[data-hovercard-type="pull_request"]', add, {signal});
+async function addForPR(actionLink: HTMLAnchorElement): Promise<void> {
+	setSearchParameter(actionLink, 'pr', String(getConversationNumber()));
+}
+
+async function initForRepositoryActionsPage(signal: AbortSignal): Promise<void> {
+	observe('div.Box-row[id^=check_suite_] a[data-hovercard-type="pull_request"]', addForRepositoryActions, {signal});
+}
+
+async function initForPRPage(signal: AbortSignal): Promise<void> {
+	// Exclude rgh-link, include isPRCommits
+	observe('main [href="/apps/github-actions"] ~ div a.status-actions', addForPR, {signal});
 }
 
 void features.add(import.meta.url, {
 	include: [
 		pageDetect.isRepositoryActions,
 	],
-	init,
+	init: initForRepositoryActionsPage,
+}, {
+	include: [
+		pageDetect.isPR,
+	],
+	init: initForPRPage,
 });
 
 /*
@@ -28,5 +47,9 @@ void features.add(import.meta.url, {
 ## Test URLs
 
 https://github.com/refined-github/refined-github/actions
+
+https://github.com/refined-github/refined-github/pull/6794
+
+https://github.com/refined-github/refined-github/pull/6794/commits
 
 */
