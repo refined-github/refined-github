37cb79d81d05033fb0071a23e8f7d231143b364c
Cooper Oh <cooper@chequer.io>
2023-08-18 14:32:09 +0900

    Refactor

--- source/features/conflict-marker.tsx
+++ source/features/conflict-marker.tsx
@@ -5,43 +5,25 @@ import * as pageDetect from 'github-url-detection';
 
 import features from '../feature-manager.js';
 import api from '../github-helpers/api.js';
-import observe from "../helpers/selector-observer.js";
-
-// TODO: Remove type
-type PRConfig = {
-	number: string;
-	user: string;
-	repo: string;
-	link: HTMLAnchorElement;
-	key: string;
-};
-
-// TODO: seperate gql
-function createQueryFragment(pr: PRConfig): string {
-	return `
-		${pr.key}: repository(owner: "${pr.user}", name: "${pr.repo}") {
-			pullRequest(number: ${pr.number}) {
-				mergeable
-			}
-		}
-	`;
-}
-
-function getPRConfig(link: HTMLAnchorElement): PRConfig {
-	const [, user, repo, , number] = link.pathname.split('/');
-	return {user, repo, number, link, key: api.escapeKey(user, repo, number)};
-}
+import observe from '../helpers/selector-observer.js';
+import getPRConfig from './conflict-marker.gql';
 
 async function addConflictMarker(link: HTMLAnchorElement): Promise<void> {
-	const prConfig = getPRConfig(link);
-	const data = await api.v4(createQueryFragment(prConfig));
+	const [, user, repo, , prNumber] = link.pathname.split('/');
+	const {repository: data} = await api.v4(getPRConfig, {
+		variables: {
+			_owner: user,
+			_name: repo,
+			prNumber: Number.parseInt(prNumber, 10),
+		},
+	});
 
-	if (data[prConfig.key].pullRequest.mergeable === 'CONFLICTING') {
-		prConfig.link.after(
+	if (data.pullRequest.mergeable === 'CONFLICTING') {
+		link.after(
 			<a
 				className="rgh-conflict-marker tooltipped tooltipped-e color-fg-muted ml-2"
 				aria-label="This PR has conflicts that must be resolved"
-				href={`${prConfig.link.pathname}#partial-pull-merging`}
+				href={`${link.pathname}#partial-pull-merging`}
 			>
 				<AlertIcon className="v-align-middle"/>
 			</a>,
@@ -49,7 +31,7 @@ async function addConflictMarker(link: HTMLAnchorElement): Promise<void> {
 	}
 }
 
-function init2(signal: AbortSignal): void {
+function init(signal: AbortSignal): void {
 	observe('.js-issue-row:has(.octicon-git-pull-request.color-fg-open) a.js-navigation-open', addConflictMarker, {signal});
 }
 
@@ -61,7 +43,7 @@ void features.add(import.meta.url, {
 		pageDetect.isGlobalIssueOrPRList,
 		pageDetect.isBlank,
 	],
-	init: init2,
+	init,
 }, {
 	include: [
 		pageDetect.isGlobalIssueOrPRList,
@@ -69,7 +51,7 @@ void features.add(import.meta.url, {
 	exclude: [
 		pageDetect.isBlank,
 	],
-	init: init2,
+	init,
 });
 
 /** Test urls
