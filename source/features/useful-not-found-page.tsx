0119ce95bcff40757c521e25bc1dc6f8875c3301
Cooper Oh <cooper@chequer.io>
2023-08-19 12:04:44 +0900

    Fix selector

--- source/features/useful-not-found-page.tsx
+++ source/features/useful-not-found-page.tsx
@@ -221,7 +221,7 @@ async function initPRCommit(): Promise<void | false> {
 }
 
 function initRepoFile(signal: AbortSignal): void {
-	observe('main #repos-header-breadcrumb-wide-heading+ol a', checkAnchor, {signal});
+	observe('#repos-header-breadcrumb-wide-heading + ol a', checkAnchor, {signal});
 	observe('main div[data-testid="eror-404-description"]', addAlternateLinkForRepoFile, {signal});	// "eror" as misspelled by GitHub
 }
 
