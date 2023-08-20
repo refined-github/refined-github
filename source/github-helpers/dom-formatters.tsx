9ecd45e18a14307c10ff289e4da1638eb6c20930
Katsute <58778985+Katsute@users.noreply.github.com>
2023-08-20 20:12:08 +0000

    Discard linkified URLs followed by …

--- source/github-helpers/dom-formatters.tsx
+++ source/github-helpers/dom-formatters.tsx
@@ -80,6 +80,10 @@ export function linkifyURLs(element: Element): Element[] | void {
 		},
 	});
 
+	if(linkified.lastChild && linkified.lastChild.textContent === '…') { // Link is followed by …
+		return;
+	}
+
 	if (linkified.children.length === 0) { // Children are <a>
 		return;
 	}
