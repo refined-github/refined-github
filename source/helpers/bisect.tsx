9758e817a4b3ca5ff190d326dd978935adb1149a
Federico Brigante <me@fregante.com>
2023-09-04 01:20:54 +0800

    word + Oxford

--- source/helpers/bisect.tsx
+++ source/helpers/bisect.tsx
@@ -35,7 +35,7 @@ async function onChoiceButtonClick({currentTarget: button}: React.MouseEvent<HTM
 	if (answer === 'yes') {
 		createMessageBox(
 			<>
-				<p>Unable to identify feature. It might be a <a href="https://github.com/refined-github/refined-github/wiki/Undo-unwanted-styles" target="_blank" rel="noreferrer">CSS-only</a>, a <a href="https://github.com/refined-github/refined-github/wiki/Meta-features" target="_blank" rel="noreferrer">meta-feature</a> or unrelated to Refined GitHub.</p>
+				<p>Unable to identify feature. It might be a <a href="https://github.com/refined-github/refined-github/wiki/Undo-unwanted-styles" target="_blank" rel="noreferrer">CSS-only feature</a>, a <a href="https://github.com/refined-github/refined-github/wiki/Meta-features" target="_blank" rel="noreferrer">meta-feature</a>, or unrelated to Refined GitHub.</p>
 				<p>Try disabling Refined GitHub to see if the change or issue is caused by the extension.</p>
 			</>);
 	} else {
