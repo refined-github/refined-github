0d6c9ae52d7c3f97e217c341922aa8a1e181b6cf
Federico Brigante <me@fregante.com>
2023-09-03 14:16:35 +0800

    Revert "Test: ERR: ${featureId} should use either `addCssFeature` or `add`, not both"
    This reverts commit 0f3db5d6e6bcd6be5e2caba429ad27adb8bdde6c.

--- source/features/align-issue-labels.tsx
+++ source/features/align-issue-labels.tsx
@@ -4,6 +4,3 @@ import * as pageDetect from 'github-url-detection';
 import features from '../feature-manager.js';
 
 void features.addCssFeature(import.meta.url, [pageDetect.isIssueOrPRList]);
-void features.add(import.meta.url, {init() {
-	console.log('sup');
-}});
