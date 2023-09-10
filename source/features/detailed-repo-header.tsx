3a514b693058336e4dcb1de47e32766baef33cc3
Cooper Oh <cooper@chequer.io>
2023-09-10 21:09:23 +0900

    Lint

--- source/features/detailed-repo-header.tsx
+++ source/features/detailed-repo-header.tsx
@@ -1,16 +1,16 @@
 import * as pageDetect from 'github-url-detection';
-import { StarFillIcon } from '@primer/octicons-react';
+import {StarIcon} from '@primer/octicons-react';
 import React from 'dom-chef';
-import { CachedFunction } from 'webext-storage-cache';
+import {CachedFunction} from 'webext-storage-cache';
 
 import observe from '../helpers/selector-observer.js';
 import features from '../feature-manager.js';
 import api from '../github-helpers/api.js';
 import GetRepositoryStargazerCount from './detailed-repo-header.gql';
 import abbreviateNumber from '../helpers/abbreviate-number.js';
-import { cacheByRepo } from '../github-helpers/index.js';
+import {cacheByRepo} from '../github-helpers/index.js';
 
-const getStargazerCount = new CachedFunction('stargazer-count', {
+const stargazerCount = new CachedFunction('stargazer-count', {
 	async updater(): Promise<number> {
 		const {repository: {stargazerCount}} = await api.v4(GetRepositoryStargazerCount);
 		return stargazerCount;
@@ -20,21 +20,19 @@ const getStargazerCount = new CachedFunction('stargazer-count', {
 	cacheKey: cacheByRepo,
 });
 
-async function addForFull(navigationList: HTMLUListElement): Promise<void> {
-	const stargazerCount =  await getStargazerCount.get();
-
+async function add(navigationList: HTMLUListElement): Promise<void> {
 	navigationList.append(
 		<li className="color-fg-muted pl-2">
-			<div className="starred ml-2">
-				<StarFillIcon className="starred-button-icon mr-1"/>
-				<span className="Counter">{abbreviateNumber(stargazerCount)}</span>
+			<div className="d-flex flex-items-center flex-justify-center ml-2 gap-1">
+				<StarIcon className="v-align-text-bottom"/>
+				<span className="Counter v-align-bottom">{abbreviateNumber(await stargazerCount.get())}</span>
 			</div>
 		</li>,
 	);
 }
 
 function init(signal: AbortSignal): void {
-	observe('header .AppHeader-context-full > nav ul', addForFull, {signal});
+	observe('header .AppHeader-context-full > nav ul', add, {signal});
 }
 
 void features.add(import.meta.url, {
