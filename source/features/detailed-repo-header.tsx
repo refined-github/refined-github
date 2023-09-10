fe4ea9b4d36917ef5ca1214fbfceb7211e5143da
Cooper Oh <cooper@chequer.io>
2023-09-10 19:52:43 +0900

    unified css selector

--- source/features/detailed-repo-header.tsx
+++ source/features/detailed-repo-header.tsx
@@ -1,14 +1,14 @@
 import * as pageDetect from 'github-url-detection';
-import {RepoForkedIcon, StarFillIcon} from '@primer/octicons-react';
+import { RepoForkedIcon, StarFillIcon } from '@primer/octicons-react';
 import React from 'dom-chef';
-import {CachedFunction} from 'webext-storage-cache';
+import { CachedFunction } from 'webext-storage-cache';
 
 import observe from '../helpers/selector-observer.js';
 import features from '../feature-manager.js';
 import api from '../github-helpers/api.js';
 import GetRepositoryDetails from './detailed-repo-header.gql';
 import abbreviateNumber from '../helpers/abbreviate-number.js';
-import {cacheByRepo} from '../github-helpers/index.js';
+import { cacheByRepo } from '../github-helpers/index.js';
 
 type ForkAndStarCount = {
 	forkCount: number;
@@ -66,8 +66,8 @@ async function addForCompact(button: HTMLButtonElement): Promise<void> {
 }
 
 function init(signal: AbortSignal): void {
-	observe('header nav[role="navigation"] > ul', addForFull, {signal});
-	observe('.AppHeader-context-compact > button', addForCompact, {signal});
+	observe('header .AppHeader-context-full > nav ul', addForFull, {signal});
+	observe('header .AppHeader-context-compact > button', addForCompact, {signal});
 }
 
 void features.add(import.meta.url, {
