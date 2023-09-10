161894ccbd1271cbe2fcb5187c1ddf2bbe549187
Cooper Oh <cooper@chequer.io>
2023-09-10 20:50:15 +0900

    Just stargazer count

--- source/features/detailed-repo-header.tsx
+++ source/features/detailed-repo-header.tsx
@@ -1,55 +1,34 @@
 import * as pageDetect from 'github-url-detection';
-import { RepoForkedIcon, StarFillIcon } from '@primer/octicons-react';
+import { StarFillIcon } from '@primer/octicons-react';
 import React from 'dom-chef';
 import { CachedFunction } from 'webext-storage-cache';
 
 import observe from '../helpers/selector-observer.js';
 import features from '../feature-manager.js';
 import api from '../github-helpers/api.js';
-import GetRepositoryDetails from './detailed-repo-header.gql';
+import GetRepositoryStargazerCount from './detailed-repo-header.gql';
 import abbreviateNumber from '../helpers/abbreviate-number.js';
 import { cacheByRepo } from '../github-helpers/index.js';
 
-type ForkAndStarCount = {
-	forkCount: number;
-	stargazerCount: number;
-};
-
-const forkAndStarCount = new CachedFunction('fork-and-star-count', {
-	async updater(): Promise<ForkAndStarCount> {
-		const {repository: {forkCount, stargazerCount}} = await api.v4(GetRepositoryDetails);
-		return {forkCount, stargazerCount};
+const getStargazerCount = new CachedFunction('stargazer-count', {
+	async updater(): Promise<number> {
+		const {repository: {stargazerCount}} = await api.v4(GetRepositoryStargazerCount);
+		return stargazerCount;
 	},
 	maxAge: {days: 1},
 	staleWhileRevalidate: {days: 3},
 	cacheKey: cacheByRepo,
 });
 
-function createForkCounter(forkCount: number): HTMLElement {
-	return (
-		<div>
-			<RepoForkedIcon className="mr-1"/>
-			<span className="Counter">{abbreviateNumber(forkCount)}</span>
-		</div>
-	);
-}
-
-function createStarCounter(stargazerCount: number): HTMLElement {
-	return (
-		<div className="starred ml-2">
-			<StarFillIcon className="starred-button-icon mr-1"/>
-			<span className="Counter">{abbreviateNumber(stargazerCount)}</span>
-		</div>
-	);
-}
-
 async function addForFull(navigationList: HTMLUListElement): Promise<void> {
-	const {forkCount, stargazerCount} = await forkAndStarCount.get();
+	const stargazerCount =  await getStargazerCount.get();
 
 	navigationList.append(
 		<li className="color-fg-muted pl-2">
-			{createForkCounter(forkCount)}
-			{createStarCounter(stargazerCount)}
+			<div className="starred ml-2">
+				<StarFillIcon className="starred-button-icon mr-1"/>
+				<span className="Counter">{abbreviateNumber(stargazerCount)}</span>
+			</div>
 		</li>,
 	);
 }
