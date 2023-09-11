d7c61adc17a9123087a8b20e884e5e264ca938a6
Cooper Oh <cooper@chequer.io>
2023-09-12 02:50:17 +0900

    Lint

--- source/helpers/abbreviate-string.ts
+++ source/helpers/abbreviate-string.ts
@@ -1,3 +1,3 @@
-export default function abbreviateString(string_: string, length: number, abbrevMarker: string = '…'): string {
+export default function abbreviateString(string_: string, length: number, abbrevMarker = '…'): string {
 	return string_.slice(0, length) + abbrevMarker;
 }
