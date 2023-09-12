e5e1eda2674ce62e6ecdebc32ee7ed18166e7d07
Federico Brigante <me@fregante.com>
2023-09-12 14:37:46 +0800

    Conditionally abbreviate string

--- source/helpers/abbreviate-string.ts
+++ source/helpers/abbreviate-string.ts
@@ -1,3 +1,3 @@
 export default function abbreviateString(string_: string, length: number, abbrevMarker = '…'): string {
-	return string_.slice(0, length) + abbrevMarker;
+	return string_.length < length ? string : string_.slice(0, length) + '…';
 }
