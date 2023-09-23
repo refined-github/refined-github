301fbb583cf47a4d3ce05cafebda2e23ba2bbe01
Federico Brigante <me@fregante.com>
2023-09-23 23:05:21 +0800

    Forgotten globals

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,10 @@
 // eslint-disable-next-line n/file-extension-in-import -- No export map support
 import {defineConfig} from 'vitest/config';
 
-export default defineConfig({});
+export default defineConfig({
+	test: {
+		setupFiles: [
+			'./test/fixtures/globals.js',
+		],
+	},
+});
