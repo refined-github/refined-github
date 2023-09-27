790025bdd9cf8559c1397e5644e35b366a84ff6c
Federico Brigante <me@fregante.com>
2023-09-27 19:01:56 +0800

    Add `rgh-pr-template` feature (#6947)

---
+++ source/features/rgh-pr-template.tsx
@@ -0,0 +1,39 @@
+import React from 'dom-chef';
+import {replace} from 'text-field-edit';
+import * as pageDetect from 'github-url-detection';
+
+import features from '../feature-manager.js';
+import {isRefinedGitHubRepo} from '../github-helpers/index.js';
+import observe from '../helpers/selector-observer.js';
+
+function extract(textarea: HTMLTextAreaElement): void {
+	replace(textarea, /<!--(.+)-->\n/s, (_, match) => {
+		textarea.closest('tab-container')!.before(
+			<div style={{whiteSpace: 'pre-wrap'}} className="flash mx-2 p-2">
+				{match.trim()}
+			</div>,
+		);
+
+		return '';
+	});
+}
+
+function init(signal: AbortSignal): void {
+	observe('#pull_request_body', extract, {signal});
+}
+
+void features.add(import.meta.url, {
+	asLongAs: [
+		isRefinedGitHubRepo,
+		pageDetect.isCompare,
+	],
+	init,
+});
+
+/*
+
+Test URLs:
+
+https://github.com/refined-github/refined-github/compare/main...sandbox/keep-branch?quick_pull=1
+
+*/
