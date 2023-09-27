d83f87f43106a9ae90747a6c265e2a74ba626396
Emily M Klassen <forivall@gmail.com>
2023-09-27 09:39:39 -0700

    fix(keyboard-navigation): preserve selection in group header

--- source/features/keyboard-navigation.tsx
+++ source/features/keyboard-navigation.tsx
@@ -6,15 +6,15 @@ import * as pageDetect from 'github-url-detection';
 import features from '../feature-manager.js';
 import {isEditable} from '../helpers/dom-utils.js';
 
-const isDisplayNone = (element: Element | undefined): boolean => !!element && getComputedStyle(element).display === 'none';
+const isDisplayNone = (element: Element | undefined): boolean => Boolean(element && getComputedStyle(element).display === 'none');
 
 const isCommentGroupMinimized = (comment: HTMLElement): boolean =>
 	elementExists('.minimized-comment:not(.d-none)', comment)
+	|| isDisplayNone(comment.closest('.js-file-content') ?? undefined) // Review comments on Files tab
 	|| Boolean(comment.closest([
 		'.js-resolvable-thread-contents.d-none', // Regular comments
 		'details.js-resolvable-timeline-thread-container:not([open])', // Review comments on Conversation tab
-	])
-	|| isDisplayNone(comment.closest('.js-file-content')!), // Review comments on Files tab
+	]),
 	);
 
 const isFileMinimized = (element: HTMLElement | undefined): boolean =>
@@ -22,7 +22,10 @@ const isFileMinimized = (element: HTMLElement | undefined): boolean =>
 
 let lastViewChange: HTMLElement | undefined;
 function trackLastViewChange(event: Event): void {
-	lastViewChange = (event.target as EventTarget & Partial<Pick<Element, 'closest'>>).closest?.('.js-targetable-element[id^="diff-"]') ?? undefined;
+	const element = (event.target as EventTarget & Partial<Pick<Element, 'closest'>>).closest?.('.js-targetable-element[id^="diff-"]') ?? undefined;
+	if (element) {
+		lastViewChange = element;
+	}
 }
 
 function runShortcuts(event: KeyboardEvent): void {
@@ -32,12 +35,13 @@ function runShortcuts(event: KeyboardEvent): void {
 
 	event.preventDefault();
 
-	const focusedComment = $optional(globalThis.location.hash || ':target') || lastViewChange;
+	const focusedComment = $optional(globalThis.location.hash || ':target') ?? lastViewChange;
 
 	if (event.key === 'x') {
 		if (!focusedComment) {
 			return;
 		}
+
 		const toggle = $optional('.js-reviewed-toggle', focusedComment);
 		if (toggle) {
 			const wasFileMinimized = isFileMinimized(focusedComment);
@@ -46,6 +50,7 @@ function runShortcuts(event: KeyboardEvent): void {
 				location.replace('#' + focusedComment.id);
 			}
 		}
+
 		return;
 	}
 
@@ -63,7 +68,13 @@ function runShortcuts(event: KeyboardEvent): void {
 	// `j` goes to the next comment, `k` goes back a comment
 	const direction = event.key === 'j' ? 1 : -1;
 	// Without `focusedElement`, it will start from -1
-	const currentIndex = items.indexOf(focusedComment!);
+	let currentIndex = items.indexOf(focusedComment!);
+	if (currentIndex < 0) {
+		const closestComment = focusedComment?.querySelector('.js-minimizable-comment-group');
+		if (closestComment) {
+			currentIndex = items.indexOf(closestComment);
+		}
+	}
 
 	// Start at 0 if nothing is; clamp index
 	const chosenCommentIndex = Math.min(
@@ -77,10 +88,12 @@ function runShortcuts(event: KeyboardEvent): void {
 			if (item.classList.contains('details-collapsed-target')) {
 				item.classList.remove('details-collapsed-target');
 			}
+
 			if (item.classList.contains('not-target')) {
 				item.classList.remove('not-target');
 			}
 		}
+
 		if (chosenComment.classList.contains('js-details-container') && isFileMinimized(chosenComment)) {
 			// Change hash without focusing and expanding
 			globalThis.history.replaceState(globalThis.history.state, '', '#' + chosenComment.id);
@@ -97,6 +110,7 @@ function runShortcuts(event: KeyboardEvent): void {
 function init(signal: AbortSignal): void {
 	document.body.addEventListener('keypress', runShortcuts, {signal});
 	document.body.addEventListener('change', trackLastViewChange);
+	document.body.addEventListener('focus', trackLastViewChange);
 }
 
 void features.add(import.meta.url, {
