23433f4b299e5932fa541a2eb329226d4bfcef44
Katsute <58778985+Katsute@users.noreply.github.com>
2023-09-28 16:55:28 +0000

    update image, init once, add subscriptions

--- source/features/small-user-avatars.tsx
+++ source/features/small-user-avatars.tsx
@@ -1,6 +1,8 @@
 import './small-user-avatars.css';
 import React from 'dom-chef';
 
+import onetime from 'onetime';
+
 import features from '../feature-manager.js';
 import observe from '../helpers/selector-observer.js';
 import getUserAvatarURL from '../github-helpers/get-user-avatar.js';
@@ -35,20 +37,22 @@ function addMentionAvatar(link: HTMLElement): void {
 	);
 }
 
-function init(signal: AbortSignal): void {
+function init(): void {
 	// Excludes bots
-	observe('.js-issue-row [data-hovercard-type="user"]', addAvatar, {signal});
-	observe('.user-mention[data-hovercard-type="user"]', addMentionAvatar, {signal});
+	observe('.js-issue-row [data-hovercard-type="user"]', addAvatar);
+	observe('.notification-thread-subscription [data-hovercard-type="user"]', addAvatar);
+	observe('.user-mention[data-hovercard-type="user"]', addMentionAvatar);
 }
 
 void features.add(import.meta.url, {
-	init,
+	init: onetime(init),
 });
 
 /*
 
 Test URLs:
 
+https://github.com/notifications/subscriptions
 https://github.com/refined-github/refined-github/issues
 https://github.com/refined-github/refined-github/issues/6919
 https://github.com/refined-github/refined-github/releases
