c7d758a4fc96818033a03787333fb333dcedc3e0
Ayub <sir.kokabi@gmail.com>
2023-10-06 12:29:31 +0330

    fix linting errors

--- source/features/quick-new-issue.tsx
+++ source/features/quick-new-issue.tsx
@@ -23,15 +23,15 @@ function add(dropdownMenu: HTMLElement): void {
 		<li role="presentation" aria-hidden="true" data-view-component="true" className="ActionList-sectionDivider"/>,
 		<li data-targets="action-list.items" role="none" data-view-component="true" className="ActionListItem">
 			<a href={buildRepoURL('issues/new/choose')} tabIndex={-1} role="menuitem" data-view-component="true" className="ActionListContent">
-			<span className="ActionListItem-visual ActionListItem-visual--leading">
-				<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" className="octicon octicon-repo-push">
-    				<path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"></path>
-					<path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0ZM1.5 8a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Z"></path>
-				</svg>
-        	</span>				
-			<span data-view-component="true" className="ActionListItem-label">
-				New issue in {getRepo()?.name}
-			</span>
+				<span className="ActionListItem-visual ActionListItem-visual--leading">
+					<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" className="octicon octicon-repo-push">
+						<path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"/>
+						<path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0ZM1.5 8a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Z"/>
+					</svg>
+				</span>
+				<span data-view-component="true" className="ActionListItem-label">
+					New issue in {getRepo()?.name}
+				</span>
 			</a>
 		</li>,
 	);
