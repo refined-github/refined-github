03777c1ed759527e588c41f9d423869dffcec898
Ayub <sir.kokabi@gmail.com>
2023-10-08 14:42:48 +0330

    use IssueOpenedIcon from octicons-react

--- source/features/quick-new-issue.tsx
+++ source/features/quick-new-issue.tsx
@@ -1,5 +1,6 @@
 import React from 'dom-chef';
 import * as pageDetect from 'github-url-detection';
+import {IssueOpenedIcon} from '@primer/octicons-react';
 
 import features from '../feature-manager.js';
 import {buildRepoURL, getRepo, isArchivedRepoAsync} from '../github-helpers/index.js';
@@ -24,10 +25,7 @@ function add(dropdownMenu: HTMLElement): void {
 		<li data-targets="action-list.items" role="none" data-view-component="true" className="ActionListItem">
 			<a href={buildRepoURL('issues/new/choose')} tabIndex={-1} role="menuitem" data-view-component="true" className="ActionListContent">
 				<span className="ActionListItem-visual ActionListItem-visual--leading">
-					<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" className="octicon octicon-repo-push">
-						<path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"/>
-						<path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0ZM1.5 8a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Z"/>
-					</svg>
+					<IssueOpenedIcon/>
 				</span>
 				<span data-view-component="true" className="ActionListItem-label">
 					New issue in {getRepo()?.name}
