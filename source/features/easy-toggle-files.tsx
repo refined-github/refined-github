ce0da27d1a58f2cac2a7cb184343e859b1e60e06
Katsute <58778985+Katsute@users.noreply.github.com>
2023-11-05 19:34:58 +0000

    toggle code search files

--- source/features/easy-toggle-files.tsx
+++ source/features/easy-toggle-files.tsx
@@ -15,8 +15,23 @@ function toggleFile(event: DelegateEvent<MouseEvent>): void {
 	}
 }
 
+function toggleCSFile(event: DelegateEvent<MouseEvent>): void {
+	const elementClicked = event.target as HTMLElement;
+	const headerBar = event.delegateTarget;
+
+	// The clicked element is either the bar itself or one of its 2 children and not the toggle button
+	if (elementClicked === headerBar || (elementClicked.parentElement === headerBar && elementClicked.tagName !== 'BUTTON')) {
+		$(['[aria-label^="Collapse "]', '[aria-label^="Expand "]'], headerBar)!
+			.dispatchEvent(new MouseEvent('click', {bubbles: true, altKey: event.altKey}));
+	}
+}
+
 function init(signal: AbortSignal): void {
 	delegate('.file-header', 'click', toggleFile, {signal});
+}
+
+function initCS(signal: AbortSignal): void {
+	delegate('div[data-testid="results-list"] > div > div:first-child', 'click', toggleCSFile, {signal});
 }
 
 void features.add(import.meta.url, {
@@ -25,4 +40,18 @@ void features.add(import.meta.url, {
 		pageDetect.isGistRevision,
 	],
 	init,
+}, {
+	include: [
+		pageDetect.isGlobalSearchResults,
+	],
+	init: initCS,
 });
+
+/*
+
+## Test URLs
+
+- Pull Request:
+- Code Search: https://github.com/search?q=repo%3Arefined-github%2Frefined-github%20easy&type=code
+
+*/
