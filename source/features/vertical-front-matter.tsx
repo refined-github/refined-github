a4a7f4839cc95b2e90a26171291b0993a1b25053
Federico Brigante <me@fregante.com>
2023-11-22 16:58:21 +0800

    `vertical-front-matter` - Fix flickering on content rehydration (#7069)

--- source/features/vertical-front-matter.tsx
+++ source/features/vertical-front-matter.tsx
@@ -2,27 +2,18 @@ import './vertical-front-matter.css';
 import React from 'dom-chef';
 import {$$} from 'select-dom';
 import * as pageDetect from 'github-url-detection';
-import elementReady from 'element-ready';
 
 import features from '../feature-manager.js';
+import observe from '../helpers/selector-observer.js';
 
 // https://github.com/github/markup/blob/cd01f9ec87c86ce5a7c70188a74ef40fc4669c5b/lib/github/markup/markdown.rb#L34
 const hasFrontMatter = (): boolean => pageDetect.isSingleFile() && /\.(mdx?|mkdn?|mdwn|mdown|markdown|litcoffee)$/.test(location.pathname);
 
-async function init(): Promise<false | void> {
-	const table = await elementReady('.markdown-body > table:first-child');
-	if (!table) {
-		return false;
-	}
-
-	const headers = $$(':scope > thead th', table);
-	if (headers.length <= 4) {
-		return false;
-	}
-
+function transpose(table: HTMLElement): void {
 	const rows = $$(':scope > tbody > tr', table);
-	if (rows.length !== 1 || headers.length !== rows[0].childElementCount) {
-		return false;
+	const headers = $$(':scope > thead th', table);
+	if (headers.length <= 4 || rows.length !== 1 || headers.length !== rows[0].childElementCount) {
+		return;
 	}
 
 	const values = [...rows[0].children];
@@ -40,10 +31,21 @@ async function init(): Promise<false | void> {
 	);
 }
 
+function init(signal: AbortSignal): void {
+	observe('.markdown-body > table:first-child', transpose, {signal});
+}
+
 void features.add(import.meta.url, {
 	include: [
 		hasFrontMatter,
 	],
-	deduplicate: '.rgh-vertical-front-matter-table',
 	init,
 });
+
+/*
+
+Test URLs:
+
+https://github.com/github/docs/blob/114de99a/content/repositories/working-with-files/managing-files/customizing-how-changed-files-appear-on-github.md
+
+*/
