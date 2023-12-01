a71642a9bd032491216e42100d38b02519662aee
Federico Brigante <me@fregante.com>
2023-12-01 13:53:05 +0800

    CLEAN 🫸😌🫷

--- source/features/clean-readme-url.tsx
+++ source/features/clean-readme-url.tsx
@@ -2,32 +2,17 @@ import * as pageDetect from 'github-url-detection';
 
 import features from '../feature-manager.js';
 
-function getCleanUrlOrNothing(url: string): string | void {
-	const parsed = new URL(url);
+function maybeCleanUrl(event?: NavigateEvent): void {
+	const parsed = new URL(event?.destination.url ?? location.href);
 	if (parsed.searchParams.get('tab') === 'readme-ov-file') {
 		parsed.searchParams.delete('tab');
-		return parsed.href;
+		history.replaceState(history.state, '', parsed.href);
 	}
-}
-
-function maybeCleanCurrentUrl(url: string): void {
-	const cleanedUrl = getCleanUrlOrNothing(url);
-	if (cleanedUrl) {
-		console.log('cleaning URL', url, '->', cleanedUrl);
-
-		history.replaceState(history.state, '', cleanedUrl);
-	}
-}
-
-function onNavigation(event: NavigateEvent): void {
-	console.log('onNavigation', event.destination.url, event);
-
-	maybeCleanCurrentUrl(event.destination.url);
 }
 
 function init(signal: AbortSignal): void {
-	maybeCleanCurrentUrl(location.href);
-	window.navigation?.addEventListener('navigate', onNavigation, {signal});
+	maybeCleanUrl();
+	window.navigation?.addEventListener('navigate', maybeCleanUrl, {signal});
 }
 
 void features.add(import.meta.url, {
