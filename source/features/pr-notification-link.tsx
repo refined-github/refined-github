2b38874f813423cd129f387a3dc3c7225578e43e
Federico Brigante <me@fregante.com>
2023-12-16 13:33:06 +0800

    Simplify for now

--- source/features/pr-notification-link.tsx
+++ source/features/pr-notification-link.tsx
@@ -1,39 +1,15 @@
 import * as pageDetect from 'github-url-detection';
-import domLoaded from 'dom-loaded';
-import delay from 'delay';
 
 import features from '../feature-manager.js';
 import observe from '../helpers/selector-observer.js';
 
 const regex = /\/files\/[\da-f]{40}..[\da-f]{40}$/;
-const rghParameter = 'latest-changes';
 
 function trimLink(link: HTMLAnchorElement): void {
 	if (regex.test(link.pathname)) {
 		link.pathname = link.pathname.replace(regex, '');
-		link.hash = '#partial-timeline';
-		const search = new URLSearchParams(link.search);
-		search.set('rgh', rghParameter);
-		link.search = String(search);
+		link.hash = '#issue-comment-box';
 	}
-}
-
-async function scrollToLatestChanges(): Promise<void> {
-	const url = new URL(location.href);
-	url.searchParams.delete('rgh');
-	url.hash = '';
-	history.replaceState(history.state, '', url.href);
-
-	const controller = new AbortController();
-	observe('new changes since you last visited', element => {
-		element.scrollIntoView();
-		controller.abort();
-	}, {signal: controller.signal});
-
-	// It won't appear after this point
-	await domLoaded;
-	await delay(100); // Allow the observer to trigger just in case
-	controller.abort();
 }
 
 function init(signal: AbortSignal): void {
@@ -46,11 +22,6 @@ void features.add(import.meta.url, {
 		pageDetect.isNotifications,
 	],
 	init,
-}, {
-	include: [
-		() => new URLSearchParams(location.search).get('rgh') === rghParameter,
-	],
-	init: scrollToLatestChanges,
 });
 
 /*
