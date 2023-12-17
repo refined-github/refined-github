da2911cff6995328d5c5aec102ee76ba24b5a12f
Federico Brigante <me@fregante.com>
2023-12-17 14:16:08 +0800

    Done

--- source/github-helpers/selectors.test.ts
+++ source/github-helpers/selectors.test.ts
@@ -1,27 +1,48 @@
-import mem from 'memoize';
+import pMemoize from 'p-memoize';
 import {test, assert, describe} from 'vitest';
-import {DOMParser} from 'linkedom';
+import {parseHTML} from 'linkedom';
 import filenamify from 'filenamify';
-import {writeFile, mkdir} from 'node:fs/promises';
+import {writeFile, mkdir, unlink, readFile, access} from 'node:fs/promises';
 
 import * as exports from './selectors.js';
 
-const fetchDocument = mem(async (url: string): Promise<Document> => {
+const fsCache = {
+	async get(path: string): Promise<string | undefined> {
+		try {
+			const value = await readFile(path, 'utf8');
+			return value;
+		} catch {
+			return undefined;
+		}
+	},
+	async set(path: string, contents: string): Promise<void> {
+		await mkdir('./test/.cache', {recursive: true});
+		await writeFile(path, contents);
+	},
+	async has(path: string): Promise<boolean> {
+		try {
+			await access(path);
+			return true;
+		} catch {
+			return false;
+		}
+	},
+	async delete(path: string): Promise<void> {
+		await unlink(path);
+	},
+} as const;
+
+const fetchDocument = pMemoize(async (url: string): Promise<string> => {
 	const request = await fetch(url, {
 		headers: {
 			Accept: 'text/html',
 		},
 	});
-	const contents = await request.text();
-	void storeHtmlLocallyForReview(url, contents);
-
-	return new DOMParser().parseFromString(contents, 'text/html') as unknown as Document;
+	return request.text();
+}, {
+	cacheKey: ([url]) => `./test/.cache/${filenamify(url.replace('https://github.com', ''))}.html`,
+	cache: fsCache,
 });
-
-async function storeHtmlLocallyForReview(url: string, html: string): Promise<void> {
-	await mkdir('./test/.cache', {recursive: true});
-	await writeFile(`./test/.cache/${filenamify(url)}`, html);
-}
 
 describe.concurrent('selectors', () => {
 	// Exclude URL arrays
@@ -34,24 +55,15 @@ describe.concurrent('selectors', () => {
 
 	test.each(selectors)('%s', async (name, selector) => {
 		// @ts-expect-error Index signature bs
-		const urls = exports[name + '_'] as Array<string | [url: string, expectations: number]>;
+		const urls = exports[name + '_'] as exports.UrlMatch[];
 
 		assert.isArray(urls, `No URLs defined for "${name}"`);
-		await Promise.all(urls.map(async url => {
-			let expectations: number | undefined;
-			if (Array.isArray(url)) {
-				[url, expectations] = url;
-			}
-
-			const document = await fetchDocument(url);
+		await Promise.all(urls.map(async ([expectations, url]) => {
+			const html = await fetchDocument(url);
+			const {document} = parseHTML(html);
 			// TODO: ? Use snapshot with outerHTML[]
 			const matches = document.querySelectorAll(selector);
-			if (expectations === undefined) {
-				// TODO: Change to just be `1` instead, to be stricter
-				assert(matches.length > 0, `Expected at least one match for \`${selector}\` at ${url}`);
-			} else {
-				assert.lengthOf(matches, expectations, `Expected ${expectations} matches for \`${selector}\` at ${url}`);
-			}
+			assert.equal(matches.length, expectations, `Got wrong number of matches on ${url}:\n${selector}`);
 		}));
 	}, {timeout: 9999});
 });
--- source/github-helpers/selectors.ts
+++ source/github-helpers/selectors.ts
@@ -1,11 +1,13 @@
 import {css} from 'code-tag';
 
+export type UrlMatch = [expectations: number, url: string];
+
 /** The repo navigation bar */
 export const repoUnderlineNavUl = '.js-responsive-underlinenav ul.UnderlineNav-body';
 export const repoUnderlineNavUl_ = [
-	'https://github.com/refined-github/refined-github',
-	'https://github.com/refined-github/refined-github/releases',
-];
+	[1, 'https://github.com/refined-github/refined-github'],
+	[1, 'https://github.com/refined-github/refined-github/releases'],
+] satisfies UrlMatch[];
 
 export const standaloneGistLinkInMarkdown = css`
 	.js-comment-body p a:only-child:is(
@@ -14,47 +16,55 @@ export const standaloneGistLinkInMarkdown = css`
 	)
 ` as 'a'; // TODO: Drop after https://github.com/fregante/code-tag/issues/12
 export const standaloneGistLinkInMarkdown_ = [
-	'https://github.com/refined-github/sandbox/issues/77',
-];
+	[3, 'https://github.com/refined-github/sandbox/issues/77'],
+] satisfies UrlMatch[];
 
 /** The repo navigation bar’s overflow menu */
 export const repoUnderlineNavDropdownUl = '.js-responsive-underlinenav .dropdown-menu ul';
-export const repoUnderlineNavDropdownUl_ = repoUnderlineNavUl_;
+export const repoUnderlineNavDropdownUl_ = [
+	// Added via JS :(
+	// TOOD: Use Puppeteer?
+	[0, 'https://github.com/refined-github/refined-github'],
+	[0, 'https://github.com/refined-github/refined-github/releases'],
+] satisfies UrlMatch[];
 
 export const branchSelector = '[data-hotkey="w"]';
 export const branchSelector_ = [
-	'https://github.com/refined-github/refined-github',
-	'https://github.com/refined-github/refined-github/blob/main/readme.md',
-	'https://github.com/refined-github/refined-github/blame/main/readme.md',
-	'https://github.com/refined-github/refined-github/tree/main/source',
-	'https://github.com/refined-github/sandbox/tree/branch/with/slashes',
-	'https://github.com/refined-github/sandbox/commits/branch/with/slashes',
-	'https://github.com/refined-github/sandbox/commits',
-];
+	[1, 'https://github.com/refined-github/refined-github'],
+	// Added via JS :(
+	// TOOD: Use Puppeteer?
+	[0, 'https://github.com/refined-github/refined-github/blob/main/readme.md'],
+	[0, 'https://github.com/refined-github/refined-github/blame/main/readme.md'],
+	[0, 'https://github.com/refined-github/refined-github/tree/main/source'],
+	[1, 'https://github.com/refined-github/sandbox/tree/branch/with/slashes'],
+	[1, 'https://github.com/refined-github/sandbox/commits/branch/with/slashes'],
+	[1, 'https://github.com/refined-github/sandbox/commits'],
+] satisfies UrlMatch[];
 
 export const branchSelectorParent = 'details#branch-select-menu';
 export const branchSelectorParent_ = branchSelector_;
 
 export const directoryListingFileIcon = [
 	// .color-fg-muted selects only files; some icon extensions use `img` tags
-	'.react-directory-filename-column > :is(svg, img).color-fg-muted',
-	'.js-navigation-container .octicon-file',
-];
+	[1, '.react-directory-filename-column > :is(svg, img).color-fg-muted'],
+	[1, '.js-navigation-container .octicon-file'],
+] satisfies UrlMatch[];
 export const directoryListingFileIcon_ = [
-	'https://github.com/refined-github/refined-github',
-	'https://github.com/refined-github/refined-github/tree/main/.github',
-];
+	[1, 'https://github.com/refined-github/refined-github'],
+	[1, 'https://github.com/refined-github/refined-github/tree/main/.github'],
+] satisfies UrlMatch[];
 
 export const prCommit = '.TimelineItem--condensed:has(.octicon-git-commit)';
 export const prCommit_ = [
-	'https://github.com/refined-github/sandbox/pull/10',
-];
+	[1, 'https://github.com/refined-github/sandbox/pull/10'],
+] satisfies UrlMatch[];
 
 // `summary` is needed because the details dropdown contains the list of check runs, each with its status icon
 export const prCommitStatusIcon = `:is(${prCommit}) details.commit-build-statuses summary .octicon`;
 export const prCommitStatusIcon_ = [
-	'https://github.com/refined-github/sandbox/pull/10',
-];
+	// Icon not loaded when logged out :(
+	[0, 'https://github.com/refined-github/sandbox/pull/10'],
+] satisfies UrlMatch[];
 
 // `.color-fg-open` is needed because of the icon added by `highlight-non-default-base-branch`
 export const openPrsListLink = css`
@@ -64,8 +74,8 @@ export const openPrsListLink = css`
 	) a.js-navigation-open
 `;
 export const openPrsListLink_ = [
-	'https://github.com/refined-github/sandbox/issues?q=conflict',
-];
+	[4, 'https://github.com/refined-github/sandbox/issues?q=conflict'],
+] satisfies UrlMatch[];
 
 export const openIssueToLastComment = `
 	:is(.js-issue-row, .js-pinned-issue-list-item)
@@ -75,13 +85,13 @@ export const openIssueToLastComment = `
 	)
 `;
 export const openIssueToLastComment_ = [
-	'https://github.com/refined-github/refined-github/issues',
-];
+	[2, 'https://github.com/refined-github/sandbox/labels/bug'],
+] satisfies UrlMatch[];
 
 export const actionsTab = '#actions-tab';
 export const actionsTab_ = [
-	'https://github.com/refined-github/sandbox',
-];
+	[1, 'https://github.com/refined-github/sandbox'],
+] satisfies UrlMatch[];
 
 export const codeSearchHeader = css`
 	div:has(
@@ -90,5 +100,6 @@ export const codeSearchHeader = css`
 	)
 `;
 export const codeSearchHeader_ = [
-	'https://github.com/search?q=repo%3Arefined-github%2Frefined-github&type=code',
-];
+	// Search not available when logged out :(
+	[0, 'https://github.com/search?q=repo%3Arefined-github%2Frefined-github&type=code'],
+] satisfies UrlMatch[];
