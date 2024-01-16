22e52ee14ee70aa1ce91c5f3f2038d8a1ff8a90a
ilike2burnthing <59480337+ilike2burnthing@users.noreply.github.com>
2024-01-16 22:25:08 +0000

    Update clean-pinned-issues.tsx

--- source/features/clean-pinned-issues.tsx
+++ source/features/clean-pinned-issues.tsx
@@ -4,3 +4,5 @@ import * as pageDetect from 'github-url-detection';
 import features from '../feature-manager.js';
 
 void features.addCssFeature(import.meta.url, [pageDetect.isRepoIssueList]);
+
+// Scopes: NONE
