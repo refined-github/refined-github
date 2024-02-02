20a943cab1e5ef73b9ba2b254946a15b3e49c151
Federico Brigante <me@fregante.com>
2024-02-02 15:20:02 +0800

    expectElement

--- source/features/profile-gists-link.tsx
+++ source/features/profile-gists-link.tsx
@@ -1,6 +1,6 @@
 import React from 'dom-chef';
 import {CachedFunction} from 'webext-storage-cache';
-import {$, elementExists} from 'select-dom';
+import {expectElement, elementExists} from 'select-dom';
 import * as pageDetect from 'github-url-detection';
 import CodeSquareIcon from 'octicons-plain-react/CodeSquare';
 
@@ -49,7 +49,7 @@ async function appendTab(navigationBar: Element): Promise<void> {
 	navigationBar.append(link);
 
 	// There are two UnderlineNav items (responsive…) that point to the same dropdown
-	const overflowNav = $(repoUnderlineNavDropdownUl)!;
+	const overflowNav = expectElement(repoUnderlineNavDropdownUl);
 	if (!elementExists('[data-rgh-label="Gists"]', overflowNav)) {
 		overflowNav.append(
 			createDropdownItem({
