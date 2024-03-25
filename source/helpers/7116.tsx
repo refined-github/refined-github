b0f15f4bf31d9723a61b82b0c7a3a6989fa6a0ac
Federico Brigante <me@fregante.com>
2024-03-25 17:37:50 +0700

    Lint

--- source/helpers/7116.tsx
+++ source/helpers/7116.tsx
@@ -1,5 +1,5 @@
-import {isPRFiles} from "github-url-detection";
-import {isChrome} from "webext-detect-page"
+import {isPRFiles} from 'github-url-detection';
+import {isChrome} from 'webext-detect-page';
 
 // TODO: Bad performance https://github.com/refined-github/refined-github/issues/7116
 export default function isBadBrowserOnPrFiles(): boolean {
