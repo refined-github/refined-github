e5f6142096fc840073683b984cfd5ec144e2bb40
Federico Brigante <me@fregante.com>
2024-05-27 22:14:10 +0800

    Discard changes to source/github-helpers/get-current-git-ref.ts

--- source/github-helpers/get-current-git-ref.ts
+++ source/github-helpers/get-current-git-ref.ts
@@ -4,17 +4,17 @@ import {$} from 'select-dom';
 import {extractCurrentBranchFromBranchPicker} from './index.js';
 import {branchSelector} from './selectors.js';
 
-const typesWithGitReference = new Set(['tree', 'blob', 'blame', 'edit', 'commit', 'commits', 'compare']);
-const titleWithGitReference = / at (?<branch>[.\w-/]+)( · [\w-]+\/[\w-]+)?$/i;
+const typesWithGitRef = new Set(['tree', 'blob', 'blame', 'edit', 'commit', 'commits', 'compare']);
+const titleWithGitRef = / at (?<branch>[.\w-/]+)( · [\w-]+\/[\w-]+)?$/i;
 
 /** Must not be async because it's used by GitHubFileURL. May return different results depending on whether it's called before or after DOM ready */
-export default function getCurrentGitReference(): string | undefined {
+export default function getCurrentGitRef(): string | undefined {
 	// Note: This is not in the <head> so it's only available on AJAXed loads.
 	// It appears on every Code page except `commits` on folders/files
 	const picker = $(branchSelector);
-	const referenceViaPicker = picker && extractCurrentBranchFromBranchPicker(picker);
-	if (referenceViaPicker) {
-		return referenceViaPicker;
+	const refViaPicker = picker && extractCurrentBranchFromBranchPicker(picker);
+	if (refViaPicker) {
+		return refViaPicker;
 	}
 
 	// Slashed branches on `commits`, including pages without a branch picker
@@ -31,20 +31,20 @@ export function getGitRef(pathname: string, title: string): string | undefined {
 		throw new TypeError(`Expected pathname starting with /, got "${pathname}"`);
 	}
 
-	const [type, gitReferenceIfNonSlashed] = pathname.split('/').slice(3);
-	if (!type || !typesWithGitReference.has(type)) {
+	const [type, gitRefIfNonSlashed] = pathname.split('/').slice(3);
+	if (!type || !typesWithGitRef.has(type)) {
 		// Root; or piece of information not applicable to the page
 		return;
 	}
 
 	// Slashed branches on `blob` and `tree`
-	const parsedTitle = titleWithGitReference.exec(title);
+	const parsedTitle = titleWithGitRef.exec(title);
 	if (parsedTitle) {
 		return parsedTitle.groups!.branch;
 	}
 
 	// Couldn't ensure it's not slashed, so we'll return the first piece whether correct or not
-	return gitReferenceIfNonSlashed;
+	return gitRefIfNonSlashed;
 }
 
 // In <head>, but not reliable https://github.com/refined-github/refined-github/assets/1402241/50357d94-505f-48dc-bd54-74e86b19d642
