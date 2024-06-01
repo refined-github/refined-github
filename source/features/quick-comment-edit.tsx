19d3c2dc0b32679c1605f36a9d11149533fd86b3
Federico <me@fregante.com>
2024-06-01 13:48:32 +0800

    `quick-comment-edit` - Drop CSS altogether, move the logic in JS

--- source/features/quick-comment-edit.tsx
+++ source/features/quick-comment-edit.tsx
@@ -1,4 +1,3 @@
-import './quick-comment-edit.css';
 import React from 'dom-chef';
 import {elementExists} from 'select-dom';
 import PencilIcon from 'octicons-plain-react/Pencil';
@@ -9,25 +8,37 @@ import observe from '../helpers/selector-observer.js';
 import features from '../feature-manager.js';
 import {isArchivedRepoAsync} from '../github-helpers/index.js';
 
-function addQuickEditButton(commentForm: Element): void {
-	const commentBody = commentForm.closest('.js-comment')!;
-	// We can't rely on a class for deduplication because the whole comment might be replaced by GitHub #5572
-	if (elementExists('.rgh-quick-comment-edit-button', commentBody)) {
+function addQuickEditButton(commentDropdown: HTMLDetailsElement): void {
+	const commentBody = commentDropdown.closest('.js-comment')!;
+
+	// TODO: Potentially move to :has selector
+	// The comment is definitely not editable
+	if (!elementExists('.js-comment-update', commentBody)) {
+		console.log('Comment is not editable');
 		return;
 	}
 
-	commentBody
-		.querySelector('.timeline-comment-actions details.position-relative')! // The dropdown
-		.before(
-			<button
-				type="button"
-				role="menuitem"
-				className="timeline-comment-action btn-link js-comment-edit-button rgh-quick-comment-edit-button"
-				aria-label="Edit comment"
-			>
-				<PencilIcon/>
-			</button>,
-		);
+	if (elementExists([
+		// We can't rely on a class for deduplication because the whole comment might be replaced by GitHub #5572
+		'.rgh-quick-comment-edit-button',
+
+		// If .js-pick-reaction is the first child, `reaction-menu` doesn't exist, which means that the conversation is locked
+		'.js-pick-reaction:first-child',
+	], commentBody)) {
+		console.log('Comment is locked');
+		return;
+	}
+
+	commentDropdown.before(
+		<button
+			type="button"
+			role="menuitem"
+			className="timeline-comment-action btn-link js-comment-edit-button rgh-quick-comment-edit-button"
+			aria-label="Edit comment"
+		>
+			<PencilIcon/>
+		</button>,
+	);
 }
 
 export function canEditEveryComment(): boolean {
@@ -52,9 +63,7 @@ async function init(signal: AbortSignal): Promise<void> {
 	// If true then the resulting selector will match all comments, otherwise it will only match those made by you
 	const preSelector = canEditEveryComment() ? '' : '.current-user';
 
-	// Find editable comments first, then traverse to the correct position
-	// TODO: Replace with :has selector
-	observe(preSelector + '.js-comment.unminimized-comment .js-comment-update', addQuickEditButton, {signal});
+	observe(preSelector + '.js-comment.unminimized-comment .timeline-comment-actions details.position-relative', addQuickEditButton, {signal});
 }
 
 void features.add(import.meta.url, {
