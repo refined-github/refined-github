af808794ef29263bb0030d304503bcf1f6d1c63b
Federico <me@fregante.com>
2024-06-02 18:50:49 +0800

    Chevron icon

--- source/features/toggle-files-button.tsx
+++ source/features/toggle-files-button.tsx
@@ -1,48 +1,52 @@
 import './toggle-files-button.css';
 import React from 'dom-chef';
-import {$} from 'select-dom';
+import {$, expectElement} from 'select-dom';
 import delegate from 'delegate-it';
+import {CachedFunction} from 'webext-storage-cache';
 import * as pageDetect from 'github-url-detection';
-import FoldIcon from 'octicons-plain-react/Fold';
-import UnfoldIcon from 'octicons-plain-react/Unfold';
+import ChevronDownIcon from 'octicons-plain-react/ChevronDown';
+import ChevronUpIcon from 'octicons-plain-react/ChevronUp';
 import ArrowUpIcon from 'octicons-plain-react/ArrowUp';
 
 import features from '../feature-manager.js';
 import attachElement from '../helpers/attach-element.js';
 import observe from '../helpers/selector-observer.js';
+import {cacheByRepo} from '../github-helpers/index.js';
 
-const cacheKey = 'files-hidden';
+const wasHidden = new CachedFunction('toggle-files-button', {
+	async updater(): Promise<boolean> {
+		return false;
+	},
+	cacheKey: cacheByRepo,
+});
 const hiddenFilesClass = 'rgh-files-hidden';
 const toggleButtonClass = 'rgh-toggle-files';
 const noticeClass = 'rgh-files-hidden-notice';
 
-// 19px align this icon with the <UnfoldIcon/> above it
-const noticeStyle = {paddingRight: '19px'};
+// 19px align this icon with the <ChevronUpIcon/> above it
+const noticeStyle = {paddingRight: '16px'};
 
-function addButton(filesBox: HTMLElement): void {
-	attachElement(filesBox, {
-		allowMissingAnchor: true,
-		className: toggleButtonClass,
-		append: () => (
-			<button
-				type="button"
-				className="btn-octicon"
-				aria-label="Toggle files section"
-			>
-				<FoldIcon/>
-				<UnfoldIcon/>
-			</button>
-		),
-	});
+function addButton(commitsLink: HTMLElement): void {
+	// It won't work with :has(), too many nested boxes
+	commitsLink.closest('[class^="Box"]')!.append(
+		<button
+			type="button"
+			className={`btn-octicon ${toggleButtonClass}`}
+			aria-label="Toggle files section"
+		>
+			<ChevronDownIcon/>
+			<ChevronUpIcon/>
+		</button>,
+	);
 }
 
-function addFilesHiddenNotice(fileBox: Element): void {
+function addFilesHiddenNotice(): void {
 	// Add notice so the user knows that the list was collapsed #5524
-	attachElement(fileBox, {
+	attachElement(expectElement('[aria-labelledby="folders-and-files"]'), {
 		className: noticeClass,
 		after: () => (
 			<div
-				className="mb-3 mt-n3 py-1 text-right text-small color-fg-subtle"
+				className="py-1 text-right text-small color-fg-subtle"
 				style={noticeStyle}
 			>
 				The file list was collapsed via Refined GitHub <ArrowUpIcon className="v-align-middle"/>
@@ -59,11 +63,24 @@ async function toggleHandler(): Promise<void> {
 	// Remove notice after the first click
 	$(`.${noticeClass}`)?.remove();
 
-	const isHidden = toggle();
+	// eslint-disable-next-line unicorn/prefer-ternary -- No.
+	if (toggle()) {
+		await wasHidden.applyOverride([], true);
+	} else {
+		await wasHidden.delete();
+	}
+}
+
+async function updateView(button: HTMLElement): Promise<void> {
+	addButton(button);
+	if (await wasHidden.getCached()) {
+		toggle(true);
+		addFilesHiddenNotice();
+	}
 }
 
 async function init(signal: AbortSignal): Promise<void> {
-	observe('[class^="Box"]:has([aria-label="Commit history"])', addButton, {signal});
+	observe('[aria-label="Commit history"]', updateView, {signal});
 	delegate(`.${toggleButtonClass}, .${noticeClass}`, 'click', toggleHandler, {signal});
 }
 
