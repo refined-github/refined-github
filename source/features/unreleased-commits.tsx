415ba9addae52411bd7e0b2a47f0a23b2dcf1e53
Yue Yang <g1enyy0ung@gmail.com>
2024-06-02 15:01:37 +0800

    `unreleased-commits` - Avoid internal wrapping (#7376)

--- source/features/unreleased-commits.tsx
+++ source/features/unreleased-commits.tsx
@@ -133,10 +133,14 @@ async function addToHome(branchSelector: HTMLButtonElement): Promise<void> {
 			await createLinkGroup(latestTag, aheadBy) as HTMLElement,
 		);
 	} else {
+		const linkGroup = await createLinkGroup(latestTag, aheadBy) as HTMLElement;
+
+		linkGroup.style.flexShrink = '0';
+
 		wrapAll(
 			<div className="d-flex gap-2"/>,
 			branchSelector,
-			await createLinkGroup(latestTag, aheadBy),
+			linkGroup,
 		);
 	}
 }
