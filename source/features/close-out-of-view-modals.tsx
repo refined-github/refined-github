f619de9f3363456605d03a42058575da8661b40e
Federico Brigante <me@fregante.com>
2024-06-03 13:07:08 +0800

    `close-out-of-view-modals` - Fix conflict with Development menu (#7392)

--- source/features/close-out-of-view-modals.tsx
+++ source/features/close-out-of-view-modals.tsx
@@ -41,6 +41,7 @@ function menuActivatedHandler(event: DelegateEvent): void {
 	const modals = $$([
 		':scope > details-menu', // "Watch repo" dropdown
 		':scope > details-dialog', // "Watch repo" dropdown
+		':scope > modal-dialog', // "Development" dropdown #7093
 		':scope > div > .dropdown-menu', // "Clone or download" and "Repo nav overflow"
 	], details);
 
@@ -56,3 +57,12 @@ function init(): void {
 void features.add(import.meta.url, {
 	init: onetime(init),
 });
+
+/*
+
+Test URLs
+
+- Dropdowns in conversation sidebar: https://github.com/refined-github/sandbox/issues/3
+- Star/Watch dropdowns: https://github.com/refined-github/sandbox
+
+*/
