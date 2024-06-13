5e2f48913c5a79df1de08522a4e47e58821c6c24
Federico <me@fregante.com>
2024-06-13 11:15:40 +0800

    /4

--- source/features/previous-version.tsx
+++ source/features/previous-version.tsx
@@ -33,10 +33,10 @@ async function add(historyButton: HTMLAnchorElement): Promise<void> {
 
 	const previousButton = historyButton.cloneNode(true);
 	previousButton.href = url.href;
+	$('span[data-component="text"]', previousButton).textContent = 'Previous';
 	$('span[data-component="leadingVisual"] svg', previousButton).replaceWith(
 		<VersionsIcon className="UnderlineNav-octicon mr-0"/>,
 	);
-	$('span[data-component="text"]', previousButton).textContent = 'Previous';
 	historyButton.before(previousButton);
 }
 
