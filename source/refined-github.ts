b24ff84219b4cf3f838fbd4de2520d29750b0d19
Federico Brigante <me@fregante.com>
2024-06-19 01:01:41 +0800

    `linkify-line-numbers` - New feature (#7426)

--- source/refined-github.ts
+++ source/refined-github.ts
@@ -223,3 +223,4 @@ import './features/clean-readme-url.js';
 import './features/pr-notification-link.js';
 import './features/click-outside-modal.js';
 import './features/comment-excess.js';
+import './features/linkify-line-numbers.js';
