a400b058ba7dd71dae13c5fd821dc6c60d0a5ee0
Federico <me@fregante.com>
2024-06-19 14:26:25 +0800

    Open options in a new tab

--- source/background.ts
+++ source/background.ts
@@ -1,8 +1,8 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import {type Runtime} from 'webextension-polyfill';
+import {Menus, type Runtime} from 'webextension-polyfill';
 import 'webext-dynamic-content-scripts';
 import {globalCache} from 'webext-storage-cache'; // Also needed to regularly clear the cache
-import {isSafari} from 'webext-detect-page';
+import {isSafari, isFirefox} from 'webext-detect-page';
 import {objectKeys} from 'ts-extras';
 import addPermissionToggle from 'webext-permission-toggle';
 import webextAlert from 'webext-alert';
@@ -69,7 +69,7 @@ browser.browserAction.onClicked.addListener(async tab => {
 		return;
 	}
 
-	void browser.tabs.create({
+	await browser.tabs.create({
 		openerTabId: tab.id,
 		url: actionUrl,
 	});
@@ -127,3 +127,20 @@ browser.permissions.onAdded.addListener(async permissions => {
 		await webextAlert('Refined GitHub is not meant to run on every website. If you’re looking to enable it on GitHub Enterprise, follow the instructions in the Options page.');
 	}
 });
+
+const FIREFOX_OPTIONS_MENU_ID = 'FIREFOX_OPTIONS';
+
+function onContextMenuClick({menuItemId}: Menus.OnClickData): void {
+	if (menuItemId === FIREFOX_OPTIONS_MENU_ID) {
+		void browser.runtime.openOptionsPage();
+	}
+}
+
+if (isFirefox()) {
+	browser.contextMenus.onClicked.addListener(onContextMenuClick);
+	browser.contextMenus.create({
+		id: FIREFOX_OPTIONS_MENU_ID,
+		title: 'Options',
+		contexts: ['browser_action'],
+	});
+}
