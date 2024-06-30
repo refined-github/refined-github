df62990c5b2357de8f4900d3622be9f3be20913e
Kid <44045911+kidonng@users.noreply.github.com>
2024-06-30 13:43:54 +0800

    `list-prs-for-file` - Style improvements (#7507)

--- source/features/list-prs-for-file.tsx
+++ source/features/list-prs-for-file.tsx
@@ -26,31 +26,35 @@ function getHovercardUrl(prNumber: number): string {
 function getDropdown(prs: number[]): HTMLElement {
 	const isEditing = pageDetect.isEditingFile();
 	const icon = isEditing
-		? <AlertIcon className="v-align-middle color-fg-attention"/>
-		: <GitPullRequestIcon className="v-align-middle"/>;
-	// Markup copied from https://primer.style/css/components/dropdown
+		? <AlertIcon className="color-fg-attention"/>
+		: <GitPullRequestIcon/>;
+
 	// TODO: use Popover API when hovercards become compatible #7496
 	return (
-		<details className="dropdown details-reset details-overlay flex-self-center rgh-list-prs-for-file">
-			<summary className="btn btn-sm">
+		<details className="dropdown">
+			<summary className="Button Button--secondary color-fg-muted">
 				{icon}
-				<span className="v-align-middle"> {prs.length} </span>
+				<span className="color-fg-default mx-1">{prs.length}</span>
 				<div className="dropdown-caret"/>
 			</summary>
 
-			<details-menu className="dropdown-menu dropdown-menu-sw" style={{width: '13em'}}>
-				<div className="dropdown-header">
+			<details-menu className="dropdown-menu dropdown-menu-sw" style={{width: '180px'}}>
+				<div className="px-3 pt-2 h6 color-fg-muted">
 					File also being edited in
 				</div>
-				{prs.map(prNumber => (
-					<a
-						className="dropdown-item"
-						href={getPRUrl(prNumber)}
-						data-hovercard-url={getHovercardUrl(prNumber)}
-					>
-						#{prNumber}
-					</a>
-				))}
+				<ul className="ActionListWrap ActionListWrap--inset">
+					{prs.map(prNumber => (
+						<li className="ActionListItem">
+							<a
+								className="ActionListContent"
+								href={getPRUrl(prNumber)}
+								data-hovercard-url={getHovercardUrl(prNumber)}
+							>
+								#{prNumber}
+							</a>
+						</li>
+					))}
+				</ul>
 			</details-menu>
 		</details>
 	);
