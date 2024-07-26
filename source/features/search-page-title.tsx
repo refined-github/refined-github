2df794f5e475ac5d60515ff719df14174c4154db
pacexy <pacexy@gmail.com>
2024-07-26 19:34:19 +0800

    handle soft nav

--- source/features/search-page-title.tsx
+++ source/features/search-page-title.tsx
@@ -2,15 +2,25 @@ import * as pageDetect from 'github-url-detection';
 
 import features from '../feature-manager.js';
 
-
-function initForGlobalSearchResults(): void {
+function updateTitle(e?: Event): void {
 	const query = new URLSearchParams(location.search).get('q')
-	if (query) {
+	if (!query) return
+
+	const title = query.trim() + ` - ${document.title}`;
+
+	if (e) {
+		document.title = title;
+	} else {
 		// This is done on load because the title is updated by GitHub after the page is loaded
 		window.addEventListener('load', () => {
-			document.title = query.trim() + ` - ${document.title}`;
+			document.title = title;
 		})
 	}
+}
+
+function init(): void {
+	updateTitle()
+	document.addEventListener('soft-nav:end', updateTitle)
 }
 
 void features.add(import.meta.url, {
@@ -19,6 +29,5 @@ void features.add(import.meta.url, {
 		pageDetect.isGlobalSearchResults,
 		pageDetect.isIssueOrPRList
 	],
-	init: initForGlobalSearchResults
+	init
 });
-
