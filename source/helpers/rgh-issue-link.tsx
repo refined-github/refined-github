0cf6e280737bdb91a9769f6fd2f828e492ab3839
Federico <me@fregante.com>
2024-08-03 23:34:14 +0700

    Lint

--- source/helpers/rgh-issue-link.tsx
+++ source/helpers/rgh-issue-link.tsx
@@ -1,6 +1,6 @@
 import React from 'dom-chef';
 
-export function createRghIssueLink(issueNumber: number | string): Element {
+export default function createRghIssueLink(issueNumber: number | string): Element {
 	const issueUrl = getRghIssueUrl(issueNumber);
 	return (
 		<a target="_blank" rel="noopener noreferrer" data-hovercard-type="issue" data-hovercard-url={issueUrl + '/hovercard'} href={issueUrl}>
