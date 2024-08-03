872c3020dab6f96f0e2636a638b1814b448eb694
Federico <me@fregante.com>
2024-08-04 00:13:01 +0700

    Validate renames

--- source/options-storage.ts
+++ source/options-storage.ts
@@ -1,5 +1,6 @@
 import OptionsSyncPerDomain from 'webext-options-sync-per-domain';
 
+import renamedFeatures from './feature-renames.json';
 import {importedFeatures} from '../readme.md';
 
 export type RGHOptions = typeof defaults;
@@ -11,46 +12,6 @@ const defaults = Object.assign({
 	logging: false,
 	logHTTP: false,
 }, Object.fromEntries(importedFeatures.map(id => [`feature:${id}`, true])));
-
-const renamedFeatures = new Map<string, string>([
-	['separate-draft-pr-button', 'one-click-pr-or-gist'],
-	['prevent-pr-commit-link-loss', 'prevent-link-loss'],
-	['remove-projects-tab', 'remove-unused-repo-tabs'],
-	['remove-unused-repo-tabs', 'clean-repo-tabs'],
-	['more-dropdown', 'clean-repo-tabs'],
-	['remove-diff-signs', 'hide-diff-signs'],
-	['remove-label-faster', 'quick-label-hiding'],
-	['edit-files-faster', 'quick-file-edit'],
-	['edit-comments-faster', 'quick-comment-edit'],
-	['delete-review-comments-faster', 'quick-review-comment-deletion'],
-	['hide-comments-faster', 'quick-comment-hiding'],
-	['faster-reviews', 'quick-review'],
-	['faster-pr-diff-options', 'quick-pr-diff-options'],
-	['hide-useless-comments', 'hide-low-quality-comments'],
-	['hide-useless-newsfeed-events', 'hide-newsfeed-noise'],
-	['hide-noisy-newsfeed-events', 'hide-newsfeed-noise'],
-	['no-useless-split-diff-view', 'no-unnecessary-split-diff-view'],
-	['unwrap-useless-dropdowns', 'unwrap-unnecessary-dropdowns'],
-	['tag-changelog-link', 'tag-changes-link'],
-	['navigate-pages-with-arrow-keys', 'pagination-hotkey'],
-	['list-pr-for-branch', 'list-prs-for-branch'],
-	['quick-label-hiding', 'quick-label-removal'],
-	['next-scheduled-github-action', 'github-actions-indicators'],
-	['raw-file-link', 'more-file-links'],
-	['conversation-filters', 'more-conversation-filters'],
-	['quick-pr-diff-options', 'one-click-diff-options'],
-	['quick-review-buttons', 'one-click-review-submission'],
-	['pull-request-hotkey', 'pull-request-hotkeys'],
-	['first-published-tag-for-merged-pr', 'closing-remarks'],
-	['scheduled-and-manual-workflow-indicators', 'github-actions-indicators'],
-	['useful-forks', 'fork-notice'],
-	['set-default-repositories-type-to-sources', 'hide-user-forks'],
-	['highlight-deleted-and-added-files-in-diffs', 'new-or-deleted-file'],
-	['enable-file-links-in-compare-view', 'actionable-pr-view-file'],
-	['use-first-commit-message-for-new-prs', 'pr-first-commit-title`'],
-	['comment-on-draft-pr-indicator', 'netiquette'],
-	['draft-pr-notice', 'netiquette'],
-]);
 
 export function isFeatureDisabled(options: RGHOptions, id: string): boolean {
 	// Must check if it's specifically `false`: It could be undefined if not yet in the readme or if misread from the entry point #6606
