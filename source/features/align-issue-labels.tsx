59c508f1ee4b21826f9630201accec2d3b1e3bbc
Federico <me@fregante.com>
2024-08-17 19:16:34 +0700

    Parte tres

--- source/features/align-issue-labels.tsx
+++ source/features/align-issue-labels.tsx
@@ -4,3 +4,14 @@ import * as pageDetect from 'github-url-detection';
 import features from '../feature-manager.js';
 
 void features.addCssFeature(import.meta.url, [pageDetect.isIssueOrPRList]);
+
+/*
+
+# Test URLs
+
+https://github.com/pulls
+https://github.com/bmish/eslint-doc-generator/pulls
+https://github.com/bmish/eslint-doc-generator/issues
+https://github.com/bmish/eslint-doc-generator/milestone/1
+
+*/
