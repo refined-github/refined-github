dd3f2f7a7c0b8ee428429fdf3e08ebeb56f3e287
Federico Brigante <me@fregante.com>
2024-08-19 01:14:07 +0700

    Discard changes to build/readme-parser.ts

--- build/readme-parser.ts
+++ build/readme-parser.ts
@@ -1,6 +1,9 @@
+/// <reference types="../source/globals.js" />
+
 import regexJoin from 'regex-join';
 import {readFileSync} from 'node:fs';
 import parseMarkdown from 'snarkdown';
+
 // Group names must be unique because they will be merged
 const simpleFeatureRegex = /^- \[]\(# "(?<simpleId>[^"]+)"\)(?: 🔥)? (?<simpleDescription>.+)$/gm;
 const highlightedFeatureRegex = /<p><a title="(?<highlightedId>[^"]+)"><\/a> (?<highlightedDescripion>.+?)\n\t+<p><img src="(?<highlightedImage>.+?)">/g;
@@ -8,41 +11,48 @@ const featureRegex = regexJoin(simpleFeatureRegex, /|/, highlightedFeatureRegex)
 const imageRegex = /\.\w{3}$/; // 3 since .png and .gif have 3 letters
 const rghUploadsRegex = /refined-github[/]refined-github[/]assets[/]/;
 const screenshotRegex = regexJoin(imageRegex, /|/, rghUploadsRegex);
-function extractDataFromMatch(match) {
-	const {simpleId, simpleDescription, highlightedId, highlightedDescripion, highlightedImage} = match.groups;
+
+function extractDataFromMatch(match: RegExpMatchArray): FeatureMeta {
+	const {
+		simpleId,
+		simpleDescription,
+		highlightedId,
+		highlightedDescripion,
+		highlightedImage,
+	} = match.groups!;
 	if (highlightedId) {
 		return {
-			id: highlightedId,
+			id: highlightedId as FeatureID,
 			description: parseMarkdown(highlightedDescripion + '.'),
 			screenshot: highlightedImage,
 		};
 	}
 
-	const urls = [];
-	function urlExtracter(_match, title, url) {
+	const urls: string[] = [];
+	function urlExtracter(_match: string, title: string, url: string): string {
 		urls.push(url);
 		return title;
 	}
 
 	const linkLessMarkdownDescription = simpleDescription.replaceAll(/\[(.+?)]\((.+?)\)/g, urlExtracter);
 	return {
-		id: simpleId,
+		id: simpleId as FeatureID,
 		description: parseMarkdown(linkLessMarkdownDescription),
 		// `null` makes the keys visible in the JSON file
 		screenshot: urls.find(url => screenshotRegex.test(url)) ?? null,
 	};
 }
 
-export function getFeaturesMeta() {
+export function getFeaturesMeta(): FeatureMeta[] {
 	const readmeContent = readFileSync('readme.md', 'utf8');
 	return [...readmeContent.matchAll(featureRegex)]
 		.map(match => extractDataFromMatch(match))
 		.sort((firstFeature, secondFeature) => firstFeature.id.localeCompare(secondFeature.id));
 }
 
-export function getImportedFeatures() {
+export function getImportedFeatures(): FeatureID[] {
 	const contents = readFileSync('source/refined-github.ts', 'utf8');
 	return [...contents.matchAll(/^import '\.\/features\/([^.]+)\.js';/gm)]
-		.map(match => match[1])
+		.map(match => match[1] as FeatureID)
 		.sort();
 }
