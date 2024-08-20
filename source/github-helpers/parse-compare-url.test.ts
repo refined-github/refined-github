f485ff91ccf1e75f7526d74481410938ea05d5fa
Federico <me@fregante.com>
2024-08-21 00:29:21 +0700

    Add tests

---
+++ source/github-helpers/parse-compare-url.test.ts
@@ -0,0 +1,21 @@
+import {expect, test} from 'vitest';
+
+import parseCompareUrl from './parse-compare-url.js';
+
+test('parseCompareUrl', () => {
+	expect(parseCompareUrl('/john/foo/compare/main...patty/patch-1')).toMatchObject({
+		base: {
+			branch: 'main',
+			repo: {
+				nameWithOwner: 'john/foo',
+			},
+		},
+		head: {
+			branch: 'patch-1',
+			foo: {
+				nameWithOwner: 'patty/foo',
+			},
+		},
+		isCrossRepo: false,
+	});
+});
