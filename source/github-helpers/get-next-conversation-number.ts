178ebe6f0f279d87ae355defb52b07f46ad4e29e
Karl Horky <karl.horky@gmail.com>
2024-08-22 18:13:28 +0200

    Add back increment

--- source/github-helpers/get-next-conversation-number.ts
+++ source/github-helpers/get-next-conversation-number.ts
@@ -2,5 +2,5 @@ import api from './api.js';
 
 export async function getNextConversationNumber(): Promise<number> {
 	const issues = await api.v3('issues?per_page=1');
-	return issues[0].number;
+	return (issues[0].number as number) + 1;
 }
