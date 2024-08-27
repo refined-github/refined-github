89c467bb91736b7442064f6547ded5cd0e414f48
Federico <me@fregante.com>
2024-08-27 14:34:55 +0700

    Right file

--- source/features/emphasize-draft-pr-label.tsx
+++ source/features/emphasize-draft-pr-label.tsx
@@ -3,3 +3,12 @@ import './emphasize-draft-pr-label.css';
 import features from '../feature-manager.js';
 
 void features.addCssFeature(import.meta.url);
+
+/*
+
+Test URLs:
+
+- https://github.com/refined-github/refined-github/pulls
+- https://github.com/pulls
+
+*/
