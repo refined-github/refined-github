f9ff3e2b0c015f79026ab214715c6e998f81ccb6
Federico <me@fregante.com>
2024-09-05 16:24:26 +0700

    `pr-branch-auto-delete` - Skip branches that have PRs pointing to them

--- source/features/pr-branch-auto-delete.tsx
+++ source/features/pr-branch-auto-delete.tsx
@@ -4,6 +4,7 @@ import InfoIcon from 'octicons-plain-react/Info';
 import elementReady from 'element-ready';
 import * as pageDetect from 'github-url-detection';
 
+import api from '../github-helpers/api.js';
 import features from '../feature-manager.js';
 import onPrMerge from '../github-events/on-pr-merge.js';
 import featureLink from '../helpers/feature-link.js';
@@ -11,6 +12,7 @@ import attachElement from '../helpers/attach-element.js';
 import {canEditEveryComment} from './quick-comment-edit.js';
 import {getBranches} from '../github-helpers/pr-branches.js';
 import matchesAnyPattern from '../helpers/matches-any-patterns.js';
+import GetPrsToBaseBranch from './pr-branch-auto-delete.gql';
 
 // TODO: Not an exact match; Moderators can edit comments but not create releases
 const canCreateRelease = canEditEveryComment;
@@ -36,8 +38,20 @@ async function init(): Promise<void> {
 		return;
 	}
 
+	// Skip branches that are likely to be long-lived https://github.com/refined-github/refined-github/issues/7755
 	const {head} = getBranches();
 	if (matchesAnyPattern(head.name, exceptions)) {
+		return;
+	}
+
+	// Skip branches that have PRs open https://github.com/refined-github/refined-github/issues/7782
+	const {repository} = await api.v4(GetPrsToBaseBranch, {
+		variables: {
+			baseRefName: head.branch,
+		},
+	});
+
+	if (repository.pullRequests.totalCount) {
 		return;
 	}
 
