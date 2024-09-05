7fbe87d57c0d1b5dde7e6f376c8d02ff739c23c5
Federico <me@fregante.com>
2024-09-05 16:12:23 +0700

    `pr-branch-auto-delete` - Add exceptions

--- source/features/pr-branch-auto-delete.tsx
+++ source/features/pr-branch-auto-delete.tsx
@@ -9,13 +9,35 @@ import onPrMerge from '../github-events/on-pr-merge.js';
 import featureLink from '../helpers/feature-link.js';
 import attachElement from '../helpers/attach-element.js';
 import {canEditEveryComment} from './quick-comment-edit.js';
+import { getBranches } from '../github-helpers/pr-branches.js';
+import matchesAnyPattern from '../helpers/matches-any-patterns.js';
 
 // TODO: Not an exact match; Moderators can edit comments but not create releases
 const canCreateRelease = canEditEveryComment;
 
+const exceptions = [
+	'dev',
+	'develop',
+	'development',
+	'main',
+	'master',
+	'next',
+	'pre',
+	'prod',
+	'production',
+	'stage',
+	/^release\//,
+	/^v\d/,
+];
+
 async function init(): Promise<void> {
 	const deleteButton = $('[action$="/cleanup"] [type="submit"]');
 	if (!deleteButton) {
+		return;
+	}
+
+	const {head} = getBranches();
+	if (matchesAnyPattern(head.name, exceptions)) {
 		return;
 	}
 
