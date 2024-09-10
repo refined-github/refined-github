6be38bc22021644152fd98d5e6823c7e70ce0d25
oSumAtrIX <johan.melkonyan1@web.de>
2024-09-10 17:35:28 +0200

    Use better assertion APIs

--- source/helpers/render-conventional-commit-types.test.ts
+++ source/helpers/render-conventional-commit-types.test.ts
@@ -3,12 +3,12 @@ import {test, assert} from 'vitest';
 import {getConventionalCommitAndScope} from './render-conventional-commit-types.js';
 
 test('getConventionalCommitAndScope', () => {
-	assert.equal(getConventionalCommitAndScope('Commit message'), undefined);
+	assert.isUndefined(getConventionalCommitAndScope('Commit message'));
 	assert.deepEqual(getConventionalCommitAndScope('fix: Commit message'), {type: 'fix', scope: undefined});
 	assert.deepEqual(getConventionalCommitAndScope('feat: Commit message'), {type: 'feat', scope: undefined});
 	assert.deepEqual(getConventionalCommitAndScope('feat(scope): Commit message'), {type: 'feat', scope: 'scope'});
 	assert.deepEqual(getConventionalCommitAndScope('feat(sco pe): Commit message'), {type: 'feat', scope: 'sco pe'});
-	assert.equal(getConventionalCommitAndScope('feat(): Commit message'), undefined);
+	assert.isUndefined(getConventionalCommitAndScope('feat(): Commit message'));
 	assert.deepEqual(getConventionalCommitAndScope('feat: Commit (message)'), {type: 'feat', scope: undefined});
-	assert.equal(getConventionalCommitAndScope('fe at(scope): Commit message) '), undefined);
+	assert.isUndefined(getConventionalCommitAndScope('fe at(scope): Commit message) '));
 });
