07a16682470a82529744134453d9529ef835138e
oSumAtrIX <johan.melkonyan1@web.de>
2024-09-16 17:26:18 +0200

    Fix tests

--- source/helpers/conventional-commits.test.ts
+++ source/helpers/conventional-commits.test.ts
@@ -2,17 +2,12 @@ import {test, assert} from 'vitest';
 
 import {parseConventionalCommit} from './conventional-commits.js';
 
-function assertRegexDeepStrictEqual(input: string, expected: [string, string | undefined]) {
-	const result = parseConventionalCommit(input);
-	assert.deepStrictEqual(result, [input, ...expected]);
-}
-
 test('parseConventionalCommit', () => {
-	assertRegexDeepStrictEqual('fix: Commit message', ['fix', undefined]);
-	assertRegexDeepStrictEqual('feat: Commit message', ['feat', undefined]);
-	assertRegexDeepStrictEqual('feat(scope): Commit message', ['feat', 'scope']);
-	assertRegexDeepStrictEqual('feat(sco pe): Commit message', ['feat', 'sco pe']);
-	assertRegexDeepStrictEqual(('feat: Commit (message)'), ['feat', undefined]);
+	assert.deepStrictEqual(parseConventionalCommit('fix: Commit message'), ['fix: ', 'fix', undefined]);
+	assert.deepStrictEqual(parseConventionalCommit('feat: Commit message'), ['feat: ', 'feat', undefined]);
+	assert.deepStrictEqual(parseConventionalCommit('feat(scope): Commit message'), ['feat(scope): ', 'feat', 'scope']);
+	assert.deepStrictEqual(parseConventionalCommit('feat(sco pe): Commit message'), ['feat(sco pe): ', 'feat', 'sco pe']);
+	assert.deepStrictEqual(parseConventionalCommit(('feat: Commit (message)')), ['feat: ', 'feat', undefined]);
 
 	assert.isUndefined(parseConventionalCommit('feat:'));
 	assert.isUndefined(parseConventionalCommit('Commit message'));
