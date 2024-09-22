2fc5f9ae64a66126e7b4efde7b85438dad52fb72
Federico <me@fregante.com>
2024-09-22 14:30:14 +0700

    Trying out rsbuild

---
+++ rsbuild.config.ts
@@ -0,0 +1,23 @@
+import {defineConfig} from '@rsbuild/core';
+
+export default defineConfig({
+	source: {
+		entry: {
+			'options': './source/options.tsx',
+			'background': './source/background.ts',
+			'refined-github': './source/refined-github.ts',
+			'content-script': './source/content-script.ts',
+			'resolve-conflicts': './source/resolve-conflicts.ts',
+		},
+	},
+	output: {
+		distPath: {
+			root: 'distribution/assets',
+		},
+	},
+	performance: {
+		chunkSplit: {
+			strategy: 'split-by-module',
+		},
+	},
+});
