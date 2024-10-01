3a2bf9c7f39e826be1b6e0a0ae4b57453a3c6502
Federico <me@fregante.com>
2024-10-01 16:05:14 +0700

    Use API to determine locked issue

--- source/features/locked-issue.tsx
+++ source/features/locked-issue.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import LockIcon from 'octicons-plain-react/Lock';
 import * as pageDetect from 'github-url-detection';
-import elementReady from 'element-ready';
 
 import features from '../feature-manager.js';
 import observe from '../helpers/selector-observer.js';
+import {hasToken} from '../options-storage.js';
+import api from '../github-helpers/api.js';
+import GetIssueLockStatus from './locked-issue.gql';
+import {getConversationNumber} from '../github-helpers/index.js';
+
+async function isConversationLocked(): Promise<boolean | undefined> {
+	if (!hasToken()) {
+		return undefined;
+	}
+
+	const {repository} = await api.v4uncached(GetIssueLockStatus, {
+		variables: {
+			number: getConversationNumber()!,
+		},
+	});
+
+	return repository.issueOrPullRequest.locked;
+}
 
 function LockedIndicator(): JSX.Element {
 	return (
@@ -16,19 +33,18 @@ function LockedIndicator(): JSX.Element {
 }
 
 function addLock(element: HTMLElement): void {
-	const classes = (element.closest('.gh-header-sticky') ? 'mr-2 ' : '') + 'mb-2 rgh-locked-issue';
+	const classes = (
+		element.closest('.gh-header-sticky')
+			? 'mr-2 '
+			: ''
+	)
+	+ 'mb-2 rgh-locked-issue';
 	element.after(
 		<LockedIndicator className={classes} />,
 	);
 }
 
 async function init(signal: AbortSignal): Promise<void | false> {
-	// If reactions-menu exists, then .js-pick-reaction is the second child
-	const reactions = await elementReady('.js-pick-reaction');
-	if (!reactions?.matches(':first-child')) {
-		return false;
-	}
-
 	observe([
 		'.gh-header-meta > :first-child', // Issue title
 		'.gh-header-sticky .flex-row > :first-child', // Sticky issue title
@@ -37,16 +53,8 @@ async function init(signal: AbortSignal): Promise<void | false> {
 
 void features.add(import.meta.url, {
 	asLongAs: [
-		// Logged out users never see the reactions menu used to determine lock status. This would lead to false positives.
-		pageDetect.isLoggedIn,
-	],
-	include: [
 		pageDetect.isConversation,
-	],
-	exclude: [
-		// TODO: Find alternative detection that works even for GHE that don't have reactions enabled
-		// https://github.com/refined-github/refined-github/issues/7063
-		pageDetect.isEnterprise,
+		async () => Boolean(await isConversationLocked()),
 	],
 	init,
 });
