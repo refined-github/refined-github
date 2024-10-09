2802e208fcbf786718b405235487646c570d5793
kovsu <2583695112@qq.com>
2024-10-09 09:20:11 +0800

    update

--- source/features/download-folder-button.tsx
+++ source/features/download-folder-button.tsx
@@ -1,3 +1,4 @@
+import './download-folder-button.css';
 import * as pageDetect from 'github-url-detection';
 import {$} from 'select-dom';
 
@@ -5,29 +6,20 @@ import features from '../feature-manager.js';
 import observe from '../helpers/selector-observer.js';
 
 function add(container: HTMLElement): void {
-	let deleteDirectoryItem;
-	const deleteButton = $('a[aria-keyshortcuts="d"]', container);
-
-	if (deleteButton) {
-		// Selector points to "Delete directory" button
-		deleteDirectoryItem = deleteButton.parentElement;
-	} else {
-		deleteDirectoryItem = $('[aria-keyshortcuts="c"]:first-child', container);
-	}
+	const deleteDirectoryItem = $('[aria-keyshortcuts="c"]:first-child', container);
 
 	if (!deleteDirectoryItem)
 		return;
 
 	const item = deleteDirectoryItem!.cloneNode(true);
-	let link = item.firstElementChild as HTMLAnchorElement;
-	if (link.tagName !== 'A') {
-		item.replaceChildren(document.createElement('a'));
-		link = item.firstElementChild as HTMLAnchorElement;
-	}
+	item.replaceChildren(document.createElement('a'));
+
+	const link = item.firstElementChild as HTMLAnchorElement;
 	const downloadUrl = new URL('https://download-directory.github.io/');
 	downloadUrl.searchParams.set('url', location.href);
 	link.href = downloadUrl.href;
 	link.textContent = 'Download directory';
+	link.classList.add('download-button');
 	link.removeAttribute('id');
 	link.removeAttribute('aria-keyshortcuts');
 	link.removeAttribute('aria-labelledby');
