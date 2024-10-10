786992443fa8b809d9fd8ad62fd83e2f7de94937
kovsu <2583695112@qq.com>
2024-10-10 10:10:11 +0800

    update

--- source/features/more-conversation-filters.tsx
+++ source/features/more-conversation-filters.tsx
@@ -1,13 +1,15 @@
 import {$} from 'select-dom';
 import * as pageDetect from 'github-url-detection';
 import {stringToBase64} from 'uint8array-extras';
-import elementReady from 'element-ready';
 
 import features from '../feature-manager.js';
 import SearchQuery from '../github-helpers/search-query.js';
 
 function init(): void {
-	const sourceItem = $('#filters-select-menu a:nth-last-child(2)')!;
+	const sourceItem = $('#filters-select-menu a:nth-last-child(2)');
+
+	if (!sourceItem)
+		return;
 
 	// "Involved" filter
 	const commentsLink = sourceItem.cloneNode(true);
@@ -33,16 +35,7 @@ function init(): void {
 	commentsLink.after(subscriptionsLink);
 }
 
-// The page below doesn't have the filters menu
-// https://github.com/tc39/proposal-await.ops/issues?q=sort%3Aupdated-desc+is%3Aissue+is%3Aopen
-async function hasFilters(): Promise<boolean> {
-	return Boolean(await elementReady('#filters-select-menu', {waitForChildren: false}));
-}
-
 void features.add(import.meta.url, {
-	asLongAs: [
-		hasFilters,
-	],
 	include: [
 		pageDetect.isRepoIssueOrPRList,
 	],
