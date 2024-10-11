d2103942205e97835ac51ca66dbc0e42334078f3
Federico <me@fregante.com>
2024-10-12 02:26:47 +0700

    Update rgh-options-link.tsx

--- source/features/rgh-options-link.tsx
+++ source/features/rgh-options-link.tsx
@@ -4,24 +4,24 @@ import delegate from 'delegate-it';
 import features from '../feature-manager.js';
 import openOptions from '../helpers/open-options.js';
 import observe from '../helpers/selector-observer.js';
+import {isRefinedGitHubRepo} from '../github-helpers/index.js';
 
-const issueUrl = 'https://github.com/refined-github/refined-github/wiki';
 const placeholdersSelector = 'a[name="user-content-options-page-link"]';
 
 function linkify(anchor: HTMLAnchorElement): void {
+	// This makes the anchor visible and clickable
 	anchor.href = '#refined-github-options';
 }
 
 function init(signal: AbortSignal): void {
-	delegate(placeholdersSelector, 'click', openOptions, {signal});
 	observe(placeholdersSelector, linkify, {signal});
+	delegate(placeholdersSelector, 'click', openOptions, {signal});
 }
 
 void features.add(import.meta.url, {
 	include: [
-		() => location.href.startsWith(issueUrl),
+		isRefinedGitHubRepo,
 	],
-	awaitDomReady: true, // Small page
 	init,
 });
 
