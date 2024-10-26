aa2b80fc5a82e3e888df2ebc5f48b5b7a43f1260
Federico <me@fregante.com>
2024-10-26 15:01:09 +0700

    Update comment

--- source/features/linkify-code.tsx
+++ source/features/linkify-code.tsx
@@ -70,7 +70,7 @@ void features.add(import.meta.url, {
 - Discussions: https://github.com/File-New-Project/EarTrumpet/discussions/877
 - Code Search: https://github.com/search?q=repo%3AKatsuteDev%2FBackground+marketplace&type=code
 - Code Search: https://github.com/search?q=%2F%23%5Cd%7B4%2C%7D%2F+language%3Atypescript&type=code
-- Comment: https://github.com/sindresorhus/linkify-urls/pull/40#pullrequestreview-1593302757
+- Clipped URL that shouldn't be linkified: https://github.com/sindresorhus/linkify-urls/pull/40#pullrequestreview-1593302757
 
 Live tests fields:
 
