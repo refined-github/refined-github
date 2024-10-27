adddce1bf3f19887747228e02b7e162f51117e24
Federico <me@fregante.com>
2024-10-27 13:01:20 +0700

    Fix types

--- source/features/command-palette-navigation-shortcuts.tsx
+++ source/features/command-palette-navigation-shortcuts.tsx
@@ -1,8 +1,8 @@
-import onetime from 'onetime';
 import delegate, {DelegateEvent} from 'delegate-it';
 
 import {isMac} from '../github-helpers/index.js';
 import features from '../feature-manager.js';
+import onetime from '../helpers/onetime.js';
 
 function commandPaletteKeydown(event: DelegateEvent<KeyboardEvent>): void {
 	const {key, ctrlKey, delegateTarget} = event;
--- source/helpers/onetime.ts
+++ source/helpers/onetime.ts
@@ -1,11 +1,12 @@
-export default function onetime<Fun extends (...arguments_: unknown[]) => unknown>(function_: Fun): Fun {
-	let ran = false;
-	return function (this: unknown, ...arguments_: unknown[]): unknown {
-		if (ran) {
-			return;
+const notRun = Symbol('false');
+export default function onetime<ArgumentsType extends unknown[], ReturnType>(function_: (...arguments_: ArgumentsType) => ReturnType): (...arguments_: ArgumentsType) => ReturnType {
+	let returnValue: ReturnType | typeof notRun = notRun;
+	return function (this: unknown, ...arguments_: ArgumentsType): ReturnType {
+		if (returnValue !== notRun) {
+			return returnValue;
 		}
 
-		ran = true;
-		return Reflect.apply(function_, this, arguments_);
-	} as Fun;
+		returnValue = Reflect.apply(function_, this, arguments_);
+		return returnValue;
+	};
 }
