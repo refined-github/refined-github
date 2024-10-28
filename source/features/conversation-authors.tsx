8d2d2543d01e84d090399dc23d343a2da8b619bd
kovsu <2583695112@qq.com>
2024-10-28 09:35:08 +0800

    `highlight-collaborators-and-own-conversations` - Update selector

--- source/features/conversation-authors.tsx
+++ source/features/conversation-authors.tsx
@@ -22,7 +22,10 @@ const collaborators = new CachedFunction('repo-collaborators', {
 
 async function highlightCollaborators(signal: AbortSignal): Promise<void> {
 	const list = await collaborators.get();
-	observe('.js-issue-row [data-hovercard-type="user"]', author => {
+	observe([
+		'.js-issue-row [data-hovercard-type="user"]',
+		'[data-testid="list-row-repo-name-and-number"] a[class^="issue-item-module__authorCreatedLink"]',
+	], author => {
 		if (list.includes(author.textContent.trim())) {
 			author.classList.add('rgh-collaborator');
 		}
