000ec3acf63f12d6e86ebfbe4ca741c8b7712a67
Federico <me@fregante.com>
2024-11-05 14:45:53 +0700

    Avoid regression

--- source/features/conversation-activity-filter.tsx
+++ source/features/conversation-activity-filter.tsx
@@ -141,12 +141,13 @@ async function addWidget(state: State, anchor: HTMLElement): Promise<void> {
 		return;
 	}
 
+	// TODO: Use `<anchored-position>` instead
 	// Try to place the dropdown to the left https://github.com/refined-github/refined-github/issues/5450#issuecomment-1068284635
+	await delay(100); // Let `clean-conversation-headers` run first
 	const availableSpaceToTheLeftOfTheDropdown
 		= position.lastElementChild!.getBoundingClientRect().right
 		- position.parentElement!.getBoundingClientRect().left;
 
-	// It may be zero on the sticky header, but `clean-conversation-headers` doesn't apply there
 	const alignment
 		= availableSpaceToTheLeftOfTheDropdown === 0
 		|| (availableSpaceToTheLeftOfTheDropdown > expectedDropdownWidth)
