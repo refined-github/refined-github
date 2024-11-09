20007b224cc7b05f6e2eab8765eba1067ddad7b4
Federico <me@fregante.com>
2024-11-09 14:07:32 +0700

    `default-branch-button` - Fix Firefox rounded corners bug

--- source/features/default-branch-button.tsx
+++ source/features/default-branch-button.tsx
@@ -34,7 +34,9 @@ async function updateUrl(event: React.MouseEvent<HTMLAnchorElement>): Promise<vo
 }
 
 function wrapButtons(buttons: HTMLElement[]): void {
-	groupButtons(buttons, 'd-flex', 'rgh-default-branch-button-group');
+	// `rounded-left-0` is for Firefox
+	// https://github.com/refined-github/refined-github/pull/8030
+	groupButtons(buttons, 'd-flex', 'rgh-default-branch-button-group', 'rounded-left-0');
 }
 
 async function add(branchSelector: HTMLElement): Promise<void> {
