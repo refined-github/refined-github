ea35e4f04bedada940b3997c72de97de467f3ef6
kovsu <2583695112@qq.com>
2024-11-18 09:29:48 +0800

    `extend-diff-expander` - Support new commit view

--- source/features/extend-diff-expander.tsx
+++ source/features/extend-diff-expander.tsx
@@ -6,16 +6,26 @@ import * as pageDetect from 'github-url-detection';
 
 import features from '../feature-manager.js';
 
+const diffSelectors = [
+	'.diff-view .js-expandable-line',
+	'.diff-line-row:has(button[data-direction])',
+];
+
+const diffExpandSelector = [
+	'.js-expand',
+	'button[data-direction]',
+];
+
 function expandDiff(event: DelegateEvent): void {
 	// Skip if the user clicked directly on the icon
-	if (!(event.target as Element).closest('.js-expand')) {
-		$('.js-expand', event.delegateTarget).click();
+	if (!(event.target as Element).closest(diffExpandSelector)) {
+		$(diffExpandSelector, event.delegateTarget).click();
 	}
 }
 
 function init(signal: AbortSignal): void {
 	document.body.classList.add('rgh-extend-diff-expander');
-	delegate('.diff-view .js-expandable-line', 'click', expandDiff, {signal});
+	delegate(diffSelectors, 'click', expandDiff, {signal});
 }
 
 void features.add(import.meta.url, {
