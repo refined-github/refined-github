2addbafce7b32e7f9b0e838ce1562d5101b5ae91
Federico <me@fregante.com>
2024-11-19 14:52:15 +0700

    /3

--- source/options/header.svelte
+++ source/options/header.svelte
@@ -26,29 +26,29 @@
 
 <style>
 	header {
-	background-color: light-dark(#f6f8fa, #02040a);
-	border-bottom: solid 1px light-dark(#d2d9e0, #3d444d);
-	padding-block: 30px;
-	padding-inline: var(--viewport-margin);
-	display: grid;
-	gap: 1em;
+		background-color: light-dark(#f6f8fa, #02040a);
+		border-bottom: solid 1px light-dark(#d2d9e0, #3d444d);
+		padding-block: 30px;
+		padding-inline: var(--viewport-margin);
+		display: grid;
+		gap: 1em;
 
-	& > * {
-		width: 100%;
-		max-width: var(--content-width);
-		margin: auto;
+		& > * {
+			width: 100%;
+			max-width: var(--content-width);
+			margin: auto;
+		}
 	}
-}
 
-h1 {
-	display: flex;
-	gap: 0.4em;
-	font-size: clamp(1.3em, 5vw, 2em);
-	font-weight: 200;
+	h1 {
+		display: flex;
+		gap: 0.4em;
+		font-size: clamp(1.3em, 5vw, 2em);
+		font-weight: 200;
 
-	img {
-		height: 1.3em;
-		width: 1.3em;
+		img {
+			height: 1.3em;
+			width: 1.3em;
+		}
 	}
-}
 </style>
