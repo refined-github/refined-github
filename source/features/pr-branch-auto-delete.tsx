77dc30993d0059f2a8a28e8aeea8327757f861ef
fregante <me@fregante.com>
2024-12-02 14:23:49 +0700

    `pr-branch-auto-delete` - Skip branches with active PRs (#7786)

--- source/features/pr-branch-auto-delete.tsx
+++ source/features/pr-branch-auto-delete.tsx
@@ -4,11 +4,15 @@ import InfoIcon from 'octicons-plain-react/Info';
 import elementReady from 'element-ready';
 import * as pageDetect from 'github-url-detection';
 
+import api from '../github-helpers/api.js';
 import features from '../feature-manager.js';
 import onPrMerge from '../github-events/on-pr-merge.js';
 import {getBranches} from '../github-helpers/pr-branches.js';
 import matchesAnyPattern from '../helpers/matches-any-patterns.js';
+import GetPrsToBaseBranch from './pr-branch-auto-delete.gql';
 
+// DO NOT ask for additions or customizations. This is just a list of "obvious" permanent branches.
+// Protect your permanent branches instead: https://docs.github.com/en/repositories/configuring-branches-and-merges-in-your-repository/managing-protected-branches/about-protected-branches
 const exceptions = [
 	'dev',
 	'develop',
@@ -31,7 +35,20 @@ async function init(): Promise<void> {
 		return;
 	}
 
-	if (matchesAnyPattern(getBranches().head.branch, exceptions)) {
+	// Skip branches that are likely to be long-lived https://github.com/refined-github/refined-github/issues/7755
+	const {head} = getBranches();
+	if (matchesAnyPattern(head.branch, exceptions)) {
+		return;
+	}
+
+	// Skip branches that have PRs open https://github.com/refined-github/refined-github/issues/7782
+	const {repository} = await api.v4(GetPrsToBaseBranch, {
+		variables: {
+			baseRefName: head.branch,
+		},
+	});
+
+	if (repository.pullRequests.totalCount) {
 		return;
 	}
 
