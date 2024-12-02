59f6cc7b65183f0bbabc87bc18c0b2bc912caa08
Federico <me@fregante.com>
2024-12-02 11:40:48 +0700

    Lint

--- source/features/pr-branch-auto-delete.tsx
+++ source/features/pr-branch-auto-delete.tsx
@@ -6,10 +6,8 @@ import * as pageDetect from 'github-url-detection';
 
 import features from '../feature-manager.js';
 import onPrMerge from '../github-events/on-pr-merge.js';
-import {getFeatureUrl} from '../helpers/rgh-links.js';
 import {getBranches} from '../github-helpers/pr-branches.js';
 import matchesAnyPattern from '../helpers/matches-any-patterns.js';
-import {getFeatureID} from '../helpers/feature-helpers.js';
 
 const exceptions = [
 	'dev',
