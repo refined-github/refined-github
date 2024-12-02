6475cddcee179971c35accd6d801bc51d3f948e2
Federico <me@fregante.com>
2024-12-02 14:15:10 +0700

    Fix

--- source/features/pr-branch-auto-delete.tsx
+++ source/features/pr-branch-auto-delete.tsx
@@ -11,6 +11,8 @@ import {getBranches} from '../github-helpers/pr-branches.js';
 import matchesAnyPattern from '../helpers/matches-any-patterns.js';
 import GetPrsToBaseBranch from './pr-branch-auto-delete.gql';
 
+// DO NOT ask for additions or customizations. This is just a list of "obvious" permanent branches.
+// Protect your permanent branches instead: https://docs.github.com/en/repositories/configuring-branches-and-merges-in-your-repository/managing-protected-branches/about-protected-branches
 const exceptions = [
 	'dev',
 	'develop',
@@ -35,7 +37,7 @@ async function init(): Promise<void> {
 
 	// Skip branches that are likely to be long-lived https://github.com/refined-github/refined-github/issues/7755
 	const {head} = getBranches();
-	if (matchesAnyPattern(head.name, exceptions)) {
+	if (matchesAnyPattern(head.branch, exceptions)) {
 		return;
 	}
 
