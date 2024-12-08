650fd51642da295d9faf66253f53304e56dbb86f
Federico <me@fregante.com>
2024-12-08 15:21:55 +0800

    Second fix

--- source/helpers/target-blank-polyfill.ts
+++ source/helpers/target-blank-polyfill.ts
@@ -12,9 +12,9 @@ function isLinkExternal(link: HTMLAnchorElement): boolean {
 // WTF Safari. It opens target="_blank" links in the same tab on extension pages
 if ('window' in globalThis && 'open' in globalThis && isSafari()) {
 	document.addEventListener('click', filterAlteredClicks(event => {
-		const clicked = event.target as HTMLAnchorElement;
-		// Don't use `instaceof` #8135
-		if (clicked.tagName === 'A' && isLinkExternal(clicked)) {
+		// WebComponents support https://stackoverflow.com/a/57963850
+		const clicked = event.composedPath().find(element => element instanceof HTMLAnchorElement);
+		if (clicked && isLinkExternal(clicked)) {
 			event.preventDefault();
 			window.open(clicked.href);
 		}
