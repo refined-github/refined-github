be684601269594322c188815adfe24c34a9e27bb
Flo Edelmann <git@flo-edelmann.de>
2024-12-09 12:17:48 +0100

    Only match whole words in `isLowQualityComment`

--- source/helpers/is-low-quality-comment.ts
+++ source/helpers/is-low-quality-comment.ts
@@ -1,4 +1,31 @@
+const lowQualityWords = [
+	'ditto',
+	'dito',
+	'me',
+	'too',
+	'there',
+	'here',
+	'on',
+	'same',
+	'this',
+	'issue',
+	'issues',
+	'please',
+	'pls',
+	'plz',
+	'any',
+	'update',
+	'updates',
+	'bump',
+	'question',
+	'solution',
+	'following',
+];
+const lowQualityWordsRegex = new RegExp(`\\b(${lowQualityWords.join('|')})\\b`, 'gi');
+
 export default function isLowQualityComment(text: string): boolean {
 	// Note: the unicode range targets skin color modifiers for the hand emojis
-	return text.replaceAll(/[\s,.!?👍👎👌🙏]+|[\u{1F3FB}-\u{1F3FF}]|[+-]\d+|⬆️|ditt?o|me|too|t?here|on|same|this|issues?|please|pl[sz]|any|updates?|bump|question|solution|following/giu, '') === '';
+	return text
+		.replaceAll(lowQualityWordsRegex, '')
+		.replaceAll(/[\s,.!?👍👎👌🙏]+|[\u{1F3FB}-\u{1F3FF}]|[+-]\d+|⬆️/gu, '') === '';
 }
