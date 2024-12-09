4721b8bd9ac5dc345b18844ee95e1d50944e2cfa
kovsu <2583695112@qq.com>
2024-12-09 23:34:03 +0800

    update

--- source/github-helpers/timeline-item.tsx
+++ source/github-helpers/timeline-item.tsx
@@ -5,7 +5,7 @@ export function TimelineItemOld(): JSX.Element {
 	return <div className="ml-0 pl-0 ml-md-6 pl-md-3 mt-3" />;
 }
 
-// #8141
+// https://github.com/refined-github/refined-github/pull/8141
 export function TimelineItem(): JSX.Element {
-	return <div className="ml-0 pl-0 pl-md-3 mt-3" />;
+	return <div className="my-2" />;
 }
