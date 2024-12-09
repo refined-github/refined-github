639b392b0b6b6d17db638f0ff6e88aa8d17b8085
fregante <me@fregante.com>
2024-12-09 11:12:37 +0800

    Faster disableable CSS features (#8140)

--- source/features/clean-pinned-issues.tsx
+++ source/features/clean-pinned-issues.tsx
@@ -1,10 +1,8 @@
 import './clean-pinned-issues.css';
 
-import * as pageDetect from 'github-url-detection';
-
 import features from '../feature-manager.js';
 
-void features.addCssFeature(import.meta.url, [pageDetect.isRepoIssueList]);
+void features.addCssFeature(import.meta.url);
 
 /*
 
