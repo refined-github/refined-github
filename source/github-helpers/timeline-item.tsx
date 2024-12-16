9034ec20a2e501d7350dea7aea1a96c1a7d765c1
Konv Suu <2583695112@qq.com>
2024-12-16 08:43:49 +0800

    `rgh-netiquette` - Support new views (#8141)

--- source/github-helpers/timeline-item.tsx
+++ source/github-helpers/timeline-item.tsx
@@ -1,6 +1,11 @@
 import React from 'dom-chef';
 
-export default function TimelineItem(): JSX.Element {
+export function TimelineItemOld(): JSX.Element {
 	// Classes copied from #issuecomment-new + mt-3 added
 	return <div className="ml-0 pl-0 ml-md-6 pl-md-3 mt-3" />;
+}
+
+// https://github.com/refined-github/refined-github/pull/8141
+export function TimelineItem(): JSX.Element {
+	return <div className="my-2" />;
 }
