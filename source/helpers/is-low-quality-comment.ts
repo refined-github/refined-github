9d8875fef3e2ee162eb4651657d7ac461afe3e18
Flo Edelmann <git@flo-edelmann.de>
2024-12-18 09:01:45 +0100

    `hide-low-quality-comments` - Hide more comments (#8145)

--- source/helpers/is-low-quality-comment.ts
+++ source/helpers/is-low-quality-comment.ts
@@ -1,4 +1,37 @@
+const lowQualityWords = [
+	'a',
+	'an',
+	'and',
+	'any',
+	'bump',
+	'dito',
+	'ditto',
+	'following',
+	'for',
+	'here',
+	'is',
+	'issue',
+	'issues',
+	'me',
+	'on',
+	'please',
+	'pls',
+	'plz',
+	'question',
+	'same',
+	'solution',
+	'still',
+	'there',
+	'this',
+	'too',
+	'update',
+	'updates',
+];
+const lowQualityWordsRegex = new RegExp(`\\b(${lowQualityWords.join('|')})\\b`, 'gi');
+
 export default function isLowQualityComment(text: string): boolean {
 	// Note: the unicode range targets skin color modifiers for the hand emojis
-	return text.replaceAll(/[\s,.!?👍👎👌🙏]+|[\u{1F3FB}-\u{1F3FF}]|[+-]\d+|⬆️|ditt?o|me|too|t?here|on|same|this|issues?|please|pl[sz]|any|updates?|bump|question|solution|following/giu, '') === '';
+	return text
+		.replaceAll(lowQualityWordsRegex, '')
+		.replaceAll(/[\s,.!?👍👎👌🙏]+|[\u{1F3FB}-\u{1F3FF}]|[+-]\d+|⬆️/gu, '') === '';
 }
