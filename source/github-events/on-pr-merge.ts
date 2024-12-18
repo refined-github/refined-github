7f6c5cd264c5be03d0de228e928c9c80520d719f
Federico <me@fregante.com>
2024-12-18 15:39:12 +0800

    `closing-remarks` - Support new PR merge box

--- source/github-events/on-pr-merge.ts
+++ source/github-events/on-pr-merge.ts
@@ -4,6 +4,9 @@ import observe from '../helpers/selector-observer.js';
 
 // This event ensures that the callback appears exclusively to the person that merged the PR and not anyone who was on the page at the time of the merge
 export default async function onPrMerge(callback: VoidFunction, signal: AbortSignal): Promise<void> {
-	await oneEvent('.js-merge-commit-button', 'click', {signal});
+	await oneEvent([
+		'.js-merge-commit-button', // TODO: Drop in May 2025
+		'[aria-label="Checks"] ~ div button[data-size="medium"][data-variant="primary"]',
+	], 'click', {signal});
 	observe('.TimelineItem-badge .octicon-git-merge', callback, {signal});
 }
