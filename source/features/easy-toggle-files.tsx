e0ae960b5fe30609b9f6fff6e18280c7618b85bb
Kid <44045911+kidonng@users.noreply.github.com>
2025-01-20 18:14:00 +0800

    `easy-toggle-files` - Support new commit view (#8189)

--- source/features/easy-toggle-files.tsx
+++ source/features/easy-toggle-files.tsx
@@ -9,9 +9,23 @@ function toggleFile(event: DelegateEvent<MouseEvent>): void {
 	const elementClicked = event.target as HTMLElement;
 	const headerBar = event.delegateTarget;
 
-	// The clicked element is either the bar itself or one of its 2 children
-	if (elementClicked === headerBar || elementClicked.parentElement === headerBar) {
-		$('[aria-label="Toggle diff contents"]', headerBar)
+	if (
+		// The clicked element is either the bar itself or one of its 2 children
+		elementClicked === headerBar
+		|| elementClicked.parentElement === headerBar
+		// React
+		// TODO: Replace with `|| !elementClicked.closest('a, button')` if it works
+		|| elementClicked.matches([
+			'[class^="DiffFileHeader-module__diff-file-header"] > div',
+			'[class^="DiffFileHeader-module__diff-file-header"] > div > div',
+		])
+	) {
+		$([
+			'[aria-label="Toggle diff contents"]',
+			// React
+			'[aria-label^="collapse file"]',
+			'[aria-label^="expand file"]',
+		], headerBar)
 			.dispatchEvent(new MouseEvent('click', {bubbles: true, altKey: event.altKey}));
 	}
 }
@@ -28,7 +42,11 @@ function toggleCodeSearchFile(event: DelegateEvent<MouseEvent>): void {
 }
 
 function init(signal: AbortSignal): void {
-	delegate('.file-header', 'click', toggleFile, {signal});
+	delegate([
+		'.file-header',
+		// React
+		'[class^="Diff-module__diffHeaderWrapper"]',
+	], 'click', toggleFile, {signal});
 }
 
 function initSearchPage(signal: AbortSignal): void {
