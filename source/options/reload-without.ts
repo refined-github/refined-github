9bdddaafc3c58b6fdeb38356d7d4eb1f0639c4ae
Federico <me@fregante.com>
2025-01-23 00:22:16 +0100

    Use `createContextMenu` in `reload-without`

--- source/options/reload-without.ts
+++ source/options/reload-without.ts
@@ -3,6 +3,7 @@ import webextAlert from 'webext-alert';
 import chromeP from 'webext-polyfill-kinda';
 import {isScriptableUrl} from 'webext-content-scripts';
 import {isFirefox} from 'webext-detect';
+import {createContextMenu} from 'webext-tools';
 
 // Always Firefox… https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/storage/StorageArea/setAccessLevel
 const area = isFirefox() ? 'local' : 'session';
@@ -12,36 +13,25 @@ export const contentScriptToggle = new StorageItem('contentScript', {
 	defaultValue: true,
 });
 
-const id = 'reload-without-content-scripts';
+async function reload(_: unknown, tab: chrome.tabs.Tab): Promise<void> {
+	if (tab.url && isScriptableUrl(tab.url) && await chromeP.permissions.contains({
+		origins: [tab.url],
+	})) {
+		await contentScriptToggle.set(false);
+		chrome.tabs.reload(tab.id!);
+	} else {
+		// TODO: Use https://github.com/fregante/webext-events/issues/31 to disable the item instead
+		webextAlert('Refined GitHub is already not running on this page');
+	}
+}
 
 export default function addReloadWithoutContentScripts(): void {
-	if (!chrome.contextMenus) {
-		// Silently ignore if the API is not available, like in Firefox Android
-		// https://github.com/fregante/webext-permission-toggle/pull/53
-		return;
-	}
-
 	chrome.storage.session.setAccessLevel?.({accessLevel: 'TRUSTED_AND_UNTRUSTED_CONTEXTS'});
 
-	chrome.contextMenus.create({
-		id,
+	void createContextMenu({
+		id: 'reload-without-content-scripts',
 		title: 'Reload without Refined GitHub',
-		contexts: 'action' in chrome ? ['action'] : ['browser_action'],
-	});
-
-	chrome.contextMenus.onClicked.addListener(async (info, tab) => {
-		if (!tab?.id || info.menuItemId !== id) {
-			return;
-		}
-
-		if (tab.url && isScriptableUrl(tab.url) && await chromeP.permissions.contains({
-			origins: [tab.url],
-		})) {
-			await contentScriptToggle.set(false);
-			chrome.tabs.reload(tab.id);
-		} else {
-			// TODO: Use https://github.com/fregante/webext-tools/issues/11 to disable the item instead
-			webextAlert('Refined GitHub is already not running on this page');
-		}
+		contexts: 'action',
+		onclick: reload,
 	});
 }
