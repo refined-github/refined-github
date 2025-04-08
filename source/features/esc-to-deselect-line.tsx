538af8b88385dcb69e6fcf590c4a415d0e1433c8
fregante <me@fregante.com>
2025-04-08 17:36:14 +0700

    `github-actions-indicators` - Open dropdown on icon click (#8369)

--- source/features/esc-to-deselect-line.tsx
+++ source/features/esc-to-deselect-line.tsx
@@ -3,6 +3,7 @@ import * as pageDetect from 'github-url-detection';
 
 import features from '../feature-manager.js';
 import {isEditable} from '../helpers/dom-utils.js';
+import removeHashFromUrlBar from '../helpers/history.js';
 
 function isLineSelected(): boolean {
 	// Example hashes:
@@ -31,7 +32,7 @@ function listener({key, target}: KeyboardEvent): void {
 			location.hash = '#no-line'; // Update UI, without `scroll-to-top` behavior
 		}
 
-		history.replaceState(undefined, document.title, location.pathname); // Drop remaining # from url
+		removeHashFromUrlBar();
 	}
 }
 
