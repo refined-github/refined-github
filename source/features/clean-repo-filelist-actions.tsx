9806f40827afa52db73ce211c2f15790a7f9d132
Konv Suu <2583695112@qq.com>
2025-05-08 11:55:39 +0800

    update

--- source/features/clean-repo-filelist-actions.tsx
+++ source/features/clean-repo-filelist-actions.tsx
@@ -6,7 +6,6 @@ import PlusIcon from 'octicons-plain-react/Plus';
 import observe from '../helpers/selector-observer.js';
 import {assertNodeContent, wrap} from '../helpers/dom-utils.js';
 import features from '../feature-manager.js';
-
 import './clean-repo-filelist-actions.css';
 
 /** Add tooltip on a wrapper to avoid breaking dropdown functionality */
