bb051a86a5302e0a1d01ec77a98f46617600aea0
Grigory <techuila.sunset@gmail.com>
2025-07-04 03:00:22 +0500

    fix(features/align-issue-labels): adapt to dom changes

--- source/features/align-issue-labels.tsx
+++ source/features/align-issue-labels.tsx
@@ -1,8 +1,57 @@
-import './align-issue-labels.css';
+import React from 'dom-chef';
+import * as pageDetect from 'github-url-detection';
+import batchedFunction from 'batched-function';
 
 import features from '../feature-manager.js';
+import observe from '../helpers/selector-observer.js';
 
-void features.addCssFeature(import.meta.url);
+const issueRowSelector = '[class^="IssueRow-module__row"]';
+const badgesContainerSelector = '[class*="trailingBadgesContainer"]';
+const mainContentInnerSelector = '[class^="MainContent-module__inner"]';
+const issueTypeContainerSelector = '[class*="issueTypeTokenContainer"]';
+
+const ghMargin = 'var(--base-size-4)';
+
+function moveBadges(issueRows: HTMLElement[]): void {
+	for (const issueRow of issueRows) {
+		const badgesContainer = issueRow.querySelector(badgesContainerSelector);
+		if (!badgesContainer) {
+			continue;
+		}
+
+		const mainContentInner = issueRow.querySelector(mainContentInnerSelector);
+		if (!mainContentInner) {
+			continue;
+		}
+
+		mainContentInner.style.gap = ghMargin;
+		mainContentInner.append(badgesContainer);
+
+		const issueTypeTokenContainer = issueRow.querySelector(issueTypeContainerSelector);
+		if (!issueTypeTokenContainer) {
+			continue;
+		}
+
+		issueTypeTokenContainer.style.marginRight = ghMargin;
+		issueTypeTokenContainer.style.display = 'inline-block';
+		badgesContainer.prepend(issueTypeTokenContainer);
+
+		issueTypeTokenContainer.after(
+			<span style={{display: 'inline-block', marginLeft: ghMargin}}>•</span>,
+		);
+	}
+}
+
+async function init(signal: AbortSignal): Promise<void> {
+	observe(issueRowSelector, batchedFunction(moveBadges, {delay: 100}), {signal});
+}
+
+void features.add(import.meta.url, {
+	include: [
+		pageDetect.isRepoIssueList,
+	],
+	init,
+});
 
 /*
 
@@ -12,5 +61,6 @@ https://github.com/pulls
 https://github.com/bmish/eslint-doc-generator/pulls
 https://github.com/bmish/eslint-doc-generator/issues
 https://github.com/bmish/eslint-doc-generator/milestone/1
+https://github.com/nrwl/nx/issues
 
 */
