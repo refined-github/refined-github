37b8bbee8ab825360a5ee6b1025e9dbed446ccb4
fregante <me@fregante.com>
2025-07-31 17:20:12 +0800

    yolo

--- source/features/global-conversation-list-filters.tsx
+++ source/features/global-conversation-list-filters.tsx
@@ -8,39 +8,31 @@ import features from '../feature-manager.js';
 import SearchQuery from '../github-helpers/search-query.js';
 import observe from '../helpers/selector-observer.js';
 
-function addLinks(container: HTMLElement): void {
-	const defaultQuery = 'is:open archived:false';
-
-	// Without this, the Issues page also displays PRs, and vice-versa
-	const isIssues = location.pathname.startsWith('/issues');
-	const typeQuery = isIssues ? 'is:issue' : 'is:pr';
-	const typeName = isIssues ? 'Issues' : 'Pull Requests';
-
-	const links = [
-		['Involved', `${typeName} you’re involved in`, 'involves:@me'],
-		['Yours', `${typeName} on your repos`, 'user:@me'],
-	] as const;
-
-	for (const [label, title, query] of links) {
-		// Create link
-		const url = new URL(isIssues ? '/issues' : '/pulls', location.origin);
-		url.searchParams.set('q', `${typeQuery} ${defaultQuery} ${query}`);
-		const link = <a href={url.href} title={title} className="subnav-item">{label}</a>;
-
-		const isCurrentPage = SearchQuery.from(location).includes(query);
-
-		// Highlight it, if that's the current page
-		if (isCurrentPage && !elementExists('.subnav-links .selected')) {
-			link.classList.add('selected');
-
-			// Other links will keep the current query, that's not what we want
-			for (const otherLink of $$('.subnav-links a')) {
-				otherLink.href = SearchQuery.from(otherLink).remove(query).href;
-			}
+function createLink(label: string, title: string, query: string): HTMLElement {
+	const url = new URL('/pulls', location.origin);
+	url.searchParams.set('q', `is:open archived:false ${query}`);
+	const link = <a href={url.href} title={title} className="subnav-item">{label}</a>;
+	
+	const isCurrentPage = SearchQuery.from(location).includes(query);
+	
+	// Highlight it, if that's the current page
+	if (isCurrentPage && !elementExists('.subnav-links .selected')) {
+		link.classList.add('selected');
+	
+		// Other links will keep the current query, that's not what we want
+		for (const otherLink of $$('.subnav-links a')) {
+			otherLink.href = SearchQuery.from(otherLink).remove(query).href;
 		}
-
-		container.append(link);
 	}
+
+	return link;
+}
+
+function addLinks(container: HTMLElement): void {
+	container.append(
+		createLink('Involved', 'Pull Requests you’re involved in', 'involves:@me'),
+		createLink('Yours', 'Pull Requests on your repos', 'user:@me'),
+	);
 }
 
 function init(signal: AbortSignal): void {
@@ -48,8 +40,9 @@ function init(signal: AbortSignal): void {
 }
 
 void features.add(import.meta.url, {
-	include: [
+	asLongAs: [
 		pageDetect.isGlobalIssueOrPRList,
+		pageDetect.isPRList, // We don't have a single `isGlobalPRList`
 	],
 	init,
 });
