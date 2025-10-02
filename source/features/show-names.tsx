a9fc7c17f38695cecb96d111afe5953be2ddb0b1
Konv Suu <2583695112@qq.com>
2025-10-02 20:57:59 +0800

    `show-names` - Exclude username in nested item (#8661)

--- source/features/show-names.tsx
+++ source/features/show-names.tsx
@@ -41,7 +41,11 @@ function appendName(element: HTMLAnchorElement, fullName: string): void {
 }
 
 async function updateLinks(found: HTMLAnchorElement[]): Promise<void> {
-	const users = Map.groupBy(found, element => element.textContent.trim());
+	const users = Map.groupBy(
+		// Exclude nested items https://github.com/refined-github/refined-github/pull/8661
+		found.filter(element => element.textContent.trim() === element.href.split('/').pop()),
+		element => element.textContent.trim(),
+	);
 	users.delete(getLoggedInUser()!);
 	users.delete('ghost'); // Consider using `github-reserved-names` if more exclusions are needed
 
