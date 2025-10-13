f89c6c56d7f19970a0ad8c4f00b18caae9f19485
Grigory <techuila.sunset@gmail.com>
2025-10-13 19:17:31 +0500

    apply suggestions from code review

--- source/features/quick-delete-cancel-workflow.tsx
+++ source/features/quick-delete-cancel-workflow.tsx
@@ -4,52 +4,45 @@ import SquareCircleIcon from 'octicons-plain-react/SquareCircle';
 import * as pageDetect from 'github-url-detection';
 import {$, $optional} from 'select-dom/strict.js';
 
-import features from '../feature-manager.js';
 import observe from '../helpers/selector-observer.js';
+import features from '../feature-manager.js';
 import './quick-delete-cancel-workflow.css';
 
-function transformIntoIconButton(button: HTMLButtonElement, icon: React.JSX.Element): void {
+function transformIntoIconButton(button: HTMLButtonElement, icon: React.JSX.Element): HTMLButtonElement {
 	button.ariaLabel = button.textContent.trim();
 	button.replaceChildren(icon);
-	button.classList = 'Button Button--iconOnly';
+	button.classList = 'lh-condensed color-fg-muted btn-link';
+	return button;
 }
 
-function removeRedundantDivider(menuItem: HTMLElement): void {
-	if (!menuItem.previousElementSibling) {
-		const nextMenuItem = menuItem.nextElementSibling;
-		if (nextMenuItem?.classList.contains('dropdown-divider')) {
-			nextMenuItem.remove();
-		}
-	}
-}
-
-function addQuickButtons(actionRunRow: HTMLDivElement): void {
-	const contextMenuDetails = $('details:has(.octicon-kebab-horizontal)', actionRunRow);
+function addQuickButtons(contextMenu: HTMLElement): void {
+	const contextMenuDetails = contextMenu.closest('details')!;
 	const rightControlsContainer = contextMenuDetails.parentElement!;
 
-	const deleteWorkflowButton = $optional('button[data-show-dialog-id^="delete-workflow-run"]', contextMenuDetails);
-	if (deleteWorkflowButton) {
-		const dialogHelper = $('dialog-helper', deleteWorkflowButton.parentElement!);
-		dialogHelper.classList.add('text-left');
+	const deleteWorkflowMenuItem = $optional('li:has(> button[data-show-dialog-id^="delete-workflow-run"])', contextMenuDetails)?.cloneNode(true);
+	if (deleteWorkflowMenuItem) {
+		const deleteWorkflowButton = $('button', deleteWorkflowMenuItem);
 		transformIntoIconButton(deleteWorkflowButton, <TrashIcon />);
 		deleteWorkflowButton.classList.add('rgh-delete-workflow-run');
-		rightControlsContainer.prepend(deleteWorkflowButton, dialogHelper);
+		rightControlsContainer.prepend(deleteWorkflowMenuItem);
 	}
 
-	const cancelForm = $optional('form[action$="/cancel"]', contextMenuDetails);
+	const cancelForm = $optional('form[action$="/cancel"]', contextMenuDetails)?.cloneNode(true);
 	if (cancelForm) {
-		const cancelWorkflowButton = $('button[type="submit"]', cancelForm);
+		const cancelWorkflowButton = $('button', cancelForm);
 		transformIntoIconButton(cancelWorkflowButton, <SquareCircleIcon />);
 		cancelWorkflowButton.classList.add('rgh-cancel-workflow-run');
-		removeRedundantDivider(cancelForm.parentElement!);
 		rightControlsContainer.prepend(cancelForm);
 	}
 
-	rightControlsContainer.classList.toggle('d-flex', !!(cancelForm || deleteWorkflowButton));
+	if (cancelForm || deleteWorkflowMenuItem) {
+		rightControlsContainer.classList.add('d-flex', 'flex-column-reverse');
+		$('summary', contextMenuDetails).classList.add('lh-condensed', 'my-1', 'p-0');
+	}
 }
 
 function init(signal: AbortSignal): void {
-	observe('#partial-actions-workflow-runs .Box-row', addQuickButtons, {signal});
+	observe('#partial-actions-workflow-runs .Box-row details .octicon-kebab-horizontal', addQuickButtons, {signal});
 }
 
 void features.add(import.meta.url, {
