8c8a904ebfb57c1e23f88914fa4c575ab8d86149
Grigory <techuila.sunset@gmail.com>
2025-10-13 19:47:56 +0500

    rename feature

--- source/features/quick-workflow-run-actions.tsx
+++ source/features/quick-workflow-run-actions.tsx
@@ -6,7 +6,7 @@ import {$, $optional} from 'select-dom/strict.js';
 
 import observe from '../helpers/selector-observer.js';
 import features from '../feature-manager.js';
-import './quick-delete-cancel-workflow.css';
+import './quick-workflow-run-actions.css';
 
 function transformIntoIconButton(button: HTMLButtonElement, icon: React.JSX.Element): HTMLButtonElement {
 	button.ariaLabel = button.textContent.trim();
