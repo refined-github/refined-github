2edca29c809545ab09c45bdb7a100001499c24ce
fregante <me@fregante.com>
2025-11-10 01:18:13 +0700

    Improve token user warning style (#8745)

--- source/features/rgh-token-user.tsx
+++ source/features/rgh-token-user.tsx
@@ -7,7 +7,6 @@ import {getLoggedInUser} from '../github-helpers/index.js';
 import {getToken} from '../options-storage.js';
 import {tokenUser} from '../github-helpers/github-token.js';
 import {api3} from '../github-helpers/api.js';
-import createBanner from '../github-helpers/banner.js';
 import onetime from '../helpers/onetime.js';
 import {OptionsLink} from '../helpers/open-options.js';
 
@@ -20,14 +19,13 @@ async function verify(header: HTMLButtonElement): Promise<void> {
 	const currentWebUser = getLoggedInUser();
 	const currentTokenUser = await tokenUser.get(api3, token);
 	if (currentWebUser !== currentTokenUser) {
-		header.after(createBanner({
-			icon: <AlertIcon />,
-			classes: ['mx-3', 'mt-3', 'mb-0', 'py-2'],
-			text: [
-				<>Your <OptionsLink className="btn-link">Refined GitHub token</OptionsLink> is for a different user, the extension will act on behalf of <code>{currentTokenUser}</code></>,
-			],
-		},
-		));
+		header.after(
+			// Use raw "flash" classes to blend in better with the dropdown menu
+			<div className="flash px-3 mt-3 mb-0 py-2 d-flex flex-items-center border-0 rounded-0">
+				<AlertIcon className="mr-2" />
+				<span>Your <OptionsLink className="btn-link">Refined GitHub token</OptionsLink> is for a different user, the extension will act on behalf of <code>{currentTokenUser}</code></span>
+			</div>,
+		);
 	}
 }
 
