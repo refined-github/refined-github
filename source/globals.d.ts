c4c3f33032270598076427c85e7183337a130d11
Federico <me@fregante.com>
2025-11-13 15:31:54 +0700

    dos

--- source/globals.d.ts
+++ source/globals.d.ts
@@ -18,7 +18,7 @@ type FeatureID = string & {feature: true};
 interface FeatureMeta {
 	id: FeatureID;
 	description: string;
-	screenshot: string | undefined; // eslint-disable-line ts/no-restricted-types -- We use `null` in the JSON file
+	screenshot: string | null; // eslint-disable-line ts/no-restricted-types -- We use `null` in the JSON file
 	css?: true;
 }
 
