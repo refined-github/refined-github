a1e945ef0e0d3eadc7488f2713c6353f969a0a29
Grigory <techuila.sunset@gmail.com>
2025-11-17 02:09:46 +0500

    `improve-shortcut-help` - Restore feature

--- source/features/improve-shortcut-help.tsx
+++ source/features/improve-shortcut-help.tsx
@@ -1,63 +1,47 @@
 import React from 'dom-chef';
-import {elementExists} from 'select-dom';
-import {$, $optional} from 'select-dom/strict.js';
+import {$} from 'select-dom/strict.js';
 
-import onetime from '../helpers/onetime.js';
 import features from '../feature-manager.js';
-import {isEditable} from '../helpers/dom-utils.js';
 import {shortcutMap} from '../helpers/feature-helpers.js';
+import observe from '../helpers/selector-observer.js';
 
-function splitKeys(keys: string): DocumentFragment[] {
-	return keys.split(' ').map(key => <> <kbd>{key}</kbd></>);
-}
+function improveShortcutHelp(columnsContainer: HTMLElement): void {
+	const lastColumn = columnsContainer.lastElementChild!;
+	const shortcutsContainer = lastColumn.firstElementChild!.cloneNode(true);
+	const shortcutsList = $('ul', shortcutsContainer);
+	const shortcutItem = $('[class^="ShortcutsGroupList-module__ShortcutItem"]', shortcutsList);
+	const keybindingHint = $('kbd', shortcutItem);
+	const chord = $('span', shortcutItem);
 
-function improveShortcutHelp(dialog: Element): void {
-	$('.Box-body .col-5 .Box:first-child', dialog).after(
-		<div className="Box Box--condensed m-4">
-			<div className="Box-header">
-				<h2 className="Box-title">Refined GitHub</h2>
-			</div>
-
-			<ul>
-				{[...shortcutMap]
-					.toSorted(([, a], [, b]) => a.localeCompare(b))
-					.map(([hotkey, description]) => (
-						<li className="Box-row d-flex flex-row">
-							<div className="flex-auto">{description}</div>
-							<div className="ml-2 no-wrap">
-								{splitKeys(hotkey)}
-							</div>
-						</li>
-					))}
-			</ul>
-		</div>,
+	$('h2', shortcutsContainer).textContent = 'Refined GitHub';
+	shortcutsList.replaceChildren(
+		...[...shortcutMap]
+			.toSorted(([, a], [, b]) => a.localeCompare(b))
+			.map(([hotkey, description]) => {
+				const currentItem = shortcutItem.cloneNode(true);
+				currentItem.firstElementChild!.textContent = description;
+				currentItem.lastElementChild!.replaceChildren(
+					<kbd className={keybindingHint.className}>
+						{hotkey.split(' ').map(key => (
+							<span className={chord.className}>
+								<span>{key.length === 1 ? key.toUpperCase() : key.charAt(0).toUpperCase() + key.slice(1)}</span>
+							</span>
+						))}
+					</kbd>,
+				);
+				return currentItem;
+			}),
 	);
+
+	lastColumn.append(shortcutsContainer);
 }
 
-const observer = new MutationObserver(([{target}]) => {
-	if (target instanceof Element && !elementExists('.js-details-dialog-spinner', target)) {
-		improveShortcutHelp(target);
-		observer.disconnect();
-	}
-});
-
-function observeShortcutModal({key, target}: KeyboardEvent): void {
-	if (key !== '?' || isEditable(target)) {
-		return;
-	}
-
-	const modal = $optional('body > details:not(.js-command-palette-dialog) > details-dialog');
-	if (modal) {
-		observer.observe(modal, {childList: true});
-	}
-}
-
-function initOnce(): void {
-	document.body.addEventListener('keypress', observeShortcutModal);
+function init(signal: AbortSignal): void {
+	observe('[class^="ShortcutsDialog-module__ColumnsContainer"]', improveShortcutHelp, {signal});
 }
 
 void features.add(import.meta.url, {
-	init: onetime(initOnce),
+	init,
 });
 
 /*
