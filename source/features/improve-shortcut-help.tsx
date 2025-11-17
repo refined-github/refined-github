da8dbeaa3312fe8c98904e93f98ed4b763dd7a35
Grigory <techuila.sunset@gmail.com>
2025-11-18 03:14:42 +0500

    ghe

--- source/features/improve-shortcut-help.tsx
+++ source/features/improve-shortcut-help.tsx
@@ -1,10 +1,63 @@
 import React from 'dom-chef';
-import {$} from 'select-dom/strict.js';
+import {elementExists} from 'select-dom';
+import {$, $optional} from 'select-dom/strict.js';
 
+import onetime from '../helpers/onetime.js';
 import features from '../feature-manager.js';
 import {shortcutMap} from '../helpers/feature-helpers.js';
+import {isEditable} from '../helpers/dom-utils.js';
 import observe from '../helpers/selector-observer.js';
 
+function splitKeys(keys: string): DocumentFragment[] {
+	return keys.split(' ').map(key => <> <kbd>{key}</kbd></>);
+}
+
+function improveShortcutHelpLegacy(dialog: Element): void {
+	$('.Box-body .col-5 .Box:first-child', dialog).after(
+		<div className="Box Box--condensed m-4">
+			<div className="Box-header">
+				<h2 className="Box-title">Refined GitHub</h2>
+			</div>
+
+			<ul>
+				{[...shortcutMap]
+					.toSorted(([, a], [, b]) => a.localeCompare(b))
+					.map(([hotkey, description]) => (
+						<li className="Box-row d-flex flex-row">
+							<div className="flex-auto">{description}</div>
+							<div className="ml-2 no-wrap">
+								{splitKeys(hotkey)}
+							</div>
+						</li>
+					))}
+			</ul>
+		</div>,
+	);
+}
+
+const observer = new MutationObserver(([{target}]) => {
+	if (target instanceof Element && !elementExists('.js-details-dialog-spinner', target)) {
+		improveShortcutHelpLegacy(target);
+		observer.disconnect();
+	}
+});
+
+function observeShortcutModal({key, target}: KeyboardEvent): void {
+	if (key !== '?' || isEditable(target)) {
+		return;
+	}
+
+	const modal = $optional('body > details:not(.js-command-palette-dialog) > details-dialog');
+	if (modal) {
+		observer.observe(modal, {childList: true});
+	}
+}
+
+function initOnce(): void {
+	document.body.addEventListener('keypress', observeShortcutModal);
+}
+
+// TODO: Remove everything above in 2026
 function improveShortcutHelp(columnsContainer: HTMLElement): void {
 	const lastColumn = columnsContainer.lastElementChild!;
 	const shortcutsContainer = lastColumn.firstElementChild!.cloneNode(true);
@@ -44,7 +97,7 @@ function init(signal: AbortSignal): void {
 }
 
 void features.add(import.meta.url, {
-	init,
+	init: [init, onetime(initOnce)],
 });
 
 /*
