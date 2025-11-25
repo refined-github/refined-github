2a3521e848348dfb3406f2c95ab760e4e88d6ff7
Grigory <techuila.sunset@gmail.com>
2025-11-25 07:01:34 +0500

    `improve-shortcut-help` - Optimize

--- source/features/improve-shortcut-help.tsx
+++ source/features/improve-shortcut-help.tsx
@@ -1,6 +1,7 @@
 import React from 'dom-chef';
 import {elementExists} from 'select-dom';
 import {$, $optional} from 'select-dom/strict.js';
+import memoize from 'memoize';
 
 import onetime from '../helpers/onetime.js';
 import features from '../feature-manager.js';
@@ -58,38 +59,53 @@ function initOnce(): void {
 }
 
 // TODO: Remove everything above in 2026
+const getRghShortcutsElement = memoize(
+	(baseShortcutsContainer: Element): Element => {
+		const rghShortcutsContainer = baseShortcutsContainer.cloneNode(true);
+		const shortcutsList = $('ul', rghShortcutsContainer);
+		const shortcutItem = $('[class^="ShortcutsGroupList-module__ShortcutItem"]', shortcutsList);
+		const keybindingHint = $('kbd', shortcutItem);
+		const chord = $('span', shortcutItem);
+
+		$('h2', rghShortcutsContainer).textContent = 'Refined GitHub';
+		shortcutsList.replaceChildren(
+			...[...shortcutMap]
+				.toSorted(([, a], [, b]) => a.localeCompare(b))
+				.map(([hotkey, description]) => {
+					const currentItem = shortcutItem.cloneNode(true);
+					currentItem.firstElementChild!.textContent = description;
+					currentItem.lastElementChild!.replaceChildren(
+						<kbd className={keybindingHint.className}>
+							{hotkey.split(' ').map((key, index) => (
+								<>
+									{index > 0 && ' '}
+									<span className={chord.className}>
+										{key.charAt(0).toUpperCase() + key.slice(1)}
+									</span>
+								</>
+							))}
+						</kbd>,
+					);
+					return currentItem;
+				}),
+		);
+
+		return rghShortcutsContainer;
+	},
+	{
+		cacheKey: () => location.origin + location.pathname,
+	},
+
+);
+
 function improveShortcutHelp(columnsContainer: HTMLElement): void {
+	if (shortcutMap.size === 0) {
+		features.unload(import.meta.url);
+		return;
+	}
+
 	const lastColumn = columnsContainer.lastElementChild!;
-	const shortcutsContainer = lastColumn.firstElementChild!.cloneNode(true);
-	const shortcutsList = $('ul', shortcutsContainer);
-	const shortcutItem = $('[class^="ShortcutsGroupList-module__ShortcutItem"]', shortcutsList);
-	const keybindingHint = $('kbd', shortcutItem);
-	const chord = $('span', shortcutItem);
-
-	$('h2', shortcutsContainer).textContent = 'Refined GitHub';
-	shortcutsList.replaceChildren(
-		...[...shortcutMap]
-			.toSorted(([, a], [, b]) => a.localeCompare(b))
-			.map(([hotkey, description]) => {
-				const currentItem = shortcutItem.cloneNode(true);
-				currentItem.firstElementChild!.textContent = description;
-				currentItem.lastElementChild!.replaceChildren(
-					<kbd className={keybindingHint.className}>
-						{hotkey.split(' ').map((key, index) => (
-							<>
-								{index > 0 && ' '}
-								<span className={chord.className}>
-									{key.charAt(0).toUpperCase() + key.slice(1)}
-								</span>
-							</>
-						))}
-					</kbd>,
-				);
-				return currentItem;
-			}),
-	);
-
-	lastColumn.append(shortcutsContainer);
+	lastColumn.append(getRghShortcutsElement(lastColumn.firstElementChild!));
 }
 
 function init(signal: AbortSignal): void {
