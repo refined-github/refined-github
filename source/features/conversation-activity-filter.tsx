455e60f001096a7a7fc74fc2f6615406e5e6fadd
Grigory <techuila.sunset@gmail.com>
2025-12-01 15:05:13 +0500

    Workaround for #6554

--- source/features/conversation-activity-filter.tsx
+++ source/features/conversation-activity-filter.tsx
@@ -265,6 +265,9 @@ async function init(signal: AbortSignal): Promise<void> {
 void features.add(import.meta.url, {
 	include: [
 		pageDetect.isConversation,
+		// Workaround for #6554
+		// TODO: remove once the issue is resolved
+		pageDetect.isIssueOrPRList,
 	],
 	shortcuts: {
 		h: 'Cycle through conversation activity filters',
