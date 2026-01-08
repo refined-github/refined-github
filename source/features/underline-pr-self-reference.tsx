4dbcee15ba6155a8cb530e57cfd789337620ed08
Harrish92 <harrishan.raveendran@epfl.ch>
2026-01-08 23:31:00 +0100

    refactor: use granular selectors

--- source/features/underline-pr-self-reference.tsx
+++ source/features/underline-pr-self-reference.tsx
@@ -3,31 +3,21 @@ import * as pageDetect from 'github-url-detection';
 import features from '../feature-manager.js';
 import observe from '../helpers/selector-observer.js';
 import {getConversationNumber} from '../github-helpers/index.js';
-import getTextNodes from '../helpers/get-text-nodes.js';
 
 function underlinePrSelfReference(prConversation: HTMLElement): void {
 	const prNumber = getConversationNumber();
 
-	const textNodesOnThisLine = getTextNodes(prConversation);
-	for (const [, textNode] of textNodesOnThisLine.entries()) {
-		const text = textNode.textContent;
-		const prLink = textNode.parentElement;
-		const prSelfReference = '#' + prNumber!.toString();
+	const prLink = prConversation.textContent;
+	const prSelfReference = '#' + prNumber!.toString();
 
-		if (prLink !== null && text.includes(prSelfReference)) {
-			prLink.style.textDecoration = 'underline wavy red';
-		}
+	if (prLink.includes(prSelfReference)) {
+		prConversation.style.textDecoration = 'underline wavy red';
 	}
 }
 
 function init(signal: AbortSignal): void {
 	observe([
-		'.comment-body',
-		'.markdown-body',
-		'.js-comment-body',
-		'.soft-wrap',
-		'.user-select-contain',
-		'.d-block',
+		'.issue-link',
 	], underlinePrSelfReference, {signal});
 }
 
