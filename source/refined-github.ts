1f44586bb8692270582dc1d2bbe7e77712522808
Harrish92 <harrishan.raveendran@epfl.ch>
2026-01-10 21:00:32 +0100

    refactor: rename the feature `no-self-reference`

--- source/refined-github.ts
+++ source/refined-github.ts
@@ -222,4 +222,4 @@ import './features/same-page-links.js';
 import './features/linkify-text.js';
 import './features/actions-run-removal.js';
 import './features/new-milestone-button.js';
-import './features/underline-pr-self-reference.js';
+import './features/no-self-reference.js';
