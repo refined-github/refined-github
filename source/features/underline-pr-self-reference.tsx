a0ef8235eedd55bc3b9c00d72c2e220a3cc27787
Harrish92 <harrishan.raveendran@epfl.ch>
2026-01-10 16:14:59 +0100

    feat: extend feature to issues

--- source/features/underline-pr-self-reference.tsx
+++ source/features/underline-pr-self-reference.tsx
@@ -3,18 +3,21 @@ import * as pageDetect from 'github-url-detection';
 import features from '../feature-manager.js';
 import observe from '../helpers/selector-observer.js';
 
-function underlinePrSelfReference(prLink: HTMLElement): void {
-	prLink.style.textDecoration = 'underline wavy red';
-	prLink.title = 'Link is a self-reference';
+function underlineSelfReference(link: HTMLElement): void {
+	link.style.textDecoration = 'underline wavy red';
+	link.title = 'Link is a self-reference';
 }
 
 function init(signal: AbortSignal): void {
-	observe(`.issue-link[href="${location.href.split('#')[0]}"]`, underlinePrSelfReference, {signal});
+	observe([
+		`.issue-link[href="${location.href.split('#')[0]}"]`,
+	], underlineSelfReference, {signal});
 }
 
 void features.add(import.meta.url, {
 	include: [
 		pageDetect.isPR,
+		pageDetect.isIssue,
 	],
 	init,
 });
@@ -24,5 +27,6 @@ void features.add(import.meta.url, {
 ## Test URLs
 
 https://github.com/refined-github/sandbox/pull/120
+https://github.com/refined-github/sandbox/issues/122
 
 */
