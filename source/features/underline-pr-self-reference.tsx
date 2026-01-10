dec1e3d4ead4dce7ccd78183c9d3b2f88c125e55
Harrish92 <harrishan.raveendran@epfl.ch>
2026-01-10 15:28:39 +0100

    fix: underline pr self reference only

--- source/features/underline-pr-self-reference.tsx
+++ source/features/underline-pr-self-reference.tsx
@@ -2,21 +2,14 @@ import * as pageDetect from 'github-url-detection';
 
 import features from '../feature-manager.js';
 import observe from '../helpers/selector-observer.js';
-import {getConversationNumber} from '../github-helpers/index.js';
 
-function underlinePrSelfReference(prConversation: HTMLElement): void {
-	const prNumber = getConversationNumber();
-
-	const prLink = prConversation.textContent;
-	const prSelfReference = '#' + prNumber!.toString();
-
-	if (prLink.includes(prSelfReference)) {
-		prConversation.style.textDecoration = 'underline wavy red';
-	}
+function underlinePrSelfReference(prLink: HTMLElement): void {
+		prLink.style.textDecoration = 'underline wavy red';
+		prLink.title = "Link is a self-reference";
 }
 
 function init(signal: AbortSignal): void {
-	observe('.issue-link', underlinePrSelfReference, {signal});
+	observe(`.issue-link[href="${location.href.split('#')[0]}"]`, underlinePrSelfReference, {signal});
 }
 
 void features.add(import.meta.url, {
