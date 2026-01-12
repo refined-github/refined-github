866833f2838a7a547036593ded8ba8318fa81d50
Grigory <techuila.sunset@gmail.com>
2026-01-12 22:17:38 +0500

    Fix Copilot mentions

--- source/github-helpers/get-user-avatar.ts
+++ source/github-helpers/get-user-avatar.ts
@@ -2,7 +2,7 @@ import {$optional} from 'select-dom/strict.js';
 import * as pageDetect from 'github-url-detection';
 
 export default function getUserAvatar(username: string, size: number): string | void {
-	let cleanName = username.replace('[bot]', '');
+	let cleanName = username.replace('[bot]', '').toLowerCase();
 
 	if (/[^\w-]/.test(cleanName)) {
 		throw new TypeError(`Expected a username, got ${cleanName}`);
@@ -14,7 +14,7 @@ export default function getUserAvatar(username: string, size: number): string | 
 		return existingAvatar.src;
 	}
 
-	if (cleanName === 'Copilot') {
+	if (cleanName === 'copilot') {
 		cleanName = 'in/1143301';
 	}
 
