a4ee8f877e15fc3a5fad1cef7ab20f5dda1e19d3
Grigory <techuila.sunset@gmail.com>
2026-01-16 10:43:55 +0500

    add comment

--- source/features/small-user-avatars.tsx
+++ source/features/small-user-avatars.tsx
@@ -23,6 +23,7 @@ function addAvatar(link: HTMLElement): void {
 }
 
 function addMentionAvatar(link: HTMLAnchorElement): void {
+	// Don't use textContent #8389
 	const username = link.href.split('/').pop()!;
 	const avatarUrl = getUserAvatarURL(username, 16)!;
 
