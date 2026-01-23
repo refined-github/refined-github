b75599e6f90cb702b7bc6b4c807b2dcbcf3470c5
Shrawani Nanda <shrawani.nanda@students.iiit.ac.in>
2026-01-23 23:41:44 +0530

    Add support for commit linkbacks to `same-page-links`

--- source/features/same-page-links.tsx
+++ source/features/same-page-links.tsx
@@ -11,6 +11,7 @@ function init(signal: AbortSignal): void {
 	observe([
 		'[data-testid="state-reason-link"] + [target="_blank"]', // "Closing issue" link
 		'[data-testid="issue-metadata-fixed"] [target="_blank"]', // Reference PR link in issue header
+		'[data-testid^="timeline-row-border"] [target="_blank"]', // Commit linkbacks in PRs
 	], fix, {signal});
 }
 
@@ -27,5 +28,5 @@ Test URLs
 
 - "Closing issue" link https://github.com/refined-github/refined-github/issues/8346#event-16947543136
 - "Reference PR link" https://github.com/refined-github/refined-github/pull/8367
-
+- "Commit Linkbacks" https://github.com/sindresorhus/np/issues/82#event-22200037448, https://github.com/cli/cli/issues/10238
 */
