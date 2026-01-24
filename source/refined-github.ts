87598c06f44c686bcb07b3ea0d497687c7142e65
Harrishan Raveendran <108529751+Harrish92@users.noreply.github.com>
2026-01-24 11:01:03 +0100

    `no-self-reference` - New feature (#8854)
    Co-authored-by: fregante <me@fregante.com>

--- source/refined-github.ts
+++ source/refined-github.ts
@@ -221,3 +221,4 @@ import './features/same-page-links.js';
 import './features/linkify-text.js';
 import './features/actions-run-removal.js';
 import './features/new-milestone-button.js';
+import './features/no-self-reference.js';
