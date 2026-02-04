e60c67f7e352e7afbfc66d3a2495df1d963f6c01
Grigory <techuila.sunset@gmail.com>
2026-02-04 09:54:47 +0500

    fix link disappearing when navigating to the page from a PR

--- source/features/linkify-symbolic-links.tsx
+++ source/features/linkify-symbolic-links.tsx
@@ -1,17 +1,21 @@
 import React from 'dom-chef';
-import {$, $optional} from 'select-dom/strict.js';
+import {elementExists} from 'select-dom';
 import * as pageDetect from 'github-url-detection';
 
 import {wrap} from '../helpers/dom-utils.js';
 import {prependAnchorsBeforeCodeOverlay} from '../github-helpers/dom-formatters.js';
+import observe from '../helpers/selector-observer.js';
 import features from '../feature-manager.js';
 
-function init(): void {
-	if ($optional('[class*="CodeSizeDetails-module__PrimerLink"]')?.textContent === 'Symbolic Link') {
-		const line = $('.react-code-line-contents');
+function linkify(line: HTMLElement): void {
+	if (elementExists('a[class*="CodeSizeDetails-module__PrimerLink"]')) {
 		wrap(line.firstChild!, <a href={line.textContent} data-turbo-frame="repo-content-turbo-frame" />);
 		prependAnchorsBeforeCodeOverlay(line);
 	}
+}
+
+function init(signal: AbortSignal): void {
+	observe('.react-code-line-contents', linkify, {signal});
 }
 
 void features.add(import.meta.url, {
@@ -21,8 +25,6 @@ void features.add(import.meta.url, {
 	exclude: [
 		pageDetect.isRepoFile404,
 	],
-	deduplicate: 'has-rgh',
-	awaitDomReady: true, // Small page
 	init,
 });
 
