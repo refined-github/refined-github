02baf2f2b3176019a447c4ce27e0ae14df5f2152
Pepe Fagoaga <pepe@prowler.com>
2026-03-07 20:17:11 +0100

    fix: use form submit event instead of button click

--- source/features/confirm-release.tsx
+++ source/features/confirm-release.tsx
@@ -3,14 +3,14 @@ import delegate, {type DelegateEvent} from 'delegate-it';
 
 import features from '../feature-manager.js';
 
-function handleClick(event: DelegateEvent<MouseEvent, HTMLButtonElement>): void {
-	if (!confirm('Are you sure you want to publish this release?')) {
+function handleSubmit(event: DelegateEvent<SubmitEvent, HTMLFormElement>): void {
+	if (event.submitter?.matches('[publish-release="true"]') && !confirm('Are you sure you want to publish this release?')) {
 		event.preventDefault();
 	}
 }
 
 function init(signal: AbortSignal): void {
-	delegate('button[publish-release="true"]', 'click', handleClick, {signal});
+	delegate('form', 'submit', handleSubmit, {signal});
 }
 
 void features.add(import.meta.url, {
