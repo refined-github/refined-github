25a21a3554b24e553a845d912b461b75af5254ef
fregante <me@fregante.com>
2026-03-08 09:00:54 +0900

    Follow review partially

--- source/features/confirm-release.tsx
+++ source/features/confirm-release.tsx
@@ -4,13 +4,13 @@ import delegate, {type DelegateEvent} from 'delegate-it';
 import features from '../feature-manager.js';
 
 function handleSubmit(event: DelegateEvent<SubmitEvent, HTMLFormElement>): void {
-	if (event.submitter?.matches('[publish-release="true"]') && !confirm('Are you sure you want to publish this release?')) {
+	if (!confirm('Are you sure you want to publish this release?')) {
 		event.preventDefault();
 	}
 }
 
 function init(signal: AbortSignal): void {
-	delegate('form', 'submit', handleSubmit, {signal});
+	delegate('form TODO:fix this', 'submit', handleSubmit, {signal});
 }
 
 void features.add(import.meta.url, {
