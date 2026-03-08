85718270c4a01ab38fa85965e2580dceffb9a1b3
Grigory <techuila.sunset@gmail.com>
2026-03-08 12:57:41 +0500

    listen for keydown and click events instead of form submit

--- source/features/confirm-release.tsx
+++ source/features/confirm-release.tsx
@@ -3,14 +3,29 @@ import delegate, {type DelegateEvent} from 'delegate-it';
 
 import features from '../feature-manager.js';
 
-function handleSubmit(event: DelegateEvent<SubmitEvent, HTMLFormElement>): void {
+function handleSubmit(event: DelegateEvent<Event, HTMLElement>): void {
 	if (!confirm('Are you sure you want to publish this release?')) {
+		event.stopImmediatePropagation();
 		event.preventDefault();
 	}
 }
 
+function handleSubmitOnCtrlEnter(event: DelegateEvent<KeyboardEvent, HTMLElement>): void {
+	if (event.key === 'Enter' && (event.ctrlKey || event.metaKey)) {
+		handleSubmit(event);
+	}
+}
+
+function handleSubmitOnEnter(event: DelegateEvent<KeyboardEvent, HTMLElement>): void {
+	if (event.key === 'Enter') {
+		handleSubmit(event);
+	}
+}
+
 function init(signal: AbortSignal): void {
-	delegate('form TODO:fix this', 'submit', handleSubmit, {signal});
+	delegate('button[publish-release="true"]', 'click', handleSubmit, {signal, capture: true});
+	delegate('textarea#release_body', 'keydown', handleSubmitOnCtrlEnter, {signal, capture: true});
+	delegate('input#release_name', 'keydown', handleSubmitOnEnter, {signal, capture: true});
 }
 
 void features.add(import.meta.url, {
