21b4e9d8e301f3f0efe1cdd1dd1cc22e87a8eb8e
Grigory <techuila.sunset@gmail.com>
2026-04-01 14:53:59 +0500

    slow-repo-deletion

--- source/features/slow-repo-deletion.tsx
+++ source/features/slow-repo-deletion.tsx
@@ -1,14 +1,21 @@
 import React from 'dom-chef';
 import elementReady from 'element-ready';
+import {$, $optional} from 'select-dom/strict.js';
 import * as pageDetect from 'github-url-detection';
 import TrashIcon from 'octicons-plain-react/Trash';
+import delegate, {type DelegateEvent} from 'delegate-it';
 
 import features from '../feature-manager.js';
-import {buildRepoUrl} from '../github-helpers/index.js';
+import {buildRepoUrl, getForkedRepo, getRepo} from '../github-helpers/index.js';
 import observe from '../helpers/selector-observer.js';
 import {userIsAdmin} from '../github-helpers/get-user-permission.js';
+import {expectTokenScope} from '../github-helpers/github-token.js';
+import addNotice from '../github-widgets/notice-bar.js';
+import api from '../github-helpers/api.js';
+import showToast from '../github-helpers/toast.js';
 
-const tooltip = 'Open repository settings to delete this fork';
+const tooltip = 'Instant deletion: shift-alt-click';
+const buttonHashSelector = '#dialog-show-repo-delete-menu-dialog';
 
 // Only if the repository hasn't been starred
 async function isRepoUnpopular(): Promise<boolean> {
@@ -16,12 +23,66 @@ async function isRepoUnpopular(): Promise<boolean> {
 	return counter!.textContent === '0';
 }
 
+async function deleteRepository(): Promise<void> {
+	const {nameWithOwner} = getRepo()!;
+	await expectTokenScope('delete_repo');
+	await api.v3('/repos/' + nameWithOwner, {
+		method: 'DELETE',
+		json: false,
+	});
+}
+
+async function modifyUiAfterSuccessfulDeletion(): Promise<void> {
+	const {nameWithOwner, owner} = getRepo()!;
+	const forkSource = '/' + getForkedRepo()!;
+	const restoreUrl = pageDetect.isOrganizationRepo()
+		? `/organizations/${owner}/settings/deleted_repositories`
+		: '/settings/deleted_repositories';
+	const otherForksUrl = `/${owner}?tab=repositories&type=fork`;
+
+	await addNotice(
+		<>
+			<TrashIcon />
+			<span>
+				Repository <strong>{nameWithOwner}</strong> deleted. <a href={restoreUrl}>Restore it</a>, <a href={forkSource}>visit the source repo</a>, or see <a href={otherForksUrl}>your other forks.</a>
+			</span>
+		</>,
+		{action: false},
+	);
+	$('.application-main').remove();
+}
+
+async function handleShiftAltClick(event: DelegateEvent<MouseEvent, HTMLElement>): Promise<void> {
+	if (!event.shiftKey || !event.altKey) {
+		return;
+	}
+
+	event.preventDefault();
+
+	// Can't really prevent default, so we must close the dialog if we're on the repo settings page
+	// https://github.com/refined-github/refined-github/pull/7866#issuecomment-2396270060
+	$optional<HTMLDialogElement>('#' + event.delegateTarget.getAttribute('data-show-dialog-id')!)?.close();
+
+	if (confirm('Are you sure you want to delete this repository?')) {
+		await showToast(deleteRepository, {
+			message: 'Deleting repo…',
+			doneMessage: 'Repo deleted',
+		});
+
+		await modifyUiAfterSuccessfulDeletion();
+	}
+}
+
+function addShortcutTooltip(button: HTMLElement): void {
+	button.setAttribute('title', tooltip);
+}
+
 function addButton(header: HTMLElement): void {
 	header.prepend(
 		<li>
 			<a
-				href={buildRepoUrl('settings')}
-				className="btn btn-sm btn-danger rgh-slow-repo-deletion"
+				href={buildRepoUrl('settings', buttonHashSelector)}
+				className="btn btn-sm btn-danger rgh-quick-repo-deletion"
 				title={tooltip}
 			>
 				<TrashIcon className="mr-2" />
@@ -31,8 +92,50 @@ function addButton(header: HTMLElement): void {
 	);
 }
 
+function autoOpenModal(signal: AbortSignal): void {
+	$(buttonHashSelector).click();
+	observe('.js-repo-delete-proceed-confirmation', field => {
+		const notice = (
+			<p className="mt-2 color-fg-muted">
+				Type one character per minute. Your patience will be rewarded with absolutely nothing.
+			</p>
+		);
+
+		field.after(notice);
+
+		let lastInputTime = 0;
+		field.addEventListener('paste', event => {
+			const clipboardEvent = event as ClipboardEvent;
+			const pastedText = clipboardEvent.clipboardData?.getData('text') ?? '';
+			if (pastedText.length > 1) {
+				clipboardEvent.preventDefault();
+			}
+		}, {signal});
+
+		field.addEventListener('beforeinput', event => {
+			const inputEvent = event as InputEvent;
+			if (!inputEvent.inputType.startsWith('insert')) {
+				return;
+			}
+
+			if (Date.now() - lastInputTime < 60_000) {
+				inputEvent.preventDefault();
+				return;
+			}
+
+			lastInputTime = Date.now();
+		}, {signal});
+	}, {signal});
+}
+
 async function initRepoRoot(signal: AbortSignal): Promise<void | false> {
 	observe('.pagehead-actions', addButton, {signal});
+	delegate('.rgh-quick-repo-deletion', 'click', handleShiftAltClick, {signal});
+}
+
+async function initRepoSettings(signal: AbortSignal): Promise<void | false> {
+	delegate(buttonHashSelector, 'click', handleShiftAltClick, {signal});
+	observe(buttonHashSelector, addShortcutTooltip, {signal});
 }
 
 void features.add(import.meta.url, {
@@ -43,6 +146,17 @@ void features.add(import.meta.url, {
 		isRepoUnpopular,
 	],
 	init: initRepoRoot,
+}, {
+	include: [
+		pageDetect.isRepoSettings,
+	],
+	init: initRepoSettings,
+}, {
+	include: [
+		() => location.hash === buttonHashSelector,
+	],
+	awaitDomReady: true, // The expected element is towards the bottom of the page
+	init: autoOpenModal,
 });
 
 /*
@@ -52,6 +166,7 @@ Test URLs:
 1. Fork a repo, like https://github.com/left-pad/left-pad
 2. Star it to see if the "Delete fork" button disappears
 3. Click "Delete fork"
-4. The repository settings page should open instead of immediate deletion
+4. The confirmation dialog should appear
+5. On the last step, the repo name field should let you type only one character per minute
 
 */
