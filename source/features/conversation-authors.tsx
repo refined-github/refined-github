b6f81f9a272189b2442036ec8901055d8ad4ebbd
Federico <me@fregante.com>
2026-04-12 16:37:21 +0700

    `conversation-authors` - Only highlight current user

--- source/features/conversation-authors.tsx
+++ source/features/conversation-authors.tsx
@@ -1,39 +1,10 @@
 import './conversation-authors.css';
 
-import {CachedFunction} from 'webext-storage-cache';
 import * as pageDetect from 'github-url-detection';
 
 import features from '../feature-manager.js';
-import api from '../github-helpers/api.js';
-import {cacheByRepo, getLoggedInUser} from '../github-helpers/index.js';
+import {getLoggedInUser} from '../github-helpers/index.js';
 import observe from '../helpers/selector-observer.js';
-import {expectToken} from '../github-helpers/github-token.js';
-import GetCollaborators from './conversation-authors.gql';
-
-const collaborators = new CachedFunction('repo-collaborators', {
-	async updater(): Promise<string[]> {
-		const {repository} = await api.v4(GetCollaborators);
-		return repository.collaborators.nodes.map((user: Record<string, string>) => user.login);
-	},
-	maxAge: {days: 1},
-	staleWhileRevalidate: {days: 20},
-	cacheKey: cacheByRepo,
-});
-
-async function highlightCollaborators(signal: AbortSignal): Promise<void> {
-	await expectToken();
-	const list = await collaborators.get();
-	observe([
-		// Old issue lists - TODO: Drop in 2026
-		'.js-issue-row [data-hovercard-type="user"]',
-		'a[class^="IssueItem-module__authorCreatedLink"]',
-	], author => {
-		const name = author.textContent.trim();
-		if (list.includes(name) && name !== getLoggedInUser()) {
-			author.classList.add('rgh-collaborator');
-		}
-	}, {signal});
-}
 
 function highlightSelf(signal: AbortSignal): void {
 	// "Opened by {user}" and "Created by {user}"
@@ -47,11 +18,6 @@ function highlightSelf(signal: AbortSignal): void {
 }
 
 void features.add(import.meta.url, {
-	include: [
-		pageDetect.isRepoIssueOrPRList,
-	],
-	init: highlightCollaborators,
-}, {
 	include: [
 		pageDetect.isIssueOrPRList,
 	],
