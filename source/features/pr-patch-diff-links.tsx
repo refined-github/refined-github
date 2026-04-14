5be5d06bb94d2d38e36dc73102fe97862648b06b
niroula-kushal <niroulakushal31@gmail.com>
2026-04-14 21:49:35 +0545

    fix: lint issues

--- source/features/pr-patch-diff-links.tsx
+++ source/features/pr-patch-diff-links.tsx
@@ -5,12 +5,9 @@ import DiffIcon from 'octicons-plain-react/Diff';
 
 import features from '../feature-manager.js';
 import {getCleanPathname} from '../github-helpers/index.js';
-
-
 import observe from '../helpers/selector-observer.js';
 
-
-function getPRUrl(extension: 'patch' | 'diff'): string {
+function getPrUrl(extension: 'patch' | 'diff'): string {
 	// Extract the repository path and PR ID
 	const pathname = getCleanPathname();
 	const [owner, repo, , id] = pathname.split('/');
@@ -19,29 +16,29 @@ function getPRUrl(extension: 'patch' | 'diff'): string {
 	return `/${owner}/${repo}/pull/${id}.${extension}`;
 }
 
-function updatePRUrl(
+function updatePrUrl(
 	event: React.FocusEvent<HTMLAnchorElement> | React.MouseEvent<HTMLAnchorElement>,
 ): void {
 	const link = event.currentTarget;
-	link.href = getPRUrl(link.textContent as 'patch' | 'diff');
+	link.href = getPrUrl(link.textContent as 'patch' | 'diff');
 }
 
 function createLink(type: 'patch' | 'diff'): JSX.Element {
 	return (
 		<a
-			href={getPRUrl(type)} // Always points to the consistent PR .diff or .patch URL
+			href={getPrUrl(type)} // Always points to the consistent PR .diff or .patch URL
 			className="sha color-fg-default"
 			// Update URL because it might be out of date due to SPA navigation
 			// https://github.com/refined-github/refined-github/issues/8737
-			onMouseEnter={updatePRUrl}
-			onFocus={updatePRUrl}
+			onMouseEnter={updatePrUrl}
+			onFocus={updatePrUrl}
 		>
 			{type}
 		</a>
 	);
 }
 
-async function addPRPatchDiffLinks(prHeader: HTMLElement): Promise<void> {
+async function addPrPatchDiffLinks(prHeader: HTMLElement): Promise<void> {
 	prHeader.append(
 		<li
 			className="mx-0 tmp-py-3 tmp-px-4 width-full rounded-0"
@@ -54,14 +51,12 @@ async function addPRPatchDiffLinks(prHeader: HTMLElement): Promise<void> {
 					{createLink('diff')}
 				</div>
 			</div>
-		</li>
+		</li>,
 	);
 }
 
 async function init(signal: AbortSignal): Promise<void> {
-	observe([
-		'.react-overview-code-button-action-list > ul'
-	], addPRPatchDiffLinks, {signal});
+	observe('.react-overview-code-button-action-list > ul', addPrPatchDiffLinks, {signal});
 }
 
 void features.add(import.meta.url, {
