f27f03c45f41f6f51da989183df1ddb8a0f132dd
Grigory <techuila.sunset@gmail.com>
2026-04-15 07:54:23 +0500

    linked prs menu

--- source/features/same-page-links.tsx
+++ source/features/same-page-links.tsx
@@ -11,6 +11,7 @@ function init(signal: AbortSignal): void {
 	observe(`a[target="_blank"]:is(${[
 		'[class^="ClosedEvent-module__closerLink"]', // "Closing issue" link
 		'[class^="LinkedPullRequest-module__pullRequestLink"]', // Linked PR links in issue headers
+		'[class^="prc-ActionList-ActionListContent"][aria-keyshortcuts="#"]', // Linked PR links in issue header menu
 		'ul[data-testid="issue-viewer-linked-pr-container"] *', // Linked PR and release links on issue sidebar
 		'[class^="ReferencedEventInner-module__commitHashLink"]', // Commit linkbacks on issue timeline
 		'div[data-testid="list-row-linked-pull-requests"] > *', // Linked PRs on issue list
@@ -32,6 +33,7 @@ Test URLs
 
 - "Closing issue" link: https://github.com/refined-github/refined-github/issues/8346#event-16947543136
 - Linked PR links on issue page: https://github.com/refined-github/refined-github/issues/8346
+- Linked PR links in issue header menu: https://github.com/refined-github/sandbox/issues/130
 - Commit linkbacks: https://github.com/sindresorhus/np/issues/82#event-22200037448, https://github.com/cli/cli/issues/10238
 - Linked PRs on issue list: https://github.com/refined-github/refined-github/issues?q=is%3Aissue%20has%3Alinked%20reason%3Acompleted
 */
