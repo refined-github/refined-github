24916f63591dc8035578a9a27a7ac8fea40120f5
fregante <me@fregante.com>
2026-04-20 18:08:12 +0700

    Better

--- source/features/fit-textareas.tsx
+++ source/features/fit-textareas.tsx
@@ -38,10 +38,16 @@ function watchTextarea(textarea: HTMLTextAreaElement, {signal}: SignalAsOptions)
 }
 
 function init(signal: AbortSignal): void {
-	// `anchored-position`: Exclude PR review box because it's in a `position:fixed` container; The scroll HAS to appear within the fixed element.
-	// `#pull_request_body_ghost`: Special textarea that GitHub just matches to the visible textarea
+	
 	observe(
-		`textarea${not('anchored-position #pull_request_review_body', '#pull_request_body_ghost', '#pull_request_body_ghost_ruler')}`,
+		'textarea' + not(
+			// `anchored-position`: Exclude PR review box because it's in a `position:fixed` container; The scroll HAS to appear within the fixed element.
+			'anchored-position #pull_request_review_body', 
+			
+			// `#pull_request_body_ghost`: Special textarea that GitHub just matches to the visible textarea
+			'#pull_request_body_ghost', 
+			'#pull_request_body_ghost_ruler',
+		),
 		watchTextarea,
 		{signal},
 	);
