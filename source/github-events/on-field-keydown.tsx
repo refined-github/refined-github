0eff266a4624c2856d5ab0f0ceca8c20518df086
Grigory <techuila.sunset@gmail.com>
2026-04-27 00:56:17 +0500

    better names

--- source/github-events/on-field-keydown.tsx
+++ source/github-events/on-field-keydown.tsx
@@ -1,11 +1,11 @@
 import delegate, {type DelegateEventHandler} from 'delegate-it';
 import {elementExists} from 'select-dom';
 
-type DelegateField = HTMLTextAreaElement | HTMLInputElement;
-type DelegateFieldEvent = DelegateEventHandler<KeyboardEvent, DelegateField>;
+type TextField = HTMLTextAreaElement | HTMLInputElement;
+type KeydownHandler = DelegateEventHandler<KeyboardEvent, TextField>;
 
-function onFieldKeydown(selector: string | readonly string[], callback: DelegateFieldEvent, signal: AbortSignal): void {
-	delegate<DelegateField, 'keydown'>(selector, 'keydown', event => {
+function onFieldKeydown(selector: string | readonly string[], callback: KeydownHandler, signal: AbortSignal): void {
+	delegate<TextField, 'keydown'>(selector, 'keydown', event => {
 		const field = event.delegateTarget;
 
 		if (
@@ -26,11 +26,11 @@ function onFieldKeydown(selector: string | readonly string[], callback: Delegate
 	});
 }
 
-export function onCommentFieldKeydown(callback: DelegateFieldEvent, signal: AbortSignal): void {
+export function onCommentFieldKeydown(callback: KeydownHandler, signal: AbortSignal): void {
 	onFieldKeydown('textarea', callback, signal);
 }
 
-export function onConversationTitleFieldKeydown(callback: DelegateFieldEvent, signal: AbortSignal): void {
+export function onConversationTitleFieldKeydown(callback: KeydownHandler, signal: AbortSignal): void {
 	onFieldKeydown([
 		'[class^="prc-PageLayout-Header"] input', // PR
 		'input[placeholder="Title"]', // Issue
@@ -39,6 +39,6 @@ export function onConversationTitleFieldKeydown(callback: DelegateFieldEvent, si
 	], callback, signal);
 }
 
-export function onCommitTitleFieldKeydown(callback: DelegateFieldEvent, signal: AbortSignal): void {
+export function onCommitTitleFieldKeydown(callback: KeydownHandler, signal: AbortSignal): void {
 	onFieldKeydown('#commit-summary-input', callback, signal);
 }
