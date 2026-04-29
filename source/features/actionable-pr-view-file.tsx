30045da790ae74ffbf4e586f3199bdbc03df8b20
Grigory <techuila.sunset@gmail.com>
2026-04-29 20:24:00 +0500

    remove old view support

--- source/features/actionable-pr-view-file.tsx
+++ source/features/actionable-pr-view-file.tsx
@@ -9,7 +9,6 @@ import {
 
 import features from '../feature-manager.js';
 import {getBranches} from '../github-helpers/pr-branches.js';
-import observe from '../helpers/selector-observer.js';
 
 /** Rebuilds the "View file" link to point to the head repo and its branch instead of the base repo and the commit */
 function rebuildFileLink(viewFileLink: HTMLAnchorElement, filePath: string): void {
@@ -19,11 +18,6 @@ function rebuildFileLink(viewFileLink: HTMLAnchorElement, filePath: string): voi
 	viewFileLink.pathname = [owner, name, 'blob', branch, ...filePath.split('/')]
 		.map(element => encodeURIComponent(element))
 		.join('/');
-}
-
-function alter(viewFileLink: HTMLAnchorElement): void {
-	const filePath = viewFileLink.closest('[data-path]')!.getAttribute('data-path')!;
-	rebuildFileLink(viewFileLink, filePath);
 }
 
 function handleMenuOpening({delegateTarget: menuButton}: DelegateEvent): void {
@@ -63,12 +57,6 @@ function init(signal: AbortSignal): void {
 		// `capture: true` to run before `more-file-links`
 		{capture: true, signal},
 	);
-	// Legacy PR files view -- TODO: Drop after it is removed
-	observe(
-		'.file-header:not([data-file-deleted="true"]) a.dropdown-item[data-ga-click^="View file"]',
-		alter,
-		{signal},
-	);
 }
 
 void features.add(import.meta.url, {
@@ -80,8 +68,6 @@ void features.add(import.meta.url, {
 		pageDetect.isClosedConversation,
 		// If you're viewing changes from partial commits, ensure you're on the latest one.
 		() => pageDetect.isPRCommit() && !elementExists('[aria-label="No next commit"]'),
-		// Legacy PR files view -- TODO: Drop after it is removed
-		() => elementExists('.js-commits-filtered') && !elementExists('[aria-label="You are viewing the latest commit"]'),
 	],
 	awaitDomReady: true, // DOM-based filters, feature is invisible and inactive until dropdown is opened
 	init,
