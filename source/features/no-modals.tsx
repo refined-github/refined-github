cc4fb1ab2bff7d73dda219962a0b19894d3e292e
fregante <me@fregante.com>
2026-05-01 01:30:15 +0700

    `no-modals` - Allow opening issues in new tab from site header (#9311)

--- source/features/no-modals.tsx
+++ source/features/no-modals.tsx
@@ -1,11 +1,31 @@
 import delegate, {type DelegateEvent} from 'delegate-it';
 import * as pageDetect from 'github-url-detection';
+import {isAlteredClick} from 'filter-altered-clicks';
 
 import features from '../feature-manager.js';
+import {buildRepoUrl} from '../github-helpers/index.js';
+import onetime from '../helpers/onetime.js';
 
 function fix(event: DelegateEvent<MouseEvent, HTMLAnchorElement>): void {
 	event.stopImmediatePropagation();
 	event.delegateTarget.removeAttribute('target');
+}
+
+function handleAlteredClick(event: DelegateEvent<MouseEvent, HTMLLIElement>): void {
+	if (isAlteredClick(event)) {
+		event.stopImmediatePropagation();
+		event.preventDefault();
+		window.open(buildRepoUrl('issues/new/choose'), '_blank');
+	}
+}
+
+function initNewIssueInNewTabOnce(): void {
+	delegate(
+		'li[aria-keyshortcuts="n"]:has(.octicon-issue-opened)',
+		'click',
+		handleAlteredClick,
+		{capture: true},
+	);
 }
 
 function init(signal: AbortSignal): void {
@@ -28,6 +48,12 @@ void features.add(import.meta.url, {
 		pageDetect.isGlobalIssueOrPRList,
 	],
 	init,
+}, {
+	include: [
+		pageDetect.isRepo,
+	],
+	// No need to continuously register and unregister the handler
+	init: onetime(initNewIssueInNewTabOnce),
 });
 
 /*
