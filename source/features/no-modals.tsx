3c6dbb973bc5153144f57a3fbbceb0c8dcf578e7
Federico <me@fregante.com>
2026-04-30 22:11:56 +0700

    Update no-modals.tsx

--- source/features/no-modals.tsx
+++ source/features/no-modals.tsx
@@ -1,14 +1,31 @@
 import delegate, {type DelegateEvent} from 'delegate-it';
 import * as pageDetect from 'github-url-detection';
 
+import {isAlteredClick} from 'filter-altered-clicks';
+
 import features from '../feature-manager.js';
+import {buildRepoUrl} from '../github-helpers/index.js';
 
 function fix(event: DelegateEvent<MouseEvent, HTMLAnchorElement>): void {
 	event.stopImmediatePropagation();
 	event.delegateTarget.removeAttribute('target');
 }
 
+function handleAlteredClick(event: DelegateEvent<MouseEvent, HTMLLIElement>): void {
+	if (isAlteredClick(event)) {
+		event.stopImmediatePropagation();
+		event.preventDefault();
+		window.open(buildRepoUrl('issues/new/choose'), '_blank');
+	}
+}
+
 function init(signal: AbortSignal): void {
+	delegate(
+		'li[aria-keyshortcuts="n"]:has(.octicon-issue-opened)',
+		'click',
+		handleAlteredClick,
+		{signal, capture: true},
+	);
 	delegate(
 		[
 			'a[href$="/issues/new/choose"]', // New issue button
