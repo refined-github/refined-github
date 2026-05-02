7b9a915cf4197466c053c81cee42ec7973556e63
agriya khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2026-05-02 20:25:14 +0530

    `clear-pr-merge-commit-message` - Add Undo button (#9337)
    Co-authored-by: Grigory <techuila.sunset@gmail.com>

--- source/features/clear-pr-merge-commit-message.tsx
+++ source/features/clear-pr-merge-commit-message.tsx
@@ -15,32 +15,37 @@ const isPrAgainstDefaultBranch = async (): Promise<boolean> => getBranches().bas
 
 async function clear(messageField: HTMLTextAreaElement): Promise<void> {
 	const originalMessage = messageField.value;
-	const cleanedMessage = cleanCommitMessage(originalMessage, !await isPrAgainstDefaultBranch());
+	let cleanedMessage = cleanCommitMessage(originalMessage, !await isPrAgainstDefaultBranch());
 
 	if (cleanedMessage === originalMessage.trim()) {
 		return;
 	}
 
+	cleanedMessage = cleanedMessage ? cleanedMessage + '\n' : '';
 	// Do not use `text-field-edit` #6348
-	messageField.value = cleanedMessage ? cleanedMessage + '\n' : '';
+	messageField.value = cleanedMessage;
 
-	// Trigger `fit-textareas` if enabled
-	messageField.dispatchEvent(new Event('input', {bubbles: true}));
+	let isUndoing = false;
+	function toggleUndoRedo({currentTarget}: React.MouseEvent<HTMLButtonElement>): void {
+		isUndoing = !isUndoing;
+		messageField.value = isUndoing ? originalMessage : cleanedMessage;
+		currentTarget.textContent = isUndoing ? 'Redo' : 'Undo';
+	}
 
 	const anchor = $closest('div[data-has-label]', messageField);
-
 	attachElement(anchor, {
 		after: () => (
 			<div className="flex-self-stretch">
 				<p className="note">
-					The description field was cleared by{' '}
+					The description field was{' '}
 					<a
 						target="_blank"
 						href="https://github.com/refined-github/refined-github/wiki/Extended-feature-descriptions#clear-pr-merge-commit-message"
 						rel="noreferrer"
 					>
-						Refined GitHub
-					</a>.
+						cleared
+					</a>
+					{' '}by Refined GitHub. <button type="button" className="btn-link" onClick={toggleUndoRedo}>Undo</button>
 				</p>
 			</div>
 		),
