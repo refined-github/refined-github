4f2ead534b1b6467dcdc05691f926815adca47ab
Grigory <techuila.sunset@gmail.com>
2026-05-07 17:05:10 +0500

    minor changes

--- source/helpers/on-altered-click.ts
+++ source/helpers/on-altered-click.ts
@@ -13,19 +13,18 @@ export default function onAlteredClick<Selector extends string>(
 		}
 	};
 
-	const auxClickListener: typeof clickListener = event => {
+	const auxClickListener: typeof callback = event => {
 		// Is middle click
 		if (event.button === 1) {
 			callback(event);
 		}
 	};
 
-	// Prevents auto-scrolling
-	const mousedownListener = (event: MouseEvent): void => {
+	const preventAutoScrolling = (event: MouseEvent): void => {
 		event.preventDefault();
 	};
 
 	delegate(selector, 'click', clickListener, options);
 	delegate(selector, 'auxclick', auxClickListener, options);
-	delegate(selector, 'mousedown', mousedownListener, {...options, capture: true});
+	delegate(selector, 'mousedown', preventAutoScrolling, {...options, capture: true});
 }
