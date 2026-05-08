8a74fcc1d28447b4c25faeb92f5eec1c138a08df
Federico <me@fregante.com>
2026-05-08 22:02:54 +0700

    Finalize + update `pr-approvals-count`

--- source/features/linkify-pr-review-state.tsx
+++ source/features/linkify-pr-review-state.tsx
@@ -3,32 +3,31 @@ import * as pageDetect from 'github-url-detection';
 import features from '../feature-manager.js';
 import SearchQuery from '../github-helpers/search-query.js';
 import observe from '../helpers/selector-observer.js';
+import {buildRepoUrl} from '../github-helpers/index.js';
 
 const reviewStateFilters = new Map([
-	[/^Draft$/, 'draft:true'],
-	[/^Changes requested$/, 'review:changes-requested'],
-	[/review approval/, 'review:approved'], // "1 review approval", "2 review approvals"
+	['Draft', 'draft:true'],
+	['Changes requested', 'review:changes-requested'],
+	['Approved', 'review:approved'],
+	['Review required', 'review:required'],
 
 	// Not shown currently
 	// [/^Awaiting review by you$/, 'review-requested:@me'],
 ]);
 
-function linkify(link: HTMLAnchorElement): void {
-	const text = link.textContent.trim() ?? '';
-	for (const [pattern, query] of reviewStateFilters) {
-		if (pattern.test(text)) {
-			link.href = SearchQuery.from(location).append(query).href;
+function alterLink(link: HTMLAnchorElement): void {
+	for (const [text, filter] of reviewStateFilters) {
+		// Use .textContent because "Draft" lacks any unique attributes
+		if (link.textContent.trim() === text) {
+			link.href = new SearchQuery(buildRepoUrl('pulls')).append(filter).href;
 			return;
 		}
 	}
 }
 
 function init(signal: AbortSignal): void {
-	observe(
-		'.js-issue-row .d-none.d-md-inline-flex a.Link--muted[href*="#"]',
-		linkify,
-		{signal},
-	);
+	// Note: This feature alters the `href` so this selector cannot be used by any other features
+	observe('.js-issue-row .text-small a[href$="#partial-pull-merging"]', alterLink, {signal});
 }
 
 void features.add(import.meta.url, {
