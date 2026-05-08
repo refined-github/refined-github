ef8e78b02632e4163e36432563d7bc75c8dfb475
Federico <me@fregante.com>
2026-05-08 23:42:31 +0700

    Format

--- source/helpers/rgh-related-issues-count.svelte
+++ source/helpers/rgh-related-issues-count.svelte
@@ -1,44 +1,44 @@
 <script lang="ts">
-import pluralize from './pluralize.js';
-import {getFeatureRelatedIssuesUrl} from './rgh-links.js';
-import getOpenRelatedIssuesCount from './rgh-related-issues-count.js';
+	import pluralize from './pluralize.js';
+  import { getFeatureRelatedIssuesUrl } from "./rgh-links.js";
+  import getOpenRelatedIssuesCount from "./rgh-related-issues-count.js";
 
-const {
-featureId,
-linkify = false,
-single,
-plural,
-zero,
-loading,
-}: {
-featureId: string;
-linkify?: boolean;
-single: string;
-plural?: string;
-zero?: string;
-loading?: string;
-} = $props();
+  const {
+    featureId,
+    linkify = false,
+    single,
+    plural,
+    zero,
+    loading,
+  }: {
+    featureId: string;
+    linkify?: boolean;
+    single: string;
+    plural?: string;
+    zero?: string;
+    loading?: string;
+  } = $props();
 
-let count = $state<number | undefined>(undefined);
-const relatedIssuesHref = $derived(getFeatureRelatedIssuesUrl(featureId).href);
+  let count = $state<number | undefined>(undefined);
+  const relatedIssuesHref = $derived(getFeatureRelatedIssuesUrl(featureId).href);
 
-$effect(() => {
-(async () => {
-count = await getOpenRelatedIssuesCount(featureId);
-})();
-});
+  $effect(() => {
+    (async () => {
+      count = await getOpenRelatedIssuesCount(featureId);
+    })();
+  });
 </script>
 
 {#if count === undefined}
-{#if loading !== undefined}{loading}{/if}
+	{#if loading !== undefined}{loading}{/if}
 {:else if count === 0}
-{#if zero !== undefined}{zero}{/if}
+	{#if zero !== undefined}{zero}{/if}
 {:else if linkify}
-<a
-class="Link--muted"
-href={relatedIssuesHref}
-data-turbo-frame="repo-content-turbo-frame"
->{pluralize(count, single, plural)}</a>
+	<a
+		class="Link--muted"
+		href={relatedIssuesHref}
+		data-turbo-frame="repo-content-turbo-frame"
+	>{pluralize(count, single, plural)}</a>
 {:else}
-{pluralize(count, single, plural)}
+	{pluralize(count, single, plural)}
 {/if}
