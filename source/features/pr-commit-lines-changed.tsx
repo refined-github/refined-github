b1d0c0b3f3921481aece16a34862c13303692bcd
fregante <me@fregante.com>
2026-05-16 20:23:56 +0800

    Update pr-commit-lines-changed.tsx
    Co-authored-by: Grigory <techuila.sunset@gmail.com>

--- source/features/pr-commit-lines-changed.tsx
+++ source/features/pr-commit-lines-changed.tsx
@@ -35,7 +35,7 @@ async function add(commitHash: HTMLElement): Promise<void> {
 	commitHash.prepend(
 		tooltipped(
 			tooltip,
-			<span className="ml-2 diffstat">
+			<span className="ml-2 d-md-block d-none diffstat">
 				<span className="color-fg-success">+{additions}</span>
 				{' '}
 				<span className="color-fg-danger">−{deletions}</span>
