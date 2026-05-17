91f119137de39bd557a618bc24e0e1e1abd3adf9
Ariel Eli <arieleli@MacBook-Air-sl-Ariel.local>
2026-05-17 20:59:26 +0300

    Add visible cmd-enter shortcut

--- source/features/cmd-enter.tsx
+++ source/features/cmd-enter.tsx
@@ -1,9 +1,27 @@
 import delegate, {type DelegateEvent} from 'delegate-it';
+import React from 'dom-chef';
 import * as pageDetect from 'github-url-detection';
-import {$, $optional} from 'select-dom';
+import {$, $optional, elementExists} from 'select-dom';
 
 import features from '../feature-manager.js';
+import {moduleKeySymbol} from '../github-helpers/hotkey.js';
 import {legacyCommentField} from '../github-helpers/selectors.js';
+import observe from '../helpers/selector-observer.js';
+
+function addShortcut(reopenButton: HTMLButtonElement): void {
+	const submitButton = $('.btn-primary[type="submit"]', reopenButton.form!);
+	if (elementExists('.rgh-cmd-enter-shortcut', submitButton)) {
+		return;
+	}
+
+	submitButton.append(
+		' ',
+		<span className="rgh-cmd-enter-shortcut text-normal" aria-hidden="true">
+			{moduleKeySymbol}
+			↵
+		</span>,
+	);
+}
 
 function handleKeyDown(event: DelegateEvent<KeyboardEvent>): void {
 	if (event.key !== 'Enter' || !(event.metaKey || event.ctrlKey)) {
@@ -20,6 +38,7 @@ function handleKeyDown(event: DelegateEvent<KeyboardEvent>): void {
 }
 
 function init(signal: AbortSignal): void {
+	observe('button[name="comment_and_open"]:disabled', addShortcut, {signal});
 	delegate(legacyCommentField, 'keydown', handleKeyDown, {signal});
 }
 
