5cc7da54d6f6bf544823e71ae05ae4ac9a01e9ca
Grigory <techuila.sunset@gmail.com>
2026-05-19 22:42:05 +0500

    fixes

--- source/features/linkify-line-numbers.tsx
+++ source/features/linkify-line-numbers.tsx
@@ -1,13 +1,13 @@
 import * as pageDetect from 'github-url-detection';
 import {$closest} from 'select-dom';
 import delegate, {type DelegateEvent} from 'delegate-it';
+import {isAlteredClick} from 'filter-altered-clicks';
 
 import features from '../feature-manager.js';
-import {isMiddleClick, isAlteredPrimaryClick} from '../helpers/on-altered-click.js';
+import onAlteredClick from '../helpers/on-altered-click.js';
 import observe from '../helpers/selector-observer.js';
 
-function openLinkToLine(event: DelegateEvent<PointerEvent, HTMLElement>): void {
-	const lineNumberCell = event.delegateTarget;
+function getLinkToLine(lineNumberCell: HTMLElement): string {
 	const {lineNumber} = lineNumberCell.dataset;
 	if (!lineNumber) {
 		throw new Error('Expected the cell to have the `data-line-number` attribute');
@@ -16,15 +16,19 @@ function openLinkToLine(event: DelegateEvent<PointerEvent, HTMLElement>): void {
 	const fileLink = $closest(['.Box', '.review-thread-component'], lineNumberCell)
 		.querySelector(['a[href*="#L"]', 'a[href*="#diff-"]'])!;
 
-	const lineUrl = fileLink.hash.startsWith('#diff-')
+	return fileLink.hash.startsWith('#diff-')
 		? fileLink.pathname + fileLink.hash + `R${lineNumber}`
 		: fileLink.pathname + `#L${lineNumber}`;
+}
 
-	if (isMiddleClick(event) || isAlteredPrimaryClick(event)) {
-		window.open(lineUrl, '_blank');
-	} else {
-		location.href = lineUrl;
+function openLinkToLine(event: DelegateEvent<PointerEvent, HTMLElement>): void {
+	if (!isAlteredClick(event)) {
+		location.href = getLinkToLine(event.delegateTarget);
 	}
+}
+
+function openLinkToLineInNewTab(event: DelegateEvent<PointerEvent, HTMLElement>): void {
+	window.open(getLinkToLine(event.delegateTarget), '_blank');
 }
 
 function visuallyLinkify(lineNumberCell: HTMLElement): void {
@@ -35,7 +39,8 @@ const lineNumberCellSelector = 'td.blob-num:not(.blob-num-hunk, .empty-cell)';
 
 function init(signal: AbortSignal): void {
 	observe(lineNumberCellSelector, visuallyLinkify, {signal});
-	delegate(lineNumberCellSelector, ['click', 'auxclick'], openLinkToLine, {signal});
+	delegate(lineNumberCellSelector, 'click', openLinkToLine, {signal});
+	onAlteredClick(lineNumberCellSelector, openLinkToLineInNewTab, {signal});
 }
 
 void features.add(import.meta.url, {
