ab9c600a17bd537391bfb12cae5375e34aa4ff9e
Federico <me@fregante.com>
2026-05-21 14:38:35 +0800

    Lint

--- source/features/extensible-nav.tsx
+++ source/features/extensible-nav.tsx
@@ -1,33 +1,35 @@
 import React from 'react';
 import * as pageDetect from 'github-url-detection';
 
+import {$$, $optional} from 'select-dom';
+
 import features from '../feature-manager.js';
 import observe from '../helpers/selector-observer.js';
-import {$$, $optional} from 'select-dom';
 
 function regenerate(nativeNav: HTMLElement): void {
 	const items = $$('a', nativeNav);
 	nativeNav.after(
 		<nav className='UnderlineNav px-4'>
-		<ul className='UnderlineNav-body'>
-			{items.map(item => {
-				const label = ($optional('[data-component="text"]', item) ?? item).textContent;
-				const counter = $optional('[data-component="counter"] [data-variant="secondary"]', item)?.textContent;
-				const selectedClass = item.hasAttribute('aria-current') ? 'selected' : '';
-				return (
-					<li key={item.href}>
-						<a href={item.href} className={'UnderlineNav-item ' + selectedClass}>
-							{label}
-							{counter && (
-								<span className='Counter'>{counter}</span>
-							)}
-						</a>
-					</li>
-				);
-			})}
-		</ul>
-		</nav>
-	)
+			<ul className='UnderlineNav-body'>
+				{items.map(item => {
+					const label = ($optional('[data-component="text"]', item) ?? item).textContent;
+					// Only a few items have counters
+					const counter = $optional('[data-component="counter"] [data-variant="secondary"]', item)?.textContent;
+					const selectedClass = item.hasAttribute('aria-current') ? 'selected' : '';
+					return (
+						<li key={item.href}>
+							<a href={item.href} className={'UnderlineNav-item ' + selectedClass}>
+								{label}
+								{counter && (
+									<span className='Counter'>{counter}</span>
+								)}
+							</a>
+						</li>
+					);
+				})}
+			</ul>
+		</nav>,
+	);
 }
 
 function init(signal: AbortSignal): void {
