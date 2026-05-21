017d141c576395bae0a488fb52531566acd6c3bf
Federico <me@fregante.com>
2026-05-21 15:16:28 +0800

    Current

--- source/features/extensible-nav.tsx
+++ source/features/extensible-nav.tsx
@@ -1,61 +1,77 @@
 import './extensible-nav.css';
 import React from 'react';
 import * as pageDetect from 'github-url-detection';
-
-import {$$, $optional} from 'select-dom';
+import {$, $$, $optional} from 'select-dom';
+import elementReady from 'element-ready';
 
 import features from '../feature-manager.js';
-import observe from '../helpers/selector-observer.js';
+import onetime from '../helpers/onetime';
 
-function regenerate(nativeNav: HTMLElement): void {
+let ready = false;
+
+function generateTab(item: HTMLAnchorElement): JSX.Element {
+	const label = ($optional('[data-component="text"]', item) ?? item).textContent;
+	// Only a few items have counters
+	const counter = $optional('[data-component="counter"] [data-variant="secondary"]', item)?.textContent;
+	const selectedClass = item.hasAttribute('aria-current') ? 'selected' : '';
+
+	// The icon may be missing if the feature runs too late and the link is found in the dropdown
+	let icon = $optional('[data-component="icon"]', item);
+	if (icon) {
+		icon = icon.cloneNode(true);
+		icon.classList.add(
+			// This class comes after d-none utility classes so they can't override it
+			'UnderlineNav-octicon',
+		);
+	}
+
+	return (
+		<li key={item.href}>
+			<a href={item.href} className={'UnderlineNav-item ' + selectedClass}>
+				{icon}
+				{label}
+				{counter && (
+					<span className='Counter'>{counter}</span>
+				)}
+			</a>
+		</li>
+	);
+}
+
+async function initOnce(): Promise<void> {
+	const nativeNav = (await elementReady('nav[aria-label="Repository"]'))!;
 	const items = $$('a', nativeNav);
-	nativeNav.after(
+	nativeNav.before(
 		<nav className='UnderlineNav rgh-extensible-nav px-4'>
 			<ul className='UnderlineNav-body'>
-				{items.map(item => {
-					const label = ($optional('[data-component="text"]', item) ?? item).textContent;
-					// Only a few items have counters
-					const counter = $optional('[data-component="counter"] [data-variant="secondary"]', item)?.textContent;
-					const selectedClass = item.hasAttribute('aria-current') ? 'selected' : '';
-
-					// The icon may be missing if the feature runs too late and the link is found in the dropdown
-					let icon = $optional('[data-component="icon"]', item);
-					if (icon) {
-						icon = icon.cloneNode(true);
-						icon.classList.add(
-							// This class comes after d-none utility classes so they can't override it
-							'UnderlineNav-octicon',
-						);
-					}
-
-					return (
-						<li key={item.href}>
-							<a href={item.href} className={'UnderlineNav-item ' + selectedClass}>
-								{icon}
-								{label}
-								{counter && (
-									<span className='Counter'>{counter}</span>
-								)}
-							</a>
-						</li>
-					);
-				})}
+				{items.map(item => generateTab(item))}
 			</ul>
 		</nav>,
 	);
 
-	nativeNav.classList.add('sr-only');
+	// nativeNav.classList.add('sr-only');
+	ready = true;
 }
 
-function init(signal: AbortSignal): void {
-	observe('nav[aria-label="Repository"]', regenerate, {signal});
+function updateCurrentTab(): void {
+	const currentTab = $('nav[aria-label="Repository"] a[aria-current]');
+	$('.rgh-extensible-nav .selected').classList.remove('selected');
+	$(`.rgh-extensible-nav a[href="${currentTab.href}"]`).classList.add('selected');
 }
 
 void features.add(import.meta.url, {
 	include: [
 		pageDetect.isRepo,
 	],
-	init,
+	init: onetime(initOnce),
+}, {
+	asLongAs: [
+		() => ready,
+	],
+	include: [
+		pageDetect.isRepo,
+	],
+	init: updateCurrentTab,
 });
 
 /*
