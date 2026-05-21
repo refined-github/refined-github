a9b50854223fff7e6466516cbbbb5ce4b2d77767
Federico <me@fregante.com>
2026-05-21 15:00:30 +0800

    Add icons when found

--- source/features/extensible-nav.tsx
+++ source/features/extensible-nav.tsx
@@ -1,3 +1,4 @@
+import './extensible-nav.css';
 import React from 'react';
 import * as pageDetect from 'github-url-detection';
 
@@ -9,16 +10,28 @@ import observe from '../helpers/selector-observer.js';
 function regenerate(nativeNav: HTMLElement): void {
 	const items = $$('a', nativeNav);
 	nativeNav.after(
-		<nav className='UnderlineNav px-4'>
+		<nav className='UnderlineNav rgh-extensible-nav px-4'>
 			<ul className='UnderlineNav-body'>
 				{items.map(item => {
 					const label = ($optional('[data-component="text"]', item) ?? item).textContent;
 					// Only a few items have counters
 					const counter = $optional('[data-component="counter"] [data-variant="secondary"]', item)?.textContent;
 					const selectedClass = item.hasAttribute('aria-current') ? 'selected' : '';
+
+					// The icon may be missing if the feature runs too late and the link is found in the dropdown
+					let icon = $optional('[data-component="icon"]', item);
+					if (icon) {
+						icon = icon.cloneNode(true);
+						icon.classList.add(
+							// This class comes after d-none utility classes so they can't override it
+							'UnderlineNav-octicon',
+						);
+					}
+
 					return (
 						<li key={item.href}>
 							<a href={item.href} className={'UnderlineNav-item ' + selectedClass}>
+								{icon}
 								{label}
 								{counter && (
 									<span className='Counter'>{counter}</span>
