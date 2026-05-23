b423518ae2dbe9b06632008c297105cccd25d591
Federico <me@fregante.com>
2026-05-23 15:59:41 +0800

    Revert "Fix selectors test import and default vitest exclusion"
    This reverts commit f69783185d885c1b4b5b05c53164a1a2654a23a0.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
-import {configDefaults, defineConfig} from 'vitest/config';
+import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
 	test: {
 		setupFiles: [
 			'./test/setup-file.js',
-		],
-		exclude: [
-			...configDefaults.exclude,
-			'./source/github-helpers/selectors.test.ts',
 		],
 	},
 });
