04b254561d2a2156a988f64d75f4e5cef45e3979
Grigory <techuila.sunset@gmail.com>
2026-05-24 01:33:44 +0500

    Revert "changes"
    This reverts commit 9fb6ec336b930557dcff0432038e319b99142df0.

--- source/features/notifications-ui.tsx
+++ source/features/notifications-ui.tsx
@@ -1,12 +1,7 @@
-import './notifications-ui.css';
-
 import React from 'dom-chef';
 import {$, $$} from 'select-dom';
-import FilterIcon from 'octicons-plain-react/Filter';
-import GearIcon from 'octicons-plain-react/Gear';
 import * as pageDetect from 'github-url-detection';
 
-import {wrapAll} from '../helpers/dom-utils.js';
 import features from '../feature-manager.js';
 
 function replaceNotificationsDropdown(dropdown: Element): void {
@@ -56,50 +51,11 @@ function replaceNotificationsDropdown(dropdown: Element): void {
 	}
 }
 
-function moveAndCompactSettingsButton(button: Element): void {
-	const {parentElement} = button;
-
-	$('.Button-label', button).replaceWith(
-		<span className="Button-visual">
-			<GearIcon />
-		</span>,
-	);
-	$('.Button-trailingAction', button).remove();
-
-	$('#dialog-show-notifications-tabs-nav').after(button);
-	button.classList.add('tmp-ml-1');
-	parentElement!.classList.add('tmp-mb-0');
-}
-
-function moveReadUnreadButtons(buttonGroup: Element): void {
-	buttonGroup.classList.add('tmp-mb-0', 'flex-shrink-0');
-	buttonGroup.classList.remove('col-12');
-
-	const queryBuilder = buttonGroup.nextElementSibling!;
-	queryBuilder.classList.add('tmp-mb-0');
-
-	wrapAll(<div className="d-flex flex-items-center tmp-mb-2 gap-1" />, buttonGroup, queryBuilder);
-}
-
-function replaceFiltersIcon(button: Element): void {
-	const icon = $('.octicon-gear', button);
-	icon.replaceWith(<FilterIcon />);
-	// Remove extra margin - `gap` provides enough spacing
-	button.classList.remove('ml-1');
-}
-
 function init(): void {
-	// There are two UIs in the DOM: one for desktop and one for mobile
 	for (const dropdown of $$(['.notification-sort-by', '.notification-group-by'])) {
 		replaceNotificationsDropdown(dropdown);
 	}
-
-	moveAndCompactSettingsButton($('.manage-notifications-responsive > .manage-notifications-menu'));
-	moveReadUnreadButtons($('.BtnGroup.col-12:has(> form[action$="set_preferred_inbox_query"])'));
-	replaceFiltersIcon($('.Button[aria-label="Customize filters"]'));
 }
-
-void features.addCssFeature(import.meta.url);
 
 void features.add(import.meta.url, {
 	include: [
