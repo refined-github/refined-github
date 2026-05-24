729de3b94164c21d8d4600e7d60e3104ea48a344
fregante <me@fregante.com>
2026-05-24 15:08:54 +0800

    `mark-merge-commits-in-list` - Fix alignment (#9596)

--- source/features/mark-merge-commits-in-list.tsx
+++ source/features/mark-merge-commits-in-list.tsx
@@ -48,8 +48,7 @@ export function getCommitHash(commit: HTMLElement): string {
 
 function updateCommitIcon(commit: HTMLElement, replace: boolean): void {
 	if (replace) {
-		// Align icon to the line; rem used to match the native units
-		$('.octicon-git-commit', commit).replaceWith(<GitMergeIcon style={{marginLeft: '0.5rem'}} />);
+		$('.octicon-git-commit', commit).replaceWith(<GitMergeIcon />);
 	} else {
 		$(commitTitleInLists, commit).prepend(<GitMergeIcon className="mr-1 tmp-mr-1" />);
 	}
@@ -93,7 +92,7 @@ void features.add(import.meta.url, {
 
 Test URLs
 
-- isPRConversation: https://github.com/refined-github/refined-github/pull/6194#event-8016526003
+- isPRConversation: https://github.com/refined-github/refined-github/pull/9523#commits-pushed-47b8135
 - isPRCommitList: https://github.com/refined-github/refined-github/pull/6194/commits
 - isCommitList: https://github.com/typed-ember/ember-cli-typescript/commits/master?after=5ff0c078a4274aeccaf83382c0d6b46323f57397+174
 - isCompare: https://github.com/refined-github/sandbox/compare/e8b25d3e...b3d0d992
