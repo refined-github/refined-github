2b073c5478a7daffbb331bdf8c89de59d8cbfa15
Federico <me@fregante.com>
2026-06-01 17:09:41 +0800

    Revert "😌"
    This reverts commit 376816eb9be71ac2ca91059e9c102cc19865e854.

--- source/features/mark-pinned.tsx
+++ source/features/mark-pinned.tsx
@@ -1,22 +1,30 @@
-import './mark-pinned.css';
 import React from 'dom-chef';
 import * as pageDetect from 'github-url-detection';
 import PinIcon from 'octicons-plain-react/Pin';
-import {$, closestElement, elementExists} from 'select-dom';
+import {elementExists} from 'select-dom';
 
 import features from '../feature-manager.js';
+import {getIdentifiers} from '../helpers/feature-helpers.js';
 import observe from '../helpers/selector-observer.js';
-import {is} from '../helpers/css-selectors.js';
-import {issueIcons} from './select-notifications.js';
+
+const {class: featureClass} = getIdentifiers(import.meta.url);
+
+function getPinnedIssueSelector(issueLink: HTMLAnchorElement): string {
+	// The href attribute in the pinned issue list contains the absolute URL
+	return `[class*='PinnedIssues-module__container'] a[href="${issueLink.href}"]`;
+}
 
 function mark(issueLink: HTMLAnchorElement): void {
-	// The href attribute in the pinned issue list contains the absolute URL
-	if (elementExists(`[class*='PinnedIssues-module__container'] a[href="${issueLink.href}"]`)) {
-		// Keep the container and its classes, replace just the contents
-		const paths = (<PinIcon />).children;
-		const icon = $(is(issueIcons), closestElement('li', issueLink));
-		icon.replaceChildren(...paths);
-		icon.parentElement!.classList.add('rgh-pinned-issue-icon');
+	if (
+		// Is pinned
+		elementExists(getPinnedIssueSelector(issueLink))
+
+		// Is not already titled with the pin emoji
+		&& !issueLink.textContent.startsWith('📌')
+	) {
+		issueLink.prepend(
+			<PinIcon className={`${featureClass} color-fg-muted mr-1 tmp-mr-1 v-align-text-bottom`} />,
+		);
 	}
 }
 
@@ -35,8 +43,7 @@ void features.add(import.meta.url, {
 
 Test URLs:
 
-https://github.com/refined-github/sandbox/issues?q=title
-
-Only for the screenshot: https://github.com/gulpjs/gulp/issues?q=in%3Atitle%20update%20docs%20for%20sort%3Aupdated-asc
+- Prepend to pinned issues: https://github.com/gulpjs/gulp/issues?q=in%3Atitle%20update%20docs%20for
+- Don't alter issues that already have an emoji: https://github.com/yaotingwangofficial/Awesome-MCoT/issues
 
 */
