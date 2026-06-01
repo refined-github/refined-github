ab743b38986b297ed5ac859a3bb71fee9aa21bc7
Federico <me@fregante.com>
2026-06-02 01:01:33 +0800

    Lint

--- source/features/tag-changes-link.tsx
+++ source/features/tag-changes-link.tsx
@@ -138,8 +138,8 @@ async function init(): Promise<void> {
 				<div className={cx(
 					'rgh-changelog-link mr-4 tmp-mr-3',
 					pageDetect.isReleases()
-					? 'tmp-my-md-2 my-md-2 mr-md-0 tmp-mr-md-0' 
-					: 'mb-2 tmp-mb-2',
+						? 'tmp-my-md-2 my-md-2 mr-md-0 tmp-mr-md-0'
+						: 'mb-2 tmp-mb-2',
 				)}>
 					{compareLink}
 				</div>,
