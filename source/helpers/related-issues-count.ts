7c3b678a4205c5a943d54336126e4439817c74c4
Federico <me@fregante.com>
2026-06-03 15:05:24 +0800

    `related-issues-count` - Exclude current issue

--- source/helpers/related-issues-count.ts
+++ source/helpers/related-issues-count.ts
@@ -1,10 +1,14 @@
+import * as pageDetect from 'github-url-detection';
+
 import api from '../github-helpers/api.js';
 import {getFeatureRelatedIssuesQuery} from './rgh-links.js';
 
 async function getOpenRelatedIssuesCount(featureId: string): Promise<number> {
 	const query = `${getFeatureRelatedIssuesQuery(featureId)} repo:refined-github/refined-github`;
 	const response = await api.v3(`/search/issues?q=${encodeURIComponent(query)}`);
-	return response.total_count;
+	// Don't count the current issue if we're on it
+	const adjustment = pageDetect.isOpenConversation() ? -1 : 0;
+	return response.total_count as number + adjustment;
 }
 
 export default getOpenRelatedIssuesCount;
