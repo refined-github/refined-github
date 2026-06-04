1b466c126e73cecba1c242523c370cbd8b53b0f7
Federico <me@fregante.com>
2026-06-04 16:07:29 +0800

    Fix Safari support

--- source/options/reload-without.ts
+++ source/options/reload-without.ts
@@ -1,6 +1,6 @@
 import webextAlert from 'webext-alert';
-import {isScriptableUrl} from 'webext-content-scripts';
 import {StorageItem} from 'webext-storage';
+import {isContentScriptRegistered} from 'webext-dynamic-content-scripts/utils.js';
 import createContextMenu from 'webext-tools/create-context-menu.js';
 
 // Always Firefox… https://developer.mozilla.org/docs/Mozilla/Add-ons/WebExtensions/API/storage/StorageArea/setAccessLevel
@@ -13,13 +13,9 @@ export const contentScriptToggle = new StorageItem('contentScript', {
 });
 
 async function reload(_: unknown, tab: chrome.tabs.Tab): Promise<void> {
-	if (
-		tab.url && isScriptableUrl(tab.url) && await chrome.permissions.contains({
-			origins: [tab.url],
-		})
-	) {
+if (tab.id && tab.url && await isContentScriptRegistered(tab.url)) {
 		await contentScriptToggle.set(false);
-		await chrome.tabs.reload(tab.id!);
+		await chrome.tabs.reload(tab.id);
 	} else {
 		// TODO: Use https://github.com/fregante/webext-events/issues/31 to disable the item instead
 		await webextAlert('Refined GitHub is already not running on this page');
