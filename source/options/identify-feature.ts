f8ebb38f0eff61f01760ddec2caa4e82bb553c47
Federico <me@fregante.com>
2026-06-04 14:10:34 +0800

    Skip confirmation

--- source/options/identify-feature.ts
+++ source/options/identify-feature.ts
@@ -3,22 +3,10 @@ import createContextMenu from 'webext-tools/create-context-menu.js';
 
 import {startFeatureIdentification} from '../helpers/bisect.js';
 
-const reloadPrompt = 'Reload the page to start identification. Reload now?';
-
 async function confirmAndReload(_menuInfo: unknown, tab: chrome.tabs.Tab): Promise<void> {
 	try {
 		await startFeatureIdentification(new URL(tab.url!).origin);
-		await chrome.scripting.executeScript({
-			target: {
-				tabId: tab.id!,
-			},
-			func(message) {
-				if (confirm(message)) {
-					location.reload();
-				}
-			},
-			args: [reloadPrompt],
-		});
+		await chrome.tabs.reload(tab.id!);
 	} catch {
 		await alert('Refined GitHub cannot start identification on this page');
 	}
@@ -27,7 +15,7 @@ async function confirmAndReload(_menuInfo: unknown, tab: chrome.tabs.Tab): Promi
 export default function addIdentifyFeatureContextMenu(): void {
 	void createContextMenu({
 		id: 'identify-feature',
-		title: 'Identify feature…',
+		title: 'Reload page and identify feature…',
 		contexts: ['action'],
 		onclick: confirmAndReload,
 	});
