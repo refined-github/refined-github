ba6ac9df34ed91924ed508be6e70f36e5db69304
Federico <me@fregante.com>
2026-06-22 17:19:50 +0800

    ok

--- source/features/one-click-review-submission.tsx
+++ source/features/one-click-review-submission.tsx
@@ -51,6 +51,7 @@ function replaceCheckboxes(originalSubmitButton: HTMLButtonElement): void {
 			'p',
 			'.FormControl-caption',
 		], parent).textContent.trim().replace(/\.$/, '');
+		assertNodeContent(labelElement, /^(?:Approve|Request changes|Comment)$/);
 
 		const classes = ['btn btn-sm'];
 
