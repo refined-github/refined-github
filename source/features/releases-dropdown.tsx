770ef59d94974fd21fb52160150472690a675bcc
fregante <me@fregante.com>
2026-06-27 14:23:00 +0800

    `releases-dropdown` - Add "Hide pre-releases" filter (#9741)

--- source/features/releases-dropdown.tsx
+++ source/features/releases-dropdown.tsx
@@ -23,6 +23,11 @@ const releasesCache = new CachedFunction('releases', {
 async function selectionHandler(event: DelegateEvent<Event, HTMLInputElement>): Promise<void> {
 	const field = event.delegateTarget;
 	const selectedTag = field.value;
+	if (selectedTag === 'prerelease:false') {
+		location.assign('?q=prerelease%3Afalse');
+		return;
+	}
+
 	const releases = await releasesCache.get(); // Expected to be in cache
 	if (!('inputType' in event) && releases.includes(selectedTag)) {
 		location.assign(buildRepoUrl('releases/tag', encodeURIComponent(selectedTag)));
@@ -36,9 +41,16 @@ async function addList(searchField: HTMLInputElement): Promise<void> {
 		return;
 	}
 
+	// `q` is only present in searches
+	const hidePreReleases = new URLSearchParams(location.search).get('q')?.includes('prerelease:false');
+
 	searchField.setAttribute('list', 'rgh-releases-dropdown');
 	searchField.after(
 		<datalist id="rgh-releases-dropdown">
+			<option
+				value="prerelease:false"
+				selected={hidePreReleases}
+			/>
 			{releases.map(tag => <option value={tag} />)}
 		</datalist>,
 	);
