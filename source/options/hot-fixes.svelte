46b242a032e7add5ed0965db625b68734306efb2
Federico <me@fregante.com>
2026-07-04 02:57:19 +0800

    hot-fixes: sveltiest

--- source/options/hot-fixes.svelte
+++ source/options/hot-fixes.svelte
@@ -16,22 +16,39 @@
 
 	const {version} = chrome.runtime.getManifest();
 
+	type HotfixData = {
+		hotfixes: string;
+		brokenFeatures: string;
+	};
 
-	async function loadCached(): Promise<string> {
-		return await styleHotfixes.getCached(version)
-			?? 'No CSS found in cache.';
+	async function loadCached(): Promise<HotfixData> {
+		const cachedBrokenFeatures = await brokenFeatures.getCached();
+		const cachedStyleHotfixes = await styleHotfixes.getCached(version);
+		return {
+			hotfixes: cachedStyleHotfixes ?? 'No CSS found in cache.',
+			brokenFeatures: cachedBrokenFeatures
+				? JSON.stringify(
+					cachedBrokenFeatures,
+					undefined,
+					2,
+				)
+				: 'No broken features found in cache. This may be indicative of a hotfix loading failure, the list is never empty.',
+		};
 	}
 
-	let brokenFeaturesText = $state<string | undefined>();
 	// eslint-disable-next-line unicorn/prefer-top-level-await -- https://github.com/sindresorhus/eslint-plugin-unicorn/issues/3488
 	let hotfixesPromise = $state(loadCached());
 
-	async function fetchHotfixes(): Promise<string> {
-		const hotfixes = await styleHotfixes.getFresh(version)
-			?? 'No hotfixes needed for this version! 🎉';
-		const storage = await brokenFeatures.getFresh();
-		brokenFeaturesText = JSON.stringify(storage, undefined, 2);
-		return hotfixes;
+	async function fetchHotfixes(): Promise<HotfixData> {
+		return {
+			hotfixes: await styleHotfixes.getFresh(version)
+				?? 'No hotfixes needed for this version! 🎉',
+			brokenFeatures: JSON.stringify(
+				await brokenFeatures.getFresh(),
+				undefined,
+				2,
+			),
+		};
 	}
 
 	function refreshHotfixes(): void {
@@ -50,18 +67,22 @@
 	{:else if enterprise}
 		<p>Hotfixes are not applied to GitHub Enterprise.</p>
 	{:else}
-		<p>This is the latest CSS fetched from the server (if any):</p>
-		{#await hotfixesPromise then hotfixes}
-			<p><textarea rows="2" readonly value={hotfixes}></textarea></p>
-			{#if brokenFeaturesText}
-				<p>
-					<textarea
-						rows="2"
-						readonly
-						value={brokenFeaturesText}
-					></textarea>
-				</p>
-			{/if}
+		<p>This is the latest data fetched from the server (or cache):</p>
+		{#await hotfixesPromise then data}
+			<p>CSS hotfixes:</p>
+			<p>
+				<textarea rows="2" readonly value={data.hotfixes}></textarea>
+			</p>
+
+			<p>Disabled features:</p>
+			<p>
+				<textarea
+					rows="2"
+					readonly
+					value={data.brokenFeatures}
+				></textarea>
+			</p>
+
 			<button type="button" onclick={refreshHotfixes}>Fetch hotfixes</button>
 		{/await}
 	{/if}
