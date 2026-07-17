c75b73d41c6566719230c4dd9ea23f0371b538f6
Federico <me@fregante.com>
2026-07-17 19:53:31 +0800

    Lint

--- source/helpers/tooltip.tsx
+++ source/helpers/tooltip.tsx
@@ -62,7 +62,7 @@ export function withTooltipRef(
 	content: string | TooltipOptions,
 	// eslint-disable-next-line @typescript-eslint/no-restricted-types -- Native type
 ): (element: Element | null) => void {
-	return (element) => {
+	return element => {
 		if (element) {
 			createTooltipFor(element, content);
 		}
