056e5915dec2cc81dacee518e70339896f9b5aa1
Federico <me@fregante.com>
2026-07-18 02:34:04 +0800

    Revert some deletions

--- source/features/status-subscription.tsx
+++ source/features/status-subscription.tsx
@@ -10,13 +10,6 @@ import StatusSubscription from './status-subscription.svelte';
 
 type SubscriptionStatus = 'none' | 'all' | 'status';
 
-const githubApiBaseHeaders = new Headers({
-	accept: 'application/json',
-	'github-verified-fetch': 'true',
-	'x-github-client-version':
-		'Refined GitHub. Please address https://github.com/orgs/community/discussions/132506#discussioncomment-11294985',
-	credentials: 'include',
-});
 
 function getLegacyReason(subscriptionButton: HTMLButtonElement): HTMLParagraphElement {
 	return $('p.reason', closestElement('.thread-subscription-status', subscriptionButton));
@@ -24,10 +17,13 @@ function getLegacyReason(subscriptionButton: HTMLButtonElement): HTMLParagraphEl
 
 function getLegacyStatus(button: HTMLButtonElement): SubscriptionStatus {
 	const reason = getLegacyReason(button).textContent;
+
+	// You’re receiving notifications because you chose custom settings for this thread.
 	if (reason.includes('custom settings')) {
 		return 'status';
 	}
 
+	// You’re not receiving notifications from this thread.
 	if (reason.includes('not receiving')) {
 		return 'none';
 	}
@@ -37,6 +33,7 @@ function getLegacyStatus(button: HTMLButtonElement): SubscriptionStatus {
 
 function addLegacyButton(subscriptionButton: HTMLButtonElement): void {
 	const status = getLegacyStatus(subscriptionButton);
+	// Save first
 	const originalId = subscriptionButton.form!.elements.id;
 
 	const container = <div /> as unknown as HTMLDivElement;
@@ -47,17 +44,28 @@ function addLegacyButton(subscriptionButton: HTMLButtonElement): void {
 		props: {status, isLegacy: true},
 	});
 
+	// Remove it only if the form was successfully added
 	originalId.remove();
 	subscriptionButton.hidden = true;
 
+	// 'all' can have many reasons, but the other two don't add further information #6684
 	if (status !== 'all') {
 		getLegacyReason(subscriptionButton).hidden = true;
 	}
 }
 
+const githubApiBaseHeaders = new Headers({
+	accept: 'application/json',
+	'github-verified-fetch': 'true',
+	'x-github-client-version':
+		'Refined GitHub. Please address https://github.com/orgs/community/discussions/132506#discussioncomment-11294985',
+	credentials: 'include',
+});
+
 async function fetchIssueData(): Promise<Record<string, any>> {
 	const {owner, name} = getRepo()!;
 	const body = {
+		// `IssueViewerSecondaryViewQuery`
 		query: 'cb9b35846fadf5f80ec3a2c05bf42a89',
 		variables: {number: getConversationNumber()!, owner, repo: name},
 	};
@@ -78,6 +86,7 @@ async function updateSubscription(targetStatus: SubscriptionStatus, id: string):
 		headers: githubApiBaseHeaders,
 		method: 'POST',
 		body: JSON.stringify({
+			// `updateIssueSubscriptionMutation`
 			query: 'd0752b2e49295017f67c84f21bfe41a3',
 			variables: {
 				input: {
@@ -136,11 +145,13 @@ async function addButton(subscriptionButton: HTMLButtonElement): Promise<void> {
 
 	mountedComponents.set(subscriptionButton, component);
 
+	// Would be missing on the first run
 	previousRghButton?.remove();
 	subscriptionButton.hidden = true;
 }
 
 function init(signal: AbortSignal): void {
+	// Repos you're ignoring can't be subscribed to, so the button is disabled
 	observe('button[data-thread-subscribe-button]:enabled', addLegacyButton, {signal});
 	if (!pageDetect.isEnterprise()) {
 		observe('button[aria-describedby*="issue-viewer-subscription-description"]', addButton, {signal});
@@ -148,7 +159,19 @@ function init(signal: AbortSignal): void {
 }
 
 void features.add(import.meta.url, {
-	include: [pageDetect.isConversation],
-	awaitDomReady: true,
+	include: [
+		pageDetect.isConversation,
+	],
+	awaitDomReady: true, // The sidebar is at the end of the page
 	init,
 });
+
+/*
+
+Test URLs
+
+- Issue: https://github.com/refined-github/sandbox/issues/3
+- PR: https://github.com/refined-github/sandbox/pull/4
+- Also test a repo you're completely ignoring
+
+*/
