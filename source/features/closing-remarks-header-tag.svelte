07b5f34885e8083c501aecc6feabe1516122e60d
fregante <me@fregante.com>
2026-07-19 03:16:19 +0800

    Add closing remarks header tag component

---
+++ source/features/closing-remarks-header-tag.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import DomChef from './dom-chef.svelte';
+	import TagIcon from 'octicons-plain-react/Tag';
+
+	const {tagName, tagUrl}: {tagName: string; tagUrl: string} = $props();
+</script>
+
+<a
+	href={tagUrl}
+	class="text-bold Link--primary no-underline"
+	title="{tagName} was the first Git tag to include this pull request"
+>
+	<DomChef as={TagIcon} class="ml-2 tmp-ml-2 mr-1 tmp-mr-1 color-fg-muted" />
+	{tagName}
+</a>
