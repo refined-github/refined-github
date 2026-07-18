aa4f60908359c2b93d15ba511cfb39466316a6d9
Grigory <techuila.sunset@gmail.com>
2026-07-18 17:28:05 +0500

    comment

--- source/features/quick-resolve-conflicts.tsx
+++ source/features/quick-resolve-conflicts.tsx
@@ -75,7 +75,7 @@ async function replaceResolveConflictsDropdown(button: HTMLButtonElement): Promi
 }
 
 function initPrConversation(signal: AbortSignal): void {
-	// `data-component` is used to avoid selecting one of the files with conflicts
+	// `data-component` is used to avoid selecting our button and one of the files with conflicts
 	observe(
 		'[aria-label="Conflicts"] [class^="MergeBoxSectionHeader-module__wrapper"] button[data-component$="Button"]',
 		replaceResolveConflictsDropdown,
