e23ed375bf2e052036967fdd3522a22d6d6ffe2e
Grigory <techuila.sunset@gmail.com>
2026-07-18 17:24:42 +0500

    `quick-resolve-conflicts` - Restore feature

--- source/features/quick-resolve-conflicts.tsx
+++ source/features/quick-resolve-conflicts.tsx
@@ -75,8 +75,9 @@ async function replaceResolveConflictsDropdown(button: HTMLButtonElement): Promi
 }
 
 function initPrConversation(signal: AbortSignal): void {
+	// `data-component` is used to avoid selecting one of the files with conflicts
 	observe(
-		'[aria-label="Conflicts"] [class^="MergeBoxSectionHeader-module__wrapper"] button[data-component="Button"]',
+		'[aria-label="Conflicts"] [class^="MergeBoxSectionHeader-module__wrapper"] button[data-component$="Button"]',
 		replaceResolveConflictsDropdown,
 		{signal},
 	);
