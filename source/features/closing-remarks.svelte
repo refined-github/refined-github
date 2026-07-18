2a82ed1a91a829c7fc8936b8f9b9b368cd6b26c1
fregante <me@fregante.com>
2026-07-19 03:15:24 +0800

    Add closing remarks feature component in Svelte

---
+++ source/features/closing-remarks.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import Banner from '../github-helpers/banner.svelte';
+	import DomChef from './dom-chef.svelte';
+	import TagIcon from 'octicons-plain-react/Tag';
+
+	type Props = {
+		tagName?: string;
+		tagUrl?: string;
+		postMerge?: boolean;
+		hasPushAccess?: boolean;
+		releaseUrl?: string;
+	};
+
+	const {tagName, tagUrl, postMerge = false, hasPushAccess, releaseUrl}: Props = $props();
+
+	const explanationHref =
+		'https://github.com/refined-github/refined-github/wiki/Extended-feature-descriptions#closing-remarks';
+</script>
+
+<!-- Classes copied from #issuecomment-new + mt-3 added (TimelineItem) -->
+<div class="ml-0 tmp-ml-0 pl-0 tmp-pl-0 ml-md-6 tmp-ml-md-6 pl-md-3 tmp-pl-md-3 mt-3 tmp-mt-3">
+	{#if tagName && tagUrl}
+		<Banner classes={['flash-success', 'rgh-bg-none']}>
+			{#snippet icon()}<DomChef as={TagIcon} class="m-0 tmp-m-0" />{/snippet}
+			{#snippet text()}
+				This pull request first <a href={explanationHref}>appeared</a> in
+				<a href={tagUrl} class="Link--primary text-bold">{tagName}</a>
+			{/snippet}
+		</Banner>
+	{:else}
+		<Banner
+			classes={['rgh-bg-none']}
+			action={hasPushAccess ? releaseUrl : undefined}
+			buttonLabel={hasPushAccess ? 'Draft a new release' : undefined}
+		>
+			{#snippet icon()}<DomChef as={TagIcon} class="m-0 tmp-m-0" />{/snippet}
+			{#snippet text()}
+				{#if postMerge}
+					Now you can release this change
+				{:else}
+					No <a href={explanationHref}>stable version tags</a> for this PR.
+				{/if}
+			{/snippet}
+		</Banner>
+	{/if}
+</div>
