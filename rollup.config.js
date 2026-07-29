b89e01a801fc2f04cfef9cd6f865361262f72fb1
Federico <me@fregante.com>
2026-07-29 15:41:38 +0800

    Drop sucrase

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import alias from '@rollup/plugin-alias';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
-import sucrase from '@rollup/plugin-sucrase';
+import typescript from '@rollup/plugin-typescript';
 import browserslist from 'browserslist';
 import {browserslistToTargets, Features} from 'lightningcss';
 import process from 'node:process';
@@ -102,15 +102,7 @@ const rollup = {
 				{find: 'react', replacement: 'dom-chef'},
 			],
 		}),
-		sucrase({
-			transforms: ['typescript', 'jsx'],
-
-			// Output modern JS
-			disableESTransforms: true,
-
-			// Drop `__self` in JSX https://github.com/alangpierce/sucrase/issues/232#issuecomment-468898878
-			production: true,
-		}),
+		typescript({filterRoot: '..', noCheck: true}),
 		resolve({browser: true}),
 		commonjs(),
 		copy({
