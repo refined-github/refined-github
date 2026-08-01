a03840b726475b71c9fd5903e28446fd4bc16d1e
Federico <me@fregante.com>
2026-08-01 16:54:26 +0800

    `clean-readme-url` - Fix "Readme" link on repo home

--- source/features/clean-readme-url.tsx
+++ source/features/clean-readme-url.tsx
@@ -1,29 +1,26 @@
 import * as pageDetect from 'github-url-detection';
 
+import delay from '../helpers/delay.js';
 import features from '../feature-manager.js';
 
-function maybeCleanUrl(event?: NavigateEvent): void {
-	// TODO [2027-01-01]: Drop setInterval and optional chaining, it's only needed to support Safari <26.2
-	const parsed = new URL(event?.destination.url ?? location.href);
-	if (parsed.searchParams.get('tab') === 'readme-ov-file') {
-		parsed.searchParams.delete('tab');
-		history.replaceState(history.state, '', parsed.href);
+async function maybeCleanUrl(): Promise<void> {
+	const parsed = new URL(location.href);
+	if (parsed.searchParams.get('tab') !== 'readme-ov-file') {
+		return;
 	}
+
+	// GitHub has some delayed logic to deal with this internally
+	// https://github.com/refined-github/refined-github/issues/9908
+	await delay(500);
+	parsed.searchParams.delete('tab');
+	history.replaceState(history.state, '', parsed.href);
 }
 
 function init(signal: AbortSignal): void {
-	maybeCleanUrl();
-	let interval: NodeJS.Timeout;
-	if ('navigation' in globalThis) {
-		navigation.addEventListener('navigate', maybeCleanUrl, {signal});
-	} else {
-		interval = setInterval(() => {
-			maybeCleanUrl();
-		}, 1000);
-		signal.addEventListener('abort', () => {
-			clearInterval(interval);
-		});
-	}
+	void maybeCleanUrl();
+
+	// TODO [2027-01-01]: Drop optional chaining, it's only needed to support Safari <26.2
+	navigation?.addEventListener('navigatesuccess', maybeCleanUrl, {signal});
 }
 
 void features.add(import.meta.url, {
