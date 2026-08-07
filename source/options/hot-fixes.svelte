9f51f209d469525be3acab8dad3edd7c7a184c9f
Federico <me@fregante.com>
2026-08-07 14:36:58 +0800

    Format

--- source/options/hot-fixes.svelte
+++ source/options/hot-fixes.svelte
@@ -41,8 +41,8 @@
 		</button>
 	</p>
 	<p hidden={!emptied} class="error-banner">
-		✅ Hotfixes have been emptied and will stay empty until the next auto-update in
-		a few hours.
+		✅ Hotfixes have been emptied and will stay empty until the next
+		auto-update in a few hours.
 	</p>
 	<h3>CSS hotfixes</h3>
 	<p>
