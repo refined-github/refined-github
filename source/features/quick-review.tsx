36eb88bdc7a2e4eb7f828ce4f268e7b508869cb9
Leo Robinovitch <leorobinovitch@gmail.com>
2026-08-14 12:12:52 -0700

    fix missing review now

--- source/features/quick-review.tsx
+++ source/features/quick-review.tsx
@@ -3,7 +3,7 @@ import delegate, {type DelegateEvent} from 'delegate-it';
 import elementReady from 'element-ready';
 import filterAlteredClicks from 'filter-altered-clicks';
 import * as pageDetect from 'github-url-detection';
-import {$} from 'select-dom';
+import {$, $optional} from 'select-dom';
 
 import features from '../feature-manager.js';
 import api from '../github-helpers/api.js';
@@ -26,7 +26,8 @@ const openReviewMenuDeepLink = 'review-changes-modal';
 const openReviewMenuDeepLinkSelector = `#${openReviewMenuDeepLink}`;
 const prFilesChangedTabSelector = 'a#prs-files-anchor-tab';
 
-const isOldPrFiles = (): boolean => $(prFilesChangedTabSelector).href.endsWith('files');
+// GHES still uses the legacy files tab without this ID #9937
+const isOldPrFiles = (): boolean => $optional(prFilesChangedTabSelector)?.href.endsWith('files') ?? true;
 
 async function quickApprove(event: MouseEvent): Promise<void> {
 	const approval = event.altKey ? '' : prompt('Approve instantly? You can add a custom message or leave empty');
