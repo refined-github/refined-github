7e29b7d7fedf942bf88e08f97afcd2c5131a863f
wakamex <mcosma@gmail.com>
2026-08-24 12:02:48 -0400

    Use observer for React sidebar

--- source/features/clean-repo-sidebar.tsx
+++ source/features/clean-repo-sidebar.tsx
@@ -1,62 +1,41 @@
 import './clean-repo-sidebar.css';
-import elementReady from 'element-ready';
 import * as pageDetect from 'github-url-detection';
 import {$optional, closestElement, elementExists} from 'select-dom';
 
 import features from '../feature-manager.js';
 import {assertNodeContent} from '../helpers/dom-utils.js';
+import observe from '../helpers/selector-observer.js';
 
 // The h2 is to avoid hiding website links that include '/releases' #4424
-async function cleanReleases(signal: AbortSignal): Promise<void> {
-	const sidebarReleases = await elementReady('[class*="PageLayout-Pane"] [class*="SidebarSection-module__sidebarSection"] h2 a[href$="/releases"]', {
-		signal,
-		stopOnDomReady: false,
-		waitForChildren: false,
-	});
-	if (!sidebarReleases) {
-		return;
-	}
-
-	const releasesSection = closestElement('[class*="SidebarSection-module__sidebarSection"]', sidebarReleases);
-	if (
-		// Hide the whole section if there's no releases
-		!elementExists('.octicon-tag', releasesSection)
-		// Don't hide the section if it has a "Create new release" link
-		&& !elementExists('a[href$="releases/new"]', releasesSection)
-	) {
-		releasesSection.hidden = true;
-	}
+function cleanReleases(signal: AbortSignal): void {
+	observe('[class*="PageLayout-Pane"] [class*="SidebarSection-module__sidebarSection"]:not(:has([data-component="SkeletonText"])) h2 a[href$="/releases"]', sidebarReleases => {
+		const releasesSection = closestElement('[class*="SidebarSection-module__sidebarSection"]', sidebarReleases);
+		if (
+			// Hide the whole section if there's no releases
+			!elementExists('.octicon-tag', releasesSection)
+			// Don't hide the section if it has a "Create new release" link
+			&& !elementExists('a[href$="releases/new"]', releasesSection)
+		) {
+			releasesSection.hidden = true;
+		}
+	}, {signal, once: true});
 }
 
-async function hideLanguageHeader(signal: AbortSignal): Promise<void> {
-	const languageHeader = await elementReady("[class*='PageLayout-Pane'] [class*='SidebarSection-module__sidebarSection']:has([data-component='ProgressBar']) > h2", {
-		signal,
-		stopOnDomReady: false,
-		waitForChildren: false,
-	});
-	if (!languageHeader) {
-		return;
-	}
-
-	assertNodeContent(languageHeader.firstChild, 'Languages');
-	languageHeader.classList.add('sr-only');
+function hideLanguageHeader(signal: AbortSignal): void {
+	observe("[class*='PageLayout-Pane'] [class*='SidebarSection-module__sidebarSection']:has([data-component='ProgressBar']) > h2", languageHeader => {
+		assertNodeContent(languageHeader.firstChild, 'Languages');
+		languageHeader.classList.add('sr-only');
+	}, {signal, once: true});
 }
 
-async function moveReportLink(signal: AbortSignal): Promise<void> {
-	const lastSection = await elementReady("[class*='PageLayout-Pane'] [class*='SidebarSection-module__sidebarSection']:last-child:not(:has([data-component='SkeletonText']))", {
-		signal,
-		stopOnDomReady: false,
-		waitForChildren: false,
-	});
-	if (!lastSection) {
-		return;
-	}
-
-	// Your own repos don't include this link
-	const reportLink = $optional("[class*='PageLayout-Pane'] a[href^='/contact/report-content']")?.parentElement;
-	if (reportLink) {
-		lastSection.append(reportLink);
-	}
+function moveReportLink(signal: AbortSignal): void {
+	observe("[class*='PageLayout-Pane'] [class*='SidebarSection-module__sidebarSection']:last-child:not(:has([data-component='SkeletonText']))", lastSection => {
+		// Your own repos don't include this link
+		const reportLink = $optional("[class*='PageLayout-Pane'] a[href^='/contact/report-content']")?.parentElement;
+		if (reportLink) {
+			lastSection.append(reportLink);
+		}
+	}, {signal, once: true});
 }
 
 async function init(): Promise<void> {
@@ -67,7 +46,6 @@ void features.add(import.meta.url, {
 	include: [
 		pageDetect.isRepoRoot,
 	],
-	deduplicate: 'has-rgh-inner',
 	init: [
 		init,
 		cleanReleases,
