59ada30c1731986fbaf0a1b30ee38127638a4ae0
fregante <me@fregante.com>
2026-08-25 01:44:20 +0800

    Move to observers

--- source/features/clean-repo-sidebar.tsx
+++ source/features/clean-repo-sidebar.tsx
@@ -6,52 +6,45 @@ import features from '../feature-manager.js';
 import {assertNodeContent} from '../helpers/dom-utils.js';
 import observe from '../helpers/selector-observer.js';
 
-// The h2 is to avoid hiding website links that include '/releases' #4424
-function cleanReleases(signal: AbortSignal): void {
-	observe('[class*="PageLayout-Pane"] [class*="SidebarSection-module__sidebarSection"]:not(:has([data-component="SkeletonText"])) h2 a[href$="/releases"]', sidebarReleases => {
-		const releasesSection = closestElement('[class*="SidebarSection-module__sidebarSection"]', sidebarReleases);
-		if (
-			// Hide the whole section if there's no releases
-			!elementExists('.octicon-tag', releasesSection)
-			// Don't hide the section if it has a "Create new release" link
-			&& !elementExists('a[href$="releases/new"]', releasesSection)
-		) {
-			releasesSection.hidden = true;
-		}
-	}, {signal, once: true});
+function cleanReleases(sidebarReleases: HTMLElement): void {
+	const releasesSection = closestElement('[class*="SidebarSection-module__sidebarSection"]', sidebarReleases);
+	if (
+		// Hide the whole section if there's no releases
+		!elementExists('.octicon-tag', releasesSection)
+		// Don't hide the section if it has a "Create new release" link
+		&& !elementExists('a[href$="releases/new"]', releasesSection)
+	) {
+		releasesSection.hidden = true;
+	}
 }
 
-function hideLanguageHeader(signal: AbortSignal): void {
-	observe("[class*='PageLayout-Pane'] [class*='SidebarSection-module__sidebarSection']:has([data-component='ProgressBar']) > h2", languageHeader => {
-		assertNodeContent(languageHeader.firstChild, 'Languages');
-		languageHeader.classList.add('sr-only');
-	}, {signal, once: true});
+function hideLanguageHeader(languageHeader: HTMLElement): void {
+	assertNodeContent(languageHeader.firstChild, 'Languages');
+	languageHeader.classList.add('sr-only');
 }
 
-function moveReportLink(signal: AbortSignal): void {
-	observe("[class*='PageLayout-Pane'] [class*='SidebarSection-module__sidebarSection']:last-child:not(:has([data-component='SkeletonText']))", lastSection => {
-		// Your own repos don't include this link
-		const reportLink = $optional("[class*='PageLayout-Pane'] a[href^='/contact/report-content']")?.parentElement;
-		if (reportLink) {
-			lastSection.append(reportLink);
-		}
-	}, {signal, once: true});
+function moveReportLink(lastSection: HTMLElement): void {
+	// Your own repos don't include this link
+	const reportLink = $optional("[class*='PageLayout-Pane'] a[href^='/contact/report-content']")?.parentElement;
+	if (reportLink) {
+		lastSection.append(reportLink);
+	}
 }
 
-async function init(): Promise<void> {
+async function init(signal: AbortSignal): Promise<void> {
 	document.documentElement.setAttribute('rgh-clean-repo-sidebar', '');
+
+	// The h2 is to avoid hiding website links that include '/releases' #4424
+	observe('[class*="PageLayout-Pane"] [class*="SidebarSection-module__sidebarSection"]:not(:has([data-component="SkeletonText"])) h2 a[href$="/releases"]', cleanReleases, {signal, once: true});
+	observe("[class*='PageLayout-Pane'] [class*='SidebarSection-module__sidebarSection']:has([data-component='ProgressBar']) > h2", hideLanguageHeader, {signal, once: true});
+	observe("[class*='PageLayout-Pane'] [class*='SidebarSection-module__sidebarSection']:last-child:not(:has([data-component='SkeletonText']))", moveReportLink, {signal, once: true});
 }
 
 void features.add(import.meta.url, {
 	include: [
 		pageDetect.isRepoRoot,
 	],
-	init: [
-		init,
-		cleanReleases,
-		hideLanguageHeader,
-		moveReportLink,
-	],
+	init,
 });
 
 /*
