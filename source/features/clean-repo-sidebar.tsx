73afd11264c0243e077d20d07e5bc048feb2ba7b
wakamex <mcosma@gmail.com>
2026-08-25 07:26:32 -0400

    `clean-repo-sidebar` - Restore feature (#9994)
    Co-authored-by: fregante <me@fregante.com>

--- source/features/clean-repo-sidebar.tsx
+++ source/features/clean-repo-sidebar.tsx
@@ -1,77 +1,50 @@
 import './clean-repo-sidebar.css';
-import domLoaded from 'dom-loaded';
-import elementReady from 'element-ready';
 import * as pageDetect from 'github-url-detection';
-import {$, $optional, closestElement, elementExists} from 'select-dom';
+import {$, $optional, elementExists} from 'select-dom';
 
 import features from '../feature-manager.js';
 import {assertNodeContent} from '../helpers/dom-utils.js';
+import observe from '../helpers/selector-observer.js';
 
-// The h2 is to avoid hiding website links that include '/releases' #4424
-// It's broken: https://github.com/refined-github/refined-github/issues/9339
-async function cleanReleases(): Promise<void> {
-	const sidebarReleases = await elementReady('[class*="PageLayout-Pane"] .BorderGrid-cell h2 a[href$="/releases"]', {
-		waitForChildren: false,
-	});
-	if (!sidebarReleases) {
+function cleanSidebarSection(section: HTMLElement): void {
+	// The h2 is to avoid hiding website links that include '/releases' #4424
+	// Hide the whole section if there's no releases, unless it has a "Create new release" link
+	if (elementExists('h2 a[href$="/releases"]', section) && !elementExists('.octicon-tag, a[href$="releases/new"]', section)) {
+		section.hidden = true;
 		return;
 	}
 
-	const releasesSection = closestElement('.BorderGrid-cell', sidebarReleases);
-	if (
-		// Hide the whole section if there's no releases
-		!elementExists('.octicon-tag', releasesSection)
-		// Don't hide the section if it has a "Create new release" link
-		&& !elementExists('a[href$="releases/new"]', releasesSection)
-	) {
-		releasesSection.hidden = true;
+	const languageHeader = $optional(':scope > h2', section);
+	if (languageHeader && elementExists('[data-component="ProgressBar"]', section)) {
+		assertNodeContent(languageHeader.firstChild, 'Languages');
+		languageHeader.classList.add('sr-only');
+		return;
 	}
-}
 
-async function hideLanguageHeader(): Promise<void> {
-	await domLoaded;
-
-	const languageHeader = $("[class*='PageLayout-Pane'] .BorderGrid-row:has(.Progress-item) h2");
-	assertNodeContent(languageHeader.firstChild, 'Languages');
-	languageHeader.classList.add('sr-only');
-}
-
-// Hide empty meta if it’s not editable by the current user
-async function hideEmptyMeta(): Promise<void> {
-	await domLoaded;
-
-	if (!pageDetect.canUserAccessRepoSettings()) {
-		// Selector only matches if it's empty
-		$optional("[class*='PageLayout-Pane'] .BorderGrid-cell > .text-italic")?.remove();
+	const emptyMeta = $optional('[class*="SidebarAbout-module__noDescription"]', section);
+	if (emptyMeta && !pageDetect.canUserAccessRepoSettings()) {
+		emptyMeta.remove();
+		// Don't return here because the next condition applies to the same block
 	}
-}
-
-async function moveReportLink(): Promise<void> {
-	await domLoaded;
 
 	// Your own repos don't include this link
-	const reportLink = $optional("[class*='PageLayout-Pane'] a[href^='/contact/report-content']")?.parentElement;
+	const reportLink = $optional('a[href^="/contact/report-content"]', section);
 	if (reportLink) {
-		$("[class*='PageLayout-Pane'] .BorderGrid-row:last-of-type .BorderGrid-cell").append(reportLink);
+		$('[class*="PageLayout-Pane"] [class*="SidebarSection-module__sidebarSection"]:last-child').append(reportLink.parentElement!);
 	}
 }
 
-async function init(): Promise<void> {
+async function init(signal: AbortSignal): Promise<void> {
 	document.documentElement.setAttribute('rgh-clean-repo-sidebar', '');
+
+	observe('[class*="PageLayout-Pane"] [class*="SidebarSection-module__sidebarSection"]', cleanSidebarSection, {signal});
 }
 
 void features.add(import.meta.url, {
 	include: [
 		pageDetect.isRepoRoot,
 	],
-	deduplicate: 'has-rgh-inner',
-	init: [
-		init,
-		cleanReleases,
-		hideLanguageHeader,
-		hideEmptyMeta,
-		moveReportLink,
-	],
+	init,
 });
 
 /*
@@ -83,5 +56,6 @@ Test URLs:
 - Repo with 1 package: https://github.com/recyclarr/recyclarr
 - Repo with tags but not releases: https://github.com/fregante/bin-dir
 - Repo with no tags: https://github.com/refined-github/yolo
+- Repo with no description: https://github.com/shadcn/Ant
 
 */
