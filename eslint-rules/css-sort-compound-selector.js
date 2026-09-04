e171392418dff8eef07d24fdb0c16cfd4587c784
Federico <me@fregante.com>
2026-09-05 03:35:45 +0800

    ok

--- eslint-rules/css-sort-compound-selector.js
+++ eslint-rules/css-sort-compound-selector.js
@@ -10,6 +10,8 @@ const sortCompoundSelector = {
 	},
 
 	create(context) {
+		const {sourceCode} = context;
+
 		const order = {
 			TypeSelector: 0,
 			IdSelector: 1,
@@ -21,32 +23,55 @@ const sortCompoundSelector = {
 
 		return {
 			Selector(node) {
-				const selectors = [...node.children];
+				let compound = [];
 
-				const sorted = selectors.toSorted(
-					(a, b) => order[a.type] - order[b.type],
-				);
+				const checkCompound = () => {
+					if (compound.length < 2) {
+						compound = [];
+						return;
+					}
 
-				if (
-					selectors.every(
-						(selector, index) => selector === sorted[index],
-					)
-				) {
-					return;
+					const sorted = compound.toSorted(
+						(a, b) => order[a.type] - order[b.type],
+					);
+
+					if (
+						compound.every(
+							(selector, index) => selector === sorted[index],
+						)
+					) {
+						compound = [];
+						return;
+					}
+
+					const firstRange = sourceCode.getRange(compound[0]);
+					const lastRange = sourceCode.getRange(compound.at(-1));
+
+					context.report({
+						node,
+						messageId: 'sort',
+						fix(fixer) {
+							return fixer.replaceTextRange(
+								[firstRange[0], lastRange[1]],
+								sorted
+									.map(selector => sourceCode.getText(selector))
+									.join(''),
+							);
+						},
+					});
+
+					compound = [];
+				};
+
+				for (const child of node.children) {
+					if (child.type === 'Combinator') {
+						checkCompound();
+					} else {
+						compound.push(child);
+					}
 				}
 
-				context.report({
-					node,
-					messageId: 'sort',
-					fix(fixer) {
-						return selectors.map((selector, index) =>
-							fixer.replaceText(
-								selector,
-								context.sourceCode.getText(sorted[index]),
-							)
-						);
-					},
-				});
+				checkCompound();
 			},
 		};
 	},
