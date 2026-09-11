4f0b1f1ff48403f8af60496ac411f2329ef914f9
fregante <me@fregante.com>
2026-09-11 16:23:00 +0800

    Meta: CSS lint rules (#10042)

---
+++ eslint-rules/css-sort-compound-selector.test.js
@@ -0,0 +1,53 @@
+import cssPlugin from '@eslint/css';
+import {RuleTester} from 'eslint';
+import {test} from 'vitest';
+
+import rule from './css-sort-compound-selector.js';
+
+test('css-sort-compound-selector', () => {
+	const ruleTester = new RuleTester({
+		plugins: {css: cssPlugin},
+		language: 'css/css',
+	});
+
+	ruleTester.run('css-sort-compound-selector', rule, {
+		valid: [
+			{code: 'a {}'},
+			{code: '#foo {}'},
+			{code: '.foo {}'},
+			{code: '[data-x] {}'},
+			{code: ':hover {}'},
+			{code: '::before {}'},
+
+			{code: 'a#foo.bar[data-x]:hover::before {}'},
+			{code: 'a#foo.bar[data-x]:not(.disabled):hover::before {}'},
+			{code: ':is(.foo, .bar) {}'},
+			{code: 'a:is(.foo, .bar)::before {}'},
+
+			{code: 'a.foo.bar[data-a][data-b]:focus:hover::before {}'},
+		],
+
+		invalid: [
+			{
+				code: '.foo#a {}',
+				output: '#a.foo {}',
+				errors: [{messageId: 'sort'}],
+			},
+			{
+				code: '[data-x]a {}',
+				output: 'a[data-x] {}',
+				errors: [{messageId: 'sort'}],
+			},
+			{
+				code: '::before:hover {}',
+				output: ':hover::before {}',
+				errors: [{messageId: 'sort'}],
+			},
+			{
+				code: '.foo#a[data-x]:hover::before {}',
+				output: '#a.foo[data-x]:hover::before {}',
+				errors: [{messageId: 'sort'}],
+			},
+		],
+	});
+});
