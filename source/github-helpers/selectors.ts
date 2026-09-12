9da058a7f7d3f5ea49ed109cab08286b2bd1c84f
Tristan Knight <tris203@gmail.com>
2026-09-12 10:23:03 +0100

    `conflict-marker` - Restore on new PR lists (#10076)

--- source/github-helpers/selectors.ts
+++ source/github-helpers/selectors.ts
@@ -64,10 +64,12 @@ export const openPrsListLink = [
 			.octicon-git-pull-request-draft
 		) a.js-navigation-open
 	`,
-	// React view
+	// React issue list
 	css`
 		li[role="listitem"] h3 a[data-hovercard-url*="/pull"]
 	`,
+	// React PR list
+	css`a[data-hovercard-type="pull_request"][data-testid="listitem-title-link"]`,
 ];
 
 export const openPrsListLink_ = [
