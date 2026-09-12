ea82af930fc778e4501a7e2bf774a9f7274559bb
fregante <me@fregante.com>
2026-09-12 17:30:29 +0800

    Lint (#10079)

--- eslint-rules/css-documentation.test.js
+++ eslint-rules/css-documentation.test.js
@@ -1,37 +1,35 @@
 import cssPlugin from '@eslint/css';
-import {RuleTester} from 'eslint';
-import {test} from 'vitest';
+import {run} from 'eslint-vitest-rule-tester';
 
 import rule from './css-documentation.js';
 
-test('css-documentation', () => {
-	const ruleTester = new RuleTester({
+run({
+	name: 'css-documentation',
+	rule,
+	configs: {
 		plugins: {css: cssPlugin},
 		language: 'css/css',
-	});
-
-	ruleTester.run('css-documentation', rule, {
-		valid: [
-			{
-				code: `/* Description */
+	},
+	valid: [
+		{
+			code: `/* Description */
 /* Info: https://github.com/refined-github/refined-github/issues/1 */
 /* Test: https://github.com/refined-github/sandbox/pull/1 */
 .selector {}`,
-			},
-		],
-		invalid: [
-			{
-				code: `/* TODO [2029-08-01]: Remove */
+		},
+	],
+	invalid: [
+		{
+			code: `/* TODO [2029-08-01]: Remove */
 /* Make the PR alert banner non-sticky so it doesn't cover the right sidebar */
 /* Info: https://github.com/refined-github/refined-github/issues/8975 */
 .sticky-header-wrapper.is-stuck .pr-alerts-banner {}`,
-				errors: [{message: /Missing: Test/}],
-			},
-			{
-				code: `/* Test: https://github.com/refined-github/sandbox/pull/47#pullrequestreview-4175514676 */
+			errors: [{message: /Missing: Test/}],
+		},
+		{
+			code: `/* Test: https://github.com/refined-github/sandbox/pull/47#pullrequestreview-4175514676 */
 review-thread-collapsible > .js-toggle-outdated-comments {}`,
-				errors: [{message: /Missing: Description, Info/}],
-			},
-		],
-	});
+			errors: [{message: /Missing: Description, Info/}],
+		},
+	],
 });
--- eslint-rules/css-prefer-nesting.test.js
+++ eslint-rules/css-prefer-nesting.test.js
@@ -1,47 +1,51 @@
 import cssPlugin from '@eslint/css';
-import {RuleTester} from 'eslint';
-import {test} from 'vitest';
+import {run} from 'eslint-vitest-rule-tester';
 
 import rule from './css-prefer-nesting.js';
 
-test('css-prefer-nesting', () => {
-	const ruleTester = new RuleTester({
+run({
+	name: 'css-prefer-nesting',
+	rule,
+	configs: {
 		plugins: {css: cssPlugin},
 		language: 'css/css',
-	});
-
-	ruleTester.run('css-prefer-nesting', rule, {
-		valid: [
-			{code: 'a:is(.foo, .bar) {}'},
-			{code: ':is(.foo, .bar)::before {}'},
-			{code: '&:is(:focus, :hover) svg {}'},
-		],
-		invalid: [
-			{
-				code: 'a :is(.foo, .bar) {}',
-				output: 'a {\n\t.foo, .bar {}\n}',
-				errors: [{messageId: 'descendantIs'}],
-			},
-			{
-				code: ':is(.foo, .bar) a[data-x] {}',
-				output: '.foo, .bar {\n\ta[data-x] {}\n}',
-				errors: [{messageId: 'descendantIs'}],
-			},
-			{
-				code: 'a :is(.foo, .bar) b {}',
-				output: null,
-				errors: [{messageId: 'descendantIs'}],
-			},
-			{
-				code: 'a :is(.foo, .bar).baz {}',
-				output: null,
-				errors: [{messageId: 'descendantIs'}],
-			},
-			{
-				code: 'a :is(.foo), b {}',
-				output: null,
-				errors: [{messageId: 'descendantIs'}],
-			},
-		],
-	});
+	},
+	valid: [
+		{code: 'a:is(.foo, .bar) {}'},
+		{code: '&:is(:focus, :hover) svg {}'},
+		{code: 'a :is(.foo, .bar), b {}'},
+		{code: 'a :is(.foo, .bar) b, c {}'},
+	],
+	invalid: [
+		{
+			code: 'a :is(.foo, .bar) {}',
+			output: 'a {\n\t.foo, .bar {}\n}',
+			errors: [{messageId: 'descendantIs'}],
+		},
+		{
+			code: ':is(.foo, .bar) a[data-x] {}',
+			output: '.foo, .bar {\n\ta[data-x] {}\n}',
+			errors: [{messageId: 'descendantIs'}],
+		},
+		{
+			code: ':is(.foo, .bar)::before {}',
+			output: '.foo, .bar {\n\t&::before {}\n}',
+			errors: [{messageId: 'descendantIs'}],
+		},
+		{
+			code: ':is(a, button).rgh-own-conversation {}',
+			output: 'a, button {\n\t&.rgh-own-conversation {}\n}',
+			errors: [{messageId: 'descendantIs'}],
+		},
+		{
+			code: 'a :is(.foo, .bar) b {}',
+			output: null,
+			errors: [{messageId: 'descendantIs'}],
+		},
+		{
+			code: 'a :is(.foo, .bar).baz {}',
+			output: null,
+			errors: [{messageId: 'descendantIs'}],
+		},
+	],
 });
