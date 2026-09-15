09ad9acd9f2cb88f105f166035dd4550e2ca9cfd
fregante <me@fregante.com>
2026-09-15 16:09:40 +0800

    `mark-pinned` - Restore feature (#10087)

--- source/features/mark-pinned.tsx
+++ source/features/mark-pinned.tsx
@@ -10,8 +10,7 @@ import observe from '../helpers/selector-observer.js';
 import {issueIcons} from './select-notifications.js';
 
 function mark(issueLink: HTMLAnchorElement): void {
-	// The href attribute in the pinned issue list contains the absolute URL
-	if (!elementExists(`[class*='PinnedIssues-module__container'] a[href="${issueLink.href}"]`)) {
+	if (!elementExists(`[class*='PinnedIssues-module__container'] a[href$="${issueLink.pathname}"]`)) {
 		return;
 	}
 
